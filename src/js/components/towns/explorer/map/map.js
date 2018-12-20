a0efbffa6ea8d4e657b28ce79e51c7ed850feb9d
Anis Safine Laget <anis@users.noreply.github.com>
2018-12-20 10:58:41 +0100

    Quick refactoring of the map component
    Essentially split big methods into smaller ones.

--- src/js/components/towns/explorer/map/map.js
+++ src/js/components/towns/explorer/map/map.js
@@ -3,6 +3,7 @@ import L from 'leaflet';
 export default {
     props: {
         towns: Array,
+        center: Array,
     },
     data() {
         return {
@@ -12,40 +13,57 @@ export default {
     },
     watch: {
         towns() {
-            this.updateMarkers();
+            this.syncMarkersToTowns();
         },
     },
     mounted() {
-        if (this.map !== null) {
-            return;
-        }
-
-        const layers = {
-            // Satellite: L.tileLayer(viz.leaflet.tiles.OpenStreetMap.satellite, { attribution: viz.leaflet.tiles.OpenStreetMap.attribution }),
-            Streets: L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FmaW5lYW4iLCJhIjoiY2pwODY2azc0MTh5aTNwbzM3ODRsbnNoNCJ9.9QOOO9zRHbrSAsYdh5DBNQ', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'mapbox.streets',
-                accessToken: 'pk.eyJ1Ijoic2FmaW5lYW4iLCJhIjoiY2pwODY2azc0MTh5aTNwbzM3ODRsbnNoNCJ9.9QOOO9zRHbrSAsYdh5DBNQ',
-            }),
-        };
-
-        this.map = L.map('map', { layers: [layers.Streets] }).setView([43.3050621, 0.684586], 8);
-        L.control.layers(layers).addTo(this.map);
-
-        this.updateMarkers();
+        this.map = this.createMap();
+        this.syncMarkersToTowns();
     },
     methods: {
-        updateMarkers() {
+        createMap() {
+            const map = L.map(
+                'map',
+                {
+                    layers: this.getMapLayers(),
+                },
+            );
+            map.setView(this.center, 8);
+            return map;
+        },
+        getMapLayers() {
+            return [
+                // Satellite view:
+                // L.tileLayer(
+                //     viz.leaflet.tiles.OpenStreetMap.satellite,
+                //     {
+                //         attribution: viz.leaflet.tiles.OpenStreetMap.attribution,
+                //     },
+                // ),
+
+                // Street view:
+                L.tileLayer(
+                    'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FmaW5lYW4iLCJhIjoiY2pwODY2azc0MTh5aTNwbzM3ODRsbnNoNCJ9.9QOOO9zRHbrSAsYdh5DBNQ',
+                    {
+                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                        maxZoom: 18,
+                        id: 'mapbox.streets',
+                    },
+                ),
+            ];
+        },
+        removeAllMarkers() {
             this.markers.forEach(marker => marker.remove());
             this.markers = [];
-
-            this.towns.forEach((town) => {
-                const { latitude, longitude } = town;
-                const marker = L.marker([latitude, longitude]);
-                marker.addTo(this.map);
-                this.markers.push(marker);
-            });
+        },
+        syncMarkersToTowns() {
+            this.removeAllMarkers();
+            this.towns.forEach(this.addMarker);
+        },
+        addMarker({ latitude, longitude }) {
+            const marker = L.marker([latitude, longitude]);
+            marker.addTo(this.map);
+            this.markers.push(marker);
         },
     },
 };
