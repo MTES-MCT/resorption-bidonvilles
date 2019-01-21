8bd924ef9bc03ce43631e3eda5ea4899378fe95a
Anis Safine Laget <anis@users.noreply.github.com>
2019-01-21 18:43:42 +0100

    Make the navbar responsive

--- src/js/app/layouts/navbar/navbar.js
+++ src/js/app/layouts/navbar/navbar.js
@@ -4,9 +4,16 @@ import { isLoaded as isConfigLoaded } from '#helpers/api/config';
 export default {
     data() {
         return {
+            isAsideMenuEnabled: false,
             loggedIn: isLoggedIn(),
             configLoaded: isConfigLoaded(),
         };
+    },
+    mounted() {
+        document.addEventListener('click', this.checkOutsideClick);
+    },
+    destroyed() {
+        document.removeEventListener('click', this.checkOutsideClick);
     },
     computed: {
         isInTownList() {
@@ -23,6 +30,17 @@ export default {
         },
     },
     methods: {
+        enableAsideMenu() {
+            this.isAsideMenuEnabled = true;
+        },
+        disableAsideMenu() {
+            this.isAsideMenuEnabled = false;
+        },
+        checkOutsideClick(event) {
+            if (!this.$el.contains(event.target)) {
+                this.isAsideMenuEnabled = false;
+            }
+        },
         logout() {
             logout();
             this.$router.push('/');
