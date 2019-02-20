0df8cdd83dcf6862179acfc83aa4bc5788e0ee00
Anis Safine Laget <anis@users.noreply.github.com>
2019-02-20 17:43:51 +0100

    (wording) rename feedback link

--- src/js/app/layouts/navbar/items.js
+++ src/js/app/layouts/navbar/items.js
@@ -2,16 +2,16 @@ export default {
     anonymous: [
         { label: 'Connexion', target: '/connexion' },
         { label: 'Demander un accès', target: '/demande-d-acces' },
-        { label: 'Signaler un bug', target: '/feedback' },
+        { label: 'Nous contacter', target: '/feedback' },
     ],
     loading: [
-        { label: 'Signaler un bug', target: '/feedback' },
+        { label: 'Nous contacter', target: '/feedback' },
         { label: 'Déconnexion', target: '/deconnexion' },
     ],
     loaded: [
         { label: 'Liste des sites', target: '/liste-des-sites', group: 'townList' },
         { label: 'Déclarer un site', target: '/nouveau-site', group: 'townCreation' },
-        { label: 'Signaler un bug', target: '/feedback' },
+        { label: 'Nous contacter', target: '/feedback' },
         { label: 'Déconnexion', target: '/deconnexion' },
     ],
 };
