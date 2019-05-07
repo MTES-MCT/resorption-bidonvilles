d97c479fcf91155368d5a72e1da43fc7f3a7212f
Anis Safine Laget <anis@users.noreply.github.com>
2019-05-07 17:22:20 +0200

    Add missing component

---
+++ src/js/app/pages/plans.details/plans.details.js
@@ -0,0 +1,154 @@
+import NavBar from '#app/layouts/navbar/navbar.vue';
+import { get } from '#helpers/api/plan';
+
+const fieldsByType = {
+    'Espace temporaire d’insertion': [
+        'households_affected', 'people_affected', 'children_schoolable', 'households_who_got_housing_with_help',
+        'households_who_got_housing_without_help', 'households_who_were_hosted', 'children_schooled',
+        'people_accessing_health', 'people_helped_for_employment', 'people_who_got_employment',
+        'households_domiciled', 'people_included', 'people_successfully_helped', 'people_excluded',
+        'people_who_resigned', 'average_duration',
+    ],
+    'Diagnostic social': [
+        'households', 'people', 'european_people', 'french_people', 'non_european_people',
+        'children_schoolable',
+    ],
+    'Accompagnement social global': [
+        'households_affected', 'people_affected', 'children_schoolable', 'households_who_got_housing_with_help',
+        'households_who_got_housing_without_help', 'households_who_were_hosted', 'children_schooled',
+        'people_accessing_health', 'people_helped_for_employment', 'people_who_got_employment',
+        'households_domiciled', 'people_included', 'people_successfully_helped', 'people_excluded',
+        'people_who_resigned', 'average_duration',
+    ],
+    'Intervention sanitaire': [
+        'households_affected', 'people_affected', 'people_asking_for_cmu', 'people_with_cmu',
+    ],
+    'Accompagnement scolaire': [
+        'households_affected', 'people_affected', 'young_kids', 'other_kids', 'schooled_kids',
+        'households_domiciled',
+    ],
+    'Protection de l’enfance': [
+        'minors_with_admin_procedure', 'minors_with_justice_procedure',
+    ],
+    'Accompagnement emploi': [
+        'households_affected', 'people_affected', 'people_pole_emploi', 'people_mission_locale',
+        'people_with_bank_account', 'people_trainee',
+    ],
+};
+
+export default {
+    components: {
+        NavBar,
+    },
+
+    data() {
+        return {
+            status: null,
+            error: null,
+            plan: null,
+            fieldsLabel: {
+                households_affected: 'Nombre de ménages concernés',
+                people_affected: 'Nombre de personnes concernées',
+                children_schoolable: 'Nombre d\'enfants en âge d\'être scolarisés',
+                households_who_got_housing_with_help: 'Nombre de ménages ayant accédé à un logement accompagné/adapté dans l\'année',
+                households_who_got_housing_without_help: 'Nombre de ménages ayant accédé à un logement sans accompagnement dans l\'année (parc privé/logement social)',
+                households_who_were_hosted: 'Nombre de ménages ayant été hébergés dans l\'année ( CHRS, CHU, ) hors mises à l\'abri',
+                children_schooled: 'Nombre d\'enfants agés de 3 à 16 ans scolarisés (inscrits) ',
+                people_accessing_health: 'Nombre de personnes bénéficiant d\'un accompagnement sanitaire dans l\'année',
+                people_helped_for_employment: 'Nombre de personnes bénéficiant d\'un accompagnement vers l\'emploi dans l\'année (inscription à Pôle emploi ou dans une mission locale, formation, POEC...)',
+                people_who_got_employment: 'Nombre d\'adultes accompagnés vers l\'emploi ayant accédé à l\'emploi dans l\'année  (CDD, CDI, interim, CUI, CDDI) ',
+                households_domiciled: 'Nombre de ménages domiciliés dans l\'année',
+                people_included: 'Nombre de personnes entrées dans le dispositif pendant l\'année',
+                people_successfully_helped: 'nombre de sorties considérées comme positives (insertion, logement, autonomie)',
+                people_excluded: 'Nombre de sorties sur exclusions',
+                people_who_resigned: 'Nombre d\'abandons volontaires sans insertion',
+                people_pole_emploi: 'Nombre de personnes inscrites à Pôle emploi dans l\'année',
+                people_mission_locale: 'Nombre de personnes inscrites dans une Mission locale dans l\'année',
+                people_with_bank_account: 'Nombre de personnes ayant ouvert un compte bancaire dans l\'année',
+                people_trainee: 'Nombre de personnes stagiaires de la formation professionnelle dans l\'année',
+                average_duration: 'Durée moyenne de présence dans le dispositif',
+                households: 'Nombre de ménages identifiés',
+                people: 'Nombre de personnes identifiées',
+                european_people: 'dont nombre de personnes intraUE',
+                french_people: 'dont nombre de personnes françaises',
+                non_european_people: 'dont nombre de personnes extraUE',
+                young_kids: 'Nombre d\'enfants 0-3 ans ',
+                other_kids: 'Nombre d\'enfants 3-16 ans',
+                schooled_kids: 'Nombre d\'enfants 3-16 ans scolarisés dans l\'année',
+                people_asking_for_cmu: 'Nombre de personnes ayant déposé une demande CMU et/ou AME dans l\'année',
+                people_with_cmu: 'Nombre de personnes ayant  une demande CMU et/ou AME ouverte dans l\'année',
+                minors_with_admin_procedure: 'Nombre de mineurs concernés par une mesure de protection administrative',
+                minors_with_justice_procedure: 'Nombre de mineurs concernés par une mesure de protection judiciaire',
+            },
+        };
+    },
+
+    created() {
+        this.load();
+    },
+
+    computed: {
+        customFields() {
+            if (!this.plan) {
+                return [];
+            }
+
+            return fieldsByType[this.plan.type.label];
+        },
+    },
+
+    methods: {
+        load() {
+            if (['loading', 'loaded'].indexOf(this.status) !== -1) {
+                return;
+            }
+
+            this.status = 'loading';
+            this.error = null;
+
+            get(this.$route.params.id)
+                .then((data) => {
+                    this.plan = data;
+                    this.status = 'loaded';
+                })
+                .catch(({ user_message: message }) => {
+                    this.error = message;
+                    this.status = 'loadingError';
+                });
+        },
+        numericValue(str) {
+            if (str === null || str === undefined) {
+                return 'Inconnu';
+            }
+
+            const int = parseInt(str, 10);
+            if (Number.isNaN(int)) {
+                return 'Inconnu';
+            }
+
+            return int;
+        },
+        formatDate: ts => App.formatDate(ts),
+        fromCamelCase(str) {
+            const atoms = str.split(/[A-Z]/g);
+            const camels = str.match(/[A-Z]/g);
+
+            let newStr = atoms[0];
+            for (let i = 0; i < camels.length; i += 1) {
+                newStr += `_${camels[i].toLowerCase()}${atoms[i + 1]}`;
+            }
+
+            return newStr;
+        },
+        toCamelCase(str) {
+            const atoms = str.split('_');
+
+            let newStr = atoms[0];
+            for (let i = 1; i < atoms.length; i += 1) {
+                newStr += atoms[i].slice(0, 1).toUpperCase() + atoms[i].slice(1);
+            }
+
+            return newStr;
+        },
+    },
+};
---
+++ src/js/app/pages/plans.details/plans.details.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    include plans.details.pug
+</template>
+
+<script>
+import Component from './plans.details.js';
+export default Component;
+</script>
+
+<style lang="scss" scoped>
+@import './plans.details.scss';
+</style>
