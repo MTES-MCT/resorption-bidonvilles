75ab9fc98b027c912371cee107af15ecc988db77
Anis Safine Laget <anis@users.noreply.github.com>
2019-06-03 13:54:18 +0200

    Use the  new form component for operators.create

--- src/js/app/pages/operators.create/operators.create.js
+++ src/js/app/pages/operators.create/operators.create.js
@@ -1,82 +1,55 @@
 import NavBar from '#app/layouts/navbar/navbar.vue';
-import OperatorContact from '#app/components/operatorContact/operatorContact.vue';
 import { create } from '#helpers/api/operator';
+import Form from '#app/components/form/form.vue';
 
 export default {
     components: {
         NavBar,
-        OperatorContact,
+        Form,
     },
 
     data() {
         return {
-            loading: false,
-            form: {
-                mainError: null,
-                errors: {},
-                input: {
-                    name: '',
-                    contacts: [],
-                },
+            formDefinition: {
+                title: 'Déclaration d\'un opérateur',
+                description: 'Le formulaire ci-dessous concerne uniquement la déclaration d\'opérateurs intervenant sur la question des bidonvilles. Il sera à terme élargi à d\'autres types d\'acteurs, dans la perspective de construction d\'un annuaire partagé.',
+                steps: [
+                    {
+                        title: 'Opérateur',
+                        wording: {
+                            error: 'La création de l\'opérateur a échoué',
+                            submit: 'Créer l\'opérateur',
+                        },
+                        sections: [
+                            {
+                                title: 'Caractéristiques de l\'opérateur',
+                                inputs: {
+                                    name: {
+                                        type: 'text',
+                                        label: 'Nom de l\'opérateur',
+                                        mandatory: true,
+                                    },
+                                },
+                            },
+                        ],
+                        submit: create,
+                    },
+                ],
             },
+            values: {},
         };
     },
 
-    computed: {
-        formData() {
-            return {
-                name: this.form.input.name,
-                contacts: this.form.input.contacts.map(({ data }) => data),
-            };
-        },
-    },
-
     methods: {
-        submit() {
-            if (this.loading === true) {
-                return;
-            }
-
-            this.loading = true;
-            this.mainError = null;
-            this.errors = {};
-
-            create(this.formData)
-                .then(() => {
-                    this.loading = false;
-
-                    this.$notify({
-                        group: 'notifications',
-                        type: 'success',
-                        title: 'Opérateur créé',
-                        text: `L'opérateur ${this.form.input.name} a bien été créé`,
-                    });
-
-                    this.$router.push('/liste-des-operateurs');
-                })
-                .catch(({ user_message: mainError, fields: errors }) => {
-                    this.form.mainError = mainError || null;
-                    this.form.errors = errors || {};
-                    this.loading = false;
-                });
-        },
-
-        addContact() {
-            if (this.loading === true) {
-                return;
-            }
-
-            this.form.input.contacts.push({
-                data: {},
+        onComplete() {
+            this.$notify({
+                group: 'notifications',
+                type: 'success',
+                title: 'Opérateur créé',
+                text: `L'opérateur ${this.values.name} a bien été créé`,
             });
-        },
 
-        removeContact(index) {
-            if (this.loading === true) {
-                return;
-            }
-
-            this.form.input.contacts.splice(index, 1);
+            this.$router.push('/liste-des-operateurs');
         },
     },
 };
