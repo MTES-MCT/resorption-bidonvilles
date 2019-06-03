6e04c0225a229641379d24d376516a8c3290a6e7
Anis Safine Laget <anis@users.noreply.github.com>
2019-06-03 13:32:41 +0200

    Use the  new form component for users.create

--- src/js/app/pages/users.create/users.create.js
+++ src/js/app/pages/users.create/users.create.js
@@ -2,11 +2,13 @@ import { get as getConfig } from '#helpers/api/config';
 import { create } from '#helpers/api/user';
 import NavBar from '#app/layouts/navbar/navbar.vue';
 import ActivationLinkModal from '#app/layouts/activationLink/activationLink.vue';
+import Form from '#app/components/form/form.vue';
 
 export default {
     components: {
         NavBar,
         ActivationLinkModal,
+        Form,
     },
 
     data() {
@@ -14,53 +16,88 @@ export default {
 
         return {
             /**
-             * Wheter a creation request is in progress or not
-             *
-             * @type {boolean}
+             * Form definition
              */
-            loading: false,
-
-            /**
-             * User error message, if any
-             *
-             * @type {string|null}
-             */
-            error: null,
-
-            /**
-             * List of errors, field by field
-             *
-             * @type {Object.<string,Array.<string>>}
-             */
-            fieldErrors: {},
-
-            /**
-             * List of departements
-             *
-             * @type {Array.<Departement>}
-             */
-            departements,
-
-            /**
-             * List of roles
-             *
-             * @type {Array.<Role>}
-             */
-            roles,
-
-            /**
-             * Inputs
-             *
-             * @type {Object.<string,string>}
-             */
-            input: {
-                email: '',
-                firstName: '',
-                lastName: '',
-                company: '',
-                departement: undefined,
-                role: undefined,
-                dataOwnerAgreement: false,
+            formDefinition: {
+                title: 'Création d\'un nouvel utilisateur',
+                description: 'Le formulaire suivant vous permet de créer un compte qui servira d\'accès à la plateforme pour un nouvel utilisateur.',
+                steps: [
+                    {
+                        title: 'Informations personnelles',
+                        wording: {
+                            submit: 'Créer l\'utiliateur',
+                        },
+                        sections: [
+                            {
+                                title: 'Informations personnelles',
+                                inputs: {
+                                    email: {
+                                        type: 'text',
+                                        label: 'Adresse email',
+                                        mandatory: true,
+                                    },
+                                    firstName: {
+                                        type: 'text',
+                                        label: 'Prénom',
+                                        mandatory: true,
+                                    },
+                                    lastName: {
+                                        type: 'text',
+                                        label: 'Nom de famille',
+                                        mandatory: true,
+                                    },
+                                },
+                            },
+                            {
+                                title: 'Structure d\'appartenance',
+                                inputs: {
+                                    company: {
+                                        type: 'text',
+                                        label: 'Structure',
+                                        mandatory: true,
+                                    },
+                                    departement: {
+                                        type: 'select',
+                                        label: 'Département de rattachement',
+                                        mandatory: true,
+                                        options: departements.map(({ code, name }) => ({
+                                            value: code,
+                                            label: `${code} - ${name}`,
+                                        })),
+                                    },
+                                },
+                            },
+                            {
+                                title: 'Droits et permissions',
+                                inputs: {
+                                    role: {
+                                        type: 'radio',
+                                        label: 'Rôle',
+                                        mandatory: true,
+                                        options: roles.map(({ id, name }) => ({
+                                            value: id,
+                                            label: name,
+                                        })),
+                                    },
+                                    dataOwnerAgreement: {
+                                        type: 'checkbox',
+                                        label: 'Accord du propriétaire',
+                                        mandatory: true,
+                                        options: [
+                                            {
+                                                value: true,
+                                                label: 'Je certifie que ces données personnelles ont été saisies avec l\'accord de leur propriétaire',
+                                            },
+                                        ],
+                                    },
+                                },
+                            },
+                        ],
+                        submit: data => create(Object.assign({}, data, {
+                            dataOwnerAgreement: data.dataOwnerAgreement[0] || null,
+                        })),
+                    },
+                ],
             },
 
             /**
@@ -74,27 +111,10 @@ export default {
 
     methods: {
         /**
-         * Sends a creation request to the API (and handles the response)
+         *
          */
-        submit() {
-            // avoid submitting the form twice at the same time
-            if (this.loading === true) {
-                return;
-            }
-
-            this.loading = true;
-            this.error = null;
-            this.fieldErrors = {};
-
-            create(this.input)
-                .then(({ activationLink }) => {
-                    this.activationLink = activationLink;
-                })
-                .catch((error) => {
-                    this.loading = false;
-                    this.error = error.user_message;
-                    this.fieldErrors = error.fields || {};
-                });
+        onComplete({ activationLink }) {
+            this.activationLink = activationLink;
         },
 
         /**
