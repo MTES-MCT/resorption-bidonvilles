b2038b81cb634566c16bbc84c150b07b0128cd40
Anis Safine Laget <anis@users.noreply.github.com>
2019-11-14 15:31:29 +0100

    Track directory-view events

---
+++ src/js/helpers/api/statistics.js
@@ -0,0 +1,16 @@
+import { postApi } from '#helpers/api/main';
+
+/**
+ * Registers a new directory-view event
+ *
+ * @param {Number} organization Id of the viewed organization
+ *
+ * @returns {Promise}
+ */
+export function directoryViews(organization) {
+    return postApi('/statistics/directory-views', {
+        organization,
+    });
+}
+
+export default directoryViews;
