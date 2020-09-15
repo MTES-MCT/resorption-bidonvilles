712161dbd724ba5858312c51309e0b2e2b670b04
gdestrem <gael.destrem@gmail.com>
2020-09-15 12:03:43 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            display: ['Gilroy', 'sans-serif'],
+            body: ['Graphik', 'sans-serif'],
+        },
+
+        extend: {
+            spacing: {
+                96: '24rem',
+                128: '32rem',
+            },
+            colors: {
+                primary: '#000091',
+                secondary: '#FF6F4C',
+                corail: '#FF6F4C',
+                blue: '#000091',
+            },
+        },
+    },
+    variants: {
+        opacity: ['responsive', 'hover'],
+    },
+};
