d6cc37347284987267ee4ccdb7dbf48dc8b1fb27
gdestrem <gael.destrem@gmail.com>
2020-09-15 16:37:27 +0200

    Setup typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 module.exports = {
     theme: {
         fontFamily: {
-            display: ['Gilroy', 'sans-serif'],
-            body: ['Graphik', 'sans-serif'],
+            display: ['Marianne', 'arial', 'sans-serif'],
+            body: ['Spectral', 'georgia', 'serif'],
+        },
+        fontSize: {
+            'size-display-xl': ['3rem', {
+                lineHeight: '1.25em',
+            }],
+            'size-display-lg': ['2rem', {
+                lineHeight: '1.25em',
+            }],
+            'size-display-md': ['1.5rem', {
+                lineHeight: '1.25em',
+            }],
+            'size-display-sm': ['1.25rem', {
+                lineHeight: '1.375em',
+            }],
+            'size-xl': ['1.5rem', {
+                lineHeight: '1.5em',
+            }],
+            'size-lg': ['1.25rem', {
+                lineHeight: '1.5em',
+            }],
+            'size-md': ['1rem', {
+                lineHeight: '1.5em',
+            }],
+            'size-sm': ['0.865rem', {
+                lineHeight: '1.5em',
+            }],
+            'size-xs': ['0.75rem', {
+                lineHeight: '0.75em',
+            }],
         },
 
         extend: {
@@ -14,7 +43,24 @@ module.exports = {
                 primary: '#000091',
                 secondary: '#FF6F4C',
                 corail: '#FF6F4C',
-                blue: '#000091',
+                blueFrance: '#000091',
+
+                // UI colors /  text
+                G800: '#1E1E1E',
+                G750: '#2A2A2A',
+                G700: '#383838',
+                G600: '#6A6A6A',
+                G500: '#9C9C9C',
+                G400: '#CECECE',
+                G300: '#E7E7E7',
+                G200: '#F0F0F0',
+                G100: '#F8F8F8',
+
+                // Functional colors
+                success: '#0D6635',
+                info: '#3A55D1',
+                warning: '#FA5C00',
+                error: '#B60000',
             },
         },
     },
