5fb93a14742537476bfefb3c4ee8c4a918dbcf28
gdestrem <gael.destrem@gmail.com>
2020-09-28 10:40:15 +0200

    Fix ContactForm input styles

--- src/js/app/pages/LandingPage/LandingPageContactForm.vue
+++ src/js/app/pages/LandingPage/LandingPageContactForm.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-row justify-center mt-4 w-full max-w-md">
-        <TextInput class="hidden md:block flex-1" :placeholder="$t('landingPage.contactForm.email')" />
+        <input class="hidden md:block border-2 border-G200 rounded-md flex-1 py-2 px-4 outline-none focus:border-G400" :placeholder="$t('landingPage.contactForm.email')" />
         <Button variant="secondary" class="ml-1">{{$t('landingPage.contactForm.cta')}}</Button>
     </div>
 </template>
