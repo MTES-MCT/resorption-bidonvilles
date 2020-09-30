aece6dbf4f8238cd890710a19303eafd683acd18
Anis Safine Laget <anis@users.noreply.github.com>
2020-09-30 12:42:46 +0200

    (tech) Amélioration de la structure des tests Cypress
    - split des commandes personnalisées pour avoir une seule commande par fichier
    - création de nombreuses commandes pour faciliter la manipulation de champs "originaux" (datepicker, champ d'adresse, etc.)
    - amélioration de la commande de saisie de dates dans le datepicker pour pouvoir sélectionner n'importe quelle date dynamiquement (avant on ne sélectionnait qu'un jour du mois courant)
    - création d'une commande spécifique d'asssertion pour contrôler l'état d'une fiche de site
    - gestion des cas de champs vides (soit pour ignorer la saisie du champ à la création, soit pour le vider à l'édition)
    - amélioration de l'édition pour mieux se rapprocher du comportement utilisateur : si la valeur d'un champ ne change pas, ne pas le vider pour le re-remplir avec la même valeur mais le laisser intouché
    - gestion des deux scénarios : un site déclaré avec le max d'info (full) puis modifié au minimum (min), et l'inverse

---
+++ cypress/support/commands/typeReplace.js
@@ -0,0 +1,15 @@
+/**
+ * This command replaces the content of a text input (if needed)
+ *
+ * @param {HTMLElement} subject The input
+ * @param {String}      str     New value of the input
+ *
+ * @returns {undefined}
+ */
+Cypress.Commands.add('typeReplace', { prevSubject: true }, (subject, str) => {
+    if (subject.value === str) {
+        return;
+    }
+
+    cy.wrap(subject).clear().type(str);
+});
