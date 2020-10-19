fd24bbc452a9167f4998a4c7b15d3039b407ebe7
gdestrem <gael.destrem@gmail.com>
2020-10-19 09:08:51 +0200

    Rework english translations + footer

--- src/js/app/messages/fr/index.js
+++ src/js/app/messages/fr/index.js
@@ -1,7 +1,9 @@
 import landingPage from './landingPage';
 import contactPage from './contactPage';
+import footer from './footer';
 
 export default {
     landingPage,
     contactPage,
+    footer
 };
