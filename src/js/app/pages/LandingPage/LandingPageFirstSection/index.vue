1be226fd1580451aca2326675c17638c2773a12f
gdestrem <gael.destrem@gmail.com>
2020-10-21 08:34:18 +0200

    Reposition landing page elements

--- src/js/app/pages/LandingPage/LandingPageFirstSection/index.vue
+++ src/js/app/pages/LandingPage/LandingPageFirstSection/index.vue
@@ -7,18 +7,19 @@
             </LandingPageResponsiveEmbed>
         </div>
         <div>
-            <div class="md:grid md:grid-cols-3 gap-16 mt-16">
-                <LandingPageFeatureBlock icon="map-marked-alt">{{$t('landingPage.firstSection.features.position')}}</LandingPageFeatureBlock>
-                <LandingPageFeatureBlock class="my-8 md:my-0" icon="chart-line">{{$t('landingPage.firstSection.features.actions')}}</LandingPageFeatureBlock>
-                <LandingPageFeatureBlock icon="comments">{{$t('landingPage.firstSection.features.exchange')}}</LandingPageFeatureBlock>
-            </div>
-            <div class="mt-24">
+          <div class="mt-24">
+              <div class="max-w-screen-sm mx-auto">
                 <div class="text-display-lg font-normal">{{$t('landingPage.firstSection.context.title')}}</div>
                 <div class="text-display-lg">{{$t('landingPage.firstSection.context.subtitle')}}</div>
-                <div class="max-w-screen-sm mx-auto mt-4">
-                    <div>{{$t('landingPage.firstSection.context.text')}}</div>
-                </div>
-            </div>
+                 <div class="mt-4">{{$t('landingPage.firstSection.context.text')}}</div>
+              </div>
+          </div>
+
+          <div class="md:grid md:grid-cols-3 gap-16 mt-16">
+            <LandingPageFeatureBlock icon="map-marked-alt">{{$t('landingPage.firstSection.features.position')}}</LandingPageFeatureBlock>
+            <LandingPageFeatureBlock class="my-8 md:my-0" icon="chart-line">{{$t('landingPage.firstSection.features.actions')}}</LandingPageFeatureBlock>
+            <LandingPageFeatureBlock icon="comments">{{$t('landingPage.firstSection.features.exchange')}}</LandingPageFeatureBlock>
+          </div>
 
             <div class="md:flex md:flex-row mt-16">
                 <LandingPageContextImage :label="$t('landingPage.firstSection.before')">
