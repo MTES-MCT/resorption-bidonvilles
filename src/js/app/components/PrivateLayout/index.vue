f4bdb55608ec40b0135bbe7f2942b6b3b48cfe40
gdestrem <gael.destrem@gmail.com>
2020-10-30 14:53:19 +0100

    Add PrivateContainer component

---
+++ src/js/app/components/PrivateLayout/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="v1">
+            <NavBar />
+        </div>
+        <slot />
+    </div>
+</template>
+
+<script>
+import NavBar from "#app/layouts/navbar/navbar.vue";
+
+export default {
+    components: {
+        NavBar
+    }
+};
+</script>
