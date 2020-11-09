34f2340f314b85d62a890a5c1a1550c309a4f005
gdestrem <gael.destrem@gmail.com>
2020-11-09 12:24:54 +0100

    Fix CTA `voir tous les sites` on autocomplete

--- src/js/app/pages/TownsList/TownsListSearchBar.vue
+++ src/js/app/pages/TownsList/TownsListSearchBar.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="mx-auto searchbox">
-        <form @submit.prevent="onSubmit">
-            <AutocompleteV2
-                id="test"
-                :defaultValue="result ? resultValue(result) : ''"
-                :search="search"
-                v-model="result"
-                @submit="$emit('input', $event)"
-                :getResultValue="resultValue"
-                :loading="loading"
-                prefixIcon="search"
-                :inputClasses="['rounded-full shadow-sm']"
-            />
-            <div class="-mt-6 px-2 text-right">
-                <Button variant="primaryText" class="text-display-sm"
-                    >Voir tous les sites de France</Button
-                >
-            </div>
-        </form>
+        <AutocompleteV2
+            id="test"
+            :defaultValue="result ? resultValue(result) : ''"
+            :search="search"
+            v-model="result"
+            @submit="$emit('input', $event)"
+            :getResultValue="resultValue"
+            :loading="loading"
+            prefixIcon="search"
+            :inputClasses="['rounded-full shadow-sm']"
+        >
+            <template v-slot:extra="{ removeItem }">
+                <div class=" px-2 text-right">
+                    <Button
+                        variant="primaryText"
+                        class="text-display-sm"
+                        @click="removeItem"
+                        >Voir tous les sites de France</Button
+                    >
+                </div>
+            </template>
+        </AutocompleteV2>
     </div>
 </template>
 
