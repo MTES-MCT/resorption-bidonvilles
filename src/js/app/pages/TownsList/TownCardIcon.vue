708d6e683bc78ca8bf5f2cfab2df44878996586e
gdestrem <gael.destrem@gmail.com>
2020-12-01 15:01:28 +0100

    Fix TownCard feedbacks

--- src/js/app/pages/TownsList/TownCardIcon.vue
+++ src/js/app/pages/TownsList/TownCardIcon.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="flex items-center">
-        <div class="mr-2">
-            <Icon class="text-green" icon="check" v-if="value === true" />
-            <Icon
-                class="text-red"
-                icon="times"
-                v-if="value === false || value === null"
-            />
+    <div :class="['flex items-center', color]">
+        <div class="mr-2 w-4">
+            <Icon icon="check" v-if="value === true" />
+            <Icon icon="times" v-if="value === false || value === null" />
         </div>
         <slot />
     </div>
@@ -17,6 +13,15 @@ export default {
     props: {
         value: {
             validator: prop => typeof prop === "boolean" || prop === null
+        }
+    },
+    computed: {
+        color() {
+            if (this.value === true) {
+                return "text-green";
+            }
+
+            return "text-red";
         }
     }
 };
