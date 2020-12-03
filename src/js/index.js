b2ac4c3bc92a98f7abbdbf7d2143ceca963201b8
Christophe BENARD <ch.benard@gmail.com>
2020-12-03 21:37:10 +0100

    (658) Désactiver Matomo hors PROD

--- src/js/index.js
+++ src/js/index.js
@@ -98,36 +98,39 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.use(TrendChart);
 Vue.use(VueRouter);
 Vue.use(VueI18n);
-Vue.use(VueMatomo, {
-    // Configure your matomo server and site by providing
-    host: "https://stats.data.gouv.fr",
-    siteId: 86,
+if (process.env.MATOMO_ON != null) {
+    if (process.env.MATOMO_ON === "true") {
+        Vue.use(VueMatomo, {
+            // Configure your matomo server and site by providing
+            host: "https://stats.data.gouv.fr",
+            siteId: 86,
 
-    // Changes the default .js and .php endpoint's filename
-    // Default: 'piwik'
-    trackerFileName: "piwik",
+            // Changes the default .js and .php endpoint's filename
+            // Default: 'piwik'
+            trackerFileName: "piwik",
 
-    // Enables automatically registering pageviews on the router
-    router,
+            // Enables automatically registering pageviews on the router
+            router,
 
-    // Enables link tracking on regular links. Note that this won't
-    // work for routing links (ie. internal Vue router links)
-    // Default: true
-    enableLinkTracking: true,
+            // Enables link tracking on regular links. Note that this won't
+            // work for routing links (ie. internal Vue router links)
+            // Default: true
+            enableLinkTracking: true,
 
-    // Require consent before sending tracking information to matomo
-    // Default: false
-    requireConsent: false,
+            // Require consent before sending tracking information to matomo
+            // Default: false
+            requireConsent: false,
 
-    // Whether to track the initial page view
-    // Default: true
-    trackInitialView: true,
+            // Whether to track the initial page view
+            // Default: true
+            trackInitialView: true,
 
-    // Whether or not to log debug information
-    // Default: false
-    debug: true
-});
-
+            // Whether or not to log debug information
+            // Default: false
+            debug: true
+        });
+    }
+}
 // Register styleguide components
 registerGlobalComponents(Vue);
 
