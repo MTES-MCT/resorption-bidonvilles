78014f0b7562a2161d5f423bb28752471f6c61a5
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-09 18:19:59 +0100

    (629) Ajout d'une aide sur le champ de la population

--- src/js/app/components/TownForm/inputs/InputPopulation.vue
+++ src/js/app/components/TownForm/inputs/InputPopulation.vue
@@ -1,29 +1,36 @@
 <template>
-    <div class="flex">
-        <TextInput
-            id="population_total"
-            label="Personnes"
-            class="w-32"
-            v-model="input.populationTotal"
-            rules="numeric|min_value:0"
-            cypressName="population_total"
-        ></TextInput>
-        <TextInput
-            id="population_couples"
-            label="Ménages"
-            class="w-32 ml-5"
-            v-model="input.populationCouples"
-            rules="numeric|min_value:0|couplesLesserThanTotal:@population_total"
-            cypressName="population_couples"
-        ></TextInput>
-        <TextInput
-            id="population_minors"
-            label="Mineurs"
-            class="w-32 ml-5"
-            v-model="input.populationMinors"
-            rules="numeric|min_value:0|minorsLesserThanTotal:@population_total"
-            cypressName="population_minors"
-        ></TextInput>
+    <div>
+        <div class="mb-3 italic text-G600">
+            Le nombre de personnes sur un site est mouvant, les données fournies
+            par les acteurs - même des estimations - participent à
+            l'amélioration de la connaissance.
+        </div>
+        <div class="flex">
+            <TextInput
+                id="population_total"
+                label="Personnes"
+                class="w-32"
+                v-model="input.populationTotal"
+                rules="numeric|min_value:0"
+                cypressName="population_total"
+            ></TextInput>
+            <TextInput
+                id="population_couples"
+                label="Ménages"
+                class="w-32 ml-5"
+                v-model="input.populationCouples"
+                rules="numeric|min_value:0|couplesLesserThanTotal:@population_total"
+                cypressName="population_couples"
+            ></TextInput>
+            <TextInput
+                id="population_minors"
+                label="Mineurs"
+                class="w-32 ml-5"
+                v-model="input.populationMinors"
+                rules="numeric|min_value:0|minorsLesserThanTotal:@population_total"
+                cypressName="population_minors"
+            ></TextInput>
+        </div>
     </div>
 </template>
 
