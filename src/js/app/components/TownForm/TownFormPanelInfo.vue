133d56decaa2486cf52262c4a1fb9803e18ba87b
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-09 13:06:05 +0100

    (629) Wording 'Qu'est-ce qu'un site'

--- src/js/app/components/TownForm/TownFormPanelInfo.vue
+++ src/js/app/components/TownForm/TownFormPanelInfo.vue
@@ -4,14 +4,12 @@
         <span class="ml-2 font-bold">Qu'est-ce qu'un site ?</span>
         <div class="bg-yellow-200 text-sm px-6 py-3 mt-2 mb-10 flex">
             <p class="py-3">
-                Ce formulaire vous permet de déclarer un site identifié sur
-                votre territoire (bidonville ou squat). Il est divisé en quatre
-                onglets : caractéristiques, habitants, conditions de vie et
-                procédure judiciaire d'expulsion des occupants. Lorsque le
-                symbole (*) apparaît en rouge au début de l'intitulé de la
-                question, cela signifie qu'il s'agit d'une question obligatoire.
-                Il n'est pas possible de valider le formulaire sans y avoir
-                répondu.
+                Un site est un bidonville, squat ou immeuble bâti occupé de
+                manière informelle à des fins d'habitation par plusieurs
+                familles ou personnes. Il se caractérise généralement par
+                l'absence de services de base : eau, électricité, gestion des
+                déchets. La plateforme permet de recenser tous les sites,
+                quelque soit l'origine des personnes et leur nombre.
             </p>
             <span class="text-lg cursor-pointer" @click="$emit('close')"
                 ><Icon icon="times"></Icon
