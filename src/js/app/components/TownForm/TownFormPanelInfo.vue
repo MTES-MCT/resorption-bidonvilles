48ecc0d89da90cecb1c57264a3fe16571a258e9a
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-09 17:18:16 +0100

    (629) Le panel info peut s'ouvrir et fermer

--- src/js/app/components/TownForm/TownFormPanelInfo.vue
+++ src/js/app/components/TownForm/TownFormPanelInfo.vue
@@ -2,18 +2,58 @@
     <div>
         <Icon icon="flag"></Icon>
         <span class="ml-2 font-bold">Qu'est-ce qu'un site ?</span>
-        <div class="bg-yellow-200 text-sm px-6 py-3 mt-2 mb-10 flex">
-            <p class="py-3">
+        <span class="ml-2" @click="toggle"
+            ><Icon :icon="togglerIcon" class="cursor-pointer"></Icon
+        ></span>
+        <transition name="toggle" mode="out-in">
+            <div class="bg-yellow-200 text-sm p-6 mt-2 flex" v-if="visible">
                 Un site est un bidonville, squat ou immeuble bâti occupé de
                 manière informelle à des fins d'habitation par plusieurs
                 familles ou personnes. Il se caractérise généralement par
                 l'absence de services de base : eau, électricité, gestion des
                 déchets. La plateforme permet de recenser tous les sites,
                 quelque soit l'origine des personnes et leur nombre.
-            </p>
-            <span class="text-lg cursor-pointer" @click="$emit('close')"
-                ><Icon icon="times"></Icon
-            ></span>
-        </div>
+            </div>
+        </transition>
     </div>
 </template>
+
+<style scoped>
+.toggle-enter-active,
+.toggle-leave-active {
+    transition: all 0.3s;
+    max-height: 500px;
+}
+
+.toggle-enter,
+.toggle-leave-to {
+    opacity: 0;
+    max-height: 0px;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            visible: true
+        };
+    },
+
+    computed: {
+        togglerIcon() {
+            if (this.visible === true) {
+                return "chevron-up";
+            }
+
+            return "chevron-down";
+        }
+    },
+
+    methods: {
+        toggle() {
+            this.visible = !this.visible;
+        }
+    }
+};
+</script>
