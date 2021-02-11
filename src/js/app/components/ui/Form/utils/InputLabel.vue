3890bd40ddecb73664a71182d2d461d68a7776da
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-11 15:19:13 +0100

    (629) Ajustement design du formulaire

--- src/js/app/components/ui/Form/utils/InputLabel.vue
+++ src/js/app/components/ui/Form/utils/InputLabel.vue
@@ -3,7 +3,7 @@
         <div v-if="label" class="font-bold">
             {{ label }} <MandatoryStar v-if="showMandatoryStar"></MandatoryStar>
         </div>
-        <div v-if="info" class="mb-3 italic text-G600">{{ info }}</div>
+        <div v-if="info" class="mb-3 text-G600">{{ info }}</div>
     </div>
 </template>
 
