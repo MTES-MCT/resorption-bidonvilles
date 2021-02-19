324740c2e1a625b150421b75074a8f4e3023cd00
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-19 14:54:43 +0100

    (691) Ajout d'une tooltip sur le bouton d'ajout de champs d'intervention

---
+++ src/js/app/components/ui/Tooltip.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="has-tooltip">
+        <div class="tooltip mt-8 bg-yellow-200 text-black py-4 px-6">
+            {{ text }}
+        </div>
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String
+        }
+    }
+};
+</script>
+
+<style scoped>
+.tooltip {
+    @apply invisible absolute;
+}
+
+.has-tooltip:hover .tooltip {
+    @apply visible z-50;
+}
+</style>
