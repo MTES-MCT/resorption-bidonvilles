51d6a2989a0718d3a8b2fdc6b6fc1e77feab2b4e
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-19 12:08:15 +0100

    (691) Ajout d'un lien 'Je n'interviens plus sur ce site'

--- src/js/app/pages/TownDetails/ui/TownDetailsSelfCard.vue
+++ src/js/app/pages/TownDetails/ui/TownDetailsSelfCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-blue100 p-4 flex">
         <div class="mr-2"><Icon icon="user" /></div>
-        <div>
+        <div class="flex-grow">
             <p class="font-bold">
                 {{ actor.first_name }} {{ actor.last_name.toUpperCase() }}
             </p>
@@ -14,6 +14,16 @@
                     {{ theme.value || themes[theme.id] }}
                 </TownDetailsSelfTag>
             </div>
+            <div class="w-100 mt-4 text-right">
+                <p
+                    class="text-primary hover:text-primaryDark focus:outline-none font-bold cursor-pointer"
+                >
+                    <span v-if="!loading" @click="removeSelf"
+                        >Je n'interviens plus sur ce site</span
+                    >
+                    <Icon v-else icon="spinner" spin />
+                </p>
+            </div>
         </div>
     </div>
 </template>
@@ -21,22 +31,62 @@
 <script>
 import TownDetailsSelfTag from "./TownDetailsSelfTag.vue";
 import { get as getConfig } from "#helpers/api/config";
+import { notify } from "#helpers/notificationHelper";
 
 export default {
     props: {
         actor: {
             type: Object,
-            required: false
+            required: true
+        },
+        townId: {
+            type: Number,
+            required: true
         }
     },
     data() {
         const { actor_themes: themes } = getConfig();
         return {
+            loading: false,
             themes
         };
     },
     components: {
         TownDetailsSelfTag
+    },
+    methods: {
+        async removeSelf() {
+            if (this.loading === true) {
+                return;
+            }
+
+            this.loading = true;
+
+            try {
+                await this.$store.dispatch("removeTownActor", {
+                    townId: this.townId,
+                    userId: this.actor.id
+                });
+
+                notify({
+                    group: "notifications",
+                    type: "success",
+                    title: "Succès",
+                    text: "Vous avez été retiré(e) des intervenants "
+                });
+            } catch (error) {
+                notify({
+                    group: "notifications",
+                    type: "error",
+                    title: "Échec",
+                    text:
+                        (error && error.user_message) ||
+                        "Une erreur inconnue est survenue"
+                });
+            }
+
+            this.loading = false;
+        }
     }
 };
 </script>
