c90df26f0e0c43a3bdc54bbe464a2b92b5ce516d
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-19 15:26:11 +0100

    (691) Ajout des intervenants à la liste des sites

--- src/js/icons/index.js
+++ src/js/icons/index.js
@@ -9,6 +9,7 @@ import {
     faPaperPlane,
     faUnlink,
     faUser,
+    faUserCircle,
     faUserCheck,
     faUserShield,
     faArrowRight,
@@ -106,6 +107,7 @@ library.add(faHistory);
 library.add(faMapPin);
 library.add(faFilePdf);
 library.add(faUsers);
+library.add(faUserCircle);
 library.add(faUserPlus);
 library.add(faFileDownload);
 library.add(faAddressBook);
