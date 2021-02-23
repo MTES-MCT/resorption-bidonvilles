700337e9d6b4d4165de5fb29e7406e8e12a7a32b
Christophe BENARD <ch.benard@gmail.com>
2021-02-23 18:46:31 +0100

    (641) Simplification diu formulaire d'invitation + alternative aec cartes pour chaque invité

---
+++ src/js/app/pages/Invitation/InvitationCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <span>
+            {{ guest.firstname }} {{ guest.lastname }} {{ guest.email }}
+        </span>
+    </div>
+</template>
+<script>
+export default {
+    name: "InvitationCard",
+    props: ["guest"],
+    methods: {
+        deleteGuest() {
+            this.$emit("deleteGuest", this.guest);
+        }
+    }
+};
+</script>
