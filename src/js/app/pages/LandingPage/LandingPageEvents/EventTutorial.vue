6038de2472d7236f4084ea7ae36721e0b193346f
Christophe BENARD <ch.benard@gmail.com>
2021-02-24 18:10:53 +0100

    (801) Ajout de deux bandeaux events sur la landing page

---
+++ src/js/app/pages/LandingPage/LandingPageEvents/EventTutorial.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="full-width h-auto mx-auto mt-8 mb-16 bg-orange300 rounded-lg overflow-hidden"
+    >
+        <div class="lg:flex">
+            <div class="lg:flex-shrink-0 justify-center">
+                <img class="m-4 h-40" src="../assets/Formation.png" alt="" />
+            </div>
+            <div class="flex-shrink-0 lg:flex-shrink px-8 py-2 lg:py-8 lg:px-4">
+                <div class="tracking-wide text-display-md text-primary">
+                    Formation personnalisée
+                </div>
+                <p class="mt-2 text-md">
+                    Prenez en main rapidement la plateforme
+                    <em>Résorption-bidonvilles</em> grâce à une présentation
+                    personnalisée. Nous répondrons à toutes vos questions.
+                </p>
+            </div>
+            <div class="px-8 py-2 lg:p-8">
+                <a
+                    href="https://app.evalandgo.com/s/index.php?a=JTk2cCU5N2slOUElQjA=&id=JTk4ayU5QW4lOTYlQUY="
+                    target="_blank"
+                >
+                    <Button class="my-2" variant="secondary">S'inscrire</Button>
+                </a>
+                <p class="text-sm font-bold">Les jeudi à 10h</p>
+                <p class="text-sm font-bold">et vendredi à 14h - 45 min</p>
+            </div>
+        </div>
+    </div>
+</template>
