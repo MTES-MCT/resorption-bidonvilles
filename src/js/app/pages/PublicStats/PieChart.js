8f185c2d0036af53708511b6fe3bb332c0f1ce6c
gdestrem <gael.destrem@gmail.com>
2021-03-09 17:16:50 +0100

    (673) Add PieChart for organization repartition

--- src/js/app/pages/PublicStats/PieChart.js
+++ src/js/app/pages/PublicStats/PieChart.js
@@ -1,8 +1,8 @@
-import { Line, mixins } from "vue-chartjs";
+import { Pie, mixins } from "vue-chartjs";
 const { reactiveProp } = mixins;
 
 export default {
-    extends: Line,
+    extends: Pie,
     mixins: [reactiveProp],
     props: ["options", "chartData"],
     mounted() {
