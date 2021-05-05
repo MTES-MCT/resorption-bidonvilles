08b8682dd1a82a12929cbff032e90ff0b8c008b7
Christophe BENARD <ch.benard@gmail.com>
2021-05-05 11:59:10 +0200

    (984) Ajout d'un contrôle sur la date pour éviter l'affichage de la bannière après la deadline

---
+++ src/js/app/pages/TownsList/FundingRequestBanner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex items-stretch border-solid mb-8">
+        <Button
+            icon="external-link-alt"
+            class="justify-self-start"
+            variant="specialEvent"
+            iconPosition="left"
+            >Faire une demande de crédits 2021</Button
+        >
+        <div class="justify-self-start ml-4 mt-2 italic">
+            <strong>Adressez vos demandes dès maintenant</strong> et jusqu'au
+            vendredi 21 mai 2021 via
+            <a
+                class="link"
+                target="_blank"
+                href="https://www.demarches-simplifiees.fr/"
+                >démarches simplifiées</a
+            >.
+        </div>
+    </div>
+</template>
