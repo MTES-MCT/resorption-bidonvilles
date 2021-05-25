abe80c6c0384b1e7687aa8da71fb466f9d859147
gdestrem <gael.destrem@gmail.com>
2021-05-25 10:43:23 +0200

    #minor (1052) Bandeau event sur landing page

--- src/js/app/pages/LandingPage/LandingPageEvents/EventTutorial.vue
+++ src/js/app/pages/LandingPage/LandingPageEvents/EventTutorial.vue
@@ -2,29 +2,36 @@
     <div
         class="full-width h-auto mx-auto mt-8 mb-16 bg-orange300 rounded-lg overflow-hidden"
     >
-        <div class="lg:flex">
+        <div class="lg:flex items-center">
             <div class="lg:flex-shrink-0 justify-center">
-                <img class="m-4 h-40" src="../assets/Formation.png" alt="" />
+                <img class="h-48 m-4" src="../assets/Webinar.jpeg" alt="" />
             </div>
             <div class="flex-shrink-0 lg:flex-shrink px-8 py-2 lg:py-8 lg:px-4">
                 <div class="tracking-wide text-display-md text-primary">
-                    Formation personnalisée
+                    Instruction du 25 janvier 2018 sur la résorption des
+                    bidonvilles, où en est-on 3 ans après ?
                 </div>
-                <p class="mt-2 text-md">
-                    Prenez en main rapidement la plateforme
-                    <em>Résorption-bidonvilles</em> grâce à une présentation
-                    personnalisée. Nous répondrons à toutes vos questions.
-                </p>
+                <div class="mt-2">
+                    Un bilan d'étape organisé par la Dihal où 3 acteurs
+                    livreront également leurs analyses.
+                </div>
+                <div class="mt-2 text-md">
+                    Informations et inscription :
+                    <a
+                        class="text-primary"
+                        href="https://hello.idealco.fr/inscription-formation-resorptionbidonvilles/"
+                        >https://hello.idealco.fr/inscription-formation-resorptionbidonvilles/</a
+                    >
+                </div>
             </div>
             <div class="px-8 py-2 lg:p-8">
                 <a
-                    href="https://app.evalandgo.com/s/index.php?a=JTk2cCU5N2slOUElQjA=&id=JTk4ayU5QW4lOTYlQUY="
+                    href="https://hello.idealco.fr/inscription-formation-resorptionbidonvilles/"
                     target="_blank"
                 >
                     <Button class="my-2" variant="secondary">S'inscrire</Button>
                 </a>
-                <p class="text-sm font-bold">Les lundi à 14h</p>
-                <p class="text-sm font-bold">et jeudi à 10h - 45 min</p>
+                <p class="text-sm font-bold">Lundi 31 mai de 15h à 16h30</p>
             </div>
         </div>
     </div>
