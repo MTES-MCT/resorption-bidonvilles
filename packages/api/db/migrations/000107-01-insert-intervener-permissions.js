e765ad397826ffc5f8d4d3992f7ae10c591d0d53
Christophe BENARD <ch.benard@gmail.com>
2021-06-02 09:16:21 +0200

    (1025) Fixing lint errors in 000107-01-insert-intervener-permissions.js

--- packages/api/db/migrations/000107-01-insert-intervener-permissions.js
+++ packages/api/db/migrations/000107-01-insert-intervener-permissions.js
@@ -3,7 +3,7 @@ const permissions = {
         intervener: {
             shantytown: {
                 list: { level: 'local', data: { data_justice: false } },
-                read: { level: 'local', data: { data_justice: false } }
+                read: { level: 'local', data: { data_justice: false } },
             },
             plan: {
                 list: { level: 'local', data: { data_finances: false } },
@@ -29,61 +29,61 @@ module.exports = {
                 transaction,
             },
         )
-        .then(() => queryInterface.sequelize.query(
-            'INSERT INTO organization_categories(uid, name_singular, name_plural) VALUES(\'intervener\', \'Intervenant\', \'Intervenants\')',
-            {
-                transaction,
-            },
-        ))
-        .then(() => queryInterface.sequelize.query(
-            'INSERT INTO organization_types(name_singular, name_plural, fk_category, fk_role) VALUES(\'Intervenant\', \'Intervenants\', \'intervener\', \'intervener\')',
-            {
-                transaction,
-            },
-        ))
-        .then(async () => {
-            const p = Object.keys(permissions).reduce((promises, roleType) => {
-                Object.keys(permissions[roleType]).forEach((roleId) => {
-                    Object.keys(permissions[roleType][roleId]).forEach((entity) => {
-                        Object.keys(permissions[roleType][roleId][entity]).forEach((feature) => {
-                            promises.push(
-                                queryInterface.sequelize.query(
-                                    `INSERT INTO
-                                        permissions(fk_organization, fk_role_admin, fk_role_regular, fk_feature, fk_entity, allowed, fk_geographic_level)
-                                    VALUES
-                                        (NULL, :adminId, :regularId, :feature, :entity, TRUE, :level)
-                                    RETURNING permission_id`,
-                                    {
-                                        transaction,
-                                        replacements: {
-                                            adminId: roleType === 'roles_admin' ? roleId : null,
-                                            regularId: roleType === 'roles_regular' ? roleId : null,
-                                            feature,
-                                            entity,
-                                            level: permissions[roleType][roleId][entity][feature].level,
-                                        },
-                                    },
-                                )
-                                    .then(([[{ permission_id: permissionId }]]) => queryInterface.bulkInsert(
-                                        `${entity}_permissions`,
-                                        [
-                                            Object.assign({ fk_permission: permissionId }, permissions[roleType][roleId][entity][feature].data || {}),
-                                        ],
+            .then(() => queryInterface.sequelize.query(
+                'INSERT INTO organization_categories(uid, name_singular, name_plural) VALUES(\'intervener\', \'Intervenant\', \'Intervenants\')',
+                {
+                    transaction,
+                },
+            ))
+            .then(() => queryInterface.sequelize.query(
+                'INSERT INTO organization_types(name_singular, name_plural, fk_category, fk_role) VALUES(\'Intervenant\', \'Intervenants\', \'intervener\', \'intervener\')',
+                {
+                    transaction,
+                },
+            ))
+            .then(async () => {
+                const p = Object.keys(permissions).reduce((promises, roleType) => {
+                    Object.keys(permissions[roleType]).forEach((roleId) => {
+                        Object.keys(permissions[roleType][roleId]).forEach((entity) => {
+                            Object.keys(permissions[roleType][roleId][entity]).forEach((feature) => {
+                                promises.push(
+                                    queryInterface.sequelize.query(
+                                        `INSERT INTO
+                                            permissions(fk_organization, fk_role_admin, fk_role_regular, fk_feature, fk_entity, allowed, fk_geographic_level)
+                                        VALUES
+                                            (NULL, :adminId, :regularId, :feature, :entity, TRUE, :level)
+                                        RETURNING permission_id`,
                                         {
                                             transaction,
+                                            replacements: {
+                                                adminId: roleType === 'roles_admin' ? roleId : null,
+                                                regularId: roleType === 'roles_regular' ? roleId : null,
+                                                feature,
+                                                entity,
+                                                level: permissions[roleType][roleId][entity][feature].level,
+                                            },
                                         },
-                                    )),
-                            );
+                                    )
+                                        .then(([[{ permission_id: permissionId }]]) => queryInterface.bulkInsert(
+                                            `${entity}_permissions`,
+                                            [
+                                                Object.assign({ fk_permission: permissionId }, permissions[roleType][roleId][entity][feature].data || {}),
+                                            ],
+                                            {
+                                                transaction,
+                                            },
+                                        )),
+                                );
+                            });
                         });
                     });
-                });
 
-                return promises;
-            }, []);
+                    return promises;
+                }, []);
 
-            return Promise.all(p);
-        },
-    )),
+                return Promise.all(p);
+            }),
+    ),
     down: queryInterface => queryInterface.sequelize.transaction(
         transaction => queryInterface.bulkDelete(
             'permissions',
@@ -94,33 +94,33 @@ module.exports = {
                 transaction,
             },
         )
-        .then(() => queryInterface.bulkDelete(
-            'organization_types',
-            {
-                fk_role: 'intervener',
-                fk_category: 'intervener'
-            },
-            {
-                transaction,
-            },
-        ))
-        .then(() => queryInterface.bulkDelete(
-            'organization_categories',
-            {
-                uid: 'intervener',
-            },
-            {
-                transaction,
-            },
-        ))
-        .then(() => queryInterface.bulkDelete(
-            'roles_regular',
-            {
-                role_id: 'intervener',
-            },
-            {
-                transaction,
-            },
-        )),
-    )
+            .then(() => queryInterface.bulkDelete(
+                'organization_types',
+                {
+                    fk_role: 'intervener',
+                    fk_category: 'intervener',
+                },
+                {
+                    transaction,
+                },
+            ))
+            .then(() => queryInterface.bulkDelete(
+                'organization_categories',
+                {
+                    uid: 'intervener',
+                },
+                {
+                    transaction,
+                },
+            ))
+            .then(() => queryInterface.bulkDelete(
+                'roles_regular',
+                {
+                    role_id: 'intervener',
+                },
+                {
+                    transaction,
+                },
+            )),
+    ),
 };
