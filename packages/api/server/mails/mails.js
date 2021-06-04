712d156723d654cbed2a6e3e6487afd71ae7f571
gdestrem <gael.destrem@gmail.com>
2021-06-04 12:27:56 +0200

    #minor (1035) Delete old mails

--- packages/api/server/mails/mails.js
+++ packages/api/server/mails/mails.js
@@ -1,4 +1,4 @@
-const { send: sendMail } = require('#server/services/mailService');
+const mailService = require('#server/services/mailService');
 const { frontUrl } = require('#server/config');
 
 const generateTrackingUTM = require('./generateTrackingUTM');
@@ -30,7 +30,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(ADMIN_CAMPAIGN, 'nouvelle-demande-acces');
 
-        return sendMail('admin_new_request_notification', {
+        return mailService.send('admin_new_request_notification', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -55,7 +55,7 @@ module.exports = {
             'nouvelle-demande-acces-3j',
         );
 
-        return sendMail('admin_request_pending_reminder_1', {
+        return mailService.send('admin_request_pending_reminder_1', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -79,7 +79,7 @@ module.exports = {
             'nouvelle-demande-acces-10j',
         );
 
-        return sendMail('admin_request_pending_reminder_2', {
+        return mailService.send('admin_request_pending_reminder_2', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -100,7 +100,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(ADMIN_CAMPAIGN, 'acces-active');
 
-        return sendMail('admin_access_activated', {
+        return mailService.send('admin_access_activated', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -122,7 +122,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(ADMIN_CAMPAIGN, 'acces-expire');
 
-        return sendMail('admin_access_expired', {
+        return mailService.send('admin_access_expired', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -148,7 +148,7 @@ module.exports = {
             'confirmation-demande-acces',
         );
 
-        return sendMail('user_access_request_confirmation', {
+        return mailService.send('user_access_request_confirmation', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -167,7 +167,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(REQUESTER_CAMPAIGN, 'activer-mon-compte');
 
-        return sendMail('user_access_granted', {
+        return mailService.send('user_access_granted', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -191,7 +191,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(REQUESTER_CAMPAIGN, 'acces-refuse');
 
-        return sendMail('user_access_denied', {
+        return mailService.send('user_access_denied', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -216,7 +216,7 @@ module.exports = {
             'activer-mon-compte-48h',
         );
 
-        return sendMail('user_access_pending', {
+        return mailService.send('user_access_pending', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -241,7 +241,7 @@ module.exports = {
             'acces-expire',
         );
 
-        return sendMail('user_access_expired', {
+        return mailService.send('user_access_expired', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -262,7 +262,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'acces-active');
 
-        return sendMail('user_access_activated_welcome', {
+        return mailService.send('user_access_activated_welcome', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -285,7 +285,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(INVITE_CAMPAIGN, 'invitation-plateforme');
 
-        return sendMail('platform_invitation', {
+        return mailService.send('platform_invitation', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -308,7 +308,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(INVITE_CAMPAIGN, 'demo-invitation');
 
-        return sendMail('demo_invitation', {
+        return mailService.send('demo_invitation', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -329,7 +329,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'invitation-intervenant');
 
-        return sendMail('shantytown_actor_invitation', {
+        return mailService.send('shantytown_actor_invitation', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -354,7 +354,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'invitation-intervenant');
 
-        return sendMail('shantytown_actor_notification', {
+        return mailService.send('shantytown_actor_notification', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -379,7 +379,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(ADMIN_CAMPAIGN, 'bienvenue');
 
-        return sendMail('admin_welcome', {
+        return mailService.send('admin_welcome', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -401,7 +401,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(INVITE_CAMPAIGN, 'invitation-idealco');
 
-        return sendMail('user_idealco_invitation', {
+        return mailService.send('user_idealco_invitation', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -422,7 +422,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'features');
 
-        return sendMail('user_features', {
+        return mailService.send('user_features', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -443,7 +443,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'partage-plateforme');
 
-        return sendMail('user_share', {
+        return mailService.send('user_share', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -464,7 +464,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'nouveau-commentaire');
 
-        return sendMail('user_new_comment', {
+        return mailService.send('user_new_comment', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -488,7 +488,7 @@ module.exports = {
 
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'nouveau-mot-de-passe');
 
-        return sendMail('user_new_password', {
+        return mailService.send('user_new_password', {
             recipient,
             variables: {
                 recipientName: `${recipient.first_name} ${recipient.last_name}`,
@@ -508,7 +508,7 @@ module.exports = {
         const { variables, preserveRecipient } = options;
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'nouveau-mot-de-passe');
 
-        return sendMail('admin_contact_message', {
+        return mailService.send('admin_contact_message', {
             recipient,
             variables: {
                 backUrl,
@@ -527,7 +527,7 @@ module.exports = {
         const { variables, preserveRecipient } = options;
         const utm = generateTrackingUTM(USER_CAMPAIGN, 'suppression-commentaire');
 
-        return sendMail('user_comment_deletion', {
+        return mailService.send('user_comment_deletion', {
             recipient,
             variables: {
                 town: variables.town,
--- packages/api/test/suites/server/services/shantytownComment/createComment.spec.js
+++ packages/api/test/suites/server/services/shantytownComment/createComment.spec.js
@@ -106,9 +106,12 @@ describe.only('services/shantytownComment', () => {
 
             it('envoie une notification mail', () => {
                 expect(dependencies.sendMail.callCount).to.be.eql(3);
-                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment', output.watchers[0], undefined, [input.shantytown, output.comment], false);
-                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment', output.watchers[1], undefined, [input.shantytown, output.comment], false);
-                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment', output.watchers[2], undefined, [input.shantytown, output.comment], false);
+                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment',
+                    { recipient: output.watchers[0], variables: { shantytown: input.shantytown, comment: output.comment } });
+                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment',
+                    { recipient: output.watchers[1], variables: { shantytown: input.shantytown, comment: output.comment } });
+                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment',
+                    { recipient: output.watchers[2], variables: { shantytown: input.shantytown, comment: output.comment } });
             });
 
             it('collecte et retourne la liste des commentaires actualisés', async () => {
