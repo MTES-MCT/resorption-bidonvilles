15e307e180acabf1173bc280c9d52f744f638063
Christophe BENARD <ch.benard@gmail.com>
2021-06-18 09:35:33 +0200

    (1100) Modification de l'image de la bannière Formation sur landing

--- packages/frontend/src/js/app/pages/LandingPage/LandingTutorialBanner.vue
+++ packages/frontend/src/js/app/pages/LandingPage/LandingTutorialBanner.vue
@@ -4,7 +4,11 @@
     >
         <div class="lg:flex">
             <div class="lg:flex-shrink-0 justify-center">
-                <img class="m-4 h-40" src="./assets/Formation.png" alt="" />
+                <img
+                    class="m-4 h-40"
+                    src="./assets/linkedin-sales-solutions-1LyBcHrH4J8-unsplash-BD.jpg"
+                    alt=""
+                />
             </div>
             <div class="flex-shrink-0 lg:flex-shrink px-8 py-2 lg:py-8 lg:px-4">
                 <div class="tracking-wide text-display-md text-primary">
