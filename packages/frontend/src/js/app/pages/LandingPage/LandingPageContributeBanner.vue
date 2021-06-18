6642eec3fe700cbd6cb348a31824bd7dd8eafcda
Christophe BENARD <ch.benard@gmail.com>
2021-06-18 09:28:46 +0200

    (1100) - Ajout d'une bannière temporaire Je contribue ! sur la page d'accueil

---
+++ packages/frontend/src/js/app/pages/LandingPage/LandingPageContributeBanner.vue
@@ -0,0 +1,52 @@
+<template>
+    <div
+        class="full-width h-auto mx-auto mt-16 mb-4 bg-blue200 rounded-lg overflow-hidden"
+        v-if="shouldBeVisible"
+    >
+        <div class="lg:flex">
+            <div class="lg:flex-shrink-0 justify-center">
+                <img
+                    class="m-4 h-40"
+                    src="./assets/ernesto-velazquez-0Uyvgcnl83g-unsplash-BD.jpg"
+                    alt=""
+                />
+            </div>
+            <div class="flex-shrink-0 lg:flex-shrink px-8 py-2 lg:py-8 lg:px-4">
+                <div class="tracking-wide text-display-md text-primary">
+                    Contribuez à la préparation de la stratégie française en
+                    réponse au cadre européen 2020-2030 pour l'&laquo;inclusion
+                    des Roms&raquo;
+                </div>
+                <a
+                    href="https://www.gouvernement.fr/la-dihal-va-lancer-une-concertation-interministerielle-pour-l-elaboration-d-une-strategie-nationale"
+                    class="link"
+                    target="blank"
+                    >En savoir plus</a
+                >
+            </div>
+            <div class="px-8 py-2 lg:p-8">
+                <a
+                    href="https://eur-lex.europa.eu/legal-content/FR-EN/TXT/?from=EN&uri=CELEX%3A32021H0319%2801%29"
+                    target="_blank"
+                >
+                    <Button class="my-2" size="md" variant="primary"
+                        ><div class="whitespace-nowrap">
+                            Je contribue
+                        </div></Button
+                    >
+                </a>
+                <p class="text-sm font-bold">Dès maintenant</p>
+                <p class="text-sm font-bold">jusqu'en spetembre 2021</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    computed: {
+        shouldBeVisible() {
+            return new Date() < new Date("2021-08-31");
+        }
+    }
+};
+</script>
