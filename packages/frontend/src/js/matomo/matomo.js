5f8fcac4e6578097aa5e66501ca6fae139a84618
gdestrem <gael.destrem@gmail.com>
2021-07-09 15:59:42 +0200

    #patch (1119) Add this.$trackMatomoEvent function

--- packages/frontend/src/js/matomo/matomo.js
+++ packages/frontend/src/js/matomo/matomo.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
+import { get as getConfig } from "#helpers/api/config";
 
 const defaultOptions = {
     debug: false,
@@ -10,7 +11,7 @@ const defaultOptions = {
 };
 
 function loadScript(trackerScript) {
-    const scriptPromise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const script = document.createElement("script");
         script.async = true;
         script.defer = true;
@@ -22,15 +23,55 @@ function loadScript(trackerScript) {
         script.onload = resolve;
         script.onerror = reject;
     });
+}
 
-    scriptPromise.catch(error => {
-        const msg = `[vue-matomo] An error occurred trying to load ${error.target.src}. `;
-        ("If the file exists you may have an ad- or trackingblocker enabled.");
+export function setCustomVariables(user) {
+    if (!this || !this.$piwik) {
+        return;
+    }
 
-        console.error(msg);
-    });
+    this.$piwik.setUserId(user.id);
 
-    return scriptPromise;
+    const location =
+        user.organization.location[user.organization.location.type];
+
+    // Convert user's data to a easily parsable string:
+    // ie: superuser:false,is_admin:false,role:association,org_category:association,org_location_type:departement,org_location_name:Gironde,org_location_code:33
+    const userData = JSON.stringify({
+        superuser: user.is_superuser,
+        is_admin: user.is_admin,
+        role: user.role_id,
+        org_category: user.organization.category.uid,
+        org_location_type: user.organization.location.type,
+        org_location_name: location?.name,
+        org_location_code: location?.code
+    })
+        .replaceAll('"', "")
+        .replaceAll("{", "")
+        .replaceAll("}", "");
+
+    this.$piwik.setCustomVariable(1, "user", userData);
+
+    const departement = user.organization.location.departement || null;
+    this.$piwik.setCustomVariable(
+        5,
+        "departement_code",
+        departement ? departement.code : null
+    );
+}
+
+function trackEvent(eventCategory, eventName, eventArgs) {
+    if (!this.piwik) {
+        return;
+    }
+
+    const { user } = getConfig();
+
+    if (user) {
+        setCustomVariables(user);
+    }
+
+    this.$piwik.trackEvent(eventCategory, eventName, eventArgs);
 }
 
 function initMatomo(Vue, options) {
@@ -42,6 +83,7 @@ function initMatomo(Vue, options) {
     // Assign matomo to Vue
     Vue.prototype.$piwik = Matomo;
     Vue.prototype.$matomo = Matomo;
+    Vue.prototype.$trackMatomoEvent = trackEvent;
 
     if (options.requireConsent) {
         Matomo.requireConsent();
@@ -92,5 +134,14 @@ export default function install(Vue, setupOptions = {}) {
     const { host, trackerFileName } = options;
     const trackerScript = `${host}/${trackerFileName}.js`;
 
-    loadScript(trackerScript).then(() => initMatomo(Vue, options));
+    loadScript(trackerScript)
+        .then(() => initMatomo(Vue, options))
+        .catch(error => {
+            const msg = `[vue-matomo] An error occurred trying to load ${error.target.src}. `;
+            ("If the file exists you may have an ad- or trackingblocker enabled.");
+
+            console.error(msg);
+
+            Vue.prototype.$trackMatomoEvent = () => {};
+        });
 }
