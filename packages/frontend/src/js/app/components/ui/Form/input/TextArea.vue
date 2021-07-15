acf6e94cb5a3be4a2c448ff8f9373d4d5dd65945
Christophe BENARD <ch.benard@gmail.com>
2021-07-15 07:44:40 +0200

    (1107) **Expected String, got Number for prop rows** corrected in ui/form/input/TextArea.vue

--- packages/frontend/src/js/app/components/ui/Form/input/TextArea.vue
+++ packages/frontend/src/js/app/components/ui/Form/input/TextArea.vue
@@ -80,7 +80,7 @@ export default {
             type: String
         },
         rows: {
-            type: String
+            type: [String, Array]
         },
         cols: {
             type: String
