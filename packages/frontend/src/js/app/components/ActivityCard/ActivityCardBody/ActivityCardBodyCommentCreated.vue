c0023b785f19aa0af8805b573424d2445a878ec4
Anis Safine Laget <anis@users.noreply.github.com>
2021-07-23 00:36:24 +0200

    (1046) Add tags to covid comment activity cards

--- packages/frontend/src/js/app/components/ActivityCard/ActivityCardBody/ActivityCardBodyCommentCreated.vue
+++ packages/frontend/src/js/app/components/ActivityCard/ActivityCardBody/ActivityCardBodyCommentCreated.vue
@@ -1,12 +1,27 @@
 <template>
     <span v-if="variant === 'small'"></span>
-    <p v-else>
-        <span class="font-bold">Message :</span> {{ activity.content }}
-    </p>
+    <div v-else>
+        <div>
+            <CovidTag
+                v-for="tag in covidTags"
+                :key="tag.prop"
+                :class="['mr-2', 'mb-2']"
+                :tag="tag"
+            />
+        </div>
+        <p><span class="font-bold">Message :</span> {{ activity.content }}</p>
+    </div>
 </template>
 
 <script>
-module.exports = {
+import CovidTag from "#app/components/CovidTag/CovidTag.vue";
+import covidTags from "#app/pages/covid/covidTags";
+
+export default {
+    components: {
+        CovidTag
+    },
+
     props: {
         activity: {
             type: Object,
@@ -16,6 +31,18 @@ module.exports = {
             // voir "ActivityCard.vue"
             type: String,
             required: true
+        }
+    },
+
+    computed: {
+        covidTags: function() {
+            if (!this.activity.covid) {
+                return [];
+            }
+
+            return covidTags.filter(t => {
+                return !!this.activity.covid[t.prop];
+            });
         }
     }
 };
