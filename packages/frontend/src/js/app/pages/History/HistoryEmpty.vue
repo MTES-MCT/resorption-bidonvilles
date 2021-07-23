90b89b2c20bbeb90962d0df5597a573f63ff9636
Anis Safine Laget <anis@users.noreply.github.com>
2021-07-23 13:28:59 +0200

    (1046) Wording

--- packages/frontend/src/js/app/pages/History/HistoryEmpty.vue
+++ packages/frontend/src/js/app/pages/History/HistoryEmpty.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-G600 italic pt-16">
-        Aucun site ne correspond à votre recherche, essayez d'autres filtres ou
-        périmètre géographique
+        Aucune activité ne correspond à votre recherche, essayez d'autres
+        filtres ou périmètre géographique
     </div>
 </template>
