e906aea03e49685929d40480473d0c4e474f3dcc
Anis Safine Laget <anis@users.noreply.github.com>
2021-07-26 11:40:59 +0200

    (1046) Remove History sort

--- packages/frontend/src/js/app/pages/History/HistoryFilterBar.vue
+++ packages/frontend/src/js/app/pages/History/HistoryFilterBar.vue
@@ -1,18 +1,15 @@
 <template>
     <div class="flex justify-between">
         <HistoryFilters></HistoryFilters>
-        <HistorySort></HistorySort>
     </div>
 </template>
 
 <script>
 import HistoryFilters from "./HistoryFilters.vue";
-import HistorySort from "./HistorySort.vue";
 
 export default {
     components: {
-        HistoryFilters,
-        HistorySort
+        HistoryFilters
     }
 };
 </script>
