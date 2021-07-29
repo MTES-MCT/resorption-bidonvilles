1f1b7985bf4bcccccfc078d8f9d94af61d8bd182
Anis Safine Laget <anis@users.noreply.github.com>
2021-07-29 21:19:11 +0200

    (1114) Update contact form to match foreign languages
    - do not redirect to the invitation page if not french
    - do not show the "register to the newsletter" option if not french
    - remove unused localized text, and add mising ones

--- packages/frontend/src/js/app/pages/Contact/index.vue
+++ packages/frontend/src/js/app/pages/Contact/index.vue
@@ -94,6 +94,7 @@
                                 checkValue="register-newsletter"
                                 :label="$t('contactPage.registerNewsletter')"
                                 v-model="commonFields.request_type"
+                                v-if="isFrenchLocale"
                                 variant="card"
                             />
                         </CheckableGroup>
@@ -441,15 +442,20 @@ export default {
                     this.$trackMatomoEvent("Contact", "Demande d'information");
                     from = "contact_others";
                 }
-                this.$router.push(
-                    `/invitation?email=${encodeURIComponent(
-                        this.commonFields.email
-                    )}&first_name=${encodeURIComponent(
-                        this.commonFields.first_name
-                    )}&last_name=${encodeURIComponent(
-                        this.commonFields.last_name
-                    )}&from=${from}`
-                );
+
+                if (this.isFrenchLocale) {
+                    this.$router.push(
+                        `/invitation?email=${encodeURIComponent(
+                            this.commonFields.email
+                        )}&first_name=${encodeURIComponent(
+                            this.commonFields.first_name
+                        )}&last_name=${encodeURIComponent(
+                            this.commonFields.last_name
+                        )}&from=${from}`
+                    );
+                } else {
+                    this.$router.push("/");
+                }
 
                 notify({
                     group: "notifications",
