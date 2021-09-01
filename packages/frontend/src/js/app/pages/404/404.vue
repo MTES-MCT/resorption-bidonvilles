25d5cc3f92c34cdcb719d7280a634cb3db8b62f2
Anis Safine Laget <anis@users.noreply.github.com>
2021-09-01 13:18:25 +0200

    (905) Add a page 404

---
+++ packages/frontend/src/js/app/pages/404/404.vue
@@ -0,0 +1,19 @@
+<template>
+    <PrivateLayout>
+        <LoadingError>
+            La page demandée n'existe pas
+        </LoadingError>
+    </PrivateLayout>
+</template>
+
+<script>
+import PrivateLayout from "#app/components/PrivateLayout";
+import LoadingError from "#app/components/PrivateLayout/LoadingError.vue";
+
+export default {
+    components: {
+        PrivateLayout,
+        LoadingError
+    }
+};
+</script>
