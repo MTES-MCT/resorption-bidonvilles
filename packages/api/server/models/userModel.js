394c8a5a4b54a9dc2e1a459529841281c5a08a48
Anis Safine Laget <anis@users.noreply.github.com>
2021-09-09 10:25:42 +0200

    (1223) Fix activity filtering on TownsList

--- packages/api/server/models/userModel.js
+++ packages/api/server/models/userModel.js
@@ -916,7 +916,17 @@ module.exports = () => {
                     lua.used_at AS "date",
                     users.first_name,
                     users.last_name,
-                    organizations.organization_id
+                    organizations.organization_id,
+                    organizations.location_type,
+                    organizations.region_code,
+                    organizations.region_name,
+                    organizations.departement_code,
+                    organizations.departement_name,
+                    organizations.epci_code,
+                    organizations.epci_name,
+                    organizations.city_code,
+                    organizations.city_name,
+                    organizations.city_main
                 FROM last_user_accesses lua
                 LEFT JOIN users ON lua.fk_user = users.user_id
                 LEFT JOIN localized_organizations organizations ON users.fk_organization = organizations.organization_id
@@ -937,6 +947,26 @@ module.exports = () => {
                 user: {
                     name: model.formatName(activity),
                     organization: activity.organization_id,
+                    location: {
+                        type: activity.location_type,
+                        region: activity.region_code !== null ? {
+                            code: activity.region_code,
+                            name: activity.region_name,
+                        } : null,
+                        departement: activity.departement_code !== null ? {
+                            code: activity.departement_code,
+                            name: activity.departement_name,
+                        } : null,
+                        epci: activity.epci_code !== null ? {
+                            code: activity.epci_code,
+                            name: activity.epci_name,
+                        } : null,
+                        city: activity.city_code !== null ? {
+                            code: activity.city_code,
+                            name: activity.city_name,
+                            main: activity.city_main,
+                        } : null,
+                    },
                 },
             }));
     };
