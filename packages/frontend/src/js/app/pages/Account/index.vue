cca6294c052676cd69deeb06a50f03be74a27d6b
gdestrem <gael.destrem@gmail.com>
2021-09-10 10:25:30 +0200

    #patch (1203) Handle error on load + change of userId

--- packages/frontend/src/js/app/pages/Account/index.vue
+++ packages/frontend/src/js/app/pages/Account/index.vue
@@ -1,12 +1,18 @@
 <template>
     <PrivateLayout class="mb-16">
-        <AccountRead v-if="!edit" :user="user" @openEdit="edit = true" />
-        <AccountEdit v-else :user="user" @cancelEdit="edit = false" />
+        <LoadingError v-if="error || (!user && !loading)">
+            L'utilisateur n'existe pas en base de données.
+        </LoadingError>
+        <div v-else>
+            <AccountRead v-if="!edit" :user="user" @openEdit="edit = true" />
+            <AccountEdit v-else :user="user" @cancelEdit="edit = false" />
+        </div>
     </PrivateLayout>
 </template>
 
 <script>
 import PrivateLayout from "#app/components/PrivateLayout";
+import LoadingError from "#app/components/PrivateLayout/LoadingError.vue";
 import AccountRead from "./AccountRead/AccountRead";
 import AccountEdit from "./AccountEdit/AccountEdit";
 import { get as getConfig } from "#helpers/api/config";
@@ -15,25 +21,40 @@ import { get as getUser } from "#helpers/api/user";
 export default {
     components: {
         PrivateLayout,
+        LoadingError,
         AccountRead,
         AccountEdit
     },
     data() {
-        const { user: connectedUser } = getConfig();
-
         return {
-            user: !this.$route.params.id ? connectedUser : null,
+            user: null,
             edit: false,
+            loading: false,
             error: null
         };
     },
-    async mounted() {
-        if (!this.user && this.$route.params.id) {
+    methods: {
+        async load() {
+            this.loading = true;
+            const { user: connectedUser } = getConfig();
+
             try {
-                this.user = await getUser(this.$route.params.id);
+                this.user = this.$route.params.id
+                    ? await getUser(this.$route.params.id)
+                    : connectedUser;
             } catch (err) {
                 this.error = err;
             }
+            this.loading = false;
+        }
+    },
+    mounted() {
+        this.load();
+    },
+    // If user id changes, reload infos
+    watch: {
+        "$route.params.id"() {
+            this.load();
         }
     }
 };
