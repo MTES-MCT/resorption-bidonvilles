1acea12618a1b2b5087e31d13bc3e3f5df45066e
Christophe BENARD <christophe.benard@developpement-durable.gouv.fr>
2021-09-16 18:35:51 +0200

    (1204) New EventPopup component added

--- packages/frontend/src/js/app/pages/NewsPopup/NewsPopup.vue
+++ packages/frontend/src/js/app/pages/NewsPopup/NewsPopup.vue
@@ -2,32 +2,41 @@
     <div>
         <div
             class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50"
-            v-if="toggleModal"
+            v-if="popupEvent.toggleModal && shouldBeVisible"
         >
             <div
                 class="bg-white relative mx-auto w-auto max-w-2xl border-2 border-black"
             >
                 <div class="p-8 w-full flex flex-col">
-                    <Button variant="primaryText" @click="toggleModal = false">
+                    <Button
+                        variant="primaryText"
+                        @click="$emit('updateToggleModal')"
+                    >
                         Fermer X
                     </Button>
-                    <div class="text-display-md text-secondary">Evènement</div>
+                    <div class="text-display-md text-secondary">
+                        {{ popupEvent.title }}
+                    </div>
                     <div class="text-display-md text-blue-600">
-                        jeudi 23 septembre de 10h à 12h
+                        {{ popupEvent.text }}
                     </div>
                     <img
                         class="mt-4"
-                        src="./assets/webinaire_acces_a_l_eau.jpg"
+                        :src="require(`./assets/${popup.imgName}`)"
                     />
                 </div>
                 <div class="mx-8 mt-4 mb-8 flex justify-between">
-                    <Button>En savoir plus</Button>
-                    <Button variant="secondary">Je m'inscris</Button>
+                    <a :href="popup.infoLink" target="_blank">
+                        <Button>En savoir plus</Button>
+                    </a>
+                    <a :href="popup.joinLink" target="_blank">
+                        <Button variant="secondary">Je m'inscris</Button>
+                    </a>
                 </div>
             </div>
         </div>
         <div
-            v-if="toggleModal"
+            v-if="popupEvent.toggleModal && shouldBeVisible"
             class="absolute inset-0 z-40 opacity-50 bg-black"
         ></div>
     </div>
@@ -35,10 +44,21 @@
 <script>
 export default {
     name: "NewsPopup",
+    props: {
+        popup: {
+            type: Object,
+            required: true
+        }
+    },
     data() {
         return {
-            toggleModal: true
+            popupEvent: { ...this.popup }
         };
+    },
+    computed: {
+        shouldBeVisible() {
+            return new Date() < new Date(this.popupEvent.maxDate);
+        }
     }
 };
 </script>
