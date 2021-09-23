70d5d50a408460cf559ad042415c21d30b218344
Christophe BENARD <ch.benard@gmail.com>
2021-09-23 18:42:58 +0200

    (1234) Adds userComment validator to validators index

--- packages/api/server/middlewares/validators/index.js
+++ packages/api/server/middlewares/validators/index.js
@@ -13,6 +13,7 @@ const invite = require('./invite');
 const createShantytownComment = require('./shantytownComment/create');
 const activityList = require('./activity/list');
 const findNearbyTowns = require('./findNearbyTowns');
+const userComments = require('./userComments');
 
 module.exports = {
     closeTown,
@@ -36,4 +37,5 @@ module.exports = {
     activity: {
         list: activityList,
     },
+    userComments,
 };
