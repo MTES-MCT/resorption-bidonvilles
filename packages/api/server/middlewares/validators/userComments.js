e6256428b486909112d9a11fbbb82f1feeeaf423
Christophe BENARD <ch.benard@gmail.com>
2021-09-23 18:48:14 +0200

    (1234) New validator to check user comment updates

---
+++ packages/api/server/middlewares/validators/userComments.js
@@ -0,0 +1,26 @@
+const { body, param } = require('express-validator');
+const { sequelize } = require('#db/models');
+const userModel = require('#server/models/userModel')(sequelize);
+
+module.exports = [
+
+    body('comment')
+        .trim(),
+
+    param('id')
+        .toInt()
+        .isInt().bail()
+        .withMessage('L\'identifiant du site est invalide')
+        .custom(async (value) => {
+            let user = null;
+            try {
+                user = await userModel.findOne(value, { auth: true });
+            } catch (error) {
+                throw new Error('Erreur lors du contrôle de l\'existence de l\'utlisateur à partir de so identifiant');
+            }
+
+            if (user === null) {
+                throw new Error('L\'utlisateur n\'existe pas en base de données');
+            }
+        }),
+];
