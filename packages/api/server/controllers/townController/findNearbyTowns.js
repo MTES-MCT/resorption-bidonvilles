9d29d46313ce16bbccad15b0cb770cd35a941966
gdestrem <gael.destrem@gmail.com>
2021-09-27 15:06:35 +0200

    #patch (1237) Restrict access to non authorized shantytowns

--- packages/api/server/controllers/townController/findNearbyTowns.js
+++ packages/api/server/controllers/townController/findNearbyTowns.js
@@ -2,7 +2,7 @@
 module.exports = models => async (req, res, next) => {
     try {
         const { latitude, longitude, distance = 1 } = req.query;
-        const towns = await models.shantytown.findNearby(latitude, longitude, distance);
+        const towns = await models.shantytown.findNearby(req.user, latitude, longitude, distance);
         return res.status(200).send({ towns });
     } catch (error) {
         res.status(500).send({
