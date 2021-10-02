99e81084e2151a8b64e232d065bf8102c242d773
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-02 07:25:59 +0200

    (1248) Split up etpTypeModel

--- packages/api/server/models/etpTypeModel/index.js
+++ packages/api/server/models/etpTypeModel/index.js
@@ -1,11 +1,5 @@
-module.exports = database => ({
-    findAll: () => database.query(
-        `SELECT
-            etp_types.uid AS uid,
-            etp_types.name AS name
-        FROM etp_types`,
-        {
-            type: database.QueryTypes.SELECT,
-        },
-    ),
+const findAll = require('./findAll');
+
+module.exports = () => ({
+    findAll,
 });
