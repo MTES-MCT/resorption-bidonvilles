972571ca1485170786a372d12139ed4c8888d231
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-02 07:56:12 +0200

    (1248) Split up ownerTypeModel

---
+++ packages/api/server/models/ownerTypeModel/findOne.js
@@ -0,0 +1,25 @@
+const { sequelize } = require('#db/models');
+
+module.exports = async (id) => {
+    const rows = await sequelize.query(
+        `SELECT
+            owner_types.owner_type_id AS id,
+            owner_types.label AS label,
+            owner_types.position AS position
+        FROM owner_types
+        WHERE owner_types.owner_type_id = :id
+        ORDER BY position ASC`,
+        {
+            type: sequelize.QueryTypes.SELECT,
+            replacements: {
+                id,
+            },
+        },
+    );
+
+    if (rows.length !== 1) {
+        return null;
+    }
+
+    return rows[0];
+};
--- packages/api/server/models/ownerTypeModel/index.js
+++ packages/api/server/models/ownerTypeModel/index.js
@@ -1,36 +1,7 @@
-module.exports = database => ({
-    findAll: () => database.query(
-        `SELECT
-            owner_types.owner_type_id AS id,
-            owner_types.label AS label,
-            owner_types.position AS position
-        FROM owner_types
-        ORDER BY position ASC`,
-        {
-            type: database.QueryTypes.SELECT,
-        },
-    ),
-    findOne: async (id) => {
-        const rows = await database.query(
-            `SELECT
-                owner_types.owner_type_id AS id,
-                owner_types.label AS label,
-                owner_types.position AS position
-            FROM owner_types
-            WHERE owner_types.owner_type_id = :id
-            ORDER BY position ASC`,
-            {
-                type: database.QueryTypes.SELECT,
-                replacements: {
-                    id,
-                },
-            },
-        );
+const findAll = require('./findAll');
+const findOne = require('./findOne');
 
-        if (rows.length !== 1) {
-            return null;
-        }
-
-        return rows[0];
-    },
+module.exports = () => ({
+    findAll,
+    findOne,
 });
