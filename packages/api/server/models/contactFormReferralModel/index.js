26d2c48062de57923487a9ed5703a77ff4c20159
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-02 07:19:21 +0200

    (1248) Split up contactFormReferralModel

--- packages/api/server/models/contactFormReferralModel/index.js
+++ packages/api/server/models/contactFormReferralModel/index.js
@@ -1,32 +1,5 @@
-module.exports = database => ({
-    async create({
-        reason,
-        reason_other = null,
-        reason_word_of_mouth = null,
-        fk_user = null,
-    }, transaction = undefined) {
-        const result = await database.query(
-            `INSERT INTO contact_form_referrals(
-                reason,
-                reason_other,
-                reason_word_of_mouth,
-                fk_user
-            ) VALUES (
-                :reason,
-                :reason_other,
-                :reason_word_of_mouth,
-                :fk_user
-            ) RETURNING contact_form_referral_id AS id`, {
-                replacements: {
-                    reason,
-                    reason_other,
-                    reason_word_of_mouth,
-                    fk_user,
-                },
-                transaction,
-            },
-        );
+const create = require('./create');
 
-        return result[0][0].id;
-    },
+module.exports = () => ({
+    create,
 });
