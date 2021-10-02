00702d0c2e48eb6e7dca5716300c059044bdd6af
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-02 07:30:38 +0200

    (1248) Split up fundingTypeModel

---
+++ packages/api/server/models/fundingTypeModel/findAll.js
@@ -0,0 +1,11 @@
+const { sequelize } = require('#db/models');
+
+module.exports = () => sequelize.query(
+    `SELECT
+        funding_types.funding_type_id AS id,
+        funding_types.label AS label
+    FROM funding_types`,
+    {
+        type: sequelize.QueryTypes.SELECT,
+    },
+);
--- packages/api/server/models/fundingTypeModel/index.js
+++ packages/api/server/models/fundingTypeModel/index.js
@@ -1,11 +1,5 @@
-module.exports = database => ({
-    findAll: () => database.query(
-        `SELECT
-            funding_types.funding_type_id AS id,
-            funding_types.label AS label
-        FROM funding_types`,
-        {
-            type: database.QueryTypes.SELECT,
-        },
-    ),
+const findAll = require('./findAll');
+
+module.exports = () => ({
+    findAll,
 });
