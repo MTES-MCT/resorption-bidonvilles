cf586f60a19a9e1fc6b3aafaab85be0e8c9b0299
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-02 07:29:37 +0200

    (1248) Split up financeTypeModel

--- packages/api/server/models/financeTypeModel/index.js
+++ packages/api/server/models/financeTypeModel/index.js
@@ -1,11 +1,5 @@
-module.exports = database => ({
-    findAll: () => database.query(
-        `SELECT
-            finance_types.uid AS uid,
-            finance_types.name AS name
-        FROM finance_types`,
-        {
-            type: database.QueryTypes.SELECT,
-        },
-    ),
+const findAll = require('./findAll');
+
+module.exports = () => ({
+    findAll,
 });
