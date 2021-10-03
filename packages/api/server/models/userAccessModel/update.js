7ef48ae90c358856f79958cf7b732a243f627069
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-03 17:27:24 +0200

    (1248) Split up userAccessModel

---
+++ packages/api/server/models/userAccessModel/update.js
@@ -0,0 +1,30 @@
+const { sequelize } = require('#db/models');
+
+module.exports = (user_access_id, data, transaction = undefined) => {
+    const params = ['used_at', 'sent_by'];
+
+    const query = params
+        // for each supported param, look if a new value is provided in object "data"
+        .reduce((acc, name) => {
+            if (data[name] === undefined) {
+                return acc;
+            }
+
+            return [...acc, `${name} = :${name}`];
+        }, [])
+        // join each query into a single string
+        .join(', ');
+
+    return sequelize.query(
+        `UPDATE user_accesses
+        SET
+            ${query}
+        WHERE user_access_id = :user_access_id`,
+        {
+            replacements: Object.assign({}, data, {
+                user_access_id,
+            }),
+            transaction,
+        },
+    );
+};
