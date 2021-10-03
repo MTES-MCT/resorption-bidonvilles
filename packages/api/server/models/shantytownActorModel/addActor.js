7e50d8525a024cc67b38a326e0b6ce59d6391e96
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-03 16:50:24 +0200

    (1248) Split up shantytownActorModel

---
+++ packages/api/server/models/shantytownActorModel/addActor.js
@@ -0,0 +1,32 @@
+const { sequelize } = require('#db/models');
+const processThemes = require('./_common/processThemes');
+
+module.exports = (shantytownId, userId, themes, createdBy, transaction = undefined) => {
+    const replacements = {
+        ...processThemes(themes),
+        fk_shantytown: shantytownId,
+        fk_user: userId,
+        created_by: createdBy,
+    };
+
+    return sequelize.query(
+        `INSERT INTO shantytown_actors
+            (
+                fk_shantytown,
+                fk_user,
+                themes,
+                autre,
+                created_by
+            )
+        VALUES (
+            :fk_shantytown,
+            :fk_user,
+            ARRAY[${replacements.themes.map(id => `'${id}'`).join(',')}]::enum_shantytown_actors_themes[],
+            :autre,
+            :created_by
+        )`, {
+            replacements,
+            transaction,
+        },
+    );
+};
