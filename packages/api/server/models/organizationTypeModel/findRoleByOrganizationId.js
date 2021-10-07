6f855e3cc8beed0ec829dc36f29c7fe6d4653e9b
Christophe BENARD <ch.benard@gmail.com>
2021-10-07 15:47:41 +0200

    (1165) findRoleByOrganizationId implementation added to oragnizationTypeModel

---
+++ packages/api/server/models/organizationTypeModel/findRoleByOrganizationId.js
@@ -0,0 +1,23 @@
+const { sequelize } = require('#db/models');
+
+module.exports = async (organization_id) => {
+    const result = await sequelize.query(
+        `SELECT
+            fk_role
+        FROM
+            organization_types ot
+        LEFT JOIN
+            organizations o
+                ON o.fk_type = ot.organization_type_id 
+        WHERE
+            o.organization_id = :organization_id`,
+        {
+            type: sequelize.QueryTypes.SELECT,
+            replacements: {
+                organization_id,
+            },
+        },
+    );
+
+    return result.length === 1 ? result[0].fk_role : null;
+};
