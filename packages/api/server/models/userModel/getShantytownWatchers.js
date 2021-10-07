d9676bcf8c6eb79dcf136998d06283502cb1c76a
Anis Safine Laget <anis@users.noreply.github.com>
2021-10-07 10:24:49 +0200

    (1251) Fix getShantytownWatchers()

--- packages/api/server/models/userModel/getShantytownWatchers.js
+++ packages/api/server/models/userModel/getShantytownWatchers.js
@@ -1,20 +1,25 @@
 const { sequelize } = require('#db/models');
 
-module.exports = async (shantytownId, includePrivate) => sequelize.query(
+module.exports = async (shantytownId, canListPrivateComments) => sequelize.query(
     `
         SELECT
-            u.email,
-            u.first_name,
-            u.last_name
-        FROM shantytown_watchers sw
-        LEFT JOIN users u ON sw.fk_user = u.user_id
-        LEFT JOIN user_actual_permissions up ON sw.fk_user = up.user_id
-        WHERE
-                sw.fk_shantytown = :shantytownId
-            AND up.fk_feature = 'listPrivate'
-            AND up.fk_entity = 'shantytown_comment'
-            ${includePrivate === true ? 'AND up.allowed = true' : ''}
-        `,
+            email,
+            first_name,
+            last_name
+        FROM (
+            SELECT
+                u.email,
+                u.first_name,
+                u.last_name,
+                ARRAY_AGG(up.fk_feature || '.' || up.fk_entity || '.' || up.allowed) AS permissions
+            FROM shantytown_watchers sw
+            LEFT JOIN users u ON sw.fk_user = u.user_id
+            LEFT JOIN user_actual_permissions up ON sw.fk_user = up.user_id
+            WHERE sw.fk_shantytown = :shantytownId
+            GROUP BY u.email, u.first_name, u.last_name
+        ) t
+        ${canListPrivateComments === true ? 'WHERE t.permissions @> \'{"listPrivate.shantytown_comment.true"}\'' : ''}
+    `,
     {
         type: sequelize.QueryTypes.SELECT,
         replacements: {
