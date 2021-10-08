3aac27b8bffaaec135169642edce1c13d8b1b2a6
Christophe BENARD <ch.benard@gmail.com>
2021-10-08 15:22:04 +0200

    (1165) Migration script divided in two files

--- packages/api/db/migrations/20211004-1165-01-add_fk_role_regular_to_user.js
+++ packages/api/db/migrations/20211004-1165-01-add_fk_role_regular_to_user.js
@@ -6,47 +6,32 @@ module.exports = {
             'fk_role_regular',
             {
                 type: Sequelize.STRING,
-                allowNull: false,
-                defaultValue: 'new',
+                allowNull: true,
             },
             {
                 transaction,
             },
         )
-            .then(() => queryInterface.changeColumn(
-                'users',
-                'fk_role_regular',
-                {
-                    type: Sequelize.STRING,
-                    allowNull: false,
-                    defaultValue: null,
-                },
-                {
-                    transaction,
-                },
-            ))
             .then(() => queryInterface.sequelize.query(
-                `UPDATE
-                    users AS us2
-                SET
-                    fk_role_regular =
-                (
-                    SELECT
-                            ot3.fk_role
-                    FROM
-                            organization_types AS ot3
-                    LEFT JOIN
-                            organizations AS org3
-                                ON org3.fk_type = ot3.organization_type_id 
-                    LEFT JOIN
-                            users AS us3
-                                ON us3.fk_organization = org3.organization_id 
-                    WHERE
-                            us3.user_id = us2.user_id
-                )`,
-                {
-                    transaction,
-                },
+                'SELECT u.user_id, ot.fk_role FROM users u LEFT JOIN organizations o ON u.fk_organization = o.organization_id LEFT JOIN organization_types ot ON o.fk_type = organization_type_id',
+                { transaction, type: queryInterface.sequelize.QueryTypes.SELECT },
+            ))
+            .then(userRoles => Promise.all(
+                userRoles.map(({ user_id, fk_role }) => queryInterface.sequelize.query(
+                    'UPDATE users SET fk_role_regular = :fk_role WHERE user_id = :user_id',
+                    { transaction, replacements: { user_id, fk_role } },
+                ))
+                    .then(() => queryInterface.changeColumn(
+                        'users',
+                        'fk_role_regular',
+                        {
+                            type: Sequelize.STRING,
+                            allowNull: false,
+                        },
+                        {
+                            transaction,
+                        },
+                    )),
             ))
             .then(() => queryInterface.addConstraint(
                 'users',
@@ -62,98 +47,19 @@ module.exports = {
                     onDelete: 'restrict',
                     transaction,
                 },
-            ))
-            .then(() => queryInterface.sequelize.query(
-                `UPDATE organizations
-                 SET fk_type = 4
-                 WHERE organization_id IN (14243, 14310, 14314, 14338, 14360, 14399, 34116, 34195)`,
-                {
-                    transaction,
-                },
-            ))
-            .then(() => queryInterface.sequelize.query(
-                `UPDATE organizations
-                 SET fk_type = 5
-                 WHERE organization_id IN (39854, 40344)`,
-                {
-                    transaction,
-                },
-            ))
-            .then(() => queryInterface.sequelize.query(
-                `UPDATE organizations
-                    SET fk_type = 6
-                    WHERE organization_id IN (40671)`,
-                {
-                    transaction,
-                },
-            ))
-            .then(() => queryInterface.sequelize.query(
-                `UPDATE organizations
-                    SET fk_type = 14
-                    WHERE organization_id IN (40904)`,
-                {
-                    transaction,
-                },
-            ))
-            .then(() => queryInterface.sequelize.query(
-                `UPDATE organizations
-                    SET fk_type = 8
-                    WHERE organization_id IN (41494, 41579, 41595, 41616, 41634, 41644, 41653, 41658, 41942)`,
-                {
-                    transaction,
-                },
-            ))
-            .then(() => queryInterface.bulkDelete(
-                'organization_types',
-                {
-                    fk_role: 'intervener',
-                    fk_category: 'intervener',
-                },
-                {
-                    transaction,
-                },
-            ))
-            .then(() => queryInterface.bulkDelete(
-                'organization_categories',
-                {
-                    uid: 'intervener',
-                },
-                {
-                    transaction,
-                },
             )),
     ),
 
     down: queryInterface => queryInterface.sequelize.transaction(
-        transaction => queryInterface.sequelize.query(
-            'INSERT INTO organization_categories(uid, name_singular, name_plural) VALUES(\'intervener\', \'Intervenant\', \'Intervenants\')',
-            {
-                transaction,
-            },
+        transaction => queryInterface.sequelize.removeConstraint(
+            'users',
+            'fk_users_role_regular',
+            { transaction },
         )
-            .then(() => queryInterface.sequelize.query(
-                'INSERT INTO organization_types(name_singular, name_plural, fk_category, fk_role, uid) VALUES(\'Intervenant\', \'Intervenants\', \'intervener\', \'intervener\', \'intervenant\')',
-                {
-                    transaction,
-                },
-            )
-                .then(() => queryInterface.sequelize.query(
-                    `UPDATE organizations
-                        SET fk_type = (SELECT organization_type_id FROM organization_types WHERE LOWER(name_singular)='intervenant')
-                        WHERE organization_id IN (14243, 14310, 14314, 14338, 14360, 14399, 34116, 34195, 39854, 40344, 40671, 40904, 41494, 41579, 41595, 41616, 41634, 41644, 41653, 41658, 41942)`,
-                    {
-                        transaction,
-                    },
-                )))
-            .then(() => queryInterface.removeConstraint(
+            .then(() => queryInterface.removeColumn(
                 'users',
-                'fk_users_role_regular',
+                'fk_role_regular',
                 { transaction },
-            )
-                .then(() => queryInterface.removeColumn(
-                    'users',
-                    'fk_role_regular',
-                    { transaction },
-                ))),
+            )),
     ),
 };
