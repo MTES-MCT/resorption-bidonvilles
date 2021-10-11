59acdd0fcf0c6fc89a54913515f4ce83552a31b4
Christophe BENARD <ch.benard@gmail.com>
2021-10-11 14:41:53 +0200

    (1240-2) Fix getWaterAccessConditions trouble

--- packages/api/server/models/shantytownModel.js
+++ packages/api/server/models/shantytownModel.js
@@ -331,23 +331,23 @@ function getWaterHandWashAccessPopulationRatio(populationTotal, waterHandWashAcc
  */
 function getWaterAccessConditions(shantytown) {
     if (shantytown.accessToWater) {
-        // const waterHandWashAccessPopulationRatio = getWaterHandWashAccessPopulationRatio(shantytown.populationTotal, shantytown.waterHandWashAccessNumber);
-        // if (
-        //     !shantytown.waterPotable
-        //     || !shantytown.waterContinuousAccess
-        //     || !shantytown.waterPublicPoint
-        //     || !shantytown.waterDistance
-        //     || shantytown.waterDistance !== '0-20'
-        //     || shantytown.waterRoadsToCross === null
-        //     || shantytown.waterRoadsToCross
-        //     || !shantytown.waterEveryoneHasAccess
-        //     || shantytown.waterStagnantWater === null
-        //     || shantytown.waterStagnantWater
-        //     || !shantytown.waterHandWashAccess
-        //     || waterHandWashAccessPopulationRatio > 20
-        // ) {
-        //     return 'toImprove';
-        // }
+        const waterHandWashAccessPopulationRatio = getWaterHandWashAccessPopulationRatio(shantytown.populationTotal, shantytown.waterHandWashAccessNumber);
+        if (
+            !shantytown.waterPotable
+            || !shantytown.waterContinuousAccess
+            || !shantytown.waterPublicPoint
+            || !shantytown.waterDistance
+            || shantytown.waterDistance !== '0-20'
+            || shantytown.waterRoadsToCross === null
+            || shantytown.waterRoadsToCross
+            || !shantytown.waterEveryoneHasAccess
+            || shantytown.waterStagnantWater === null
+            || shantytown.waterStagnantWater
+            || !shantytown.waterHandWashAccess
+            || waterHandWashAccessPopulationRatio > 20
+        ) {
+            return 'toImprove';
+        }
         return 'true';
     }
     return shantytown.accessToWater === false ? 'false' : null;
