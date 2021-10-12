a2be0a9a160fe83719939ae2a1f4ad441ac8608f
gdestrem <gael.destrem@gmail.com>
2021-10-12 15:19:24 +0200

    #patch (1245) Catch only router/guardian exceptions

--- packages/frontend/cypress/support/index.js
+++ packages/frontend/cypress/support/index.js
@@ -1,7 +1,13 @@
 import "./commands/index";
 
-Cypress.on("uncaught:exception", () => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
+Cypress.on("uncaught:exception", error => {
+    // Vue router guardians throws an error
+    if (
+        error.message.includes("Redirected") ||
+        error.message.includes("NavigationDuplicated")
+    ) {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false;
+    }
 });
