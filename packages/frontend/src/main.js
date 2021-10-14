5ab015ef72b2207874f302980a210dce05777213
gdestrem <gael.destrem@gmail.com>
2021-10-14 14:14:13 +0200

    #patch (Gridsome) Add matomo & sentry on bootstrap

--- packages/frontend/src/main.js
+++ packages/frontend/src/main.js
@@ -1,17 +1,26 @@
-import './init/styles'
-import registerIcons from './init/icons'
+import "./init/styles";
 import registerGlobalComponents from "#app/components/ui/registerGlobalComponents";
 
 // Import vue libs
 import VueI18n from "vue-i18n";
 import messages from "#app/messages";
 
+// Lazyload non critical modules (Sentry, Matomo, VeeValidate)
+const asyncMatomo = import("./init/matomo").then(m => m.default);
+const asyncSentry = import("./init/sentry").then(m => m.default);
+const asyncIcons = import("./init/icons").then(m => m.default);
+
 // WARNING: This file should be as small as possible as it's loaded on EVERY pages
 // Please try to lazyload modules instead
 export default function(Vue, { appOptions }) {
+    asyncSentry.then(m => m(Vue));
+    asyncMatomo.then(m => m(Vue));
+    asyncIcons.then(m => m(Vue));
+
     // Register lazy loaded components
-    registerIcons(Vue);
     registerGlobalComponents(Vue);
+
+    // Init critical stuff
     Vue.use(VueI18n);
     appOptions.i18n = new VueI18n({
         locale: "fr",
