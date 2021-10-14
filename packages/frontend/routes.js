57eb648a5e741351fc036282e6bac44edd3607a7
gdestrem <gael.destrem@gmail.com>
2021-10-14 14:29:13 +0200

    #patch (Gridsome) Add public routes stats, connexion, invitation, mentions legales, nouveau mdp

--- packages/frontend/routes.js
+++ packages/frontend/routes.js
@@ -11,5 +11,66 @@ module.exports = [
             beforeEnter: "home",
             ssr: true
         }
+    },
+    {
+        path: "/contact",
+        component: path.join(__dirname, "./src/js/app/pages/Contact/index.vue"),
+        context: {
+            beforeEnter: "anonymous",
+            ssr: true
+        }
+    },
+    {
+        path: "/stats",
+        component: path.join(
+            __dirname,
+            "./src/js/app/pages/PublicStats/index.vue"
+        ),
+        context: {
+            beforeEnter: "anonymous",
+            ssr: true
+        }
+    },
+    {
+        path: "/connexion",
+        component: path.join(__dirname, "./src/js/app/pages/SignIn/index.vue"),
+        context: {
+            beforeEnter: "anonymous",
+            ssr: true
+        }
+    },
+    {
+        path: "/invitation",
+        component: path.join(
+            __dirname,
+            "./src/js/app/pages/Invitation/index.vue"
+        ),
+        context: {
+            ssr: true
+        }
+    },
+    {
+        path: "/mentions-legales",
+        component: path.join(
+            __dirname,
+            "./src/js/app/pages/legalMentions/legalMentions.vue"
+        ),
+        context: {
+            ssr: true
+        }
+    },
+    {
+        path: "/nouveau-mot-de-passe",
+        component: path.join(
+            __dirname,
+            "./src/js/app/pages/UserRequestNewPassword/index.vue"
+        ),
+        context: {
+            meta: {
+                group: "users"
+            },
+            beforeEnter: "anonymous",
+            ssr: true
+        }
     }
 ];
