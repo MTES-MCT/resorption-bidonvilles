1d648a4b3191dea42a80109780c92c463a0532af
Thomazeau Grégoire <>
2021-11-23 17:57:59 +0100

    (1296) modification adapts seeder to change in users table structure

--- packages/api/db/seeders/000001-users.js
+++ packages/api/db/seeders/000001-users.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-function generate(email, password, first_name, last_name, fk_role, phone, position) {
+function generate(email, password, first_name, last_name, fk_role, fk_role_regular, phone, position) {
     const salt = crypto.randomBytes(16).toString('hex');
 
     return {
@@ -10,6 +10,7 @@ function generate(email, password, first_name, last_name, fk_role, phone, positi
         first_name,
         last_name,
         fk_role,
+	fk_role_regular,
         fk_status: 'active',
         access_request_message: 'Compte généré automatiquement',
         phone,
@@ -28,6 +29,7 @@ module.exports = {
                 'Administrateur',
                 'Résorption Bidonvilles',
                 'national_admin',
+		'direct_collaborator',
                 '00 00 00 00 00',
                 'Administrateur',
             ),
