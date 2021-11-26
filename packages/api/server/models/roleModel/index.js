09223f93c9b4bf9cd663049d845e9bd07f0c6e67
Christophe BENARD <ch.benard@gmail.com>
2021-11-26 15:37:32 +0100

    (1292) findOneByName method exported from roleModel

--- packages/api/server/models/roleModel/index.js
+++ packages/api/server/models/roleModel/index.js
@@ -1,7 +1,9 @@
 const findAll = require('./findAll');
 const findOne = require('./findOne');
+const findOneByName = require('./findOneByName');
 
 module.exports = () => ({
     findAll,
     findOne,
+    findOneByName,
 });
