943418847a2ec3b5c152ffef0449b4ab2c1b7b53
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2021-12-17 14:48:06 +0100

    (1306) correction import findNearbyTowns

--- packages/api/server/controllers/townController/findClosedNearbyTowns.js
+++ packages/api/server/controllers/townController/findClosedNearbyTowns.js
@@ -1,9 +1,11 @@
+const { sequelize } = require('#db/models');
+const { findNearby } = require('#server/models/shantytownModel')(sequelize);
 
-module.exports = models => async (req, res, next) => {
+module.exports = () => async (req, res, next) => {
     try {
         const { latitude, longitude } = req.query;
         const distance = 0.5;
-        const closedTowns = await models.shantytown.findNearby(req.user, latitude, longitude, distance, true);
+        const closedTowns = await findNearby(req.user, latitude, longitude, distance, true);
         return res.status(200).send({ closedTowns });
     } catch (error) {
         res.status(500).send({
--- packages/api/server/controllers/townController/findNearbyTowns.js
+++ packages/api/server/controllers/townController/findNearbyTowns.js
@@ -1,9 +1,11 @@
+const { sequelize } = require('#db/models');
+const { findNearby } = require('#server/models/shantytownModel')(sequelize);
 
-module.exports = models => async (req, res, next) => {
+module.exports = () => async (req, res, next) => {
     try {
         const { latitude, longitude } = req.query;
         const distance = 0.5;
-        const towns = await models.shantytown.findNearby(req.user, latitude, longitude, distance);
+        const towns = await findNearby(req.user, latitude, longitude, distance);
         return res.status(200).send({ towns });
     } catch (error) {
         res.status(500).send({
