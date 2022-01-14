37e2810c45482907794c9b46474593b85c0177e7
Christophe BENARD <ch.benard@gmail.com>
2022-01-14 13:55:54 +0100

    (ds-v2-common) Adds a table component

---
+++ packages/frontend/src/js/app/pages/TestTable/TestTable.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <h1>Tableau 1</h1>
+        <Simple-table :dataTable="dataTable" />
+        <br />
+        <h1>Tableau 2</h1>
+        <Simple-table :dataTable="dataTable2" />
+        <br />
+    </div>
+</template>
+
+<script>
+import SimpleTable from "#app/components/Table/SimpleTable.vue";
+// import RbTable from "#app/components/Table/RbTable.vue";
+
+export default {
+    components: {
+        SimpleTable
+        // RbTable
+    },
+    data() {
+        return {
+            dataTable: {
+                caption: "Dispositifs - National",
+                columns: [
+                    "Nom du dispositif",
+                    "Dpt",
+                    "Lieu",
+                    "Pilote",
+                    "Opérateur"
+                ],
+                datas: [
+                    {
+                        name:
+                            "Programme d’accès au logement et à l’insertion pour des familles sorties des squats et des campements du département des Alpes-Maritimes",
+                        departement: "06",
+                        site:
+                            "Sur site, dans le logement, au sein de locaux associatifs...",
+                        manager: "Préfecture de département - Alpes-Maritimes",
+                        operator: "Accompagnement promotion insertion Provence"
+                    },
+                    {
+                        name:
+                            "Accompagnement des familles intracommunautaires.",
+                        departement: "94",
+                        site: "3 Avenue Pierre Sémard",
+                        manager:
+                            "Direction Régionale et Interdépartementale de l'Hébergement et du Logement - Île-de-France",
+                        operator: "Convivances"
+                    }
+                ],
+                emptyTableText: "Aucun élément à afficher"
+            },
+            dataTable2: {
+                caption: "Dispositifs - National",
+                columns: [
+                    "Nom du dispositif",
+                    "Dpt",
+                    "Lieu",
+                    "Pilote",
+                    "Opérateur"
+                ],
+                datas: [],
+                emptyTableText: "Aucun élément à afficher"
+            }
+        };
+    }
+};
+</script>
