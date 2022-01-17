8952f9ab97bbf7720378367667800c7a95f92bc3
Christophe BENARD <ch.benard@gmail.com>
2022-01-17 15:48:19 +0100

    (ds-v2-common) odd/even rows have distinct bg-color

--- packages/frontend/src/js/app/components/Table/RbTable.vue
+++ packages/frontend/src/js/app/components/Table/RbTable.vue
@@ -4,7 +4,7 @@
             v-if="dataTable.caption"
             :caption="dataTable.caption"
         ></rb-table-caption>
-        <table>
+        <table class="zebra">
             <!-- header rows and columns -->
             <thead>
                 <rb-table-row :row="dataTable.columns" />
@@ -51,3 +51,11 @@ export default {
     }
 };
 </script>
+<style scoped>
+.zebra tr:nth-child(even) {
+    @apply bg-G200;
+}
+.zebra tr:nth-child(odd) {
+    @apply bg-G100;
+}
+</style>
