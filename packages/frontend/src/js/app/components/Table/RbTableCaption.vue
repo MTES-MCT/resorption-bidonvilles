743db8fdff97e05aabf39cb88505a769af8221fe
Christophe BENARD <ch.benard@gmail.com>
2022-01-17 09:08:47 +0100

    (ds-v2-common) Components renamed with Rb prefix

---
+++ packages/frontend/src/js/app/components/Table/RbTableCaption.vue
@@ -0,0 +1,13 @@
+<template>
+    <span>
+        {{ caption }}
+    </span>
+</template>
+<script>
+export default {
+    name: "RbTableCaption",
+    props: {
+        caption: String
+    }
+};
+</script>
