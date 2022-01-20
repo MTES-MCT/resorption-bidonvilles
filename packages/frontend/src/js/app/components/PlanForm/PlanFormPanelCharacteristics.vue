789df2253ef044442daebee7470f9cc8242c8f21
Anis Safine Laget <anis@users.noreply.github.com>
2022-01-20 17:52:12 +0100

    (954) Ajout des rubriques, vides

---
+++ packages/frontend/src/js/app/components/PlanForm/PlanFormPanelCharacteristics.vue
@@ -0,0 +1,22 @@
+<template>
+    <FormGroup title="Intervention">
+        Incoming...
+    </FormGroup>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Object,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            input: this.value
+        };
+    }
+};
+</script>
--- packages/frontend/src/js/app/components/PlanForm/PlanFormPanelInfo.vue
+++ packages/frontend/src/js/app/components/PlanForm/PlanFormPanelInfo.vue
@@ -3,7 +3,8 @@
         <template v-slot:title>Qu'est-ce qu'un dispositif ?</template>
         <template v-slot:content>
             C’est une action mise en place et financée en partie ou en totalité
-            par un acteur public. Sa finalité est la résorption des bidonvilles.
+            par un acteur public. Sa finalité est la résorption des
+            bidonvilles.<br />
             <strong>Qui doit le déclarer ?</strong> Le service de l'Etat à
             l'initiative du dispositif doit le déclarer. L'acteur de terrain
             sera en charge de renseigner les informations relatives à l'action
