d2a4e3cbbaf8f76411adca7d1d1931f041c43598
Christophe BENARD <ch.benard@gmail.com>
2022-02-11 09:04:50 +0100

    (1342) Menu for desktop resolution added

---
+++ packages/frontend/src/js/app/layouts/Navbar/DesktopMenuLinkItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <router-link :to="target">
+        <p :class="classes" class="hover:text-gray-600">
+            {{ label }}
+        </p>
+    </router-link>
+</template>
+<script>
+export default {
+    props: {
+        target: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        },
+        group: {
+            type: String,
+            required: false
+        },
+        classes: {
+            type: String,
+            required: false
+        }
+    },
+    methods: {
+        isCurrentRouteAMemberOf(group) {
+            return this.$route.matched.some(
+                route => route.meta.group === group
+            );
+        }
+    }
+};
+</script>
