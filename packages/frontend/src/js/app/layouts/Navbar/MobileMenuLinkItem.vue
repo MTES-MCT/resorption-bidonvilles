69efea2b2d27c3e75f643dc76af07dadda4e7c73
Christophe BENARD <ch.benard@gmail.com>
2022-02-11 09:06:34 +0100

    (1342) Menu items for mobile menu

---
+++ packages/frontend/src/js/app/layouts/Navbar/MobileMenuLinkItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="border-b-2 border-gray-300 py-3">
+        <router-link :to="target">
+            <p class="no-underline text-display-sm text-black">
+                {{ label }}
+            </p>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        target: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
