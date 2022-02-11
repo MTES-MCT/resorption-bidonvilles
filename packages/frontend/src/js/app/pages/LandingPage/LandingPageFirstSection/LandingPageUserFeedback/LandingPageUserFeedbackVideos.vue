060da4832b1be5473fa5b287942172faa48ac781
Christophe BENARD <ch.benard@gmail.com>
2022-02-11 16:19:32 +0100

    (1139) New video users's feedback section

---
+++ packages/frontend/src/js/app/pages/LandingPage/LandingPageFirstSection/LandingPageUserFeedback/LandingPageUserFeedbackVideos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        class="mt-8 md:mt-20 sm:flex sm:flex-col sm:space-y-8 md:space-y-0 md:flex md:flex-row items-center"
+    >
+        <div class="md:w-1/2 md:mr-16 bg-gray-500">
+            <video controls>
+                <source
+                    src="./assets/Temoignage-Leduc-BD_mars2021.mp4"
+                    type="video/mp4"
+                />
+                Votre navigateur ne supporte pas la balise video.
+            </video>
+        </div>
+        <div class="md:w-1/2 md:mr-16 bg-gray-500">
+            <video controls>
+                <source
+                    src="./assets/Temoignage-LeMinor-BD_mars2021.mp4"
+                    type="video/mp4"
+                />
+                Votre navigateur ne supporte pas la balise video.
+            </video>
+        </div>
+    </div>
+</template>
