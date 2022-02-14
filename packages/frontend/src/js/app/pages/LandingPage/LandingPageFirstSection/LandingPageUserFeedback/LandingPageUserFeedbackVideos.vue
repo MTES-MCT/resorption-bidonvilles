106c922a08117fee333663686d464bdce2099a9e
Anis Safine Laget <anis@users.noreply.github.com>
2022-02-14 10:42:39 +0100

    (1139) Simplify alignment

--- packages/frontend/src/js/app/pages/LandingPage/LandingPageFirstSection/LandingPageUserFeedback/LandingPageUserFeedbackVideos.vue
+++ packages/frontend/src/js/app/pages/LandingPage/LandingPageFirstSection/LandingPageUserFeedback/LandingPageUserFeedbackVideos.vue
@@ -1,8 +1,6 @@
 <template>
-    <div
-        class="mt-8 md:mt-20 sm:flex sm:flex-col sm:space-y-8 md:space-y-0 md:flex md:flex-row items-center"
-    >
-        <div class="md:w-1/2 md:mr-16 bg-gray-500">
+    <div class="mt-8 lg:flex lg:space-x-8">
+        <div class="bg-gray-500">
             <video controls>
                 <source
                     src="./assets/Temoignage-Leduc-BD_mars2021.mp4"
@@ -11,7 +9,7 @@
                 Votre navigateur ne supporte pas la balise video.
             </video>
         </div>
-        <div class="md:w-1/2 md:mr-16 bg-gray-500">
+        <div class="bg-gray-500 mt-8 lg:mt-0">
             <video controls>
                 <source
                     src="./assets/Temoignage-LeMinor-BD_mars2021.mp4"
