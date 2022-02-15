afd7e97b07a1312184c46dfd47d797aa21729ea4
Christophe BENARD <ch.benard@gmail.com>
2022-02-15 21:06:17 +0100

    (1357) Use a varenv for Matomo auth_token

--- packages/api/server/controllers/matomoController.js
+++ packages/api/server/controllers/matomoController.js
@@ -4,7 +4,7 @@ module.exports = () => ({
     // For test purpose, this method returns the number of weekly active user for the last week of december 2021
     async test(req, res, next) {
         const statisticAtDate = '2021-12-31';
-        const MATOMO_AUTH_TOKEN = 'ac8317623ab44fb0aee280c5fc06e697';
+        const MATOMO_AUTH_TOKEN = process.env.VUE_APP_MATOMO_AUTH_TOKEN;
         const BASE_URL = `https://stats.data.gouv.fr/index.php?module=API&method=VisitsSummary.getUniqueVisitors&idSite=86&period=month&date=${statisticAtDate}&format=JSON&token_auth=${MATOMO_AUTH_TOKEN}`;
 
         try {
