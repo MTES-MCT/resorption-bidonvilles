1c3f950fc3dcb7f47bccef188e8df01f154683e8
Anis Safine Laget <anis@users.noreply.github.com>
2022-02-15 11:46:33 +0100

    (1348) Setup Dashboard shantytowns

---
+++ packages/frontend/src/js/app/pages/Dashboard/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <PrivateLayout>
+        <PrivateContainer class="pt-10 mb-12">
+            <DashboardShantytowns />
+        </PrivateContainer>
+    </PrivateLayout>
+</template>
+
+<script>
+import PrivateLayout from "#app/components/PrivateLayout";
+import PrivateContainer from "#app/components/PrivateLayout/PrivateContainer";
+
+import DashboardShantytowns from "./DashboardShantytowns/DashboardShantytowns";
+
+export default {
+    components: {
+        PrivateLayout,
+        PrivateContainer,
+        DashboardShantytowns
+    }
+};
+</script>
---
+++ packages/frontend/src/js/app/pages/Dashboard/DashboardSection.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <header class="md:flex items-center mb-5">
+            <h1 class="text-xl font-bold">{{ title }}</h1>
+            <nav class="flex flex-1 justify-between items-center mt-1">
+                <slot name="header_left" />
+                <slot name="header_right" />
+            </nav>
+        </header>
+
+        <slot name="body" />
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
