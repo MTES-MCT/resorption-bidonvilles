668df1e824b343fa4e3a99e8ef81f99339785c78
Christophe BENARD <ch.benard@gmail.com>
2022-02-16 18:54:30 +0100

    (1357) request-promise replaced by node-fetch - request-promise is obsolete

--- packages/api/server/controllers/matomoController.js
+++ packages/api/server/controllers/matomoController.js
@@ -1,4 +1,5 @@
-const request = require('request-promise');
+// const request = require('request-promise');
+const fetch = require('node-fetch');
 
 module.exports = () => ({
     // For test purpose, this method returns the number of weekly active user for the last week of december 2021
@@ -8,14 +9,14 @@ module.exports = () => ({
         const BASE_URL = `https://stats.data.gouv.fr/index.php?module=API&method=VisitsSummary.getUniqueVisitors&idSite=86&period=month&date=${statisticAtDate}&format=JSON&token_auth=${MATOMO_AUTH_TOKEN}`;
 
         try {
-            const returnValue = await request(BASE_URL, {
+            const response = await fetch(BASE_URL, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                json: true,
             });
-            return res.status(200).send(returnValue);
+            const json = await response.json();
+            return res.status(200).send(json);
         } catch (error) {
             res.status(500).send({ user_message: 'Une erreur est survenue lors de l\'exécution de la requête' });
             return next(error);
