4d1d45f0a638d1ba78188e47017be4033ec0033e
Christophe BENARD <ch.benard@gmail.com>
2022-02-21 16:30:18 +0100

    (1357) 'request-promise' replaced by 'node-fetch'

--- packages/api/server/controllers/poiController.js
+++ packages/api/server/controllers/poiController.js
@@ -1,27 +1,50 @@
-const request = require('request-promise');
+const nodeFetch = require('node-fetch');
 
 module.exports = () => ({
     async findAll(req, res, next) {
-        const authKey = process.env.RB_API_SOLIGUIDE_KEY;
+        class HTTPResponseError extends Error {
+            constructor(response, ...args) {
+                super(`HTTP Error Response: ${response.status} ${response.statusText}`, ...args);
+                this.response = response;
+            }
+        }
 
+        const checkStatus = (response) => {
+            if (response.ok) {
+                // response.status >= 200 && response.status < 300
+                return response;
+            }
+            throw new HTTPResponseError(response);
+        };
+
+        const authKey = process.env.RB_API_SOLIGUIDE_KEY;
         try {
             if (!authKey) {
                 return res.status(200).send([]);
             }
-
-            // docs: https://solinum.gitbook.io/soliguide-api/detail-de-lapi/lieu-et-services
-            const { places } = await request('https://api.soliguide.fr/new-search', {
+            const url = 'https://api.soliguide.fr/new-search';
+            const body = { location: { geoType: 'pays', geoValue: 'France' }, categorie: 601, options: { limit: 1000 } };
+            const response = await nodeFetch(url, {
                 method: 'POST',
-                body: { location: { geoType: 'pays', geoValue: 'France' }, categorie: 601, options: { limit: 1000 } },
+                body: JSON.stringify(body),
                 headers: {
                     'Content-Type': 'application/json',
                     Authorization: authKey,
                 },
                 json: true,
             });
-            return res.status(200).send(places);
+
+            try {
+                checkStatus(response);
+                const { places } = await response.json();
+                return res.status(200).send(places);
+            } catch (error) {
+                const errorBody = await error.response.text();
+                res.status(500).send({ error_message: errorBody });
+            }
+            return next({ user_message: 'Erreur inconue !!!' });
         } catch (error) {
-            res.status(500).send({ user_message: 'Une erreur est survenue lors de la lecture en base de données' });
+            res.status(500).send({ user_message: 'Une erreur est survenue lors de la récupération des points de distribution alimentaire' });
             return next(error);
         }
     },
