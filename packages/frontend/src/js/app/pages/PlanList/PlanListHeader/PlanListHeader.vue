2bc3a4763fff880515bd0daa433a63406ea8fc73
Christophe BENARD <ch.benard@gmail.com>
2022-02-26 15:03:46 +0100

    (1359) Adds a button on plans list to export plans - Access permission only to national administrators

--- packages/frontend/src/js/app/pages/PlanList/PlanListHeader/PlanListHeader.vue
+++ packages/frontend/src/js/app/pages/PlanList/PlanListHeader/PlanListHeader.vue
@@ -5,30 +5,60 @@
             L'ensemble des dispositifs sur votre territoire :
             <span class="font-bold">{{ location.label }}</span>
         </h2>
-
-        <div v-if="hasPermission('plan.create')" align="right" class="mb-6">
-            <router-link to="/nouveau-dispositif">
-                <Button
-                    icon="plus"
-                    iconPosition="left"
-                    variant="secondary"
-                    class="whitespace-no-wrap"
-                >
-                    Déclarer un nouveau dispositif</Button
-                >
-            </router-link>
+        <div class="md:flex md:flex-row-reverse mb-6">
+            <div v-if="hasPermission('plan.create')">
+                <router-link to="/nouveau-dispositif">
+                    <Button
+                        icon="plus"
+                        iconPosition="left"
+                        variant="secondary"
+                        class="whitespace-no-wrap mb-4 md:mb-0"
+                    >
+                        Déclarer un nouveau dispositif</Button
+                    >
+                </router-link>
+            </div>
+            <Button
+                v-if="hasPermission('plan.export')"
+                icon="file-excel"
+                iconPosition="left"
+                variant="primary"
+                class="mr-6"
+                @click="exportPlans"
+                >Exporter</Button
+            >
         </div>
     </div>
 </template>
 
 <script>
+import { exportPlans } from "#helpers/api/plan";
 import { hasPermission } from "#helpers/api/config";
 import { mapGetters } from "vuex";
+import { notify } from "#helpers/notificationHelper";
 
 export default {
     methods: {
         hasPermission(...args) {
             return hasPermission(...args);
+        },
+        async exportPlans() {
+            try {
+                const { csv } = await exportPlans();
+                const hiddenElement = document.createElement("a");
+                hiddenElement.href =
+                    "data:text/csv;charset=utf-8," + encodeURI(csv);
+                hiddenElement.target = "_blank";
+                hiddenElement.download = "plans.csv";
+                hiddenElement.click();
+            } catch (err) {
+                notify({
+                    group: "notifications",
+                    type: "error",
+                    title: "Une erreur est survenue",
+                    text: "Une erreur est survenue durant l'export des actions"
+                });
+            }
         }
     },
 
