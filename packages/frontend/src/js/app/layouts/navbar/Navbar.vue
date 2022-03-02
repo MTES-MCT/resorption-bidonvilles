39ff4a30fe0d39a7b71bc809be31f75c93fe51db
Anis Safine Laget <anis@users.noreply.github.com>
2022-03-02 10:55:39 +0100

    (1342) Bidouille rebase

--- packages/frontend/src/js/app/layouts/navbar/Navbar.vue
+++ packages/frontend/src/js/app/layouts/navbar/Navbar.vue
@@ -1,25 +1,28 @@
+<!-- A renommer Navbar, ainsi que le répertoire, une fois toute référence à navbar (v1) supprimées -->
 <template>
-    <nav class="container px-10 mx-auto bg-white my-4">
-        <div ref="menu" class="flex lg:flex-row-reverse justify-between">
-            <div class="hidden lg:flex space-x-4">
-                <div class="flex flex-row-reverse items-center">
-                    <!-- Navigation items - emphased menu items -->
+    <nav class="bg-white my-1">
+        <div class="lg:border-g300 lg:border-b-1">
+            <div
+                class="flex justify-between lg:justify-items-start lg:py-6 px-6 lg:px-12"
+            >
+                <!-- logos -->
+                <div class="flex flex-shrink-0 items-center xl:space-x-8">
+                    <img
+                        class="hidden xl:block h-20 mb-4 lg:mb-0"
+                        src="./assets/bm_rp.png"
+                    />
+                    <router-link to="/">
+                        <img
+                            class="hidden xl:block h-16"
+                            src="./assets/logo-resorption-bidonvilles.png"
+                        />
+                    </router-link>
+                </div>
+                <div class="hidden lg:flex space-x-4">
+                    <!-- Navigation items - upper menu items -->
                     <div
-                        v-for="item in emphasedMenuItems.reverse().slice()"
-                        :key="item.target"
-                    >
-                        <DesktopMenuLinkItem
-                            :target="item.target"
-                            :label="item.label"
-                            :classes="item.classes"
-                            :group="item.group"
-                        ></DesktopMenuLinkItem>
-                    </div>
-                    <span>|</span>
-                    <!-- Navigation items - normal menu items -->
-                    <div
-                        class="flex items-center"
-                        v-for="item in primaryMenuItems.reverse().slice()"
+                        class="flex flex-row items-center"
+                        v-for="item in upperMenuItems"
                         :key="item.target"
                     >
                         <DesktopMenuLinkItem
@@ -30,36 +33,36 @@
                         ></DesktopMenuLinkItem>
                     </div>
                 </div>
+                <!-- Mobile button goes here -->
+                <div class="lg:hidden flex items-center">
+                    <button class="mobile-menu-button" @click="openMobileMenu">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-10 w-10"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                    </button>
+                </div>
             </div>
-            <!-- logos -->
-            <div class="flex flex-shrink-0 items-center lg:space-x-8 xl:ml-8">
-                <img
-                    class="hidden xl:block h-20 mb-4 lg:mb-0"
-                    src="./assets/logo-marianne.png"
-                />
-                <router-link to="/">
-                    <img
-                        class="h-16"
-                        src="./assets/logo-resorption-bidonvilles.png"
-                    />
-                </router-link>
-            </div>
-            <!-- Mobile button goes here -->
-            <div class="lg:hidden flex items-center">
-                <button class="mobile-menu-button" @click="openMobileMenu">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-10 w-10"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </button>
+        </div>
+        <div class="flex lg:justify-items-start lg:border-g300 lg:border-b-1">
+            <div class="hidden lg:flex space-x-4 px-4 lg:px-10">
+                <!-- Navigation items - lower menu items -->
+                <div v-for="item in lowerMenuItems" :key="item.target">
+                    <DesktopMenuLinkItem
+                        :target="item.target"
+                        :label="item.label"
+                        :classes="item.classes"
+                        :group="item.group"
+                    ></DesktopMenuLinkItem>
+                </div>
             </div>
         </div>
         <!-- mobile menu side panel -->
@@ -80,7 +83,7 @@ import {
     hasPermission,
     hasAcceptedCharte
 } from "#helpers/api/config";
-import ITEMS from "./menuItems";
+import menuItems from "./menuItems";
 
 export default {
     components: {
@@ -95,20 +98,20 @@ export default {
     computed: {
         items() {
             if (isLoggedIn() !== true) {
-                return ITEMS.anonymous;
+                return menuItems.anonymous;
             }
 
             if (isConfigLoaded() !== true || hasAcceptedCharte() !== true) {
-                return ITEMS.loading;
+                return menuItems.loading;
             }
 
-            return this.filterItems(ITEMS.loaded);
+            return this.filterItems(menuItems.loaded);
         },
-        primaryMenuItems() {
-            return this.filterMenuItems(this.items, "primary");
+        upperMenuItems() {
+            return this.filterMenuItems(this.items, "upper");
         },
-        emphasedMenuItems() {
-            return this.filterMenuItems(this.items, "emphased");
+        lowerMenuItems() {
+            return this.filterMenuItems(this.items, "lower");
         }
     },
     methods: {
