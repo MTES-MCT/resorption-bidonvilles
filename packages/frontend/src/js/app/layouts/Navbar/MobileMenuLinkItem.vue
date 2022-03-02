6f6437d8c7504b6b7f13e10b24ce13051e5328f4
Christophe BENARD <ch.benard@gmail.com>
2022-03-02 17:19:41 +0100

    (1342) Navbar with new Design replaces navbar

---
+++ packages/frontend/src/js/app/layouts/Navbar/MobileMenuLinkItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="border-b-2 border-gray-300 py-3">
+        <router-link :to="target">
+            <p class="text-display-xs font-normal text-black">
+                {{ label }}
+            </p>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        target: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
