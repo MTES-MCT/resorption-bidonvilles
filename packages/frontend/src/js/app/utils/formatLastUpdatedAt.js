762470a20f3fa9ff891b616654986dc7da261283
Anis Safine Laget <anis@users.noreply.github.com>
2022-03-15 15:03:48 +0100

    (1363) Wording

--- packages/frontend/src/js/app/utils/formatLastUpdatedAt.js
+++ packages/frontend/src/js/app/utils/formatLastUpdatedAt.js
@@ -2,7 +2,7 @@ import formatDateSinceActivity from "#app/utils/formatDateSinceActivity";
 
 export default town => {
     if (Math.abs(town.updatedAt - town.createdAt) < 1) {
-        return `Saisi ${formatDateSinceActivity(town.createdAt)}`;
+        return `Ajouté ${formatDateSinceActivity(town.createdAt)}`;
     }
 
     return `Mis à jour ${formatDateSinceActivity(town.updatedAt)}`;
