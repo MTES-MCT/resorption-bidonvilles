4ce611f76b29fd790204595dc68077cd7a146538
Anis Safine Laget <anis@users.noreply.github.com>
2022-03-16 15:36:11 +0100

    (hotfix) setup rate-limiting on API

---
+++ packages/api/server/loaders/rateLimiterLoader.ts
@@ -0,0 +1,10 @@
+import rateLimit from 'express-rate-limit';
+
+export default (app) => {
+    const apiRequestLimiter = rateLimit({
+        windowMs: 1 * 60 * 1000, // 1 minute
+        max: 60 // max requests allowed per IP in the span of windowMs
+    })
+
+    app.use(apiRequestLimiter)
+};
