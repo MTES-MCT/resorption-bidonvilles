574e8dd6a08b01d04d1161440c80ffbbd8af2662
Anis Safine Laget <anis@users.noreply.github.com>
2022-03-17 17:15:18 +0100

    (1387) Track activity filters

--- packages/frontend/src/js/app/pages/Dashboard/DashboardActivities/DashboardActivitiesFilters.vue
+++ packages/frontend/src/js/app/pages/Dashboard/DashboardActivities/DashboardActivitiesFilters.vue
@@ -19,15 +19,36 @@ export default {
     data() {
         return {
             sections: [
-                { id: "all", label: "Toutes les activités" },
-                { id: "shantytown_creation", label: "Nouveaux sites" },
-                { id: "shantytown_closing", label: "Sites fermés" },
+                {
+                    id: "all",
+                    label: "Toutes les activités",
+                    track_id: "toutes les activités"
+                },
+                {
+                    id: "shantytown_creation",
+                    label: "Nouveaux sites",
+                    track_id: "nouveaux sites"
+                },
+                {
+                    id: "shantytown_closing",
+                    label: "Sites fermés",
+                    track_id: "sites fermés"
+                },
                 {
                     id: "shantytown_closing_with_solutions",
-                    label: "Sites résorbés"
+                    label: "Sites résorbés",
+                    track_id: "sites résorbés"
                 },
-                { id: "user_creation", label: "Nouveaux utilisateurs" },
-                { id: "comment_creation", label: "Nouveaux messages" }
+                {
+                    id: "user_creation",
+                    label: "Nouveaux utilisateurs",
+                    track_id: "nouveaux utilisateurs"
+                },
+                {
+                    id: "comment_creation",
+                    label: "Nouveaux messages",
+                    track_id: "nouveaux messages"
+                }
             ]
         };
     }
