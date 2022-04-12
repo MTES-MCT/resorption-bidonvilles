6bcb1d9e8661f0d539a674ca623600acbe931b7d
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-04-12 16:23:09 +0200

    (tests) index shantytown/services

--- packages/api/server/services/shantytown/index.js
+++ packages/api/server/services/shantytown/index.js
@@ -1,5 +1,19 @@
 const create = require('./create');
+const list = require('./list');
+const find = require('./find');
+const close = require('./close');
+const deleteTown = require('./deleteTown');
+const deleteComment = require('./deleteComment');
+const exportTown = require('./exportTown');
+const createCovidComment = require('./createCovidComment.js');
 
 module.exports = {
     create,
+    list,
+    find,
+    close,
+    deleteTown,
+    deleteComment,
+    exportTown,
+    createCovidComment,
 };
