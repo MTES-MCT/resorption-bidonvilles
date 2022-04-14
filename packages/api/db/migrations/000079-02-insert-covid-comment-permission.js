152d134d4bc05f48f77e0bdc204916fd451c7c10
Christophe BENARD <ch.benard@gmail.com>
2022-04-14 14:10:28 +0200

    (1396) Correction mineure: indentation du code

--- packages/api/db/migrations/000079-02-insert-covid-comment-permission.js
+++ packages/api/db/migrations/000079-02-insert-covid-comment-permission.js
@@ -6,55 +6,55 @@ module.exports = {
                 transaction,
             },
         )
-        .then(() => queryInterface.sequelize.query(
-            'INSERT INTO features(name, fk_entity) VALUES(\'list\', \'covid_comment\')',
-            {
-                transaction,
-            },
-        ))
-        .then(() => {
-            const roles = [
-                { type: 'regular', value: 'direct_collaborator' },
-                { type: 'regular', value: 'collaborator' },
-                { type: 'regular', value: 'association' },
-                { type: 'regular', value: 'national_establisment' },
-                { type: 'admin', value: 'local_admin' },
-                { type: 'admin', value: 'national_admin' },
-            ];
-            const promises = roles.map(({ type, value }) => queryInterface.sequelize.query(
-                `INSERT INTO
+            .then(() => queryInterface.sequelize.query(
+                'INSERT INTO features(name, fk_entity) VALUES(\'list\', \'covid_comment\')',
+                {
+                    transaction,
+                },
+            ))
+            .then(() => {
+                const roles = [
+                    { type: 'regular', value: 'direct_collaborator' },
+                    { type: 'regular', value: 'collaborator' },
+                    { type: 'regular', value: 'association' },
+                    { type: 'regular', value: 'national_establisment' },
+                    { type: 'admin', value: 'local_admin' },
+                    { type: 'admin', value: 'national_admin' },
+                ];
+                const promises = roles.map(({ type, value }) => queryInterface.sequelize.query(
+                    `INSERT INTO
                     permissions(fk_role_regular, fk_role_admin, fk_feature, fk_entity, allowed, fk_geographic_level)
                 VALUES
                     (:regular, :admin, 'list', 'covid_comment', true, 'local')
                 RETURNING permission_id AS id`,
-                {
-                    replacements: {
-                        regular: type === 'regular' ? value : null,
-                        admin: type === 'admin' ? value : null,
+                    {
+                        replacements: {
+                            regular: type === 'regular' ? value : null,
+                            admin: type === 'admin' ? value : null,
+                        },
+                        transaction,
                     },
-                    transaction,
-                },
-            ));
-            return Promise.all(promises);
-        })
-        .then((response) => {
-            const permissionIds = response.map(r => r[0][0].id);
-            return Promise.all(
-                permissionIds
-                    .map(id => queryInterface.sequelize.query(
-                        `INSERT INTO
+                ));
+                return Promise.all(promises);
+            })
+            .then((response) => {
+                const permissionIds = response.map(r => r[0][0].id);
+                return Promise.all(
+                    permissionIds
+                        .map(id => queryInterface.sequelize.query(
+                            `INSERT INTO
                             covid_comment_permissions(fk_permission)
                         VALUES
                             (:id)`,
-                        {
-                            replacements: {
-                                id,
+                            {
+                                replacements: {
+                                    id,
+                                },
+                                transaction,
                             },
-                            transaction,
-                        },
-                    )),
-            );
-        }),
+                        )),
+                );
+            }),
     ),
 
     down: queryInterface => queryInterface.sequelize.transaction(
