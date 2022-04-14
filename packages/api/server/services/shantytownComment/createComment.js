abd8b07da4bf38ffa6b242f4a8c5261e3af20bae
Christophe BENARD <ch.benard@gmail.com>
2022-04-14 13:56:24 +0200

    (1396) utilisation de #db/sequelize
        - Remplacement de la méthode sequelize de #db/models/index.js par
         #db/sequelize dans les middlewares/validators
    
    - Remplacement de la méthode sequelize de #db/models/index.js par
     #db/sequelize dans les services

--- packages/api/server/services/shantytownComment/createComment.js
+++ packages/api/server/services/shantytownComment/createComment.js
@@ -1,9 +1,7 @@
-const { sequelize } = require('#db/models');
-
+const sequelize = require('#db/sequelize');
 const shantytownCommentModel = require('#server/models/shantytownCommentModel')();
 const shantytownModel = require('#server/models/shantytownModel')(sequelize);
 const mattermostUtils = require('#server/utils/mattermost');
-
 const userModel = require('#server/models/userModel')(sequelize);
 const mails = require('#server/mails/mails');
 const ServiceError = require('#server/errors/ServiceError');
