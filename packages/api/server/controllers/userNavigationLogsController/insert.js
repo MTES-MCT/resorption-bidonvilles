aaf24f245092346e375bf200d392e741ee27bc32
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-20 17:37:49 +0200

    (1390) feed: use code 201 for created resource

--- packages/api/server/controllers/userNavigationLogsController/insert.js
+++ packages/api/server/controllers/userNavigationLogsController/insert.js
@@ -25,7 +25,7 @@ module.exports = async (req, res, next) => {
         return next((error && error.nativeError) || error);
     }
 
-    return res.status(200).send({
+    return res.status(201).send({
         userNavigationLogsId,
     });
 };
