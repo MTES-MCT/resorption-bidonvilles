810777a3174976371d8c5d732c5a9bc3add30883
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-21 11:34:25 +0200

    (1396) feed: simplify statsExports.create()

--- packages/api/server/models/statsExports/create.js
+++ packages/api/server/models/statsExports/create.js
@@ -1,33 +1,30 @@
 const sequelize = require('#db/sequelize');
 
-module.exports = async ({
+module.exports = ({
     fk_region, fk_departement, fk_epci, fk_city, closed_shantytowns, exported_by,
-}, transaction = undefined) => {
-    await sequelize.query(
-        `INSERT INTO stats_exports(
+}) => sequelize.query(
+    `INSERT INTO stats_exports(
+        fk_region,
+        fk_departement,
+        fk_epci,
+        fk_city,
+        closed_shantytowns,
+        exported_by
+    ) VALUES (
+        :fk_region,
+        :fk_departement,
+        :fk_epci,
+        :fk_city,
+        :closed_shantytowns,
+        :exported_by
+    )`, {
+        replacements: {
             fk_region,
             fk_departement,
             fk_epci,
             fk_city,
             closed_shantytowns,
-            exported_by
-        ) VALUES (
-            :fk_region,
-            :fk_departement,
-            :fk_epci,
-            :fk_city,
-            :closed_shantytowns,
-            :exported_by
-        )`, {
-            replacements: {
-                fk_region,
-                fk_departement,
-                fk_epci,
-                fk_city,
-                closed_shantytowns,
-                exported_by,
-            },
-            transaction,
+            exported_by,
         },
-    );
-};
+    },
+);
