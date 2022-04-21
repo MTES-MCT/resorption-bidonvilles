c0b0e4b76e4cda9c492e27507f0d0869f4245338
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-21 09:20:22 +0200

    (1396) feed: fix lint errors

--- packages/api/db/sequelize.js
+++ packages/api/db/sequelize.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const config = require('./config/config.js');
+const config = require('./config/config');
 
 const sequelize = new Sequelize(
     config.database,
