3a091b22bd2ffad2086d9fe7c3206734dbc0a442
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-21 11:23:24 +0200

    (1396) feed: simplify model

--- packages/api/server/models/socialOriginModel/create.js
+++ packages/api/server/models/socialOriginModel/create.js
@@ -1,13 +1,12 @@
 const sequelize = require('#db/sequelize');
 
-module.exports = async (shantytown_id, social_origin_id) => {
-    await sequelize.query(
-        `INSERT INTO shantytown_origins(fk_shantytown, fk_social_origin)
-         VALUES (:shantytown_id, :social_origin_id)`,
-        {
-            replacements: {
-                shantytown_id, social_origin_id,
-            },
+module.exports = (shantytown_id, social_origin_id) => sequelize.query(
+    `INSERT INTO shantytown_origins(fk_shantytown, fk_social_origin)
+        VALUES (:shantytown_id, :social_origin_id)`,
+    {
+        replacements: {
+            shantytown_id,
+            social_origin_id,
         },
-    );
-};
+    },
+);
