fe4323e72d839f1692e9089e07eb049e6a6c44a3
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-21 11:01:47 +0200

    (1396) feed: fix response of count() models

--- packages/api/server/models/statsExports/count.js
+++ packages/api/server/models/statsExports/count.js
@@ -1,10 +1,14 @@
 const sequelize = require('#db/sequelize');
 
-module.exports = () => sequelize.query(
-    `SELECT
-        count(*) as total
-    FROM stats_exports`,
-    {
-        type: sequelize.QueryTypes.SELECT,
-    },
-);
+module.exports = async () => {
+    const rows = await sequelize.query(
+        `SELECT
+            count(*) as total
+        FROM stats_exports`,
+        {
+            type: sequelize.QueryTypes.SELECT,
+        },
+    );
+
+    return rows[0].total;
+};
