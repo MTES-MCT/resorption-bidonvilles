6286381db7ce50556e352c99cbccbb8d5032c370
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-21 17:58:34 +0200

    (1398) handle errors on docx generation

--- packages/api/server/controllers/townController/exportOne.js
+++ packages/api/server/controllers/townController/exportOne.js
@@ -1,6 +1,17 @@
 const exportShantytown = require('#server/services/shantytown/export');
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
+    let buffer;
+    try {
+        buffer = await exportShantytown();
+    } catch (error) {
+        res.status(500).send({
+            user_message: 'Une erreur inconnue est survenue',
+        });
+        next(error.nativeError || error);
+        return;
+    }
+
     res.attachment('fiche-de-site.docx');
-    res.end(await exportShantytown());
+    res.end(buffer);
 };
