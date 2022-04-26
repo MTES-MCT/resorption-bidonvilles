201948f866b32095ccf4edf22ff172a0393d881f
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-26 17:11:35 +0200

    (1416) Exclude national admins from WAU calculation

--- packages/api/server/models/statsModel/wau.js
+++ packages/api/server/models/statsModel/wau.js
@@ -5,9 +5,10 @@ module.exports = () => sequelize.query(
         COUNT(DISTINCT fk_user) AS wau,
         to_char(t.monday, 'DD/MM/YYYY') AS monday
     FROM (
-        SELECT fk_user, DATE_TRUNC('week', datetime) AS monday
+        SELECT user_navigation_logs.fk_user, DATE_TRUNC('week', user_navigation_logs.datetime) AS monday
         FROM user_navigation_logs
-        WHERE datetime < DATE_TRUNC('week', NOW())
+        LEFT JOIN users ON user_navigation_logs.fk_user = users.user_id
+        WHERE user_navigation_logs.datetime < DATE_TRUNC('week', NOW()) AND (users.fk_role <> 'national_admin' OR users.fk_role IS NULL)
     ) t
     GROUP BY t.monday ORDER BY monday ASC`,
     {
