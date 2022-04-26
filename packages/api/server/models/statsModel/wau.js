2a3d2dee2f39c67fd862bad58679b5d463ca6142
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-26 17:07:39 +0200

    (1416) Properly format WAU date

--- packages/api/server/models/statsModel/wau.js
+++ packages/api/server/models/statsModel/wau.js
@@ -3,7 +3,7 @@ const sequelize = require('#db/sequelize');
 module.exports = () => sequelize.query(
     `SELECT
         COUNT(DISTINCT fk_user) AS wau,
-        t.monday
+        to_char(t.monday, 'DD/MM/YYYY') AS monday
     FROM (
         SELECT fk_user, DATE_TRUNC('week', datetime) AS monday
         FROM user_navigation_logs
