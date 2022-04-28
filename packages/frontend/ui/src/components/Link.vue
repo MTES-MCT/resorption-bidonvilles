ff07da6b051d5049484f838af73ffaa1c17f4d89
Anis Safine Laget <anis@users.noreply.github.com>
2022-04-28 18:51:06 +0200

    (1403) Add an icon to external links

--- packages/frontend/ui/src/components/Link.vue
+++ packages/frontend/ui/src/components/Link.vue
@@ -1,13 +1,22 @@
 <template>
-    <router-link v-if="internalLink" :to="to" :class="linkClasses"
-        ><slot></slot
-    ></router-link>
-    <a v-else :href="to" :class="linkClasses"><slot></slot></a>
+    <span>
+        <Icon icon="external-link-alt" v-if="!internalLink" :class="linkClasses" />
+        <router-link v-if="internalLink" :to="to" :class="linkClasses"
+            ><slot></slot
+        ></router-link>
+        <a v-else :href="to" :class="linkClasses"><slot></slot></a>
+    </span>
 </template>
 
 <script>
+import Icon from "./Icon.vue";
+
 export default {
     name: "Link",
+
+    components: {
+        Icon
+    },
 
     props: {
         to: {
