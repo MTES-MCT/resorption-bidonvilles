294a37534c0c2032081319dcc59099051f4075c8
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-05-06 15:22:47 +0200

    (tests) suppression async/await inutile

--- packages/api/server/models/shantytownCommentModel/deleteComment.js
+++ packages/api/server/models/shantytownCommentModel/deleteComment.js
@@ -3,8 +3,8 @@ const sequelize = require('#db/sequelize');
 /**
  * @param {Number} id A shantytown_comment_id
  */
-module.exports = async (commentId) => {
-    await sequelize.query(
+module.exports = (commentId) => {
+    sequelize.query(
         'DELETE FROM shantytown_comments WHERE shantytown_comment_id = :id',
         {
             replacements: {
