7399dd4309658f3baef9c1244246f4651608fd82
Anis Safine Laget <anis@users.noreply.github.com>
2022-05-11 11:42:03 +0200

    (1419) Feed: forbid future exports on API-side

--- packages/api/server/middlewares/validators/exportTowns.js
+++ packages/api/server/middlewares/validators/exportTowns.js
@@ -8,5 +8,17 @@ module.exports = [
         .withMessage('La date doit être un timestamp'),
 
     query('date')
-        .customSanitizer(value => value || Date.now()),
+        .customSanitizer(value => value || Date.now())
+        .custom((value) => {
+            const today = new Date();
+            const then = new Date(value);
+            today.setHours(0, 0, 0, 0);
+            then.setHours(0, 0, 0, 0);
+
+            if (then > today) {
+                throw new Error('Vous ne pouvez pas générer un export pour une date future');
+            }
+
+            return true;
+        }),
 ];
