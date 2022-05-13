96f0cdb7d8a572888b59fec22404cec6982eb25f
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-05-13 10:41:11 +0200

    (1443) ajout filtre lieu d'intervention

--- packages/frontend/webapp/src/js/app/pages/PlanList/PlanFilters.vue
+++ packages/frontend/webapp/src/js/app/pages/PlanList/PlanFilters.vue
@@ -1,17 +1,30 @@
 <template>
     <nav>
-        <p>Filtrer par</p>
-        <CustomFilter
-            title="Champs d'intervention"
-            :options="topicOptions"
-            v-model="topicFilter"
-        />
+        <p class="mb-2">Filtrer par</p>
+        <div class="flex">
+            <CustomFilter
+                class="mr-2"
+                title="Champs d'intervention"
+                :options="topicOptions"
+                v-model="topicFilter"
+            />
+            <CustomFilter
+                title="Lieu d'intervention"
+                :options="interventionLocationFilters"
+                v-model="interventionLocationFilter"
+            />
+        </div>
     </nav>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
     computed: {
+        ...mapGetters({
+            interventionLocationFilters: "plansInterventionLocation"
+        }),
         topicOptions() {
             return this.$store.state.config.configuration.topics.map(
                 ({ uid, name }) => ({
@@ -27,6 +40,14 @@ export default {
             },
             set(value) {
                 this.$store.commit("setTopicFilter", value);
+            }
+        },
+        interventionLocationFilter: {
+            get() {
+                return this.$store.state.plans.interventionLocationFilter;
+            },
+            set(value) {
+                this.$store.commit("setInterventionLocationFilter", value);
             }
         }
     }
