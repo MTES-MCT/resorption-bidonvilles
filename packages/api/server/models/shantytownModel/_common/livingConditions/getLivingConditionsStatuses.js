b1b4bb4748aeffad0723b6bf55aba6985f1e669b
Anis Safine Laget <anis@users.noreply.github.com>
2022-05-17 10:43:54 +0200

    (1446) Handle versioning of serialization

--- packages/api/server/models/shantytownModel/_common/livingConditions/getLivingConditionsStatuses.js
+++
@@ -1,7 +0,0 @@
-const getStatusesV1 = require('./v1/statuses/main');
-
-const STRATEGIES = {
-    1: getStatusesV1,
-};
-
-module.exports = town => STRATEGIES[1](town);
--- packages/api/server/models/shantytownModel/_common/livingConditions/serializeLivingConditions.js
+++ packages/api/server/models/shantytownModel/_common/livingConditions/serializeLivingConditions.js
@@ -1,64 +1,7 @@
-const getLivingConditionsStatuses = require('./getLivingConditionsStatuses');
+const serializeV1 = require('./v1/serialize');
 
-module.exports = (town) => {
-    const livingConditions = {
-        version: town.livingConditionsVersion,
-        electricity: {
-            status: null,
-            type: {
-                id: town.electricityTypeId,
-                label: town.electricityTypeLabel,
-            },
-            comments: town.electricityComments,
-        },
-        water: {
-            status: null,
-            access: town.accessToWater,
-            comments: town.waterComments,
-            potable: town.waterPotable,
-            continuousAccess: town.waterContinuousAccess,
-            publicPoint: town.waterPublicPoint,
-            distance: town.waterDistance,
-            roadsToCross: town.waterRoadsToCross,
-            everyoneHasAccess: town.waterEveryoneHasAccess,
-            stagnantWater: town.waterStagnantWater,
-            handWashAccess: town.waterHandWashAccess,
-            handWashAccessNumber: town.waterHandWashAccessNumber,
-        },
-        trash: {
-            status: null,
-            evacuation: town.trashEvacuation,
-            cansOnSite: town.trashCansOnSite,
-            accumulation: town.trashAccumulation,
-            evacuationRegular: town.trashEvacuationRegular,
-        },
-        sanitary: {
-            status: null,
-            access: town.accessToSanitary,
-            comments: town.sanitaryComments,
-            number: town.sanitaryNumber,
-            insalubrious: town.sanitaryInsalubrious,
-            onSite: town.sanitaryOnSite,
-        },
-        vermin: {
-            status: null,
-            vermin: town.vermin,
-            comments: town.verminComments,
-        },
-        firePrevention: {
-            status: null,
-            measures: town.firePreventionMeasures,
-            diagnostic: town.firePreventionDiagnostic,
-            siteAccessible: town.firePreventionSiteAccessible,
-            devices: town.firePreventionDevices,
-            comments: town.firePreventionComments,
-        },
-    };
-
-    const statuses = getLivingConditionsStatuses(town);
-    Object.keys(statuses).forEach((key) => {
-        livingConditions[key].status = statuses[key];
-    });
-
-    return livingConditions;
+const STRATEGIES = {
+    1: serializeV1,
 };
+
+module.exports = town => STRATEGIES[1](town);
