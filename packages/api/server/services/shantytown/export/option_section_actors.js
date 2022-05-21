f121bd813cc4ff8f23e99c4d128661469d43c67d
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-05-21 21:08:23 +0200

    (1442) export file section intervenants

---
+++ packages/api/server/services/shantytown/export/option_section_actors.js
@@ -0,0 +1,69 @@
+const {
+    Paragraph, TextRun, SectionType,
+} = require('docx');
+
+const themes = require('#server/config/shantytown_actor_themes');
+const heading = require('./heading');
+
+
+module.exports = shantytown => ({
+    properties: {
+        type: SectionType.CONTINUOUS,
+    },
+    children: [
+        heading('Intervenants'),
+        new Paragraph({
+            spacing: {
+                before: 300,
+                after: 100,
+            },
+            children: [
+                ...shantytown.actors.reduce((acc, actor) => [
+                    ...acc,
+                    new TextRun({
+                        text: `    -    ${actor.first_name} ${actor.last_name}, `,
+                        break: 1,
+                        bold: true,
+                        size: 22,
+                        font: 'Arial',
+                    }),
+                    new TextRun({
+                        text: `${actor.organization.name}`,
+                        bold: false,
+                        size: 22,
+                        font: 'Arial',
+                    }),
+                    new TextRun({
+                        text: `         ${actor.themes.map(theme => `«${theme.value || themes[theme.id]}»`).join(' , ')}`,
+                        bold: false,
+                        break: 1,
+                        size: 22,
+                        font: 'Arial',
+                    }),
+                ], []),
+
+            ],
+        }),
+        new Paragraph({
+            spacing: {
+                before: 300,
+                after: 100,
+            },
+            children: [
+                new TextRun({
+                    text: 'Pour consulter les coordonnées, rendez-vous sur l\'annuaire de la plateforme ',
+                    bold: false,
+                    size: 22,
+                    font: 'Arial',
+                }),
+                new TextRun({
+                    text: 'Résorption-bidonvilles',
+                    bold: false,
+                    italics: true,
+                    size: 22,
+                    font: 'Arial',
+                }),
+            ],
+        }),
+    ],
+});
