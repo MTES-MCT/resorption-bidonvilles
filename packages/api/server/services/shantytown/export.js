1ca89d1631ee9053ee112fe73d6b632435186476
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-05-21 11:46:29 +0200

    (1442) adaptation export service aux options

--- packages/api/server/services/shantytown/export.js
+++ packages/api/server/services/shantytown/export.js
@@ -1,13 +1,41 @@
 const { Document, Packer } = require('docx');
+const logos = require('./export/section_logos');
 const context = require('./export/1_section_context');
+const people = require('./export/section_people');
 const lifeConditions = require('./export/2_section_life_conditions');
+const actors = require('./export/option_section_actors');
+const plans = require('./export/option_section_plans');
+const justiceProcedure = require('./export/option_justice_procedure');
+const comments = require('./export/option_section_comments');
+const changelog = require('./export/option_section_changelog');
 
-module.exports = (user, shantytown) => {
+module.exports = (user, shantytown, options) => {
+    const sections = [
+        logos(),
+        context(user, shantytown),
+        people(shantytown),
+        lifeConditions(shantytown),
+
+    ];
+
+    if (options.includes('actors')) {
+        sections.push(actors(shantytown));
+    }
+    if (options.includes('actions')) {
+        sections.push(plans(shantytown));
+    }
+    if (options.includes('justice')) {
+        sections.push(justiceProcedure(shantytown));
+    }
+    if (options.includes('comments')) {
+        sections.push(comments(shantytown));
+    }
+    if (options.includes('history')) {
+        sections.push(changelog(shantytown));
+    }
     const doc = new Document({
-        sections: [
-            context(user, shantytown),
-            lifeConditions(shantytown),
-        ],
+
+        sections,
     });
 
     return Packer.toBuffer(doc);
