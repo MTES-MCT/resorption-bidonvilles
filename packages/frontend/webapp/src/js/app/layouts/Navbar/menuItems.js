35be71d8c3f0377117430788ecad437a6e7e091b
Christophe BENARD <ch.benard@gmail.com>
2022-05-24 15:00:51 +0200

    (1467) Ajout d'une propriété links aux liens de la lower navbar - Cette propriété sert à ce que toutes les pages concernées soient liées à la section du menu dont elles relèvent

--- packages/frontend/webapp/src/js/app/layouts/Navbar/menuItems.js
+++ packages/frontend/webapp/src/js/app/layouts/Navbar/menuItems.js
@@ -32,11 +32,13 @@ export default {
         {
             label: "Accueil",
             target: "/",
+            links: ["/"],
             menu: "lower"
         },
         {
             label: "Covid-19",
             target: "/covid-19",
+            links: ["/covid-19"],
             classes: "font-bold uppercase",
             color: "text-red600",
             menu: "lower"
@@ -44,39 +46,55 @@ export default {
         {
             label: "Sites",
             target: "/liste-des-sites",
+            links: ["/liste-des-sites", "/site", "/nouveau-site"],
             menu: "lower",
             group: "townList"
         },
         {
             label: "Actions",
             target: "/liste-des-actions",
+            links: [
+                "/liste-des-actions",
+                "/nouvelle-action",
+                "/action",
+                "/modifier-action"
+            ],
             menu: "lower"
         },
         {
             label: "Annuaire",
             target: "/annuaire",
+            links: ["/annuaire"],
             menu: "lower",
             group: "directory"
         },
         {
             label: "Carte",
             target: "/cartographie",
+            links: ["/cartographie"],
             menu: "lower"
         },
         {
             label: "Dernières activités",
             target: "/activites",
+            links: ["/activites"],
             menu: "lower"
         },
         {
             label: "Statistiques",
             target: "/statistiques",
+            links: ["/statistiques"],
             menu: "lower",
             group: "stats"
         },
         {
             label: "Communauté",
             target: "/liste-des-utilisateurs",
+            links: [
+                "/liste-des-utilisateurs",
+                "/nouvel-utilisateur",
+                "/utilisateur"
+            ],
             menu: "lower",
             group: "users"
         },
