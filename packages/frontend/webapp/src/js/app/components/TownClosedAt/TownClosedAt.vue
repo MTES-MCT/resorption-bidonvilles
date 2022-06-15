a411ba846aca9369df4e86b58aa4ee803fa5ccca
Christophe BENARD <ch.benard@gmail.com>
2022-06-15 08:50:15 +0200

    (1465) Création d'un champ d'affichage de la date de fermeture pour la liste des sites fermés

---
+++ packages/frontend/webapp/src/js/app/components/TownClosedAt/TownClosedAt.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex items-center">
+        <div class="mr-2">
+            {{ formatDate(closedAt) }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        closedAt: {
+            type: Number,
+            required: true
+        }
+    },
+    methods: {
+        formatDate(timestamp) {
+            const dateObject = new Date(timestamp * 1000);
+            const year = dateObject.getFullYear();
+            const month = `${dateObject.getMonth() + 1}`.padStart(2, "0");
+            const day = `${dateObject.getDate()}`.padStart(2, "0");
+
+            return `${day}/${month}/${year}`;
+        }
+    }
+};
+</script>
