0ec5144b0d3d1e97af43eda4564b794063f1c910
Christophe BENARD <ch.benard@gmail.com>
2022-06-15 11:41:35 +0200

    (1465) Ajout de la méthode getDepartementForCity

--- packages/api/server/models/geoModel/index.js
+++ packages/api/server/models/geoModel/index.js
@@ -1,9 +1,11 @@
 const getDepartementsFor = require('./getDepartementsFor');
 const getLocation = require('./getLocation');
 const search = require('./search');
+const getDepartementForCity = require('./getDepartementForCity');
 
 module.exports = {
     getDepartementsFor,
     getLocation,
     search,
+    getDepartementForCity,
 };
