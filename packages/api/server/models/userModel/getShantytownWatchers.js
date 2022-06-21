b066637012be2be8ef35518a0381c1ad5c6264fc
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-06-21 17:46:53 +0200

    (1486) adapt envoi de mails

--- packages/api/server/models/userModel/getShantytownWatchers.js
+++ packages/api/server/models/userModel/getShantytownWatchers.js
@@ -1,8 +1,63 @@
 const sequelize = require('#db/sequelize');
 
-module.exports = async (shantytownId, canListPrivateComments) => sequelize.query(
-    `WITH constants(departement, epci) AS (
-        SELECT cities.fk_departement AS departement, cities.fk_epci AS epci
+module.exports = async (shantytownId, commentId, isPrivate) => {
+    if (isPrivate) {
+        return sequelize.query(`
+        WITH constants(departement) AS (
+            SELECT cities.fk_departement AS departement
+            FROM shantytowns
+            LEFT JOIN cities ON shantytowns.fk_city = cities.code
+            WHERE shantytowns.shantytown_id = :shantytownId
+        ),
+        email_subscriptions AS (
+            SELECT fk_user, ARRAY_AGG(email_subscription) AS subscriptions FROM user_email_subscriptions GROUP BY fk_user
+        )
+        
+        SELECT
+            u.email,
+            u.first_name,
+            u.last_name
+        FROM (
+            (SELECT scua.fk_user 
+            FROM shantytown_comment_user_access scua
+            WHERE scua.fk_comment = :commentId)
+
+            UNION
+
+            (SELECT u.user_id AS fk_user
+            FROM users u
+            LEFT JOIN localized_organizations lo ON u.fk_organization = lo.organization_id
+            LEFT JOIN shantytown_comment_organization_access scoa ON scoa.fk_organization = lo.organization_id
+            WHERE scoa.fk_comment = :commentId
+            )
+
+            UNION
+
+            (SELECT u.user_id AS fk_user
+            FROM users u
+            LEFT JOIN constants ON TRUE
+            LEFT JOIN localized_organizations lo ON u.fk_organization = lo.organization_id
+            LEFT JOIN user_actual_permissions up ON u.user_id = up.user_id
+            WHERE up.fk_feature = 'listPrivate' AND up.fk_entity = 'shantytown_comment' AND up.allowed IS TRUE
+            AND lo.departement_code = constants.departement
+            )
+        ) t
+        LEFT JOIN users u ON t.fk_user = u.user_id
+        LEFT JOIN email_subscriptions ON email_subscriptions.fk_user = u.user_id
+        LEFT JOIN constants ON TRUE
+        LEFT JOIN localized_organizations lo ON u.fk_organization = lo.organization_id
+        WHERE u.fk_status = 'active'
+        AND lo.active IS TRUE AND ('comment_notification' = ANY(email_subscriptions.subscriptions))`, {
+            type: sequelize.QueryTypes.SELECT,
+            replacements: {
+                shantytownId,
+                commentId,
+            },
+        });
+    }
+    return sequelize.query(
+        `WITH constants(departement) AS (
+        SELECT cities.fk_departement AS departement
         FROM shantytowns
         LEFT JOIN cities ON shantytowns.fk_city = cities.code
         WHERE shantytowns.shantytown_id = :shantytownId
@@ -25,29 +80,21 @@ module.exports = async (shantytownId, canListPrivateComments) => sequelize.query
         (SELECT u.user_id AS fk_user
         FROM users u
         LEFT JOIN constants ON TRUE
-        LEFT JOIN organizations o ON u.fk_organization = o.organization_id
-        WHERE o.fk_departement = constants.departement
-            AND o.active IS TRUE)
-
-        UNION
-
-        (SELECT u.user_id AS fk_user
-        FROM users u
-        LEFT JOIN constants ON TRUE
         LEFT JOIN localized_organizations lo ON u.fk_organization = lo.organization_id
-        WHERE lo.epci_code = constants.epci
+        WHERE lo.departement_code = constants.departement
             AND lo.active IS TRUE)
+
     ) t
     LEFT JOIN users u ON t.fk_user = u.user_id
     LEFT JOIN email_subscriptions ON email_subscriptions.fk_user = u.user_id
-    LEFT JOIN user_actual_permissions up ON t.fk_user = up.user_id AND up.fk_feature = 'listPrivate' AND up.fk_entity = 'shantytown_comment' AND up.allowed IS TRUE
     WHERE u.fk_status = 'active' AND ('comment_notification' = ANY(email_subscriptions.subscriptions))
-    ${canListPrivateComments === true ? 'AND up.allowed IS TRUE' : ''}
     `,
-    {
-        type: sequelize.QueryTypes.SELECT,
-        replacements: {
-            shantytownId,
+        {
+            type: sequelize.QueryTypes.SELECT,
+            replacements: {
+                shantytownId,
+                commentId,
+            },
         },
-    },
-);
+    );
+};
