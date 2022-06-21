fafaaa9037eeda57a426f5508988c98a7bda0d50
Christophe BENARD <ch.benard@gmail.com>
2022-06-21 15:01:30 +0200

    (1465) Debuf infos cleaned

--- packages/api/server/loaders/routesLoader.ts
+++ packages/api/server/loaders/routesLoader.ts
@@ -691,18 +691,4 @@ export default (app) => {
         middlewares.appVersion.sync,
         controllers.contactFormReferral.export,
     );
-
-    app.use((error, req, res, next) => {
-        console.log("Error Handling Middleware called")
-        console.log('Path: ', req.path)
-        console.error('Error: ', error)
-       
-        if (error.type == 'redirect')
-            res.redirect('/error')
-      
-         else if (error.type == 'time-out') // arbitrary condition check
-            res.status(408).send(error)
-        else
-            res.status(500).send(error)
-      })      
 };
