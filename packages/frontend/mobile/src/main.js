d99f5d95980f2aa91af7f6f9cf1eaa9ef87f0780
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-06-29 15:39:36 +0200

    (1495) creation store mobile

--- packages/frontend/mobile/src/main.js
+++ packages/frontend/mobile/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import store from "#src/store/index";
 
 new Vue({
   el: "#app",
+  store,
   render: (h) => h(App),
 });
