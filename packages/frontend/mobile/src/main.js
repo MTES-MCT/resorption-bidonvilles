a50458aa3f585ab9abc7605b296f9fd4c1fadf76
Christophe BENARD <ch.benard@gmail.com>
2022-06-30 11:10:22 +0200

    (1495) Ajout de tailwindcss

--- packages/frontend/mobile/src/main.js
+++ packages/frontend/mobile/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "#src/store/index";
+import '@resorptionbidonvilles/ui/src/css/styles.css';
 
 new Vue({
   el: "#app",
