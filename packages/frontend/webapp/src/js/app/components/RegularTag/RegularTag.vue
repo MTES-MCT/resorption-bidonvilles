d5d4b208c972f3ecafed98743bffedae239764bd
Christophe BENARD <ch.benard@gmail.com>
2022-07-04 15:58:18 +0200

    (1484) Création d'un composant tag pour qualifier les commentaires réguliers

---
+++ packages/frontend/webapp/src/js/app/components/RegularTag/RegularTag.vue
@@ -0,0 +1,21 @@
+<template>
+    <Tag
+        variant="withoutBackground"
+        :class="[
+            'inline-block text-blue600 ',
+            tag.type === 'ordinaire' ? 'bg-blue200' : 'bg-red300'
+        ]"
+        >{{ tag.label }}
+    </Tag>
+</template>
+
+<script>
+export default {
+    props: {
+        tag: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
