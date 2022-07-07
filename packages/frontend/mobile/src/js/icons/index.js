c98a0cdbedeeec1a897f146999860b7b8fde8277
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-07-07 17:48:18 +0200

    (1495) add icons to mobile

---
+++ packages/frontend/mobile/src/js/icons/index.js
@@ -0,0 +1,179 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTwitterSquare,
+  faFacebookSquare,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faChalkboardTeacher,
+  faFlag,
+  faLock,
+  faPaperPlane,
+  faUnlink,
+  faUser,
+  faUserCircle,
+  faUserCheck,
+  faUserShield,
+  faArrowRight,
+  faEye,
+  faEyeSlash,
+  faPencilAlt,
+  faTimes,
+  faTimesCircle,
+  faMapMarkedAlt,
+  faChartLine,
+  faComments,
+  faEnvelope,
+  faPlayCircle,
+  faFileExcel,
+  faSortDown,
+  faCaretSquareLeft,
+  faCaretSquareRight,
+  faCaretUp,
+  faCaretDown,
+  faComment,
+  faTrashAlt,
+  faHistory,
+  faMapPin,
+  faFilePdf,
+  faUsers,
+  faUserPlus,
+  faFileDownload,
+  faAddressBook,
+  faSync,
+  faInfoCircle,
+  faList,
+  faTable,
+  faBookReader,
+  faEuroSign,
+  faPlusCircle,
+  faIdCard,
+  faUserMd,
+  faBriefcase,
+  faHome,
+  faSeedling,
+  faQuestion,
+  faTint,
+  faTintSlash,
+  faHandHoldingMedical,
+  faMapMarkerAlt,
+  faBook,
+  faFileSignature,
+  faChevronUp,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faSpinner,
+  faArrowLeft,
+  faSearch,
+  faPlus,
+  faCheck,
+  faCheckCircle,
+  faPen,
+  faExclamation,
+  faExclamationTriangle,
+  faBan,
+  faMale,
+  faBalanceScale,
+  faScroll,
+  faCalendar,
+  faPrint,
+  faFile,
+  faCopy,
+  faExternalLinkAlt,
+  faPhone,
+  faSchool,
+  faBullseye,
+  faSquare,
+  faBars,
+  faChild,
+  faArrowAltCircleRight,
+  faBolt,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faChalkboardTeacher);
+library.add(faFlag);
+library.add(faBan);
+library.add(faChevronDown);
+library.add(faPaperPlane);
+library.add(faUnlink);
+library.add(faUser);
+library.add(faUserCheck);
+library.add(faUserShield);
+library.add(faArrowRight);
+library.add(faEye);
+library.add(faEyeSlash);
+library.add(faPencilAlt);
+library.add(faTimes);
+library.add(faTimesCircle);
+library.add(faMapMarkedAlt);
+library.add(faChartLine);
+library.add(faComments);
+library.add(faEnvelope);
+library.add(faPhone);
+library.add(faPlayCircle);
+library.add(faFileExcel);
+library.add(faSortDown);
+library.add(faCaretSquareLeft);
+library.add(faCaretSquareRight);
+library.add(faCaretUp);
+library.add(faCaretDown);
+library.add(faComment);
+library.add(faTrashAlt);
+library.add(faHistory);
+library.add(faMapPin);
+library.add(faFilePdf);
+library.add(faUsers);
+library.add(faUserCircle);
+library.add(faUserPlus);
+library.add(faFileDownload);
+library.add(faAddressBook);
+library.add(faSync);
+library.add(faInfoCircle);
+library.add(faList);
+library.add(faTable);
+library.add(faBookReader);
+library.add(faEuroSign);
+library.add(faPlusCircle);
+library.add(faIdCard);
+library.add(faUserMd);
+library.add(faBriefcase);
+library.add(faHome);
+library.add(faSeedling);
+library.add(faQuestion);
+library.add(faTint);
+library.add(faTintSlash);
+library.add(faHandHoldingMedical);
+library.add(faMapMarkerAlt);
+library.add(faBook);
+library.add(faFileSignature);
+library.add(faChevronUp);
+library.add(faChevronLeft);
+library.add(faChevronRight);
+library.add(faArrowLeft);
+library.add(faSpinner);
+library.add(faTwitterSquare);
+library.add(faFacebookSquare);
+library.add(faSearch);
+library.add(faPlus);
+library.add(faCheck);
+library.add(faCheckCircle);
+library.add(faPen);
+library.add(faExclamation);
+library.add(faExclamationTriangle);
+library.add(faMale);
+library.add(faBalanceScale);
+library.add(faScroll);
+library.add(faCalendar);
+library.add(faPrint);
+library.add(faFile);
+library.add(faCopy);
+library.add(faLock);
+library.add(faExternalLinkAlt);
+library.add(faGithub);
+library.add(faSchool);
+library.add(faBullseye);
+library.add(faSquare);
+library.add(faBars);
+library.add(faChild);
+library.add(faArrowAltCircleRight);
+library.add(faBolt);
