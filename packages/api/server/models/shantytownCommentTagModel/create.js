796ff1347ef9a8d0f56347488e7c05200c8a46d5
Christophe BENARD <ch.benard@gmail.com>
2022-07-20 14:43:12 +0200

    (1484) Insertion de tous les tags au lieu d'un seul à la fois

--- packages/api/server/models/shantytownCommentTagModel/create.js
+++ packages/api/server/models/shantytownCommentTagModel/create.js
@@ -1,25 +1,23 @@
 const sequelize = require('#db/sequelize');
 
-module.exports = async (commentId, commentTagId) => sequelize.transaction(async (transaction) => {
-    // comment
-    await sequelize.query(
-        `INSERT INTO
-                shantytown_comment_tags(
-                    fk_shantytown_comment,
-                    fk_comment_tag
-                )
-                
-            VALUES
-                (
-                    :commentId,
-                    :commentTagId
-                )`,
+/**
+ * @param {Number} commentId
+ * @param {String} commentTagId
+ * @returns
+ */
+module.exports = async (commentId, commentTagIds, argTransaction = undefined) => {
+    let transaction = argTransaction;
+    if (transaction === undefined) {
+        transaction = await sequelize.transaction();
+    }
+    await sequelize.getQueryInterface().bulkInsert(
+        'shantytown_comment_tags',
+        commentTagIds.map(comment_tag_id => ({
+            fk_shantytown_comment: commentId,
+            fk_comment_tag: comment_tag_id,
+        })),
         {
-            replacements: {
-                commentId,
-                commentTagId,
-            },
             transaction,
         },
     );
-});
+};
