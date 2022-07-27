1bb73ed372e7b3f314505482b33aa158eec77c88
Anis Safine Laget <anis@users.noreply.github.com>
2022-07-27 15:11:41 +0200

    (1446) Fix type error

--- packages/frontend/webapp/src/js/app/components/TownForm/TownForm.vue
+++ packages/frontend/webapp/src/js/app/components/TownForm/TownForm.vue
@@ -156,7 +156,7 @@ export default {
         return {
             nearbyClosedShantytowns: [],
             mainError: null,
-            errors: [],
+            errors: {},
             loading: false,
             showInfo: true,
             initialTown: this.formatTown(this.data),
