ecd1ef27a2d9315e833a5f28bfc9ff44cf31d003
Christophe BENARD <ch.benard@gmail.com>
2022-08-02 14:50:37 +0200

    (1465) Restauration du calcul de la liste des watchers comme faite initialement - Le calcul ne retournait qu'un utilisateur pour les fins de test en mode Dev.

--- packages/api/server/models/userModel/getLocationWatchers.js
+++ packages/api/server/models/userModel/getLocationWatchers.js
@@ -3,7 +3,6 @@ const { mailBlacklist } = require('#server/config');
 
 module.exports = async (location, watcherType = 'shantytown_creation', applyBlacklist = false) => {
     const users = await sequelize.query(
-        /*
         `WITH email_subscriptions AS (
             SELECT fk_user, ARRAY_AGG(email_subscription) AS subscriptions FROM user_email_subscriptions GROUP BY fk_user
         )
@@ -24,20 +23,6 @@ module.exports = async (location, watcherType = 'shantytown_creation', applyBlac
             AND u.fk_status = 'active'
             AND lo.active = TRUE
             AND (:watcherType = ANY(email_subscriptions.subscriptions))`,
-*/
-        `WITH email_subscriptions AS (
-            SELECT fk_user, ARRAY_AGG(email_subscription) AS subscriptions FROM user_email_subscriptions GROUP BY fk_user
-        )
-        SELECT
-            u.user_id,
-            u.email,
-            u.first_name,
-            u.last_name
-        FROM localized_organizations lo
-        LEFT JOIN users u ON u.fk_organization = lo.organization_id
-        LEFT JOIN email_subscriptions ON email_subscriptions.fk_user = u.user_id
-        WHERE
-            u.user_id = 513`,
         {
             type: sequelize.QueryTypes.SELECT,
             replacements: {
