716e50c2ba44a727a7850f3fbd1d6f09e95cb008
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-08-16 16:18:00 +0200

    (1525) FRONT creation inputUpdatedAt

---
+++ packages/frontend/webapp/src/js/app/components/TownForm/TownFormPanelUpdatedAt.vue
@@ -0,0 +1,51 @@
+<template>
+    <FormGroup title="Date de mise à jour">
+        <FormParagraph
+            title="Les données correspondent-elles à une date antérieure à aujourd'hui ?"
+        >
+            <div>
+                <p class="mb-4">
+                    Il est impossible de spécifier une date antérieure à la
+                    dernière modification
+                </p>
+                <InputUpdatedAt
+                    v-model="updatedAt"
+                    :disableBefore="disableBefore"
+                ></InputUpdatedAt>
+            </div>
+        </FormParagraph>
+    </FormGroup>
+</template>
+
+<script>
+import InputUpdatedAt from "./inputs/InputUpdatedAt.vue";
+
+export default {
+    components: {
+        InputUpdatedAt
+    },
+
+    props: {
+        value: {
+            type: Date,
+            required: true
+        },
+
+        disableBefore: {
+            type: Date,
+            required: false
+        }
+    },
+
+    data() {
+        return {
+            updatedAt: this.value
+        };
+    },
+    watch: {
+        updatedAt() {
+            this.$emit("input", this.updatedAt);
+        }
+    }
+};
+</script>
---
+++ packages/frontend/webapp/src/js/app/components/TownForm/inputs/InputUpdatedAt.vue
@@ -0,0 +1,42 @@
+<template>
+    <DatepickerV2
+        id="updated_at"
+        label="Date de mise à jour"
+        v-model="updatedAt"
+        rules="required"
+        :disabled-dates="{ to: this.disableBefore, from: new Date() }"
+        cypressName="updated_at"
+    ></DatepickerV2>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Date,
+            required: false
+        },
+
+        disableBefore: {
+            type: Date,
+            required: false
+        }
+    },
+
+    data() {
+        return {
+            updatedAt: new Date()
+        };
+    },
+
+    watch: {
+        value() {
+            this.input = this.value;
+        },
+
+        updatedAt() {
+            this.$emit("input", this.updatedAt);
+        }
+    }
+};
+</script>
