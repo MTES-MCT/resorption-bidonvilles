041ee669ac0acc0c8da29b67920656d1c0ca964a
Anis Safine Laget <anis@users.noreply.github.com>
2022-08-23 11:27:10 +0200

    (1532) feed: wording

--- packages/frontend/webapp/src/js/app/pages/TownsList/TownsListFilters/TownsListFilters.vue
+++ packages/frontend/webapp/src/js/app/pages/TownsList/TownsListFilters/TownsListFilters.vue
@@ -187,7 +187,7 @@ export default {
                     ]
                 },
                 {
-                    label: "AlerteCanicule",
+                    label: "Alerte Canicule",
                     id: "heatwave",
                     display: true,
                     options: [
