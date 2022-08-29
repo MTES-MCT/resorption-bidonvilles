2cfc0dd4ec19548133c37d985ac7b816531f90cf
Anis Safine Laget <anis@users.noreply.github.com>
2022-08-29 11:19:07 +0200

    (1484) feed: simplify model method

--- packages/api/server/models/shantytownCommentTagModel/create.js
+++ packages/api/server/models/shantytownCommentTagModel/create.js
@@ -5,19 +5,13 @@ const sequelize = require('#db/sequelize');
  * @param {String} commentTagId
  * @returns
  */
-module.exports = async (commentId, commentTagIds, argTransaction = undefined) => {
-    let transaction = argTransaction;
-    if (transaction === undefined) {
-        transaction = await sequelize.transaction();
-    }
-    await sequelize.getQueryInterface().bulkInsert(
-        'shantytown_comment_tags',
-        commentTagIds.map(comment_tag_id => ({
-            fk_shantytown_comment: commentId,
-            fk_comment_tag: comment_tag_id,
-        })),
-        {
-            transaction,
-        },
-    );
-};
+module.exports = (commentId, commentTagIds, transaction = undefined) => sequelize.getQueryInterface().bulkInsert(
+    'shantytown_comment_tags',
+    commentTagIds.map(comment_tag_id => ({
+        fk_shantytown_comment: commentId,
+        fk_comment_tag: comment_tag_id,
+    })),
+    {
+        transaction,
+    },
+);
