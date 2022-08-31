36db58ef4ecbcab1b86dd8902782a3a2a3538303
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-08-31 16:29:37 +0200

    (1463) extend email notif to full organization

--- packages/api/server/models/userModel/getPlanObservers.js
+++ packages/api/server/models/userModel/getPlanObservers.js
@@ -6,6 +6,12 @@ module.exports = async (planId, commentId) => sequelize.query(
         FROM plan_departements
         WHERE fk_plan = :planId
     ),
+    operator_organization AS (
+        SELECT lo.organization_id, po.fk_plan
+        FROM plan_operators po
+        LEFT JOIN users u  ON po.fk_user = u.user_id
+        LEFT JOIN localized_organizations lo ON lo.organization_id = u.fk_organization
+    ),
     email_unsubscriptions AS (
         SELECT fk_user, ARRAY_AGG(email_subscription) AS unsubscriptions FROM user_email_unsubscriptions GROUP BY fk_user
     )
@@ -17,13 +23,13 @@ module.exports = async (planId, commentId) => sequelize.query(
     FROM users u
     LEFT JOIN constants ON TRUE
     LEFT JOIN localized_organizations lo ON u.fk_organization = lo.organization_id
+    LEFT JOIN operator_organization op ON op.organization_id = lo.organization_id AND op.fk_plan = :planId
     LEFT JOIN email_unsubscriptions ON email_unsubscriptions.fk_user = u.user_id
-    LEFT JOIN plan_operators po ON po.fk_user = u.user_id AND po.fk_plan = :planId
     WHERE u.fk_status = 'active'
         AND (lo.departement_code = constants.departement
             AND lo.active IS TRUE)
         AND (email_unsubscriptions.unsubscriptions IS NULL OR NOT('plan_comment_notification' = ANY(email_unsubscriptions.unsubscriptions)))
-        AND (u.fk_role_regular = 'direct_collaborator' OR po.fk_plan IS NOT null)
+        AND (u.fk_role_regular = 'direct_collaborator' OR op.fk_plan IS NOT null)
     `,
     {
         type: sequelize.QueryTypes.SELECT,
