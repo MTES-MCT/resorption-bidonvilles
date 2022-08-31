ee488776d58931722af5edf66db93570a415805b
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-08-31 16:31:16 +0200

    (1463) enable unsubscription from plan_comment email notification

---
+++ packages/api/db/migrations/30000023-03-add-email-unsubscription-plan-comment.js
@@ -0,0 +1,58 @@
+module.exports = {
+    async up(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+        await queryInterface.sequelize.query(
+            "ALTER TYPE enum_user_email_subscriptions_email_subscription ADD VALUE 'plan_comment_notification'",
+            {
+                transaction,
+            },
+        );
+
+        await transaction.commit();
+    },
+
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        await Promise.all(
+            [
+                queryInterface.sequelize.query(
+                    "DELETE FROM user_email_unsubscriptions WHERE email_subscription = 'plan_comment_notification'",
+                    {
+                        transaction,
+                    },
+                ),
+
+                queryInterface.sequelize.query(
+                    "CREATE TYPE enum_user_email_subscriptions_email_subscription_new AS ENUM ('weekly_summary', 'comment_notification', 'shantytown_closure', 'shantytown_creation')",
+                    {
+                        transaction,
+                    },
+                ),
+                queryInterface.sequelize.query(
+                    `ALTER TABLE user_email_unsubscriptions
+                        ALTER COLUMN email_subscription TYPE enum_user_email_subscriptions_email_subscription_new
+                      USING (email_subscription::text::enum_user_email_subscriptions_email_subscription_new)`,
+                    {
+                        transaction,
+                    },
+                ),
+                queryInterface.sequelize.query(
+                    'DROP TYPE enum_user_email_subscriptions_email_subscription',
+                    {
+                        transaction,
+                    },
+                ),
+                queryInterface.sequelize.query(
+                    'ALTER TYPE enum_user_email_subscriptions_email_subscription_new RENAME TO enum_user_email_subscriptions_email_subscription',
+                    {
+                        transaction,
+                    },
+                ),
+            ],
+        );
+
+        await transaction.commit();
+    },
+
+};
--- packages/api/server/config/email_subscriptions.js
+++ packages/api/server/config/email_subscriptions.js
@@ -1,6 +1,7 @@
 module.exports = [
     'weekly_summary',
     'comment_notification',
+    'plan_comment_notification',
     'shantytown_closure',
     'shantytown_creation',
 ];
