2a77c4d3036a1ec30d1b90e5a6b08f3e58952095
Christophe BENARD <ch.benard@gmail.com>
2022-09-01 17:22:01 +0200

    (1551) inclure les admin nationaux dans le calcul des WAU

--- packages/api/server/models/statsModel/wau.js
+++ packages/api/server/models/statsModel/wau.js
@@ -8,7 +8,7 @@ module.exports = () => sequelize.query(
         SELECT user_navigation_logs.fk_user, DATE_TRUNC('week', user_navigation_logs.datetime) AS monday
         FROM user_navigation_logs
         LEFT JOIN users ON user_navigation_logs.fk_user = users.user_id
-        WHERE user_navigation_logs.datetime < DATE_TRUNC('week', NOW()) AND (users.fk_role <> 'national_admin' OR users.fk_role IS NULL)
+        WHERE user_navigation_logs.datetime < DATE_TRUNC('week', NOW()) 
     ) t
     GROUP BY t.monday ORDER BY t.monday ASC`,
     {
