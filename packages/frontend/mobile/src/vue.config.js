d95546c60578471a075652e381b8b78a9580fa08
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-08 10:57:17 +0200

    (1495) Init PWA

--- packages/frontend/mobile/src/vue.config.js
+++ packages/frontend/mobile/src/vue.config.js
@@ -2,37 +2,49 @@ const path = require("path");
 const { DefinePlugin } = require("webpack");
 const { version } = require("./package.json");
 
-const {
-    VUE_APP_MOBILE_HOST
-} = require("./src/env.js");
+const { VUE_APP_MOBILE_HOST } = require("./src/env.js");
 
 module.exports = {
-    css: {
-        loaderOptions: { 
-            postcss: {
-                theme: {},
-                variants: {},
-                plugins: [require("tailwindcss"), require("autoprefixer")()]
-            }
-        }
+  css: {
+    loaderOptions: {
+      postcss: {
+        theme: {},
+        variants: {},
+        plugins: [require("tailwindcss"), require("autoprefixer")()],
+      },
     },
+  },
 
-    configureWebpack: {
-        devServer: {
-            host: "0.0.0.0",
-            port: "8093",
-            public: VUE_APP_MOBILE_HOST,
-            disableHostCheck: true
-        },
-        plugins: [
-            new DefinePlugin({
-                "process.env.APP_VERSION": JSON.stringify(version)
-            })
-        ]
+  configureWebpack: {
+    devServer: {
+      host: "0.0.0.0",
+      port: "8093",
+      public: VUE_APP_MOBILE_HOST,
+      disableHostCheck: true,
     },
+    plugins: [
+      new DefinePlugin({
+        "process.env.APP_VERSION": JSON.stringify(version),
+      }),
+    ],
+  },
 
-    chainWebpack: config => {
-        config.resolve.alias
-            .set("#src", path.resolve(__dirname, "./src/"));
+  chainWebpack: (config) => {
+    config.resolve.alias.set("#src", path.resolve(__dirname, "./src/"));
+  },
+
+  pwa: {
+    name: "Résorption-bidonvilles",
+    themeColor: "#00006D",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
     },
+  },
 };
