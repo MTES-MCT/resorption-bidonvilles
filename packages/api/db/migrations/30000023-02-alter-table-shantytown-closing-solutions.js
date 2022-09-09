2d52be9a09e96d3aaa0031c8270fad5c67231b6f
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-09 12:38:20 +0200

    (1539) correction migration closing_solutions

--- packages/api/db/migrations/30000023-02-alter-table-shantytown-closing-solutions.js
+++ packages/api/db/migrations/30000023-02-alter-table-shantytown-closing-solutions.js
@@ -1,183 +1,110 @@
+
+const oldValues = [
+    'Mise à l’abri / hébergement d’urgence (CHU)',
+    'Dispositif dédié (CADA, HUDA, CAO, CHUM, ARV, ...)',
+    'Dispositif spécifique d’insertion au territoire',
+    'Hébergement d’insertion (CHRS, ALT)',
+    'Logement ordinaire ou adapté (résidences sociales, IML, pensions de famille, logements social ou privé accompagnés)',
+    'Dispositifs de veille sociale ou sans solution',
+];
+
+const newValues = [
+    'Mise à l’abri temporaire (quelques jours ou semaines)',
+    'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion',
+    'Logement',
+    'Aucune',
+    'Autre',
+];
+
 const solution_correspondance = [
-    { oldValues: ['Mise à l’abri / hébergement d’urgence (CHU)', 'Dispositif dédié (CADA, HUDA, CAO, CHUM, ARV, ...)'], newValue: 'Mise à l’abri temporaire (quelques jours ou semaines)' },
-    { oldValues: ['Dispositif spécifique d’insertion au territoire', 'Hébergement d’insertion (CHRS, ALT)', 'Logement ordinaire ou adapté (résidences sociales, IML, pensions de famille, logements social ou privé accompagnés)'], newValue: 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion' },
-    { oldValues: ['Dispositifs de veille sociale ou sans solution'], newValue: 'Aucune' },
+    {
+        oldValues:
+            [
+                'Mise à l’abri / hébergement d’urgence (CHU)',
+                'Dispositif dédié (CADA, HUDA, CAO, CHUM, ARV, ...)',
+            ],
+        newValue: 'Mise à l’abri temporaire (quelques jours ou semaines)',
+    },
+    {
+        oldValues:
+            [
+                'Dispositif spécifique d’insertion au territoire',
+                'Hébergement d’insertion (CHRS, ALT)',
+                'Logement ordinaire ou adapté (résidences sociales, IML, pensions de famille, logements social ou privé accompagnés)',
+            ],
+        newValue: 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion',
+    },
+    {
+        oldValues: ['Dispositifs de veille sociale ou sans solution'],
+        newValue: 'Aucune',
+    },
 ];
 
 
 module.exports = {
     async up(queryInterface, Sequelize) {
         const transaction = await queryInterface.sequelize.transaction();
-        // create temporary tables
-        await queryInterface.createTable(
-            'closing_solutions_temp',
-            {
-                closing_solution_id: {
-                    type: Sequelize.INTEGER,
-                    allowNull: false,
-                    primaryKey: true,
-                    autoIncrement: true,
-                },
-                label: {
-                    type: Sequelize.STRING,
-                    allowNull: false,
-                },
-                created_at: {
-                    type: Sequelize.DATE,
-                    allowNull: false,
-                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                },
-                updated_at: {
-                    type: Sequelize.DATE,
-                    allowNull: false,
-                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                    onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
-                },
-            }, { transaction },
-        );
-        // populate closing_solutions_temp with new values
-        await Promise.all(
-            [
 
-                queryInterface.bulkInsert(
-                    'closing_solutions_temp',
-                    [
-                        { label: 'Mise à l’abri temporaire (quelques jours ou semaines)' },
-                        { label: 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion' },
-                        { label: 'Logement' },
-                        { label: 'Aucune' },
-                        { label: 'Autre' },
-                    ],
-                    { transaction },
-                ),
-            ],
-        );
         await Promise.all(
             [
-                queryInterface.createTable(
-                    'shantytown_closing_solutions_temp',
-                    {
-                        fk_shantytown: {
-                            type: Sequelize.INTEGER,
-                            allowNull: false,
-                        },
-                        fk_closing_solution: {
-                            type: Sequelize.INTEGER,
-                            allowNull: false,
-                        },
-                        number_of_people_affected: {
-                            type: Sequelize.INTEGER,
-                            allowNull: true,
-                        },
-                        number_of_households_affected: {
-                            type: Sequelize.INTEGER,
-                            allowNull: true,
-                        },
-                        message: {
-                            type: Sequelize.STRING,
-                            allowNull: true,
-                        },
-                        created_at: {
-                            type: Sequelize.DATE,
-                            allowNull: false,
-                            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                        },
-                        updated_at: {
-                            type: Sequelize.DATE,
-                            allowNull: false,
-                            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                            onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
-                        },
-                    },
+                // populate closing_solutions with new values
+                queryInterface.bulkInsert(
+                    'closing_solutions',
+                    newValues.map(solution => ({ label: solution })),
                     { transaction },
                 ),
+                // add column message to shantytown_closing_solutions
+                queryInterface.addColumn(
+                    'shantytown_closing_solutions',
+                    'message',
+                    {
+                        type: Sequelize.STRING,
+                        allowNull: true,
+                    },
+                    {
+                        transaction,
+                    },
+                ),
+                // remove not used table
+                queryInterface.dropTable('ShantytownClosingSolutionHistories', { transaction }),
             ],
         );
-        // create constraints
-        await Promise.all([
-            queryInterface.addConstraint('closing_solutions_temp', ['label'], {
-                type: 'unique',
-                name: 'uk_closing_solutions_label_temp',
-                transaction,
-            }),
-            queryInterface.addConstraint(
-                'shantytown_closing_solutions_temp',
-                ['fk_shantytown'],
-                {
-                    type: 'foreign key',
-                    name: 'fk_shantytown_closing_solutions_shantytown_temp',
-                    references: {
-                        table: 'shantytowns',
-                        field: 'shantytown_id',
-                    },
-                    onDelete: 'cascade',
-                    onUpdate: 'cascade',
-                    transaction,
-                },
-            ),
-            queryInterface.addConstraint(
-                'shantytown_closing_solutions_temp',
-                ['fk_closing_solution'],
-                {
-                    type: 'foreign key',
-                    name: 'fk_shantytown_closing_solutions_solution_temp',
-                    references: {
-                        table: 'closing_solutions_temp',
-                        field: 'closing_solution_id',
-                    },
-                    onUpdate: 'cascade',
-                    onDelete: 'cascade',
-                    transaction,
-                },
-            ),
-            queryInterface.addConstraint('shantytown_closing_solutions_temp', ['fk_shantytown', 'fk_closing_solution'], {
-                type: 'primary key',
-                name: 'pk_shantytown_closing_solutions_temp',
-                transaction,
-            }),
-        ]);
 
         // populate shantytown_closing_solutions with correspondance
         await Promise.all(solution_correspondance.map(correspondance => queryInterface.sequelize.query(
-            `INSERT INTO shantytown_closing_solutions_temp
+            `INSERT INTO shantytown_closing_solutions
                     (fk_shantytown, fk_closing_solution, number_of_people_affected, number_of_households_affected)
-                SELECT fk_shantytown, cst.closing_solution_id, SUM(number_of_people_affected), SUM(number_of_households_affected)
+                WITH constantes(newClosingSolutionId) AS (
+                    SELECT closing_solution_id FROM closing_solutions WHERE label = '${correspondance.newValue}'
+                )
+                SELECT scs.fk_shantytown, constantes.newClosingSolutionId, SUM(scs.number_of_people_affected), SUM(scs.number_of_households_affected)
                 FROM shantytown_closing_solutions scs
                 LEFT JOIN closing_solutions cs ON cs.closing_solution_id = scs.fk_closing_solution
-                LEFT JOIN closing_solutions_temp cst ON cst.label = '${correspondance.newValue}'
+                LEFT JOIN constantes ON true
                 WHERE cs.label IN ('${correspondance.oldValues.join('\', \'')}')
-                GROUP BY fk_shantytown, cst.closing_solution_id
+                GROUP BY scs.fk_shantytown, constantes.newClosingSolutionId
             `,
             { transaction },
         )));
 
-        // drop old tables, rename new
-        await Promise.all(
-            [
-                queryInterface.dropTable('ShantytownClosingSolutionHistories', { transaction }),
-                queryInterface.dropTable('shantytown_closing_solutions', { transaction }),
-                queryInterface.dropTable('closing_solutions', { transaction }),
-                queryInterface.sequelize.query(
-                    `ALTER TABLE shantytown_closing_solutions_temp
-                    RENAME TO shantytown_closing_solutions
-                    `,
-                    { transaction },
-                ),
-                queryInterface.sequelize.query(
-                    `ALTER TABLE closing_solutions_temp
-                    RENAME TO closing_solutions
-                    `,
-                    { transaction },
-                ),
-            ],
+        await queryInterface.sequelize.query(
+            `DELETE FROM shantytown_closing_solutions
+            WHERE fk_closing_solution IN
+                (
+                    SELECT closing_solution_id 
+                    FROM closing_solutions
+                    WHERE label IN ('${oldValues.join('\', \'')}')
+                )
+            `,
+            { transaction },
         );
 
-        await Promise.all(
-            [
-
-            ],
+        await queryInterface.sequelize.query(
+            `DELETE FROM closing_solutions
+            WHERE label IN ('${oldValues.join('\', \'')}')
+            `,
+            { transaction },
         );
-
         return transaction.commit();
     },
 
@@ -185,76 +112,9 @@ module.exports = {
     async down(queryInterface, Sequelize) {
         const transaction = await queryInterface.sequelize.transaction();
 
-        await Promise.all([
-            queryInterface.dropTable('shantytown_closing_solutions', { transaction }),
-            queryInterface.dropTable('closing_solutions', { transaction }),
-
-        ]);
-
-        // create tables closing_solutions, shantytown_closing_solutions and ShantytownClosingSolutionHistories
         await Promise.all(
             [
-                queryInterface.createTable(
-                    'closing_solutions',
-                    {
-                        closing_solution_id: {
-                            type: Sequelize.INTEGER,
-                            allowNull: false,
-                            primaryKey: true,
-                            autoIncrement: true,
-                        },
-                        label: {
-                            type: Sequelize.STRING,
-                            allowNull: false,
-                        },
-                        created_at: {
-                            type: Sequelize.DATE,
-                            allowNull: false,
-                            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                        },
-                        updated_at: {
-                            type: Sequelize.DATE,
-                            allowNull: false,
-                            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                            onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
-                        },
-                    },
-                    { transaction },
-                ),
-
-                queryInterface.createTable(
-                    'shantytown_closing_solutions',
-                    {
-                        fk_shantytown: {
-                            type: Sequelize.INTEGER,
-                            allowNull: false,
-                        },
-                        fk_closing_solution: {
-                            type: Sequelize.INTEGER,
-                            allowNull: false,
-                        },
-                        number_of_people_affected: {
-                            type: Sequelize.INTEGER,
-                            allowNull: true,
-                        },
-                        number_of_households_affected: {
-                            type: Sequelize.INTEGER,
-                            allowNull: true,
-                        },
-                        created_at: {
-                            type: Sequelize.DATE,
-                            allowNull: false,
-                            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                        },
-                        updated_at: {
-                            type: Sequelize.DATE,
-                            allowNull: false,
-                            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                            onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
-                        },
-                    },
-                    { transaction },
-                ),
+                // create table ShantytownClosingSolutionHistories
                 queryInterface.createTable(
                     'ShantytownClosingSolutionHistories',
                     {
@@ -298,52 +158,37 @@ module.exports = {
                         },
                     },
                     { transaction },
-                )],
+                ),
+                // delete column message in shantytown_closing_solutions
+                queryInterface.removeColumn(
+                    'shantytown_closing_solutions',
+                    'message',
+                    { transaction },
+                ),
+                // delete values in shantytown_closing_solutions
+                queryInterface.sequelize.query(
+                    'DELETE FROM shantytown_closing_solutions',
+                    { transaction },
+                ),
+                // populate closing_solutions with old values
+                queryInterface.bulkInsert(
+                    'closing_solutions',
+                    oldValues.map(solution => ({ label: solution })),
+                    { transaction },
+                ),
+                // suppress new values
+                queryInterface.sequelize.query(
+                    `DELETE FROM closing_solutions
+                    WHERE  label IN ('${newValues.join('\', \'')}')
+                    `,
+                    { transaction },
+                ),
+            ],
         );
 
 
-        // create constraints
         await Promise.all([
-            queryInterface.addConstraint('closing_solutions', ['label'], {
-                type: 'unique',
-                name: 'uk_closing_solutions_label',
-                transaction,
-            }),
-            queryInterface.addConstraint(
-                'shantytown_closing_solutions',
-                ['fk_shantytown'],
-                {
-                    type: 'foreign key',
-                    name: 'fk_shantytown_closing_solutions_shantytown',
-                    references: {
-                        table: 'shantytowns',
-                        field: 'shantytown_id',
-                    },
-                    onDelete: 'cascade',
-                    onUpdate: 'cascade',
-                    transaction,
-                },
-            ),
-            queryInterface.addConstraint(
-                'shantytown_closing_solutions',
-                ['fk_closing_solution'],
-                {
-                    type: 'foreign key',
-                    name: 'fk_shantytown_closing_solutions_solution',
-                    references: {
-                        table: 'closing_solutions',
-                        field: 'closing_solution_id',
-                    },
-                    onUpdate: 'cascade',
-                    onDelete: 'cascade',
-                    transaction,
-                },
-            ),
-            queryInterface.addConstraint('shantytown_closing_solutions', ['fk_shantytown', 'fk_closing_solution'], {
-                type: 'primary key',
-                name: 'pk_shantytown_closing_solutions',
-                transaction,
-            }),
+            // create constraints
             queryInterface.addConstraint('ShantytownClosingSolutionHistories', ['fk_shantytown'], {
                 type: 'foreign key',
                 name: 'fk_shantytown_closing_solutions_shantytown',
@@ -367,20 +212,7 @@ module.exports = {
                 onDelete: 'cascade',
                 transaction,
             }),
-            // populate closing_solutions with old values
-            queryInterface.bulkInsert(
-                'closing_solutions',
-                [
-                    { label: 'Mise à l’abri / hébergement d’urgence (CHU)' },
-                    { label: 'Hébergement d’insertion (CHRS, ALT)' },
-                    { label: 'Logement ordinaire ou adapté (résidences sociales, IML, pensions de famille, logements social ou privé accompagnés)' },
-                    { label: 'Dispositif dédié (CADA, HUDA, CAO, CHUM, ARV, ...)' },
-                    { label: 'Dispositif spécifique d’insertion au territoire' },
-                    { label: 'Dispositifs de veille sociale ou sans solution' },
 
-                ],
-                { transaction },
-            ),
         ]);
 
 
