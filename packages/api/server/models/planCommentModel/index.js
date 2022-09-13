28be8b71c9cac53b6d5450bf2712b3cbead6b170
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-13 15:36:35 +0200

    (1558) create planCommentModel.findAll

--- packages/api/server/models/planCommentModel/index.js
+++ packages/api/server/models/planCommentModel/index.js
@@ -1,7 +1,10 @@
 const create = require('./create');
 const findOne = require('./findOne');
+const findAll = require('./findAll');
+
 
 module.exports = {
     create,
     findOne,
+    findAll,
 };
