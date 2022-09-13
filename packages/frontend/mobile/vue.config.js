a375a1ace4b252313f17b761ca11e4c382b4043f
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-13 08:55:44 +0200

    (1495) alias #frontend

--- packages/frontend/mobile/vue.config.js
+++ packages/frontend/mobile/vue.config.js
@@ -30,7 +30,9 @@ module.exports = {
   },
 
   chainWebpack: (config) => {
-    config.resolve.alias.set("#src", path.resolve(__dirname, "./src/"));
+    config.resolve.alias
+      .set("#src", path.resolve(__dirname, "./src/"))
+      .set("#frontend", path.resolve(__dirname, ".."));;
   },
 
   pwa: {
