3357c4b7bd83b72c98f7204d2aefee13078d140b
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-13 11:36:13 +0200

    (1495) gestion routes login logout

---
+++ packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="mx-4 my-4 font-bold text-xl">
+      Bienvenue {{ user.first_name }} {{ user.last_name }}
+    </div>
+
+    <Button @click="disconnect" class="mb-8"> Me déconnecter </Button>
+  </div>
+</template>
+
+<script>
+import { Button } from "@resorptionbidonvilles/ui";
+
+export default {
+  components: {
+    Button
+  },
+  computed: {
+    user() {
+      return this.$store.state.config.configuration.user;
+    }
+  },
+  methods: {
+    disconnect() {
+      this.$router.push("/deconnexion");
+    }
+  }
+};
+</script>
