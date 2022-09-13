4e3c1990d525329ae8497cb423868cce95227722
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-13 11:19:56 +0200

    (1495)  correction imports

--- packages/frontend/webapp/src/js/app/pages/TownDetails/TownDetailsNewComment.vue
+++ packages/frontend/webapp/src/js/app/pages/TownDetails/TownDetailsNewComment.vue
@@ -150,7 +150,7 @@
 </template>
 
 <script>
-import { autocompleteOrganization } from "#helpers/api/user";
+import { autocompleteOrganization } from "#frontend/common/api/user";
 
 export default {
     data() {
