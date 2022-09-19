1b5ed71ac6816a1eb96df90363269bf146a4f2f1
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-19 10:43:00 +0200

    (1561) mobile add towns to store

--- packages/frontend/mobile/src/store/index.js
+++ packages/frontend/mobile/src/store/index.js
@@ -3,13 +3,15 @@ import Vuex from "vuex";
 import api from "#src/js/api";
 import moduleConfig from "#frontend/common/store/modules/config";
 import moduleUser from "#frontend/common/store/modules/user";
+import towns from "./modules/towns";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
         user: moduleUser,
-        config: moduleConfig
+        config: moduleConfig,
+        towns
     }
 });
 api.setStore(store);
