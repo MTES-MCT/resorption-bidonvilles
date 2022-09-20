b006efc683febff6af76af1b981152b3bd7803f2
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-20 15:45:13 +0200

    (1559) common linting with webapp

--- packages/frontend/mobile/src/env.js
+++ packages/frontend/mobile/src/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-    VUE_APP_MOBILE_HOST: process.env.VUE_APP_MOBILE_HOST || "${VUE_APP_MOBILE_HOST}"
+    VUE_APP_MOBILE_HOST:
+        process.env.VUE_APP_MOBILE_HOST || "${VUE_APP_MOBILE_HOST}"
 };
--- packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
+++ packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
@@ -1,29 +1,29 @@
 <template>
-  <div>
-    <div class="mx-4 my-4 font-bold text-xl">
-      Bienvenue {{ user.first_name }} {{ user.last_name }}
-    </div>
+    <div>
+        <div class="mx-4 my-4 font-bold text-xl">
+            Bienvenue {{ user.first_name }} {{ user.last_name }}
+        </div>
 
-    <Button @click="disconnect" class="mb-8"> Me déconnecter </Button>
-  </div>
+        <Button @click="disconnect" class="mb-8"> Me déconnecter </Button>
+    </div>
 </template>
 
 <script>
 import { Button } from "@resorptionbidonvilles/ui";
 
 export default {
-  components: {
-    Button
-  },
-  computed: {
-    user() {
-      return this.$store.state.config.configuration.user;
+    components: {
+        Button
+    },
+    computed: {
+        user() {
+            return this.$store.state.config.configuration.user;
+        }
+    },
+    methods: {
+        disconnect() {
+            this.$router.push("/deconnexion");
+        }
     }
-  },
-  methods: {
-    disconnect() {
-      this.$router.push("/deconnexion");
-    }
-  }
 };
 </script>
