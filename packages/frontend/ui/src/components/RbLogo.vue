881ed01df089431518ba7b0d75c2e76cf18a9bf0
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-20 16:20:38 +0200

    (1559) setup UI of Signin page

---
+++ packages/frontend/ui/src/components/RbLogo.vue
@@ -0,0 +1,15 @@
+<template>
+    <img class="h-32" :src="logo" />
+</template>
+
+<script>
+import logo from "../img/logo.svg";
+
+export default {
+    data() {
+        return {
+            logo
+        };
+    }
+};
+</script>
