1ead0d4f0d8002d315dd2a99fa60d9cd6370e5da
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-20 16:40:50 +0200

    (1560) setup layout with navbar

---
+++ packages/frontend/mobile/src/js/components/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <slot />
+        <Navbar />
+    </div>
+</template>
+
+<script>
+import Navbar from "./Navbar/Navbar.vue";
+
+export default {
+    components: {
+        Navbar
+    }
+};
+</script>
