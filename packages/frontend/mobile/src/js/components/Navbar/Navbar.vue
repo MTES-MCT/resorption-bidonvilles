2aea8052134c5e6106857f6a912738f35ac428f4
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-20 16:51:17 +0200

    (1560) support navigation inside navbar

--- packages/frontend/mobile/src/js/components/Navbar/Navbar.vue
+++ packages/frontend/mobile/src/js/components/Navbar/Navbar.vue
@@ -2,9 +2,9 @@
     <div
         class="fixed bottom-0 bg-primary text-white w-full py-4 px-6 flex justify-between"
     >
-        <NavbarItem icon="home" label="Sites" />
+        <NavbarItem icon="home" label="Sites" @click.native="showTownsTab" />
         <NavbarItem icon="pen" label="Notes" />
-        <NavbarItem icon="unlink" label="Déconnexion" />
+        <NavbarItem icon="unlink" label="Déconnexion" @click.native="signout" />
     </div>
 </template>
 
@@ -14,6 +14,18 @@ import NavbarItem from "./NavbarItem";
 export default {
     components: {
         NavbarItem
+    },
+    methods: {
+        showTownsTab() {
+            if (this.$router.currentRoute.path === "/liste-des-sites") {
+                return;
+            }
+
+            this.$router.push("/liste-des-sites");
+        },
+        signout() {
+            this.$router.push("/deconnexion");
+        }
     }
 };
 </script>
