ed184921cb2b8dd31e59e89da5ae4008c333e4e8
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-20 15:45:13 +0200

    (1559) common linting with webapp

--- packages/frontend/mobile/src/js/pages/Signin.vue
+++ packages/frontend/mobile/src/js/pages/Signin.vue
@@ -1,25 +1,25 @@
 <template>
-  <ValidationObserver ref="form" v-slot="{ handleSubmit }">
-    <form @submit.prevent="handleSubmit(onLogin)">
-      <TextInput
-        placeholder="marcel.dupont@example.com"
-        label="Votre courriel"
-        v-model="email"
-      />
+    <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+        <form @submit.prevent="handleSubmit(onLogin)">
+            <TextInput
+                placeholder="marcel.dupont@example.com"
+                label="Votre courriel"
+                v-model="email"
+            />
 
-      <PasswordInput label="Votre mot de passe" v-model="password" />
+            <PasswordInput label="Votre mot de passe" v-model="password" />
 
-      <div class="text-center">
-        <Button type="submit" class="mb-8" :loading="loading">
-          Me connecter
-        </Button>
+            <div class="text-center">
+                <Button type="submit" class="mb-8" :loading="loading">
+                    Me connecter
+                </Button>
 
-        <div v-if="error" class="bg-red200 p-3 mb-8">
-          {{ error }}
-        </div>
-      </div>
-    </form>
-  </ValidationObserver>
+                <div v-if="error" class="bg-red200 p-3 mb-8">
+                    {{ error }}
+                </div>
+            </div>
+        </form>
+    </ValidationObserver>
 </template>
 
 <script>
@@ -27,45 +27,46 @@ import { mapGetters } from "vuex";
 import { Button, TextInput, PasswordInput } from "@resorptionbidonvilles/ui";
 
 export default {
-  data() {
-    return {
-      loading: null,
-      error: null,
-      email: "",
-      password: ""
-    };
-  },
-  components: {
-    Button,
-    TextInput,
-    PasswordInput
-  },
-  computed: {
-    ...mapGetters({
-      isLoggedIn: "user/loggedIn"
-    })
-  },
-  methods: {
-    async onLogin() {
-      try {
-        this.error = null;
-        this.loading = true;
-        await this.$store.dispatch("user/login", {
-          email: this.email,
-          password: this.password
-        });
+    data() {
+        return {
+            loading: null,
+            error: null,
+            email: "",
+            password: ""
+        };
+    },
+    components: {
+        Button,
+        TextInput,
+        PasswordInput
+    },
+    computed: {
+        ...mapGetters({
+            isLoggedIn: "user/loggedIn"
+        })
+    },
+    methods: {
+        async onLogin() {
+            try {
+                this.error = null;
+                this.loading = true;
+                await this.$store.dispatch("user/login", {
+                    email: this.email,
+                    password: this.password
+                });
 
-        this.$router.push({ path: "/" }).catch(() => {});
+                this.$router.push({ path: "/" }).catch(() => {});
 
-        this.loading = false;
-      } catch (err) {
-        this.error = err.user_message || "Une erreur inconnue est survenue";
-        this.loading = false;
-        if (err.fields) {
-          this.$refs.form.setErrors(err.fields);
+                this.loading = false;
+            } catch (err) {
+                this.error =
+                    err.user_message || "Une erreur inconnue est survenue";
+                this.loading = false;
+                if (err.fields) {
+                    this.$refs.form.setErrors(err.fields);
+                }
+            }
         }
-      }
     }
-  }
 };
 </script>
