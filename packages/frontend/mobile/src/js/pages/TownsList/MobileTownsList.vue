de9008d8a76c279758f2005b6a41bf5f29758e42
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-20 16:51:51 +0200

    (1560) remove signout button from TownsList page

--- packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
+++ packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
@@ -3,28 +3,19 @@
         <div class="mx-4 my-4 font-bold text-xl">
             Bienvenue {{ user.first_name }} {{ user.last_name }}
         </div>
-
-        <Button @click="disconnect" class="mb-8"> Me déconnecter </Button>
     </Layout>
 </template>
 
 <script>
 import Layout from "#src/js/components/Layout.vue";
-import { Button } from "@resorptionbidonvilles/ui";
 
 export default {
     components: {
-        Button,
         Layout
     },
     computed: {
         user() {
             return this.$store.state.config.configuration.user;
-        }
-    },
-    methods: {
-        disconnect() {
-            this.$router.push("/deconnexion");
         }
     }
 };
