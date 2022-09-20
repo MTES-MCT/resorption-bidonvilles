9d128070291ed025cade1d36edc6133fde9fd736
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-20 15:10:20 +0200

    (1562) ajout message si aucun site intervenant/consulté

--- packages/frontend/mobile/src/js/api.js
+++ packages/frontend/mobile/src/js/api.js
@@ -1,6 +1,9 @@
 import { createApi } from "#frontend/common/api/main";
 
-const api = createApi("https://api.resorption-bidonvilles.localhost", "1.0.0");
+const api = createApi(
+  "https://api.preprod.resorption-bidonvilles.beta.gouv.fr",
+  "1.0.0"
+);
 export default api;
 
 const { getApi, postApi, patchApi, putApi, deleteApi, open } = api;
--- packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
+++ packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
@@ -4,9 +4,15 @@
       <div class="my-8 font-bold text-xl">
         Bienvenue {{ user.first_name }} {{ user.last_name }}
       </div>
-      <span class="font-bold"> Mes sites ({{ myTowns.length }}) </span>
+      <div class="font-bold">Mes sites ({{ myTowns.length }})</div>
+      <div class="italic" v-if="myTowns.length === 0">
+        vous n'intervenez sur aucun site
+      </div>
     </Container>
-    <div class="whitespace-no-wrap overflow-y-auto mb-8 mt-2">
+    <div
+      v-if="myTowns.length > 0"
+      class="whitespace-no-wrap overflow-y-auto mt-2"
+    >
       <Container>
         <TownCard
           v-for="town in myTowns"
@@ -17,12 +23,18 @@
     </div>
 
     <Container>
-      <span class="font-bold">
+      <div class="font-bold mt-8">
         Sites récemment consultés ({{ consultedTowns.length }})
-      </span>
+      </div>
+      <div class="italic" v-if="consultedTowns.length === 0">
+        vous n'avez consulté aucun site récemment
+      </div>
     </Container>
 
-    <div class="whitespace-no-wrap overflow-y-auto mb-8 mt-2">
+    <div
+      v-if="consultedTowns.length > 0"
+      class="whitespace-no-wrap overflow-y-auto mb-8 mt-2"
+    >
       <Container>
         <TownCard
           v-for="town in consultedTowns"
