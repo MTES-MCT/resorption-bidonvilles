5d34417e737ddc7fa8eb3c4119791deaecfa6dab
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-21 15:37:09 +0200

    (1562) linting

--- packages/frontend/mobile/src/js/pages/TownsList/TownCard.vue
+++ packages/frontend/mobile/src/js/pages/TownsList/TownCard.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="inline-block border h-20 border-1 border-blue300 px-4 mr-4">
-    <p class="font-bold">
-      {{ town.addressSimple }}
-    </p>
-    {{ town.city.name }}
-  </div>
+    <div class="inline-block border h-20 border-1 border-blue300 px-4 mr-4">
+        <p class="font-bold">
+            {{ town.addressSimple }}
+        </p>
+        {{ town.city.name }}
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    town: {
-      type: Object
+    props: {
+        town: {
+            type: Object
+        }
     }
-  }
 };
 </script>
