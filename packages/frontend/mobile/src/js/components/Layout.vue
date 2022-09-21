09f164a3d04e5041ebada6c4b2609bf122ad7391
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-21 15:34:27 +0200

    (1563) add logo to header

--- packages/frontend/mobile/src/js/components/Layout.vue
+++ packages/frontend/mobile/src/js/components/Layout.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+        <header class="mt-8 mb-6 flex items-center justify-center space-x-2">
+            <img
+                class="h-10"
+                src="/img/logo-marianne.svg"
+                alt="République Française"
+            />
+            <h1 class="font-bold text-xl">Résorption-bidonvilles</h1>
+        </header>
         <slot />
         <Navbar />
     </div>
