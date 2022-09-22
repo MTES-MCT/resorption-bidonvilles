b3b4005d6426bf8649de15a10bf12f3f17c776be
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-22 10:26:24 +0200

    (1563) implement notes list

---
+++ packages/frontend/mobile/src/js/pages/NotesList/NotesListItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <article>
+        <section>
+            <p class="font-bold truncate text-G500 text-lg">
+                <span v-if="note.title">{{ note.title }}</span>
+                <span v-else>Note sans titre</span>
+            </p>
+            <p class="truncate">
+                <span v-if="note.description">{{ note.description }}</span>
+                <span v-else>Cette note ne contient aucun contenu</span>
+            </p>
+        </section>
+        <footer class="text-sm mt-1 flex space-x-3">
+            <p><Icon icon="pen" class="mr-1" />Créée le {{ createdAt }}</p>
+            <p :class="publishedColor">
+                <Icon icon="paper-plane" class="mr-1" />
+                <span v-if="note.published">Publiée</span>
+                <span v-else>Non publiée</span>
+            </p>
+        </footer>
+    </article>
+</template>
+
+<script>
+import { Icon } from "@resorptionbidonvilles/ui";
+
+export default {
+    components: { Icon },
+    props: {
+        note: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        publishedColor() {
+            return this.note.published ? "text-green" : "text-red";
+        },
+        createdAt() {
+            return `${this.note.created_at.getDate()}/${this.note.created_at.getMonth() +
+                1}/${this.note.created_at.getFullYear()}`;
+        }
+    }
+};
+</script>
