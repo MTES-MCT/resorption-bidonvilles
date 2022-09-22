282d5c698e7b07f1c543f10f5adfd5773c7b5c1e
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-22 10:52:22 +0200

    (1562) linting

--- packages/frontend/mobile/src/js/helpers/town.js
+++ packages/frontend/mobile/src/js/helpers/town.js
@@ -4,12 +4,12 @@ import { getApi } from "#src/js/api";
  * GET /towns/actors/user_id
  */
 export function findAllByActor(userId) {
-  return getApi(`/towns/actors/${userId}`);
+    return getApi(`/towns/actors/${userId}`);
 }
 
 /**
  * GET /towns/navigation_logs/user_id
  */
 export function findByNavigationLog(userId) {
-  return getApi(`/towns/navigation_logs/${userId}`);
+    return getApi(`/towns/navigation_logs/${userId}`);
 }
