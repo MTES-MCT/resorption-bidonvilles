2c650bcb8d50f68a6f6133488876eb34fa169951
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-27 12:36:46 +0200

    (1583) create new Component BottomSlidingBlock

--- packages/frontend/mobile/src/js/pages/NotesForm/NotesForm.vue
+++ packages/frontend/mobile/src/js/pages/NotesForm/NotesForm.vue
@@ -1,44 +1,54 @@
 <template>
-    <Layout>
-        <template slot="header">
-            <Container class="flex justify-between mb-4">
-                <Button
-                    icon="paper-plane"
-                    iconPosition="left"
-                    size="sm"
-                    variant="text-primary"
-                    class="font-bold"
-                    @click="$router.push('/liste-des-notes')"
-                    >Publier</Button
-                >
-                <Button
-                    icon="arrow-left"
-                    iconPosition="left"
-                    size="sm"
-                    variant="text-primary"
-                    class="font-bold"
-                    @click="$router.push('/liste-des-notes')"
-                    >Retour aux notes</Button
-                >
-            </Container>
-        </template>
-        <template slot="scroll">
-            <textarea
-                class="px-6 w-full h-full outline-none"
-                ref="textarea"
-                v-model="description"
-            ></textarea>
-        </template>
-    </Layout>
+    <div>
+        <Layout>
+            <template slot="header">
+                <Container class="flex justify-between mb-4">
+                    <Button
+                        icon="paper-plane"
+                        iconPosition="left"
+                        size="sm"
+                        variant="text-primary"
+                        class="font-bold"
+                        @click="showPublish"
+                        >Publier</Button
+                    >
+                    <Button
+                        icon="arrow-left"
+                        iconPosition="left"
+                        size="sm"
+                        variant="text-primary"
+                        class="font-bold"
+                        @click="$router.push('/liste-des-notes')"
+                        >Retour aux notes</Button
+                    >
+                </Container>
+            </template>
+            <template slot="scroll">
+                <textarea
+                    class="px-6 w-full h-full outline-none"
+                    ref="textarea"
+                    v-model="description"
+                ></textarea>
+            </template>
+        </Layout>
+        <BottomSlidingBlock ref="publishBlock">
+            <template slot="header">Publier ma note</template>
+            <template slot="body">
+                <Container>test</Container>
+            </template>
+        </BottomSlidingBlock>
+    </div>
 </template>
 
 <script>
 import { Button } from "@resorptionbidonvilles/ui";
+import BottomSlidingBlock from "#src/js/components/BottomSlidingBlock.vue";
 import Container from "#src/js/components/Container.vue";
 import Layout from "#src/js/components/Layout.vue";
 
 export default {
     components: {
+        BottomSlidingBlock,
         Button,
         Container,
         Layout
@@ -62,6 +72,11 @@ export default {
                     description: text
                 });
             }
+        }
+    },
+    methods: {
+        showPublish() {
+            this.$refs.publishBlock.show();
         }
     }
 };
