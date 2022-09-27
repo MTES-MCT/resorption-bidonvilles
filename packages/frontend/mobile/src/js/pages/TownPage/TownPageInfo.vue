0d94be7ea3ad2b14fa345ca5689823bf4a10370f
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-09-27 14:32:26 +0200

    (1566) ajout Icon to TownPageInfo component

--- packages/frontend/mobile/src/js/pages/TownPage/TownPageInfo.vue
+++ packages/frontend/mobile/src/js/pages/TownPage/TownPageInfo.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="grid grid-cols-2 py-2">
-        <div class="font-bold">
+    <div
+        class="grid grid-cols-2 py-4 justify-items-center border-t-1 border-G200"
+    >
+        <div class="font-bold justify-self-start">
+            <Icon v-if="icon" :icon="icon" />
             {{ title }}
         </div>
         <slot />
@@ -8,12 +11,21 @@
 </template>
 
 <script>
+import { Icon } from "@resorptionbidonvilles/ui";
+
 export default {
     props: {
         title: {
             type: String,
             required: true
+        },
+        icon: {
+            type: String,
+            required: false
         }
+    },
+    components: {
+        Icon
     }
 };
 </script>
