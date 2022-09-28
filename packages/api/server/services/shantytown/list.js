1599ff1ce2d5381c220785a646eb6a286b331566
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-28 16:39:26 +0200

    (1587) support search by name/address on GET /towns

--- packages/api/server/services/shantytown/list.js
+++ packages/api/server/services/shantytown/list.js
@@ -2,10 +2,25 @@ const shantytownModel = require('#server/models/shantytownModel');
 const ServiceError = require('#server/errors/ServiceError');
 
 
-module.exports = async (user) => {
+module.exports = async (user, search = undefined) => {
     let shantytowns;
     try {
-        shantytowns = await shantytownModel.findAll(user, [], 'list');
+        shantytowns = await shantytownModel.findAll(
+            user,
+            [
+                {
+                    name: {
+                        operator: 'ILIKE',
+                        value: `%${search}%`,
+                    },
+                    address: {
+                        operator: 'ILIKE',
+                        value: `%${search}%`,
+                    },
+                },
+            ],
+            'list',
+        );
     } catch (error) {
         throw new ServiceError('fetch_failed', error);
     }
