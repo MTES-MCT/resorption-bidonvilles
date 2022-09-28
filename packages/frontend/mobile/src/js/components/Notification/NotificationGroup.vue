17c387e035e4486a2470887460b6e9f37c7bf91e
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-28 11:39:16 +0200

    (1583) create a Notification component

---
+++ packages/frontend/mobile/src/js/components/Notification/NotificationGroup.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="fixed top-0 left-0 w-full z-10">
+        <Notification v-for="item in items" v-bind="item" :key="item.id" />
+    </section>
+</template>
+
+<script>
+import Notification from "./Notification.vue";
+
+export default {
+    components: {
+        Notification
+    },
+    computed: {
+        items() {
+            return this.$store.state.notifications.items;
+        }
+    }
+};
+</script>
--- packages/frontend/mobile/src/store/index.js
+++ packages/frontend/mobile/src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 import api from "#src/js/api";
 import moduleConfig from "#frontend/common/store/modules/config";
 import moduleNotes from "./modules/notes";
+import moduleNotifications from "./modules/notifications";
 import moduleSearch from "./modules/search";
 import moduleTowns from "./modules/towns";
 import moduleUser from "#frontend/common/store/modules/user";
@@ -13,6 +14,7 @@ const store = new Vuex.Store({
     modules: {
         config: moduleConfig,
         notes: moduleNotes,
+        notifications: moduleNotifications,
         search: moduleSearch,
         towns: moduleTowns,
         user: moduleUser
