f4dabcfb50015364f83e5b58d1f17c0625f0b756
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-28 13:01:03 +0200

    (1583) split NotesList header into little components

---
+++ packages/frontend/mobile/src/js/pages/NotesList/header/NotesListTopBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <Container>
+        <div class="flex justify-between items-center">
+            <h1 class="font-bold text-lg">Liste de vos notes</h1>
+            <div>
+                <Button
+                    size="sm"
+                    icon="filter"
+                    iconPosition="left"
+                    variant="primaryText"
+                    @click.native="$emit('toggle')"
+                    >Filtrer</Button
+                >
+                <Button
+                    size="sm"
+                    icon="plus"
+                    iconPosition="left"
+                    variant="primaryText"
+                    @click.native="$emit('create')"
+                    >Créer</Button
+                >
+            </div>
+        </div>
+    </Container>
+</template>
+
+<script>
+import Container from "#src/js/components/Container.vue";
+import { Button } from "@resorptionbidonvilles/ui";
+
+export default {
+    components: {
+        Container,
+        Button
+    }
+};
+</script>
