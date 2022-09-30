282da3e4473aa2f3ee367741436cb000c194eae7
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-30 11:20:09 +0200

    (1590) feed: remove Promise.all() unecessary

--- packages/api/db/migrations/30000027-01-convert-shantytown-address-to-text.js
+++ packages/api/db/migrations/30000027-01-convert-shantytown-address-to-text.js
@@ -1,15 +1,11 @@
 module.exports = {
-    up: (queryInterface, Sequelize) => Promise.all([
-        queryInterface.changeColumn('shantytowns', 'address_details', {
-            type: Sequelize.TEXT,
-            allowNull: true,
-        }),
-    ]),
+    up: (queryInterface, Sequelize) => queryInterface.changeColumn('shantytowns', 'address_details', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+    }),
 
-    down: (queryInterface, Sequelize) => Promise.all([
-        queryInterface.changeColumn('shantytowns', 'address_details', {
-            type: Sequelize.STRING,
-            allowNull: true,
-        }),
-    ]),
+    down: (queryInterface, Sequelize) => queryInterface.changeColumn('shantytowns', 'address_details', {
+        type: Sequelize.STRING,
+        allowNull: true,
+    }),
 };
