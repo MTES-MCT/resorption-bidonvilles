d77cddfaa80ee949bcf98fce7fdcdbdd8c39a6d0
Anis Safine Laget <anis@users.noreply.github.com>
2022-09-30 11:20:47 +0200

    (1590) feed: fix linting

--- packages/frontend/webapp/src/js/app/components/TownForm/inputs/InputDetailedAddress.vue
+++ packages/frontend/webapp/src/js/app/components/TownForm/inputs/InputDetailedAddress.vue
@@ -1,37 +1,37 @@
 <template>
-  <TextArea
-    id="detailed_address"
-    label="Informations d'accès"
-    info="Saisissez ici toutes les informations qui, en plus de l'adresse, peuvent être utiles pour l'accès au site."
-    v-model="input"
-    cypressName="detailed_address"
-  ></TextArea>
+    <TextArea
+        id="detailed_address"
+        label="Informations d'accès"
+        info="Saisissez ici toutes les informations qui, en plus de l'adresse, peuvent être utiles pour l'accès au site."
+        v-model="input"
+        cypressName="detailed_address"
+    ></TextArea>
 </template>
 
 <script>
 export default {
-  props: {
-    value: {
-      type: String,
-      required: false,
-      default: "",
-    },
-  },
-
-  data() {
-    return {
-      input: this.value,
-    };
-  },
-
-  watch: {
-    value() {
-      this.input = this.value;
+    props: {
+        value: {
+            type: String,
+            required: false,
+            default: ""
+        }
     },
 
-    input() {
-      this.$emit("input", this.input);
+    data() {
+        return {
+            input: this.value
+        };
     },
-  },
+
+    watch: {
+        value() {
+            this.input = this.value;
+        },
+
+        input() {
+            this.$emit("input", this.input);
+        }
+    }
 };
 </script>
