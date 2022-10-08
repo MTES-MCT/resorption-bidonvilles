d9e0525820346d3a54fcc893be983fefff69457e
Anis Safine Laget <anis@users.noreply.github.com>
2022-10-08 23:12:18 +0200

    add an error state to mobile TownsList

--- packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
+++ packages/frontend/mobile/src/js/pages/TownsList/MobileTownsList.vue
@@ -10,40 +10,60 @@
             </Container>
         </template>
         <template v-slot:scroll>
-            <Container class="mt-6">
-                <div class="font-bold text-lg">
-                    Mes sites ({{ myTowns.length }})
-                </div>
-                <div class="italic">
-                    <span v-if="state !== 'loaded'"
-                        >chargement en cours...</span
-                    >
-                    <span v-else-if="myTowns.length === 0"
-                        >vous n'intervenez sur aucun site</span
-                    >
-                </div>
-            </Container>
-            <TownCarousel :towns="myTowns" />
+            <template v-if="state !== 'error'">
+                <Container class="mt-6">
+                    <div class="font-bold text-lg">
+                        Mes sites ({{ myTowns.length }})
+                    </div>
+                    <div class="italic">
+                        <span v-if="state === 'loading'"
+                            >chargement en cours...</span
+                        >
+                        <span v-else-if="myTowns.length === 0"
+                            >vous n'intervenez sur aucun site</span
+                        >
+                    </div>
+                </Container>
+                <TownCarousel :towns="myTowns" />
 
-            <Container class="mt-6">
-                <div class="font-bold text-lg">
-                    Sites récemment consultés ({{ consultedTowns.length }})
-                </div>
-                <div class="italic">
-                    <span v-if="state !== 'loaded'"
-                        >chargement en cours...</span
+                <Container class="mt-6">
+                    <div class="font-bold text-lg">
+                        Sites récemment consultés ({{ consultedTowns.length }})
+                    </div>
+                    <div class="italic">
+                        <span v-if="state === 'loading'"
+                            >chargement en cours...</span
+                        >
+                        <span v-else-if="consultedTowns.length === 0"
+                            >vous n'avez consulté aucun site récemment</span
+                        >
+                    </div>
+                </Container>
+                <TownCarousel :towns="consultedTowns" />
+            </template>
+
+            <Container class="mt-24 text-center" v-else>
+                <p>
+                    <span class="font-bold text-primary"
+                        >Le chargement des données a échoué :</span
+                    ><br />
+                    <span>{{ error }}</span
+                    ><br />
+                    <Button
+                        class="mt-3"
+                        @click="load"
+                        icon="arrow-alt-circle-right"
                     >
-                    <span v-else-if="consultedTowns.length === 0"
-                        >vous n'avez consulté aucun site récemment</span
-                    >
-                </div>
+                        Réessayer
+                    </Button>
+                </p>
             </Container>
-            <TownCarousel :towns="consultedTowns" />
         </template>
     </Layout>
 </template>
 
 <script>
+import { Button } from "@resorptionbidonvilles/ui";
 import Container from "../../components/Container.vue";
 import TownCarousel from "./TownCarousel.vue";
 import { mapGetters } from "vuex";
@@ -51,6 +71,7 @@ import Layout from "#src/js/components/Layout.vue";
 
 export default {
     components: {
+        Button,
         Container,
         Layout,
         TownCarousel
