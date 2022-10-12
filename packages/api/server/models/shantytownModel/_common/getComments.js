064d5739e085baf2c8e3bd61d240e9a35de34aa2
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-10-12 17:46:11 +0200

    (1602) optimisation getComments

--- packages/api/server/models/shantytownModel/_common/getComments.js
+++ packages/api/server/models/shantytownModel/_common/getComments.js
@@ -35,18 +35,20 @@ module.exports = async (user, shantytownIds, covid = false) => {
             shantytown_comments.description AS "commentDescription",
             shantytown_comments.created_at AS "commentCreatedAt",
             shantytown_comments.created_by AS "commentCreatedBy",
-            shantytown_covid_comments.date AS "covidCommentDate",
-            shantytown_covid_comments.equipe_maraude AS "covidEquipeMaraude",
-            shantytown_covid_comments.equipe_sanitaire AS "covidEquipeSanitaire",
-            shantytown_covid_comments.equipe_accompagnement AS "covidEquipeAccompagnement",
-            shantytown_covid_comments.distribution_alimentaire AS "covidDistributionAlimentaire",
-            shantytown_covid_comments.action_mediation_sante as "covidActionMediationSante",
-            shantytown_covid_comments.sensibilisation_vaccination as "covidSensibilisationVaccination",
-            shantytown_covid_comments.equipe_mobile_depistage as "covidEquipeMobileDepistage",
-            shantytown_covid_comments.equipe_mobile_vaccination as "covidEquipeMobileVaccination",
-            shantytown_covid_comments.personnes_orientees AS "covidPersonnesOrientees",
-            shantytown_covid_comments.personnes_avec_symptomes AS "covidPersonnesAvecSymptomes",
-            shantytown_covid_comments.besoin_action AS "covidBesoinAction",
+            ${covid === true ? `
+                shantytown_covid_comments.date AS "covidCommentDate",
+                shantytown_covid_comments.equipe_maraude AS "covidEquipeMaraude",
+                shantytown_covid_comments.equipe_sanitaire AS "covidEquipeSanitaire",
+                shantytown_covid_comments.equipe_accompagnement AS "covidEquipeAccompagnement",
+                shantytown_covid_comments.distribution_alimentaire AS "covidDistributionAlimentaire",
+                shantytown_covid_comments.action_mediation_sante as "covidActionMediationSante",
+                shantytown_covid_comments.sensibilisation_vaccination as "covidSensibilisationVaccination",
+                shantytown_covid_comments.equipe_mobile_depistage as "covidEquipeMobileDepistage",
+                shantytown_covid_comments.equipe_mobile_vaccination as "covidEquipeMobileVaccination",
+                shantytown_covid_comments.personnes_orientees AS "covidPersonnesOrientees",
+                shantytown_covid_comments.personnes_avec_symptomes AS "covidPersonnesAvecSymptomes",
+                shantytown_covid_comments.besoin_action AS "covidBesoinAction",`
+        : ''}
             users.first_name AS "userFirstName",
             users.last_name AS "userLastName",
             users.position AS "userPosition",
@@ -58,12 +60,12 @@ module.exports = async (user, shantytownIds, covid = false) => {
         FROM shantytown_comments
         LEFT JOIN users ON shantytown_comments.created_by = users.user_id
         LEFT JOIN organizations ON users.fk_organization = organizations.organization_id
-        LEFT JOIN shantytown_covid_comments ON shantytown_covid_comments.fk_comment = shantytown_comments.shantytown_comment_id
+        ${covid === true ? 'INNER JOIN shantytown_covid_comments ON shantytown_covid_comments.fk_comment = shantytown_comments.shantytown_comment_id' : ''}
         LEFT JOIN organization_comment_access ON organization_comment_access.shantytown_comment_id = shantytown_comments.shantytown_comment_id
         LEFT JOIN user_comment_access ON user_comment_access.shantytown_comment_id = shantytown_comments.shantytown_comment_id
         WHERE
             shantytown_comments.fk_shantytown IN (:ids) 
-            AND shantytown_covid_comment_id IS ${covid === true ? 'NOT ' : ''}NULL
+            ${covid === false ? 'AND shantytown_comments.shantytown_comment_id NOT IN (SELECT fk_comment FROM shantytown_covid_comments)' : ''}
             ${filterPrivateComments === true ? `AND (
                 (
                     :userId = ANY(user_comment_access.user_target_id)
