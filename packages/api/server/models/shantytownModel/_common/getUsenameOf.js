484dbbc79c7d27646e08332e88430347429dac3e
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-10-13 15:03:55 +0200

    (1602) correction getUsenameOf

--- packages/api/server/models/shantytownModel/_common/getUsenameOf.js
+++ packages/api/server/models/shantytownModel/_common/getUsenameOf.js
@@ -1,7 +1,7 @@
 const getAddressSimpleOf = require('./getAddressSimpleOf');
 
 module.exports = (shantytown) => {
-    const addressSimple = getAddressSimpleOf(shantytown.address);
+    const addressSimple = shantytown.addressSimple || getAddressSimpleOf(shantytown.address);
 
     // process usename
     if (shantytown.name) {
