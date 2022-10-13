d7d07b6086326f66d89fa46836631421f1e269bf
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-10-13 14:30:38 +0200

    (1602) console.time userActivityModel.getHistory

--- packages/api/server/models/userActivityModel/getHistory.js
+++ packages/api/server/models/userActivityModel/getHistory.js
@@ -11,7 +11,7 @@ const planCommentModel = require('#server/models/planCommentModel');
  * @param {Array.<String>} entities List of entities to be included
  */
 module.exports = async (user, location, entities, numberOfActivities, lastDate, maxDate) => {
-    const promises = [];
+    console.time('==========Analyse de userActivityModel.getHistory===============');
     const shantytownFilter = [];
     if (entities.includes('shantytownCreation')) {
         shantytownFilter.push('shantytownCreation');
@@ -22,23 +22,51 @@ module.exports = async (user, location, entities, numberOfActivities, lastDate, 
     if (entities.includes('shantytownClosing')) {
         shantytownFilter.push('shantytownClosing');
     }
+
+    let townActivities = [];
+    let commentActivities = [];
+    let covidCommentActivities = [];
+    let userActivities = [];
+    let planCommentActivities = [];
+
     if (shantytownFilter.length > 0) {
-        promises.push(shantytownModel.getHistory(user, location, shantytownFilter, numberOfActivities, lastDate, maxDate));
+        console.time('SQL-History-TOWNS');
+        townActivities = await shantytownModel.getHistory(user, location, shantytownFilter, numberOfActivities, lastDate, maxDate);
+        console.timeEnd('SQL-History-TOWNS');
     }
+
     if (entities.includes('shantytownComment')) {
-        promises.push(shantytownCommentModel.getHistory(user, location, numberOfActivities, lastDate, maxDate, entities.includes('onlyCovid')));
+        console.time('SQL-History-COMMENTS');
+        commentActivities = await shantytownCommentModel.getHistory(user, location, numberOfActivities, lastDate, maxDate, entities.includes('onlyCovid'));
+        console.timeEnd('SQL-History-COMMENTS');
     }
     if (entities.includes('highCovidComment')) {
-        promises.push(highCovidCommentModel.getHistory(user, location, numberOfActivities, lastDate, maxDate));
+        console.time('SQL-History-COVIDCOMMENTS');
+        covidCommentActivities = await highCovidCommentModel.getHistory(user, location, numberOfActivities, lastDate, maxDate);
+        console.timeEnd('SQL-History-COVIDCOMMENTS');
     }
     if (entities.includes('user')) {
-        promises.push(userModel.getHistory(location, numberOfActivities, lastDate, maxDate));
+        console.time('SQL-History-USERS');
+        userActivities = await userModel.getHistory(location, numberOfActivities, lastDate, maxDate);
+        console.timeEnd('SQL-History-USERS');
     }
     if (entities.includes('planComment')) {
-        promises.push(planCommentModel.getHistory(user, location, numberOfActivities, lastDate, maxDate));
+        console.time('SQL-History-PLANCOMMENTS');
+        planCommentActivities = planCommentModel.getHistory(user, location, numberOfActivities, lastDate, maxDate);
+        console.timeEnd('SQL-History-PLANCOMMENTS');
     }
-    const activities = await Promise.all(promises);
+    console.time('Attente de la promise');
+    // const promiseActivities = await Promise.all(promises);
+    console.timeEnd('Attente de la promise');
+
+    console.time('analyse de la concaténation des activités');
+    const activities = [...townActivities, ...commentActivities, ...covidCommentActivities, ...userActivities, ...planCommentActivities];
+    console.timeEnd('analyse de la concaténation des activités');
+
+
     const sortedActivities = activities.flat().sort((a, b) => (a.date > b.date ? -1 : 1));
+
+    console.timeEnd('==========Analyse de userActivityModel.getHistory===============');
 
     if (numberOfActivities !== -1) {
         return sortedActivities.slice(0, numberOfActivities);
