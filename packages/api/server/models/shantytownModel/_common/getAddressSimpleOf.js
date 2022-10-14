4ba6449f7543b265f98069d99718acac79e405a8
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-10-14 14:38:55 +0200

    (1602) correction getAddressSimple

--- packages/api/server/models/shantytownModel/_common/getAddressSimpleOf.js
+++ packages/api/server/models/shantytownModel/_common/getAddressSimpleOf.js
@@ -1,4 +1,7 @@
 module.exports = (address) => {
+    if (!address) {
+        return 'Pas d\'adresse précise';
+    }
     const reg = address.match(/^(.+) [0-9]+ [^,]+,? [0-9]+,? [^, ]+(,.+)?$/);
     if (!reg) {
         return 'Pas d\'adresse précise';
