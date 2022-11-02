c09072e4b6bf8a5f97f396ba7c16c86aa4c0d3dd
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-11-02 15:03:16 +0100

    (1627) modification TownPage

--- packages/frontend/mobile/src/js/pages/TownPage/TownPage.vue
+++ packages/frontend/mobile/src/js/pages/TownPage/TownPage.vue
@@ -12,16 +12,19 @@
             </template>
         </Layout>
 
+        <Layout v-else-if="error !== null">
+            <template v-slot:header>
+                <header>
+                    <p class="text-center text-red text-display-lg mb-4">
+                        {{ error }}
+                    </p>
+                </header>
+            </template>
+        </Layout>
+
         <Layout v-else-if="this.town !== null">
             <template v-slot:header>
                 <header>
-                    <p
-                        v-if="error !== null"
-                        class="text-center text-red text-display-lg mb-4"
-                    >
-                        {{ error }}
-                    </p>
-
                     <div
                         class="bg-G200 text-display-sm font-bold text-center pt-3 pb-2"
                         style="line-height: 1em"
@@ -112,18 +115,18 @@ export default {
         TownPagePanelActors,
         TownPagePanelJudicial,
         Icon,
-        Spinner
+        Spinner,
     },
     data() {
         return {
-            error: null
+            error: null,
         };
     },
     computed: {
         ...mapGetters({
             town: "detailedTown",
-            state: "townsState"
-        })
+            state: "townsState",
+        }),
     },
     async mounted() {
         if (this.$store.state.towns.state !== "loaded") {
@@ -145,10 +148,13 @@ export default {
             this.error = "Erreur: " + error.message;
         }
     },
+    beforeUnmount() {
+        this.$store.commit("setDetailedTown", null);
+    },
     methods: {
         async toTownsList() {
             this.$router.push(`/liste-des-sites`);
-        }
-    }
+        },
+    },
 };
 </script>
