065d9d1715ec45af89ae9b1b92ef19615dc2f7f9
Anis Safine Laget <anis@users.noreply.github.com>
2022-11-03 15:57:52 +0100

    (1601) linting

--- packages/frontend/webapp3/src/components/ListeDesSites/ListeDesSitesVide.vue
+++ packages/frontend/webapp3/src/components/ListeDesSites/ListeDesSitesVide.vue
@@ -2,8 +2,10 @@
     <ViewError variant="vide">
         <template v-slot:title>Aucun résultat pour cette recherche</template>
         <template v-slot:code>Filtres ou recherche</template>
-        <template v-slot:content>Il semblerait qu'il n'existe aucun site répondant à vos
-            critères.</template>
+        <template v-slot:content
+            >Il semblerait qu'il n'existe aucun site répondant à vos
+            critères.</template
+        >
         <template v-slot:actions>&nbsp;</template>
     </ViewError>
 </template>
