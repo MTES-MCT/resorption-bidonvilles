51a90414e10fb082d4d59f6d3489f0d8ad412acd
Anis Safine Laget <anis@users.noreply.github.com>
2022-11-14 11:21:57 +0100

    (1629) navigation log on mobile + linting

---
+++ packages/frontend/mobile/src/js/helpers/navigationLogs.js
@@ -0,0 +1,8 @@
+import { postApi } from "#src/js/api";
+
+/**
+ * POST /me/navigationLogs
+ */
+export function insert(page) {
+    return postApi("/me/navigationLogs", { page, domain: "mobile" });
+}
