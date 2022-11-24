a8c27d9bff0f9e690e84a9f9e09413d2a0e9c9ec
Christophe BENARD <ch.benard@gmail.com>
2022-11-24 11:45:51 +0100

    (1647) Fixes error when loading Sentry (API) - `Tracing` replaced by `* as Tracing` in import statement - Try/Catch added to avoir warning if `!(config.sentry.dsn)`

--- packages/api/server/index.ts
+++ packages/api/server/index.ts
@@ -1,36 +1,40 @@
 import '../module_alias';
 import * as Sentry from '@sentry/node';
-import Tracing from '@sentry/tracing';
+import * as Tracing from '@sentry/tracing';
 import app from '#server/app';
 import { version } from '#root/package.json';
 
 import config from './config';
 
 app.start().then((expressApp) => {
-    if (config.sentry.dsn) {
-        Sentry.init({
-            dsn: config.sentry.dsn,
-            release: `rb-api@${version}`,
-            environment: 'production',
-            beforeSend(err) {
-                // console.log('sentry send', err);
-                return err;
-            },
-            integrations: [
-                // enable HTTP calls tracing
-                new Sentry.Integrations.Http({ tracing: true }),
-                // enable Postres calls tracing
-                new Tracing.Integrations.Postgres(), // Add this integration
-                new Tracing.Integrations.Express({
-                    // to trace all requests to the default router
-                    app: expressApp,
-                }),
-            ],
+    try {
+        if (config.sentry.dsn) {
+            Sentry.init({
+                dsn: config.sentry.dsn,
+                release: `rb-api@${version}`,
+                environment: 'production',
+                beforeSend(err) {
+                    console.log('sentry send', err);
+                    return err;
+                },
+                integrations: [
+                    // enable HTTP calls tracing
+                    new Sentry.Integrations.Http({ tracing: true }),
+                    // enable Postres calls tracing
+                    new Tracing.Integrations.Postgres(), // Add this integration
+                    new Tracing.Integrations.Express({
+                        // to trace all requests to the default router
+                        app: expressApp,
+                    }),
+                ],
 
-            // Set tracesSampleRate to 1.0 to capture 100%
-            // of transactions for performance monitoring.
-            // We recommend adjusting this value in production
-            tracesSampleRate: 1.0,
-        });
+                // Set tracesSampleRate to 1.0 to capture 100%
+                // of transactions for performance monitoring.
+                // We recommend adjusting this value in production
+                tracesSampleRate: 1.0,
+            });
+        }
+    } catch (error) {
+        // Do nothing - Try / catch is here to avoid warning if !(config.sentry.dsn)
     }
 });
