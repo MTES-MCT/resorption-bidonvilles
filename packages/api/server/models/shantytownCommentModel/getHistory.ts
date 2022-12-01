8974265c79d727876c7b82152eedd9e3485a48a3
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-01 15:37:10 +0100

    (1663) correction shantytownComment.getHistory

--- packages/api/server/models/shantytownCommentModel/getHistory.ts
+++ packages/api/server/models/shantytownCommentModel/getHistory.ts
@@ -45,27 +45,27 @@ export default async (user, location, numberOfActivities, lastDate, maxDate) => 
         permissionWhere.publicComments.push('false');
     } else if (restrictedLocations.public.type !== 'nation') {
         // geo permission
-        const geo = [`${fromGeoLevelToTableName(restrictedLocations.public.type)}.code = :shantytownCommentLocationCode`];
+        const publicCommentLocationClause = [`${fromGeoLevelToTableName(restrictedLocations.public.type)}.code = :shantytownCommentLocationCode`];
         if (restrictedLocations.public.type === 'city') {
-            geo.push(`${fromGeoLevelToTableName(restrictedLocations.public.type)}.fk_main = :shantytownCommentLocationCode`);
+            publicCommentLocationClause.push(`${fromGeoLevelToTableName(restrictedLocations.public.type)}.fk_main = :shantytownCommentLocationCode`);
         }
 
-        permissionWhere.publicComments.push(`(${geo.join(' OR ')})`);
+        permissionWhere.publicComments.push(`(${publicCommentLocationClause.join(' OR ')})`);
         replacements.shantytownCommentLocationCode = restrictedLocations.public[restrictedLocations.public.type].code;
     }
 
     // private comments
-    const geo = [];
+    const privateCommentLocationClause = [];
     if (restrictedLocations.private !== null) {
         if (restrictedLocations.private.type !== 'nation') {
-            geo.push(`${fromGeoLevelToTableName(restrictedLocations.private.type)}.code = :privateShantytownCommentLocationCode`);
+            privateCommentLocationClause.push(`${fromGeoLevelToTableName(restrictedLocations.private.type)}.code = :privateShantytownCommentLocationCode`);
             if (restrictedLocations.public.type === 'city') {
-                geo.push(`${fromGeoLevelToTableName(restrictedLocations.private.type)}.fk_main = :privateShantytownCommentLocationCode`);
+                privateCommentLocationClause.push(`${fromGeoLevelToTableName(restrictedLocations.private.type)}.fk_main = :privateShantytownCommentLocationCode`);
             }
 
             replacements.privateShantytownCommentLocationCode = restrictedLocations.private[restrictedLocations.private.type].code;
         } else {
-            geo.push('true');
+            privateCommentLocationClause.push('true');
         }
     }
 
@@ -77,10 +77,9 @@ export default async (user, location, numberOfActivities, lastDate, maxDate) => 
                 :userId = ANY(uca.user_target_id)
             OR :organizationId = ANY(oca.organization_target_id)
             OR :userId = comments.created_by
-            ${geo.length > 0 ? `OR (${geo.join(' OR ')})` : ''}
+            ${privateCommentLocationClause.length > 0 ? `OR (${privateCommentLocationClause.join(' OR ')})` : ''}
         )`,
     );
-    permissionWhere.privateComments.push(`(${geo.join(' OR ')})`);
 
     replacements.userId = user.id;
     replacements.organizationId = user.organization.id;
@@ -92,6 +91,18 @@ export default async (user, location, numberOfActivities, lastDate, maxDate) => 
             (${permissionWhere.privateComments.join(' AND ')})
         )`,
     );
+
+    // on vérifie que le commentaire est bien sur le territoire de la recherche 
+    const searchLocationClause = [];
+    if (location.type !== 'nation') {
+        searchLocationClause.push(`${fromGeoLevelToTableName(location.type)}.code = :shantytownCommentSearchLocationCode`);
+        if (location.type === 'city') {
+            searchLocationClause.push(`${fromGeoLevelToTableName(location.type)}.fk_main = :shantytownCommentSearchLocationCode`);
+        }
+        replacements.shantytownCommentSearchLocationCode = location[location.type].code
+    } else { searchLocationClause.push('true') }
+
+    where.push(`(${searchLocationClause.join(' OR ')})`);
 
     // additional filters
     where.push(`comments.created_at < '${lastDate}'`);
