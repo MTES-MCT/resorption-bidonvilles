4bf775069e81bcdf81e80aee9ab084a66fc8e8d7
Anis Safine Laget <anis@users.noreply.github.com>
2022-12-01 10:53:52 +0100

    (1601) reorder api call

--- packages/frontend/webapp3/src/api/config.api.js
+++ packages/frontend/webapp3/src/api/config.api.js
@@ -1,9 +1,9 @@
 import { axios } from "@/helpers/axios";
 
-export function get() {
-    return axios.get("/config");
-}
-
 export function closeChangelog(version) {
     return axios.post("/changelog", { version });
+}
+
+export function get() {
+    return axios.get("/config");
 }
