d85f4d0718224fb1d77b6ef6402b96bb2834eb57
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-02 16:46:22 +0100

    (1658) remove navigation logs for untracked users

---
+++ packages/api/db/migrations/30000032-02-remove-untracked-users-from-navigation_logs.js
@@ -0,0 +1,30 @@
+module.exports = {
+    async up(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        await queryInterface.sequelize.query(
+            `DELETE FROM user_webapp_navigation_logs
+            WHERE fk_user IN 
+                (SELECT user_id
+                FROM users
+                WHERE to_be_tracked = false
+                ) `,
+            {
+            transaction,
+        },)
+
+        await queryInterface.sequelize.query(
+            `DELETE FROM user_mobile_navigation_logs
+            WHERE fk_user IN 
+                (SELECT user_id
+                FROM users
+                WHERE to_be_tracked = false
+                ) `,
+            {
+            transaction,
+        },)
+        return transaction.commit();
+    },
+
+    down: () => Promise.resolve(),
+};
