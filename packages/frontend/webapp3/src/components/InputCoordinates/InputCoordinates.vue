49899c58401a112a036ae32eddacc8710f892270
Anis Safine Laget <anis@users.noreply.github.com>
2022-12-05 15:54:26 +0100

    (1601) add tooltip to input coordinates

--- packages/frontend/webapp3/src/components/InputCoordinates/InputCoordinates.vue
+++ packages/frontend/webapp3/src/components/InputCoordinates/InputCoordinates.vue
@@ -1,11 +1,20 @@
 <template>
-    <Carte withInput v-model="modelValue" :defaultView="{ center: view, zoom: DEFAULT_ZOOM }" ref="map" />
+    <div class="relative h-full">
+        <InputCoordinatesTooltip />
+        <Carte
+            withInput
+            v-model="modelValue"
+            :defaultView="{ center: view, zoom: DEFAULT_ZOOM }"
+            ref="map"
+        />
+    </div>
 </template>
 
 <script setup>
 import { defineProps, toRefs, computed, watch, ref } from "vue";
 import { useField } from "vee-validate";
 import Carte from "@/components/Carte/Carte.vue";
+import InputCoordinatesTooltip from "./InputCoordinatesTooltip.vue";
 
 const DEFAULT_ZOOM = 14;
 const map = ref(null);
