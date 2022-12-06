35457ec8473dd417130a993cb4e56a20fae7a08c
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-06 13:27:50 +0100

    (1669) passage de composants www à Vue3

--- packages/frontend/www/components/Layout/Navbar/NavBarMobileMenu.vue
+++ packages/frontend/www/components/Layout/Navbar/NavBarMobileMenu.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="origin-top-right right-0 mt-2 w-48 rounded-md shadow-lg">
+  <div ref="navbarMobile" class="origin-top-right right-0 mt-2 w-48 rounded-md shadow-lg">
     <Menu>
       <MenuItem>
-        <Link :to="`${WEBAPP_URL}/connexion`">
-          {{ $t("landingPage.header.connect") }}
-        </Link>
+      <Link :to="`${WEBAPP_URL}/connexion`">
+      {{ $t("landingPage.header.connect") }}
+      </Link>
       </MenuItem>
 
       <MenuItem>
-        <Link :to="`${WEBAPP_URL}/contact`">
-          {{ $t("landingPage.header.contact") }}
-        </Link>
+      <Link :to="`${WEBAPP_URL}/contact`">
+      {{ $t("landingPage.header.contact") }}
+      </Link>
       </MenuItem>
       <MenuItem>
-        <LanguagePicker />
+      <LanguagePicker />
       </MenuItem>
     </Menu>
   </div>
 </template>
 
-<script>
+<script setup>
+import { defineProps, toRefs, computed, ref, onMounted, onUnmounted, defineEmits } from "vue";
+
+
 import { Link, Menu, MenuItem } from "@resorptionbidonvilles/ui";
 import LanguagePicker from "./LanguagePicker";
 
-export default {
-  props: {
-    closeMenu: {
-      required: true,
-      type: Function
-    },
-    displayLanguagePicker: {
-      type: Boolean
-    }
+const { WEBAPP_URL } = useRuntimeConfig();
+
+const props = defineProps({
+  closeMenu: {
+    required: true,
+    type: Function
   },
-  components: { Link, LanguagePicker, Menu, MenuItem },
-  data() {
-    const { WEBAPP_URL } = useRuntimeConfig();
-    return {
-        WEBAPP_URL
-    };
-  },
-  mounted() {
-    // Delay listener, otherwise the check happens before the menu is rendered and close the menu immediately
-    setTimeout(() => {
-      document.addEventListener("click", this.checkOutsideClick);
-    }, 0);
-  },
-  destroyed() {
-    document.removeEventListener("click", this.checkOutsideClick);
-  },
-  methods: {
-    checkOutsideClick(event) {
-      if (!this.$el.contains(event.target)) {
-        this.closeMenu();
-      }
-    }
+  displayLanguagePicker: {
+    type: Boolean
   }
-};
+});
+
+const emit = defineEmits(["closeMenu"]);
+
+const navbarMobile = ref(null)
+
+const { closeMenu, displayLanguagePicker } = toRefs(props);
+onMounted(() => {
+  // Delay listener, otherwise the check happens before the menu is rendered and close the menu immediately
+  setTimeout(() => {
+    document.addEventListener("click", checkOutsideClick);
+  }, 0);
+})
+
+onUnmounted(() => {
+  document.removeEventListener("click", checkOutsideClick);
+})
+
+function checkOutsideClick(event) {
+  if (!navbarMobile.value.contains(event.target)) {
+    emit('closeMenu');
+  }
+}
 </script>
--- packages/frontend/www/components/Layout/Navbar/Navbar.vue
+++ packages/frontend/www/components/Layout/Navbar/Navbar.vue
@@ -1,21 +1,18 @@
 <template>
-  <div>
+  <div ref="navbar">
     <div :class="!sticky && 'hidden'">
       <NavBarSticky :menuDisplayed="menuDisplayed" :toggleMenu="toggleMenu">
         <template v-slot:anchors>
           <div class="mr-8 hidden xl:inline-block space-x-5">
             <slot name="anchors"></slot>
           </div>
-        </template> 
-      </NavBarSticky> 
+        </template>
+      </NavBarSticky>
     </div>
 
     <div :class="sticky && 'hidden'">
       <Container>
-        <header
-          role="navigation"
-          class="py-4 flex flex-row justify-between items-center"
-        >
+        <header role="navigation" class="py-4 flex flex-row justify-between items-center">
           <NavBarLogo />
 
           <div class="hidden md:flex flex-row items-center">
@@ -25,9 +22,9 @@
 
             <div>
               <Link :to="`${WEBAPP_URL}/connexion`" class="ml-2">
-                <Button variant="primary">{{
+              <Button variant="primary">{{
                   $t("landingPage.header.connect")
-                }}</Button>
+              }}</Button>
               </Link>
             </div>
             <LanguagePicker v-if="displayLanguagePicker" class="ml-2" />
@@ -37,16 +34,15 @@
         </header>
       </Container>
     </div>
-    <NavBarMobileMenu
-      class="fixed top-0 right-0 mt-16 z-10"
-      v-if="menuDisplayed"
-      :closeMenu="closeMenu"
-      :displayLanguagePicker="displayLanguagePicker"
-    />
+    <NavBarMobileMenu class="fixed top-0 right-0 mt-16 z-10" v-if="menuDisplayed" @closeMenu="closeMenu"
+      :displayLanguagePicker="displayLanguagePicker" />
   </div>
 </template>
 
-<script>
+<script setup>
+import { defineProps, toRefs, computed, ref, onMounted, onUnmounted } from "vue";
+
+
 import NavBarLogo from "./NavBarLogo.vue";
 import NavBarSticky from "./NavBarSticky.vue";
 import LanguagePicker from "./LanguagePicker";
@@ -55,63 +51,51 @@ import NavBarMobileMenu from "./NavBarMobileMenu.vue";
 import Container from "~/components/Layout/Container/Container.vue";
 import { Link, Button } from "@resorptionbidonvilles/ui";
 
-export default {
-  props: {
-    stickyHeader: {
-      type: Boolean
-    },
-    displayLanguagePicker: {
-      type: Boolean,
-      default: true
-    }
+const props = defineProps({
+  stickyHeader: {
+    type: Boolean
   },
-  components: {
-    NavBarLogo,
-    NavBarSticky,
-    Link,
-    Button,
-    NavBarMobileMenu,
-    NavBarMobileButton,
-    Container,
-    LanguagePicker
-  },
-  data() {
-    const { WEBAPP_URL } = useRuntimeConfig();
-
-    return {
-      WEBAPP_URL,
-      scrollTop: 0,
-      menuDisplayed: false
-    };
-  },
-  methods: {
-    handleScroll() {
-      // Header is 76px but 0px when sticky
-      const navbarHeight = this.$el.offsetHeight;
-
-      this.scrollTop = window.scrollY - navbarHeight;
-    },
-    openMenu() {
-      this.menuDisplayed = true;
-    },
-    closeMenu() {
-      this.menuDisplayed = false;
-    },
-
-    toggleMenu() {
-      this.menuDisplayed = !this.menuDisplayed;
-    }
-  },
-  computed: {
-    sticky() {
-      return this.scrollTop > 200 && this.stickyHeader;
-    }
-  },
-  mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  destroyed() {
-    window.removeEventListener("scroll", this.handleScroll);
+  displayLanguagePicker: {
+    type: Boolean,
+    default: true
   }
-};
+});
+const { stickyHeader, displayLanguagePicker } = toRefs(props);
+
+const { WEBAPP_URL } = useRuntimeConfig();
+const scrollTop = ref(0)
+const menuDisplayed = ref(false)
+const navbar = ref(null)
+
+function handleScroll() {
+  // Header is 76px but 0px when sticky
+  const navbarHeight = navbar.value.offsetHeight;
+
+  scrollTop.value = window.scrollY - navbarHeight;
+}
+
+function openMenu() {
+  menuDisplayed.value = true;
+}
+
+function closeMenu() {
+  menuDisplayed.value = false;
+}
+
+function toggleMenu() {
+  menuDisplayed.value = !this.menuDisplayed;
+}
+const sticky = computed(() => {
+  return scrollTop.value > 200 && stickyHeader.value;
+})
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+
+})
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+
+})
 </script>
