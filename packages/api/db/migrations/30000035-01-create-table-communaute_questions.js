9f76f5f63b5e6def0486c1664e287cdce24fbde6
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-08 11:08:18 +0100

    (1683) migration create table communaute_questions

---
+++ packages/api/db/migrations/30000035-01-create-table-communaute_questions.js
@@ -0,0 +1,83 @@
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        await queryInterface.createTable(
+            'communaute_questions',
+            {
+                communaute_question_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true,
+                },
+                question: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                details: {
+                    type: Sequelize.TEXT,
+                    allowNull: false, // false ou true ?
+                },
+                other_tags: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                people_affected: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+                },
+                updated_at: {
+                    type: Sequelize.DATE,
+                    allowNull: true,
+                    defaultValue: null,
+                    onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
+                },
+                solved_at: {
+                    type: Sequelize.DATE,
+                    allowNull: true,
+                    defaultValue: null,
+                },
+                created_by: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+            },
+            { transaction },
+        );
+
+        await queryInterface.addConstraint('communaute_questions', {
+            fields: ['created_by'],
+            type: 'foreign key',
+            name: 'fk_communaute_question_creator',
+            references: {
+                table: 'users',
+                field: 'user_id',
+            },
+            onUpdate: 'cascade',
+            onDelete: 'restrict',
+            transaction,
+        });
+
+        return transaction.commit();
+    },
+
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        await queryInterface.removeConstraint(
+            'communaute_questions',
+            'fk_communaute_question_creator',
+            { transaction },
+        );
+
+        await queryInterface.dropTable('communaute_questions', { transaction });
+
+        return transaction.commit();
+    },
+};
