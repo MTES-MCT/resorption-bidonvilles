d62eab7a36b2f07b3c65e4e01f699fd0279bf5c2
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-08 11:07:40 +0100

    (1683) linting on older migration

--- packages/api/db/migrations/30000031-01-fix-wording-closing_solution.js
+++ packages/api/db/migrations/30000031-01-fix-wording-closing_solution.js
@@ -2,16 +2,16 @@ module.exports = {
     async up(queryInterface) {
         return queryInterface.sequelize.query(
             `UPDATE closing_solutions
-            SET label = \'Hébergement ou logement adapté longue durée avec accompagnement, dont espace temporaire d’insertion\'
-            WHERE label = \'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion\'`,
+            SET label = 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace temporaire d’insertion'
+            WHERE label = 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion'`,
         );
     },
 
     async down(queryInterface) {
         return queryInterface.sequelize.query(
             `UPDATE closing_solutions
-            SET label = \'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion\'
-            WHERE label = \'Hébergement ou logement adapté longue durée avec accompagnement, dont espace temporaire d’insertion\'`,
+            SET label = 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace terrain d’insertion'
+            WHERE label = 'Hébergement ou logement adapté longue durée avec accompagnement, dont espace temporaire d’insertion'`,
         );
     },
 };
