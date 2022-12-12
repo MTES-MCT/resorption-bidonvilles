32a41b151ee594cf10ed13c204e1b0ef076d594e
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-12 18:31:20 +0100

    (1683) form creation inputs

---
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputPeopleAffected.vue
@@ -0,0 +1,11 @@
+<template>
+    <TextInput
+        name="people_affected"
+        id="people_affected"
+        info="Incitez les autres membres de la communauté à vous répondre rapidement en indiquant le nombre de personnes qui ont besoin d'aide"
+    />
+</template>
+
+<script setup>
+import { TextInput } from "@resorptionbidonvilles/ui";
+</script>
