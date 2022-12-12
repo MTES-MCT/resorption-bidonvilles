c0d320933daedda85d2bfb66cc1e95f74c0848c4
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-12 18:31:20 +0100

    (1683) form creation inputs

---
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputOtherTag.vue
@@ -0,0 +1,6 @@
+<template>
+    <TextInput name="other_tag" id="other_tag" />
+</template>
+<script setup>
+import { TextInput } from "@resorptionbidonvilles/ui";
+</script>
---
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputTags.vue
@@ -0,0 +1,24 @@
+<template>
+    <CheckableGroup id="tags" :label="labels.tags">
+        <Checkbox
+            v-for="item in items"
+            :key="item.uid"
+            :value="item.uid"
+            :label="item.name"
+            name="tags"
+        />
+    </CheckableGroup>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useConfigStore } from "@/stores/config.store";
+import { CheckableGroup, Checkbox } from "@resorptionbidonvilles/ui";
+
+import labels from "../FormNouvelleQuestion.labels";
+
+const configStore = useConfigStore();
+const items = computed(() => {
+    return configStore.config.question_tags;
+});
+</script>
