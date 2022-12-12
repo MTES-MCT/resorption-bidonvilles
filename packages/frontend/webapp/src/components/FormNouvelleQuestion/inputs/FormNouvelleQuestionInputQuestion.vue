1bb4f3e0b7f5f79e6cbde368c2c13ec6a4525402
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-12 18:31:20 +0100

    (1683) form creation inputs

---
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputQuestion.vue
@@ -0,0 +1,11 @@
+<template>
+    <TextInput
+        name="question"
+        id="question"
+        info="Résumez votre question à une courte phrase à destination de la communauté, vous pourrez détailler votre question plus bas."
+    />
+</template>
+
+<script setup>
+import { TextInput } from "@resorptionbidonvilles/ui";
+</script>
