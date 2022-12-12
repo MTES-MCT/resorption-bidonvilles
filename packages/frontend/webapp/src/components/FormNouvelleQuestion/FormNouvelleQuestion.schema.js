efea401817438ee8a8f11d1cade5b8d005e64cd3
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-12 18:29:12 +0100

    (1683) form labels and schema

---
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/FormNouvelleQuestion.schema.js
@@ -0,0 +1,19 @@
+import { object, string, number, array } from "yup";
+import { useConfigStore } from "@/stores/config.store";
+import labels from "./FormNouvelleQuestion.labels";
+
+const configStore = useConfigStore();
+
+export default object({
+    question: string().required().label(labels.question),
+    tags: array()
+        .of(
+            string().oneOf([
+                ...configStore.config.question_tags.map(({ uid }) => uid),
+            ])
+        )
+        .label(labels.tags),
+    other_tag: string().label(labels.other_tag),
+    people_affected: number().label(labels.people_affected),
+    details: string().required().label(labels.details),
+});
