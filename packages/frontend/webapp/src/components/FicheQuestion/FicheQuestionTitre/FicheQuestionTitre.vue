d5c608fbb9863c79f8e5c5185c3132b65125aa94
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-14 12:17:52 +0100

    (1686) fiche question sections

---
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionTitre/FicheQuestionTitre.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="text-primary text-xl">{{ question.question }}</h1>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+const props = defineProps({
+    question: Object,
+});
+const { question } = toRefs(props);
+</script>
