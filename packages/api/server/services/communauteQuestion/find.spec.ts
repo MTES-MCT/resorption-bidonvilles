1b4d640409fca04cb48ecafe7ac8b7a8486946e5
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-14 10:22:00 +0100

    (1686) creation service + test question.find

---
+++ packages/api/server/services/communauteQuestion/find.spec.ts
@@ -0,0 +1,51 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import questionModel from '#server/models/questionModel';
+import ServiceError from '#server/errors/ServiceError';
+import findService from './find';
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+describe('services/question', () => {
+    describe('find()', () => {
+        let stubs;
+        beforeEach(() => {
+            stubs = {
+                findOne: sinon.stub(questionModel, 'findOne'),
+            };
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('retourne la question', async () => {
+            const question = {
+                id: 1,
+                question: 'question test',
+                details: 'details test',
+                peopleAffected: 5,
+                tags: [],
+                other_tag: null,
+            };
+            stubs.findOne.resolves(question);
+            const response = await findService(1);
+            expect(response).to.be.an('object');
+            expect(response).to.be.eql(question);
+        });
+        it('renvoie une exception ServiceError \'fetch_failed\'  si la question n\'existe pas en bdd', async () => {
+            stubs.findOne.resolves(null);
+            let responseError = null;
+            try {
+                await findService(1);
+            } catch (error) {
+                responseError = error;
+            }
+            expect(responseError).to.be.instanceOf(ServiceError);
+            expect(responseError.code).to.be.eql('fetch_failed');
+        });
+    });
+});
