4839e9a4dac0772fadaa52aa571d0083c3af61b4
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-14 12:17:52 +0100

    (1686) fiche question sections

---
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionAuthor/FicheQuestionAuthor.vue
@@ -0,0 +1,28 @@
+<template>
+    <article class="flex items-center">
+        <aside class="self-start">
+            <span
+                :class="`bg-G200 mr-4 text-xl rounded-full inline-flex items-center justify-center w-12 h-12`"
+            >
+                <Icon icon="user" />
+            </span>
+        </aside>
+        <section>
+            <p>{{ author.first_name }} {{ author.last_name }}</p>
+            <p class="italic text-G500">
+                {{ author.organization }}
+            </p>
+        </section>
+    </article>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+import { Icon } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    author: Object,
+});
+const { author } = toRefs(props);
+</script>
---
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionDetails/FicheQuestionDetails.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>{{ question.details }}</p>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+const props = defineProps({
+    question: Object,
+});
+const { question } = toRefs(props);
+</script>
