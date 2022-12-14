ef114c12dd41325ee191084c6e81fd57a9a5a45e
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-14 10:42:14 +0100

    (1686) route API question.list et question.fetch

--- packages/frontend/webapp/src/api/questions.api.js
+++ packages/frontend/webapp/src/api/questions.api.js
@@ -1,5 +1,13 @@
 import { axios } from "@/helpers/axios";
 
+export function list() {
+    return axios.get("/questions");
+}
+
+export function fetch(id) {
+    return axios.get(`questions/${id}`);
+}
+
 export function createQuestion(question) {
-    return axios.post(`/questions/`, question);
+    return axios.post(`/questions`, question);
 }
