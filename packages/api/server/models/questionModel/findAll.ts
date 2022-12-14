9a8d0ad1363677226ff0886117c8e36f67ea4bca
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-14 10:20:59 +0100

    (1686) model question.findAll

---
+++ packages/api/server/models/questionModel/findAll.ts
@@ -0,0 +1,60 @@
+import { sequelize } from '#db/sequelize';
+import { QueryTypes } from 'sequelize';
+import serializeQuestion from './serializeQuestion';
+
+export default async () => {
+    const rows:any = await sequelize.query(
+        `
+        WITH aggregate_communaute_question_tags AS (
+            SELECT 
+                cq.communaute_question_id AS id,
+                ARRAY_AGG(cqt.name) AS tags
+            FROM 
+                communaute_questions cq
+            LEFT JOIN
+                communaute_question_to_tags cqtt ON cqtt.fk_question = cq.communaute_question_id
+            LEFT JOIN
+                communaute_question_tags cqt ON cqt.uid = cqtt.fk_question_tag
+            GROUP BY 
+                cq.communaute_question_id
+        )
+        SELECT
+            cq.communaute_question_id AS "questionId",
+            cq.question AS "question",
+            cq.details AS "details",
+            cq.people_affected AS "peopleAffected",
+            acqt.tags,
+            cq.other_tags,
+            cq.created_at AS "questionCreatedAt",
+            cq.updated_at AS "questionUpdatedAt",
+            cq.solved_at AS "questionSolvedAt",
+            cq.created_by "questionCreatedBy",
+            u.user_id AS "userId",
+            u.first_name AS "userFirstName",
+            u.last_name AS "userLastName",
+            u.position AS "userPosition",
+            rr.name AS "userRole",
+            o.organization_id AS "organizationId",
+            o.name AS "organizationName",
+            o.abbreviation AS "organizationAbbreviation"
+        FROM
+            communaute_questions cq
+        LEFT JOIN 
+            aggregate_communaute_question_tags acqt ON acqt.id = cq.communaute_question_id
+        LEFT JOIN
+            users u ON cq.created_by = u.user_id
+        LEFT JOIN
+            organizations o ON u.fk_organization = o.organization_id
+        LEFT JOIN
+            roles_regular rr ON u.fk_role_regular = rr.role_id`,
+        {
+            type: QueryTypes.SELECT,
+        },
+    );
+
+    if (rows.length !== 1) {
+        return null;
+    }
+
+    return rows[0].map(question => serializeQuestion(question));
+};
