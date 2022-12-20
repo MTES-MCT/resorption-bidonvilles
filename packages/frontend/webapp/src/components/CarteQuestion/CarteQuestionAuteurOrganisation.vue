77a8a95b23dcf9be8b980fad100d17abde09643b
Christophe BENARD <ch.benard@gmail.com>
2022-12-20 10:35:05 +0100

    (1682) questions list view and card components added

---
+++ packages/frontend/webapp/src/components/CarteQuestion/CarteQuestionAuteurOrganisation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <CarteQuestionAuteur :auteur="auteur" />
+        <CarteQuestionOrganisation :organization="auteur.organization" />
+    </div>
+</template>
+
+<script setup>
+import { toRefs } from "vue";
+import CarteQuestionAuteur from "./CarteQuestionAuteur.vue";
+import CarteQuestionOrganisation from "./CarteQuestionOrganisation.vue";
+
+const props = defineProps({
+    auteur: Object,
+});
+const { auteur } = toRefs(props);
+</script>
