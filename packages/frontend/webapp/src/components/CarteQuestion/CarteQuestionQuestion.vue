ed317e52d4248a7dc091356af45817c8c36f8f47
Christophe BENARD <ch.benard@gmail.com>
2022-12-20 10:35:05 +0100

    (1682) questions list view and card components added

---
+++ packages/frontend/webapp/src/components/CarteQuestion/CarteQuestionQuestion.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-md pt-4 pb-2">
+        <div class="text-primary text-display-md font-bold">
+            <span class="font-bold">
+                {{ question }}
+            </span>
+        </div>
+    </div>
+</template>
+<script setup>
+import { toRefs } from "vue";
+const props = defineProps({
+    question: {
+        type: String,
+        required: true,
+    },
+});
+const { question } = toRefs(props);
+</script>
