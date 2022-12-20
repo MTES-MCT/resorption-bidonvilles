a1252e68bafd0d8c599fba40f89bc9ad2c40c137
Christophe BENARD <ch.benard@gmail.com>
2022-12-20 10:35:05 +0100

    (1682) questions list view and card components added

---
+++ packages/frontend/webapp/src/components/CarteQuestion/CarteQuestionDateCreation.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex justify-start text-G600">
+        <!-- {{ formatDate(question.createdAt, "d M y à h:i") }} -->
+        {{ question.createdAt }}
+    </div>
+</template>
+
+<script setup>
+import { toRefs } from "vue";
+// import formatDate from "@/utils/formatDate";
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+const { question } = toRefs(props);
+</script>
---
+++ packages/frontend/webapp/src/components/ListeDesQuestions/ListeDesQuestionsVide.vue
@@ -0,0 +1,23 @@
+<template>
+    <div
+        class="max-w-lg px-6 mx-auto flex flex-col items-center justify-center md:max-w-screen-lg md:flex-row md:space-x-8"
+    >
+        <img :src="videImg" class="max-w-xs w-3/4" />
+        <div class="mt-10 md:mt-20 mb-10 md:mb-20">
+            <h1 class="text-3xl font-bold">
+                Aucun résultat pour cette recherche
+            </h1>
+            <article class="mt-4">
+                Soyez le premier - la première à
+                <Link to="/communaute/nouvelle-question">
+                    poser une question à la communauté
+                </Link>
+            </article>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import videImg from "@/assets/img/illustrations/vide.svg";
+import { Link } from "@resorptionbidonvilles/ui";
+</script>
