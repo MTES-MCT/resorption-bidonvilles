4d3ac5224001f5fc6f3af3f16d0b23b2f321cc80
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-28 15:44:29 +0100

    (1687) fiche question espace réponses

---
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionReponses.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-blue200 py-10">
+        <FicheQuestionNouvelleReponse :question="question" class="mb-12" />
+
+        <h1 class="text-lg font-bold mb-2">
+            {{ question.answers.length }} réponse{{
+                question.answers.length > 1 ? "s" : ""
+            }}
+        </h1>
+        <FicheQuestionListeDesReponses
+            :answers="question.answers"
+            v-if="question.answers.length > 0"
+        />
+    </div>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+import FicheQuestionNouvelleReponse from "./FicheQuestionNouvelleReponse/FicheQuestionNouvelleReponse.vue";
+import FicheQuestionListeDesReponses from "./FicheQuestionListeDesReponses.vue";
+
+const props = defineProps({
+    question: Object,
+});
+const { question } = toRefs(props);
+</script>
