ad1fe0f59a6017a2b7d0720c58b4f4a6dff40e79
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-28 15:36:05 +0100

    (1687) renommage questionService + ajout findAll

--- packages/api/server/services/question/findOne.ts
+++ packages/api/server/services/question/findOne.ts
@@ -2,9 +2,16 @@ import questionModel from '#server/models/questionModel';
 import ServiceError from '#server/errors/ServiceError';
 
 export default async (questionId) => {
-    const question = await questionModel.findOne(questionId);
+    let question;
+    let answers;
+    try {
+        question = await questionModel.findOne(questionId);
+        answers = await questionModel.getAnswers([questionId]);
+    } catch (error) {
+        throw new ServiceError('fetch_failed', error);
+    }
     if (question === null) {
         throw new ServiceError('fetch_failed', new Error('Impossible de retrouver la question en base de données'));
     }
-    return question;
+    return { ...question, answers };
 };
