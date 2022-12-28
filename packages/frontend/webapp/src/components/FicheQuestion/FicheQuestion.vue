283042da106e758620f727140a88b96a2d9af3c9
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-28 15:44:29 +0100

    (1687) fiche question espace réponses

--- packages/frontend/webapp/src/components/FicheQuestion/FicheQuestion.vue
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestion.vue
@@ -8,6 +8,7 @@
         <FicheQuestionDetails class="mb-2" :question="question" />
         <FicheQuestionDate class="mb-4" :question="question" />
     </ContentWrapper>
+    <FicheQuestionReponses :question="question" class="px-10 mt-10" />
 </template>
 
 <script setup>
@@ -20,6 +21,7 @@ import FicheQuestionTitre from "./FicheQuestionTitre/FicheQuestionTitre.vue";
 import FicheQuestionAuthor from "./FicheQuestionAuthor/FicheQuestionAuthor.vue";
 import FicheQuestionDetails from "./FicheQuestionDetails/FicheQuestionDetails.vue";
 import FicheQuestionDate from "./FicheQuestionDate/FicheQuestionDate.vue";
+import FicheQuestionReponses from "./FicheQuestionReponses/FicheQuestionReponses.vue";
 
 const props = defineProps({
     question: Object,
