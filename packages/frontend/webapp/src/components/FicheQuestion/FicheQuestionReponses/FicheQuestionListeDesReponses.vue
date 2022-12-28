3b7f450e6b42067cf4c6594a8c83931aacabca45
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-28 15:43:57 +0100

    (1687) fiche question liste des réponses

---
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionListeDesReponses.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <CarteCommentaire
+            v-for="answer in answers"
+            :key="answer.id"
+            :id="`message${answer.id}`"
+            :comment="answer"
+        />
+    </section>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+import CarteCommentaire from "@/components/CarteCommentaire/CarteCommentaire.vue";
+
+const props = defineProps({
+    answers: Array,
+});
+const { answers } = toRefs(props);
+</script>
