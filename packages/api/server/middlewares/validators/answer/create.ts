806161ddf371472c95bd0a8218ce4ba56a9384da
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-28 15:37:35 +0100

    (1687) route le validator createAnswer

---
+++ packages/api/server/middlewares/validators/answer/create.ts
@@ -0,0 +1,27 @@
+/* eslint-disable newline-per-chained-call */
+import { body, param } from 'express-validator';
+import questionModel from '#server/models/questionModel';
+
+export default [
+    param('id')
+        .custom(async (value, { req }) => {
+            let question;
+            try {
+                question = await questionModel.findOne(value);
+            } catch (error) {
+                throw new Error('Impossible de retrouver la question concernée en base de données');
+            }
+
+            if (question === null) {
+                throw new Error('La question concernée par la réponse n\'existe pas');
+            }
+
+            req.body.question = question;
+            return true;
+        }),
+
+    body('description')
+        .trim()
+        .notEmpty().withMessage('La description est obligatoire'),
+
+];
