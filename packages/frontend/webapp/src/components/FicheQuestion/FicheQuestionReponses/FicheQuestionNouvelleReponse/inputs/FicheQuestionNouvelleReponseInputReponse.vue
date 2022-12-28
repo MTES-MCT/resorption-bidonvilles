958dd647f8b680f29d2bd844693b1534d8976eba
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2022-12-28 15:43:23 +0100

    (1687) ficheQuestion composant nouvelle réponse

---
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionNouvelleReponse/inputs/FicheQuestionNouvelleReponseInputReponse.vue
@@ -0,0 +1,13 @@
+<template>
+    <TextArea
+        :rows="5"
+        id="answer"
+        :label="labels.answer"
+        placeholder="Saisissez ici votre réponse"
+    />
+</template>
+
+<script setup>
+import labels from "../FicheQuestionNouvelleReponse.labels";
+import { TextArea } from "@resorptionbidonvilles/ui";
+</script>
