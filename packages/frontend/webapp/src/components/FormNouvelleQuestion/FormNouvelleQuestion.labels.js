b1607679ba9ad131411b2dc10f09da3a785119cb
Christophe BENARD <ch.benard@gmail.com>
2023-01-09 14:54:08 +0100

    (1744) Rephrasing question about themes involved in a question

--- packages/frontend/webapp/src/components/FormNouvelleQuestion/FormNouvelleQuestion.labels.js
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/FormNouvelleQuestion.labels.js
@@ -1,6 +1,7 @@
 export default {
     question: "Votre question en une phrase",
-    tags: "La ou les thématique(s) de votre question",
+    tags: "Quelle(s) thématique(s) votre question aborde-t-elle ?",
+    tags_info: "Indiquez la ou les thématique(s) de votre question",
     other_tag: "Veuillez préciser votre thématique",
     people_affected: "Combien d'habitants sont concernés par votre question ?",
     details: "Votre question, en détail",
