29dd9be093c3903a35e5a45218f4a6fef91cd529
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-09 16:07:58 +0100

    (1737) section droits communs

---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/sections/FormMiseAJourIndicateursDroitsCommuns.vue
@@ -0,0 +1,26 @@
+<template>
+    <FormSection id="droits_communs">
+        <template v-slot:title>Droits communs et ressources</template>
+
+        <FormParagraph title="Nombre de personnes avec...">
+            <FormMiseAJourIndicateursTextInput
+                name="domiciliation"
+                label="une domiciliation"
+            />
+            <FormMiseAJourIndicateursTextInput
+                name="droits_caf"
+                label="des droits CAF ouverts"
+            />
+            <FormMiseAJourIndicateursTextInput
+                name="emploi_stable"
+                label="un emploi stable / ressources suffisantes"
+            />
+        </FormParagraph>
+    </FormSection>
+</template>
+
+<script setup>
+import FormSection from "@/components/FormSection/FormSection.vue";
+import FormParagraph from "@/components/FormParagraph/FormParagraph.vue";
+import FormMiseAJourIndicateursTextInput from "../inputs/FormMiseAJourIndicateursTextInput.vue";
+</script>
