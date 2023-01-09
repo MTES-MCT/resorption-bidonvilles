0bd87c31c0d300e4d80f85656bccb10e6626b848
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-09 16:07:33 +0100

    (1737) section date

---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/inputs/FormMiseAJourIndicateursInputDate.vue
@@ -0,0 +1,28 @@
+<template>
+    <DatepickerInput
+        name="date"
+        id="date"
+        :label="labels.date"
+        :minDate="minDate"
+        :maxDate="today"
+        width="w-64"
+        showMandatoryStar
+    />
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+import labels from "../FormMiseAJourIndicateurs.labels";
+
+import { DatepickerInput } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    minDate: {
+        type: Date,
+        required: false,
+        default: undefined,
+    },
+});
+const { minDate } = toRefs(props);
+const today = new Date();
+</script>
---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/sections/FormMiseAJourIndicateursDate.vue
@@ -0,0 +1,22 @@
+<template>
+    <InputDate :minDate="minDate" withoutMargin />
+</template>
+
+<script setup>
+import { defineProps, toRefs, computed } from "vue";
+
+import InputDate from "../inputs/FormMiseAJourIndicateursInputDate.vue";
+const props = defineProps({
+    plan: Object,
+});
+const { plan } = toRefs(props);
+
+const minDate = computed(() => {
+    if (plan.value.states.length === 0) {
+        return;
+    }
+    const d = new Date(plan.value.states.slice(-1)[0].date);
+    d.setDate(d.getDate() + 1);
+    return d;
+});
+</script>
