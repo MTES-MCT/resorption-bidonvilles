bdda1d4e76b75334eb0a676f14c61a0ee3f39ace
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-09 16:03:26 +0100

    (1737) FormIndicateursMiseAjourTextInput.vue

---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/inputs/FormMiseAJourIndicateursTextInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <TextInput
+        :name="name"
+        :label="label"
+        width="w-32"
+        rules="numeric|min_value:0"
+    ></TextInput>
+</template>
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+import { TextInput } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    name: String,
+    label: String,
+});
+const { name, label } = toRefs(props);
+</script>
