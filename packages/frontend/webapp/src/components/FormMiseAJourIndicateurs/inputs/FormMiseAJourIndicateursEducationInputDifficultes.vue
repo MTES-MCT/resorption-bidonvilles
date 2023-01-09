325ee4efd4ec3fe2d5dc215f000004eade5f7344
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-09 16:08:43 +0100

    (1737) section education

---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/inputs/FormMiseAJourIndicateursEducationInputDifficultes.vue
@@ -0,0 +1,32 @@
+<template>
+    <InputLabel :label="labels.difficultes" />
+    <CheckableGroup :label="label" direction="row">
+        <Checkbox
+            v-for="option in options"
+            :key="option.value"
+            :value="option.value"
+            name="difficultes"
+            :label="option.label"
+        />
+    </CheckableGroup>
+</template>
+<script setup>
+import { ref } from "vue";
+
+import labels from "../FormMiseAJourIndicateurs.labels";
+
+import {
+    InputLabel,
+    CheckableGroup,
+    Checkbox,
+} from "@resorptionbidonvilles/ui";
+
+const options = ref([
+    { value: "cantine", label: "Cantine" },
+    {
+        value: "place_up2a",
+        label: "Manque de place en UP2A",
+    },
+    { value: "transport", label: "Transport" },
+]);
+</script>
---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/sections/FormMiseAJourIndicateursEducation.vue
@@ -0,0 +1,48 @@
+<template>
+    <FormSection id="education">
+        <template v-slot:title>Éducation et scolarisation</template>
+
+        <FormMiseAJourIndicateursTextInput
+            name="scolarisables"
+            :label="labels.scolarisables"
+        />
+        <FormMiseAJourIndicateursTextInput
+            name="en_mediation"
+            :label="labels.en_mediation"
+        />
+
+        <FormParagraph title="Nombre d'enfants inscrits en...">
+            <div class="flex">
+                <FormMiseAJourIndicateursTextInput
+                    name="maternelles"
+                    :label="labels.maternelles"
+                />
+                <FormMiseAJourIndicateursTextInput
+                    class="ml-4"
+                    name="elementaires"
+                    :label="labels.elementaires"
+                />
+                <FormMiseAJourIndicateursTextInput
+                    class="ml-4"
+                    name="colleges"
+                    :label="labels.colleges"
+                />
+                <FormMiseAJourIndicateursTextInput
+                    class="ml-4"
+                    name="lycees"
+                    :label="labels.lycees"
+                />
+            </div>
+        </FormParagraph>
+        <InputDifficultes />
+    </FormSection>
+</template>
+
+<script setup>
+import labels from "../FormMiseAJourIndicateurs.labels";
+
+import FormMiseAJourIndicateursTextInput from "../inputs/FormMiseAJourIndicateursTextInput.vue";
+import InputDifficultes from "../inputs/FormMiseAJourIndicateursEducationInputDifficultes.vue";
+import FormSection from "@/components/FormSection/FormSection.vue";
+import FormParagraph from "@/components/FormParagraph/FormParagraph.vue";
+</script>
