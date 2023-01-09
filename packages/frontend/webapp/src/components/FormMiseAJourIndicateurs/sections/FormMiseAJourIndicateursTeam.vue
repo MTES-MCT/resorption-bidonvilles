b2e21466e95134e7116c2263e1b939288043705d
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-09 16:05:11 +0100

    (1737) section etp

---
+++ packages/frontend/webapp/src/components/FormMiseAJourIndicateurs/sections/FormMiseAJourIndicateursTeam.vue
@@ -0,0 +1,12 @@
+<template>
+    <FormSection id="team">
+        <template v-slot:title>Equipe</template>
+
+        <FormMiseAJourIndicateursInputEtp />
+    </FormSection>
+</template>
+
+<script setup>
+import FormSection from "@/components/FormSection/FormSection.vue";
+import FormMiseAJourIndicateursInputEtp from "../inputs/FormMiseAJourIndicateursInputEtp.vue";
+</script>
