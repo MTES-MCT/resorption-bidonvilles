482238d357c19c5c645efb9b1089f61cfbd3371c
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-10 16:44:05 +0100

    (1708) ajout bouton historique à chaque rubrique de la fiche site

--- packages/frontend/webapp/src/components/FicheRubrique/FicheRubrique.vue
+++ packages/frontend/webapp/src/components/FicheRubrique/FicheRubrique.vue
@@ -1,15 +1,33 @@
 <template>
     <section class="shadow-lg p-6">
-        <h1 class="text-3xl text-primary">{{ title }}</h1>
+        <div class="flex justify-between">
+            <h1 class="text-3xl text-primary">{{ title }}</h1>
+            <Button
+                size="sm"
+                icon="history"
+                iconPosition="left"
+                variant="primaryText"
+                @click="openHistorique"
+                >voir l'historique des modifications</Button
+            >
+        </div>
         <slot />
     </section>
 </template>
 
 <script setup>
 import { defineProps, toRefs } from "vue";
+import { useEventBus } from "@/helpers/event-bus";
+
+import { Button } from "@resorptionbidonvilles/ui";
 
 const props = defineProps({
     title: String,
 });
 const { title } = toRefs(props);
+const { emit } = useEventBus();
+
+function openHistorique() {
+    emit("fichesite:openHistorique");
+}
 </script>
