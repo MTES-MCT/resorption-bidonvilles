3976bdc534a88cc9a7de6e5125ebede1787aecd1
Christophe BENARD <ch.benard@gmail.com>
2023-01-16 16:41:42 +0100

    (1744) Fix the size of the response block

--- packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionAuthor/FicheQuestionAuthor.vue
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionAuthor/FicheQuestionAuthor.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="flex justify-between">
-        <div class="text-primary font-bold mb-1 mt-2">
-            <LinkOrganization :to="`/structure/${author.organization_id}`">
-                {{ author.first_name }}
-                {{ author.last_name }} -
-                {{ author.organization }}
-            </LinkOrganization>
+    <ContentWrapper>
+        <div class="flex justify-between">
+            <div class="text-primary font-bold mb-1 mt-2">
+                <LinkOrganization :to="`/structure/${author.organization_id}`">
+                    {{ author.first_name }}
+                    {{ author.last_name }} -
+                    {{ author.organization }}
+                </LinkOrganization>
+            </div>
         </div>
-    </div>
+    </ContentWrapper>
 </template>
 
 <script setup>
 import { defineProps, toRefs } from "vue";
-
 import { LinkOrganization } from "@resorptionbidonvilles/ui";
+import ContentWrapper from "@/components/ContentWrapper/ContentWrapper.vue";
 
 const props = defineProps({
     author: Object,
