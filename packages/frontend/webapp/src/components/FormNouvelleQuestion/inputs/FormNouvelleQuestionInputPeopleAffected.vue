17518918ced375add498cfb61af45ada93a60aa8
Christophe BENARD <ch.benard@gmail.com>
2023-01-16 15:09:01 +0100

    (1744) Reduce the size of the "number of inhabitants concerned" field

--- packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputPeopleAffected.vue
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputPeopleAffected.vue
@@ -3,6 +3,7 @@
         name="people_affected"
         id="people_affected"
         info="Incitez les autres membres de la communauté à vous répondre rapidement en indiquant le nombre de personnes qui ont besoin d'aide"
+        width="w-28"
     />
 </template>
 
