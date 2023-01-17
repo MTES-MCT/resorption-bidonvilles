5ceeb58a5c74f0e8e71a9bd867c20ceebcdf72b3
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-01-17 16:11:11 +0100

    (1742) add event Listener callRouter

--- packages/frontend/mobile/src/App.vue
+++ packages/frontend/mobile/src/App.vue
@@ -6,6 +6,14 @@
 </template>
 
 <script setup>
+import { onMounted } from "vue";
 import NotificationGroup from "#src/js/components/Notification/NotificationGroup.vue";
 import { RouterView } from "vue-router";
+import router from "./js/router";
+
+onMounted(() => {
+    window.addEventListener("callRouter", ({ detail }) => {
+        router[detail.routerMethod](...(detail.routerArgs || []));
+    });
+});
 </script>
