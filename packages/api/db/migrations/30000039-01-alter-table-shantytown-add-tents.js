4f40097fd11f5d44d705fe02706dc1e0b196ae49
Christophe BENARD <ch.benard@gmail.com>
2023-01-18 14:24:19 +0100

    (1640) Rollback a transaction in case of failure

--- packages/api/db/migrations/30000039-01-alter-table-shantytown-add-tents.js
+++ packages/api/db/migrations/30000039-01-alter-table-shantytown-add-tents.js
@@ -1,43 +1,62 @@
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
-        transaction => Promise.all([
-            queryInterface.addColumn(
-                'shantytowns',
-                'tents',
-                {
-                    type: Sequelize.INTEGER,
-                    allowNull: true,
-                },
-                {
-                    transaction,
-                },
-            ),
-            queryInterface.addColumn(
-                'ShantytownHistories',
-                'tents',
-                {
-                    type: Sequelize.INTEGER,
-                    allowNull: true,
-                },
-                {
-                    transaction,
-                },
-            ),
-        ]),
-    ),
+    async up(queryInterface, Sequelize) {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await Promise.all(
+                [
+                    queryInterface.addColumn(
+                        'shantytowns',
+                        'tents',
+                        {
+                            type: Sequelize.INTEGER,
+                            allowNull: true,
+                        },
+                        {
+                            transaction,
+                        },
+                    ),
+                    queryInterface.addColumn(
+                        'ShantytownHistories',
+                        'tents',
+                        {
+                            type: Sequelize.INTEGER,
+                            allowNull: true,
+                        },
+                        {
+                            transaction,
+                        },
+                    ),
+                ],
+            );
+            return transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
 
-    down: queryInterface => queryInterface.sequelize.transaction(
-        transaction => Promise.all([
-            queryInterface.removeColumn(
-                'shantytowns',
-                'tents',
-                { transaction },
-            ),
-            queryInterface.removeColumn(
-                'ShantytownHistories',
-                'tents',
-                { transaction },
-            ),
-        ]),
-    ),
+
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await Promise.all(
+                [
+                    queryInterface.removeColumn(
+                        'shantytowns',
+                        'tents',
+                        { transaction },
+                    ),
+                    queryInterface.removeColumn(
+                        'ShantytownHistories',
+                        'tents',
+                        { transaction },
+                    ),
+                ],
+            );
+            return transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
 };
