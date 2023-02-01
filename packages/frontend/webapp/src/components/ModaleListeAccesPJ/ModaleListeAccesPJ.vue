90f5bf0944a399fdb067039a43381795aab3b136
Christophe BENARD <ch.benard@gmail.com>
2023-02-01 19:22:55 +0100

    (1749) Modale affichant la liste des droits sur les PJ

---
+++ packages/frontend/webapp/src/components/ModaleListeAccesPJ/ModaleListeAccesPJ.vue
@@ -0,0 +1,58 @@
+<template>
+    <Modal :isOpen="isOpen" closeWhenClickOutside @close="close">
+        <template v-slot:title>
+            <!-- Qui a accès aux données judiciaires de ce site ? -->
+            {{ title }}
+        </template>
+
+        <template v-slot:body>
+            <CarteAutorisationAccesAuxPJ
+                :usersWithPermissionOnJustice="permissionsToAccessJustice"
+                class="max-w-2xl"
+            />
+        </template>
+        <template v-slot:footer>
+            <!-- <Button variant="primaryText" @click="isOpen = false" -->
+            <Button class="ml-5" :loading="loading" @click="isOpen = false"
+                >J'ai compris</Button
+            >
+        </template>
+    </Modal>
+</template>
+
+<script setup>
+import { ref, defineExpose, toRefs } from "vue";
+import { Button, Modal } from "@resorptionbidonvilles/ui";
+import CarteAutorisationAccesAuxPJ from "@/components/CarteAutorisationAccesAuxPJ/CarteAutorisationAccesAuxPJ.vue";
+
+const props = defineProps({
+    permissionsToAccessJustice: {
+        type: Array,
+        default: () => {},
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+});
+
+const { permissionsToAccessJustice, title } = toRefs(props);
+
+const isOpen = ref(false);
+const error = ref(null);
+
+function reset() {
+    error.value = null;
+}
+
+function close() {
+    isOpen.value = false;
+    reset();
+}
+
+defineExpose({
+    open() {
+        isOpen.value = true;
+    },
+});
+</script>
