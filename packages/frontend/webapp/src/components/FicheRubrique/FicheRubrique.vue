79d907fed6e3447af32db1d8ffbceeabfc6216bd
Christophe BENARD <ch.benard@gmail.com>
2023-02-01 16:56:33 +0100

    (1749) + lien pour accéder aux acteurs qui ont accès aux PJ

--- packages/frontend/webapp/src/components/FicheRubrique/FicheRubrique.vue
+++ packages/frontend/webapp/src/components/FicheRubrique/FicheRubrique.vue
@@ -2,15 +2,16 @@
     <section class="shadow-lg p-6">
         <div class="flex justify-between">
             <h1 class="text-3xl text-primary">{{ title }}</h1>
-            <Button
-                v-if="category"
-                size="sm"
-                icon="history"
-                iconPosition="left"
-                variant="primaryText"
-                @click="openHistorique"
-                >voir l'historique des modifications</Button
-            >
+            <div>
+                <Button v-if="category == 'justice'" size="sm" icon="user-group" iconPosition="left"
+                    variant="primaryText" @click="openListAccesPJ">
+                    Qui a accès aux données judiciaires de ce site ?
+                </Button>
+                <Button v-if="category" size="sm" icon="history" iconPosition="left" variant="primaryText"
+                    @click="openHistorique">
+                    voir l'historique des modifications
+                </Button>
+            </div>
         </div>
         <slot />
     </section>
@@ -31,5 +32,9 @@ const { emit } = useEventBus();
 
 function openHistorique() {
     emit("fichesite:openHistorique", category.value);
+}
+
+function openListAccesPJ() {
+    emit("fichesitepj:openListAccesPJ");
 }
 </script>
