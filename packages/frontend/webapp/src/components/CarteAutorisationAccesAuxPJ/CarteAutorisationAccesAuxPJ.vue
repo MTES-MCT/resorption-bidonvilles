3c9eb063005179a0d0769b41173573b894b3766d
Christophe BENARD <ch.benard@gmail.com>
2023-02-07 14:17:54 +0100

    (1749) Affichage d'un loader si la liste des permissions n'est pas chargée

--- packages/frontend/webapp/src/components/CarteAutorisationAccesAuxPJ/CarteAutorisationAccesAuxPJ.vue
+++ packages/frontend/webapp/src/components/CarteAutorisationAccesAuxPJ/CarteAutorisationAccesAuxPJ.vue
@@ -1,21 +1,26 @@
 <template>
     <div @mouseenter="isHover = true" @mouseleave="isHover = false">
-        <div v-if="organizationsWithUsersHavingJusticePermissions.length > 0">
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                <CarteStructure
-                    v-for="organization in organizationsWithUsersHavingJusticePermissions"
-                    :key="organization.id"
-                    :organization="organization"
-                    :wording="wording"
-                />
-            </div>
-        </div>
+        <Loading v-if="loading" />
         <div v-else>
-            <Icon icon="lock" class="text-red" />
-            <span class="pl-1 font-bold">
-                Seuls les utilisateurs en préfecture et DEETS / DREETS ont accès
-                aux données judiciaires de ce site.</span
+            <div
+                v-if="organizationsWithUsersHavingJusticePermissions.length > 0"
             >
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    <CarteStructure
+                        v-for="organization in organizationsWithUsersHavingJusticePermissions"
+                        :key="organization.id"
+                        :organization="organization"
+                        :wording="wording"
+                    />
+                </div>
+            </div>
+            <div v-else>
+                <Icon icon="lock" class="text-red" />
+                <span class="pl-1 font-bold">
+                    Seuls les utilisateurs en préfecture et DEETS / DREETS ont
+                    accès aux données judiciaires de ce site.</span
+                >
+            </div>
         </div>
     </div>
 </template>
@@ -23,6 +28,7 @@
 import { defineProps, toRefs, ref, computed } from "vue";
 import CarteStructure from "@/components/CarteStructure/CarteStructure.vue";
 import { Icon } from "@resorptionbidonvilles/ui";
+import Loading from "@/components/Loading/Loading.vue";
 
 const props = defineProps({
     permissionsToAccessJustice: Object,
@@ -36,7 +42,13 @@ const wording = ref({
     second: "à accéder aux procédures judiciaires",
 });
 
+let loading = ref(true);
+
 const organizationsWithUsersHavingJusticePermissions = computed(() => {
     return Object.values(permissionsToAccessJustice.value);
+});
+
+loading = computed(() => {
+    return organizationsWithUsersHavingJusticePermissions.value ? false : true;
 });
 </script>
