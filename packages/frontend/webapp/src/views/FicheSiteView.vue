7b1ea04a24dbf4cff26a89b5e3b9ab5cbb53c00f
Christophe BENARD <ch.benard@gmail.com>
2023-02-08 10:28:38 +0100

    (1749) Suppression du peuplement des autoristions d'accès aux PJ transfert du peuplement dans la modale

--- packages/frontend/webapp/src/views/FicheSiteView.vue
+++ packages/frontend/webapp/src/views/FicheSiteView.vue
@@ -23,11 +23,7 @@
     </LayoutError>
 
     <Layout :paddingBottom="false" v-else>
-        <FicheSite
-            :town="town"
-            :permissionsToAccessJustice="permissionsToAccessJustice"
-            :v-if="town"
-        />
+        <FicheSite :town="town" :v-if="town" />
     </Layout>
 </template>
 
@@ -43,14 +39,11 @@ import LayoutError from "@/components/LayoutError/LayoutError.vue";
 import LayoutLoading from "@/components/LayoutLoading/LayoutLoading.vue";
 import FicheSite from "@/components/FicheSite/FicheSite.vue";
 import ButtonContact from "@/components/ButtonContact/ButtonContact.vue";
-import { fetchAll } from "@/api/permissions.api";
-import getReducedLoadedPermissionsToAccessJustice from "@common/helpers/permission/getReducedLoadedPermissionsToAccessJustice";
 
 const townsStore = useTownsStore();
 const isLoading = ref(null);
 const error = ref(null);
 const town = ref(null);
-const loadedPermissionsToAccessJustice = ref(null);
 
 const townId = computed(() => {
     return parseInt(router.currentRoute.value.params.id, 10);
@@ -74,37 +67,10 @@ async function load() {
     try {
         town.value = await townsStore.fetchTown(townId.value);
         townsStore.townCategoryFilter = [];
-        await loadPermissionsToAccessJustice();
     } catch (e) {
         error.value = e?.code || "Erreur inconnue";
     }
 
     isLoading.value = false;
 }
-
-async function loadPermissionsToAccessJustice() {
-    try {
-        const location = {
-            type: "city",
-            city: town.value.city,
-            epci: town.value.epci,
-            departement: town.value.departement,
-            region: town.value.region,
-        };
-        loadedPermissionsToAccessJustice.value = await fetchAll(location);
-    } catch (e) {
-        // Do nothing
-    }
-}
-
-const permissionsToAccessJustice = computed(() => {
-    let usersWithPermissionsToAccessJustice = [];
-    if (loadedPermissionsToAccessJustice.value) {
-        usersWithPermissionsToAccessJustice =
-            getReducedLoadedPermissionsToAccessJustice(
-                loadedPermissionsToAccessJustice.value
-            );
-    }
-    return usersWithPermissionsToAccessJustice;
-});
 </script>
