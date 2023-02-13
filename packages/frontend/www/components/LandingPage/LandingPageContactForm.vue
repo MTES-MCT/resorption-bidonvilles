7dedb8f4b414e6ef238644e8bf33b7cb547f6696
Christophe BENARD <ch.benard@gmail.com>
2023-02-13 16:31:15 +0100

    [WIP] Remplacer le champ email par le bouton "Demander un accès"

--- packages/frontend/www/components/LandingPage/LandingPageContactForm.vue
+++ packages/frontend/www/components/LandingPage/LandingPageContactForm.vue
@@ -1,11 +1,9 @@
 <template>
-    <form class="flex flex-row justify-center mt-4 w-full max-w-md" @submit.prevent="onSubmit">
-        <input class="hidden md:block border-2 border-G200 rounded-md flex-1 py-2 px-4 outline-none focus:border-G400"
-            :placeholder="$t('landingPage.contactForm.email')" v-model="email" />
-        <Button type="submit" variant="secondary" class="ml-1">{{
-                $t("landingPage.contactForm.cta")
-        }}</Button>
-    </form>
+    <Button variant="secondary" class="xs:ml-1 sm:mr-4 my-4" @click="() => redirectToContact()">{{ $t("landingPage.firstBanner.cta") }}</Button>
+    
+    <Button variant="primary" class="xs:ml-1 my-4">{{
+            $t("landingPage.contactForm.cta")
+    }}</Button>
 </template>
 
 <script>
