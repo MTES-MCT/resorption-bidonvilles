86863645af30a57f4f8807c1e8662f2ea81c677a
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-02-15 09:51:20 +0100

    (1787) permettre opérateur et pilote indentiques

--- packages/api/server/models/actionModel/create/insertPermissions.ts
+++ packages/api/server/models/actionModel/create/insertPermissions.ts
@@ -5,8 +5,10 @@ import { Attachment } from '#server/models/permissionModel/types/Attachment.d';
 export default async (actionId: number, users: number[], transaction: Transaction): Promise<void> => {
     const attachment: Attachment[] = [{ type: 'action', id: actionId }];
 
+    const uniqueUsers = new Set(users);
+
     await Promise.all(
-        users.map(userId => [
+        Array.from(uniqueUsers).map(userId => [
             applyAttachments(attachment).toUser(userId).onFeature('read', 'action', transaction),
             applyAttachments(attachment).toUser(userId).onFeature('read', 'action_comment', transaction),
             applyAttachments(attachment).toUser(userId).onFeature('update', 'action', transaction),
