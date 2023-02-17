ca1e5146ab15139cb85de0143d71c3bcac57c7f3
Christophe BENARD <ch.benard@gmail.com>
2023-02-17 14:57:46 +0100

    (1778) Add a method to redirect to access request form

---
+++ packages/frontend/www/utils/redirectToAccessRequest.js
@@ -0,0 +1,5 @@
+export default function () {
+    const { WEBAPP_URL, DOMAIN } = useRuntimeConfig();
+    document.cookie = `device=webapp;domain=${DOMAIN}`;
+    window.location = `${WEBAPP_URL}/contact?request_type=access-request`;
+}
