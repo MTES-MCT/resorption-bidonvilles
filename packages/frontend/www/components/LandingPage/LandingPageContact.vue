55b813b2aee82760e3084612965168e2e5463037
Christophe BENARD <ch.benard@gmail.com>
2023-02-17 15:30:13 +0100

    (1778) Add a method to redirect to access request vue renamed because it is no more a form (LandingPageContactForm.vue is now LandingPageContact.vue)

---
+++ packages/frontend/www/components/LandingPage/LandingPageContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <Button type="button" variant="secondary" class="xs:ml-1 sm:mr-4 my-4" @click="routeToAccessRequest">{{
+        $t("landingPage.firstBanner.cta") }}</Button>
+
+    <Button type="button" variant="primary" class="xs:ml-1 my-4" @click="routeToContact">{{
+        $t("landingPage.contactForm.cta")
+    }}</Button>
+</template>
+
+<script>
+import { Button } from "@resorptionbidonvilles/ui";
+import redirectToContact from "~~/utils/redirectToContact";
+import redirectToAccessRequest from "~~/utils/redirectToAccessRequest";
+
+export default {
+    components: {
+        Button
+    },
+    data() {
+        return {
+            email: "",
+        };
+    },
+    methods: {
+        routeToAccessRequest() {
+            redirectToAccessRequest();
+        },
+        routeToContact() {
+            redirectToContact();
+        }
+    }
+};
+</script>
