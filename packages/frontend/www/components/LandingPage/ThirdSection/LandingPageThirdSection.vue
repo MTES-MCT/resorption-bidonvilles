43bdcd283d5a888d244bc17b655f34ad49c97c8b
Christophe BENARD <ch.benard@gmail.com>
2023-02-17 15:32:38 +0100

    (1778) `LandingPageContactForm` renamed `LandingPageContact`

--- packages/frontend/www/components/LandingPage/ThirdSection/LandingPageThirdSection.vue
+++ packages/frontend/www/components/LandingPage/ThirdSection/LandingPageThirdSection.vue
@@ -1,14 +1,12 @@
 <template>
     <div>
         <LandingPageFAQ v-if="$i18n.locale === 'fr'" />
-        <LandingPageNewsletter
-            :class="$i18n.locale === 'fr' ? 'mt-20' : ''"
-        />
+        <LandingPageNewsletter :class="$i18n.locale === 'fr' ? 'mt-20' : ''" />
         <div class="text-center mt-20">
             <h2 class="text-display-lg font-bold text-secondary">
                 {{ $t("landingPage.hero.subtitle") }}
             </h2>
-            <LandingPageContactForm class="mx-auto" />
+            <LandingPageContact class="mx-auto" />
         </div>
     </div>
 </template>
@@ -16,5 +14,5 @@
 <script setup>
 import LandingPageFAQ from "./LandingPageFAQ.vue";
 import LandingPageNewsletter from "./LandingPageNewsletter.vue";
-import LandingPageContactForm from "../LandingPageContactForm.vue";
+import LandingPageContact from "../LandingPageContact.vue";
 </script>
