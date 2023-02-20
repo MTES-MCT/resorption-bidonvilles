24b048ee10128010508d268665c9129b55abfaa6
Anis Safine Laget <anis@users.noreply.github.com>
2023-02-20 17:35:39 +0100

    (1752) simplifier un modèle

--- packages/api/server/models/organizationModel/findByName.ts
+++ packages/api/server/models/organizationModel/findByName.ts
@@ -1,18 +1,15 @@
 import { sequelize } from '#db/sequelize';
 import { QueryTypes } from 'sequelize';
 
-export default async (name) => {
-    const result = await sequelize.query(
-        `SELECT
-            organization_id as id
-        FROM organizations
-        WHERE UNACCENT(organizations.name) ILIKE UNACCENT(:name)`,
-        {
-            type: QueryTypes.SELECT,
-            replacements: {
-                name,
-            },
+export default name => sequelize.query(
+    `SELECT
+        organization_id as id
+    FROM organizations
+    WHERE UNACCENT(organizations.name) ILIKE UNACCENT(:name)`,
+    {
+        type: QueryTypes.SELECT,
+        replacements: {
+            name,
         },
-    );
-    return result;
-};
+    },
+);
