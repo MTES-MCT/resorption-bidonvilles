f9fcd672b8b021345d88934c7d0d61b0d08ccae8
Anis Safine Laget <anis@users.noreply.github.com>
2023-02-20 10:18:12 +0100

    (1778) supprimer l'utils redirectToContact

--- packages/frontend/www/utils/redirectToContact.js
+++
@@ -1,5 +0,0 @@
-export default function () {
-    const { WEBAPP_URL, DOMAIN } = useRuntimeConfig();
-    document.cookie = `device=webapp;domain=${DOMAIN}`;
-    window.location = `${WEBAPP_URL}/contact`;
-}
