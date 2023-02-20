7d3b8919f8778d1d4fb51837556ae59d4714f6a6
Anis Safine Laget <anis@users.noreply.github.com>
2023-02-20 17:27:20 +0100

    (1735) corriger la disposition des filtres mobile

--- packages/frontend/mobile/src/js/pages/TownsSearch/TownSearchFilter.vue
+++ packages/frontend/mobile/src/js/pages/TownsSearch/TownSearchFilter.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="flex justify-center py-2 bg-G300 border-b-2 border-G400">
+    <div
+        class="whitespace-nowrap overflow-y-auto flex pt-2 pb-4 bg-G300 border-b-2 border-G400"
+    >
         <div
             class="px-3 text-primary"
             v-for="filter in filters"
