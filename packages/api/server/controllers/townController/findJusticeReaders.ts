4fe88b3f432abc3c4dd033b1573b308c7d3b05b5
Anis Safine Laget <anis@users.noreply.github.com>
2023-02-21 11:34:28 +0100

    (1749) ajouter types sur ctrl findJusticeReaders

--- packages/api/server/controllers/townController/findJusticeReaders.ts
+++ packages/api/server/controllers/townController/findJusticeReaders.ts
@@ -1,14 +1,23 @@
 import shantytownService from '#server/services/shantytown';
+import { Request, NextFunction, Response } from 'express';
+
+interface FindJusticeReadersRequest extends Request {
+    params: {
+        id: string,
+    }
+}
 
 const ERROR_RESPONSES = {
     fetch_failed: { code: 400, message: 'Une lecture en base de données a échoué' },
     undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
 };
 
-export default async (req, res, next): Promise<void> => {
+export default async (req: FindJusticeReadersRequest, res: Response, next: NextFunction): Promise<void> => {
     try {
         res.status(200).send(
-            await shantytownService.findJusticeReaders(req.params.id),
+            await shantytownService.findJusticeReaders(
+                parseInt(req.params.id, 10),
+            ),
         );
     } catch (error) {
         const { code, message } = ERROR_RESPONSES[error && error.code] || ERROR_RESPONSES.undefined;
