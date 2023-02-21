c7fa124cca13e60dae8110011acb44345e0444d8
Anis Safine Laget <anis@users.noreply.github.com>
2023-02-21 16:00:25 +0100

    (1777) mettre en évidence le n° de hotline

---
+++ packages/frontend/webapp/src/components/FooterBar/FooterBarHotline.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="max-w-md mt-8 lg:mt-0">
+        <h1 class="font-bold text-lg">Un problème, une question ?</h1>
+        <p class="text-sm">Appelez nous au +33 1 40 81 95 39</p>
+    </section>
+</template>
