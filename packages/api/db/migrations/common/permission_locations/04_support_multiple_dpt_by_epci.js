e84a05ff4e5c75521314c0d99162b3b311216f5c
Anis Safine Laget <anis@users.noreply.github.com>
2023-03-01 12:23:12 +0100

    (1810) fix permission for epci users

---
+++ packages/api/db/migrations/common/permission_locations/04_support_multiple_dpt_by_epci.js
@@ -0,0 +1,69 @@
+module.exports = [
+    // writing_locations
+    `CREATE OR REPLACE VIEW local_writing_locations AS
+    SELECT
+        organization_id,
+        CASE
+            WHEN location_type = 'nation' then null
+            WHEN location_type = 'region' then array[region_code]
+            ELSE array[]::varchar[]
+        END AS "regions",
+        CASE
+            WHEN location_type = 'nation' then null
+            WHEN location_type = 'departement' then array[departement_code]
+            ELSE array[]::varchar[]
+        END AS "departements",
+        CASE
+            WHEN location_type = 'nation' then null
+            WHEN location_type = 'epci' then array[epci_code]
+            ELSE array[]::varchar[]
+        END AS "epci",
+        CASE
+            WHEN location_type = 'nation' then null
+            WHEN location_type = 'city' then array[city_code]
+            ELSE array[]::varchar[]
+        END AS "cities",
+        CASE
+            WHEN location_type = 'nation' then NULL
+            ELSE array[]::integer[]
+        END AS "shantytowns",
+        CASE
+            WHEN location_type = 'nation' then null
+            ELSE array[]::integer[]
+        END AS "actions"
+    FROM localized_organizations`,
+
+    // reading_locations
+    `CREATE OR REPLACE VIEW local_reading_locations AS
+    SELECT
+        organization_id,
+        CASE
+            WHEN location_type = 'nation' then null
+            WHEN location_type = 'region' then array[region_code]
+            ELSE array[]::varchar[]
+        END AS "regions",
+        CASE
+            WHEN location_type = 'nation' then null
+            WHEN location_type = 'epci' then epci_departements.departements
+            WHEN location_type IN ('city', 'departement') then array[departement_code]
+            ELSE array[]::varchar[]
+        END AS "departements",
+        CASE
+            WHEN location_type = 'nation' then null
+            ELSE array[]::varchar[]
+        END AS "epci",
+        CASE
+            WHEN location_type = 'nation' then null
+            ELSE array[]::varchar[]
+        END AS "cities",
+        CASE
+            WHEN location_type = 'nation' then NULL
+            ELSE array[]::integer[]
+        END AS "shantytowns",
+        CASE
+            WHEN location_type = 'nation' then null
+            ELSE array[]::integer[]
+        END AS "actions"
+    FROM localized_organizations
+    LEFT JOIN epci_departements ON localized_organizations.epci_code = epci_departements.fk_epci`,
+];
