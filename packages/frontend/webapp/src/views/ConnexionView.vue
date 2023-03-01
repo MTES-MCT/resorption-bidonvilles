ce5b6c7323a4fa02d100c29bb5b999e2484f6470
Christophe BENARD <ch.benard@gmail.com>
2023-03-01 22:21:33 +0100

    (1802) Add a property `reason`

--- packages/frontend/webapp/src/views/ConnexionView.vue
+++ packages/frontend/webapp/src/views/ConnexionView.vue
@@ -1,10 +1,16 @@
 <template>
     <Layout hero>
-        <FormConnexion />
+        <FormConnexion :reason="reason" />
     </Layout>
 </template>
 
 <script setup>
 import Layout from "@/components/Layout/Layout.vue";
 import FormConnexion from "@/components/FormConnexion/FormConnexion.vue";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+let reason = ref("");
+const route = useRoute();
+reason.value = route.query.reason ? route.query.reason : null;
 </script>
