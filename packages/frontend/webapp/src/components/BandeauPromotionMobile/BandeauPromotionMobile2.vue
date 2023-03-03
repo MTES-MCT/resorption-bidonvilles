8128b9e116d30ace6af8aaa1169221f7a29eb460
Christophe BENARD <ch.benard@gmail.com>
2023-03-03 15:07:35 +0100

    (1806) Bandeau pour la promo de l'appli mobile

---
+++ packages/frontend/webapp/src/components/BandeauPromotionMobile/BandeauPromotionMobile2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-yellow-200 px-4 py-2 flex items-center justify-between">
+        <div class="flex items-center">
+            <div
+                class="rounded-full bg-yellow-400 w-6 h-6 text-center text-size-xs leading-6"
+            >
+                <Icon icon="flag" />
+            </div>
+            <p class="ml-4">
+                <span class="font-bold">Vous intervenez sur ce site ?</span>
+                Faites le savoir à la communauté.<br />
+                <span class="text-sm"
+                    >Votre intervention mérite d'être signalée même si vous
+                    n'accompagnez pas l'ensemble des habitants du site.</span
+                >
+            </p>
+        </div>
+    </div>
+</template>
