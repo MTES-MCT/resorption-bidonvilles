2470c2a28d2033de2abc395dfd2fd8394bd5537f
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-03-07 10:10:28 +0100

    (1808) fix month and image names

--- packages/api/db/seeders/000000-changelog-2.3.6.js
+++ packages/api/db/seeders/000000-changelog-2.3.6.js
@@ -2,17 +2,17 @@ const { backUrl } = require('./config/config');
 
 const changelog = {
     app_version: '2.3.6',
-    date: new Date(2023, 3, 2),
+    date: new Date(2023, 2, 2),
     items: [
         {
             title: 'La plateforme dans votre poche !',
             description: '<p>Rendez-vous sur votre téléphone portable pour découvrir la première version mobile qui vous permettra de consulter la fiche d\'un site, d\'écrire sur le journal du site et de créer des notes personnelles. Pour vous connecter, utilisez la même adresse : https://resorption-bidonvilles.beta.gouv.fr/ et vos identifiants habituels.</p>',
-            image: `${backUrl}/assets/changelog/2.3.6/item_1.jpeg`,
+            image: `${backUrl}/assets/changelog/2.3.6/item_1.png`,
         },
         {
             title: 'Faites-le bilan de vos actions !',
             description: '<p>Rendez-vous sur le volet action pour déclarer vos actions et transmettre vos résultats 2022. Une belle occasion de valoriser le travail de terrain mené !</p>',
-            image: `${backUrl}/assets/changelog/2.3.6/item_2.gif`,
+            image: `${backUrl}/assets/changelog/2.3.6/item_2.png`,
         },
     ],
 };
