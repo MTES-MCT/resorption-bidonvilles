b5fb1da87e1a9164134a8a50d6cb86548b0ce414
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-03-08 11:26:24 +0100

    (1808) corrections seeder

--- packages/api/db/seeders/000000-changelog-2.3.0.js
+++ packages/api/db/seeders/000000-changelog-2.3.0.js
@@ -1,12 +1,12 @@
 const { backUrl } = require('./config/config');
 
 const changelog = {
-    app_version: '2.3.6',
-    date: new Date(2023, 2, 2),
+    app_version: '2.3.0',
+    date: new Date(2023, 1, 17),
     items: [
         {
             title: 'La plateforme dans votre poche !',
-            description: '<p>Rendez-vous sur votre téléphone portable pour découvrir la première version mobile qui vous permettra de consulter la fiche d\'un site, d\'écrire sur le journal du site et de créer des notes personnelles. Pour vous connecter, utilisez la même adresse : https://resorption-bidonvilles.beta.gouv.fr/ et vos identifiants habituels.</p>',
+            description: '<p>Rendez-vous sur votre téléphone portable pour découvrir la première version mobile qui vous permettra de consulter la fiche d\'un site, d\'écrire sur le journal du site et de créer des notes personnelles. Pour vous connecter, utilisez la même adresse : resorption-bidonvilles.beta.gouv.fr/ et vos identifiants habituels.</p>',
             image: `${backUrl}/assets/changelog/2.3.6/item_1.png`,
         },
         {
