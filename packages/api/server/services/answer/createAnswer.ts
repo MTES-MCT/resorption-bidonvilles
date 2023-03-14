44646debc62092ba1e8eeee8c836a26abe744572
Christophe BENARD <ch.benard@gmail.com>
2023-03-14 17:29:45 +0100

    (1773) Crée une méthode pour l'envoi d'un mail lors du post d'une réponse à une question dans l'espace "Communauté"

--- packages/api/server/services/answer/createAnswer.ts
+++ packages/api/server/services/answer/createAnswer.ts
@@ -1,5 +1,7 @@
+import userModel from '#server/models/userModel';
 import answerModel from '#server/models/answerModel';
 import ServiceError from '#server/errors/ServiceError';
+import sendMailForNewAnswer from './_common/sendMailForNewAnswer';
 
 export default async (answer, question, author) => {
     // on insère la réponse
@@ -12,6 +14,14 @@ export default async (answer, question, author) => {
         });
     } catch (error) {
         throw new ServiceError('insert_failed', error);
+    }
+
+    // On essaie d'envoyer un mail de notification à l'auteur de la question
+    try {
+        const questionAuthor = await userModel.findOne(question.createdBy.id);
+        await sendMailForNewAnswer(question.id, author, questionAuthor);
+    } catch (ignore) {
+        // ignore
     }
 
     // on retourne la réponse
