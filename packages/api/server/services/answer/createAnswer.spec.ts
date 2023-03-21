94ac0ceb7139c7e608fddc8ed696e4c1bc92091d
Anis Safine Laget <anis@users.noreply.github.com>
2023-03-21 17:08:49 +0100

    (1773) fix typescript errors

--- packages/api/server/services/answer/createAnswer.spec.ts
+++ packages/api/server/services/answer/createAnswer.spec.ts
@@ -6,6 +6,7 @@ import answerModel from '#server/models/answerModel';
 
 import { serialized as fakeUser } from '#test/utils/user';
 
+import Question from '#server/models/questionModel/Question.d';
 import createAnswerService from './createAnswer';
 
 const { expect } = chai;
@@ -14,15 +15,25 @@ chai.use(sinonChai);
 describe('services/answer', () => {
     describe('createAnswer()', () => {
         const user = fakeUser();
-        const question = {
+        const question: Question = {
             id: 2,
             question: 'question test',
+            details: 'Détails de la question',
+            peopleAffected: null,
             tags: [],
             answers: [],
             createdBy: {
                 id: 1,
-
+                first_name: 'Jean',
+                last_name: 'Dupont',
+                position: 'Mock',
+                role: 'Acteur national',
+                organization_id: 2,
+                organization: 'Délégation Interministérielle à l\'Hébergement et à l\'Accès au Logement',
             },
+            createdAt: new Date().getTime() / 1000,
+            updatedAt: null,
+            solvedAt: null,
         };
         const newAnswer = { description: 'ceci est une réponse de test', id: 2 };
         let stubs;
