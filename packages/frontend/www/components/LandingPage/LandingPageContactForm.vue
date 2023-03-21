a0a8a744edbc51282623617aa595f3c7b57c1b49
Christophe BENARD <ch.benard@gmail.com>
2023-03-21 15:07:29 +0100

    (1820) Inverser les couleurs des boutons "Demander un accès" et "Nous contacter"

--- packages/frontend/www/components/LandingPage/LandingPageContactForm.vue
+++ packages/frontend/www/components/LandingPage/LandingPageContactForm.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-row justify-center mt-4 w-full max-w-md">
-        <ButtonContact variant="secondary" class="ml-1" isDemandeAcces>{{
+        <ButtonContact variant="primary" class="ml-1" isDemandeAcces>{{
             $t("landingPage.contactForm.ctaSignup")
         }}</ButtonContact>
-        <ButtonContact variant="primary" class="ml-1">{{
+        <ButtonContact variant="secondary" class="ml-1">{{
             $t("landingPage.contactForm.ctaContact")
         }}</ButtonContact>
     </div>
