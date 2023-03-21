712d034040d51fd576322929cc2417cfb752de43
Anis Safine Laget <anis@users.noreply.github.com>
2023-03-21 10:45:03 +0100

    (1767) automatic focus of textarea

--- packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionNouvelleReponse/inputs/FicheQuestionNouvelleReponseInputReponse.vue
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionNouvelleReponse/inputs/FicheQuestionNouvelleReponseInputReponse.vue
@@ -4,10 +4,21 @@
         id="answer"
         :label="labels.answer"
         placeholder="Saisissez ici votre réponse"
+        ref="textarea"
     />
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import router from "@/helpers/router";
+
 import labels from "../FicheQuestionNouvelleReponse.labels";
 import { TextArea } from "@resorptionbidonvilles/ui";
+
+const textarea = ref(null);
+onMounted(() => {
+    if (router.currentRoute.value.hash === "#reponses") {
+        textarea.value.focus();
+    }
+});
 </script>
