a36c71d2aa3251a016f9ddf87ececef41fb97f40
Anis Safine Laget <anis@users.noreply.github.com>
2023-03-23 11:40:56 +0100

    (1819) add missing translation on landing

--- packages/frontend/www/components/LandingPage/LandingPageHero.vue
+++ packages/frontend/www/components/LandingPage/LandingPageHero.vue
@@ -20,7 +20,8 @@
             <span class="text-white bg-black px-2 py-1 ml-1">8</span>
             {{ $t("landingPage.hero.review") }} 2019
         </div>
-        <p class="text-lg font-bold">Au total plus de <span class="underline">3 000</span> personnes relogées</p>
+        <p class="text-lg font-bold">{{ $t("landingPage.hero.moreThan") }} <span class="underline">3 000</span> {{
+            $t("landingPage.hero.people") }}</p>
     </div>
 </template>
 
