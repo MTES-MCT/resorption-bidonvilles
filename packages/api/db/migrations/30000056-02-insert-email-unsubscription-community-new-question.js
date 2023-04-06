90d66f9b1f24ee2e67d0b3e46a9a105c37178c9c
Anis Safine Laget <anis@users.noreply.github.com>
2023-04-06 13:21:40 +0200

    (1837) remove useless transactions in migration files

--- packages/api/db/migrations/30000056-02-insert-email-unsubscription-community-new-question.js
+++ packages/api/db/migrations/30000056-02-insert-email-unsubscription-community-new-question.js
@@ -1,45 +1,16 @@
 module.exports = {
-    async up(queryInterface) {
-        const transaction = await queryInterface.sequelize.transaction();
+    up: queryInterface => queryInterface.sequelize.query(
+        `INSERT INTO user_email_unsubscriptions (
+            fk_user,
+            email_subscription
+        )
+        SELECT
+            user_id,
+            'community_new_question'
+        FROM users`,
+    ),
 
-        try {
-            await queryInterface.sequelize.query(
-                `INSERT INTO user_email_unsubscriptions (
-                    fk_user,
-                    email_subscription
-                )
-                SELECT
-                    user_id,
-                    'community_new_question'
-                FROM 
-                    users
-                WHERE
-                    fk_status = 'active'`,
-                { transaction },
-            );
-            return transaction.commit();
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        }
-    },
-
-    async down(queryInterface) {
-        const transaction = await queryInterface.sequelize.transaction();
-
-        try {
-            await queryInterface.sequelize.query(
-                "DELETE FROM user_email_unsubscriptions WHERE email_subscription = 'community_new_question'",
-                {
-                    transaction,
-                },
-            );
-
-            return transaction.commit();
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        }
-    },
-
+    down: queryInterface => queryInterface.sequelize.query(
+        "DELETE FROM user_email_unsubscriptions WHERE email_subscription = 'community_new_question'",
+    ),
 };
