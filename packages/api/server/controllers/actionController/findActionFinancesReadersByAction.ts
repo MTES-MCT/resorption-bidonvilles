286b2b8a30bfa4b0b235cdbb5a2d0fc0a7152a38
Christophe BENARD <ch.benard@gmail.com>
2023-04-14 16:14:19 +0200

    (1834) Créer deux contrôleurs pour récupérer les `action_finances_readers`

---
+++ packages/api/server/controllers/actionController/findActionFinancesReadersByAction.ts
@@ -0,0 +1,29 @@
+import actionService from '#server/services/action/actionService';
+import { NextFunction, Response } from 'express';
+
+interface FindActionFinancesReadersRequest extends Request {
+    params: {
+        id: string,
+    }
+}
+
+const ERROR_RESPONSES = {
+    fetch_failed: { code: 400, message: 'Une lecture en base de données a échoué' },
+    undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
+};
+
+export default async (req: FindActionFinancesReadersRequest, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        res.status(200).send(
+            await actionService.findActionFinancesReadersByAction(
+                parseInt(req.params.id, 10),
+            ),
+        );
+    } catch (error) {
+        const { code, message } = ERROR_RESPONSES[error && error.code] || ERROR_RESPONSES.undefined;
+        res.status(code).send({
+            user_message: message,
+        });
+        next(error.nativeError || error);
+    }
+};
