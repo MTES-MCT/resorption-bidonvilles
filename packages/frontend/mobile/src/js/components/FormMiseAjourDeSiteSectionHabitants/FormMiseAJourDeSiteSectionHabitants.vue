968d47b93673339a8f121a71f027e11c9c327421
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-04-24 10:28:06 +0200

    (1814) composants du formulaire

--- packages/frontend/mobile/src/js/components/FormMiseAjourDeSiteSectionHabitants/FormMiseAJourDeSiteSectionHabitants.vue
+++ packages/frontend/mobile/src/js/components/FormMiseAjourDeSiteSectionHabitants/FormMiseAJourDeSiteSectionHabitants.vue
@@ -1,2 +1,16 @@
-<template>test habitants</template>
-<script setup></script>
+<template>
+    <h1 class="text-2xl sm:text-3xl font-bold px-5">Habitants</h1>
+    <p class="px-5">
+        Situation du site au
+        {{ formatDate(today.getTime() / 1000, "d M y") }}
+    </p>
+    <inputPopulation class="px-5" />
+    <inputSocialOrigins class="px-5" />
+</template>
+<script setup>
+import formatDate from "#frontend/common/utils/formatDate";
+import inputPopulation from "./inputs/InputPopulation.vue";
+import inputSocialOrigins from "./inputs/InputSocialOrigins.vue";
+
+const today = new Date();
+</script>
