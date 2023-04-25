e7e50eb684db9ad3740479024129b8cd0df7ac51
Anis Safine Laget <anis@users.noreply.github.com>
2023-04-25 13:06:49 +0200

    fix(1861): update user_question_subscriptions.updated_at

--- packages/api/server/models/userQuestionSubscriptionModel/setSubscriptionState.ts
+++ packages/api/server/models/userQuestionSubscriptionModel/setSubscriptionState.ts
@@ -2,7 +2,7 @@ import { sequelize } from '#db/sequelize';
 
 export default async (userId: number, questionId: number, active: boolean): Promise<void> => {
     await sequelize.query(
-        'UPDATE user_question_subscriptions SET active = :active WHERE fk_user = :userId AND fk_question = :questionId',
+        'UPDATE user_question_subscriptions SET active = :active, updated_at = NOW() WHERE fk_user = :userId AND fk_question = :questionId',
         {
             replacements: {
                 userId,
