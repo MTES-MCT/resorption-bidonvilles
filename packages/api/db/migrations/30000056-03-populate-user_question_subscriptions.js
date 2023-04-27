de0b102293af5eef2e075b1fd6fb806c6c30084b
Anis Safine Laget <anis@users.noreply.github.com>
2023-04-27 15:41:57 +0200

    refactor(1863): add try/catch on transactions

--- packages/api/db/migrations/30000056-03-populate-user_question_subscriptions.js
+++ packages/api/db/migrations/30000056-03-populate-user_question_subscriptions.js
@@ -2,36 +2,43 @@ module.exports = {
     async up(queryInterface) {
         const transaction = await queryInterface.sequelize.transaction();
 
-        const mapping = await queryInterface.sequelize.query(
-            `SELECT * FROM (SELECT
-                question_id,
-                created_by AS user_id
-            FROM questions
-            
-            UNION
-            
-            SELECT
-                fk_question AS question_id,
-                created_by AS user_id
-            FROM answers) t
-            GROUP BY question_id, user_id`,
-            {
-                type: queryInterface.sequelize.QueryTypes.SELECT,
-                transaction,
-            },
-        );
+        try {
+            const mapping = await queryInterface.sequelize.query(
+                `SELECT * FROM (SELECT
+                    question_id,
+                    created_by AS user_id
+                FROM questions
+                
+                UNION
+                
+                SELECT
+                    fk_question AS question_id,
+                    created_by AS user_id
+                FROM answers) t
+                GROUP BY question_id, user_id`,
+                {
+                    type: queryInterface.sequelize.QueryTypes.SELECT,
+                    transaction,
+                },
+            );
 
-        await queryInterface.bulkInsert(
-            'user_question_subscriptions',
-            mapping.map(({ question_id, user_id }) => ({
-                fk_user: user_id,
-                fk_question: question_id,
-                active: true,
-            })),
-            { transaction },
-        );
+            if (mapping.length > 0) {
+                await queryInterface.bulkInsert(
+                    'user_question_subscriptions',
+                    mapping.map(({ question_id, user_id }) => ({
+                        fk_user: user_id,
+                        fk_question: question_id,
+                        active: true,
+                    })),
+                    { transaction },
+                );
+            }
 
-        await transaction.commit();
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
     },
 
     down: queryInterface => queryInterface.bulkDelete('user_question_subscriptions', {}),
