803d3885117726fd46cf4f587dee438eec76be64
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-05-02 17:23:08 +0200

    (1866) API questionModel.getHistory

--- packages/api/server/models/questionModel/index.ts
+++ packages/api/server/models/questionModel/index.ts
@@ -3,6 +3,7 @@ import findOne from './findOne';
 import findAll from './findAll';
 import serializeQuestion from './serializeQuestion';
 import getAnswers from './_common/getAnswers';
+import getHistory from './getHistory';
 
 export default {
     create,
@@ -10,4 +11,5 @@ export default {
     findAll,
     serializeQuestion,
     getAnswers,
+    getHistory,
 };
