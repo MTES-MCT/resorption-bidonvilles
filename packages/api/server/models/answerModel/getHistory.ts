d5e91b0bf60a38ec60418faf252dcb1b080e50e6
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-05-02 17:23:31 +0200

    (1866) API answerModel.getHistory

---
+++ packages/api/server/models/answerModel/getHistory.ts
@@ -0,0 +1,79 @@
+
+import { sequelize } from '#db/sequelize';
+import { QueryTypes } from 'sequelize';
+
+import userModel from '../userModel';
+
+export default async (numberOfActivities, lastDate, maxDate): Promise<any[]> => {
+    const limit = numberOfActivities !== -1 ? `limit ${numberOfActivities}` : '';
+
+    const where = [];
+
+    const replacements: any = {
+        maxDate,
+    };
+
+    where.push(`answers.created_at < '${lastDate}'`);
+    if (maxDate) {
+        where.push('answers.created_at >= :maxDate');
+    }
+
+    const activities = await sequelize.query(
+        `
+        SELECT 
+            answers.description,
+            answers.fk_question AS question_id,
+            answers.created_at AS "date",
+            questions.question,
+            answer_author.first_name AS answer_author_first_name,
+            answer_author.last_name AS answer_author_last_name,
+            answer_author.fk_organization as answer_author_organization,
+            question_author.first_name AS question_author_first_name,
+            question_author.last_name AS question_author_last_name,
+            question_author.fk_organization as question_author_organization
+        FROM answers
+        LEFT JOIN questions on questions.question_id = answers.fk_question
+        LEFT JOIN users answer_author ON answer_author.user_id = answers.created_by
+        LEFT JOIN users question_author ON question_author.user_id = questions.created_by
+        ${where.length > 0 ? `WHERE (${where.join(') AND (')})` : ''}
+        ORDER BY answers.created_at
+        ${limit}
+        `,
+        {
+            type: QueryTypes.SELECT,
+            replacements,
+        },
+    );
+
+
+    return activities
+        .map((activity: any) => {
+            const o = {
+                entity: 'answer',
+                action: 'creation',
+                date: activity.date.getTime() / 1000,
+                question: {
+                    id: activity.question_id,
+                    question: activity.question,
+                },
+                question_author: {
+                    name: userModel.formatName({
+                        first_name: activity.question_author_first_name,
+                        last_name: activity.question_author_last_name,
+                    }),
+                    organization: activity.question_author_organization,
+                },
+                answer_author: {
+                    name: userModel.formatName({
+                        first_name: activity.answer_author_first_name,
+                        last_name: activity.answer_author_last_name,
+                    }),
+                    organization: activity.answer_author_organization,
+                },
+                answer: {
+                    description: activity.description,
+                },
+            };
+            return o;
+        });
+};
