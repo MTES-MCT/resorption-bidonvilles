2a390365b734b67d191b8dc5eafe96e283d898c8
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-08 18:08:33 +0200

    feat: création d'un modèle `dataReportModel.getRawData()`
    Cette fonction renvoie la liste de tous les états des sites entre deux dates, triés par site puis par date antéchronologique.
    
    refers to https://trello.com/c/yyyCvq6U/1883

---
+++ packages/api/server/models/dataReportModel/getRawData.ts
@@ -0,0 +1,112 @@
+import { sequelize } from '#db/sequelize';
+import { QueryTypes } from 'sequelize';
+
+type DataReportRawData = {
+    shantytown_id: number,
+    date: Date,
+    known_since: Date,
+    closed_at: Date,
+    population_total: number,
+    population_minors: number,
+    minors_in_school: number,
+    origins: 'european' | 'french' | 'other' | 'mixed' | null
+};
+
+export default async (from: Date, to: Date): Promise<DataReportRawData[]> => sequelize.query(
+    `SELECT
+            t.*
+        FROM (
+            (WITH shantytowns_today AS (
+                SELECT
+                    shantytown_id,
+                    COALESCE(built_at, declared_at, created_at) AS known_since,
+                    closed_at
+                FROM shantytowns
+            ),
+            shantytown_agg_origins AS (
+                SELECT
+                    fk_shantytown AS shantytown_id,
+                    array_agg(social_origins.uid) AS origin_uids
+                FROM shantytown_origins
+                LEFT JOIN social_origins ON shantytown_origins.fk_social_origin = social_origins.social_origin_id
+                GROUP BY fk_shantytown
+            )
+            SELECT
+                shantytowns.shantytown_id,
+                COALESCE(shantytowns.updated_at, shantytowns.created_at) AS date,
+                shantytowns_today.known_since,
+                shantytowns_today.closed_at,
+                shantytowns.population_total,
+                shantytowns.population_minors_0_3
+                    + shantytowns.population_minors_3_6
+                    + shantytowns.population_minors_6_12
+                    + shantytowns.population_minors_12_16
+                    + shantytowns.population_minors_16_18 AS population_minors,
+                shantytowns.minors_in_school,
+                CASE
+                    WHEN cardinality(shantytown_agg_origins.origin_uids) = 0 THEN NULL
+                    WHEN cardinality(shantytown_agg_origins.origin_uids) = 1 THEN
+                        CASE
+                            WHEN shantytown_agg_origins.origin_uids[1] = 'european' THEN 'european'
+                            WHEN shantytown_agg_origins.origin_uids[1] = 'french' THEN 'french'
+                            ELSE 'other'
+                        END
+                    ELSE 'mixed'
+                END AS origins
+            FROM shantytowns
+            LEFT JOIN shantytowns_today ON shantytowns_today.shantytown_id = shantytowns.shantytown_id
+            LEFT JOIN shantytown_agg_origins ON shantytown_agg_origins.shantytown_id = shantytowns.shantytown_id)
+
+            UNION
+
+            (WITH shantytowns_today AS (
+                SELECT
+                    shantytown_id,
+                    COALESCE(built_at, declared_at, created_at) AS known_since,
+                    closed_at
+                FROM shantytowns
+            ),
+            shantytown_history_agg_origins AS (
+                SELECT
+                    fk_shantytown AS hid,
+                    array_agg(social_origins.uid) AS origin_uids
+                FROM "ShantytownOriginHistories"
+                LEFT JOIN social_origins ON "ShantytownOriginHistories".fk_social_origin = social_origins.social_origin_id
+                GROUP BY fk_shantytown
+            )
+            SELECT
+                "ShantytownHistories".shantytown_id,
+                COALESCE("ShantytownHistories".updated_at, "ShantytownHistories".created_at) AS date,
+                shantytowns_today.known_since,
+                shantytowns_today.closed_at,
+                "ShantytownHistories".population_total,
+                "ShantytownHistories".population_minors_0_3
+                    + "ShantytownHistories".population_minors_3_6
+                    + "ShantytownHistories".population_minors_6_12
+                    + "ShantytownHistories".population_minors_12_16
+                    + "ShantytownHistories".population_minors_16_18 AS population_minors,
+                "ShantytownHistories".minors_in_school,
+                CASE
+                    WHEN cardinality(shantytown_history_agg_origins.origin_uids) = 0 THEN NULL
+                    WHEN cardinality(shantytown_history_agg_origins.origin_uids) = 1 THEN
+                        CASE
+                            WHEN shantytown_history_agg_origins.origin_uids[1] = 'european' THEN 'european'
+                            WHEN shantytown_history_agg_origins.origin_uids[1] = 'french' THEN 'french'
+                            ELSE 'other'
+                        END
+                    ELSE 'mixed'
+                END AS origins
+            FROM "ShantytownHistories"
+            LEFT JOIN shantytowns_today ON shantytowns_today.shantytown_id = "ShantytownHistories".shantytown_id
+            LEFT JOIN shantytown_history_agg_origins ON shantytown_history_agg_origins.hid = "ShantytownHistories".hid)
+        ) t
+        WHERE t.date >= :from AND t.date <= :to
+        ORDER BY t.shantytown_id ASC, t.date DESC`,
+    {
+        type: QueryTypes.SELECT,
+        replacements: {
+            from: from.toISOString().slice(0, 10),
+            to: to.toISOString().slice(0, 10),
+        },
+    },
+);
