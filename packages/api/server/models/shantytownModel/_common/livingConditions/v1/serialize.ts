f0e646706d999767248162db4dca78080469e246
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-05-11 14:45:53 +0200

    (1866) create types for living conditions

--- packages/api/server/models/shantytownModel/_common/livingConditions/v1/serialize.ts
+++ packages/api/server/models/shantytownModel/_common/livingConditions/v1/serialize.ts
@@ -1,7 +1,9 @@
 import getLivingConditionsStatuses from './statuses/main';
 
-export default (town) => {
-    const livingConditions = {
+import { LivingConditionsV1 } from '../LivingConditions.d';
+
+export default (town): LivingConditionsV1 => {
+    const livingConditions: LivingConditionsV1 = {
         version: town.livingConditionsVersion,
         electricity: {
             status: null,
