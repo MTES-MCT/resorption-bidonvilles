5c50266cb27d7c59124561faa43233ad4244f76c
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-15 14:58:40 +0200

    feat: improve UX for file inputs
    refers to https://trello.com/c/Jr3mH03F/1848

---
+++ packages/frontend/ui/src/components/FilePreview/FilePreviewGrid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-2 mt-1">
+        <FilePreview v-for="(file, index) in files" :key="file.id" :file="file" @delete="$emit('delete', file, index)" />
+    </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue';
+import FilePreview from "./FilePreview.vue";
+
+const props = defineProps({
+    files: {
+        type: Array,
+        default() {
+            return [];
+        },
+    },
+});
+const { files } = toRefs(props);
+</script>
