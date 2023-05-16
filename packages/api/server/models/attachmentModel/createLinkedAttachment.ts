74653296d8b66fcf2ba167feb69e1589914297d1
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-16 15:56:18 +0200

    feat: création d'un service attachmentService.upload
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/models/attachmentModel/createLinkedAttachment.ts
+++ packages/api/server/models/attachmentModel/createLinkedAttachment.ts
@@ -2,9 +2,9 @@ import { sequelize } from '#db/sequelize';
 import { QueryTypes, Transaction } from 'sequelize';
 import create from './create';
 
-type EntityType = 'shantytown_comment' | 'action_comment' | 'question' | 'answer';
+export type AttachmentEntityType = 'shantytown_comment' | 'action_comment' | 'question' | 'answer';
 
-export default async (entityType: EntityType, entityId: number, key: string, name: string, mimetype: string, size: number, authorId: number, argTransaction?: Transaction): Promise<void> => {
+export default async (entityType: AttachmentEntityType, entityId: number, key: string, name: string, mimetype: string, size: number, authorId: number, argTransaction?: Transaction): Promise<void> => {
     const transaction = argTransaction || await sequelize.transaction();
 
     try {
