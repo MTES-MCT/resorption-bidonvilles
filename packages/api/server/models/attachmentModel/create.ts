f50a31502cd41f18386c2143c7e4b691dbf24cad
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-16 12:11:06 +0200

    feat: ajout d'un attachment model
    refers to https://trello.com/c/Jr3mH03F/1848

---
+++ packages/api/server/models/attachmentModel/create.ts
@@ -0,0 +1,26 @@
+import { sequelize } from '#db/sequelize';
+import { QueryTypes, Transaction } from 'sequelize';
+
+export default async (key: string, name: string, mimetype: string, size: number, authorId: number, transaction?: Transaction): Promise<number> => {
+    const data = await sequelize.query(
+        `INSERT INTO attachments(url_original, url_preview, original_name, mimetype, size, created_by)
+        VALUES (:key, :key, :name, :mimetype, :size, :authorId)
+        RETURNING attachment_id`,
+        {
+            transaction,
+            replacements: {
+                key,
+                name,
+                mimetype,
+                size,
+                authorId,
+            },
+            type: QueryTypes.INSERT,
+        },
+    );
+
+    type ReturnValue = { attachment_id: number };
+    const rows: ReturnValue[] = (data[0] as unknown) as ReturnValue[];
+
+    return rows[0].attachment_id;
+};
