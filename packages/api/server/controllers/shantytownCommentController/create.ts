96e1657afbb148d91fa77915fcaeb98a8d3db40a
Christophe BENARD <ch.benard@gmail.com>
2023-05-16 18:01:22 +0200

    (1848) Supporter les pièces jointes dans les commentaires sites

--- packages/api/server/controllers/shantytownCommentController/create.ts
+++ packages/api/server/controllers/shantytownCommentController/create.ts
@@ -1,37 +1,32 @@
 import shantytownCommentService from '#server/services/shantytownComment';
 
+const ERROR_RESPONSES = {
+    insert_failed: { code: 500, message: 'Votre commentaire n\'a pas pu être enregistré.' },
+    upload_failed: { code: 500, message: 'L\'enregistrement des pièces jointes a échoué.' },
+    commit_failed: { code: 500, message: 'L\'enregistrement du commentaire et/ou des pièces jointes a échoué.' },
+    undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
+};
 export default async (req, res, next) => {
     let comments;
-
     try {
         comments = await shantytownCommentService.createComment(
             {
                 description: req.body.comment,
                 targets: req.body.targets,
                 tags: req.tags,
+                files: req.files,
             },
             req.body.shantytown,
             req.user,
         );
     } catch (error) {
-        let message;
-        switch (error && error.code) {
-            case 'insert_failed':
-                message = 'Votre commentaire n\'a pas pu être enregistré.';
-                break;
-
-            case 'fetch_failed':
-                message = 'Votre commentaire a bien été enregistré mais la liste des commentaires n\'a pas pu être actualisée.';
-                break;
-
-            default:
-                message = 'Une erreur inconnue est survenue.';
-        }
-
-        res.status(500).send({
-            user_message: message,
+        const { code, message } = ERROR_RESPONSES[error && error.code] || ERROR_RESPONSES.undefined;
+        res.status(code).send({
+            error: {
+                user_message: message,
+            },
         });
-        return next((error && error.nativeError) || error);
+        return next(error.nativeError || error);
     }
 
     return res.status(200).send({
