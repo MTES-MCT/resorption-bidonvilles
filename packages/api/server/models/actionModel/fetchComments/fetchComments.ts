3bcbfb6f5d4e02b3f15ed44dbceb92d4ff42c321
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-05-17 17:05:36 +0200

    (1848) fetch action_comment_attachments

--- packages/api/server/models/actionModel/fetchComments/fetchComments.ts
+++ packages/api/server/models/actionModel/fetchComments/fetchComments.ts
@@ -14,6 +14,7 @@ export type ActionCommentRow = {
     creator_organization_name: string,
     creator_organization_abbreviation: string | null,
     creator_user_role: string,
+    attachments: string[]
 };
 
 export default function fetchComments(actionIds: number[] = null, commentIds: number[] = null, clauseGroup: object = {}, transaction?: Transaction): Promise<ActionCommentRow[]> {
@@ -32,7 +33,24 @@ export default function fetchComments(actionIds: number[] = null, commentIds: nu
     enrichWhere(where, replacements, clauseGroup);
 
     return sequelize.query(
-        `SELECT
+        `
+        WITH grouped_attachments AS (
+            SELECT
+                aca.fk_action_comment,
+                array_remove(array_agg(
+                    a.attachment_id || '@.;.@'
+                    || a.original_file_key || '@.;.@'
+                    || a.preview_file_key || '@.;.@'
+                    || a.original_name || '@.;.@'
+                    || a.mimetype || '@.;.@'
+                    || a.size || '@.;.@'
+                    || a.created_by
+                ), null) AS attachments
+            FROM action_comment_attachments aca
+            LEFT JOIN attachments a ON aca.fk_attachment = a.attachment_id
+            GROUP BY aca.fk_action_comment
+        )
+        SELECT
             action_comments.fk_action AS action_id,
             action_comments.action_comment_id AS id,
             action_comments.description,
@@ -43,7 +61,8 @@ export default function fetchComments(actionIds: number[] = null, commentIds: nu
             roles_regular.name AS creator_user_role,
             creator_organizations.organization_id AS creator_organization_id,
             creator_organizations.name AS creator_organization_name,
-            creator_organizations.abbreviation AS creator_organization_abbreviation
+            creator_organizations.abbreviation AS creator_organization_abbreviation,
+            grouped_attachments.attachments AS "attachments"
         FROM action_comments
         LEFT JOIN users AS creator ON action_comments.created_by = creator.user_id
         LEFT JOIN roles_regular ON creator.fk_role_regular = roles_regular.role_id
@@ -51,6 +70,7 @@ export default function fetchComments(actionIds: number[] = null, commentIds: nu
         LEFT JOIN actions ON action_comments.fk_action = actions.action_id
         LEFT JOIN departements ON actions.fk_departement = departements.code
         LEFT JOIN regions ON departements.fk_region = regions.code
+        LEFT JOIN grouped_attachments ON grouped_attachments.fk_action_comment = action_comments.action_comment_id
         ${where.length > 0 ? `WHERE ${where.join(' AND ')}` : ''}
         ORDER BY action_comments.created_at DESC
         `,
