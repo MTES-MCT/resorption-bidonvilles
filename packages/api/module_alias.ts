1e0551ebc42b438634d9f553696897dd3c95049d
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 16:50:18 +0200

    feat: validation du type mime des fichiers côté API
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/module_alias.ts
+++ packages/api/module_alias.ts
@@ -5,6 +5,7 @@ moduleAlias.addAliases({
     '#db': `${__dirname}/db`,
     '#fixtures': `${__dirname}/test/fixtures`,
     '#test': `${__dirname}/test`,
+    '#common': `${__dirname}/../frontend/common`,
     '#root': __dirname,
     '#types': `${__dirname}/types`,
 });
--- packages/api/server/middlewares/validators/file.ts
+++ packages/api/server/middlewares/validators/file.ts
@@ -1,11 +1,29 @@
 /* eslint-disable no-param-reassign */
 import { NextFunction, Request, Response } from 'express';
+import { body } from 'express-validator';
+import fileType from 'file-type';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import extensions from '#common/utils/allowed_file_extensions';
 
-export default (req: Request, res: Response, next: NextFunction) => {
-    if (Array.isArray(req.files)) {
-        req.files.forEach((f) => {
+export default async (req: Request, res: Response, next: NextFunction) => {
+    const { files } = req;
+    if (Array.isArray(files)) {
+        files.forEach((f) => {
             f.originalname = Buffer.from(f.originalname, 'latin1').toString('utf8');
         });
+
+        // si un des fichiers a un type interdit : générer une erreur
+        const fn = body('attachments')
+            .custom(async () => {
+                const types = await Promise.all(
+                    files.map(f => fileType.fromBuffer(f.buffer)),
+                );
+
+                if (types.some(type => !extensions.includes(type.ext))) {
+                    throw new Error('Un des fichiers est d\'un type non autorisé');
+                }
+            });
+        await fn(req, res, next);
     } else if (req.files === undefined || req.files === null) {
         req.files = [];
     }
