f5749793d869d0d72ffc02a6c9d616f29f3e65b1
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 14:34:18 +0200

    feat: autoriser les PJs sur la saisie d'une réponse (entraide)
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionNouvelleReponse/FicheQuestionNouvelleReponse.schema.js
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionReponses/FicheQuestionNouvelleReponse/FicheQuestionNouvelleReponse.schema.js
@@ -1,6 +1,8 @@
 import { object, string } from "yup";
 import labels from "./FicheQuestionNouvelleReponse.labels";
+import yup_attachment_schema from "@/utils/yup_attachment_schema";
 
 export default object({
     answer: string().required().label(labels.answer),
+    attachments: yup_attachment_schema().label(labels.attachments),
 });
