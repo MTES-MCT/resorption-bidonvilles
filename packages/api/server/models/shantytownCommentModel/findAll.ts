56c457c26f1dba01bfe5236e2d49fbb0c58d81bb
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 16:23:23 +0200

    refactor: correction de noms de colonnes suite à un rebase
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/models/shantytownCommentModel/findAll.ts
+++ packages/api/server/models/shantytownCommentModel/findAll.ts
@@ -80,8 +80,8 @@ export default async (user, location = null, privateLocation = null) => {
                 sca.fk_shantytown_comment,
                 array_remove(array_agg(
                     a.attachment_id || '@.;.@'
-                    || a.url_original || '@.;.@'
-                    || a.url_preview || '@.;.@'
+                    || a.original_file_key || '@.;.@'
+                    || a.preview_file_key || '@.;.@'
                     || a.original_name || '@.;.@'
                     || a.mimetype || '@.;.@'
                     || a.size || '@.;.@'
