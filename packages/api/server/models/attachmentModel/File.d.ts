354342a9bb4ad359a1843566d2569224c9bcc31a
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-05-17 17:05:36 +0200

    (1848) fetch action_comment_attachments

---
+++ packages/api/server/models/attachmentModel/File.d.ts
@@ -0,0 +1,12 @@
+export type File = {
+    state: string,
+    id: number,
+    name: string,
+    size: number,
+    urls: {
+        original: string,
+        preview: string,
+    },
+    extension: string,
+    created_by: string,
+};
