e7a0d1d3d55b0d5d86df3de9eb4d3c783df7f3c4
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 15:34:42 +0200

    feat: gestion du drag&drop sur une nouvelle question
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputAttachments.vue
+++ packages/frontend/webapp/src/components/FormNouvelleQuestion/inputs/FormNouvelleQuestionInputAttachments.vue
@@ -1,7 +1,14 @@
 <template>
-    <InputFiles name="attachments" :multiple="true" />
+    <InputFiles name="attachments" :multiple="true" ref="input" />
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { InputFiles } from "@resorptionbidonvilles/ui";
+
+const input = ref(null);
+defineExpose({
+    addFiles: (...args) =>
+        input.value?.addFiles ? input.value?.addFiles(...args) : null,
+});
 </script>
