7fe70ccb66c393d53129553150474fbda553846b
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 17:01:33 +0200

    feat: vérifier le poids maximal des fichiers uploadés côté API
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/middlewares/validators/file.ts
+++ packages/api/server/middlewares/validators/file.ts
@@ -1,9 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-param-reassign */
 import { NextFunction, Request, Response } from 'express';
 import { body } from 'express-validator';
 import fileType from 'file-type';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import extensions from '#common/utils/allowed_file_extensions';
+import { MAX_FILE_SIZE } from '#common/utils/max_file_size';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
     const { files } = req;
@@ -12,15 +13,31 @@ export default async (req: Request, res: Response, next: NextFunction) => {
             f.originalname = Buffer.from(f.originalname, 'latin1').toString('utf8');
         });
 
-        // si un des fichiers a un type interdit : générer une erreur
         const fn = body('attachments')
             .custom(async () => {
+                // si un des fichiers a un type interdit : générer une erreur
                 const types = await Promise.all(
                     files.map(f => fileType.fromBuffer(f.buffer)),
                 );
 
-                if (types.some(type => !extensions.includes(type.ext))) {
-                    throw new Error('Un des fichiers est d\'un type non autorisé');
+                const wrongTypeFiles = types
+                    .map((type, index) => ({
+                        ...type,
+                        file: files[index].originalname,
+                    }))
+                    .filter(type => !extensions.includes(type.ext));
+                if (wrongTypeFiles.length > 0) {
+                    throw new Error(
+                        `Un ou plusieurs fichiers sont d'un type non autorisé : ${wrongTypeFiles.map(f => f.file).join(', ')}`,
+                    );
+                }
+
+                // si un des fichiers est trop gros : générer une erreur
+                const tooBigFiles = files.filter(f => f.size > MAX_FILE_SIZE);
+                if (tooBigFiles.length > 0) {
+                    throw new Error(
+                        `Un ou plusieurs fichiers dépassent le poids maximal autorisé : ${tooBigFiles.map(f => f.originalname).join(', ')}`,
+                    );
                 }
             });
         await fn(req, res, next);
