de9d5bf4eb6572888ce3291ceceb48175052318c
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 14:21:08 +0200

    feat: autoriser saisie de PJ sur le msg d'une action
    refers to https://trello.com/c/Jr3mH03F/1848

---
+++ packages/frontend/webapp/src/components/FicheAction/FicheActionJournal/FicheActionJournalFormNouveauMessage/inputs/FormNouveauMessageInputAttachments.vue
@@ -0,0 +1,12 @@
+<template>
+    <InputFiles
+        name="attachments"
+        :label="labels.attachments"
+        :multiple="true"
+    />
+</template>
+
+<script setup>
+import { InputFiles } from "@resorptionbidonvilles/ui";
+import labels from "../FicheActionJournalFormNouveauMessage.labels";
+</script>
