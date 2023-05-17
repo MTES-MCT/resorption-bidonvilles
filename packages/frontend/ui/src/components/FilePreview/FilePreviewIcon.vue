e4e35c4eea93904fd088fa9881752bb58bfb7b46
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-17 11:05:36 +0200

    feat: ajout de couleurs aux icônes de documents
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/frontend/ui/src/components/FilePreview/FilePreviewIcon.vue
+++ packages/frontend/ui/src/components/FilePreview/FilePreviewIcon.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="w-12 h-12 bg-G300 rounded-lg border overflow-hidden relative flex items-center justify-center">
+    <div class="w-12 h-12 rounded-lg overflow-hidden relative flex items-center justify-center"
+        :class="color ? `bg-${color.bg}` : 'bg-G300'">
         <img v-if="isImage" class="object-cover w-12 h-12 max-w-none absolute" :src="file.urls.preview" />
-        <span class="text-xl text-G600">
+        <span class="text-xl" :class="color ? `text-${color.text}` : 'text-G600'">
             <Icon :icon="icon" />
         </span>
     </div>
@@ -38,5 +39,15 @@ const icon = computed(() => {
         null: 'file',
     };
     return icons[file.value.extension?.toLowerCase()] || 'file';
+});
+const color = computed(() => {
+    const icons = {
+        'file-image': null,
+        'file-word': { text: 'primary', bg: 'blue200' },
+        'file-excel': { text: 'green', bg: 'green100' },
+        'file-pdf': { text: 'red', bg: 'red100' },
+        file: null,
+    };
+    return icons[icon.value] || null;
 });
 </script>
