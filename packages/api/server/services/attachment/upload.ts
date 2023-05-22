cb39b23e8f269a5535b0f7f8236717a925605af6
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-22 12:56:11 +0200

    feat: génération de miniatures pour chaque image uploadée
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/services/attachment/upload.ts
+++ packages/api/server/services/attachment/upload.ts
@@ -1,4 +1,5 @@
 import { S3 } from '#server/utils/s3';
+import sharp from 'sharp';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import config from '#server/config';
 import attachmentModel from '#server/models/attachmentModel';
@@ -6,18 +7,57 @@ import { AttachmentEntityType } from '#server/models/attachmentModel/createLinke
 import fromMimeToExtension from '#server/utils/fromMimeToExtension';
 import { Transaction } from 'sequelize';
 
-export default (entityType: AttachmentEntityType, entityId: number, createdBy: number, files: Express.Multer.File[], transaction?: Transaction): Promise<any[]> => Promise.all(
-    files.map((f, index) => {
-        const Key = `${entityType}_author${createdBy}_comment${entityId}_file${index + 1}.${fromMimeToExtension[f.mimetype]}`;
+export default async (entityType: AttachmentEntityType, entityId: number, createdBy: number, files: Express.Multer.File[], transaction?: Transaction): Promise<any[]> => {
+    const previews: Buffer[] = await Promise.all(
+        files.map((f) => {
+            if (!f.mimetype.startsWith('image/')) {
+                return null;
+            }
 
-        return [
-            attachmentModel.createLinkedAttachment(entityType, entityId, Key, f.originalname, f.mimetype, f.size, createdBy, transaction),
-            S3.send(new PutObjectCommand({
-                Bucket: config.S3.bucket,
-                ACL: 'public-read',
-                Key,
-                Body: f.buffer,
-            })),
-        ];
-    }).flat(),
-);
+            return sharp(f.buffer).resize({ width: 50 }).toBuffer();
+        }),
+    );
+
+    return Promise.all(
+        files.map((f, index) => {
+            const Key = `${entityType}_author${createdBy}_comment${entityId}_file${index + 1}.${fromMimeToExtension[f.mimetype]}`;
+            let PreviewKey = null;
+            const promises: Promise<any>[] = [
+                S3.send(new PutObjectCommand({
+                    Bucket: config.S3.bucket,
+                    ACL: 'public-read',
+                    Key,
+                    Body: f.buffer,
+                })),
+            ];
+
+            if (previews[index] !== null) {
+                PreviewKey = `${entityType}_author${createdBy}_comment${entityId}_file${index + 1}_min.${fromMimeToExtension[f.mimetype]}`;
+                promises.push(
+                    S3.send(new PutObjectCommand({
+                        Bucket: config.S3.bucket,
+                        ACL: 'public-read',
+                        Key: PreviewKey,
+                        Body: previews[index],
+                    })),
+                );
+            }
+
+            promises.push(
+                attachmentModel.createLinkedAttachment(
+                    entityType,
+                    entityId,
+                    Key,
+                    PreviewKey,
+                    f.originalname,
+                    f.mimetype,
+                    f.size,
+                    createdBy,
+                    transaction,
+                ),
+            );
+
+            return promises;
+        }).flat(),
+    );
+};
