b45295b3e8a9270454a06eb193a8c4920d70cbe8
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-22 11:07:43 +0200

    refactor: mutualiser la sérialisation des attachments
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/models/shantytownModel/_common/serializeComment.ts
+++ packages/api/server/models/shantytownModel/_common/serializeComment.ts
@@ -1,6 +1,7 @@
 import ShantytownComment from '#server/models/shantytownCommentModel/ShantytownComment.d';
 import { CommentTag } from '#server/models/shantytownCommentTagModel/serializeCommentTag';
 import config from '#server/config';
+import attachmentModel from '#server/models/attachmentModel';
 import { ShantytownCommentRow } from '../../shantytownCommentModel/ShantytownCommentRow.d';
 
 type shantytownCommentRowWithTags = ShantytownCommentRow & {
@@ -41,22 +42,6 @@ export default (comment: shantytownCommentRowWithTags):ShantytownComment => ({
         }
         : {}),
     attachments: comment.attachments?.length
-        ? comment.attachments.map((attachment) => {
-            const [id, key, , original_name, mimetype, size, created_by] = attachment.split('@.;.@');
-            const url = `${config.S3.endpoint}/${config.S3.bucket}/${key}`;
-
-            return {
-                state: 'uploaded',
-                id: parseInt(id, 10),
-                name: original_name,
-                size: parseInt(size, 10),
-                urls: {
-                    original: url,
-                    preview: url,
-                },
-                extension: mimetype,
-                created_by,
-            };
-        })
+        ? comment.attachments.map(attachmentModel.serializeAttachment)
         : [],
 });
