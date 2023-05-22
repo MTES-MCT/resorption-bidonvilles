8a32273ac4b07f5d4473df1ab51765d2e2fafd9e
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-22 15:39:43 +0200

    fix: retourner les PJs après création d'un commentaire action
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/models/actionModel/createComment/createComment.ts
+++ packages/api/server/models/actionModel/createComment/createComment.ts
@@ -1,16 +1,12 @@
 import { QueryTypes } from 'sequelize';
 import { sequelize } from '#db/sequelize';
-import fetchComments from '../fetchComments/fetchComments';
-
-import { Comment } from '../fetch/Action';
-import serializeComment from '../fetchComments/serializeComment';
 
 export type ActionCommentInsertionRow = {
     description: string,
     created_by: number,
 };
 
-export default async (actionId: number, comment: ActionCommentInsertionRow): Promise<Comment> => {
+export default async (actionId: number, comment: ActionCommentInsertionRow): Promise<number> => {
     const data = await sequelize.query(
         `INSERT INTO action_comments(fk_action, description, created_by)
         VALUES (:actionId, :description, :created_by)
@@ -27,6 +23,5 @@ export default async (actionId: number, comment: ActionCommentInsertionRow): Pro
     type ReturnValue = { action_comment_id: number };
     const rows: ReturnValue[] = (data[0] as unknown) as ReturnValue[];
 
-    const comments = await fetchComments(null, [rows[0].action_comment_id]);
-    return serializeComment(comments[0]);
+    return rows[0].action_comment_id;
 };
