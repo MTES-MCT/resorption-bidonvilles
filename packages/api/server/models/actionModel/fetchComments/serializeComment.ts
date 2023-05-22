f355e5ef4522ffa136cf14c29eb3cb49d897cdf8
Anis Safine Laget <anis@users.noreply.github.com>
2023-05-22 11:07:43 +0200

    refactor: mutualiser la sérialisation des attachments
    refers to https://trello.com/c/Jr3mH03F/1848

--- packages/api/server/models/actionModel/fetchComments/serializeComment.ts
+++ packages/api/server/models/actionModel/fetchComments/serializeComment.ts
@@ -1,4 +1,4 @@
-import config from '#server/config';
+import attachmentModel from '#server/models/attachmentModel';
 import { ActionCommentRow } from './fetchComments';
 import { Comment } from '../fetch/Action';
 
@@ -17,22 +17,6 @@ export default (row: ActionCommentRow): Comment => ({
         organization: row.creator_organization_abbreviation || row.creator_organization_name,
     },
     attachments: row.attachments?.length
-        ? row.attachments.map((attachment) => {
-            const [id, key, , original_name, mimetype, size, created_by] = attachment.split('@.;.@');
-            const url = `${config.S3.endpoint}/${config.S3.bucket}/${key}`;
-
-            return {
-                state: 'uploaded',
-                id: parseInt(id, 10),
-                name: original_name,
-                size: parseInt(size, 10),
-                urls: {
-                    original: url,
-                    preview: url,
-                },
-                extension: mimetype,
-                created_by,
-            };
-        })
+        ? row.attachments.map(attachmentModel.serializeAttachment)
         : [],
 });
