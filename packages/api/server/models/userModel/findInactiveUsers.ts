bd10dae5941e823bfdede8c4fe79a2c2da858801
Christophe BENARD <ch.benard@gmail.com>
2023-06-01 17:22:02 +0200

    (1898) Corriger la requête retournant les users inactifs

--- packages/api/server/models/userModel/findInactiveUsers.ts
+++ packages/api/server/models/userModel/findInactiveUsers.ts
@@ -11,6 +11,12 @@ export default () => query(
                 operator: '>',
                 value: delayBeforeAlert,
             },
+            last_access_is_null: {
+                // la précédence des opérateurs en SQL fait que le AND s'applique avant le OR
+                query: 'users.last_access IS NULL AND (NOW() - users.created_at)',
+                operator: '>',
+                value: delayBeforeAlert,
+            },
         },
         {
             inactivity_alert_sent_at: { value: null },
