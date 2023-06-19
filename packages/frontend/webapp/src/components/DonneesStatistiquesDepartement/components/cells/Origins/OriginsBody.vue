ac0a95a809306aba4325524c28ada3ccb83b43b2
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-06-19 16:09:09 +0200

    (1904) changement flag pour extra-UE

--- packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/Origins/OriginsBody.vue
+++ packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/Origins/OriginsBody.vue
@@ -1,11 +1,13 @@
 <template>
     <template v-if="flags.length > 0">
-        <span
-            v-for="flag in flags"
-            :key="flag.icon"
-            :class="`mx-px fi ${flag.icon}`"
-            :title="flag.title"
-        />
+        <div class="flex flex-wrap">
+            <img
+                v-for="flag in flags"
+                :key="flag.icon"
+                :src="flag.icon"
+                class="mx-px pt-1 w-6"
+            />
+        </div>
     </template>
     <span v-else class="text-G500">n/c</span>
 </template>
@@ -13,12 +15,16 @@
 <script setup>
 import { computed, toRefs } from "vue";
 
+import flagFR from "@/assets/img/flags/fr.png";
+import flagEU from "@/assets/img/flags/eu.png";
+import flagExtraCommunautaires from "@/assets/img/flags/extra-communautaires.png";
+
 const props = defineProps({ town: Object });
 const { town } = toRefs(props);
 const flagMap = {
-    french: { icon: "fi-fr", title: "Français(e)s" },
-    european: { icon: "fi-eu", title: "Intra-communautaires" },
-    other: { icon: "fi-un", title: "Extra-communautaires" },
+    french: { icon: flagFR, title: "Français(e)s" },
+    european: { icon: flagEU, title: "Intra-communautaires" },
+    other: { icon: flagExtraCommunautaires, title: "Extra-communautaires" },
 };
 
 const flags = computed(() => {
