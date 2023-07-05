40eaf270a2a155590781109fe3da312b5bcaa6b8
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-07-05 12:26:11 +0200

    (1922) uniformisation ? au juridique

--- packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/BoolCell.vue
+++ packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/BoolCell.vue
@@ -1,5 +1,6 @@
 <template>
-    <Icon :icon="icon" :class="color" />
+    <Icon v-if="typeof value === 'boolean'" :icon="icon" :class="color" />
+    <span v-else class="text-G500">?</span>
 </template>
 
 <script setup>
@@ -10,18 +11,10 @@ const props = defineProps({ value: Boolean });
 const { value } = toRefs(props);
 
 const icon = computed(() => {
-    if (typeof value.value !== "boolean") {
-        return "question";
-    }
-
     return value.value ? "check" : "times";
 });
 
 const color = computed(() => {
-    if (typeof value.value !== "boolean") {
-        return "text-G500";
-    }
-
     return value.value ? "text-success" : "text-error";
 });
 </script>
