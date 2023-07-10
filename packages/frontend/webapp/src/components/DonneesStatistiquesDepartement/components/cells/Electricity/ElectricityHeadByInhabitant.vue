bb51e59c2c2a760022f297ff95d8859bb1b9c906
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-07-10 11:05:20 +0200

    (1924) apply format stat in each file

--- packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/Electricity/ElectricityHeadByInhabitant.vue
+++ packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/Electricity/ElectricityHeadByInhabitant.vue
@@ -1,11 +1,12 @@
 <template>
-    {{ data.summary.number_of_inhabitants_with_electricity }}<br />
+    {{ formatStat(data.summary.number_of_inhabitants_with_electricity) }}<br />
     <span class="text-sm"
         >({{ data.summary.percentage_of_inhabitants_with_electricity }} %)</span
     >
 </template>
 
 <script setup>
+import formatStat from "@/utils/formatStat";
 import { toRefs } from "vue";
 
 const props = defineProps({ data: Object });
--- packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/Fire/FireHeadByTown.vue
+++ packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/components/cells/Fire/FireHeadByTown.vue
@@ -1,11 +1,12 @@
 <template>
-    {{ data.summary.number_of_towns_with_fire_prevention }}<br />
+    {{ formatStat(data.summary.number_of_towns_with_fire_prevention) }}<br />
     <span class="text-sm"
         >({{ data.summary.percentage_of_towns_with_fire_prevention }} %)</span
     >
 </template>
 
 <script setup>
+import formatStat from "@/utils/formatStat";
 import { toRefs } from "vue";
 
 const props = defineProps({ data: Object });
