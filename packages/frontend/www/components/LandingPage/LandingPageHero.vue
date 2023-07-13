ee845b08d0488d6d52c85a6fb3e3fd4d284cb70c
Christophe BENARD <ch.benard@gmail.com>
2023-07-13 14:32:26 +0200

    feat: Retirer “Posez nous une question ou demandez un accès" sur la phrase d’accroche “Agir pour résorber les bidonvilles". refers to ...

--- packages/frontend/www/components/LandingPage/LandingPageHero.vue
+++ packages/frontend/www/components/LandingPage/LandingPageHero.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="text-center">
         <h1 class="text-display-xl font-bold text-black">{{ $t("landingPage.hero.title") }}</h1>
-        <h2 class="text-secondary text-display-lg font-bold mt-2">
-            {{ $t("landingPage.hero.subtitle") }}
-        </h2>
         <LandingPageContactForm class="mx-auto mb-8" />
         <div class="my-4 text-display-md font-bold">
             <span class="text-white bg-black px-2 py-1">2</span>
