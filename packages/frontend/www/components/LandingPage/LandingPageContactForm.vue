689d8212c7a0286c0e0351d5814905ec1c01f6b7
Christophe BENARD <ch.benard@gmail.com>
2023-07-13 17:21:31 +0200

    feat: Changer "Demander un accès" en "Nous contacter" pour toute autre langue que fr dans hero

--- packages/frontend/www/components/LandingPage/LandingPageContactForm.vue
+++ packages/frontend/www/components/LandingPage/LandingPageContactForm.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="flex flex-row justify-center mt-4 w-full max-w-md">
-        <ButtonContact variant="primary" class="ml-1" isDemandeAcces>{{
+        <ButtonContact v-if="i18n.locale.value === 'fr'" variant="primary" class="ml-1" isDemandeAcces>{{
             $t("landingPage.contactForm.ctaSignup")
+        }}</ButtonContact>
+        <ButtonContact v-else variant="primary" class="ml-1">{{
+            $t("landingPage.contactForm.ctaContact")
         }}</ButtonContact>
     </div>
 </template>
 
 <script>
 import ButtonContact from "~~/components/ButtonContact/ButtonContact.vue";
+import { useI18n } from 'vue-i18n';
 
 export default {
     components: {
         ButtonContact
+    },
+    setup() {
+        const i18n = useI18n();
+        return {
+            i18n
+        };
     },
 };
 </script>
