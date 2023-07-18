e6cd2386b86e096d3c3a3dbb9f0f95d1fd656361
Anis Safine Laget <anis@users.noreply.github.com>
2023-07-18 14:37:02 +0200

    feat: permettre le filtre par mot-clé des questions

--- packages/frontend/webapp/src/components/Entraide/Entraide.vue
+++ packages/frontend/webapp/src/components/Entraide/Entraide.vue
@@ -8,6 +8,7 @@
 
     <template v-else>
         <ListeDesQuestionsFiltres class="-mt-5 mb-4" />
+        <ListeDesQuestionsRecherche class="mb-4" />
         <ListeDesQuestions v-if="questionsStore.filteredQuestions.length > 0" />
         <ListeDesQuestionsVide v-else />
     </template>
@@ -20,6 +21,7 @@ import Loading from "@/components/Loading/Loading.vue";
 import EntraideBanniere from "./EntraideBanniere.vue";
 import EntraideHeader from "./EntraideHeader.vue";
 import ListeDesQuestionsFiltres from "./ListeDesQuestionsFiltres.vue";
+import ListeDesQuestionsRecherche from "./ListeDesQuestionsRecherche.vue";
 import ListeDesQuestions from "./ListeDesQuestions.vue";
 import ListeDesQuestionsVide from "./ListeDesQuestionsVide.vue";
 import EntraideErreur from "./EntraideErreur.vue";
--- packages/frontend/webapp/src/utils/filterQuestions.js
+++ packages/frontend/webapp/src/utils/filterQuestions.js
@@ -1,8 +1,25 @@
 export default function (questions, filters) {
+    const trimmedSearch = (filters.search || "")
+        .toLowerCase()
+        .replace(/^\s*|\s*$/g, "")
+        .replace(/\s+/g, " ");
+    const searchKeywords = trimmedSearch !== "" ? trimmedSearch.split(" ") : [];
+
     return questions.filter((question) => {
         if (
             filters.tags.length > 0 &&
             !question.tags.some(({ uid }) => filters.tags.includes(uid))
+        ) {
+            return false;
+        }
+
+        if (
+            searchKeywords.length > 0 &&
+            !searchKeywords.every(
+                (keyword) =>
+                    question.question.toLowerCase().includes(keyword) ||
+                    question.details.toLowerCase().includes(keyword)
+            )
         ) {
             return false;
         }
