8828479d27b97b34a5a7d86e43990794df96f57f
Christophe BENARD <ch.benard@gmail.com>
2023-07-25 17:01:35 +0200

    feat: adapter le paramétrage de vueMatomo au service refers to https://trello.com/c/lGiLev2f/1482

--- packages/frontend/webapp/src/helpers/matomo.js
+++ packages/frontend/webapp/src/helpers/matomo.js
@@ -15,10 +15,27 @@ export function useMatomo(app, router) {
     app.use(VueMatomo, {
         host: MATOMO.HOST,
         siteId: MATOMO.SITE_ID,
-        router,
+        // Si on utilise le serveur matomo DNUM, il faut modifier l'URL
+        // avant l'envoi à Matomo pour rester compatible avec Xiti
+        // Le séparateur original "/" est remplacé par "::"
+        router:
+            typeof MATOMO.DESCRIPTION_PAGE_SEPARATOR !== "undefined" &&
+            MATOMO.DESCRIPTION_PAGE_SEPARATOR !== "/"
+                ? // MATOMO.DESCRIPTION_PAGE_SEPARATOR est initialisée avec une valeur
+                  {
+                      beforeTrack(to) {
+                          const modifiedPath = to.fullPath.replace(
+                              /\//g,
+                              MATOMO.DESCRIPTION_PAGE_SEPARATOR
+                          );
+                          return { ...to, fullPath: modifiedPath };
+                      },
+                  }
+                : router,
         trackInitialView: false,
         cookieDomain: `*.${MATOMO.DOMAIN}`,
         domains: `*.${MATOMO.DOMAIN}`,
+        trackerFileName: MATOMO.TRACKER_FILENAME,
     });
 }
 
