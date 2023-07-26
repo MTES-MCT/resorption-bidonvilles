581e11f0afb140066f62ea1a22115f25acdf44cc
Christophe BENARD <ch.benard@gmail.com>
2023-07-26 12:05:49 +0200

    feat: adapter le paramétrage de vueMatomo au service DNUM refers to https://trello.com/c/lGiLev2f/1482

--- packages/frontend/www/plugins/matomo.client.ts
+++ packages/frontend/www/plugins/matomo.client.ts
@@ -1,13 +1,39 @@
-import VueMatomo from 'vue-matomo';
+import VueMatomo, { TrackerCustomizer } from 'vue-matomo';
+const {
+    MATOMO_HOST,
+    MATOMO_SITE_ID,
+    MATOMO_DOMAIN,
+    MATOMO_TRACKER_FILENAME,
+    MATOMO_DESCRIPTION_PAGE_SEPARATOR,
+} = useRuntimeConfig().public;
+
+// Fonction pour remplacer le séparateur original "/" par "::"
+const replaceSeparator = (url: string) => {
+    return url.replace(/\//g, '::');
+};
+
+const shouldUseCustomizer = (): boolean => {
+    return (
+        MATOMO_DESCRIPTION_PAGE_SEPARATOR !== "undefined" &&
+        MATOMO_DESCRIPTION_PAGE_SEPARATOR !== "/"
+    );
+};
 
 export default defineNuxtPlugin((nuxtApp) => {
     const { DOMAIN } = useRuntimeConfig().public;
     nuxtApp.vueApp.use(VueMatomo, {
-        host: "https://stats.data.gouv.fr",
-        siteId: 86,
-        domains: `*.${DOMAIN}`,
-        cookieDomain: `*.${DOMAIN}`,
+        host: MATOMO_HOST,
+        siteId: MATOMO_SITE_ID,
+        domains: `*.${MATOMO_DOMAIN}`,
+        cookieDomain: `*.${MATOMO_DOMAIN}`,
         router: nuxtApp.$router,
-        trackerScriptUrl: '/matomo.js'
+        trackerScriptUrl: `'/'${MATOMO_TRACKER_FILENAME}`,
+        // Modifier l'URL avant de la suivre
+        trackPageViewCustomizer: shouldUseCustomizer()
+            ? (data: TrackerCustomizer) => {
+                data.url = replaceSeparator(data.url);
+                return data;
+            }
+            : undefined,
     });
 });
