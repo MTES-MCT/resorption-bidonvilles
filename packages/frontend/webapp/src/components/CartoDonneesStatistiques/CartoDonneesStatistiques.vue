17db32f771aa72907a74e46d295ef7bd3654695b
gregoirethomazeau <thomazeau.gregoire@orange.fr>
2023-07-31 16:58:56 +0200

    (1925) alémioration légende

--- packages/frontend/webapp/src/components/CartoDonneesStatistiques/CartoDonneesStatistiques.vue
+++ packages/frontend/webapp/src/components/CartoDonneesStatistiques/CartoDonneesStatistiques.vue
@@ -15,16 +15,64 @@
             ref="legendeConditionsDeVie"
             class="bg-white ml-3 my-3 border-2 border-G500 py-1 px-2 rounded"
         >
-            <label class="flex items-center space-x-2">
-                <Icon class="text-success mx-2" icon="faucet-drip" />
-                Accès satisfaisant
-            </label>
-            <label class="flex items-center space-x-2">
-                <Icon class="text-error mx-2" icon="faucet-drip" />
-                Accès à améliorer
-            </label>
-        </div></Carto
-    >
+            <div @click="changeLegendeStatus" class="mb-2 mx-2 cursor-pointer">
+                <Icon
+                    :icon="`${
+                        legendeStatus === true ? 'chevron-up' : 'chevron-down'
+                    }`"
+                />
+                <label class="font-bold mx-2"> Légende </label>
+            </div>
+            <div v-if="legendeStatus === true" class="flex">
+                <div class="flex">
+                    <div class="grid grid-cols-1">
+                        <label class="flex items-center space-x-2">
+                            <Icon class="mx-2" icon="faucet-drip" />
+                        </label>
+                        <label class="flex items-center space-x-2">
+                            <Icon class="mx-2" icon="bolt" />
+                        </label>
+                        <label class="flex items-center space-x-2">
+                            <Icon class="mx-2" icon="trash-alt" />
+                        </label>
+                        <label class="flex items-center space-x-2">
+                            <Icon class="mx-2" icon="fire-extinguisher" />
+                        </label>
+                        <label class="flex items-center space-x-2">
+                            <Icon class="mx-2" icon="toilet" />
+                        </label>
+                        <label class="flex items-center space-x-2">
+                            <Icon class="mx-2" icon="bug-slash" />
+                        </label>
+                    </div>
+
+                    <div class="grid grid-cols-1">
+                        <label> Accès à l'eau </label>
+                        <label> Accès à l'électricité </label>
+                        <label> Evacuation des déchets </label>
+                        <label> Prévention incendie </label>
+                        <label> Accès aux toilettes </label>
+                        <label> Absence de nuisibles </label>
+                    </div>
+                </div>
+                <div class="flex flex-col ml-4">
+                    <div class="flex mb-2">
+                        <div class="bg-success w-10 border"></div>
+                        <label class="ml-2"> Satisfaisant </label>
+                    </div>
+
+                    <div class="flex mb-2">
+                        <div class="bg-error w-10"></div>
+                        <label class="ml-2"> A améliorer </label>
+                    </div>
+                    <div class="flex">
+                        <div class="border w-10 crossed"></div>
+                        <label class="ml-2"> Inexistant </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Carto>
 </template>
 
 <script setup>
@@ -40,6 +88,7 @@ const departementMetricsStore = useDepartementMetricsStore();
 const carto = ref(null);
 const markersGroup = ref(L.geoJSON([], {}));
 const legendeConditionsDeVie = ref(null);
+const legendeStatus = ref(false);
 
 watch(carto, () => {
     if (carto.value) {
@@ -70,6 +119,10 @@ function createLegende() {
     });
 
     return new Legende();
+}
+
+function changeLegendeStatus() {
+    legendeStatus.value = !legendeStatus.value;
 }
 
 defineExpose({
@@ -105,3 +158,21 @@ defineExpose({
     },
 });
 </script>
+
+<style scoped>
+.crossed {
+    pointer-events: none;
+    content: "";
+    background: linear-gradient(
+        to left bottom,
+        transparent 49%,
+        currentColor 49%,
+        currentColor 51%,
+        transparent 51%
+    );
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+}
+</style>
