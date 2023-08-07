086b91b7eecbf6b6801b64dd32ad8e31046ba65f
Anis Safine Laget <anis@users.noreply.github.com>
2023-08-07 11:36:55 +0200

    fix: correction de l'état de focus sur le TB

---
+++ packages/frontend/webapp/src/components/IconeAdministrateur/IconeAdministrateur.vue
@@ -0,0 +1,11 @@
+<template>
+    <Icon
+        icon="user-shield"
+        class="text-info"
+        title="Pictogramme administrateur local"
+    />
+</template>
+
+<script setup>
+import { Icon } from "@resorptionbidonvilles/ui";
+</script>
