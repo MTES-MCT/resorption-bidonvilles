060206cd4538e217a7c5ac686e6ba9ef6954748b
Christophe BENARD <ch.benard@gmail.com>
2023-09-01 17:56:00 +0200

    fix: Ajouter un nom accessible aux liens du footer refers to https://trello.com/c/ePmf55jC

--- packages/frontend/ui/src/components/FooterBar/FooterBarFootLink.vue
+++ packages/frontend/ui/src/components/FooterBar/FooterBarFootLink.vue
@@ -1,9 +1,8 @@
 <template>
-    <a
-        :href="to"
-        class="text-xs border-b-2 border-transparent hover:border-G500 focus:outline-none focus:ring-2 ring-offset-2 ring-info"
-        ><slot
-    /></a>
+    <a :href="to" :title="title"
+        class="text-xs border-b-2 border-transparent hover:border-G500 focus:outline-none focus:ring-2 ring-offset-2 ring-info">
+        <slot />
+    </a>
 </template>
 
 <script setup>
@@ -11,7 +10,11 @@ import { toRefs } from "vue";
 
 const props = defineProps({
     to: String,
+    title: {
+        type: String,
+        default: "",
+    }
 });
 
-const { to } = toRefs(props);
+const { to, title } = toRefs(props);
 </script>
