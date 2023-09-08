ed3fbafc14717529afc67d95bc0f8b5250e17ace
Christophe BENARD <ch.benard@gmail.com>
2023-09-08 10:16:50 +0200

    fix: Améliorer l'accessibilité de la carte question refers to https://trello.com/c/ePmf55jC

--- packages/frontend/webapp/src/components/CarteQuestion/CarteQuestion.vue
+++ packages/frontend/webapp/src/components/CarteQuestion/CarteQuestion.vue
@@ -1,39 +1,53 @@
 <template>
-    <router-link
-        class="rounded border-1 border-blue300 px-4 pb-4 cursor-pointer hover:bg-blue200 flex flex-col relative focus:outline-none focus:ring-2 ring-offset-2 ring-info"
-        :to="`/question/${question.id}`"
-    >
-        <div class="absolute right-4 text-secondary px-2">
-            <div
-                class="text-sm font-bold"
-                v-if="question.peopleAffected && question.peopleAffected > 0"
-            >
-                <Icon icon="exclamation-circle" />
-                {{ question.peopleAffected }}
-                {{
-                    question.peopleAffected > 1
-                        ? "habitants concernés"
-                        : "habitant concerné"
-                }}
+    <router-link custom v-slot="{ navigate }" :to="`/question/${question.id}`">
+        <div
+            :aria-label="`Fiche question: ${question.question}`"
+            role="button"
+            tabindex="0"
+            @click="navigate"
+            class="rounded border-1 border-blue300 px-4 pb-4 cursor-pointer hover:bg-blue200 flex flex-col relative focus:outline-none focus:ring-2 ring-offset-2 ring-info"
+        >
+            <div class="absolute right-4 text-secondary px-2">
+                <div
+                    class="text-sm font-bold"
+                    v-if="
+                        question.peopleAffected && question.peopleAffected > 0
+                    "
+                >
+                    <Icon icon="exclamation-circle" />
+                    {{ question.peopleAffected }}
+                    {{
+                        question.peopleAffected > 1
+                            ? "habitants concernés"
+                            : "habitant concerné"
+                    }}
+                </div>
             </div>
+            <CarteQuestionQuestion class="pt-4" :question="question.question" />
+            <div class="text-primary font-bold mb-3">
+                Par
+                <LinkOrganization
+                    :to="`/structure/${question.createdBy.organization_id}`"
+                    :ariaLabel="`Afficher la fiche de la structure de l'auteur ${question.createdBy.first_name}
+                        ${question.createdBy.last_name} -
+                        ${question.createdBy.organization}`"
+                >
+                    {{ question.createdBy.first_name }}
+                    {{ question.createdBy.last_name }} -
+                    {{ question.createdBy.organization }}
+                </LinkOrganization>
+            </div>
+            <div aria-label="Liste des étiquettes caractérisant la question">
+                <QuestionTag
+                    v-for="tag in question.tags"
+                    :key="tag.uid"
+                    :tag="tag"
+                >
+                    {{ tag.name }}
+                </QuestionTag>
+            </div>
+            <CarteQuestionFooter :question="question" />
         </div>
-        <CarteQuestionQuestion class="pt-4" :question="question.question" />
-        <div class="text-primary font-bold mb-3">
-            Par
-            <LinkOrganization
-                :to="`/structure/${question.createdBy.organization_id}`"
-            >
-                {{ question.createdBy.first_name }}
-                {{ question.createdBy.last_name }} -
-                {{ question.createdBy.organization }}
-            </LinkOrganization>
-        </div>
-        <div>
-            <QuestionTag v-for="tag in question.tags" :key="tag.uid" :tag="tag">
-                {{ tag.name }}
-            </QuestionTag>
-        </div>
-        <CarteQuestionFooter :question="question" />
     </router-link>
 </template>
 
--- packages/frontend/webapp/src/components/CarteQuestion/CarteQuestionFooter.vue
+++ packages/frontend/webapp/src/components/CarteQuestion/CarteQuestionFooter.vue
@@ -3,14 +3,16 @@
         <CarteQuestiondateCreation :question="question" />
         <div class="flex justify-end items-center space-x-4">
             <Button
+                :aria-label="`Voir la question ${question.question}`"
                 variant="primary"
                 size="sm"
                 icon="rectangle-list"
                 iconPosition="left"
-                :href="`/question/${question.id}#reponses`"
+                :href="`/question/${question.id}`"
                 >Voir la question</Button
             >
             <Button
+                :aria-label="`Voir les ${seeAnswerWording}`"
                 v-if="questionHasAtLeastOneAnswer"
                 variant="primary"
                 size="sm"
