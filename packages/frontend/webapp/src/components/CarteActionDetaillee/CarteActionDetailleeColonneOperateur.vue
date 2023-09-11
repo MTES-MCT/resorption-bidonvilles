416743c9931363639b67aeaf010451425f461fe0
Christophe BENARD <ch.benard@gmail.com>
2023-09-11 16:46:23 +0200

    fix: rendre les infos de l'opérateur de l'action accessibles refers to https://trello.com/c/ePmf55jC

--- packages/frontend/webapp/src/components/CarteActionDetaillee/CarteActionDetailleeColonneOperateur.vue
+++ packages/frontend/webapp/src/components/CarteActionDetaillee/CarteActionDetailleeColonneOperateur.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
-        <div class="ml-2">
+        <div class="ml-2" tabindex="0" aria-label="Opérateur">
             <Icon icon="user-circle" class="mr-1" />
             <span class="text-dark font-bold">Opérateur(s)</span><br />
             <p v-for="operator in operators" :key="operator.id">
-                <LinkOrganization :to="`/structure/${operator.id}`">
+                <LinkOrganization
+                    :to="`/structure/${operator.id}`"
+                    :ariaLabel="`Afficher la fiche de la structure de l'opérateur, ${
+                        operator.abbreviation || operator.name
+                    }`"
+                >
                     {{ operator.abbreviation || operator.name }}
                 </LinkOrganization>
             </p>
