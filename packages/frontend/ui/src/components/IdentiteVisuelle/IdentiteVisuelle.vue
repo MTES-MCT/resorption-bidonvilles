6e8d5a8706c66702c8636137aad7311e580efcee
Christophe BENARD <ch.benard@gmail.com>
2023-09-11 10:34:34 +0200

    fix: Corriger l'accessibilité du bloc logo en mode connecté refers to https://trello.com/c/ePmf55jC

--- packages/frontend/ui/src/components/IdentiteVisuelle/IdentiteVisuelle.vue
+++ packages/frontend/ui/src/components/IdentiteVisuelle/IdentiteVisuelle.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link to="/" title="Retourner à la page d'accueil en mode connecté, sinon, au formulaire de connexion"
+    <router-link to="/" aria-label="Retourner à la page d'accueil en mode connecté, sinon, au formulaire de connexion"
         class="flex items-center space-x-8 hover:bg-G100 p-3 focus:outline-none focus:ring-2 ring-offset-2 ring-info">
         <div class="flex flex-col items-start space-y-1">
             <img :src="marianne" :class="sizeClass.marianneH" alt="Bloc marque de l'état" />
