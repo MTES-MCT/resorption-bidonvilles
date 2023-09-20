68c40cb47a959d85d086c0e2709705f59279fd93
Christophe BENARD <ch.benard@gmail.com>
2023-09-20 19:00:11 +0200

    fix: Remplacer les titres `<h1>` par des éléments `<legend>` refers to https://trello.com/c/ygFnLbLs

---
+++ packages/frontend/ui/src/components/Fieldset.vue
@@ -0,0 +1,25 @@
+<template>
+    <fieldset>
+        <legend class="font-bold text-xl mb-2">
+            {{ legend }}
+            <MandatoryStar v-if="showMandatoryStar" />
+        </legend>
+        <slot />
+    </fieldset>
+</template>
+<script setup>
+import { toRefs } from "vue";
+import { MandatoryStar } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    legend: {
+        type: String,
+        required: false,
+    },
+    showMandatoryStar: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+});
+</script>
