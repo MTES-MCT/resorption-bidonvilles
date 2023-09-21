8611680b2a890b72c6b7b5e50df74842c94969b8
Christophe BENARD <ch.benard@gmail.com>
2023-09-21 18:12:27 +0200

    fix: Remplacer l’élément div par un élément p refers to https://trello.com/c/H3rV1uT7

--- packages/frontend/www/components/LandingPage/LandingPageHero.vue
+++ packages/frontend/www/components/LandingPage/LandingPageHero.vue
@@ -5,21 +5,21 @@
             {{ $t("landingPage.hero.subtitle") }}
         </h2>
         <LandingPageContactForm class="mx-auto mb-8" />
-        <div class="my-4 text-display-md font-bold">
+        <p class="my-4 text-display-md font-bold">
             <span class="text-white bg-black px-2 py-1">2</span>
             <span class="text-white bg-black px-2 py-1 ml-1">8</span>
             {{ $t("landingPage.hero.review") }} 2021
-        </div>
-        <div class="my-4 text-display-md font-bold">
+        </p>
+        <p class="my-4 text-display-md font-bold">
             <span class="text-white bg-black px-2 py-1">1</span>
             <span class="text-white bg-black px-2 py-1 ml-1">6</span>
             {{ $t("landingPage.hero.review") }} 2020
-        </div>
-        <div class="my-4 text-display-md font-bold">
+        </p>
+        <p class="my-4 text-display-md font-bold">
             <span class="text-white bg-black px-2 py-1">1</span>
             <span class="text-white bg-black px-2 py-1 ml-1">8</span>
             {{ $t("landingPage.hero.review") }} 2019
-        </div>
+        </p>
         <p class="text-lg font-bold">{{ $t("landingPage.hero.moreThan") }} <span class="underline">3 000</span> {{
             $t("landingPage.hero.people") }}</p>
     </div>
