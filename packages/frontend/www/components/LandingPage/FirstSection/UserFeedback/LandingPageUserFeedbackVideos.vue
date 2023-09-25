18180b4a965ee64090755769a95d11a20be3e59c
Christophe BENARD <ch.benard@gmail.com>
2023-09-25 15:20:00 +0200

    fix: Ajouter une transcription textuelle sous les vidéos refers to https://trello.com/c/H3rV1uT7

--- packages/frontend/www/components/LandingPage/FirstSection/UserFeedback/LandingPageUserFeedbackVideos.vue
+++ packages/frontend/www/components/LandingPage/FirstSection/UserFeedback/LandingPageUserFeedbackVideos.vue
@@ -1,22 +1,70 @@
 <template>
     <div class="mt-8 lg:flex lg:space-x-4">
         <div class="flex-1">
-            <video class="inline-block" preload="none" controls poster="~/assets/img/LandingPage/FirstSection/poster_temoignage_leduc.jpg">
-                <source
-                    src="~/assets/video/Temoignage-Leduc-BD_mars2021.mp4"
-                    type="video/mp4"
-                />
-                Votre navigateur ne supporte pas la balise video.
-            </video> 
-        </div>
-        <div class="flex-1 mt-8 lg:mt-0">
-            <video class="inline-block" preload="none" controls poster="~/assets/img/LandingPage/FirstSection/poster_temoignage_leminor.jpg">
-                <source
-                    src="~/assets/video/Temoignage-LeMinor-BD_mars2021.mp4"
-                    type="video/mp4"
-                />
+            <video :aria-label="`Témoignage utilisateur:  ${$t('landingPage.firstSection.feedback.1.author')},`"
+                class="inline-block" preload="none" controls
+                poster="~/assets/img/LandingPage/FirstSection/poster_temoignage_leduc.jpg">
+                <source src="~/assets/video/Temoignage-Leduc-BD_mars2021.mp4" type="video/mp4" :aria-label="``" />
                 Votre navigateur ne supporte pas la balise video.
             </video>
+            <button @click="toggleTranscription(1)" class="mt-2 text-primary hover:underline cursor-pointer"
+                :aria-label="`${showTranscription[1] ? $t('landingPage.notranscription_title') : $t('landingPage.transcription_title')},`">
+                <span v-if="!showTranscription[1]">{{ $t("landingPage.transcription_title") }}</span>
+                <span v-if="showTranscription[1]">{{ $t("landingPage.notranscription_title") }}</span>
+            </button>
+            <div v-if="showTranscription[1]" tabindex="0" class="m-4 text-left">
+                <p class="font-bold mb-2">Témoignage utilisateur - {{ $t("landingPage.firstSection.feedback.1.author") }}
+                </p>
+                <p>{{ $t("landingPage.firstSection.feedback.videos.1.transcription_part1") }}</p>
+                <p>{{ $t("landingPage.firstSection.feedback.videos.1.transcription_part2") }}</p>
+                <p>{{ $t("landingPage.firstSection.feedback.videos.1.transcription_part3") }}</p>
+            </div>
+
+        </div>
+        <div class="flex-1 mt-8 lg:mt-0">
+            <video :aria-label="`Témoignage utilisateur:  ${$t('landingPage.firstSection.feedback.2.author')},`"
+                class="inline-block" preload="none" controls
+                poster="~/assets/img/LandingPage/FirstSection/poster_temoignage_leminor.jpg">
+                <source src="~/assets/video/Temoignage-LeMinor-BD_mars2021.mp4" type="video/mp4" />
+                Votre navigateur ne supporte pas la balise video.
+            </video>
+            <button @click="toggleTranscription(2)" class="mt-2 text-primary hover:underline cursor-pointer"
+                :aria-label="`${showTranscription[2] ? $t('landingPage.notranscription_title') : $t('landingPage.transcription_title')},`">
+                <span v-if="!showTranscription[2]">{{ $t("landingPage.transcription_title") }}</span>
+                <span v-if="showTranscription[2]">{{ $t("landingPage.notranscription_title") }}</span>
+            </button>
+            <div v-if="showTranscription[2]" tabindex="0" class="m-4 text-left">
+                <p class="font-bold mb-2">Témoignage utilisateur - {{ $t("landingPage.firstSection.feedback.2.author") }}
+                </p>
+                <p>{{ $t("landingPage.firstSection.feedback.videos.2.transcription_part1") }}</p>
+                <p>{{ $t("landingPage.firstSection.feedback.videos.2.transcription_part2") }}</p>
+                <p>{{ $t("landingPage.firstSection.feedback.videos.2.transcription_part3") }}</p>
+            </div>
         </div>
     </div>
-</template> 
+</template>
+<script>
+import { useI18n } from 'vue-i18n';
+
+export default {
+    setup() {
+        const i18n = useI18n();
+        return {
+            i18n
+        };
+    },
+    data() {
+        return {
+            showTranscription: {
+                1: false,
+                2: false,
+            }
+        };
+    },
+    methods: {
+        toggleTranscription(index) {
+            this.showTranscription[index] = !this.showTranscription[index];
+        },
+    }
+}
+</script>
