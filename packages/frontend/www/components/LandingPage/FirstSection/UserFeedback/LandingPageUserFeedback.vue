17412ba8299438da5cbd2e0b32579e8f4297f0f4
Christophe BENARD <ch.benard@gmail.com>
2023-09-29 15:15:39 +0200

    fix: Ajouter "play/pause" sur les slides "témoignages" refers to https://trello.com/c/H3rV1uT7

--- packages/frontend/www/components/LandingPage/FirstSection/UserFeedback/LandingPageUserFeedback.vue
+++ packages/frontend/www/components/LandingPage/FirstSection/UserFeedback/LandingPageUserFeedback.vue
@@ -7,7 +7,7 @@
                     $t("landingPage.firstSection.feedback.title")
                 }}</span>
             </h2>
-            <div class="mt-8">
+            <div class="mt-8" @mouseover="setCarouselOff" @mouseleave="setCarouselOn">
                 <LandingPageUserFeedbackSection :active="active === 1"
                     :text="$t('landingPage.firstSection.feedback.1.text')"
                     :author="$t('landingPage.firstSection.feedback.1.author')" />
@@ -20,60 +20,86 @@
             </div>
         </div>
         <div class="mt-2 text-center">
-            <LandingPageUserFeedbackBullet alt="Premier témoignage" :onClick="() => setSection(1)" :active="active === 1" />
-            <LandingPageUserFeedbackBullet alt="Second témoignage" :onClick="() => setSection(2)" :active="active === 2" />
-            <LandingPageUserFeedbackBullet alt="Troisième témoignage" :onClick="() => setSection(3)"
-                :active="active === 3" />
+            <LandingPageUserFeedbackBullet :alt="[
+                $t('landingPage.firstSection.feedback.1.label'), ' ',
+                $t('landingPage.firstSection.feedback.1.author'), ' ',
+                $t('landingPage.firstSection.feedback.1.text'), ' ',
+            ]" :onClick="() => setSection(1)" :active="active === 1" />
+            <LandingPageUserFeedbackBullet :alt="[
+                $t('landingPage.firstSection.feedback.2.label'), ' ',
+                $t('landingPage.firstSection.feedback.2.author'), ' ',
+                $t('landingPage.firstSection.feedback.2.text'), ' ',
+            ]" :onClick="() => setSection(2)" :active="active === 2" />
+            <LandingPageUserFeedbackBullet :alt="[
+                $t('landingPage.firstSection.feedback.3.label'), ' ',
+                $t('landingPage.firstSection.feedback.3.author'), ' ',
+                $t('landingPage.firstSection.feedback.3.text'), ' ',
+            ]" :onClick="() => setSection(3)" :active="active === 3" /><br />
+            <button @click="playPauseCarousel()" class="mt-2 text-black cursor-pointer"
+                :aria-label="`${carouselOn ? $t('landingPage.firstSection.feedback.carousel_stop') : $t('landingPage.firstSection.feedback.carousel_start')},`"
+                tabindex="-1">
+                <Icon :icon="`${carouselOn ? 'fa-solid fa-pause' : 'fa-solid fa-play'}`" class="mr-2" />
+                <span v-if="!carouselOn">{{ $t("landingPage.firstSection.feedback.carousel_start") }}</span>
+                <span v-if="carouselOn">{{ $t("landingPage.firstSection.feedback.carousel_stop")
+                }}</span>
+            </button>
             <LandingPageUserFeedbackVideos v-if="$i18n.locale === 'fr'" />
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
+import {
+    ref,
+    onBeforeUnmount,
+} from "vue";
 import LandingPageUserFeedbackBullet from "./LandingPageUserFeedbackBullet.vue";
 import LandingPageUserFeedbackSection from "./LandingPageUserFeedbackSection.vue";
 import LandingPageUserFeedbackVideos from "./LandingPageUserFeedbackVideos.vue";
+import { Icon } from "@resorptionbidonvilles/ui";
 
-export default {
-    setup() {
-        const i18n = useI18n();
-        return {
-            i18n
-        };
-    },
-    data() {
-        return {
-            active: 1,
-            interval: null
-        };
-    },
-    methods: {
-        setNextSection() {
-            if (this.active === 1) {
-                this.active = 2;
-            } else if (this.active === 2) {
-                this.active = 3;
-            } else if (this.active === 3) {
-                this.active = 1;
-            }
-        },
-        setSection(activeSection) {
-            this.active = activeSection;
-            // reset delay
-            clearInterval(this.interval);
-            this.interval = setInterval(this.setNextSection, 8000);
-        }
-    },
-    mounted() {
-        this.interval = setInterval(this.setNextSection, 8000);
-    },
-    beforeDestroy() {
-        clearInterval(this.interval);
-    },
-    components: {
-        LandingPageUserFeedbackBullet,
-        LandingPageUserFeedbackSection,
-        LandingPageUserFeedbackVideos
+const i18n = useI18n();
+const carouselOn = ref(true);
+let active = ref(1);
+let interval = ref(null);
+
+function setNextSection() {
+    if (active.value === 1) {
+        active.value = 2;
+    } else if (active.value === 2) {
+        active.value = 3;
+    } else if (active.value === 3) {
+        active.value = 1;
     }
-};
+}
+function setSection(activeSection) {
+    active.value = activeSection;
+    // reset delay
+    clearInterval(interval.value);
+    interval.value = setInterval(setNextSection, 8000);
+}
+
+function playPauseCarousel() {
+    if (carouselOn.value === true) {
+        setCarouselOff();
+    } else {
+        setCarouselOn();
+    }
+}
+function setCarouselOn() {
+    setNextSection();
+    interval.value = setInterval(setNextSection, 8000);
+    carouselOn.value = true;
+}
+function setCarouselOff() {
+    clearInterval(interval.value);
+    carouselOn.value = false;
+}
+onMounted(() => {
+    interval.value = setInterval(setNextSection, 8000);
+});
+
+onBeforeUnmount(() => {
+    clearInterval(interval.value);
+})
 </script>
