3bad7d07cd626486d11e1b4fb63b3fae10cf35a0
Christophe BENARD <ch.benard@gmail.com>
2023-09-29 21:55:35 +0200

    fix: Remplacer la `<div>` "Vous souhaitez être informé des..." par `<p>` refers to https://trello.com/c/H3rV1uT7

--- packages/frontend/www/components/LandingPage/ThirdSection/LandingPageNewsletter.vue
+++ packages/frontend/www/components/LandingPage/ThirdSection/LandingPageNewsletter.vue
@@ -3,15 +3,15 @@
         <p class="text-display-lg font-bold text-primary">
             {{ $t("landingPage.newsletter.title") }}
         </p>
-        <div class="mb-4 max-w-screen-sm mx-auto">
+        <p class="mb-4 max-w-screen-sm mx-auto">
             {{ $t("landingPage.newsletter.text") }}
-        </div>
+        </p>
         <Button variant="primary" href="https://email.developpement-durable.gouv.fr/users/subscribe/js_id/5n4i/id/3">{{
             $t("landingPage.newsletter.cta") }}</Button>
-        <div class="mt-6">
+        <p class="mt-6">
             <a class="link" href="https://www.gouvernement.fr/resorption-des-bidonvilles">{{
                 $t("landingPage.newsletter.viewAll") }}</a>
-        </div>
+        </p>
     </div>
 </template>
 
