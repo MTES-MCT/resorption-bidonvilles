4a51ca273fe5ccd27d1bb0e3d82c7d56de8907b5
Christophe BENARD <ch.benard@gmail.com>
2023-09-29 15:19:25 +0200

    fix: Rendre cohérent la struxcture de la citation et de son auteur refers to https://trello.com/c/H3rV1uT7

--- packages/frontend/www/components/LandingPage/FirstSection/UserFeedback/LandingPageUserFeedbackSection.vue
+++ packages/frontend/www/components/LandingPage/FirstSection/UserFeedback/LandingPageUserFeedbackSection.vue
@@ -1,30 +1,25 @@
 <template>
-    <blockquote
-        :class="
-            active
-                ? 'transition duration-500 block opacity-100 visible'
-                : 'invisible opacity-0 absolute'
-        "
-    >
-        <p class="relative text-xl feedbackText">{{ text }}</p>
-        <footer class="text-lg font-light">{{ author }}</footer>
-    </blockquote>
+    <div :class="active
+        ? 'transition duration-500 block opacity-100 visible'
+        : 'invisible opacity-0 absolute'
+        ">
+        <blockquote class="relative text-xl feedbackText">{{ text }}</blockquote>
+        <p class="text-lg font-light">{{ author }}</p>
+    </div>
 </template>
 
-<script>
-export default {
-    props: {
-        text: {
-            type: String
-        },
-        author: {
-            type: String
-        },
-        active: {
-            type: Boolean
-        }
+<script setup>
+const props = defineProps({
+    text: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    active: {
+        type: Boolean
     }
-};
+});
 </script>
 
 <style scoped>
@@ -37,6 +32,7 @@ export default {
 
         @apply text-secondary text-size-display-xl;
     }
+
     &::after {
         content: "»";
         @apply ml-1 text-secondary text-size-lg;
