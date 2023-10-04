06c2a2cc8522854cc5d420b1bc09a29aca937fea
Christophe BENARD <ch.benard@gmail.com>
2023-10-04 10:27:48 +0200

    fix: Rendre les icônes des réseaux sociaux accessibles refers to https://trello.com/c/wShljlQS/1987

--- packages/frontend/ui/.storybook/preview.js
+++ packages/frontend/ui/.storybook/preview.js
@@ -20,14 +20,20 @@ const i18n = createI18n({
                 "newsletterTitle": "Gardons le contact",
                 "newsletterBody": "Recevez l'actualité sur le sujet des bidonvilles tous les mois.",
                 "newsletterCta": "Je m'abonne",
-            
+
                 "contactTitle": "Un problème, une question?",
                 "contactBody1": "Appelez nous au +33 1 40 81 95 39",
                 "contactBody2": "Écrivez-nous à",
-            
+
                 "socialTitle1": "Suivez-nous",
                 "socialTitle2": "sur les réseaux sociaux",
-            
+
+                "socialOn": "sur",
+
+                "twitter": "Twitter",
+                "facebook": "Facebook",
+                "linkedin": "Linkedin",
+
                 "sourceCode": "Code source",
                 "statistics": "Mesures d'impact",
                 "legal": "Mentions légales",
--- packages/frontend/ui/src/components/FooterBar/FooterBarSocialMedia.vue
+++ packages/frontend/ui/src/components/FooterBar/FooterBarSocialMedia.vue
@@ -4,14 +4,27 @@
             {{ $t('footer.socialTitle1') }} <br class="hidden lg:inline" />{{ $t('footer.socialTitle2') }}
         </h2>
         <p class="mt-2 flex space-x-4 -ml-2">
-            <FooterBarSocialMediaLink href="https://twitter.com/dihal_gouv" icon="fa-twitter" />
+            <FooterBarSocialMediaLink href="https://twitter.com/dihal_gouv" icon="fa-twitter"
+                :socialVerb="$t('footer.socialTitle1')" :socialOn="$t('footer.socialOn')"
+                :socialName="$t('footer.twitter')" />
             <FooterBarSocialMediaLink href="https://www.facebook.com/DIHAL.delegation.interministerielle/"
-                icon="fa-facebook" />
-            <FooterBarSocialMediaLink href="https://fr.linkedin.com/company/dihal" icon="fa-linkedin" />
+                icon="fa-facebook" :socialVerb="$t('footer.socialTitle1')" :socialOn="getSocialOnFacebook()"
+                :socialName="$t('footer.facebook')" />
+            <FooterBarSocialMediaLink href="https://fr.linkedin.com/company/dihal" icon="fa-linkedin"
+                :socialVerb="$t('footer.socialTitle1')" :socialOn="$t('footer.socialOn')"
+                :socialName="$t('footer.linkedin')" />
         </p>
     </section>
 </template>
 
 <script setup>
 import FooterBarSocialMediaLink from "./FooterBarSocialMediaLink.vue";
+import { useI18n } from 'vue-i18n';
+
+const { t, locale } = useI18n();
+
+// PEtite subtilité pour la traduction en bulgare lorsqu'il s'agit de Facebook
+function getSocialOnFacebook() {
+    return locale.value === 'bg' ? t('footer.socialOnFacebook') : t('footer.socialOn');
+}
 </script>
