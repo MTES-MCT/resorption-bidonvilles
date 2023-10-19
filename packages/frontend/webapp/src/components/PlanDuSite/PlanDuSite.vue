eeb4df5ed5e384f8c05d2e81e1e4bcc58108af9f
Christophe BENARD <ch.benard@gmail.com>
2023-10-19 13:17:12 +0200

    fix: automatiser la liste des liens du plan du site refers to: https://github.com/MTES-MCT/resorption-bidonvilles/pull/846#discussion_r1364108844

--- packages/frontend/webapp/src/components/PlanDuSite/PlanDuSite.vue
+++ packages/frontend/webapp/src/components/PlanDuSite/PlanDuSite.vue
@@ -7,7 +7,7 @@
                     :to="lien.to"
                     color="text-black"
                     hoverColor="text-black"
-                    >{{ lien.linkLabel }}</Link
+                    >{{ lien.label }}</Link
                 >
             </li>
         </ul>
@@ -17,152 +17,31 @@
 import { computed } from "vue";
 import { Link } from "@resorptionbidonvilles/ui";
 import { useUserStore } from "@/stores/user.store";
-
+import router from "@/helpers/router.js";
 const userStore = useUserStore();
 
-const liens = [
-    { to: "/", linkLabel: "Accueil", entity: "none", feature: "none" },
-    {
-        to: "/connexion",
-        linkLabel: "S'identifier sur la plateforme",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/deconnexion",
-        linkLabel: "Se déconnecter",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/mon-compte",
-        linkLabel: "Consulter ou modifier les informations liées à mon compte",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "https://www.blog-resorption-bidonvilles.fr/",
-        linkLabel: "Accéder au blog",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "https://www.blog-resorption-bidonvilles.fr/accueil/categories/ressources",
-        linkLabel: "Accéder aux ressources",
-        entity: "none",
-        feature: "none",
-    },
-
-    {
-        to: "/tableau-de-bord",
-        linkLabel: "Consulter le tableau de bord",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/liste-des-sites",
-        linkLabel: "Afficher la liste des sites",
-        entity: "shantytown",
-        feature: "list",
-    },
-    {
-        to: "/site/nouveau",
-        linkLabel: "Déclarer un site",
-        entity: "shantytown",
-        feature: "create",
-    },
-    {
-        to: "/site/signalement",
-        linkLabel: "Signaler un site",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/liste-des-actions",
-        linkLabel: "Afficher la liste des actions",
-        entity: "action",
-        feature: "list",
-    },
-    {
-        to: "/action/nouveau",
-        linkLabel: "Créer une fiche action",
-        entity: "action",
-        feature: "create",
-    },
-    {
-        to: "/communaute",
-        linkLabel: "Demander de l'aide à la communauté",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/communaute/nouvelle-question",
-        linkLabel: "Poser une question à la communauté",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/annuaire",
-        linkLabel: "Consulter l'annuaire",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/cartographie",
-        linkLabel: "Visualiser la carte des bidonvilles",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/activites",
-        linkLabel: "Visualiser l'historique des activités",
-        entity: "none",
-        feature: "none",
-    },
-
-    {
-        to: "/visualisation-donnees",
-        linkLabel: "Visualiser les données",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "/acces",
-        linkLabel: "Consulter la liste des comptes",
-        entity: "user",
-        feature: "list",
-    },
-    {
-        to: "/nouvel-utilisateur",
-        linkLabel: "Ajouter un nouvel utilisateur",
-        entity: "user",
-        feature: "create",
-    },
-    {
-        to: "/invitation",
-        linkLabel: "Inviter vos contacts sur la plateforme",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "https://resorption-bidonvilles.localhost/stats",
-        linkLabel: "Visualiser les statistiques du site",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "https://resorption-bidonvilles.localhost/mentions-legales",
-        linkLabel: "Visualiser les mentions légales",
-        entity: "none",
-        feature: "none",
-    },
-    {
-        to: "https://resorption-bidonvilles.localhost/conditions-d-utilisation.pdf",
-        linkLabel: "Visualiser les conditions générales d'utilisation",
-        entity: "none",
-        feature: "none",
-    },
-];
+const filteredAndSortedRoutes = router
+    .getRoutes()
+    .reduce((acc, route) => {
+        if (route.meta?.title && route.meta?.displayOrderOnSiteMap !== 0) {
+            const { path, meta } = route;
+            const { title, displayOrderOnSiteMap, permissions } = meta;
+            const permissionsInfo = permissions
+                ? {
+                      entity: permissions[0].split(".")[0],
+                      feature: permissions[0].split(".")[1],
+                  }
+                : { entity: "none", feature: "none" };
+            acc.push({
+                to: path,
+                label: title,
+                displayOrder: displayOrderOnSiteMap || 0, // Utilisation de 0 par défaut si displayOrderOnSiteMap est absent
+                ...permissionsInfo,
+            });
+        }
+        return acc;
+    }, [])
+    .sort((a, b) => a.displayOrder - b.displayOrder);
 
 const userPermissions = computed(() => {
     return userStore.user.permissions;
@@ -172,7 +51,7 @@ const shantytownCreateAllowed =
     userPermissions.value["shantytown"]["create"].allowed;
 
 const liensAutorises = computed(() => {
-    return liens
+    return filteredAndSortedRoutes
         .map((lien) => {
             const entity = lien.entity;
             const feature = lien.feature;
