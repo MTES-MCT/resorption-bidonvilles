eda48322561a8c0199d2947330a0f10731aeb005
Christophe BENARD <ch.benard@gmail.com>
2023-10-20 18:19:29 +0200

    fix: Créer une route "/mentions-legales" dans `router.js` côté webapp refers to: https://trello.com/c/hlJ7ssLm/1997

--- packages/frontend/webapp/src/helpers/router.js
+++ packages/frontend/webapp/src/helpers/router.js
@@ -452,6 +452,14 @@ const router = createRouter({
                 authRequirement: "none",
             },
         },
+        {
+            path: "/mentions-legales",
+            component: () => import("@/views/MentionsLegales.vue"),
+            meta: {
+                title: "Mentions légales",
+                authRequirement: "none",
+            },
+        },
 
         // special
         {
