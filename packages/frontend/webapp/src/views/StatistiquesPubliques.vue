aa719cfd949fefb9c5d6bccd060561412b786b45
Christophe BENARD <ch.benard@gmail.com>
2023-10-20 18:14:38 +0200

    fix: Déplacer `/packages/frontend/www/pages/stats.vue` vers webapp refers to https://trello.com/c/hlJ7ssLm/1997

---
+++ packages/frontend/webapp/src/views/StatistiquesPubliques.vue
@@ -0,0 +1,10 @@
+<template>
+    <Layout>
+        <StatistiquesPubliques />
+    </Layout>
+</template>
+
+<script setup>
+import Layout from "@/components/Layout/Layout.vue";
+import StatistiquesPubliques from "@/components/StatistiquesPubliques/StatistiquesPubliques.vue";
+</script>
