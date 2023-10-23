9320f71edde434366782fc7a02520f299585ff9e
Anis Safine Laget <anis@users.noreply.github.com>
2023-10-23 15:03:23 +0200

    fix: correc° de l'état par défaut du lgPicker

--- packages/frontend/ui/src/components/LanguagePicker.vue
+++ packages/frontend/ui/src/components/LanguagePicker.vue
@@ -1,9 +1,9 @@
 <template>
-  <select @change=" pickLang($event.target.value)"
-    :style="`background-image: url('assets/images/flags/${selectedLang}.svg');`"
-    class="bg-[length:30px] bg-left bg-[.5rem] bg-no-repeat focus:ring-2 ring-offset-2 ring-info bg-white text-lg border-2 border-primary text-primary focus:outline-none p-2 pl-12"
-    name="language" :label="language === 'fr' ? 'Changer la langue' : 'Change language'" :disabled="disabled">
-    <option class="hover:bg-primary" v-for="lang in languages" :key="lang.key" :alt="lang.alt" :value="lang.key"
+  <select @change="pickLang($event.target.value)"
+    :style="`background-image: url('assets/images/flags/${language.toUpperCase()}.svg'); background-position: 0.5rem center`"
+    class="bg-[length:1.9rem] bg-no-repeat focus:ring-2 ring-offset-2 ring-info bg-white text-lg border-2 border-primary text-primary focus:outline-none p-2 pl-12"
+    name="language" label="Change language" :disabled="disabled">
+    <option class="hover:bg-primary" v-for="lang in languages" :key="lang.key" :alt="lang.alt" :value="lang.key" :selected="lang.key === language.toLowerCase()"
       :lang="lang.key" @change="pickLang(lang.key)">
       {{ lang.label }}
     </option>
@@ -11,7 +11,7 @@
 </template>
 
 <script setup>
-import { ref, toRefs } from 'vue';
+import { toRefs } from 'vue';
 
 const props = defineProps({
   language: {
@@ -19,7 +19,6 @@ const props = defineProps({
   },
 });
 const { language } = toRefs(props);
-const selectedLang = ref("FR");
 
 const languages = [
   { "key": "fr", "label": "Français", "alt": "Version Française", },
@@ -28,8 +27,8 @@ const languages = [
   { "key": "bg", "label": "Български", "alt": "Българска версия", },
 ];
 const emit = defineEmits(['update:modelValue']);
+
 function pickLang(lang) {
-  selectedLang.value = lang === "en" ? "UK" : lang.toUpperCase();
   emit('update:modelValue', lang)
 }
 </script>
