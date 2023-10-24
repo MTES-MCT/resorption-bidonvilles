70e32845076b88879d79ac9449fa64aadb291ae1
Anis Safine Laget <anis@users.noreply.github.com>
2023-10-24 15:04:26 +0200

    wording: remplissage de la page 'Désactiver mon compte'

--- packages/frontend/webapp/src/components/ProfilUtilisateur/FormDesactiverCompte/FormDesactiverCompte.vue
+++ packages/frontend/webapp/src/components/ProfilUtilisateur/FormDesactiverCompte/FormDesactiverCompte.vue
@@ -1,3 +1,50 @@
 <template>
-    <p>Hello World</p>
+    <p class="font-bold">
+        Désactiver un compte
+        <span class="font-italic">Résorption-bidonvilles</span>
+    </p>
+    <p>
+        Si vous n'intervenez plus sur la question des bidonvilles et squats en
+        France, vous pouvez alors désactiver votre compte en cliquant sur le
+        bouton ci-dessous.
+    </p>
+    <p class="mt-2">
+        Vous n'aurez plus accès à la plateforme ni ne serez sollicité(e) par
+        courriel.
+    </p>
+    <p class="mt-2 underline">
+        Cette action est réversible uniquement sur demande à votre
+        administrateur local.
+    </p>
+
+    <p class="mt-6">
+        <Button
+            variant="primary"
+            icon="fa-regular fa-trash-alt"
+            iconPosition="left"
+            >{{
+                self ? "Désactiver mon compte" : "Désactiver ce compte"
+            }}</Button
+        >
+    </p>
 </template>
+
+<script setup>
+import { defineProps, toRefs, computed } from "vue";
+import { useUserStore } from "@/stores/user.store";
+
+import { Button } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    user: {
+        type: Object,
+        required: true,
+    },
+});
+const { user } = toRefs(props);
+
+const self = computed(() => {
+    const userStore = useUserStore();
+    return user.value.id === userStore.user?.id;
+});
+</script>
