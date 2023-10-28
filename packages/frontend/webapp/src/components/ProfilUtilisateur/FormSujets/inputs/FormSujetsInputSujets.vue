0d1d82e6d2b72b727407d0ed05d9663f27c20b3f
Anis Safine Laget <anis@users.noreply.github.com>
2023-10-28 12:29:50 +0200

    feature: ajout d'un onglet 'mes sujets' dans 'mon profil'

---
+++ packages/frontend/webapp/src/components/ProfilUtilisateur/FormSujets/inputs/FormSujetsInputSujets.vue
@@ -0,0 +1,31 @@
+<template>
+    <CheckableGroup id="tags" :label="labels.tags">
+        <Checkbox
+            v-for="item in items"
+            :key="item.value"
+            :value="item.value"
+            :label="item.label"
+            variant="checkbox"
+            name="tags"
+            v-model="values.tags"
+        />
+    </CheckableGroup>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useFormValues } from "vee-validate";
+import { CheckableGroup, Checkbox } from "@resorptionbidonvilles/ui";
+import labels from "../FormSujets.labels";
+import { useConfigStore } from "@/stores/config.store";
+
+const configStore = useConfigStore();
+const items = computed(() => {
+    return configStore.config.question_tags.map((tag) => ({
+        value: tag.uid,
+        label: tag.name,
+    }));
+});
+
+const values = useFormValues();
+</script>
