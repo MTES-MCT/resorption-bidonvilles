0d6b238635449ab7803d73ea7c0e9a340f1e64ec
Anis Safine Laget <anis@users.noreply.github.com>
2023-11-06 11:01:12 +0100

    fix: set updated_at quand une mise à jour est faite dans users

--- packages/api/server/models/userModel/resetInactivityAlertSentAt.ts
+++ packages/api/server/models/userModel/resetInactivityAlertSentAt.ts
@@ -2,7 +2,7 @@ import { sequelize } from '#db/sequelize';
 
 export default async (userId: number): Promise<void> => {
     await sequelize.query(
-        'UPDATE users SET inactivity_alert_sent_at = NULL WHERE user_id = :userId',
+        'UPDATE users SET inactivity_alert_sent_at = NULL, updated_at = NOW() WHERE user_id = :userId',
         {
             replacements: { userId },
         },
