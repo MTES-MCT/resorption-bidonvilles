db85059407e973d6df7a62408c697c7bf9ea71be
Anis Safine Laget <anis@users.noreply.github.com>
2023-11-13 17:03:45 +0100

    design: align content of the quick-access nav

--- packages/frontend/ui/src/components/SkipToMainContentLink.vue
+++ packages/frontend/ui/src/components/SkipToMainContentLink.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="bg-G300 p-4 absolute top-0 -translate-y-full focus-within:relative focus-within:translate-y-0">
-        <nav role="navigation" aria-label="Accès rapide">
-            <ul>
-                <li>
-                    <Link to="#contenu-principal" @click="resetFocus">Aller au
-                        contenu
-                        principal</Link>
-                </li>
-            </ul>
-        </nav>
+        <ContentWrapper>
+            <nav role="navigation" aria-label="Accès rapide">
+                <ul>
+                    <li>
+                        <Link to="#contenu-principal" @click="resetFocus">Aller au
+                            contenu
+                            principal</Link>
+                    </li>
+                </ul>
+            </nav>
+        </ContentWrapper>
     </div>
 </template> 
 
 <script setup>
 import { nextTick } from 'vue';
-import { Link } from '@resorptionbidonvilles/ui';
+import { ContentWrapper, Link } from '@resorptionbidonvilles/ui';
 
 function resetFocus() {
     nextTick(() => {
