fff281a27f372e3804158b3427bd16134330bce7
Christophe BENARD <ch.benard@gmail.com>
2023-11-14 15:49:53 +0100

    fix: Corriger nombre de sites habitants et origine refers to https://trello.com/c/iEtzffn4/2030

--- packages/frontend/www/components/LandingPage/SecondSection/LandingPageSecondSection.vue
+++ packages/frontend/www/components/LandingPage/SecondSection/LandingPageSecondSection.vue
@@ -2,14 +2,11 @@
     <div>
         <div class="md:grid md:grid-cols-3 md:gap-16">
             <LandingPageDataBlock :line1="$t('landingPage.secondSection.data.1.line1')"
-                :line2="$t('landingPage.secondSection.data.1.line2')"
-                :line3="$t('landingPage.secondSection.data.1.line3')" />
+                :line2="$t('landingPage.secondSection.data.1.line2')" />
             <LandingPageDataBlock class="my-4 md:my-0" :line1="$t('landingPage.secondSection.data.2.line1')"
-                :line2="$t('landingPage.secondSection.data.2.line2')"
-                :line3="$t('landingPage.secondSection.data.2.line3')" />
+                :line2="$t('landingPage.secondSection.data.2.line2')" />
             <LandingPageDataBlock :line1="$t('landingPage.secondSection.data.3.line1')"
-                :line2="$t('landingPage.secondSection.data.3.line2')"
-                :line3="$t('landingPage.secondSection.data.3.line3')" />
+                :line2="$t('landingPage.secondSection.data.3.line2')" />
         </div>
 
         <div class="max-w-screen-sm mx-auto ">
