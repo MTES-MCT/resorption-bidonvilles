c441e202d1ca50a6c485fa7c1d0d399878b3c141
Christophe BENARD <ch.benard@gmail.com>
2023-11-28 16:47:56 +0100

    feat: Ajout des sujets d'expertises ou d'intérêts dans les users refers to https://trello.com/c/TPgRKFHh/2039

--- packages/api/server/models/userModel/getDirectory.ts
+++ packages/api/server/models/userModel/getDirectory.ts
@@ -25,11 +25,20 @@ type OrganizationRow = {
     user_email: string,
     user_phone: string | null,
     user_position: string,
+    user_expertise_topic_id: string | null,
+    user_expertise_topic_type: string | null,
+    user_expertise_topic_label: string | null,
     user_role_regular: string,
     type_id: number,
     type_category: string,
     type_name: string,
     type_abbreviation: string | null,
+};
+
+type SerializedUserExpertiseTopics = {
+    id: string,
+    type: string,
+    label: string,
 };
 
 type SerializedOrganizationUser = {
@@ -41,6 +50,7 @@ type SerializedOrganizationUser = {
     email: string,
     phone: string | null,
     position: string,
+    expertise_topics: SerializedUserExpertiseTopics[],
 };
 
 export type SerializedOrganization = {
@@ -81,6 +91,16 @@ export type SerializedOrganization = {
 export default async (): Promise<SerializedOrganization[]> => {
     const users: OrganizationRow[] = await sequelize.query(
         `
+        WITH users_with_expertise_topics AS
+        (SELECT
+            utet.fk_user,
+            utet.fk_expertise_topic,
+            utet."type",
+            et."label"
+        FROM 
+            user_to_expertise_topics utet
+        LEFT JOIN
+            expertise_topics et ON et.uid = utet.fk_expertise_topic)
         SELECT
             organizations.organization_id,
             organizations.name,
@@ -104,6 +124,9 @@ export default async (): Promise<SerializedOrganization[]> => {
             users.email AS "user_email",
             users.phone AS "user_phone",
             users.position AS "user_position",
+            user_expertise_topics.fk_expertise_topic AS "user_expertise_topic_id",
+            user_expertise_topics."type" AS "user_expertise_topic_type",
+            user_expertise_topics."label" AS "user_expertise_topic_label",
             user_roles_regular.name AS user_role_regular,
             organizations.fk_type AS "type_id",
             organization_types.fk_category AS "type_category",
@@ -114,6 +137,7 @@ export default async (): Promise<SerializedOrganization[]> => {
         LEFT JOIN organization_types ON organizations.fk_type = organization_types.organization_type_id
         LEFT JOIN roles_regular AS user_roles_regular ON users.fk_role_regular = user_roles_regular.role_id
         LEFT JOIN roles_admin AS user_roles_admin ON users.fk_role = user_roles_admin.role_id
+        LEFT JOIN users_with_expertise_topics AS user_expertise_topics ON users.user_id = user_expertise_topics.fk_user
         WHERE
             organizations.active = TRUE
             AND
@@ -169,7 +193,9 @@ export default async (): Promise<SerializedOrganization[]> => {
         }
 
         if (user.user_id !== null) {
-            hash[user.organization_id].users.push({
+            const existingUser = hash[user.organization_id].users.find(u => u.id === user.user_id);
+
+            const serializedUser: SerializedOrganizationUser = {
                 id: user.user_id,
                 is_admin: user.user_role_admin !== null,
                 role: user.user_role_admin || user.user_role_regular,
@@ -178,9 +204,28 @@ export default async (): Promise<SerializedOrganization[]> => {
                 email: user.user_email,
                 phone: user.user_phone,
                 position: user.user_position,
-            });
+                expertise_topics: [],
+            };
+
+            if (user.user_expertise_topic_id !== null) {
+                serializedUser.expertise_topics.push({
+                    id: user.user_expertise_topic_id,
+                    type: user.user_expertise_topic_type,
+                    label: user.user_expertise_topic_label,
+                });
+            }
+
+            if (!existingUser) {
+                hash[user.organization_id].users.push(serializedUser);
+            } else if (user.user_expertise_topic_id !== null) {
+                // Si le user existe déjà, on merge expertise_topics
+                existingUser.expertise_topics.push({
+                    id: user.user_expertise_topic_id,
+                    type: user.user_expertise_topic_type,
+                    label: user.user_expertise_topic_label,
+                });
+            }
         }
     });
-
     return organizations;
 };
