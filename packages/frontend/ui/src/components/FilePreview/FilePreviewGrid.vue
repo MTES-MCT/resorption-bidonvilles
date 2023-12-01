386fe10382ccab840c1eebfb08cd4e36a29225a8
Christophe BENARD <ch.benard@gmail.com>
2023-12-01 16:39:23 +0100

    fix: Ajouter un attribut `id` à chaque élément `div` (FilePreview) refers to https://trello.com/c/kJtAxqzy/2041

--- packages/frontend/ui/src/components/FilePreview/FilePreviewGrid.vue
+++ packages/frontend/ui/src/components/FilePreview/FilePreviewGrid.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-2 mt-1">
         <FilePreview v-for="(file, index) in files" :key="file.id" :file="file" @delete="$emit('delete', file, index)"
-            :disallowAttachmentsRemoval="disallowAttachmentsRemoval" />
+            :id="index" :disallowAttachmentsRemoval="disallowAttachmentsRemoval" />
     </div>
 </template>
 
