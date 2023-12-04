464b18ada20477539e9c8128c53c5178a7b259f0
Anis Safine Laget <anis@users.noreply.github.com>
2023-12-04 15:14:13 +0100

    feature: distinguer actions en cours/finies sur la fiche d'un site
    refers to https://trello.com/c/TUTQ2dyl/2040

--- packages/api/server/models/actionModel/fetchByShantytown/hashActions.ts
+++ packages/api/server/models/actionModel/fetchByShantytown/hashActions.ts
@@ -5,27 +5,33 @@ export type ActionHash = {
     [key: number]: ShantytownAction
 };
 
-export default (actions: ActionSelectRow[]): ActionHash => actions.reduce((acc, row) => {
-    acc[row.id] = {
-        id: row.id,
-        name: row.name,
-        topics: [],
-        operators: [],
-        shantytowns: row.shantytowns,
-        location: {
-            type: 'departement',
-            city: null,
-            epci: null,
-            departement: {
-                code: row.departement_code,
-                name: row.departement_name,
-            },
-            region: {
-                code: row.region_code,
-                name: row.region_name,
-            },
-        },
-    };
+export default (actions: ActionSelectRow[]): ActionHash => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-    return acc;
-}, {} as ActionHash);
+    return actions.reduce((acc, row) => {
+        acc[row.id] = {
+            id: row.id,
+            name: row.name,
+            topics: [],
+            operators: [],
+            shantytowns: row.shantytowns,
+            location: {
+                type: 'departement',
+                city: null,
+                epci: null,
+                departement: {
+                    code: row.departement_code,
+                    name: row.departement_name,
+                },
+                region: {
+                    code: row.region_code,
+                    name: row.region_name,
+                },
+            },
+            is_ended: row.ended_at !== null && new Date(row.ended_at) < today,
+        };
+
+        return acc;
+    }, {} as ActionHash);
+};
