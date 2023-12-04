93138eb700e8555233d32332ab038fe42709bbd7
Christophe BENARD <ch.benard@gmail.com>
2023-12-04 11:50:12 +0100

    fix: Retirer le picto poubelle et sésactiver le focus en mode "draft" refers to https://trello.com/c/kJtAxqzy/2041

--- packages/frontend/ui/src/components/FilePreview/FilePreview.vue
+++ packages/frontend/ui/src/components/FilePreview/FilePreview.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="inline-block border rounded p-1 flex space-x-2 items-center cursor-pointer hover:bg-blue100 bg-white"
-        tabindex="0" @keyup.enter="displayAttachment(file)" @click="displayAttachment(file)">
+    <div class="inline-block border rounded p-1 flex space-x-2 items-center"
+        :class="[!isStatusDraft ? 'cursor-pointer hover:bg-blue100 bg-white' : '']"
+        :tabindex="`${isStatusDraft ? '-1' : '0'}`" @keyup.enter="displayAttachment(file)" @click="displayAttachment(file)">
         <FilePreviewIcon aria-hidden="true" class="flex-shrink-0" :file="file" />
         <span class="sr-only">Cliquer pour consulter la pièce jointe, {{ file.name }}</span>
         <div class="text-sm overflow-hidden flex-1">
@@ -11,18 +12,13 @@
                 <span :aria-label="`Taille du fichier, ${humanFileSize(file.size)}`">{{ humanFileSize(file.size) }}</span>
             </p>
         </div>
-        <Button
-            :class="(file.state === 'draft' || file.createdByCurrentUser || isHovered) && !disallowAttachmentsRemoval ? 'visible' : 'invisible'"
-            type="button" icon="trash-alt" size="sm" @click.stop="$emit('delete')" variant="primaryOutlineAlt"
-            :title="`Supprimer la pièce jointe, ${file.name}`" />
     </div>
 </template>
 
 <script setup>
-import { ref, toRefs } from 'vue';
+import { ref, toRefs, computed } from 'vue';
 import FilePreviewIcon from "./FilePreviewIcon.vue";
 import humanFileSize from '../../utils/humanFileSize';
-import Button from "../Button.vue";
 import { useNotificationStore } from "@/stores/notification.store";
 
 const props = defineProps({
@@ -43,12 +39,10 @@ const isHovered = ref(false);
 function displayAttachment(file) {
     if (file.state !== 'draft') {
         window.open(file.urls.original);
-    } else {
-        const notificationStore = useNotificationStore();
-        notificationStore.error(
-            "Visualiser une pièce jointe",
-            "Il n'est pas possible de visualiser une pièce-jointe non enregistrée"
-        );
     }
 }
+
+const isStatusDraft = computed(() => {
+    return file.value.state === 'draft';
+});
 </script>
