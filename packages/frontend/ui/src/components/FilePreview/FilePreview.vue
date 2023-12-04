7490274b189a60f4a27f865dad2198ac586f981a
Christophe BENARD <ch.benard@gmail.com>
2023-12-04 09:26:18 +0100

    fix: Rendre le composant accessible refers to https://trello.com/c/kJtAxqzy/2041

--- packages/frontend/ui/src/components/FilePreview/FilePreview.vue
+++ packages/frontend/ui/src/components/FilePreview/FilePreview.vue
@@ -1,15 +1,21 @@
 <template>
-    <a class="inline-block border rounded p-1 flex space-x-2 items-center cursor-pointer hover:bg-blue100 bg-white"
-        @mousemove="isHovered = true" @mouseleave="isHovered = false" :title="file.name" :href="file.urls.original">
-        <FilePreviewIcon class="flex-shrink-0" :file="file" />
+    <div class="inline-block border rounded p-1 flex space-x-2 items-center cursor-pointer hover:bg-blue100 bg-white"
+        tabindex="0" @keyup.enter="displayAttachment(file)" @click="displayAttachment(file)">
+        <FilePreviewIcon aria-hidden="true" class="flex-shrink-0" :file="file" />
+        <span class="sr-only">Cliquer pour consulter la pièce jointe, {{ file.name }}</span>
         <div class="text-sm overflow-hidden flex-1">
-            <p class="truncate">{{ file.name }}</p>
-            <p class="text-G500"><span>{{ file.extension?.toUpperCase() }}</span> {{ humanFileSize(file.size) }}</p>
+            <p aria-hidden="true" class="truncate">{{ file.name }}</p>
+            <p class="text-G500">
+                <span :aria-label="`Extension du fichier, ${file.extension?.toUpperCase()}`">{{
+                    file.extension?.toUpperCase() }}</span> -
+                <span :aria-label="`Taille du fichier, ${humanFileSize(file.size)}`">{{ humanFileSize(file.size) }}</span>
+            </p>
         </div>
         <Button
             :class="(file.state === 'draft' || file.createdByCurrentUser || isHovered) && !disallowAttachmentsRemoval ? 'visible' : 'invisible'"
-            type="button" icon="trash-alt" size="sm" @click.prevent="$emit('delete')" variant="primaryOutlineAlt" />
-    </a>
+            type="button" icon="trash-alt" size="sm" @click.stop="$emit('delete')" variant="primaryOutlineAlt"
+            :title="`Supprimer la pièce jointe, ${file.name}`" />
+    </div>
 </template>
 
 <script setup>
@@ -17,6 +23,7 @@ import { ref, toRefs } from 'vue';
 import FilePreviewIcon from "./FilePreviewIcon.vue";
 import humanFileSize from '../../utils/humanFileSize';
 import Button from "../Button.vue";
+import { useNotificationStore } from "@/stores/notification.store";
 
 const props = defineProps({
     file: {
@@ -32,4 +39,16 @@ const props = defineProps({
 const { disallowAttachmentsRemoval, file } = toRefs(props);
 
 const isHovered = ref(false);
+
+function displayAttachment(file) {
+    if (file.state !== 'draft') {
+        window.open(file.urls.original);
+    } else {
+        const notificationStore = useNotificationStore();
+        notificationStore.error(
+            "Visualiser une pièce jointe",
+            "Il n'est pas possible de visualiser une pièce-jointe non enregistrée"
+        );
+    }
+}
 </script>
