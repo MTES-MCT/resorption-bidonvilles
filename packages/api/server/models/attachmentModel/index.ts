c75d060e60e1d82dc4718af52611736b0b046e45
Christophe BENARD <ch.benard@gmail.com>
2023-12-14 11:19:28 +0100

    feat: Supprimer un attachment de la table intermédiaire refers to https://trello.com/c/2tHmzS1I/2054

--- packages/api/server/models/attachmentModel/index.ts
+++ packages/api/server/models/attachmentModel/index.ts
@@ -1,11 +1,13 @@
 import createLinkedAttachment from './createLinkedAttachment';
 import deleteAttachment from './deleteAttachment';
+import deleteLinkedAttachment from './deleteLinkedAttachment';
 import findKeys from './findKeys';
 import serializeAttachment from './serializeAttachment';
 
 export default {
     createLinkedAttachment,
     deleteAttachment,
+    deleteLinkedAttachment,
     findKeys,
     serializeAttachment,
 };
