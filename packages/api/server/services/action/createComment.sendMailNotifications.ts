660cbf7ec4ca534b4fdec9d0f0d77e24f87b779f
Anis SAFINE LAGET <anis.safine@beta.gouv.fr>
2023-12-21 11:27:14 +0100

    fix: correc° d'une erreur de typage

--- packages/api/server/services/action/createComment.sendMailNotifications.ts
+++ packages/api/server/services/action/createComment.sendMailNotifications.ts
@@ -3,10 +3,7 @@ import userModel from '#server/models/userModel/index';
 import Action, { Comment } from '#root/types/resources/Action.d';
 
 export default async (action: Action, comment: Comment): Promise<number> => {
-    const observers = await userModel.getActionObservers(
-        action.id,
-        comment.id,
-    );
+    const observers = await userModel.getActionObservers(action.id);
 
     if (observers.length > 0) {
         await Promise.all(
