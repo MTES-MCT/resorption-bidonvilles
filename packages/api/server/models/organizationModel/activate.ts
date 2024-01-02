6143e0568b3d1c21c9e3bfde5c2a3497e21d95ba
Anis SAFINE LAGET <anis.safine@beta.gouv.fr>
2024-01-02 16:16:20 +0100

    fix: correc° d'une erreur TS (argument manquant)

--- packages/api/server/models/organizationModel/activate.ts
+++ packages/api/server/models/organizationModel/activate.ts
@@ -1,11 +1,13 @@
 import { sequelize } from '#db/sequelize';
+import { Transaction } from 'sequelize';
 
-export default async (organizationId: number): Promise<void> => {
+export default async (organizationId: number, transaction?: Transaction): Promise<void> => {
     await sequelize.query(
         'UPDATE organizations SET active = TRUE, updated_at = NOW() WHERE organization_id = :organizationId', {
             replacements: {
                 organizationId,
             },
+            transaction,
         },
     );
 };
