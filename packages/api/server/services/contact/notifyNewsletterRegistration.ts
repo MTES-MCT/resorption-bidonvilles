ca17f4a8e95980bc86b93adb44a3615217c62286
Anis SAFINE LAGET <anis.safine@beta.gouv.fr>
2024-01-04 13:03:53 +0100

    chore: mise à jour de Mailjet de v3 à v6

--- packages/api/server/services/contact/notifyNewsletterRegistration.ts
+++ packages/api/server/services/contact/notifyNewsletterRegistration.ts
@@ -1,21 +1,23 @@
 import mailsUtils from '#server/mails/mails';
 import { EmailRecipient } from '#root/types/resources/EmailRecipient.d';
 
-export default (recipient: EmailRecipient): Promise<void> => mailsUtils.sendContactNewsletterRegistration(
-    {
-        email: 'sales@resorption-bidonvilles.beta.gouv.fr',
-        first_name: 'Équipe Sales',
-        last_name: 'Résorption Bidonvilles',
-    },
-    {
-        variables: {
-            email: recipient.email,
+export default async (recipient: EmailRecipient): Promise<void> => {
+    await mailsUtils.sendContactNewsletterRegistration(
+        {
+            email: 'sales@resorption-bidonvilles.beta.gouv.fr',
+            first_name: 'Équipe Sales',
+            last_name: 'Résorption Bidonvilles',
         },
-        preserveRecipient: true,
-        replyTo: {
-            email: recipient.email,
-            last_name: recipient.last_name,
-            first_name: recipient.first_name,
+        {
+            variables: {
+                email: recipient.email,
+            },
+            preserveRecipient: true,
+            replyTo: {
+                email: recipient.email,
+                last_name: recipient.last_name,
+                first_name: recipient.first_name,
+            },
         },
-    },
-);
+    );
+};
