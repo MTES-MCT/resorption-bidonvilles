e930d1d068cb7f4b80ae790bf993649830baee12
Anis SAFINE LAGET <anis.safine@beta.gouv.fr>
2024-01-15 12:12:17 +0100

    feature: adapta° de la vue d'excep° de permi°

--- packages/api/server/controllers/userController/listWithPermissions.ts
+++ packages/api/server/controllers/userController/listWithPermissions.ts
@@ -6,45 +6,32 @@ export default async (req: Request, res: Response, next: NextFunction): Promise<
     try {
         res.status(200).send({
             users: await sequelize.query(
-                `WITH grouped_attachments AS (
-                    SELECT
-                        upa.fk_user_permission,
-                        array_remove(array_agg(upa.fk_region), null) AS regions,
-                        array_remove(array_agg(upa.fk_departement), null) AS departements,
-                        array_remove(array_agg(upa.fk_epci), null) AS epci,
-                        array_remove(array_agg(upa.fk_city), null) AS cities,
-                        array_remove(array_agg(upa.fk_plan), null) AS plans,
-                        array_remove(array_agg(upa.fk_action), null) AS actions
-                    FROM user_permission_attachments upa
-                    GROUP BY upa.fk_user_permission
-                )
-                
-                SELECT
-                    *
-                FROM user_permissions
-                LEFT JOIN users u ON u.user_id = user_permissions.fk_user
-                LEFT JOIN grouped_attachments ga ON ga.fk_user_permission = user_permissions.user_permission_id
-                WHERE user_permissions.fk_user IS NOT NULL
-                
+                `SELECT
+                    permissions.fk_feature,
+                    permissions.fk_entity,
+                    permissions.allowed,
+                    u.user_id,
+                    u.first_name,
+                    u.last_name,
+                    u.email,
+                    u.fk_organization,
+                    u.fk_status,
+                    COUNT(CASE WHEN permissions.type = 'nation' THEN 1 ELSE NULL END) > 0 AS is_national,
+                    array_remove(array_agg(permissions.fk_region), null) AS regions,
+                    array_remove(array_agg(permissions.fk_departement), null) AS departements,
+                    array_remove(array_agg(permissions.fk_epci), null) AS epci,
+                    array_remove(array_agg(permissions.fk_city), null) AS cities
+                FROM permissions
+                LEFT JOIN users u ON u.user_id = permissions.fk_user
+                WHERE permissions.fk_user IS NOT NULL
+                GROUP BY permissions.fk_feature, permissions.fk_entity, permissions.allowed, u.user_id, u.first_name, u.last_name, u.email, u.fk_organization, u.fk_status
                 ORDER BY u.user_id ASC`,
                 {
                     type: QueryTypes.SELECT,
                 },
             ),
             organizations: await sequelize.query(
-                `WITH grouped_attachments AS (
-                    SELECT
-                        upa.fk_user_permission,
-                        array_remove(array_agg(upa.fk_region), null) AS regions,
-                        array_remove(array_agg(upa.fk_departement), null) AS departements,
-                        array_remove(array_agg(upa.fk_epci), null) AS epci,
-                        array_remove(array_agg(upa.fk_city), null) AS cities,
-                        array_remove(array_agg(upa.fk_plan), null) AS plans,
-                        array_remove(array_agg(upa.fk_action), null) AS actions
-                    FROM user_permission_attachments upa
-                    GROUP BY upa.fk_user_permission
-                ),
-                organizations_with_active_users AS (
+                `WITH organizations_with_active_users AS (
                     SELECT
                         o.organization_id,
                         COUNT(u.user_id) AS count
@@ -55,12 +42,23 @@ export default async (req: Request, res: Response, next: NextFunction): Promise<
                 )
 
                 SELECT
-                    *
-                FROM user_permissions
-                LEFT JOIN organizations o ON o.organization_id = user_permissions.fk_organization
+                    permissions.fk_feature,
+                    permissions.fk_entity,
+                    permissions.allowed,
+                    o.organization_id,
+                    o.name,
+                    o.abbreviation,
+                    owau.count,
+                    COUNT(CASE WHEN permissions.type = 'nation' THEN 1 ELSE NULL END) > 0 AS is_national,
+                    array_remove(array_agg(permissions.fk_region), null) AS regions,
+                    array_remove(array_agg(permissions.fk_departement), null) AS departements,
+                    array_remove(array_agg(permissions.fk_epci), null) AS epci,
+                    array_remove(array_agg(permissions.fk_city), null) AS cities
+                FROM permissions
+                LEFT JOIN organizations o ON o.organization_id = permissions.fk_organization
                 LEFT JOIN organizations_with_active_users owau ON owau.organization_id = o.organization_id
-                LEFT JOIN grouped_attachments ga ON ga.fk_user_permission = user_permissions.user_permission_id
-                WHERE user_permissions.fk_organization IS NOT NULL
+                WHERE permissions.fk_organization IS NOT NULL
+                GROUP BY permissions.fk_feature, permissions.fk_entity, permissions.allowed, o.organization_id, o.name, o.abbreviation, owau.count
                 ORDER BY o.organization_id ASC`,
                 {
                     type: QueryTypes.SELECT,
