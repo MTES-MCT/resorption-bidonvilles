726e8054a5b880ea7c7ffd88bcd96cb8cfc4cc5d
Christophe BENARD <ch.benard@gmail.com>
2024-01-16 11:22:48 +0100

    fix: Supprimer une méthode non utile refers to https://trello.com/c/2tHmzS1I/2054

--- packages/api/server/models/attachmentModel/index.ts
+++ packages/api/server/models/attachmentModel/index.ts
@@ -1,13 +1,11 @@
 import createLinkedAttachment from './createLinkedAttachment';
 import deleteAttachment from './deleteAttachment';
-import deleteLinkedAttachment from './deleteLinkedAttachment';
 import findKeys from './findKeys';
 import serializeAttachment from './serializeAttachment';
 
 export default {
     createLinkedAttachment,
     deleteAttachment,
-    deleteLinkedAttachment,
     findKeys,
     serializeAttachment,
 };
