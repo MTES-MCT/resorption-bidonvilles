3aa080583dcb223cd0d769a7737262df66f50d13
Christophe BENARD <ch.benard@gmail.com>
2024-01-16 11:03:55 +0100

    feat: Appel aux stores pour supprimer les pièces jointes (reponses) ref https://trello.com/c/2tHmzS1I/2054

--- packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionAttachments/FicheQuestionAttachments.vue
+++ packages/frontend/webapp/src/components/FicheQuestion/FicheQuestionAttachments/FicheQuestionAttachments.vue
@@ -1,13 +1,39 @@
 <template>
-    <FilePreviewList :files="attachments" collapsedByDefault />
+    <FilePreviewList
+        :files="attachments"
+        collapsedByDefault
+        @delete="handleDeleteAttachment"
+    />
 </template>
 
 <script setup>
 import { toRefs } from "vue";
 import { FilePreviewList } from "@resorptionbidonvilles/ui";
+import { useQuestionsStore } from "@/stores/questions.store";
 
 const props = defineProps({
+    entityId: Number,
     attachments: Array,
 });
-const { attachments } = toRefs(props);
+const { entityId, attachments } = toRefs(props);
+const questionsStore = useQuestionsStore();
+
+async function handleDeleteAttachment(file) {
+    // emit("deleteAttachment", file, index);
+    console.log(
+        "handleDeleteAttachment" +
+            "\n entity_type: " +
+            "question" +
+            "\n entityId: " +
+            entityId.value +
+            "\n attachmentId: " +
+            file.id +
+            "\n" +
+            " from FicheQuestionAttachments.vue"
+    );
+    console.log(
+        "Appel de questionStore.deleteAttachment from FicheQuestionAttachments.vue"
+    );
+    await questionsStore.deleteQuestionAttachment(entityId.value, file.id);
+}
 </script>
