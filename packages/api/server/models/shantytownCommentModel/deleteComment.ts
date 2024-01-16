04299b4dee2531017b29de4f39497f41d1a7f159
Christophe BENARD <ch.benard@gmail.com>
2024-01-16 16:09:50 +0100

    fix: Ajouter les types dans le modèle

--- packages/api/server/models/shantytownCommentModel/deleteComment.ts
+++ packages/api/server/models/shantytownCommentModel/deleteComment.ts
@@ -1,9 +1,9 @@
 import { sequelize } from '#db/sequelize';
 
 /**
- * @param {Number} id A shantytown_comment_id
+ * @param {Number} commentId
  */
-export default async (commentId) => {
+export default async (commentId: number): Promise<void> => {
     await sequelize.query(
         'DELETE FROM shantytown_comments WHERE shantytown_comment_id = :id',
         {
