9584e99555e53f51fb2ebc19fcd552a50c63c009
Anis SAFINE LAGET <anis.safine@beta.gouv.fr>
2024-01-16 16:01:09 +0100

    design: améliora° du design des courbes

--- packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/utils/GraphiquesDonneesStatistiques/generateDataset.js
+++ packages/frontend/webapp/src/components/DonneesStatistiquesDepartement/utils/GraphiquesDonneesStatistiques/generateDataset.js
@@ -10,11 +10,14 @@ function setBackgroundColor(context, bgColor, start = 1) {
 
     const gradientBg = ctx.createLinearGradient(0, top, 0, bottom);
     if (start < 1) {
-        gradientBg.addColorStop(1, "transparent");
-        gradientBg.addColorStop(1 - start - 0.01, "transparent");
+        gradientBg.addColorStop(1, "rgba(255, 255, 255, 0)");
+        gradientBg.addColorStop(1 - start - 0.01, "rgba(255, 255, 255, 0)");
     }
     gradientBg.addColorStop(1 - start, bgColor);
-    gradientBg.addColorStop(Math.min(1, 1 - start + 0.7), "transparent");
+    gradientBg.addColorStop(
+        Math.min(1, 1 - start + 0.7),
+        "rgba(255, 255, 255, 0)"
+    );
     return gradientBg;
 }
 
