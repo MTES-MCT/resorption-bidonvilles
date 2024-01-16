e63fd9d51a61bf28498ff76db53a31bfbc723a06
Gilles COGNIN <gillescognin@gmail.com>
2024-01-16 13:33:32 +0100

    feat: Amélioration du traitement du texte de la modale

--- packages/frontend/webapp/src/components/ModaleModerationQuestion/ModaleModerationQuestion.vue
+++ packages/frontend/webapp/src/components/ModaleModerationQuestion/ModaleModerationQuestion.vue
@@ -5,9 +5,7 @@
         </template>
 
         <template v-slot:body>
-            Confirmez-vous la suppression de la question de
-            {{ author.first_name }} {{ author.last_name }} ainsi que ses
-            {{ answersCount }} réponses ?
+            {{ wording }}
             <ErrorSummary v-if="error" :message="error" class="mb-0 mt-6" />
         </template>
 
@@ -21,7 +19,7 @@
 </template>
 
 <script setup>
-import { defineProps, toRefs, ref } from "vue";
+import { defineProps, toRefs, ref, computed } from "vue";
 import { Button, ErrorSummary, Modal } from "@resorptionbidonvilles/ui";
 
 const props = defineProps({
@@ -37,6 +35,14 @@ const { answersCount, author } = toRefs(props);
 const loading = ref(false);
 const error = ref(null);
 const isOpen = ref(false);
+
+const wording = computed(() => {
+    const baseWording = `Confirmez-vous la suppression de la question de
+            ${author.value.first_name} ${author.value.last_name}`;
+    return answersCount.value === 0
+        ? `${baseWording} ?`
+        : `${baseWording} ainsi que les ${answersCount.value} réponse(s) associée(s)?`;
+});
 
 function reset() {
     loading.value = false;
