09be29385d35d354b3263a99e95cd537370b7b49
Christophe BENARD <ch.benard@gmail.com>
2024-01-16 11:24:33 +0100

    fix: Ajouter un code erreur en cas d'échec de suppression de PJ (bucket) refers to https://trello.com/c/2tHmzS1I/2054

--- packages/api/server/controllers/attachmentController/deleteAttachment.ts
+++ packages/api/server/controllers/attachmentController/deleteAttachment.ts
@@ -2,6 +2,7 @@ import attachmentService from '#server/services/attachment';
 
 const ERROR_RESPONSES = {
     delete_failed: { code: 500, message: 'Une erreur est survenue lors de l\'écriture en base de données' },
+    bucket_delete_failed: { code: 500, message: 'Une erreur est survenue lors de la suppression de la pièce-jointe' },
     undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
 };
 
