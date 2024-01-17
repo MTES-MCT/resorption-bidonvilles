a1a220facf9f066ccf4a84eeece792025ac6010e
Gilles COGNIN <gillescognin@gmail.com>
2024-01-17 17:05:14 +0100

    feat: Ajout du validator pour la suppression de question

---
+++ packages/api/server/middlewares/validators/question/delete.ts
@@ -0,0 +1,22 @@
+/* eslint-disable newline-per-chained-call */
+import { Meta, param } from 'express-validator';
+import findOne from '#server/models/questionModel/findOne';
+
+
+export default [
+    param('id')
+        .custom(async (value:string, { req }:Meta):Promise<void> =>  {
+            let question:object;
+            try {
+                question = await findOne(parseInt(value, 10));
+            } catch (error) {
+                throw new Error('Une erreur de lecture en base de données est survenue');
+            }
+
+            if (question === null) {
+                throw new Error('La question à supprimer n\'existe pas');
+            }
+
+            req.question = question;
+        }),
+];
