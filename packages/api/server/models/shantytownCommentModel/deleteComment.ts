994587b331708c51ffb29397ab6442fd99f85ee8
Christophe BENARD <ch.benard@gmail.com>
2024-01-22 11:47:58 +0100

    feat: Appel d'un nouveau service deleteAttachments refers to https://trello.com/c/2tHmzS1I/2054

--- packages/api/server/models/shantytownCommentModel/deleteComment.ts
+++ packages/api/server/models/shantytownCommentModel/deleteComment.ts
@@ -1,15 +1,18 @@
+import { Transaction, QueryTypes } from 'sequelize';
 import { sequelize } from '#db/sequelize';
 
 /**
  * @param {Number} commentId
  */
-export default async (commentId: number): Promise<void> => {
+export default async (commentId: number, transaction: Transaction): Promise<void> => {
     await sequelize.query(
         'DELETE FROM shantytown_comments WHERE shantytown_comment_id = :id',
         {
+            type: QueryTypes.DELETE,
             replacements: {
                 id: commentId,
             },
+            transaction,
         },
     );
 };
