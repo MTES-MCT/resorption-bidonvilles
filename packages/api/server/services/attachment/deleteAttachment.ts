ccd2f3c5b13dd1b49b0a749d4f67054d8b22f80d
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-01-23 11:53:26 +0100

    feat: simplifica° du service de suppression d'un attachment

--- packages/api/server/services/attachment/deleteAttachment.ts
+++ packages/api/server/services/attachment/deleteAttachment.ts
@@ -1,35 +1,11 @@
 import { AttachmentKeys } from '#server/models/attachmentModel/findKeys';
 import attachmentModel from '#server/models/attachmentModel';
 import ServiceError from '#server/errors/ServiceError';
-import { S3 } from '#server/utils/s3';
-import config from '#server/config';
-import { DeleteObjectCommand } from '@aws-sdk/client-s3';
 
 export default async (keys: AttachmentKeys): Promise<void> => {
     try {
         await attachmentModel.deleteAttachment(keys.attachment_id);
     } catch (e) {
         throw new ServiceError('delete_failed', e);
-    }
-
-    try {
-        const promises = [
-            S3.send(new DeleteObjectCommand({
-                Bucket: config.S3.bucket,
-                Key: keys.original_file_key,
-            })),
-        ];
-        if (keys.preview_file_key) {
-            promises.push(
-                S3.send(new DeleteObjectCommand({
-                    Bucket: config.S3.bucket,
-                    Key: keys.preview_file_key,
-                })),
-            );
-        }
-
-        await Promise.all(promises);
-    } catch (error) {
-        // ignore
     }
 };
