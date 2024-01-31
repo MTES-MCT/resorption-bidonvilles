655f91e2b73f588b6f79a4ad2826ba757d334060
Christophe BENARD <ch.benard@gmail.com>
2024-01-31 17:42:16 +0100

    fix: ajouter le rendu audio des conditions de vie refers to https://trello.com/c/GKXgOuis/2048

--- packages/frontend/webapp/src/components/CarteSiteDetaillee/CarteSiteDetailleeLivingConditions.vue
+++ packages/frontend/webapp/src/components/CarteSiteDetaillee/CarteSiteDetailleeLivingConditions.vue
@@ -1,34 +1,71 @@
 <template>
+    <span class="sr-only">Statut des conditions de vie</span>
     <ul v-if="shantytown.livingConditions.version === 2">
         <CarteSiteDetailleeLivingConditionIcon
             :status="shantytown.livingConditions.water.status.status"
+            :audio="
+                getLibelleAudio(
+                    'water',
+                    shantytown.livingConditions.water.status.status
+                )
+            "
             >eau</CarteSiteDetailleeLivingConditionIcon
         >
         <CarteSiteDetailleeLivingConditionIcon
             :status="shantytown.livingConditions.sanitary.status.status"
+            :audio="
+                getLibelleAudio(
+                    'sanitary',
+                    shantytown.livingConditions.sanitary.status.status
+                )
+            "
             >toilettes</CarteSiteDetailleeLivingConditionIcon
         >
         <CarteSiteDetailleeLivingConditionIcon
             :status="shantytown.livingConditions.electricity.status.status"
+            :audio="
+                getLibelleAudio(
+                    'electricity',
+                    shantytown.livingConditions.electricity.status.status
+                )
+            "
             >électricité</CarteSiteDetailleeLivingConditionIcon
         >
 
         <CarteSiteDetailleeLivingConditionIcon
             :status="shantytown.livingConditions.trash.status.status"
-            >évac. des déchets</CarteSiteDetailleeLivingConditionIcon
+            :audio="
+                getLibelleAudio(
+                    'trash',
+                    shantytown.livingConditions.trash.status.status
+                )
+            "
+            >évacuation des déchets</CarteSiteDetailleeLivingConditionIcon
         >
 
         <CarteSiteDetailleeLivingConditionIcon
             :status="shantytown.livingConditions[verminKey].status.status"
+            :audio="
+                getLibelleAudio(
+                    'vermin',
+                    shantytown.livingConditions[verminKey].status.status
+                )
+            "
             >{{
                 shantytown.livingConditions[verminKey].status.status === "good"
-                    ? "abs. de nuisibles"
-                    : "pres. de nuisibles"
+                    ? "absence de nuisibles"
+                    : "presence de nuisibles"
             }}
         </CarteSiteDetailleeLivingConditionIcon>
         <CarteSiteDetailleeLivingConditionIcon
             :status="shantytown.livingConditions[fireKey].status.status"
-            >prev. incendie</CarteSiteDetailleeLivingConditionIcon
+            :audio="
+                getLibelleAudio(
+                    'firePrevention',
+                    shantytown.livingConditions[fireKey].status.status
+                )
+            "
+            >prévention incendie</CarteSiteDetailleeLivingConditionIcon
         >
     </ul>
     <div v-else>
@@ -59,4 +96,35 @@ const fireKey = computed(() => {
         ? "fire_prevention"
         : "firePrevention";
 });
+function getLibelleAudio(key, status) {
+    const titles = {
+        water: "Eau",
+        electricity: "Electricité",
+        sanitary: "Toilettes",
+        trash: "Evacuation des déchets",
+        vermin: "",
+        firePrevention: "",
+    };
+
+    const audio = {
+        vermin: {
+            good: "Absence de nuisibles",
+            bad: "Présence de nuisibles",
+        },
+        firePrevention: {
+            good: "Plan de prévention incendie existant",
+            bad: "Pas de plan de prévention incendie",
+            unknown: "Existence d'un plan de prévention incendie inconnue",
+        },
+        default: {
+            good: "Bonnes conditions d'accès",
+            bad: "Mauvaises conditions d'accès",
+            unknown: "Conditions d'accès inconnues",
+            toImprove: "Conditions d'accès à améliorer",
+        },
+    };
+    return Object.prototype.hasOwnProperty.call(audio, key)
+        ? titles[key] + " " + audio[key][status]
+        : titles[key] + ", " + audio.default[status];
+}
 </script>
