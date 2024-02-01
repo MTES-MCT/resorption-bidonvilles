05fc378af89fe0b99ab88c64bc7167c5e844699c
Gilles COGNIN <gillescognin@gmail.com>
2024-02-01 12:51:17 +0100

    feat: Ajout d'un style spécifique "disabled"

--- packages/frontend/ui/src/components/Input/utils/getInputClasses.js
+++ packages/frontend/ui/src/components/Input/utils/getInputClasses.js
@@ -1,35 +1,40 @@
 // Common classes between TextInput / TextArea / Select
 export default function getInputClasses(variant, inputOptions = {}) {
-    const { error, prefixIcon, suffixIcon, clear, size } = inputOptions;
+    const { error, prefixIcon, suffixIcon, clear, size, disabled } = inputOptions;
 
     return {
         state: [
             "bg-G200 border-b-2 border-black rounded rounded-b-none w-full py-2 placeholder-gray-700",
             error && "border-error",
             prefixIcon && "pl-10",
-            suffixIcon && "pr-10"
+            suffixIcon && "pr-10",
+            disabled && "text-gray-400 bg-G200 py-1 px-2 w-full outline-none",
         ],
         classic: [
             "border-1 border-G200 rounded-md w-full py-2 px-4 outline-none focus:border-info placeholder-gray-700",
             error && "border-error",
             prefixIcon && "pl-10",
-            suffixIcon && "pr-10"
+            suffixIcon && "pr-10",
+            disabled && "text-gray-400 bg-G200 py-1 px-2 w-full outline-none",
         ],
         default: [
             "border-2 border-G500 py-1 px-2 w-full outline-none focus:border-info hover:border-blue400 placeholder-gray-700",
             size === "sm" ? "text-xs" : "",
             prefixIcon && "pl-10",
             suffixIcon && "pr-10",
-            clear && "pr-20"
+            clear && "pr-20",
+            disabled && "text-gray-400 bg-G200 py-1 px-2 w-full outline-none",
         ],
         filter: [
             "border border-primary px-2 w-full rounded outline-none leading-7 hover placeholder-gray-700",
             prefixIcon && "pl-10",
-            suffixIcon && "pr-10"
+            suffixIcon && "pr-10",
+            disabled && "text-gray-400 bg-G200 py-1 px-2 w-full outline-none",
         ],
         minimal: [
             "border border-white py-1 px-1 outline-none focus:border-info hover:border-blue400 placeholder-gray-700",
             size === "sm" ? "text-xs" : "",
+            disabled && "text-gray-400 bg-G200 py-1 px-2 w-full outline-none",
         ],
     }[variant];
 }
