975d777290fb0b1b501909734734f098b43e3f17
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-05 17:05:28 +0100

    fix: correc° du chargement de matomo

--- packages/frontend/webapp/src/helpers/matomo.js
+++ packages/frontend/webapp/src/helpers/matomo.js
@@ -12,26 +12,26 @@ export function useMatomo(app, router) {
         return;
     }
 
+    // Si on utilise le serveur matomo DNUM, il faut modifier l'URL
+    // avant l'envoi à Matomo pour rester compatible avec Xiti
+    // Le séparateur original "/" est remplacé par "::"
+    if (
+        typeof MATOMO.DESCRIPTION_PAGE_SEPARATOR !== "undefined" &&
+        MATOMO.DESCRIPTION_PAGE_SEPARATOR !== "/"
+    ) {
+        router.beforeTrack = (to) => {
+            const modifiedPath = to.fullPath.replace(
+                /\//g,
+                MATOMO.DESCRIPTION_PAGE_SEPARATOR
+            );
+            return { ...to, fullPath: modifiedPath };
+        };
+    }
+
     app.use(VueMatomo, {
         host: MATOMO.HOST,
         siteId: MATOMO.SITE_ID,
-        // Si on utilise le serveur matomo DNUM, il faut modifier l'URL
-        // avant l'envoi à Matomo pour rester compatible avec Xiti
-        // Le séparateur original "/" est remplacé par "::"
-        router:
-            typeof MATOMO.DESCRIPTION_PAGE_SEPARATOR !== "undefined" &&
-            MATOMO.DESCRIPTION_PAGE_SEPARATOR !== "/"
-                ? // MATOMO.DESCRIPTION_PAGE_SEPARATOR est initialisée avec une valeur
-                  {
-                      beforeTrack(to) {
-                          const modifiedPath = to.fullPath.replace(
-                              /\//g,
-                              MATOMO.DESCRIPTION_PAGE_SEPARATOR
-                          );
-                          return { ...to, fullPath: modifiedPath };
-                      },
-                  }
-                : router,
+        router,
         trackInitialView: false,
         cookieDomain: `*.${MATOMO.DOMAIN}`,
         domains: `*.${MATOMO.DOMAIN}`,
