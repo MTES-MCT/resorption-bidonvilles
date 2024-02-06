e7dafed9e8f554bd021e62d3ed52943610eddd50
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-06 22:27:48 +0100

    feat: typage + gestion de transac° dans org.findByIds

--- packages/api/server/models/organizationModel/findOneById.ts
+++ packages/api/server/models/organizationModel/findOneById.ts
@@ -1,7 +1,8 @@
+import { Transaction } from 'sequelize';
 import findByIds from './findByIds';
 import { Organization } from '#root/types/resources/Organization.d';
 
-export default async (id: number): Promise<Organization | null> => {
-    const result = await findByIds([id]);
+export default async (id: number, transaction?: Transaction): Promise<Organization | null> => {
+    const result = await findByIds([id], transaction);
     return result.length === 1 ? result[0] : null;
 };
