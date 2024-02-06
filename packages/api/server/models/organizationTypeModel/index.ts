edd9157e20031105e400164d4270681c6fdfe65d
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-06 22:31:08 +0100

    feat: création d'un modèle orgType.findAll()

--- packages/api/server/models/organizationTypeModel/index.ts
+++ packages/api/server/models/organizationTypeModel/index.ts
@@ -1,8 +1,10 @@
+import findAll from './findAll';
 import findByCategory from './findByCategory';
 import findOneById from './findOneById';
 import findRoleByOrganizationId from './findRoleByOrganizationId';
 
 export default {
+    findAll,
     findByCategory,
     findOneById,
     findRoleByOrganizationId,
