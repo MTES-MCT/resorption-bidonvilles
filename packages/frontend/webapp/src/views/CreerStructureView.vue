f4672932ec73ba8bc4455be246fe2d7e2e81bdef
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-06 22:47:54 +0100

    feat: créa° d'un formulaire de créa° de structure

---
+++ packages/frontend/webapp/src/views/CreerStructureView.vue
@@ -0,0 +1,36 @@
+<template>
+    <LayoutForm size="large">
+        <template v-slot:title>Créer une nouvelle structure</template>
+        <template v-slot:buttons>
+            <Button variant="primaryOutline" type="button" @click="back"
+                >Annuler</Button
+            >
+            <Button @click="submit" :loading="form?.isSubmitting"
+                >Créer la structure</Button
+            >
+        </template>
+
+        <ContentWrapper size="large">
+            <FormCreerStructure ref="form" />
+        </ContentWrapper>
+    </LayoutForm>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import router from "@/helpers/router";
+
+import { Button, ContentWrapper } from "@resorptionbidonvilles/ui";
+import LayoutForm from "@/components/LayoutForm/LayoutForm.vue";
+import FormCreerStructure from "@/components/FormCreerStructure/FormCreerStructure.vue";
+
+const form = ref(null);
+
+function back() {
+    router.back();
+}
+
+function submit(...args) {
+    form.value.submit(...args);
+}
+</script>
