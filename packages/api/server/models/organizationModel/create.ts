81572449f9769f9a9a1b63dd8f6ef05c3c818d19
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-07 14:26:53 +0100

    fix: ajout d'un rollback manquant d'une transaction

--- packages/api/server/models/organizationModel/create.ts
+++ packages/api/server/models/organizationModel/create.ts
@@ -10,57 +10,65 @@ type OrganizationCreateData = {
 };
 
 export default async (createdBy: number, data: OrganizationCreateData, argTransaction?: Transaction): Promise<number> => {
-    const isExternalTransaction = argTransaction !== undefined;
+    const isLocalTransaction = argTransaction === undefined;
 
     let transaction: Transaction = argTransaction;
-    if (!isExternalTransaction) {
+    if (isLocalTransaction) {
         transaction = await sequelize.transaction();
     }
 
-    const response = await sequelize.query(
-        `INSERT INTO
-            organizations(name, abbreviation, active, fk_type, created_by)
-        VALUES
-            (:name, :abbreviation, TRUE, :type, :createdBy)
-        RETURNING organization_id`,
-        {
-            replacements: {
-                name: data.name,
-                abbreviation: data.abbreviation,
-                type: data.type,
-                createdBy,
-            },
-            transaction,
-        },
-    );
-
-    type ReturnValue = { organization_id: number };
-    const rows: ReturnValue[] = (response[0] as unknown) as ReturnValue[];
-    const { organization_id: organizationId } = rows[0];
-
-    await Promise.all(
-        data.intervention_areas.map(({ type, code }) => sequelize.query(
+    try {
+        const response = await sequelize.query(
             `INSERT INTO
-                intervention_areas(fk_organization, type, is_main_area, fk_region, fk_departement, fk_epci, fk_city)
+                organizations(name, abbreviation, active, fk_type, created_by)
             VALUES
-                (:organizationId, :type, TRUE, :region, :departement, :epci, :city)`,
+                (:name, :abbreviation, TRUE, :type, :createdBy)
+            RETURNING organization_id`,
             {
                 replacements: {
-                    organizationId,
-                    type,
-                    region: type === 'region' ? code : null,
-                    departement: type === 'departement' ? code : null,
-                    epci: type === 'epci' ? code : null,
-                    city: type === 'city' ? code : null,
+                    name: data.name,
+                    abbreviation: data.abbreviation,
+                    type: data.type,
+                    createdBy,
                 },
                 transaction,
             },
-        )),
-    );
+        );
 
-    if (!isExternalTransaction) {
-        await transaction.commit();
+        type ReturnValue = { organization_id: number };
+        const rows: ReturnValue[] = (response[0] as unknown) as ReturnValue[];
+        const { organization_id: organizationId } = rows[0];
+
+        await Promise.all(
+            data.intervention_areas.map(({ type, code }) => sequelize.query(
+                `INSERT INTO
+                    intervention_areas(fk_organization, type, is_main_area, fk_region, fk_departement, fk_epci, fk_city)
+                VALUES
+                    (:organizationId, :type, TRUE, :region, :departement, :epci, :city)`,
+                {
+                    replacements: {
+                        organizationId,
+                        type,
+                        region: type === 'region' ? code : null,
+                        departement: type === 'departement' ? code : null,
+                        epci: type === 'epci' ? code : null,
+                        city: type === 'city' ? code : null,
+                    },
+                    transaction,
+                },
+            )),
+        );
+
+        if (isLocalTransaction) {
+            await transaction.commit();
+        }
+
+        return organizationId;
+    } catch (error) {
+        if (isLocalTransaction) {
+            await transaction.rollback();
+        }
+
+        throw error;
     }
-
-    return organizationId;
 };
