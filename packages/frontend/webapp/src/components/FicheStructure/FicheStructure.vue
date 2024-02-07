1088c4e0cce1ddaf41c2547445b4a719e6f09719
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-07 22:36:46 +0100

    feat: ajout d'un filtre par expertise sur fiche structure

--- packages/frontend/webapp/src/components/FicheStructure/FicheStructure.vue
+++ packages/frontend/webapp/src/components/FicheStructure/FicheStructure.vue
@@ -12,23 +12,85 @@
         </ViewHeader>
 
         <FicheStructureInfos :organization="organization" />
+        <FicheStructureFiltres v-model="expertiseTopicsFilter" class="mb-6" />
+        <div
+            v-if="
+                expertiseTopicsFilter.length > 0 &&
+                organization.users.length !== filteredUsers.length
+            "
+            class="flex gap-4 bg-G200 border-l-4 border-blue400 px-4 py-6 mb-6"
+        >
+            <Icon icon="circle-info" />
+            <div class="pt-6">
+                <h3 class="font-bold text-xl">Filtre actif</h3>
+                <p>
+                    Attention, la liste des membres de cette structure est
+                    actuellement filtrée par sujets d'expertise.<br />
+                    <template
+                        v-if="
+                            organization.users.length - filteredUsers.length > 1
+                        "
+                    >
+                        <span class="font-bold"
+                            >{{
+                                organization.users.length - filteredUsers.length
+                            }}
+                            utilisateurs</span
+                        >
+                        sont actuellement masqués.
+                    </template>
+                    <template v-else>
+                        <span class="font-bold">1 utilisateur</span> est
+                        actuellement masqué.
+                    </template>
+                </p>
+                <RbButton
+                    size="sm"
+                    @click="expertiseTopicsFilter = []"
+                    class="mt-4"
+                    >Cliquez ici pour retirer ce filtre</RbButton
+                >
+            </div>
+        </div>
 
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 auto-rows-fr">
+        <div
+            class="grid grid-cols-1 lg:grid-cols-2 gap-4 auto-rows-fr"
+            v-if="filteredUsers.length > 0"
+        >
             <CarteUtilisateur
-                v-for="user in organization.users"
+                v-for="user in filteredUsers"
                 :key="user.id"
                 :user="user"
+                :topics="expertiseTopicsFilter"
             />
         </div>
+        <ViewError v-else variant="vide">
+            <template v-slot:title
+                >Aucun résultat pour cette recherche</template
+            >
+            <template v-slot:code>Filtres actifs</template>
+            <template v-slot:content
+                >Il semblerait qu'il n'existe aucun membre de cette structure
+                répondant à vos critères.</template
+            >
+            <template v-slot:actions>&nbsp;</template>
+        </ViewError>
     </ContentWrapper>
 </template>
 
 <script setup>
-import { defineProps, toRefs } from "vue";
-import { ContentWrapper } from "@resorptionbidonvilles/ui";
+import { computed, ref, toRefs } from "vue";
+import { useDirectoryStore } from "@/stores/directory.store";
+import {
+    ContentWrapper,
+    Icon,
+    Button as RbButton,
+} from "@resorptionbidonvilles/ui";
 import ViewHeader from "@/components/ViewHeader/ViewHeader.vue";
 import CarteUtilisateur from "@/components/CarteUtilisateur/CarteUtilisateur.vue";
+import FicheStructureFiltres from "./FicheStructureFiltres.vue";
 import FicheStructureInfos from "./FicheStructureInfos.vue";
+import ViewError from "@/components/ViewError/ViewError.vue";
 
 const props = defineProps({
     organization: {
@@ -37,4 +99,18 @@ const props = defineProps({
     },
 });
 const { organization } = toRefs(props);
+const directoryStore = useDirectoryStore();
+const expertiseTopicsFilter = ref(directoryStore.filters.expertiseTopics || []);
+
+const filteredUsers = computed(() => {
+    if (expertiseTopicsFilter.value.length === 0) {
+        return organization.value.users;
+    }
+
+    return organization.value.users.filter((user) => {
+        return (user?.expertise_topics || []).some(({ uid }) =>
+            expertiseTopicsFilter.value.includes(uid)
+        );
+    });
+});
 </script>
