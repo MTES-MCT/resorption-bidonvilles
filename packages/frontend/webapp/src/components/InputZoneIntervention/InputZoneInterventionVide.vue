8bbeb8b4203df0d46410d99812fce21abdf8cfb7
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-07 12:01:17 +0100

    tech: découpage fin de InputZoneIntervention

---
+++ packages/frontend/webapp/src/components/InputZoneIntervention/InputZoneInterventionVide.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        class="flex flex-col h-full justify-center items-center text-G500 text-3xl"
+    >
+        <Icon icon="square-dashed-circle-plus" />
+        <p class="text-center">
+            Ajoutez des territoires<br />
+            <span class="text-xl"
+                >Remplissez le champ ci-dessus pour ajouter des
+                territoires</span
+            >
+        </p>
+    </div>
+</template>
+
+<script setup>
+import { Icon } from "@resorptionbidonvilles/ui";
+</script>
