9de7370554bcda0c725672c22a18651bcc6a5b56
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-12 14:57:30 +0100

    chore: retrait d'un fichier non utilisé

--- packages/api/server/models/userModel/getDirectory.ts
+++
@@ -1,5 +0,0 @@
-import { type Transaction } from 'sequelize';
-import find from './_common/find';
-import { Organization } from '#root/types/resources/Organization.d';
-
-export default (transaction?: Transaction): Promise<Organization[]> => find({ activeOnly: true }, transaction);
