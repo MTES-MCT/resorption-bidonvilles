2242cc28e66d3cfaff62bb61a3fc12800952f906
Anis SAFINE LAGET <anis@users.noreply.github.com>
2024-02-12 12:05:36 +0100

    tech: mutualisa° du fetch des structures
        Modification de quelques modèles organization pour retourner un Organization complet plutôt qu'un OrganizationRaw. L'objectif est de disposer d'une fonction modèle capable de retourner un objet organization complet sur la base d'un id.

--- packages/api/server/models/organizationModel/findOneById.ts
+++ packages/api/server/models/organizationModel/findOneById.ts
@@ -1,6 +1,7 @@
 import findByIds from './findByIds';
+import { Organization } from '#root/types/resources/Organization.d';
 
-export default async (id) => {
+export default async (id: number): Promise<Organization | null> => {
     const result = await findByIds([id]);
     return result.length === 1 ? result[0] : null;
 };
