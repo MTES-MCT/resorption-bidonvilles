fc919f21e98382e61bdc7aebab88e10f0a1a4589
Gilles COGNIN <gillescognin@gmail.com>
2024-02-20 12:29:46 +0100

    chore: Suppression des types inutiles

--- packages/api/types/resources/UpdatedQuestion.d.ts
+++
@@ -1,8 +0,0 @@
-import type { QuestionUpdatedBy } from './QuestionUpdatedBy';
-
-export type UpdatedQuestion = {
-    id: number,
-    details: string,
-    updatedAt: Date,
-    updatedBy: QuestionUpdatedBy
-};
