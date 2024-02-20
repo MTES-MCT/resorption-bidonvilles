0e58d63e75828a53c1cc39c82036f28fda5e2ee4
Gilles COGNIN <gillescognin@gmail.com>
2024-02-20 12:30:08 +0100

    fix: Optimisation du controller

--- packages/api/server/controllers/question/update/question.update.ts
+++ packages/api/server/controllers/question/update/question.update.ts
@@ -1,22 +1,27 @@
 import questionService from '#server/services/question';
+import { Request } from 'express';
+import { User } from '#root/types/resources/User.d';
+import { Question } from '#root/types/resources/Question.d';
+
+interface QuestionUpdateRequest extends Request {
+    user: User;
+    question: Question;
+}
 
 const ERRORS = {
     undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
 };
 
-export default async (req, res, next) => {
-    if (req.user.role_id !== 'national_admin') {
-        const question = await questionService.findOne(req.body.question_id);
-        if (question.createdBy.id !== req.user.id) {
-            return res.status(403).send({
-                user_message: 'Vous n\'avez pas les droits suffisants pour modifier cette question',
-            });
-        }
+export default async (req: QuestionUpdateRequest, res, next) => {
+    if (req.user.role_id !== 'national_admin' && req.question.createdBy.id !== req.user.id) {
+        return res.status(403).send({
+            user_message: 'Vous n\'avez pas les droits suffisants pour modifier cette question',
+        });
     }
 
     try {
         const updateQuestion = await questionService.update(
-            req.params.id,
+            req.question.id,
             req.body,
             req.user.id,
         );
