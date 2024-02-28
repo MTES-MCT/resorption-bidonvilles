8d23407efce3f38ab1012044c6ead0c407027da8
Christophe BENARD <ch.benard@gmail.com>
2024-02-28 17:12:36 +0100

    fix: corriger l'encart sur les risques d'usurpation refers to https://trello.com/c/OS2yP2R3/2147

--- packages/frontend/webapp/src/components/FicheAcces/FicheAccesBody/FicheAccesBodyWarning.vue
+++ packages/frontend/webapp/src/components/FicheAcces/FicheAccesBody/FicheAccesBodyWarning.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="mt-6 bg-yellow-200 py-6 px-8">
         <Icon icon="triangle-exclamation" /> N'envoyez
-        <span class="font-bold underline">jamais</span> un accès tant que vous
-        avez un doute sur l’identité de la personne.
+        <span class="font-bold underline">jamais</span> un accès si vous avez un
+        doute sur l'identité de la personne pour éviter tout risque d'usurpation
+        d'identité.
+        <Link
+            withStyle
+            @click="openGuide('/doc/guide_de_l_administrateur.pdf')"
+        >
+            <Icon icon="file-pdf" /> Consulter le guide des accès et de
+            l'administrateur
+        </Link>
     </div>
 </template>
 
 <script setup>
-import { Icon } from "@resorptionbidonvilles/ui";
+import { Icon, Link } from "@resorptionbidonvilles/ui";
+
+function openGuide(link) {
+    window.location = link;
+}
 </script>
