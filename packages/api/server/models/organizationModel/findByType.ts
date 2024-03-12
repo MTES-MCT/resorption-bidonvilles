fe39ea3992763ae46eb0f82f498ca058c86f73a9
Gilles COGNIN <gillescognin@gmail.com>
2024-03-12 17:59:27 +0100

    feat: Modification de la requête pour retourner les structures listées par types de territoires

--- packages/api/server/models/organizationModel/findByType.ts
+++ packages/api/server/models/organizationModel/findByType.ts
@@ -5,25 +5,53 @@ type OrganizationMin = {
     id: number,
     name: string,
     abbreviation: string | null,
-    organization_type_name: string,
+    territory: string | null,
 };
 
-export default (typeId: number): Promise<OrganizationMin[]> => sequelize.query(
+type StructureList = {
+    type: string,
+    organization_type_name: string,
+    organizations: OrganizationMin[],
+};
+
+export default (typeId: number): Promise<StructureList[]> => sequelize.query(
     `SELECT
-        organizations.organization_id AS id,
-        organizations.name,
-        organizations.abbreviation,
-        organization_types.name_singular AS organization_type_name
+        jsonb_build_object(
+            'type', intervention_areas.type,
+            'organization_type_name', organization_types.name_singular,
+            'organizations', jsonb_agg(
+                jsonb_build_object(
+                    'id', organizations.organization_id,
+                    'name', organizations.name,
+                    'abbreviation', organizations.abbreviation,
+                    'territory', CASE
+                        WHEN intervention_areas.type = 'city' THEN cities.fk_departement || ' - ' ||  cities.name
+                        WHEN intervention_areas.type = 'epci' THEN epci.name
+                        WHEN intervention_areas.type = 'departement' THEN departements.code || ' - ' || departements.name
+                        WHEN intervention_areas.type = 'region' THEN regions.name
+                        WHEN intervention_areas.type = 'nation' THEN 'France entière'
+                        ELSE NULL
+                    END
+                )
+                ORDER BY (
+                    CASE
+                        WHEN intervention_areas.type = 'departement' THEN departements.code
+                        WHEN intervention_areas.type = 'region' THEN regions.name
+                        WHEN intervention_areas.type = 'city' THEN cities.name
+                        WHEN intervention_areas.type = 'epci' THEN epci.name
+                    END
+                ) ASC
+            )
+        ) as structuresList
     FROM organizations
     LEFT JOIN organization_types ON organizations.fk_type = organization_types.organization_type_id
-    WHERE
-        organizations.fk_type = :typeId
-    ORDER BY
-        CASE organization_types.uid
-            WHEN 'rectorat' THEN SUBSTRING(organizations.name, '(?<=(?:d''|de la |de ))[A-Z].+')
-            ELSE '' END
-        ASC,
-        UNACCENT(organizations.name) ASC`,
+    LEFT JOIN intervention_areas ON organizations.organization_id = intervention_areas.fk_organization
+    LEFT JOIN regions ON intervention_areas.fk_region = regions.code
+    LEFT JOIN departements ON intervention_areas.fk_departement = departements.code
+    LEFT JOIN epci ON intervention_areas.fk_epci = epci.code
+    LEFT JOIN cities ON intervention_areas.fk_city = cities.code
+    WHERE organizations.fk_type = :typeId
+    GROUP BY intervention_areas.type, organization_types.name_singular;`,
     {
         type: QueryTypes.SELECT,
         replacements: {
