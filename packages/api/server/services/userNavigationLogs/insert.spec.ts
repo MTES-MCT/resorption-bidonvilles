26edab5b3985f57f6779d8647157598f68c0fe3c
Christophe BENARD <ch.benard@gmail.com>
2024-03-12 13:21:34 +0100

    feat: tester les transactions refers to https://trello.com/c/g2rbxJmR/2156

--- packages/api/server/services/userNavigationLogs/insert.spec.ts
+++ packages/api/server/services/userNavigationLogs/insert.spec.ts
@@ -1,57 +1,78 @@
-import insert from '#server/services/userNavigationLogs/insert';
-import userNavigationLogsModel from '#server/models/userNavigationLogsModel';
-import userModel from '#server/models/userModel';
 import ServiceError from '#server/errors/ServiceError';
 
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+import rewiremock from 'rewiremock/node';
 
 const { expect } = chai;
 chai.use(sinonChai);
 
-describe('services/userNavigationLogs/insert()', () => {
-    let stubs;
+const sandbox = sinon.createSandbox();
+const sequelize = {
+    transaction: sandbox.stub(),
+};
+const userModel = {
+    isTracked: sandbox.stub(),
+    update: sandbox.stub(),
+};
 
+const userNavigationLogsModel = {
+    insertWebapp: sandbox.stub(),
+}
+
+rewiremock('#db/sequelize').with({ sequelize });
+rewiremock('#server/models/userModel/index').with(userModel);
+rewiremock('#server/models/userNavigationLogsModel/index').with(userNavigationLogsModel);
+
+rewiremock.enable();
+import insertNavigationLog from '#server/services/userNavigationLogs/insert';
+rewiremock.disable();
+
+describe('services/userNavigationLogs/insert()', () => {
+
+    let transaction;
     beforeEach(() => {
-        stubs = {
-            insertWebapp: sinon.stub(userNavigationLogsModel, 'insertWebapp'),
-            isTracked: sinon.stub(userModel, 'isTracked'),
-            update: sinon.stub(userModel, 'update'),
+        transaction = {
+            commit: sandbox.stub(),
+            rollback: sandbox.stub(),
         };
+        sequelize.transaction.withArgs().resolves(transaction);
     });
 
     afterEach(() => {
-        sinon.restore();
+        sandbox.reset();
     });
 
-    it('demande l\'insertion du log en base de données', async () => {
-        stubs.isTracked.resolves(true);
-        await insert(1, 'page');
+    it('insert un log de navigation en base de données', async () => {
+        userModel.isTracked.resolves(true);
+        userNavigationLogsModel.insertWebapp.resolves(1);
+        await insertNavigationLog(1, 'page');
     });
 
-    it('demande l\'insertion du log avec l\'origine', async () => {
-        stubs.isTracked.resolves(true);
-        await insert(1, 'page');
-        expect(stubs.insertWebapp).to.have.been.calledOnceWith(1, 'page');
+    it('demande l\'insertion du log de navigation avec les paramètres attendus', async () => {
+        // userModel.isTracked.resolves(true);
+        // userNavigationLogsModel.insertWebapp.resolves(1);
+        await insertNavigationLog(1, 'page');
+        expect(userNavigationLogsModel.insertWebapp).to.have.been.calledOnceWith(1, 'page', transaction);
     });
 
     it('enregistre la date et l\'heure de cet accès dans la table users', async () => {
-        stubs.isTracked.resolves(true);
-        await insert(1, 'page');
+        userModel.isTracked.resolves(true);
+        await insertNavigationLog(1, 'page');
     });
 
     it('retourne l\'identifiant du log nouvellement inséré', async () => {
-        stubs.isTracked.resolves(true);
-        stubs.insertWebapp.resolves(2);
-        const logId = await insert(1, 'page');
+        userModel.isTracked.resolves(true);
+        userNavigationLogsModel.insertWebapp.resolves(2);
+        const logId = await insertNavigationLog(1, 'page');
         expect(logId).to.be.equal(2);
     });
 
     it('génère une exception adaptée en cas d\'erreur pour vérifier si l\'utilisateur est tracké ', async () => {
-        stubs.isTracked.rejects(new Error('fetch failed'));
+        userModel.isTracked.rejects(new Error('fetch failed'));
         try {
-            await insert(1, 'page');
+            await insertNavigationLog(1, 'page');
         } catch (error) {
             expect(error).to.be.instanceOf(ServiceError);
             expect(error.code).to.be.equal('fetch_failed');
@@ -59,28 +80,71 @@ describe('services/userNavigationLogs/insert()', () => {
             return;
         }
 
-        expect.fail();
+        expect.fail('Should have thrown an error !');
     });
 
     it('n\'insère pas de log si l\'utilisateur n\'est pas tracké', async () => {
-        stubs.isTracked.resolves(false);
-        await insert(1, 'page');
-        expect(stubs.insertWebapp).not.to.have.been.called;
+        userModel.isTracked.resolves(false);
+        await insertNavigationLog(1, 'page');
+        expect(userNavigationLogsModel.insertWebapp).not.to.have.been.called;
     });
 
     it('génère une exception adaptée en cas d\'erreur d\'insertion en base de données', async () => {
-        stubs.isTracked.resolves(true);
-        stubs.insertWebapp.rejects(new Error('insertion failed'));
+        userModel.isTracked.resolves(true);
+        userNavigationLogsModel.insertWebapp.rejects(new Error('insertion failed'));
 
         try {
-            await insert(1, 'page');
+            await insertNavigationLog(1, 'page');
         } catch (error) {
             expect(error).to.be.instanceOf(ServiceError);
             expect(error.code).to.be.equal('insert_failed');
-            expect(error.message).to.be.equal('insertion failed');
             return;
         }
 
         expect.fail();
+    });
+
+    it('génère une exception adaptée en cas d\'erreur de mise à jour en base de données', async () => {
+        userModel.isTracked.resolves(true);
+        userModel.update.rejects(new Error('update_user_failed'));
+
+        try {
+            await insertNavigationLog(1, 'page');
+        } catch (error) {
+            expect(error).to.be.instanceOf(ServiceError);
+            expect(error.code).to.be.equal('update_user_failed');
+            return;
+        }
+
+        expect.fail();
+    });
+
+    it('commit la transaction une fois toutes les requêtes effectuées', async () => {
+        userModel.isTracked.resolves(true);
+        userNavigationLogsModel.insertWebapp.resolves(2);
+        await insertNavigationLog(1, 'page');
+        expect(transaction.commit).to.have.been.calledOnce;
+    });
+
+    it('ne committe pas la transaction si une requête échoue', async () => {
+        userModel.isTracked.resolves(true);
+        userModel.update.rejects(new Error('update_user_failed'));
+        try {
+            await insertNavigationLog(1, 'page');
+        } catch (error) {
+            expect(error).to.be.instanceOf(ServiceError);
+            expect(transaction.commit).not.to.have.been.calledOnce;
+        }
+    });
+
+    it('rollback la transaction si une requête échoue', async () => {
+        userModel.isTracked.resolves(true);
+        userModel.update.rejects(new Error('update_user_failed'));
+        try {
+            await insertNavigationLog(1, 'page');
+        } catch (error) {
+            expect(error).to.be.instanceOf(ServiceError);
+            expect(transaction.rollback).to.have.been.calledOnce;
+        }
     });
 });
