c776d0179b02e03348bc514df105f34a55b7ce6c
Christophe BENARD <ch.benard@gmail.com>
2024-03-12 12:57:03 +0100

    fix: améliorer la gestion des erreurs liées aux transactions refers to https://trello.com/c/g2rbxJmR/2156

--- packages/api/server/services/userNavigationLogs/insert.ts
+++ packages/api/server/services/userNavigationLogs/insert.ts
@@ -20,14 +20,21 @@ export default async (fk_user: number, page: string): Promise<number> => {
     const transaction = await sequelize.transaction();
     try {
         logId = await userNavigationLogsModel.insertWebapp(fk_user, page, transaction);
+    } catch (error) {
+        await transaction.rollback();
+        throw new ServiceError('insert_failed', error);
+    }
+
+    try {
         await userModel.update(fk_user, {
             last_access: new Date(),
             transaction
         });
     } catch (error) {
         await transaction.rollback();
-        throw new ServiceError('insert_failed', error);
+        throw new ServiceError('update_user_failed', error);
     }
+
     try {
         await transaction.commit();
     } catch (error) {
