bb435f17390117b86eeea1b68eb7418be2623341
Christophe BENARD <ch.benard@gmail.com>
2024-03-13 09:02:47 +0100

    fix: autoriser l'export des sites aux intervenants refers to https://trello.com/c/PuNYnXYp/2161

---
+++ packages/api/db/migrations/30000076-01-insert-export-shantytown-role_permission.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    up: (queryInterface) => {
+        return queryInterface.sequelize.query(
+            `INSERT INTO role_permissions(fk_role_regular, fk_feature, fk_entity, allowed, allow_all) VALUES 
+                (
+                    'intervener',
+                    'export',
+                    'shantytown',
+                    true,
+                    false
+                )`,
+        );
+    },
+
+    down: queryInterface => queryInterface.sequelize.query(
+        `DELETE FROM role_permissions 
+        WHERE
+            fk_role_regular = 'intervener' 
+        AND
+            fk_feature = 'export'
+        AND
+            fk_entity = 'shantytown'`),
+};
