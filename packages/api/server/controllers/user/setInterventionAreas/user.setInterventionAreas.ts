414ca9c87ce744e82c236d48d2fd1c0b7dbab4d2
Christophe BENARD <ch.benard@gmail.com>
2024-03-15 15:41:12 +0100

    fix: Limiter l'extension des territoires aux admins nationaux refers to https://trello.com/c/ZEiXZJSh/2166

--- packages/api/server/controllers/user/setInterventionAreas/user.setInterventionAreas.ts
+++ packages/api/server/controllers/user/setInterventionAreas/user.setInterventionAreas.ts
@@ -17,7 +17,12 @@ interface UserSetInterventionAreasRequest extends Request {
 
 export default async (req: UserSetInterventionAreasRequest, res: Response, next: NextFunction) => {
     try {
-        const user = await setInterventionAreas(
+        if (req.user.is_superuser !== true) {
+            res.status(403).send({
+                user_message: 'La modification des territoires d\'intervention est temporairement réservée aux administrateurs nationaux',
+            });
+            return;
+        }        const user = await setInterventionAreas(
             req.user,
             req.userToUpdate,
             req.body.organization_areas,
