754fd383847bf0ce1f0c22dbdd51ce890d96ddce
Christophe BENARD <ch.benard@gmail.com>
2024-04-02 08:59:49 +0200

    fix: corriger un `else if`

--- packages/api/server/services/user/setRoleRegular.ts
+++ packages/api/server/services/user/setRoleRegular.ts
@@ -4,20 +4,16 @@ import ServiceError from '#server/errors/ServiceError';
 import { User } from '#root/types/resources/User.d';
 
 function canUpdateRole(user: User, roleId: string): void {
-    if (roleId && roleId === 'intervener')
-    {
+    if (roleId && roleId === 'intervener') {
         if (user.is_admin !== true) {
             throw new ServiceError('permission_denied', new Error('Vous devez être Administrateur Local pour pouvoir accorder ce rôle'));
         }
-    } else {
-        if (user.is_superuser !== true) {
-            throw new ServiceError('permission_denied', new Error('Vous devez être Administrateur National pour pouvoir accorder ce rôle'));
-        }
+    } else if (user.is_superuser !== true) {
+        throw new ServiceError('permission_denied', new Error('Vous devez être Administrateur National pour pouvoir accorder ce rôle'));
     }
 }
 
 export default async (currentUser: User, userToUpdateId: number, roleId: string): Promise<User> => {
-
     canUpdateRole(currentUser, roleId);
 
     let updatedUser: User;
