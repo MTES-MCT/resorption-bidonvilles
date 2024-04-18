dab764d1757b63a860c63a89e205fc43905d190e
Gilles COGNIN <gillescognin@gmail.com>
2024-04-18 18:29:53 +0200

    feat: Intégration du système de blocage temporaire (front) après 5 tentatives en erreur

--- packages/frontend/webapp/src/components/ProfilUtilisateur/FormIdentifiants/FormIdentifiants.vue
+++ packages/frontend/webapp/src/components/ProfilUtilisateur/FormIdentifiants/FormIdentifiants.vue
@@ -21,10 +21,15 @@
 
         <!-- autres champs modifiables -->
         <div class="max-w-lg mt-5">
-            <FormIdentifiantsInputPassword :user="user" />
+            <FormIdentifiantsInputPassword
+                :user="user"
+                :disabled="passwordCheckBlocked"
+            />
         </div>
 
-        <Button type="submit" @click="onSubmit">Sauvegarder</Button>
+        <Button type="submit" @click="onSubmit" :disabled="passwordCheckBlocked"
+            >Sauvegarder</Button
+        >
     </form>
 </template>
 
@@ -47,27 +52,76 @@ import FormIdentifiantsInputPassword from "./inputs/FormIdentifiantsInputPasswor
 const userStore = useUserStore();
 const user = userStore.user;
 const error = ref(null);
+const passwordCheckAttempts = ref(0);
+const passwordCheckBlocked = ref(false);
+const passwordCheckBlockTime = ref(null);
 
 const { handleSubmit, setErrors } = useForm({
     validationSchema: schema,
 });
 
 const onSubmit = handleSubmit(async (values) => {
+    const notificationStore = useNotificationStore();
     error.value = null;
+    if (passwordCheckBlocked.value) {
+        const now = Date.now();
+        const diff = Math.round((now - this.blockTime) / 1000);
+        if (diff < 300) {
+            notificationStore.error(
+                "Changement de mot de passe bloqué",
+                "Vous avez renseigné trop de fois un mot de passe incorrect. Le changement de mot de passe est bloqué pendant 5 minutes."
+            );
+            return;
+        } else {
+            passwordCheckBlocked.value = false;
+            passwordCheckAttempts.value = 0;
+            passwordCheckBlockTime.value = null;
+        }
+    }
 
-    try {
-        await edit(values);
-        const notificationStore = useNotificationStore();
-        notificationStore.success(
-            "Vos identifiants",
-            "Votre mot de passe a bien été modifié"
-        );
-        userStore.setPasswordConformity(true);
-        router.push("/tableau-de-bord");
-    } catch (e) {
-        error.value = e?.user_message || "Une erreur inconnue est survenue";
-        if (e?.fields) {
-            setErrors(e.fields);
+    const passwordCheck = await userStore.checkActualPassword(
+        userStore.user.email,
+        values.actualPassword
+    );
+
+    if (passwordCheck.checkActualPassword === false) {
+        passwordCheckAttempts.value++;
+        if (passwordCheckAttempts.value >= 5) {
+            passwordCheckBlocked.value = true;
+            passwordCheckBlockTime.value = Date.now();
+            notificationStore.error(
+                "Changement de mot de passe bloqué",
+                "Vous avez renseigné un mot de passe incorrect à de trop nombreuses reprises. Le changement de mot de passe est bloqué pendant 5 minutes."
+            );
+            return;
+        } else {
+            setErrors({
+                actualPassword: "Mot de passe invalide",
+            });
+            notificationStore.error(
+                "Mot de passe invalide",
+                "Votre mot de passe actuel est invalide"
+            );
+            return;
+        }
+    } else {
+        passwordCheckAttempts.value = 0;
+        passwordCheckBlocked.value = false;
+        passwordCheckBlockTime.value = null;
+
+        try {
+            await edit({ password: values.newPassword });
+            notificationStore.success(
+                "Vos identifiants",
+                "Votre mot de passe a bien été modifié"
+            );
+            userStore.setPasswordConformity(true);
+            router.push("/tableau-de-bord");
+        } catch (e) {
+            error.value = e?.user_message || "Une erreur inconnue est survenue";
+            if (e?.fields) {
+                setErrors(e.fields);
+            }
         }
     }
 });
