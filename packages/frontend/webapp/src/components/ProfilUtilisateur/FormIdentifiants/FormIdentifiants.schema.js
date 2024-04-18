19da8f5f38106a3f9be5a6f9ea60527edf6d56bd
Gilles COGNIN <gillescognin@gmail.com>
2024-04-18 18:38:50 +0200

    feat: Intégration des nouveaux champs à valider lors du changement de mot de passe

--- packages/frontend/webapp/src/components/ProfilUtilisateur/FormIdentifiants/FormIdentifiants.schema.js
+++ packages/frontend/webapp/src/components/ProfilUtilisateur/FormIdentifiants/FormIdentifiants.schema.js
@@ -1,12 +1,20 @@
-import { object, string } from "yup";
+import { object, string, ref } from "yup";
 import labels from "./FormIdentifiants.labels";
 import { useUserStore } from "@/stores/user.store";
 
 const userStore = useUserStore();
 
 export default object({
-    password: string()
+    actualPassword: string().required().label(labels.actualPassword),
+    newPassword: string()
         .required()
         .min(userStore.user.is_admin ? 16 : 12)
         .label(labels.password),
+    retypeNewPassword: string()
+        .required()
+        .oneOf(
+            [ref("newPassword"), null],
+            "Les mots de passe ne correspondent pas"
+        )
+        .label(labels.retypeNewPassword),
 });
