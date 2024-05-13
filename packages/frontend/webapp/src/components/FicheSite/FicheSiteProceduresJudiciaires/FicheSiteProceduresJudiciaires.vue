29085e23141d89b34c165fada159f76a766ece86
Christophe BENARD <ch.benard@gmail.com>
2024-05-13 14:14:22 +0200

    feat: ajouter les procédures adm. à la fiche site refers to https://trello.com/c/uqdG1Cv7/2171

--- packages/frontend/webapp/src/components/FicheSite/FicheSiteProceduresJudiciaires/FicheSiteProceduresJudiciaires.vue
+++ packages/frontend/webapp/src/components/FicheSite/FicheSiteProceduresJudiciaires/FicheSiteProceduresJudiciaires.vue
@@ -1,41 +1,138 @@
 <template>
-    <FicheRubrique title="Procédure judiciaire" category="justice">
-        <FicheSiteProceduresJudiciaireLigne
-            icon="scroll"
-            label="Dépôt de plainte du propriétaire"
-            :border="false"
-            :marginTop="false"
-        >
-            {{ formatBool(town.ownerComplaint) }}
-        </FicheSiteProceduresJudiciaireLigne>
-        <FicheSiteProceduresJudiciaireLigne
-            icon="balance-scale"
-            label="Existence d’une procédure judiciaire"
-        >
-            {{ formatBool(town.justiceProcedure) }}
-        </FicheSiteProceduresJudiciaireLigne>
+    <FicheRubrique
+        title="Procédure judiciaire ou administrative"
+        category="procedure"
+    >
+        <FicheSiteProceduresJudiciairesRubrique>
+            <FicheSiteProceduresJudiciairesLigne
+                icon="scroll"
+                label="Une plainte a-t-elle été déposée par le propriétaire ?"
+                :border="false"
+            >
+                {{ formatBool(town.ownerComplaint) }}
+            </FicheSiteProceduresJudiciairesLigne>
 
-        <FicheSiteProceduresJudiciaireLigne label="Décision de justice rendue">
-            {{ justiceRendered }}
-        </FicheSiteProceduresJudiciaireLigne>
+            <FicheSiteProceduresJudiciairesLigne
+                icon="balance-scale"
+                label="Une procédure judiciaire a-t-elle été engagée ?"
+            >
+                {{ formatBool(town.justiceProcedure) }}
+            </FicheSiteProceduresJudiciairesLigne>
 
-        <FicheSiteProceduresJudiciaireLigne label="Contentieux">
-            {{ formatBool(town.justiceChallenged) }}
-        </FicheSiteProceduresJudiciaireLigne>
+            <FicheSiteProceduresJudiciairesLigne
+                label="Décision de justice rendue"
+                v-if="town.justiceProcedure"
+            >
+                {{ justiceRendered }}
+            </FicheSiteProceduresJudiciairesLigne>
 
-        <FicheSiteProceduresJudiciaireLigne
-            icon="person-military-pointing"
-            label="Concours de la force publique"
-        >
-            {{ policeStatus }}
-        </FicheSiteProceduresJudiciaireLigne>
+            <FicheSiteProceduresJudiciairesLigne
+                label="Contentieux"
+                v-if="town.justiceRendered"
+            >
+                {{ formatBool(town.justiceChallenged) }}
+            </FicheSiteProceduresJudiciairesLigne>
 
-        <FicheSiteProceduresJudiciaireLigne
-            icon="file"
-            label="Nom de l'étude d'huissier"
-        >
-            {{ town.bailiff || "non communiqué" }}
-        </FicheSiteProceduresJudiciaireLigne>
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.justiceProcedure"
+                icon="person-military-pointing"
+                label="Concours de la force publique"
+            >
+                {{ policeStatus }}
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.justiceProcedure"
+                icon="file"
+                label="Nom de l'étude d'huissier"
+            >
+                {{ town.bailiff || "non communiqué" }}
+            </FicheSiteProceduresJudiciairesLigne>
+        </FicheSiteProceduresJudiciairesRubrique>
+
+        <FicheSiteProceduresJudiciairesRubrique>
+            <FicheSiteProceduresJudiciairesLigne
+                :border="false"
+                icon="file-contract"
+                label="Une procédure administrative prescrivant l'évacuation sous délai est-elle en cours ?"
+            >
+                <p>{{ administrativeOrderStatus }}</p>
+                <p
+                    v-if="
+                        town.evacuationUnderTimeLimit &&
+                        town.administrativeOrderDecisionRenderedBy
+                    "
+                >
+                    Par : {{ town.administrativeOrderDecisionRenderedBy }}
+                </p>
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.evacuationUnderTimeLimit"
+                icon="person-military-pointing"
+                label="Concours de la force
+                publique"
+            >
+                {{ evacuationPoliceStatus }}
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.evacuationUnderTimeLimit"
+                icon="file"
+                label="Nom de l'étude d'huissier"
+            >
+                {{ town.evacuationBailiff || "non communiqué" }}
+            </FicheSiteProceduresJudiciairesLigne>
+        </FicheSiteProceduresJudiciairesRubrique>
+
+        <FicheSiteProceduresJudiciairesRubrique>
+            <FicheSiteProceduresJudiciairesLigne
+                :border="false"
+                icon="right-from-bracket"
+                label="Un arrêté d'insalubrité dans le cadre d'une opération RHI bidonville est-il en cours ?"
+            >
+                {{ formatBool(town.insalubrityOrder) }}
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.insalubrityOrder"
+                icon="signs-post"
+                label="Affichage de l'arrêté ou notification"
+            >
+                <p>{{ formatBool(town.insalubrityOrderDisplayed) }}</p>
+                <p>
+                    {{ insalubrityOrderDisplayStatus }}
+                </p>
+                <p v-if="town.insalubrityOrderBy">
+                    Pris par : {{ town.insalubrityOrderBy }}
+                </p>
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.insalubrityOrder"
+                icon="map-location-dot"
+                label="Quelles sont les parcelles concernées"
+            >
+                {{ town.insalubrityParcels || "non précisé" }}
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.insalubrityOrder"
+                icon="person-military-pointing"
+                label="Concours de la force
+                publique"
+            >
+                {{ insalubrityPoliceStatus }}
+            </FicheSiteProceduresJudiciairesLigne>
+
+            <FicheSiteProceduresJudiciairesLigne
+                v-if="town.insalubrityOrder"
+                icon="file"
+                label="Nom de l'étude d'huissier"
+            >
+                {{ town.insalubrityBailiff || "non communiqué" }}
+            </FicheSiteProceduresJudiciairesLigne>
+        </FicheSiteProceduresJudiciairesRubrique>
     </FicheRubrique>
 </template>
 
@@ -48,7 +145,8 @@ import { useModaleStore } from "@/stores/modale.store";
 import { useEventBus } from "@common/helpers/event-bus";
 
 import FicheRubrique from "@/components/FicheRubrique/FicheRubrique.vue";
-import FicheSiteProceduresJudiciaireLigne from "./FicheSiteProceduresJudiciaireLigne.vue";
+import FicheSiteProceduresJudiciairesRubrique from "./FicheSiteProceduresJudiciairesRubrique.vue";
+import FicheSiteProceduresJudiciairesLigne from "./FicheSiteProceduresJudiciairesLigne.vue";
 import ModaleListeAccesPJ from "@/components/ModaleListeAccesPJ/ModaleListeAccesPJ.vue";
 
 const props = defineProps({
@@ -74,23 +172,93 @@ const justiceRendered = computed(() => {
     return `${str} par « ${town.value.justiceRenderedBy} »`;
 });
 
-const policeStatus = computed(() => {
-    if (town.value.policeStatus === "none") {
+function getPoliceStatus(policeStatus, policeRequestedAt, policeGrantedAt) {
+    if (policeStatus === "none") {
         return "non demandé";
     }
 
-    if (town.value.policeStatus === "requested") {
-        return `demandé le ${formatDate(
-            town.value.policeRequestedAt,
-            "d/m/y"
-        )}`;
+    if (policeStatus === "requested") {
+        return `demandé le ${formatDate(policeRequestedAt, "d/m/y")}`;
     }
 
-    if (town.value.policeStatus === "granted") {
-        return `accordé le ${formatDate(town.value.policeGrantedAt, "d/m/y")}`;
+    if (policeStatus === "granted") {
+        return `accordé le ${formatDate(policeGrantedAt, "d/m/y")}`;
     }
 
     return "non communiqué";
+}
+
+const policeStatus = computed(() => {
+    return getPoliceStatus(
+        town.value.policeStatus,
+        town.value.policeRequestedAt,
+        town.value.policeGrantedAt
+    );
+});
+
+const insalubrityPoliceStatus = computed(() => {
+    return getPoliceStatus(
+        town.value.insalubrityPoliceStatus,
+        town.value.insalubrityPoliceRequestedAt,
+        town.value.insalubrityPoliceGrantedAt
+    );
+});
+
+const evacuationPoliceStatus = computed(() => {
+    return getPoliceStatus(
+        town.value.evacuationPoliceStatus,
+        town.value.evacuationPoliceRequestedAt,
+        town.value.evacuationPoliceGrantedAt
+    );
+});
+
+const administrativeOrderStatus = computed(() => {
+    if (town.value.evacuationUnderTimeLimit === null) {
+        return "non communiqué";
+    }
+
+    if (town.value.evacuationUnderTimeLimit !== true) {
+        return "non";
+    }
+
+    return `${
+        town.value.administrativeOrderDecisionAt === null
+            ? "oui"
+            : "oui, arrêté pris le " +
+              formatDate(town.value.administrativeOrderDecisionAt, "d/m/y")
+    }`;
+});
+
+const insalubrityOrderDisplayStatus = computed(() => {
+    // Décomposition préliminaire pour améliorer la lisibilité
+    const {
+        insalubrityOrder,
+        insalubrityOrderDisplayed,
+        insalubrityOrderType,
+        insalubrityOrderAt,
+    } = town.value;
+
+    // Vérifie d'abord si les ordres d'insalubrité sont activés et affichés
+    if (insalubrityOrder && insalubrityOrderDisplayed) {
+        let displayStatus = "affiché";
+
+        // Si un type d'ordre est présent, ajoute ce type au statut d'affichage
+        if (insalubrityOrderType) {
+            displayStatus = `${insalubrityOrderType} affiché`;
+
+            // Si une date est également présente, l'ajoute au statut
+            if (insalubrityOrderAt) {
+                displayStatus += ` le ${formatDate(
+                    insalubrityOrderAt,
+                    "d/m/y"
+                )}`;
+            }
+        }
+        return displayStatus;
+    }
+
+    // Retourne null si les conditions de base ne sont pas remplies
+    return null;
 });
 
 watch(
