eb30568cc779203807a59eef5c231949f1e22d83
Christophe BENARD <ch.benard@gmail.com>
2024-05-14 13:24:20 +0200

    fix: supprimer `police...` et ajouter `existing_litigation` refers to https://trello.com/c/UpuC5sBY/2186

--- packages/api/db/migrations/30000078-01-alter-table-shantytowns-add-fields.js
+++ packages/api/db/migrations/30000078-01-alter-table-shantytowns-add-fields.js
@@ -2,6 +2,17 @@ function addColumnsTo(queryInterface, Sequelize, tableName, transaction) {
     return Promise.all([
         queryInterface.addColumn(
             tableName,
+            'existing_litigation',
+            {
+                type: Sequelize.BOOLEAN,
+                allowNull: true,
+            },
+            {
+                transaction,
+            },
+        ),
+        queryInterface.addColumn(
+            tableName,
             'evacuation_under_time_limit',
             {
                 type: Sequelize.BOOLEAN,
@@ -38,50 +49,6 @@ function addColumnsTo(queryInterface, Sequelize, tableName, transaction) {
             'administrative_order_evacuation_at',
             {
                 type: Sequelize.DATEONLY,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'evacuation_police_status',
-            {
-                type: Sequelize.ENUM('none', 'requested', 'granted'),
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'evacuation_police_requested_at',
-            {
-                type: Sequelize.DATEONLY,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'evacuation_police_granted_at',
-            {
-                type: Sequelize.DATEONLY,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'evacuation_bailiff',
-            {
-                type: Sequelize.STRING,
                 allowNull: true,
             },
             {
@@ -154,73 +121,22 @@ function addColumnsTo(queryInterface, Sequelize, tableName, transaction) {
                 transaction,
             },
         ),
-        queryInterface.addColumn(
-            tableName,
-            'insalubrity_police_status',
-            {
-                type: Sequelize.ENUM('none', 'requested', 'granted'),
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'insalubrity_police_requested_at',
-            {
-                type: Sequelize.DATEONLY,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'insalubrity_police_granted_at',
-            {
-                type: Sequelize.DATEONLY,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        queryInterface.addColumn(
-            tableName,
-            'insalubrity_bailiff',
-            {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
     ]);
 }
 
 function removeColumnsFrom(queryInterface, tableName, transaction) {
     return Promise.all([
-        queryInterface.removeColumn(tableName, 'insalubrity_bailiff', { transaction }),
-        queryInterface.removeColumn(tableName, 'insalubrity_police_granted_at', { transaction }),
-        queryInterface.removeColumn(tableName, 'insalubrity_police_requested_at', { transaction }),
-        queryInterface.removeColumn(tableName, 'insalubrity_police_status', { transaction }),
         queryInterface.removeColumn(tableName, 'insalubrity_parcels', { transaction }),
         queryInterface.removeColumn(tableName, 'insalubrity_order_at', { transaction }),
         queryInterface.removeColumn(tableName, 'insalubrity_order_by', { transaction }),
         queryInterface.removeColumn(tableName, 'insalubrity_order_type', { transaction }),
         queryInterface.removeColumn(tableName, 'insalubrity_order_displayed', { transaction }),
         queryInterface.removeColumn(tableName, 'insalubrity_order', { transaction }),
-        queryInterface.removeColumn(tableName, 'evacuation_bailiff', { transaction }),
-        queryInterface.removeColumn(tableName, 'evacuation_police_granted_at', { transaction }),
-        queryInterface.removeColumn(tableName, 'evacuation_police_requested_at', { transaction }),
-        queryInterface.removeColumn(tableName, 'evacuation_police_status', { transaction }),
         queryInterface.removeColumn(tableName, 'administrative_order_evacuation_at', { transaction }),
         queryInterface.removeColumn(tableName, 'administrative_order_decision_rendered_by', { transaction }),
         queryInterface.removeColumn(tableName, 'administrative_order_decision_at', { transaction }),
         queryInterface.removeColumn(tableName, 'evacuation_under_time_limit', { transaction }),
+        queryInterface.removeColumn(tableName, 'existing_litigation', { transaction }),
     ]);
 }
 
