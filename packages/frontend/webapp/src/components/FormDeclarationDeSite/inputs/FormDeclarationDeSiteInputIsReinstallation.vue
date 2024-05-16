fa1e0f535f6d2d493da82ccdb5182f2a4b623bd5
Christophe BENARD <ch.benard@gmail.com>
2024-05-16 14:24:28 +0200

    fix: utiliser les items mutualisés refers to https://trello.com/c/UpuC5sBY/2186

--- packages/frontend/webapp/src/components/FormDeclarationDeSite/inputs/FormDeclarationDeSiteInputIsReinstallation.vue
+++ packages/frontend/webapp/src/components/FormDeclarationDeSite/inputs/FormDeclarationDeSiteInputIsReinstallation.vue
@@ -18,10 +18,5 @@
 <script setup>
 import { CheckableGroup, Radio } from "@resorptionbidonvilles/ui";
 import labels from "../FormDeclarationDeSite.labels";
-
-const items = [
-    { value: 1, label: "Oui" },
-    { value: 0, label: "Non" },
-    { value: -1, label: "Inconnu" },
-];
+import items from "@/utils/yesNoItems";
 </script>
