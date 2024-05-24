aab0314dfeb13f63acccfbd2a63747f703c1deea
Christophe BENARD <ch.benard@gmail.com>
2024-05-24 18:20:23 +0200

    feat: Ajouter "refusé" (refused) dans l'historique refers to https://trello.com/c/UpuC5sBY/2186

--- packages/api/db/migrations/30000079-01-alter-table-shantytowns-alter-enum_shantytowns_police_status.js
+++ packages/api/db/migrations/30000079-01-alter-table-shantytowns-alter-enum_shantytowns_police_status.js
@@ -1,63 +1,93 @@
+function addValueFromEnumSahntytownPoliceStatus(queryInterface, tableName, transaction) {
+    return Promise.all([
+        queryInterface.sequelize.query(
+            `ALTER TYPE "enum_${tableName}_police_status" ADD VALUE 'refused' ;`,
+            {
+                transaction,
+            },
+        ),
+    ]);
+}
+
+
+function removeValueFromEnumSahntytownPoliceStatus(queryInterface, tableName, transaction) {
+    return Promise.all([
+        queryInterface.sequelize.query(
+            `UPDATE "${tableName}" SET police_status = NULL WHERE police_status = 'refused' ;`,
+            {
+                transaction,
+            },
+        ),
+
+        queryInterface.sequelize.query(
+            `CREATE TYPE "enum_${tableName}_police_status_new" AS ENUM (
+                'none',
+                'requested',
+                'granted') ;`,
+            {
+                transaction,
+            },
+        ),
+
+        queryInterface.sequelize.query(
+            `ALTER TABLE "${tableName}"
+                ALTER COLUMN police_status TYPE "enum_${tableName}_police_status_new"
+            USING (police_status::text::"enum_${tableName}_police_status_new") ;`,
+            {
+                transaction,
+            },
+        ),
+
+        queryInterface.sequelize.query(
+            `DROP TYPE "enum_${tableName}_police_status" ;`,
+            {
+                transaction,
+            },
+        ),
+
+
+        queryInterface.sequelize.query(
+            `ALTER TYPE "enum_${tableName}_police_status_new" RENAME TO "enum_${tableName}_police_status" ;`,
+            {
+                transaction,
+            },
+        ),
+
+        queryInterface.sequelize.query(
+            `ALTER TABLE "${tableName}"
+                ALTER COLUMN police_status TYPE "enum_${tableName}_police_status"
+            USING (police_status::text::"enum_${tableName}_police_status") ;`,
+            {
+                transaction,
+            },
+        ),
+    ]);
+}
+
+
 module.exports = {
-    up: queryInterface => queryInterface.sequelize.query(
-        'ALTER TYPE enum_shantytowns_police_status ADD VALUE \'refused\'',
-    ),
+    async up(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await Promise.all([
+                addValueFromEnumSahntytownPoliceStatus(queryInterface, 'shantytowns', transaction),
+                addValueFromEnumSahntytownPoliceStatus(queryInterface, 'ShantytownHistories', transaction),
+            ]);
+            await transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
 
     async down(queryInterface) {
         const transaction = await queryInterface.sequelize.transaction();
 
         try {
             await Promise.all([
-
-                queryInterface.sequelize.query(
-                    'UPDATE shantytowns SET police_status = NULL WHERE police_status = \'refused\';',
-                    {
-                        transaction,
-                    },
-                ),
-
-                queryInterface.sequelize.query(
-                    `CREATE TYPE enum_shantytowns_police_status_new AS ENUM (
-                        'none',
-                        'requested',
-                        'granted');`,
-                    {
-                        transaction,
-                    },
-                ),
-
-                queryInterface.sequelize.query(
-                    `ALTER TABLE shantytowns
-                        ALTER COLUMN police_status TYPE enum_shantytowns_police_status_new
-                    USING (police_status::text::enum_shantytowns_police_status_new);`,
-                    {
-                        transaction,
-                    },
-                ),
-
-                queryInterface.sequelize.query(
-                    'DROP TYPE enum_shantytowns_police_status ;',
-                    {
-                        transaction,
-                    },
-                ),
-
-
-                queryInterface.sequelize.query(
-                    'ALTER TYPE enum_shantytowns_police_status_new RENAME TO enum_shantytowns_police_status ;',
-                    {
-                        transaction,
-                    },
-                ),
-
-                queryInterface.sequelize.query(
-                    `ALTER TABLE shantytowns
-                        ALTER COLUMN police_status TYPE enum_shantytowns_police_status
-                    USING (police_status::text::enum_shantytowns_police_status) ;`,
-                    {
-                        transaction,
-                    },
-                ),
+                removeValueFromEnumSahntytownPoliceStatus(queryInterface, 'shantytowns', transaction),
+                removeValueFromEnumSahntytownPoliceStatus(queryInterface, 'ShantytownHistories', transaction),
             ]);
             await transaction.commit();
         } catch (error) {
