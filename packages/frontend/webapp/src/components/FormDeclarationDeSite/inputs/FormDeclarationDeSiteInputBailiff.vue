3baabebf4c5de4a905f90c224013cccf2e9e45f8
Christophe BENARD <ch.benard@gmail.com>
2024-05-31 11:53:23 +0200

    fix: supprimer une prorpiété inutilisée refers to https://github.com/MTES-MCT/resorption-bidonvilles/pull/989#discussion_r1618613588

--- packages/frontend/webapp/src/components/FormDeclarationDeSite/inputs/FormDeclarationDeSiteInputBailiff.vue
+++ packages/frontend/webapp/src/components/FormDeclarationDeSite/inputs/FormDeclarationDeSiteInputBailiff.vue
@@ -1,17 +1,8 @@
 <template>
-    <TextInput :name="fieldName" :id="fieldName" :label="labels.bailiff" />
+    <TextInput name="bailiff" id="bailiff" :label="labels.bailiff" />
 </template>
 
 <script setup>
-import { toRefs } from "vue";
 import { TextInput } from "@resorptionbidonvilles/ui";
 import labels from "../FormDeclarationDeSite.labels";
-
-const props = defineProps({
-    fieldName: {
-        type: String,
-        required: true,
-    },
-});
-const { fieldName } = toRefs(props);
 </script>
