9a6af80c4b7e67f78282c5d1bcad173149438ac1
Gilles COGNIN <gillescognin@gmail.com>
2024-06-21 01:48:38 +0200

    feat: Ajout du controller

---
+++ packages/api/server/controllers/user/refuseAccess/user.refuseAccess.ts
@@ -0,0 +1,19 @@
+import userService from '#server/services/user/index';
+
+const ERRORS = {
+    undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
+};
+
+export default async (req, res, next) => {
+    try {
+        const updatedUser = await userService.refuse(req.params.id);
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        const { code, message } = ERRORS[error?.code] || ERRORS.undefined;
+        res.status(code).send({
+            user_message: message,
+        });
+
+        next(error?.nativeError || error);
+    }
+};
