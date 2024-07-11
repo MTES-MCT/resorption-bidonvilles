3409275c0b0037aafda0d4b8c0d6544ae5aa858e
Christophe BENARD <ch.benard@gmail.com>
2024-07-11 17:52:16 +0200

    feat: Ajout de `serializeAttachment` à l'index du modèle refers to https://trello.com/c/eu0AzUV4/2182

--- packages/api/server/models/attachmentModel/index.ts
+++ packages/api/server/models/attachmentModel/index.ts
@@ -1,9 +1,7 @@
 import createLinkedAttachment from './createLinkedAttachment';
 import findKeys from './findKeys';
-import serializeAttachment from './serializeAttachment';
 
 export default {
     createLinkedAttachment,
     findKeys,
-    serializeAttachment,
 };
