625dfbb36ee353d4b64c9c0313af3d43963bed19
Christophe BENARD <ch.benard@gmail.com>
2024-07-11 17:33:18 +0200

    feat: Sérialiser les PJs avec des liens signés refers to https://trello.com/c/5N7PDRHr/2182

---
+++ packages/api/server/services/attachment/serializeAttachment.ts
@@ -0,0 +1,20 @@
+import fromMimeToExtension from '#server/utils/fromMimeToExtension';
+import getSignedUrl from './getSignedUrl';
+import { Attachment } from './Attachment.d';
+
+export default async (attachment: string): Promise<Attachment> => {
+    const [id, key, previewKey, original_name, mimetype, size, created_by] = attachment.split('@.;.@');
+
+    return {
+        state: 'uploaded',
+        id: parseInt(id, 10),
+        name: original_name,
+        size: parseInt(size, 10),
+        urls: {
+            original: key ? await getSignedUrl(key) : null,
+            preview: previewKey ? await getSignedUrl(previewKey) : null,
+        },
+        extension: fromMimeToExtension[mimetype] || 'inconnu',
+        created_by,
+    };
+};
