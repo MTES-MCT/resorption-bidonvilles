e5afa818a0c7991293794ab7ca8021dfd81d458b
Christophe BENARD <ch.benard@gmail.com>
2024-08-09 10:21:08 +0200

    fix: ajouter `await` pour attendre la résolution de la promesse

--- packages/api/server/services/question/findOne.ts
+++ packages/api/server/services/question/findOne.ts
@@ -4,10 +4,14 @@ import enrichQuestion from '#server/services/question/common/enrichQuestion';
 import { EnrichedQuestion } from '#root/types/resources/QuestionEnriched.d';
 
 export default async (questionId: number, transaction?: Transaction): Promise<EnrichedQuestion> => {
-    let enrichedQuestion: Promise<EnrichedQuestion> | null = null;
+    let enrichedQuestion: EnrichedQuestion | null = null;
 
     try {
-        enrichedQuestion = enrichQuestion(questionId, transaction);
+        enrichedQuestion = await enrichQuestion(questionId, transaction);
+
+        if (enrichedQuestion === null) {
+                throw new ServiceError('fetch_failed', new Error('Impossible de retrouver la question en base de données'));
+        }
     } catch (error) {
         throw new ServiceError('fetch_failed', error);
     }
