9838aee4777518c9899664cdbd4123319b865f88
Christophe BENARD <ch.benard@gmail.com>
2024-09-10 11:01:30 +0200

    feat: Ajouter le filtre "activeOnly" côté back refers to https://trello.com/c/q0mbeidh/2221

--- packages/api/server/models/organizationModel/findByIds.ts
+++ packages/api/server/models/organizationModel/findByIds.ts
@@ -2,4 +2,4 @@ import { type Transaction } from 'sequelize';
 import find from './_common/find';
 import { Organization } from '#root/types/resources/Organization.d';
 
-export default (ids: number[], transaction?: Transaction): Promise<Organization[]> => find({ ids }, transaction);
+export default (ids: number[], activeOnly: boolean = false, transaction?: Transaction): Promise<Organization[]> => find({ ids, activeOnly }, transaction);
