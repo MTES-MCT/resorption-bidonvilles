ce7c9c7bfab9379172e9243d67656ea7dcb6b8e8
Gilles COGNIN <gillescognin@gmail.com>
2024-09-23 10:57:59 +0200

    fix: Correction de la transmission des données vers Vee-Validate

--- packages/frontend/webapp/src/components/InputAddress/InputAddress.vue
+++ packages/frontend/webapp/src/components/InputAddress/InputAddress.vue
@@ -5,10 +5,8 @@
             id="address"
             v-bind="$attrs"
             :fn="searchAddress"
-            v-model="address"
             ref="address"
         />
-        <!-- @update:modelValue="handleAddress" -->
         <InputError v-if="errors.length">{{ errors[0] }}</InputError>
     </InputWrapper>
 </template>
@@ -35,42 +33,19 @@ const props = defineProps({
     },
 });
 const { modelValue, withoutMargin } = toRefs(props);
-// const emit = defineEmits(["update:modelValue"]);
-// const address = useFieldValue("address");
-// const { value: address, errors } = useField("address");
 
 const { handleChange, errors } = useField("address");
 const address = computed({
     get() {
-        console.log("Getting value: ", modelValue.value);
-
-        // handleChange(modelValue.value, modelValue.value?.search !== "");
         if (modelValue.value) {
             handleChange(modelValue.value, false);
         }
         return modelValue.value;
     },
     set(value) {
-        console.log("Setting value: ", value);
         if (value?.data) {
-            // emit("update:modelValue", value);
-            console.log("Emitting value: ", value.data);
-
             handleChange(value, true);
         }
-        // emit("update:modelValue", value);
     },
 });
-
-// const handleAddress = (value) => {
-//     console.log(value);
-//     // modelValue.value = value;
-//     address.value = value;
-// };
-
-// defineExpose({
-//     focus: () => {
-//         address.value.focus();
-//     },
-// });
 </script>
