ce109c121400e424e25e43759fbcd352f42d712b
Gilles COGNIN <gillescognin@gmail.com>
2024-10-01 03:45:19 +0200

    fix: modification de l'appel à findJusticeReaders

--- packages/api/server/services/shantytown/findJusticeReaders.ts
+++ packages/api/server/services/shantytown/findJusticeReaders.ts
@@ -4,7 +4,7 @@ import { Organization } from '#root/types/resources/Organization.d';
 
 export default async (shantytownId: number): Promise<Organization[]> => {
     try {
-        return await organizationModel.findJusticeReadersByShantytown(shantytownId);
+        return await organizationModel.findJusticeReadersByShantytown(shantytownId, null);
     } catch (error) {
         throw new ServiceError('fetch_failed', error);
     }
