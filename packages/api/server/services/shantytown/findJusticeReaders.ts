8407e6f4d0b5f874838e2d8677d3543349a17f5d
Gilles COGNIN <gillescognin@gmail.com>
2024-10-02 16:23:23 +0200

    feat: ajout de l'utilisateur dans la requête

--- packages/api/server/services/shantytown/findJusticeReaders.ts
+++ packages/api/server/services/shantytown/findJusticeReaders.ts
@@ -2,9 +2,11 @@ import organizationModel from '#server/models/organizationModel';
 import ServiceError from '#server/errors/ServiceError';
 import { Organization } from '#root/types/resources/Organization.d';
 
-export default async (shantytownId: number): Promise<Organization[]> => {
+export default async (shantytownId: number, userId: number = null): Promise<Organization[]> => {
+    console.log('User ID: ', userId, 'Shantytown ID: ', shantytownId);
+
     try {
-        return await organizationModel.findJusticeReadersByShantytown(shantytownId, null);
+        return await organizationModel.findJusticeReadersByShantytown(shantytownId, userId);
     } catch (error) {
         throw new ServiceError('fetch_failed', error);
     }
