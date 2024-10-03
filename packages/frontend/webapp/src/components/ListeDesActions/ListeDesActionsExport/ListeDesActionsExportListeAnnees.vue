1b176361843b9441aa901c7a193727f41b2cb307
Christophe BENARD <ch.benard@gmail.com>
2024-10-03 12:38:17 +0200

    feat: Créer un composant permettant le choix de l'année refers to https://trello.com/c/U4dXC8md/2234

---
+++ packages/frontend/webapp/src/components/ListeDesActions/ListeDesActionsExport/ListeDesActionsExportListeAnnees.vue
@@ -0,0 +1,26 @@
+<template>
+    <Select
+        width="w-1/4"
+        name="selected_year"
+        label="Choisissez l'année"
+        info="Sélectionnez l'année pour laquelle vous souhaitez exporter les actions"
+        showMandatoryStar
+    >
+        <option v-for="year in years" :key="year" :value="year">
+            {{ year }}
+        </option>
+    </Select>
+</template>
+
+<script setup>
+import { toRefs, defineProps } from "vue";
+import { Select } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    years: {
+        type: Array,
+        required: true,
+    },
+});
+const { years } = toRefs(props);
+</script>
