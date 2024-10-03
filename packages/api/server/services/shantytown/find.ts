61667c9ba860b3fda41cf4922982aa1cec1c1758
Gilles COGNIN <gillescognin@gmail.com>
2024-10-03 20:49:35 +0200

    feat: Ajout des arrêtés dans le retour de la route towns/:id

--- packages/api/server/services/shantytown/find.ts
+++ packages/api/server/services/shantytown/find.ts
@@ -1,10 +1,11 @@
 import shantytownModel from '#server/models/shantytownModel';
 import ServiceError from '#server/errors/ServiceError';
 import findJusticeReaders from '#server/services/shantytown/findJusticeReaders';
-// import findJusticeReadersByShantytown from '#server/models/organizationModel/findJusticeReadersByShantytown';
+import shantytownDecree from '#server/services/shantytownDecree/findAll';
 import enrichCommentsAttachments from './_common/enrichCommentsAttachments';
+import { Shantytown } from '#root/types/resources/Shantytown.d';
 
-export default async (user, townId) => {
+export default async (user, townId): Promise<Shantytown> => {
     const town = await shantytownModel.findOne(user, townId);
 
     /*
@@ -16,17 +17,13 @@ export default async (user, townId) => {
     }
     const { comments, ...townWithoutComments } = town;
     const commentsWithEnrichedAttachments = await Promise.all(comments.map(async comment => enrichCommentsAttachments(comment)));
-    // const decrees = await shantytownDecree.findAll(user, townId);
-    // console.log((await findJusticeReaders(townId, 2588)).length);
-    console.log('User: ', user);
-
+    let decrees = null;
     if (user.role_id === 'national_admin' || (await findJusticeReaders(townId, user.id)).length > 0) {
-        console.log("L'utilisateur a accès, on va chercher les arrêtés");
-    } else {
-        console.log("L'utilisateur n'a pas accès");
+        decrees = { decrees: await shantytownDecree(user, townId) };
     }
     return {
         ...townWithoutComments,
         comments: commentsWithEnrichedAttachments,
+        ...decrees,
     };
 };
