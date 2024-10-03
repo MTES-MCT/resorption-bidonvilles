4c0b53b903e12bba6761b96ff120ba1a0f026158
Christophe BENARD <ch.benard@gmail.com>
2024-10-03 12:15:00 +0200

    feat: Modifier la route pour l'extraction des données à exporter refers to https://trello.com/c/U4dXC8md/2234

--- packages/api/server/controllers/action/exportList/action.exportList.route.ts
+++ packages/api/server/controllers/action/exportList/action.exportList.route.ts
@@ -2,7 +2,7 @@ import { type ApplicationWithCustomRoutes } from '#server/loaders/customRouteMet
 import controller from './action.exportList';
 
 export default (app: ApplicationWithCustomRoutes): void => {
-    app.customRoutes.get('/actions/export/csv', controller, undefined, {
+    app.customRoutes.get('/actions/export/excel', controller, undefined, {
         authenticate: true,
         multipart: false,
     });
