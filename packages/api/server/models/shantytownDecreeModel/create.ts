7a6cfe29ce5759df9f30e3305d9db47779ad256f
Gilles COGNIN <gillescognin@gmail.com>
2024-10-10 21:06:22 +0200

    feat: Création du model d'ajout du lien site - pièce jointe

--- packages/api/server/models/shantytownDecreeModel/create.ts
+++ packages/api/server/models/shantytownDecreeModel/create.ts
@@ -1,32 +1,29 @@
 import { sequelize } from '#db/sequelize';
+import { QueryTypes, Transaction } from 'sequelize';
 import ServiceError from '#server/errors/ServiceError';
-// import { ShantytownDecree } from '#server/types/resources/ShantytownDecree.d';
 
-type ShantytownDecreeLink = {
-    shantytown_decree_id: number;
-    fk_shantytown: number;
-    fk_attachment: number;
+type InsertDecreeData = {
+    shantytownId: number;
+    attachmentId: number;
 };
-// export default async (decree, shantytown, author): Promise<{ decrees: ShantytownDecree[] }> => true;
-export default async (datas, transaction = null): Promise<number> => {
-    const [decrees]: [ShantytownDecreeLink[], unknown] = await sequelize.query<ShantytownDecreeLink>(
+
+export default async (datas: InsertDecreeData, transaction: Transaction = null): Promise<number> => {
+    const [decreeId]: [number, number] = await sequelize.query(
         `INSERT INTO shantytown_decrees(
             fk_shantytown,
             fk_attachment
         )
-        VALUES (
-            :shantytown_id,
-            :attachment_id
-        )
+        VALUES (:shantytownId, :attachmentId)
         RETURNING shantytown_decree_id`,
         {
-            replacements: datas,
+            replacements: { shantytownId: datas.shantytownId, attachmentId: datas.attachmentId },
             transaction,
+            type: QueryTypes.INSERT,
         },
     );
 
-    if (decrees.length > 0 && decrees[0].shantytown_decree_id) {
-        return decrees[0].shantytown_decree_id;
+    if (decreeId) {
+        return decreeId;
     }
     throw new ServiceError('insert_failed', new Error('Impossible de créer le décret en base de données'));
 };
