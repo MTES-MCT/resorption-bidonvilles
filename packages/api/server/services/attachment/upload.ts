aa3fad985539894bae505d89fd54aa3f6efea665
Christophe BENARD <ch.benard@gmail.com>
2024-10-17 11:53:51 +0200

    feat: Appeler le service de scan des pièces jointes refers to https://trello.com/c/JTQDOzHB/2235

--- packages/api/server/services/attachment/upload.ts
+++ packages/api/server/services/attachment/upload.ts
@@ -2,6 +2,7 @@ import { S3 } from '#server/utils/s3';
 import sharp from 'sharp';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import config from '#server/config';
+import scanAttachment from '#server/services/attachment/scanAttachment';
 import attachmentModel from '#server/models/attachmentModel';
 import { AttachmentEntityType } from '#server/models/attachmentModel/createLinkedAttachment';
 import fromMimeToExtension from '#server/utils/fromMimeToExtension';
@@ -9,6 +10,17 @@ import { Transaction } from 'sequelize';
 import { v4 as uuidv4 } from 'uuid';
 
 export default async (entityType: AttachmentEntityType, entityId: number, createdBy: number, files: Express.Multer.File[], transaction?: Transaction): Promise<any[]> => {
+    // Scan les fichiers à la recherche d'un éventuelle virus
+    const results = await Promise.all(files.map(scanAttachment));
+
+    if (results.length > 0) {
+        for (let i = 0; i < results.length; i += 1) {
+            if (results[i].status !== 200) {
+                throw new Error(results[i].status.toString());
+            }
+        }
+    }
+
     const previews: Buffer[] = await Promise.all(
         files.map((f) => {
             if (!f.mimetype.startsWith('image/')) {
