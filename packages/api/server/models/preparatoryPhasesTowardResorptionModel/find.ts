3079ed3c087ed4f6a33b8b9e97d33c7493320c22
Christophe BENARD <ch.benard@gmail.com>
2024-10-28 13:57:18 +0100

    feat: Ajouter le modèle retournant les phases préparatoires refers to https://trello.com/c/M2OFZfb5/2252

---
+++ packages/api/server/models/preparatoryPhasesTowardResorptionModel/find.ts
@@ -0,0 +1,37 @@
+import { sequelize } from '#db/sequelize';
+import { QueryTypes } from 'sequelize';
+import { AuthUser } from '#server/middlewares/authMiddleware';
+import { PreparatoryPhasesTowardResorption, PreparatoryPhasesTowardResorptionRow } from '#root/types/resources/PreparatoryPhasesTowardResorption.d';
+import serializePreparatoryPhasesTowardResorption from './common/serializePreparatoryPhasesTowardResorption';
+
+export default async (user: AuthUser, ids: string[]): Promise<PreparatoryPhasesTowardResorption[]> => {
+    const rows: PreparatoryPhasesTowardResorptionRow[] = await sequelize.query(
+        `SELECT
+            spptr.fk_shantytown AS town_id,
+            pptr.uid AS preparatory_phase_id,
+            spptr.created_at AS created_at,
+            spptr.created_by AS created_by,
+            pptr.name AS preparatory_phase_name,
+            us.first_name as author_first_name,
+            us.last_name as author_last_name,
+            us.fk_organization as organization_id,
+            o."name" as organization_name
+        FROM 
+            shantytown_preparatory_phases_toward_resorption spptr
+        LEFT JOIN 
+            preparatory_phases_toward_resorption pptr ON spptr.fk_preparatory_phase = pptr.uid
+        LEFT JOIN
+            users us ON us.user_id = spptr.created_by
+        LEFT JOIN
+            organizations o ON o.organization_id = us.fk_organization
+        WHERE 
+            spptr.fk_shantytown IN (:ids)`,
+        {
+            type: QueryTypes.SELECT,
+            replacements: {
+                ids,
+            },
+        },
+    );
+    return serializePreparatoryPhasesTowardResorption(rows);
+};
