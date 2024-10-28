ef0ee5124b66f900ec26083cdb4db91955db155b
Christophe BENARD <ch.benard@gmail.com>
2024-10-28 14:06:06 +0100

    feat: Ajouter un type décrivant les phases préparatoires à la résorption refers to https://trello.com/c/M2OFZfb5/2252

--- packages/api/types/resources/Shantytown.d.ts
+++ packages/api/types/resources/Shantytown.d.ts
@@ -6,6 +6,7 @@ import { Attachment } from '#server/services/attachment/Attachment.d';
 import { ShantytownRawComment } from '#root/types/resources/ShantytownCommentRaw.d';
 import { ShantytownAction } from '#root/types/resources/Action.d';
 import { SocialOrigin } from '#root/types/resources/SocialOrigin.d';
+import { PreparatoryPhaseTowardResorption } from '#root/types/resources/PreparatoryPhasesTowardResorption.d';
 
 type ShantytownUser = {
     id: number,
@@ -131,6 +132,7 @@ type BaseShantytown =
     resorptionTarget: number | null,
     completionRate: number,
     distance?: number | null
+    preparatoryPhasesTowardResorption: PreparatoryPhaseTowardResorption[],
 };
 
 type ShantytownWithJustice = BaseShantytown & {
