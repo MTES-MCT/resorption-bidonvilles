a2d844a640cf8e9d7980e6775eabf086d9a5ab4b
Christophe BENARD <ch.benard@gmail.com>
2024-10-28 14:57:23 +0100

    feat: Ajouter le composant affichant les phases de résorption refers to https://trello.com/c/M2OFZfb5/2252

---
+++ packages/frontend/webapp/src/components/CarteSiteDetaillee/CarteSiteDetailleePhasesPreparatoiresResorption.vue
@@ -0,0 +1,40 @@
+<template>
+    <span class="sr-only">Phases préparatoires à la résorption du site</span>
+    <ul v-if="shantytown.livingConditions.version === 2">
+        <li
+            v-for="phase in shantytown.preparatoryPhasesTowardResorption"
+            :key="phase.preparatoryPhaseId"
+            class="flex items-center"
+        >
+            <!-- <span>{{ phase.preparatoryPhaseName }}</span> -->
+            <span
+                :class="[
+                    'flex rounded-full text-xs border-2 mr-3 mb-1 h-6 w-6 items-center justify-center text-green500 border-green500',
+                ]"
+                style="padding: 0.2em"
+                ariaHidden="true"
+            >
+                <Icon icon="check" />
+            </span>
+            <div>
+                {{ phase.preparatoryPhaseName }}
+            </div>
+        </li>
+    </ul>
+    <div v-else>
+        <Tag variant="pin_red"
+            >Les conditions de vie évoluent : mettez les à jour !</Tag
+        >
+    </div>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+import { Icon, Tag } from "@resorptionbidonvilles/ui";
+
+const props = defineProps({
+    shantytown: Object,
+});
+const { shantytown } = toRefs(props);
+</script>
