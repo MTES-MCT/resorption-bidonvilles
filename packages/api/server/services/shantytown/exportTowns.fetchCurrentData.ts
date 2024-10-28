615f98f93ec7c56bb590f8cab6899717e170a700
Christophe BENARD <ch.benard@gmail.com>
2024-10-28 17:03:35 +0100

    fix: Changer le type `User` en `AuthUser`

--- packages/api/server/services/shantytown/exportTowns.fetchCurrentData.ts
+++ packages/api/server/services/shantytown/exportTowns.fetchCurrentData.ts
@@ -4,10 +4,10 @@ import shantytownModel from '#server/models/shantytownModel';
 import { WhereClauseGroup } from '#server/models/_common/types/WhereClauseGroup';
 import { Where } from '#server/models/_common/types/Where';
 import { Location } from '#server/models/geoModel/Location.d';
-import { User } from '#root/types/resources/User.d';
+import { AuthUser } from '#server/middlewares/authMiddleware';
 import { Shantytown } from '#root/types/resources/Shantytown.d';
 
-export default async (user: User, locations: Location[], closedTowns: boolean): Promise<Shantytown[]> => {
+export default async (user: AuthUser, locations: Location[], closedTowns: boolean): Promise<Shantytown[]> => {
     const isNationalExport = locations.some(l => l.type === 'nation');
     const filters: Where = [
         {
