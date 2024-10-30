0ae01d83882f3d7599d53f15f98c856f2676f8b5
Gilles COGNIN <gillescognin@gmail.com>
2024-10-30 14:55:45 +0100

    feat: Ajout des contraintes dans la migration

--- packages/api/db/migrations/30000082-01-create-table-shantytown_decrees_relationships.js
+++ packages/api/db/migrations/30000082-01-create-table-shantytown_decrees_relationships.js
@@ -1,23 +1,65 @@
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.createTable(
-        'shantytown_decrees',
-        {
-            shantytown_decree_id: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                autoIncrement: true,
-                primaryKey: true,
-            },
-            fk_shantytown: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-            },
-            fk_attachment: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-            },
-        },
-    ),
+    up: async (queryInterface, Sequelize) => {
+        const transaction = await queryInterface.sequelize.transaction();
 
-    down: queryInterface => queryInterface.dropTable('shantytown_decrees'),
+        try {
+            await queryInterface.createTable(
+                'shantytown_decree_attachments',
+                {
+                    shantytown_decree_id: {
+                        type: Sequelize.INTEGER,
+                        allowNull: true,
+                        autoIncrement: true,
+                        primaryKey: true,
+                    },
+                    fk_shantytown: {
+                        type: Sequelize.INTEGER,
+                        allowNull: false,
+                    },
+                    fk_attachment: {
+                        type: Sequelize.INTEGER,
+                        allowNull: false,
+                    },
+                    attachment_type: {
+                        type: Sequelize.STRING,
+                        allowNull: false,
+                    },
+                },
+                { transaction },
+            );
+            await Promise.all([
+            // clés étrangères
+                queryInterface.addConstraint('shantytown_decree_attachments', {
+                    fields: ['fk_shantytown'],
+                    type: 'foreign key',
+                    name: 'fk_decree__shantytown',
+                    references: {
+                        table: 'shantytowns',
+                        field: 'shantytown_id',
+                    },
+                    onUpdate: 'cascade',
+                    onDelete: 'cascade',
+                    transaction,
+                }),
+                queryInterface.addConstraint('shantytown_decree_attachments', {
+                    fields: ['fk_attachment'],
+                    type: 'foreign key',
+                    name: 'fk_decree__attachment',
+                    references: {
+                        table: 'attachments',
+                        field: 'attachment_id',
+                    },
+                    onUpdate: 'cascade',
+                    onDelete: 'cascade',
+                    transaction,
+                }),
+            ]);
+            return transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+
+    down: queryInterface => queryInterface.dropTable('shantytown_decree_attachments'),
 };
