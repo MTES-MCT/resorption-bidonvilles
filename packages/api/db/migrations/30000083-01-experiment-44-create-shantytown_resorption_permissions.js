1decef4c7440ecf48a450bccdb4d22b8ba773d09
Christophe BENARD <ch.benard@gmail.com>
2024-11-08 12:20:26 +0100

    feat: Ajouter les admin nationaux aux permissions refers to https://trello.com/c/8MMnnupB/2249

--- packages/api/db/migrations/30000083-01-experiment-44-create-shantytown_resorption_permissions.js
+++ packages/api/db/migrations/30000083-01-experiment-44-create-shantytown_resorption_permissions.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 const { QueryTypes } = require('sequelize');
 
 const deptsInvolved = ('44');
@@ -9,17 +11,47 @@ function getInsertObject(user, feature) {
         fk_feature: feature,
         fk_entity: 'shantytown_resorption',
         allowed: true,
-        type: 'departement',
+        type: user.fk_role === 'national_admin' ? 'nation' : 'departement',
         fk_region: null,
-        fk_departement: user.fk_departement,
+        fk_departement: user.fk_role === 'national_admin' ? null : user.fk_departement,
         fk_epci: null,
         fk_city: null,
     };
 }
 
-async function insertPermissions(queryInterface, feature, user, transaction) {
-    const insertObject = getInsertObject(user, feature);
-    await queryInterface.bulkInsert('permissions', [insertObject], { transaction });
+async function createShantytownResorptionEntity(queryInterface, transaction) {
+    await queryInterface.bulkInsert(
+        'entities',
+        [{ name: 'shantytown_resorption' }],
+        { transaction },
+    );
+}
+
+async function createShantytownResorptionFeatures(queryInterface, features, transaction) {
+    const featuresObjectsToInsert = features.map(feature => ({
+        name: feature,
+        fk_entity: 'shantytown_resorption',
+        is_writing: true,
+    }));
+
+    await queryInterface.bulkInsert(
+        'features',
+        featuresObjectsToInsert,
+        { transaction },
+    );
+}
+
+async function insertPermissionsForUser(queryInterface, features, user, transaction) {
+    const insertObjects = features.map(feature => getInsertObject(user, feature));
+    await Promise.all(await queryInterface.bulkInsert('permissions', insertObjects, { transaction }));
+}
+
+async function insertPermissions(queryInterface, users, transaction) {
+    if (users.length > 0) {
+        for (const user of users) {
+            await insertPermissionsForUser(queryInterface, ['create', 'update'], user, transaction);
+        }
+    }
 }
 
 async function deleteFromTable(queryInterface, tableName, condition, transaction) {
@@ -29,62 +61,53 @@ async function deleteFromTable(queryInterface, tableName, condition, transaction
     );
 }
 
+async function getUsers(queryInterface, transaction) {
+    const users = await queryInterface.sequelize.query(
+        `SELECT DISTINCT ON (u.user_id, u.fk_role, u.fk_role_regular)
+                uap.fk_departement,
+                u.user_id,
+                u.fk_role,
+                u.fk_role_regular
+            FROM
+                users u
+            LEFT JOIN
+                user_actual_permissions uap ON uap.fk_user = u.user_id
+            WHERE
+                u.fk_status = 'active'
+            AND
+            (
+                (
+                    u.fk_role_regular = 'direct_collaborator'
+                    AND
+                        uap.fk_departement IN (:deptsCodes)
+                )
+                OR
+                    u.fk_role = 'national_admin'                    
+            )
+            ORDER BY 
+                u.user_id, u.fk_role, u.fk_role_regular, uap.fk_departement ;`,
+        {
+            transaction,
+            replacements: {
+                deptsCodes: deptsInvolved,
+            },
+            type: QueryTypes.SELECT,
+        },
+    );
+    return users.length > 0 ? users : [];
+}
+
 module.exports = {
     async up(queryInterface) {
         const transaction = await queryInterface.sequelize.transaction();
 
         try {
-            await queryInterface.bulkInsert(
-                'entities',
-                [{ name: 'shantytown_resorption' }],
-                { transaction },
-            );
-            await queryInterface.bulkInsert(
-                'features',
-                [{ name: 'create', fk_entity: 'shantytown_resorption', is_writing: true }],
-                { transaction },
-            );
-            await queryInterface.bulkInsert(
-                'features',
-                [{ name: 'update', fk_entity: 'shantytown_resorption', is_writing: true }],
-                { transaction },
-            );
+            await createShantytownResorptionEntity(queryInterface, transaction);
+            await createShantytownResorptionFeatures(queryInterface, ['create', 'update'], transaction);
+            const users = await getUsers(queryInterface, transaction);
 
-            const users = await queryInterface.sequelize.query(
-                `SELECT DISTINCT ON (u.user_id, u.fk_role_regular)
-                        uap.fk_departement,
-                        u.user_id,
-                        u.fk_role_regular
-                    FROM
-                        users u
-                    LEFT JOIN
-                        user_actual_permissions uap ON uap.fk_user = u.user_id
-                    WHERE
-                        u.fk_status = 'active'
-                    AND
-                        u.fk_role_regular = 'direct_collaborator'
-                    AND
-                        uap.fk_departement IN (:deptsCodes)
-                    ORDER BY 
-                        u.user_id, u.fk_role_regular, uap.fk_departement ;`,
-                {
-                    transaction,
-                    replacements: {
-                        deptsCodes: deptsInvolved,
-                    },
-                    type: QueryTypes.SELECT,
-                },
-            );
             if (users.length > 0) {
-                await Promise.all(users.map(async (user) => {
-                    try {
-                        await insertPermissions(queryInterface, 'create', user, transaction);
-                        await insertPermissions(queryInterface, 'update', user, transaction);
-                    } catch (err) {
-                        // Re-throw the error to propagate it to the main catch block
-                        throw err;
-                    }
-                }));
+                await insertPermissions(queryInterface, users, transaction);
             }
             return transaction.commit();
         } catch (err) {
