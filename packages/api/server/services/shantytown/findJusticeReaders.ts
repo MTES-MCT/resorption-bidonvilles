4099d7f0dc39273ea2f19ebedafed304b774933e
Gilles COGNIN <gillescognin@gmail.com>
2024-11-13 16:53:29 +0100

    chore: Suppression de log

--- packages/api/server/services/shantytown/findJusticeReaders.ts
+++ packages/api/server/services/shantytown/findJusticeReaders.ts
@@ -3,8 +3,6 @@ import ServiceError from '#server/errors/ServiceError';
 import { Organization } from '#root/types/resources/Organization.d';
 
 export default async (shantytownId: number, userId: number = null): Promise<Organization[]> => {
-    console.log('User ID: ', userId, 'Shantytown ID: ', shantytownId);
-
     try {
         return await organizationModel.findJusticeReadersByShantytown(shantytownId, userId);
     } catch (error) {
