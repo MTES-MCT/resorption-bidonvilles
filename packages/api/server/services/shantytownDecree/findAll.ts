6aa1551e6acea6230a6151d6f42575e72e35fdc4
Gilles COGNIN <gillescognin@gmail.com>
2024-11-13 17:52:59 +0100

    chore: Typage de shantytownDecrees

--- packages/api/server/services/shantytownDecree/findAll.ts
+++ packages/api/server/services/shantytownDecree/findAll.ts
@@ -1,6 +1,7 @@
 import shantytownModel from '#server/models/shantytownModel';
 import shantytownDecreeModel from '#server/models/shantytownDecreeModel';
 import ServiceError from '#server/errors/ServiceError';
+import { ShantytownDecree } from '#server/models/shantytownDecreeModel/shantytownDecrees.d';
 import { User } from '#root/types/resources/User.d';
 
 export default async (user: User, townId: number) => {
@@ -10,7 +11,7 @@ export default async (user: User, townId: number) => {
         throw new ServiceError('fetch_failed', new Error('Impossible de retrouver le site en base de données'));
     }
 
-    const shantytownDecrees = await shantytownDecreeModel.findAll(townId);
+    const shantytownDecrees: ShantytownDecree[] = await shantytownDecreeModel.findAll(townId);
 
     return shantytownDecrees;
 };
