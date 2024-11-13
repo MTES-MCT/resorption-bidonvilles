1e0689020c511518326b3735661017afa329739a
Gilles COGNIN <gillescognin@gmail.com>
2024-11-13 16:36:27 +0100

    chore: Suppression de saut de ligne en trop

--- packages/api/server/services/attachment/upload.ts
+++ packages/api/server/services/attachment/upload.ts
@@ -9,7 +9,6 @@ import { Transaction } from 'sequelize';
 import { v4 as uuidv4 } from 'uuid';
 import { ShantytownAttachmentObject } from '#root/types/resources/Shantytown.d';
 
-
 export default async (entityType: AttachmentEntityType, entityId: number, createdBy: number, files: Express.Multer.File[], transaction?: Transaction, attachmentType?: ShantytownAttachmentObject[]): Promise<any[]> => {
     const previews: Buffer[] = await Promise.all(
         files.map((f) => {
