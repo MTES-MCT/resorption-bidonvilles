2962451248f59d9af4142e4969d5239f4c8749ef
Christophe BENARD <ch.benard@gmail.com>
2024-11-13 16:32:22 +0100

    fix: Corriger le nom du type dans le type Shantytown refers to https://trello.com/c/8MMnnupB/2249

--- packages/api/types/resources/Shantytown.d.ts
+++ packages/api/types/resources/Shantytown.d.ts
@@ -6,7 +6,7 @@ import { Attachment } from '#server/services/attachment/Attachment.d';
 import { ShantytownRawComment } from '#root/types/resources/ShantytownCommentRaw.d';
 import { ShantytownAction } from '#root/types/resources/Action.d';
 import { SocialOrigin } from '#root/types/resources/SocialOrigin.d';
-import { PreparatoryPhaseTowardResorption } from '#root/types/resources/PreparatoryPhasesTowardResorption.d';
+import { ShantytownPreparatoryPhaseTowardResorption } from '#root/types/resources/ShantytownPreparatoryPhasesTowardResorption.d';
 
 type ShantytownUser = {
     id: number,
@@ -132,7 +132,7 @@ type BaseShantytown =
     resorptionTarget: number | null,
     completionRate: number,
     distance?: number | null
-    preparatoryPhasesTowardResorption: PreparatoryPhaseTowardResorption[],
+    preparatoryPhasesTowardResorption: ShantytownPreparatoryPhaseTowardResorption[],
 };
 
 type ShantytownWithJustice = BaseShantytown & {
