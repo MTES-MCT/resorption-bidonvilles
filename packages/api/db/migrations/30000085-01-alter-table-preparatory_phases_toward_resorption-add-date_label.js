6270e2158e5fd593f678385e86a4a7e1acf7f1df
Christophe BENARD <ch.benard@gmail.com>
2024-11-14 15:05:33 +0100

    feat: Ajouter le libellé pour les dates refers to https://trello.com/c/tSWMTnSk/2251

---
+++ packages/api/db/migrations/30000085-01-alter-table-preparatory_phases_toward_resorption-add-date_label.js
@@ -0,0 +1,65 @@
+const values = [
+    { uid: 'contract_preparation', name: 'Finalisé le' },
+    { uid: 'land_equipment_development', name: 'Terminé le' },
+    { uid: 'family_information', name: 'Réalisée le' },
+    { uid: 'contractualization_of_families', name: 'Terminée le' },
+    { uid: 'official_opening', name: 'Le' },
+    { uid: 'sociological_diagnosis', name: 'Réalisé le' },
+    { uid: 'social_assessment', name: 'Réalisée le' },
+    { uid: 'political_validation', name: 'Obtenue le' },
+];
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.addColumn(
+                'preparatory_phases_toward_resorption',
+                'date_label',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    defaultValue: '',
+                },
+                { transaction },
+            );
+
+            const promises = values.map(({ uid, name }) => queryInterface.bulkUpdate(
+                'preparatory_phases_toward_resorption',
+                {
+                    date_label: name,
+                },
+                {
+                    uid: {
+                        [Sequelize.Op.eq]: uid,
+                    },
+                },
+                { transaction },
+            ));
+            await Promise.all(promises);
+
+            return transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.removeColumn(
+                'preparatory_phases_toward_resorption',
+                'date_label',
+                { transaction },
+            );
+
+            return transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+};
