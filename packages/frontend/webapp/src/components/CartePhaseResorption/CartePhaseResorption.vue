a25230b0ddfed7ff9d28b9e2b5834d3660dfd174
Christophe BENARD <ch.benard@gmail.com>
2024-12-02 16:35:15 +0100

    feat: Ajouter les phases sur la liste des sites refers to https://trello.com/c/tSWMTnSk/2251

---
+++ packages/frontend/webapp/src/components/CartePhaseResorption/CartePhaseResorption.vue
@@ -0,0 +1,23 @@
+<template>
+    <article class="p-4 flex space-x-2 border">
+        <div class="flex-1">
+            <h3 class="font-bold">
+                {{ phase.preparatoryPhaseName }}
+            </h3>
+            <p v-if="phase.completedAt !== null">
+                {{ phase.preparatoryPhaseDateLabel }}:
+                {{ formatDate(phase.completedAt) }}
+            </p>
+        </div>
+    </article>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from "vue";
+import formatDate from "@common/utils/formatDate.js";
+
+const props = defineProps({
+    phase: Object,
+});
+const { phase } = toRefs(props);
+</script>
