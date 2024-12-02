e150b8a39d2f3d6ba0741a6bb677e359a1afa980
Christophe BENARD <ch.benard@gmail.com>
2024-12-02 16:17:40 +0100

    feat: Initialiser la propriété preparatoryPhasesTowardResorption refers to https://trello.com/c/tSWMTnSk/2251

--- packages/api/server/models/shantytownModel/_common/serializeShantytown.ts
+++ packages/api/server/models/shantytownModel/_common/serializeShantytown.ts
@@ -136,7 +136,7 @@ export default (town: ShantytownRow, user): Shantytown => {
         heatwaveStatus: town.heatwaveStatus,
         resorptionTarget: town.resorptionTarget,
         completionRate: 0,
-        preparatoryPhasesTowardResorption: null,
+        preparatoryPhasesTowardResorption: [],
     };
 
     // generé par findNearby
