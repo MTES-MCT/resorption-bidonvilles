2c0280bc2aea2d08ce57cad2c927b3f7bdd9c6a2
Christophe BENARD <ch.benard@gmail.com>
2024-12-09 11:41:44 +0100

    fix: rendre la création transactionnelle refers to https://trello.com/c/tSWMTnSk/2251

--- packages/api/server/models/shantytownPreparatoryPhasesTowardResorptionModel/create.ts
+++ packages/api/server/models/shantytownPreparatoryPhasesTowardResorptionModel/create.ts
@@ -1,25 +1,41 @@
 import { sequelize } from '#db/sequelize';
-import { QueryTypes } from 'sequelize';
+import { QueryTypes, Transaction } from 'sequelize';
 
 type ShantytownPreparatoryPhaseTowardResorptionParam = {
     fk_shantytown: number;
     fk_preparatory_phase: string;
     created_by: number;
+    completed_at?: number | string | null;
 };
 
-export default async (data: ShantytownPreparatoryPhaseTowardResorptionParam, transaction = undefined): Promise<number> => {
-    const result = await sequelize.query(
-        `INSERT INTO shantytown_preparatory_phases_toward_resorption (fk_shantytown, fk_preparatory_phase, created_by)
-            VALUES (:shantytownId, :preparatoryPhaseId, :createdBy) RETURNING fk_shantytown`,
-        {
-            type: QueryTypes.INSERT,
-            replacements: {
-                shantytownId: data.fk_shantytown,
-                preparatoryPhaseId: data.fk_preparatory_phase,
-                createdBy: data.created_by,
+export default async (data: ShantytownPreparatoryPhaseTowardResorptionParam, argTransaction: Transaction = undefined): Promise<number> => {
+    let transaction: Transaction = argTransaction;
+    if (transaction === undefined) {
+        transaction = await sequelize.transaction();
+    }
+    try {
+        const result = await sequelize.query(
+            `INSERT INTO shantytown_preparatory_phases_toward_resorption (fk_shantytown, fk_preparatory_phase, created_by ${data.completed_at ? ', completed_at' : ''})
+                VALUES (:shantytownId, :preparatoryPhaseId, :createdBy ${data.completed_at ? ', :completedAt' : ''}) RETURNING fk_shantytown`,
+            {
+                type: QueryTypes.INSERT,
+                replacements: {
+                    shantytownId: data.fk_shantytown,
+                    preparatoryPhaseId: data.fk_preparatory_phase,
+                    createdBy: data.created_by,
+                    completedAt: data.completed_at ? data.completed_at : null,
+                },
+                transaction,
             },
-            transaction,
-        },
-    );
-    return result[0][0].fk_shantytown;
+        );
+        if (argTransaction === undefined) {
+            await transaction.commit();
+        }
+        return result[0][0].fk_shantytown;
+    } catch (error) {
+        if (argTransaction === undefined) {
+            await transaction.rollback();
+        }
+        throw error;
+    }
 };
