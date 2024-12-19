919dcd0fa8c63b4d75578108bdd0f0241b4bfeb3
Christophe BENARD <ch.benard@gmail.com>
2024-12-19 16:24:03 +0100

    feat: Ajouter l'info "fait l'objet d'actions financées" refers to https://trello.com/c/0DccNZoS/2295

--- packages/api/server/services/shantytown/exportTowns.fetchCurrentData.ts
+++ packages/api/server/services/shantytown/exportTowns.fetchCurrentData.ts
@@ -1,13 +1,40 @@
+import moment from 'moment';
 import geoUtils from '#server/utils/geo';
 import shantytownModel from '#server/models/shantytownModel';
+import where from '#server/utils/permission/where';
 
 import { WhereClauseGroup } from '#server/models/_common/types/WhereClauseGroup';
 import { Where } from '#server/models/_common/types/Where';
 import { Location } from '#server/models/geoModel/Location.d';
+import actionModel from '#server/models/actionModel';
+import { ActionSelectRow } from '#server/models/actionModel/fetchByShantytown/fetchFinancedActionsByYear';
 import { User } from '#root/types/resources/User.d';
 import { Shantytown } from '#root/types/resources/Shantytown.d';
 
-export default async (user: User, locations: Location[], closedTowns: boolean): Promise<Shantytown[]> => {
+type ShantytownWithFinancedAction = Shantytown & {
+    hasAtLeastOneActionFinanced: boolean,
+};
+
+type FinancedShantytownAction = {
+    shantytown_id: number,
+    hasAtLeastOneActionFinanced: boolean,
+};
+
+function transformShantytowns(actions: ActionSelectRow[]): FinancedShantytownAction[] {
+    const shantytownMap = actions.reduce((acc, action) => {
+        if (!acc[action.shantytown_id]) {
+            acc[action.shantytown_id] = {
+                shantytown_id: action.shantytown_id,
+                hasAtLeastOneActionFinanced: action.financed,
+            };
+        }
+        return acc;
+    }, {});
+
+    return Object.values(shantytownMap);
+}
+
+export default async (user: User, locations: Location[], closedTowns: boolean): Promise<ShantytownWithFinancedAction[]> => {
     const isNationalExport = locations.some(l => l.type === 'nation');
     const filters: Where = [
         {
@@ -38,5 +65,18 @@ export default async (user: User, locations: Location[], closedTowns: boolean): 
         );
     }
 
-    return shantytownModel.findAll(user, filters, 'export');
+    const towns = await shantytownModel.findAll(user, filters, 'export');
+
+    const clauseGroup = where().can(user).do('export', 'action');
+    const currentYear = moment(new Date()).format('YYYY');
+    const townsWithFinancedActions = await actionModel.fetchFinancedActionsByYear(null, parseInt(currentYear, 10), clauseGroup);
+    const transformedShantytowns = transformShantytowns(townsWithFinancedActions);
+    return towns.map((town: ShantytownWithFinancedAction) => {
+        const townWithFinancedActions: FinancedShantytownAction = transformedShantytowns.find((t:FinancedShantytownAction) => t.shantytown_id === town.id);
+        if (townWithFinancedActions) {
+            // eslint-disable-next-line no-param-reassign
+            town.hasAtLeastOneActionFinanced = townWithFinancedActions.hasAtLeastOneActionFinanced;
+        }
+        return town;
+    });
 };
