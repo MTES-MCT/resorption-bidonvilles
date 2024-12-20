77d357d6ba2a4a3549ceaf3c979f6e9c0441624d
Christophe BENARD <ch.benard@gmail.com>
2024-12-20 15:55:27 +0100

    fix: Préciser l'intitulé d'une colonne refers to https://trello.com/c/0DccNZoS/2295

--- packages/api/server/services/shantytown/_common/serializeExportProperties.ts
+++ packages/api/server/services/shantytown/_common/serializeExportProperties.ts
@@ -1196,7 +1196,7 @@ export default (closingSolutions: ClosingSolution[]) => {
         },
 
         hasAtLeastOneActionFinanced: {
-            title: 'Le site fait-il l’objet d’une action financée ?',
+            title: 'Le site fait-il l’objet d’une action financée l’année correspondant à la date d’extraction ?',
             data: (shantytown: ShantytownWithFinancedAction) => {
                 if (!('hasAtLeastOneActionFinanced' in shantytown)) {
                     return 'non';
