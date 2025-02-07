bb0c60836965bd9eecd50e032bf4b9fcc6cc282d
Christophe BENARD <ch.benard@gmail.com>
2025-02-07 13:48:10 +0100

    feat: Ajouter une config pour l'accès à la BDD Majic refers to https://trello.com/c/v5HKcOAV/2301

--- packages/api/db/config/config.js
+++ packages/api/db/config/config.js
@@ -4,11 +4,12 @@ const pg = require('pg');
 dotenv.config();
 
 function getConfig(database) {
+    const port = database === 'majic' ? `${process.env.RB_API_MAJIC_POSTGRES_PORT}` : `${process.env.RB_API_POSTGRES_PORT}`;
     return {
         username: process.env.RB_API_POSTGRES_USER,
         password: process.env.RB_API_POSTGRES_PASSWORD,
         host: process.env.RB_API_POSTGRES_HOST,
-        port: parseInt(process.env.RB_API_POSTGRES_PORT, 10),
+        port: parseInt(port, 10),
         database,
         dialect: 'postgres',
         dialectModule: pg,
@@ -32,6 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 const configs = {
     development: getConfig(`${process.env.RB_API_POSTGRES_DB}`),
     production: getConfig(process.env.RB_API_POSTGRES_DB),
+    majic: getConfig(process.env.RB_API_MAJIC_POSTGRES_DB),
     test: getConfig(`${process.env.RB_API_POSTGRES_DB}_e2e`),
 };
 configs.default = configs[env];
