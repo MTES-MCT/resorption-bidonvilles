5a1c177d5539735b364754141656234a43692f0a
Christophe BENARD <ch.benard@gmail.com>
2025-02-10 13:37:45 +0100

    fix: Supprimer le risque d'injection SQL refers to https://trello.com/c/v5HKcOAV/2301

--- packages/api/server/models/majicModel/findParcel/findParcel.ts
+++ packages/api/server/models/majicModel/findParcel/findParcel.ts
@@ -1,11 +1,22 @@
 import { sequelize } from '#db/majicSequelize';
 import { QueryTypes } from 'sequelize';
 import { RawParcel } from './RawParcel.d';
+import { getAllowedSchemas, getAllowedTables } from '../common/getWhiteLists';
 
-export default async (id: string, schema: string, tableName: string): Promise<RawParcel> => {
+export default async (id: string, dept: string, schema: string, shortTableName: string, tableName: string): Promise<RawParcel> => {
+    const currentYear = new Date().getFullYear();
+    const ALLOWED_SCHEMAS = getAllowedSchemas(currentYear);
+    const ALLOWED_TABLES = getAllowedTables(currentYear, dept, shortTableName);
+
+    if (!ALLOWED_SCHEMAS.includes(schema) || !ALLOWED_TABLES.includes(tableName)) {
+        return null;
+    }
+
+    const fullTableName = `"${schema}"."${tableName}"`;
+
     const parcelles: RawParcel[] = await sequelize.query(
         `SELECT idpar, idcom, dnupro, dnuvoi, cconvo, dvoilib, idcomtxt
-            FROM ${schema}.${tableName} fpp 
+            FROM ${fullTableName} fpp 
             WHERE fpp.idpar = :id`,
         {
             type: QueryTypes.SELECT,
