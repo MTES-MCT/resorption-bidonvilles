0967aba438a8f7eb8504beea26b3fdf073c417ce
Christophe BENARD <ch.benard@gmail.com>
2025-02-24 15:34:21 +0100

    wording: changer la formule "évacuation effectuée" en "évacuation planifiée" refers to https://trello.com/c/0fd4wWCO/2339

--- packages/frontend/webapp/src/utils/enrichShantytown.js
+++ packages/frontend/webapp/src/utils/enrichShantytown.js
@@ -64,7 +64,7 @@ export default function (shantytown, fieldTypes) {
             justiceStatuses.push({
                 section: "evacuationUnderTimeLimit",
                 icon: "file-contract",
-                label: "Évacuation effectuée",
+                label: "Évacuation planifiée",
                 date: shantytown.administrativeOrderEvacuationAt,
             });
         }
