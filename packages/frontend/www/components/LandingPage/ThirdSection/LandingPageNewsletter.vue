b2429f7f4ba64652824380c8cb64278cc59bce4f
Gilles COGNIN <gillescognin@gmail.com>
2025-02-26 16:50:12 +0100

    Wording: Remplacement du lien erroné d'inscription à la newsletter

--- packages/frontend/www/components/LandingPage/ThirdSection/LandingPageNewsletter.vue
+++ packages/frontend/www/components/LandingPage/ThirdSection/LandingPageNewsletter.vue
@@ -6,12 +6,8 @@
         <p class="mb-4 max-w-screen-sm mx-auto">
             {{ $t("landingPage.newsletter.text") }}
         </p>
-        <Button variant="primary" href="https://email.developpement-durable.gouv.fr/users/subscribe/js_id/5n4i/id/3">{{
+        <Button variant="primary" href=" https://a757ac69.sibforms.com/serve/MUIFALVz73sp9nySQNDVaSPuG57ypOIGQrx7oMDqyu-lukbiAq1DqhoTh4UQfghOgE-jTVCzMUDQJ6CAQG5GtpsztQ3C3hPleVgcZDEEU0Y_3aPMffVdQjm_YRNdGAnjF4sET4aCQynW4QVbe1bjXnRuyTR0ETJCNgdje0QbaxOzYTKMIAPCdKNJkcYdS3Boj9Vsbj1RXYTzE_Q=">{{
             $t("landingPage.newsletter.cta") }}</Button>
-        <p class="mt-6">
-            <a class="link" href="https://www.gouvernement.fr/resorption-des-bidonvilles">{{
-                $t("landingPage.newsletter.viewAll") }}</a>
-        </p>
     </div>
 </template>
 
