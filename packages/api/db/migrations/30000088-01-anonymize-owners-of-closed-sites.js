40189e95a0947342bcb50fa4af2ea6a928c3cc3f
Christophe BENARD <ch.benard@gmail.com>
2025-03-10 13:47:19 +0100

    feat: Ajouter une migration pour anonymiser les propriétaires sur les sites déjà fernés refers to https://trello.com/c/Do3PDf4K/2356

---
+++ packages/api/db/migrations/30000088-01-anonymize-owners-of-closed-sites.js
@@ -0,0 +1,41 @@
+module.exports = {
+    async up(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await Promise.all([
+                queryInterface.sequelize.query(
+                    `UPDATE shantytowns SET owner = NULL WHERE shantytown_id IN
+                        (
+                            SELECT shantytown_id FROM shantytowns WHERE status  NOT LIKE 'open' AND owner IS NOT NULL
+                        ) ;`,
+                    {
+                        type: queryInterface.sequelize.QueryTypes.UPDATE,
+                        transaction,
+                    },
+                ),
+                queryInterface.sequelize.query(
+                    `UPDATE "ShantytownHistories" 
+                    SET owner = NULL
+                    WHERE 
+                        shantytown_id IN 
+                            (
+                                SELECT shantytown_id FROM shantytowns WHERE status  NOT LIKE 'open' AND owner IS NOT NULL
+                            )
+                        AND
+                            owner IS NOT NULL ;`,
+                    {
+                        type: queryInterface.sequelize.QueryTypes.UPDATE,
+                        transaction,
+                    },
+                ),
+            ]);
+            return transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+
+    down: () => Promise.resolve(),
+};
