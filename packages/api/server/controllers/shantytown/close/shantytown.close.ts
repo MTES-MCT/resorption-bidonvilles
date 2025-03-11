a3956b8db7f6c6df86d9581ab54c8df780b89900
Christophe BENARD <ch.benard@gmail.com>
2025-03-11 20:25:59 +0100

    fix: Améliorer la gestion des erreurs retournées par le service refers to https://trello.com/c/Do3PDf4K/2356

--- packages/api/server/controllers/shantytown/close/shantytown.close.ts
+++ packages/api/server/controllers/shantytown/close/shantytown.close.ts
@@ -1,14 +1,24 @@
 import shantytownService from '#server/services/shantytown';
 
+const ERROR_RESPONSES = {
+    closing_shantytown_not_allowed: { code: 403, message: 'Vous n\'êtes pas autorisé à fermer ce site' },
+    update_failed: { code: 400, message: 'Une mise à jour en base de données a échoué' },
+    anonymisation_failed: { code: 400, message: 'L\'anonymisation du nom du popriétaire a échoué' },
+    fetch_failed: { code: 400, message: 'Une lecture en base de données a échoué' },
+    undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
+};
+
 export default async (req, res, next) => {
     // close the town
     try {
         const updatedTown = await shantytownService.close(req.user, req.body);
         return res.status(200).send(updatedTown);
-    } catch (e) {
-        res.status(500).send({
-            user_message: 'Une erreur est survenue dans l\'enregistrement du site en base de données',
+    } catch (error) {
+        const { code, message } = ERROR_RESPONSES[error && error.code] || ERROR_RESPONSES.undefined;
+
+        res.status(code).send({
+            user_message: message,
         });
-        return next(e);
+        return next(error.nativeError || error);
     }
 };
