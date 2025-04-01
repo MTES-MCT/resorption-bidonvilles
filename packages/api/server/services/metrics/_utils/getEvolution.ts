3a2d2833c0d4156590b74d85baf2d56ada5edb23
Gilles COGNIN <gillescognin@gmail.com>
2025-04-01 17:48:17 +0200

    feat: Prise en compte des cas d'évolution partant de 0

--- packages/api/server/services/metrics/_utils/getEvolution.ts
+++ packages/api/server/services/metrics/_utils/getEvolution.ts
@@ -1,7 +1,9 @@
+// export default (initialValue: number, finalValue: number): number => {
 export default (initialValue: number, finalValue: number): number => {
     if (initialValue === 0) {
-        return 0;
+        return null;
     }
     const percentage = Math.round(((finalValue - initialValue) / initialValue) * 100);
+
     return Number.isNaN(percentage) ? 0 : percentage;
 };
