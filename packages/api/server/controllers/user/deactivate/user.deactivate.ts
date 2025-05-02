e2bcbde25e39b8c1e5ecc9e2ee19b3fb94f6cbd7
Christophe BENARD <ch.benard@gmail.com>
2025-05-02 10:23:43 +0200

    fix: AJouter deux messages d'erreur et régler les conflits Récupérer l'id du user à désactivzer depuis le paramètre de requête refers to https://trello.com/c/W0uSVxko/2395

--- packages/api/server/controllers/user/deactivate/user.deactivate.ts
+++ packages/api/server/controllers/user/deactivate/user.deactivate.ts
@@ -4,9 +4,11 @@ import { User } from '#root/types/resources/User.d';
 
 const ERRORS = {
     undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
+    deactivation_permission_failure: { code: 500, message: 'Vous n\'avez pas la perlmission de désactiver ce compte' },
     deactivation_failure: { code: 500, message: 'Une erreur est survenue lors de la désactivation du compte' },
     refresh_failure: { code: 500, message: 'Une erreur est survenue lors de la lecture des données en base' },
     transaction_failure: { code: 500, message: 'Une erreur est survenue lors de la validation de la désactivation' },
+    user_already_inactive: { code: 400, message: 'L\'utilisateur est déjà inactif' },
 };
 
 interface UserDeactivateRequest extends Request {
@@ -20,7 +22,8 @@ interface UserDeactivateRequest extends Request {
 
 export default async (req: UserDeactivateRequest, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const updatedUser = await userService.deactivate(req.body.user.id, req.user.id === req.body.user.id, req.body.user, req.body.reason, req.body.anonymizationRequested);
+        const userId = parseInt(req.params.id, 10);
+        const updatedUser = await userService.deactivate(userId, userId === req.user.id, req.user, req.body.reason, req.body.anonymizationRequested);
         res.status(200).send(updatedUser);
     } catch (error) {
         const { code, message } = ERRORS[error?.code] || ERRORS.undefined;
