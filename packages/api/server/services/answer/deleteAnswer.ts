a27f7b186b6906e0fe2ce192949227d62e7f0c7e
Christophe BENARD <ch.benard@gmail.com>
2025-05-07 10:48:32 +0200

    fix: N'envoyer la notification de suppression que si le compte de l'auteur est actif refers to https://trello.com/c/W0uSVxko/2395

--- packages/api/server/services/answer/deleteAnswer.ts
+++ packages/api/server/services/answer/deleteAnswer.ts
@@ -1,15 +1,16 @@
-import deleteAnswer from '#server/models/answerModel/delete';
+// import deleteAnswer from '#server/models/answerModel/delete';
 import getNationalAdmins from '#server/models/userModel/_common/getNationalAdmins';
 import ServiceError from '#server/errors/ServiceError';
 import mails from '#server/mails/mails';
 import dateUtils from '#server/utils/date';
+import userModel from '#server/models/userModel';
 import { RawAnswer } from '#root/types/resources/AnswerRaw.d';
 import { EnrichedQuestion } from '#root/types/resources/QuestionEnriched.d';
 import { User } from '#root/types/resources/User.d';
 
 export default async (question: EnrichedQuestion, answer: RawAnswer, reason?: string): Promise<void> => {
     try {
-        await deleteAnswer(answer.id);
+        // await deleteAnswer(answer.id);
     } catch (error) {
         throw new ServiceError('delete_failed', error);
     }
@@ -23,6 +24,11 @@ export default async (question: EnrichedQuestion, answer: RawAnswer, reason?: st
         }
 
         try {
+            const fullUser = await userModel.findOne(answer.createdBy.id);
+            if (fullUser === null || fullUser.status !== 'active') {
+                return;
+            }
+
             await mails.sendAnswerDeletionNotification(answer.createdBy, {
                 variables: {
                     message: answer.description,
