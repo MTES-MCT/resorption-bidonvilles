6f47d34dbc8afe1eb6d9d9e19973673f4b1694b0
Gilles COGNIN <gillescognin@gmail.com>
2025-05-20 01:00:04 +0200

    design: Correction CSS du bouton "Contactez-nous"

--- packages/frontend/webapp/src/components/ButtonContact/ButtonContact.vue
+++ packages/frontend/webapp/src/components/ButtonContact/ButtonContact.vue
@@ -1,5 +1,8 @@
 <template>
-    <Button variant="primaryOutline" :href="`mailto:${CONTACT_EMAIL}`"
+    <Button
+        variant="primaryOutline"
+        class="!border-2 !border-primary hover:!bg-primaryDark"
+        :href="`mailto:${CONTACT_EMAIL}`"
         >Contactez-nous</Button
     >
 </template>
@@ -9,3 +12,8 @@ import ENV from "@/helpers/env.js";
 import { Button } from "@resorptionbidonvilles/ui";
 const { CONTACT_EMAIL } = ENV;
 </script>
+<style scoped>
+button {
+    border: inherit;
+}
+</style>
