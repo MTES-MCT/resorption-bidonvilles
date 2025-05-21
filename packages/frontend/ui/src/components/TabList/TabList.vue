abe19a73e78cd9cb2b44ab8a84e621245c03997a
Christophe BENARD <ch.benard@gmail.com>
2025-05-21 18:02:09 +0200

    Fix: Formater les nombres en séparant les milliers refers to https://trello.com/c/ohVmlYzu/2407

--- packages/frontend/ui/src/components/TabList/TabList.vue
+++ packages/frontend/ui/src/components/TabList/TabList.vue
@@ -1,54 +1,58 @@
 <template>
     <div>
-        <Tab v-for="tab in tabs" :key="tab.id" :active="activeTab === tab.id" @click="onTabClick(tab.id)">
+        <Tab 
+            v-for="tab in tabs" 
+            :key="tab.id" 
+            :active="activeTab === tab.id" 
+            @click="onTabClick(tab.id)"
+        >
             <template v-if="tab.id === 'inProgress'" v-slot:ofwhich>dont</template>
-            <template v-if="tab.total !== undefined" v-slot:prefix>{{ tab.total }}</template>
+            <template v-if="tab.total !== undefined" v-slot:prefix>
+                {{ formatNumber(tab.total) }}
+            </template>
             {{ tab.label }}
         </Tab>
     </div>
 </template>
 
-<script>
-import Tab from "./Tab.vue";
+<script setup>
+import { ref, watch, computed } from 'vue'
+import Tab from "./Tab.vue"
+import useNumberFormat from '../../composables/useNumberFormat'
 
-export default {
-    components: {
-        Tab
+const props = defineProps({
+    tabs: {
+        type: Array,
+        required: true
     },
-    props: {
-        tabs: {
-            type: Array,
-            required: true
-        },
-        modelValue: {
-            type: [String, Number],
-            required: false
-        }
-    },
-    data() {
-        const defaultActiveTab =
-            this.tabs.length > 0 ? this.tabs[0].id : undefined;
-
-        return {
-            activeTab: this.modelValue !== undefined ? this.modelValue : defaultActiveTab
-        };
-    },
-    watch: {
-        modelValue() {
-            this.activeTab = this.modelValue;
-        },
-        tabs() {
-            if (!this.tabs.some(({ id }) => id === this.activeTab)) {
-                this.activeTab =
-                    this.tabs.length > 0 ? this.tabs[0].id : undefined;
-            }
-        }
-    },
-    methods: {
-        onTabClick(id) {
-            this.activeTab = id;
-            this.$emit("update:modelValue", id);
-        }
+    modelValue: {
+        type: [String, Number],
+        required: false
     }
-};
+});
+
+const { formatNumber } = useNumberFormat()
+
+const emit = defineEmits(['update:modelValue'])
+
+const defaultActiveTab = computed(() => 
+    props.tabs.length > 0 ? props.tabs[0].id : undefined
+)
+
+const activeTab = ref(props.modelValue !== undefined ? props.modelValue : defaultActiveTab.value)
+
+watch(() => props.modelValue, (newValue) => {
+    activeTab.value = newValue
+})
+
+watch(() => props.tabs, () => {
+    if (!props.tabs.some(({ id }) => id === activeTab.value)) {
+        activeTab.value = props.tabs.length > 0 ? props.tabs[0].id : undefined
+    }
+}, { deep: true })
+
+const onTabClick = (id) => {
+    activeTab.value = id
+    emit('update:modelValue', id)
+}
 </script>
