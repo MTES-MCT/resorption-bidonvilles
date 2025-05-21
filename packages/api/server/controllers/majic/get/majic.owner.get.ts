f9f4b4ace8a80179d9fc2dcab475ad5f89d05cbf
Christophe BENARD <ch.benard@gmail.com>
2025-05-21 15:11:18 +0200

    Fix: Utiliser l'opérateur de coalescence nul (`??`) au lieu de l'opérateur logique ou (`||`), qui est plus sûr. refers to https://trello.com/c/v5HKcOAV/2301

--- packages/api/server/controllers/majic/get/majic.owner.get.ts
+++ packages/api/server/controllers/majic/get/majic.owner.get.ts
@@ -29,7 +29,7 @@ export default async (req: MajicParcelGetRequest, res: Response, next: NextFunct
         const { code } = ERROR_RESPONSES[error?.code] ?? ERROR_RESPONSES.undefined;
 
         res.status(code).send({
-            user_message: `${error.nativeError?.message || error.message || 'Une erreur inconnue est survenue'}`,
+            user_message: `${error.nativeError?.message ?? error.message ?? 'Une erreur inconnue est survenue'}`,
         });
         return next(error.nativeError || error);
     }
