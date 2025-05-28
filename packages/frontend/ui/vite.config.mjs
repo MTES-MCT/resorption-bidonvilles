4c123839bb831172af8fd2973eb0358b2f308caa
Gilles COGNIN <gillescognin@gmail.com>
2025-05-28 10:56:32 +0200

    chore: Suppression de commentaires

--- packages/frontend/ui/vite.config.mjs
+++ packages/frontend/ui/vite.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  // ... autres configurations
   resolve: {
     alias: {
       "@common": fileURLToPath(new URL("../common", import.meta.url)),
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      // Ajoutez d'autres alias si nécessaire
     },
   },
 })
