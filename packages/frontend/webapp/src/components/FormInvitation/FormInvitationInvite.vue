7101e8c5f8c64ff3b66a4de165eac6271680e3fe
Gilles COGNIN <gillescognin@gmail.com>
2025-06-13 10:46:02 +0200

    fix: Correction de la couleur du composant "Retirer"

--- packages/frontend/webapp/src/components/FormInvitation/FormInvitationInvite.vue
+++ packages/frontend/webapp/src/components/FormInvitation/FormInvitationInvite.vue
@@ -1,11 +1,11 @@
 <template>
     <article class="border border-G400 flex w-96 hover:border-G500">
         <div class="w-32 h-32 bg-G200 flex items-center justify-center">
-            <img :src="avatarImg" class="w-28" />
+            <img :src="avatarImg" class="w-28" alt="invité" />
         </div>
         <div class="flex-1 flex flex-col justify-center px-4 relative">
             <a
-                class="absolute right-2 top-2 text-primary text-sm hover:underline !cursor-pointer"
+                class="absolute right-2 top-2 !text-primary text-sm hover:underline !cursor-pointer"
                 @click="$emit('remove')"
                 ><Icon icon="fa-regular fa-trash-alt" /> Retirer</a
             >
