c0973af106eb9e1359a07db6db5d846cf1cf6f30
Victor Hristea <victor.hristea@epitech.eu>
2025-06-16 11:25:52 +0200

    Feat: Update du parcours utilisateur pour simplifier la sélection de la structure de l'utilisateur refers to https://trello.com/c/djFTsrya/2446

---
+++ packages/frontend/webapp/src/components/FormUtilisateur/inputs/FormUtilisateurInputStructure.vue
@@ -0,0 +1,13 @@
+<template>
+    <InputStructure
+        name="Structures"
+        :label="label"
+        placeholder="Nom de votre Structure, Collectivité territoriale, Association, Autre ?"
+        showCategory
+        showMandatoryStar
+    />
+</template>
+
+<script setup>
+import InputStructure from "@/components/InputStructure/InputStructure.vue";
+</script>
