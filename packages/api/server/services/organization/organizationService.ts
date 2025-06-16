f308a3fdd237e2689208fdcc1c66d5bef1781e54
Victor Hristea <victor.hristea@epitech.eu>
2025-06-16 11:10:13 +0200

    Feat: créer un service retournant l'ensemble des structures refers to https://trello.com/c/djFTsrya/2446

--- packages/api/server/services/organization/organizationService.ts
+++ packages/api/server/services/organization/organizationService.ts
@@ -1,9 +1,11 @@
 import findJusticeReadersByLocation from './findJusticeReadersByLocation/findJusticeReadersByLocation';
 import search from './search/search';
 import searchTerritorialCollectivities from './searchTerritorialCollectivities/searchTerritorialCollectivities';
+import listAll from './listAll/listAll';
 
 export default {
     findJusticeReadersByLocation,
     search,
     searchTerritorialCollectivities,
+    listAll,
 };
