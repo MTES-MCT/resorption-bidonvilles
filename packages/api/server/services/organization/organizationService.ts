7bbf0dc4ee0014083e16c26a7cbc44fb5e61cf66
Victor Hristea <victor.hristea@epitech.eu>
2025-06-18 11:31:07 +0200

    Fix: supprimer du code dupliqué et résoudre probleme de lint refers to https://trello.com/c/djFTsrya/2446

--- packages/api/server/services/organization/organizationService.ts
+++ packages/api/server/services/organization/organizationService.ts
@@ -1,11 +1,9 @@
 import findJusticeReadersByLocation from './findJusticeReadersByLocation/findJusticeReadersByLocation';
 import search from './search/search';
-import searchTerritorialCollectivities from './searchTerritorialCollectivities/searchTerritorialCollectivities';
 import listAll from './listAll/listAll';
 
 export default {
     findJusticeReadersByLocation,
     search,
-    searchTerritorialCollectivities,
     listAll,
 };
