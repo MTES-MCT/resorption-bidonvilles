d33931bdd0aa9092d63f15922ecff3707cbf2113
Christophe BENARD <ch.benard@gmail.com>
2025-06-25 15:36:20 +0200

    Fix: Typer location refers to https://trello.com/c/8hTtUFm4/2442

--- packages/api/server/controllers/actionFinanceReader/listForNewAction/actionFinanceReader.listForNewAction.validator.ts
+++ packages/api/server/controllers/actionFinanceReader/listForNewAction/actionFinanceReader.listForNewAction.validator.ts
@@ -1,12 +1,13 @@
 /* eslint-disable newline-per-chained-call */
 import { param, query } from 'express-validator';
 import geoModel from '#server/models/geoModel/index';
+import type { Location as GeoLocation } from '#server/models/geoModel/Location';
 
 export default [
     param('departement')
         .exists({ checkNull: true }).bail().withMessage('La localisation est obligatoire')
         .custom(async (value, { req }) => {
-            let location;
+            let location: GeoLocation;
             try {
                 location = await geoModel.getLocation('departement', value);
             } catch (e) {
