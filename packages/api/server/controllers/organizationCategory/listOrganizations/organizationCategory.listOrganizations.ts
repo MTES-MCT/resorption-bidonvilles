a1af39b6f5876485d92ae06dfb6c1b066d7f794f
Christophe BENARD <ch.benard@gmail.com>
2025-06-26 15:59:08 +0200

    Fix: utiliser l'opérateur de coalescence nul (??) au lieu de l'opérateur logique ou (`||`)  refers to https://trello.com/c/8hTtUFm4/2442

--- packages/api/server/controllers/organizationCategory/listOrganizations/organizationCategory.listOrganizations.ts
+++ packages/api/server/controllers/organizationCategory/listOrganizations/organizationCategory.listOrganizations.ts
@@ -1,5 +1,5 @@
 import organizationModel from '#server/models/organizationModel';
 
 export default async (req, res) => res.status(200).send({
-    organizations: await organizationModel.findByCategory(req.params.categoryId, req.query.search || null),
+    organizations: await organizationModel.findByCategory(req.params.categoryId, req.query.search ?? null),
 });
