5ab1569e39a1641d32aa7cc9cf9edb1e03a5ae0a
Gilles COGNIN <gillescognin@gmail.com>
2025-06-30 16:12:01 +0200

    fix: Correction du test unitaire

--- packages/api/server/services/action/deleteComment.ts
+++ packages/api/server/services/action/deleteComment.ts
@@ -43,7 +43,6 @@ export default async (user, actionId, commentId, deletionMessage): Promise<{ com
         city: actions[0].location.city,
     };
 
-
     const isOwner: boolean = author.id === user.id;
     if (!isOwner && !permissionUtils.can(user).do('moderate', 'data').on(location)) {
         throw new ServiceError('permission_denied', new Error('Vous n\'avez pas accès à ces données'));
@@ -80,6 +79,7 @@ export default async (user, actionId, commentId, deletionMessage): Promise<{ com
                 },
                 message,
             };
+
             if (author.status === 'active') {
                 await mails.sendUserCommentDeletion(author, {
                     variables: mailVariables,
