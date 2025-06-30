092cd45fcb70ced296e1ecb1ab474838e39d68e9
Gilles COGNIN <gillescognin@gmail.com>
2025-06-30 11:48:20 +0200

    fix: Corrections partielles et désactivation temporaire

--- packages/api/server/services/shantytownComment/createComment.spec.ts
+++ packages/api/server/services/shantytownComment/createComment.spec.ts
@@ -1,98 +1,121 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import SequelizeMock from 'sequelize-mock';
-import shantytownCommentModel from '#server/models/shantytownCommentModel';
-import shantytownCommentTagModel from '#server/models/shantytownCommentTagModel';
-import shantytownModel from '#server/models/shantytownModel';
-import userModel from '#server/models/userModel';
-import attachmentService from '#server/services/attachment';
-import mattermostUtils from '#server/utils/mattermost';
-import mails from '#server/mails/mails';
-import createComment from '#server/services/shantytownComment/createComment';
+import chaiSubset from 'chai-subset';
+
+import { rewiremock } from '#test/rewiremock';
 import { serialized as fakeUser } from '#test/utils/user';
 import { serialized as fakeShantytown } from '#test/utils/shantytown';
 import fakeFile from '#test/utils/file';
-import { Transaction } from 'sequelize';
 import fakeCommentInput from '#test/utils/shantytownCommentInput';
 import { ShantytownCommentAuthor } from '#root/types/resources/ShantytownCommentGeneric.d';
-import * as enrichCommentsAttachments from '../shantytown/_common/enrichCommentsAttachments';
 import { ShantytownEnrichedComment } from '#root/types/resources/ShantytownCommentEnriched.d';
-
-chai.use(sinonChai);
+import scanAttachmentErrors from '../attachment/scanAttachmentErrors';
 
 const { expect } = chai;
+chai.use(sinonChai);
+chai.use(chaiSubset);
+
+const sandbox = sinon.createSandbox();
+const stubs = {
+    shantytownCommentModel: {
+        create: sandbox.stub(),
+        findOne: sandbox.stub(),
+    },
+    shantytownCommentTagModel: {
+        create: sandbox.stub(),
+    },
+    shantytownModel: {
+        getComments: sandbox.stub(),
+    },
+    userModel: {
+        getShantytownWatchers: sandbox.stub(),
+    },
+    attachment: {
+        upload: sandbox.stub(),
+        serializeAttachment: sandbox.stub(),
+    },
+    mattermostUtils: {
+        triggerNewComment: sandbox.stub(),
+    },
+    mails: {
+        sendUserNewComment: sandbox.stub(),
+    },
+    sequelize: {
+        transaction: sandbox.stub(),
+    },
+    transaction: {
+        commit: sandbox.stub(),
+        rollback: sandbox.stub(),
+    },
+    enrichCommentsAttachments: sandbox.stub(),
+};
+
+rewiremock('#server/models/shantytownCommentModel').with(stubs.shantytownCommentModel);
+rewiremock('#server/models/shantytownCommentTagModel').with(stubs.shantytownCommentTagModel);
+rewiremock('#server/models/shantytownModel').with(stubs.shantytownModel);
+rewiremock('#server/models/userModel').with(stubs.userModel);
+rewiremock('#server/services/attachment').with(stubs.attachment);
+rewiremock('#server/utils/mattermost').with(stubs.mattermostUtils);
+rewiremock('#server/mails/mails').with(stubs.mails);
+rewiremock('#db/sequelize').with({ sequelize: stubs.sequelize });
+rewiremock('../shantytown/_common/enrichCommentsAttachments').with(stubs.enrichCommentsAttachments);
+rewiremock('#server/services/attachment/scanAttachmentErrors').with(scanAttachmentErrors);
+
+rewiremock.enable();
+// eslint-disable-next-line import/newline-after-import, import/first
+import createComment from './createComment';
+rewiremock.disable();
 
 
-const sequelizeStub = new SequelizeMock();
+describe('services/shantytownComment.create', () => {
+    let fakeEnrichedComment: () => ShantytownEnrichedComment;
+    let fakeAuthor: () => ShantytownCommentAuthor;
 
-const fakeAuthor = (): ShantytownCommentAuthor => ({
-    id: fakeUser().id,
-    first_name: fakeUser().first_name,
-    last_name: fakeUser().last_name,
-    organization: fakeUser().organization.name,
-    organization_id: fakeUser().organization.id,
-    position: fakeUser().position,
-});
-
-const fakeEnrichedComment = (): ShantytownEnrichedComment => ({
-    id: 1,
-    description: 'Un commentaire',
-    createdAt: 1723539551.172,
-    organization_target_name: [],
-    user_target_name: [],
-    createdBy: fakeAuthor(),
-    shantytown: 1,
-    tags: [
-        { uid: 'conditions_de_vie', tag: 'Conditions de vie' },
-        { uid: 'passage_sur_site', tag: 'Passage sur site' },
-    ],
-    attachments: [
-        {
-            state: 'uploaded',
-            id: 1,
-            name: 'test1.txt',
-            size: 2048,
-            urls: {
-                original: '',
-                preview: '',
-            },
-            extension: 'text/plain',
-            created_by: '2',
-        },
-    ],
-});
-
-describe.skip('services/shantytownComment.create', () => {
-    const dependencies = {
-        createComment: undefined,
-        createCommentTag: undefined,
-        findOneComment: undefined,
-        getComments: undefined,
-        triggerNewComment: undefined,
-        getShantytownWatchers: undefined,
-        sendMail: undefined,
-        uploadFiles: undefined,
-        enrichCommentsAttachments: undefined,
-        serializeAttachment: undefined,
-    };
     beforeEach(() => {
-        dependencies.getComments = sinon.stub(shantytownModel, 'getComments');
-        dependencies.getShantytownWatchers = sinon.stub(userModel, 'getShantytownWatchers');
-        dependencies.createComment = sinon.stub(shantytownCommentModel, 'create');
-        dependencies.createCommentTag = sinon.stub(shantytownCommentTagModel, 'create');
-        dependencies.findOneComment = sinon.stub(shantytownCommentModel, 'findOne');
-        dependencies.triggerNewComment = sinon.stub(mattermostUtils, 'triggerNewComment');
-        dependencies.sendMail = sinon.stub(mails, 'sendUserNewComment');
-        dependencies.uploadFiles = sinon.stub(attachmentService, 'upload');
-        dependencies.serializeAttachment = sinon.stub(attachmentService, 'serializeAttachment');
-        dependencies.enrichCommentsAttachments = sinon.stub(enrichCommentsAttachments, 'default');
+        fakeAuthor = (): ShantytownCommentAuthor => ({
+            id: fakeUser().id,
+            first_name: fakeUser().first_name,
+            last_name: fakeUser().last_name,
+            organization: fakeUser().organization.name,
+            organization_id: fakeUser().organization.id,
+            position: fakeUser().position,
+        });
+
+        fakeEnrichedComment = (): ShantytownEnrichedComment => ({
+            id: 1,
+            description: 'Un commentaire',
+            createdAt: 1723539551.172,
+            organization_target_name: [],
+            user_target_name: [],
+            createdBy: fakeAuthor(),
+            shantytown: 1,
+            tags: [
+                { uid: 'conditions_de_vie', tag: 'Conditions de vie' },
+                { uid: 'passage_sur_site', tag: 'Passage sur site' },
+            ],
+            attachments: [
+                {
+                    state: 'uploaded',
+                    id: 1,
+                    name: 'test1.txt',
+                    size: 2048,
+                    urls: {
+                        original: '',
+                        preview: '',
+                    },
+                    extension: 'text/plain',
+                    created_by: '2',
+                },
+            ],
+        });
+        stubs.sequelize.transaction.resolves(stubs.transaction);
     });
     afterEach(() => {
-        sinon.restore();
+        sandbox.restore();
     });
 
-    describe('createComment()', () => {
+    describe.skip('createComment()', () => {
         describe('', () => {
             let input;
             let output;
@@ -151,37 +174,39 @@ describe.skip('services/shantytownComment.create', () => {
                 };
 
                 // createComment() retourne un id de commentaire
-                dependencies.createComment
+                stubs.shantytownCommentModel.create
                     .resolves(1);
 
                 // createCommentTag() retourne un tableau d'objet identifiant chaque tag du commentaire
-                dependencies.createCommentTag
+                stubs.shantytownCommentTagModel.create
                     .resolves();
 
                 // uploadFiles() retourne une liste d'objets identifiant chaque fichier uploadé
-                dependencies.uploadFiles.resolves();
+                stubs.attachment.upload.resolves();
 
                 // getComments() retourne une liste de commentaires
-                dependencies.getComments
+                stubs.shantytownModel.getComments
                     .withArgs(input.user, { shantytown: [input.shantytown.id] })
                     .resolves(fakeCommentInput);
 
                 // findOneComment() retourne un commentaire
-                dependencies.findOneComment
+                stubs.shantytownCommentModel.findOne
                     .withArgs(1)
                     .resolves(output.comment);
 
                 // getShantytownWatchers() retourne une liste d'utilisateurs
-                dependencies.getShantytownWatchers
+                stubs.userModel.getShantytownWatchers
                     .withArgs(input.shantytown.id)
                     .resolves(output.watchers);
 
-                sequelizeStub.$queueResult([[{ shantytown_comment_id: output.comment.id }]]);
+                stubs.sequelize.transaction
+                    .withArgs([[{ shantytown_comment_id: output.comment.id }]])
+                    .resolves(stubs.transaction);
                 response = await createComment(input.comment, input.shantytown, input.user);
             });
 
             it('insère le commentaire en base de données via le modèle shantytownComment/create', () => {
-                expect(dependencies.createComment).to.have.been.calledOnceWith({
+                expect(stubs.shantytownCommentModel.create).to.have.been.calledOnceWith({
                     description: 'Un commentaire',
                     targets: {
                         users: [],
@@ -193,14 +218,14 @@ describe.skip('services/shantytownComment.create', () => {
             });
 
             it('insère les tags du commentaire en base de données via le modèle shantytownCommentTag/create', () => {
-                expect(dependencies.createCommentTag).to.have.been.calledOnceWith(1, [
+                expect(stubs.shantytownCommentTagModel.create).to.have.been.calledOnceWith(1, [
                     'conditions_de_vie',
                     'passage_sur_site',
                 ]);
             });
 
             it('envoie une notification mattermost', () => {
-                expect(dependencies.triggerNewComment).to.have.been.calledOnceWith(
+                expect(stubs.mattermostUtils.triggerNewComment).to.have.been.calledOnceWith(
                     'Un commentaire',
                     ['Conditions de vie', 'Passage sur site'],
                     input.shantytown,
@@ -209,21 +234,21 @@ describe.skip('services/shantytownComment.create', () => {
             });
 
             it('envoie une notification mail', () => {
-                expect(dependencies.sendMail.callCount).to.be.eql(3);
-                expect(dependencies.sendMail).to.have.been.calledWithExactly(output.watchers[0], {
+                expect(stubs.mails.sendUserNewComment.callCount).to.be.eql(3);
+                expect(stubs.mails.sendUserNewComment).to.have.been.calledWithExactly(output.watchers[0], {
                     variables: {
                         shantytown: input.shantytown,
                         comment: output.comment,
                     },
 
                 });
-                expect(dependencies.sendMail).to.have.been.calledWithExactly(output.watchers[1], {
+                expect(stubs.mails.sendUserNewComment).to.have.been.calledWithExactly(output.watchers[1], {
                     variables: {
                         shantytown: input.shantytown,
                         comment: output.comment,
                     },
                 });
-                expect(dependencies.sendMail).to.have.been.calledWithExactly(output.watchers[2], {
+                expect(stubs.mails.sendUserNewComment).to.have.been.calledWithExactly(output.watchers[2], {
                     variables: {
                         shantytown: input.shantytown,
                         comment: output.comment,
@@ -241,11 +266,11 @@ describe.skip('services/shantytownComment.create', () => {
 
         it('les pièces sont bien uploadées', async () => {
             const files = [fakeFile()];
-            dependencies.getComments.resolves({
+            stubs.shantytownModel.getComments.resolves({
                 1: [fakeCommentInput()],
             });
-            dependencies.getShantytownWatchers.resolves([]);
-            dependencies.createComment.resolves(1);
+            stubs.userModel.getShantytownWatchers.resolves([]);
+            stubs.shantytownCommentModel.create.resolves(1);
             await createComment(fakeCommentInput({
                 files,
             }), fakeShantytown(), fakeUser({ id: 2 }));
@@ -258,8 +283,8 @@ describe.skip('services/shantytownComment.create', () => {
                 transaction,
             );
             */
-            expect(dependencies.uploadFiles).to.have.been.calledOnceWith('shantytown_comment', 1, 2, files, sinon.match.instanceOf(Transaction));
-            expect(dependencies.uploadFiles.getCall(0).args[4]).to.be.instanceOf(Transaction);
+            expect(stubs.attachment.upload).to.have.been.calledOnceWith('shantytown_comment', 1, 2, files, sinon.match.same(stubs.transaction));
+            expect(stubs.attachment.upload.getCall(0).args[4]).to.be.eql(stubs.transaction);
         });
 
         /*
