82dc6d0d1fd304d52fe29476d157cece11e3e277
Christophe BENARD <ch.benard@gmail.com>
2025-06-30 11:49:52 +0200

    Fix: rendre la méthode asynchrone (sinon await génère une alerte SonarQube dans le service) refers to https://trello.com/c/8hTtUFm4/2442

--- packages/api/server/models/shantytownCommentModel/deleteComment.ts
+++ packages/api/server/models/shantytownCommentModel/deleteComment.ts
@@ -3,7 +3,7 @@ import { sequelize } from '#db/sequelize';
 /**
  * @param {Number} id A shantytown_comment_id
  */
-export default (commentId) => {
+export default async (commentId) => {
     sequelize.query(
         'DELETE FROM shantytown_comments WHERE shantytown_comment_id = :id',
         {
