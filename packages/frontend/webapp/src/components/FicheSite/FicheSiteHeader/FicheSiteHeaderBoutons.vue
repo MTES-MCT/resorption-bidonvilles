f0dcacc007f7d11a43965873f7c2f34f80d98013
Gilles COGNIN <gillescognin@gmail.com>
2025-07-22 16:40:31 +0200

    feat: Ajout d'un bouton d'activer/désactivation de l'alerte canicule sur la fiche site

--- packages/frontend/webapp/src/components/FicheSite/FicheSiteHeader/FicheSiteHeaderBoutons.vue
+++ packages/frontend/webapp/src/components/FicheSite/FicheSiteHeader/FicheSiteHeaderBoutons.vue
@@ -1,97 +1,124 @@
 <template>
-    <p class="flex flex-col items-start gap-2 sm:flex-row sm:items-center">
-        <RouterLink
-            to="#journal_du_site"
-            v-if="
-                userStore.hasLocalizedPermission(
-                    'shantytown_comment.list',
-                    town
-                )
-            "
+    <div
+        class="flex flex-col justify-between items-start gap-2 sm:flex-row sm:items-center"
+    >
+        <div
+            class="flex flex-col items-start gap-2 sm:flex-row sm:items-center"
         >
+            <RouterLink
+                to="#journal_du_site"
+                v-if="
+                    userStore.hasLocalizedPermission(
+                        'shantytown_comment.list',
+                        town
+                    )
+                "
+            >
+                <Button
+                    size="sm"
+                    variant="primaryOutline"
+                    icon="comment"
+                    iconPosition="left"
+                    tabindex="-1"
+                    class="!border-2 !border-primary hover:!bg-primary"
+                    >Journal du site</Button
+                >
+            </RouterLink>
             <Button
                 size="sm"
-                variant="primaryOutline"
-                icon="comment"
+                icon="file-word"
                 iconPosition="left"
-                tabindex="-1"
+                variant="primaryOutline"
+                @click="openExportModal"
                 class="!border-2 !border-primary hover:!bg-primary"
-                >Journal du site</Button
+                >Exporter</Button
             >
-        </RouterLink>
-        <Button
-            size="sm"
-            icon="file-word"
-            iconPosition="left"
-            variant="primaryOutline"
-            @click="openExportModal"
-            class="!border-2 !border-primary hover:!bg-primary"
-            >Exporter</Button
-        >
-        <Button
-            v-if="
-                userStore.hasLocalizedPermission('shantytown.close', town) &&
-                town.status === 'open'
-            "
-            size="sm"
-            variant="primary"
-            icon="house-circle-xmark"
-            iconPosition="left"
-            :href="`/site/${town.id}/fermeture`"
-            class="hover:!bg-primaryDark"
-            >Fermer le site</Button
-        >
+            <Button
+                v-if="
+                    userStore.hasLocalizedPermission(
+                        'shantytown.close',
+                        town
+                    ) && town.status === 'open'
+                "
+                size="sm"
+                variant="primary"
+                icon="house-circle-xmark"
+                iconPosition="left"
+                :href="`/site/${town.id}/fermeture`"
+                class="hover:!bg-primaryDark"
+                >Fermer le site</Button
+            >
 
-        <Button
-            v-if="
-                userStore.hasLocalizedPermission(
-                    'shantytown.fix_status',
-                    town
-                ) && town.status !== 'open'
-            "
-            size="sm"
-            variant="primary"
-            icon="house-circle-xmark"
-            iconPosition="left"
-            :href="`/site/${town.id}/fermeture`"
-            class="hover:!bg-primaryDark"
-            >Corriger la fermeture du site</Button
-        >
-        <Button
-            size="sm"
-            variant="primary"
-            icon="pencil"
-            iconPosition="left"
-            v-if="
-                userStore.hasLocalizedPermission('shantytown.update', town) &&
-                town.status === 'open'
-            "
-            :href="`/site/${town.id}/mise-a-jour`"
-            class="hover:!bg-primaryDark"
-            >Mettre à jour</Button
-        >
-        <Button
-            v-if="displayStartResorptionButton"
-            size="sm"
-            variant="primary"
-            icon="fa-regular fa-play"
-            iconPosition="left"
-            @click="startResorption"
-            :loading="startResorptionIsLoading"
-            >Démarrer la résorption</Button
-        >
-        <Button
-            v-if="userStore.hasLocalizedPermission('shantytown.delete', town)"
-            size="sm"
-            variant="primary"
-            icon="fa-regular fa-trash-alt"
-            iconPosition="left"
-            @click="deleteTown"
-            :loading="deleteIsLoading"
-            class="!border-2 !border-primary hover:!bg-primaryDark"
-            >Supprimer le site</Button
-        >
-    </p>
+            <Button
+                v-if="
+                    userStore.hasLocalizedPermission(
+                        'shantytown.fix_status',
+                        town
+                    ) && town.status !== 'open'
+                "
+                size="sm"
+                variant="primary"
+                icon="house-circle-xmark"
+                iconPosition="left"
+                :href="`/site/${town.id}/fermeture`"
+                class="hover:!bg-primaryDark"
+                >Corriger la fermeture du site</Button
+            >
+            <Button
+                size="sm"
+                variant="primary"
+                icon="pencil"
+                iconPosition="left"
+                v-if="
+                    userStore.hasLocalizedPermission(
+                        'shantytown.update',
+                        town
+                    ) && town.status === 'open'
+                "
+                :href="`/site/${town.id}/mise-a-jour`"
+                class="hover:!bg-primaryDark"
+                >Mettre à jour</Button
+            >
+            <Button
+                v-if="displayStartResorptionButton"
+                size="sm"
+                variant="primary"
+                icon="fa-regular fa-play"
+                iconPosition="left"
+                @click="startResorption"
+                :loading="startResorptionIsLoading"
+                >Démarrer la résorption</Button
+            >
+            <Button
+                v-if="
+                    userStore.hasLocalizedPermission('shantytown.delete', town)
+                "
+                size="sm"
+                variant="primary"
+                icon="fa-regular fa-trash-alt"
+                iconPosition="left"
+                @click="deleteTown"
+                :loading="deleteIsLoading"
+                class="!border-2 !border-primary hover:!bg-primaryDark"
+                >Supprimer le site</Button
+            >
+        </div>
+        <div>
+            <DsfrButton
+                size="sm"
+                :label="
+                    heatwaveStatus
+                        ? 'Supprimer l\'alerte Canicule'
+                        : 'Activer l\'alerte Canicule'
+                "
+                icon="fr-icon-thermometer-line"
+                class="hover:!text-white"
+                secondary
+                :disabled="heatwaveRequestStatus?.loading"
+                @click.prevent.stop="toggleHeatwave"
+            />
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -114,6 +141,8 @@ const props = defineProps({
 });
 const { town } = toRefs(props);
 const userStore = useUserStore();
+const notificationStore = useNotificationStore();
+const townsStore = useTownsStore();
 
 const { displayPhasesPreparatoiresResorption } =
     usePhasesPreparatoiresResorption(town);
@@ -215,9 +244,6 @@ async function startResorption() {
 
     startResorptionIsLoading.value = true;
 
-    const notificationStore = useNotificationStore();
-    const townsStore = useTownsStore();
-
     nextTick(async () => {
         try {
             await townsStore.startResorption(town.value.id);
@@ -246,6 +272,46 @@ const displayStartResorptionButton = computed(() => {
         !townIsClosed.value
     );
 });
+
+const heatwaveStatus = computed(() => {
+    return town.value.heatwaveStatus;
+});
+
+const heatwaveRequestStatus = computed(() => {
+    return townsStore.heatwaveStatuses[town.value.id] || null;
+});
+
+async function toggleHeatwave() {
+    try {
+        await townsStore.setHeatwaveStatus(
+            town.value.id,
+            !town.value.heatwaveStatus
+        );
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log("Erreur lors de la modification du statut canicule :", e);
+
+        notificationStore.error(
+            "Risque canicule",
+            "Une erreur inconnue est survenue"
+        );
+        return;
+    }
+
+    if (heatwaveRequestStatus.value?.error !== null) {
+        notificationStore.error(
+            "Risque canicule",
+            heatwaveRequestStatus.value.error
+        );
+    } else {
+        notificationStore.success(
+            "Risque canicule",
+            town.value.heatwaveStatus === true
+                ? "Le site a été marqué comme particulièrement exposé à la canicule"
+                : "Le site n'est plus marqué comme à risque"
+        );
+    }
+}
 </script>
 
 <style scoped>
