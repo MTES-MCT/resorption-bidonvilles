cc0d4cbf86f14d5b6f67ffa73ab7358896ab2a04
Gilles COGNIN <gillescognin@gmail.com>
2025-08-01 10:20:31 +0200

    feat: Ajout des models liés aux propriétaires de parcelles

--- packages/api/server/models/shantytownParcelOwnerModel/index.ts
+++ packages/api/server/models/shantytownParcelOwnerModel/index.ts
@@ -1,9 +1,11 @@
 import create from './create';
 import deleteOwner from './delete';
 import findAll from './findAll';
+import update from './update';
 
 export default {
     create,
     deleteOwner,
     findAll,
+    update,
 };
