e2eff6af2a1ebbfb8ec4ea0609757882d52d5de5
Gilles COGNIN <gillescognin@gmail.com>
2025-08-01 10:07:21 +0200

    feat: Ajout des propriétaires multiples

--- packages/frontend/webapp/src/components/FormDeclarationDeSite/sections/FormDeclarationDeSiteCaracteristiques.vue
+++ packages/frontend/webapp/src/components/FormDeclarationDeSite/sections/FormDeclarationDeSiteCaracteristiques.vue
@@ -10,20 +10,109 @@
         </Fieldset>
 
         <Fieldset legend="Quel est le propriétaire ?" showMandatoryStar>
-            <InputOwnerType />
+            <!-- <InputOwnerType />
             <InputOwner
                 v-if="
                     values.owner_type &&
                     values.owner_type !== unknownOwnerTypeId &&
                     userStore.hasOwnerPermission
                 "
-            />
+            /> -->
+
+            <div
+                v-for="(owner, index) in sortedOwners"
+                :key="owner.ownerId || owner.name"
+            >
+                <div
+                    v-if="
+                        (!showDeletedOwners && owner.active) ||
+                        showDeletedOwners
+                    "
+                    class="flex flex-col justify-evenly items-center border border-primary rounded mb-4 px-4"
+                    :class="
+                        (!owner.ownerId ? 'shadow-lg' : '',
+                        !owner.active ? 'opacity-50 bg-G200' : '')
+                    "
+                >
+                    <div
+                        class="w-full p-2 rounded text-sm font-bold mt-2 mb-3 space-between flex flex-row justify-between items-center"
+                        :class="owner.ownerId ? 'bg-G200' : 'bg-blue200'"
+                    >
+                        <p v-if="!owner.ownerId" class="font-bold">
+                            <i class="fa-solid fa-circle-plus text-blue500" />
+                            Nouveau propriétaire
+                        </p>
+                        <p v-else class="font-bold">
+                            Propriétaire {{ index + 1 }}
+                        </p>
+                        <DsfrButton
+                            class=""
+                            icon="fr-icon-delete-line"
+                            icon-only
+                            label="Effacer le propriétaire"
+                            no-outline
+                            tertiary
+                            size="sm"
+                            @click.prevent="removeOwner(owner)"
+                        />
+                    </div>
+                    <div
+                        class="flex flex-row justify-evenly items-center gap-4 w-full pb-4"
+                    >
+                        <InputOwner
+                            class="w-2/3 !mb-0"
+                            v-if="
+                                values.owner_type &&
+                                values.owner_type !== unknownOwnerTypeId &&
+                                userStore.hasOwnerPermission
+                            "
+                            :name="`owner.owners[${getOriginalIndex(
+                                owner
+                            )}].name`"
+                            :disabled="!owner.active"
+                        />
+                        <InputOwnerType
+                            class="w-auto !mb-0"
+                            :name="`owner.owners[${getOriginalIndex(
+                                owner
+                            )}].type`"
+                            :disabled="!owner.active"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-center gap-2 justify-between">
+                <DsfrButton
+                    icon="fr-icon-user-add-fill"
+                    label="Ajouter un propriétaire"
+                    @click.prevent="
+                        values.owner.owners.push({
+                            ownerId: null,
+                            name: '',
+                            type: unknownOwnerTypeId,
+                            active: true,
+                        })
+                    "
+                />
+                <DsfrButton
+                    :icon="
+                        showDeletedOwners
+                            ? 'fr-icon-eye-off-line'
+                            : 'fr-icon-eye-fill'
+                    "
+                    :label="`${
+                        showDeletedOwners ? 'Masquer' : 'Afficher'
+                    } les anciens propriétaires connus`"
+                    tertiary
+                    @click.prevent="displayFullOwnersList"
+                />
+            </div>
         </Fieldset>
     </FormSection>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useFormValues } from "vee-validate";
 import { useConfigStore } from "@/stores/config.store";
 import { useUserStore } from "@/stores/user.store";
@@ -39,6 +128,33 @@ import InputOwner from "../inputs/FormDeclarationDeSiteInputOwner.vue";
 
 const values = useFormValues();
 const userStore = useUserStore();
+const showDeletedOwners = ref(false);
+
+const sortedOwners = computed(() => {
+    const owners = values.value.owner?.owners ?? [];
+    // On crée une copie pour ne pas muter l'état original
+    return [...owners].sort((a, b) => {
+        if (a.active !== b.active) {
+            return a.active ? -1 : 1;
+        }
+
+        if (a.createdAt && b.createdAt) {
+            return new Date(a.createdAt) - new Date(b.createdAt);
+        }
+        return 0;
+    });
+});
+
+const getOriginalIndex = (ownerToFind) => {
+    return values.value.owner?.owners.findIndex((o) => o === ownerToFind);
+};
+
+const removeOwner = (ownerToRemove) => {
+    const index = getOriginalIndex(ownerToRemove);
+    if (index !== -1) {
+        values.value.owner.owners.splice(index, 1);
+    }
+};
 
 const unknownOwnerTypeId = computed(() => {
     const configStore = useConfigStore();
@@ -47,4 +163,13 @@ const unknownOwnerTypeId = computed(() => {
     );
     return type.id;
 });
+
+const displayFullOwnersList = () => {
+    showDeletedOwners.value = !showDeletedOwners.value;
+    if (!showDeletedOwners.value) {
+        values.value.owner.owners = values.value.owner.owners.filter(
+            (owner) => owner.active
+        );
+    }
+};
 </script>
