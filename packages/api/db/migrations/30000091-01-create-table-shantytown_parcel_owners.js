54496c821e00746a4af1e87452469cb227ee3c10
Gilles COGNIN <gillescognin@gmail.com>
2025-08-07 14:12:34 +0200

    fix: Update de la migration

--- packages/api/db/migrations/30000091-01-create-table-shantytown_parcel_owners.js
+++ packages/api/db/migrations/30000091-01-create-table-shantytown_parcel_owners.js
@@ -51,6 +51,7 @@ module.exports = {
                     created_at: {
                         type: Sequelize.DATE,
                         allowNull: false,
+                        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
                     },
                 },
                 { transaction },
