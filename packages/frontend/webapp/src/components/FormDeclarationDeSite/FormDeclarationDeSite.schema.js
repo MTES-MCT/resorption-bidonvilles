0a17a157d9d26946f90dd23ebdc33b6b36d04101
Gilles COGNIN <gillescognin@gmail.com>
2025-08-07 14:25:21 +0200

    feat: Mise à jour du validateur

--- packages/frontend/webapp/src/components/FormDeclarationDeSite/FormDeclarationDeSite.schema.js
+++ packages/frontend/webapp/src/components/FormDeclarationDeSite/FormDeclarationDeSite.schema.js
@@ -1,4 +1,4 @@
-import { object, string, date, number, array, ref } from "yup";
+import { object, string, date, number, array, ref, lazy } from "yup";
 import { computed } from "vue";
 import labels from "@/components/Common/FormEtFicheSite.labels";
 
@@ -63,32 +63,40 @@ export default function (
             detailed_address: string()
                 .nullable()
                 .label(labels.detailed_address),
-            owner_type: number().required().label(labels.owner_type),
             owner: hasOwnerPermission.value
-                ? object({
-                      owners: array().of(
-                          object({
-                              name: string()
-                                  .required(
-                                      "Le nom du propriétaire est obligatoire"
-                                  )
-                                  .matches(
-                                      /^[^<>{}]*$/,
-                                      "Le nom du propriétaire contient des caractères non valides"
+                ? // On utilise yup.lazy pour rendre la validation de "owner" conditionnelle
+                  lazy((value) => {
+                      const owners = value?.owners;
+
+                      if (
+                          Array.isArray(owners) &&
+                          owners.length === 1 &&
+                          !owners[0].name
+                      ) {
+                          return object().nullable();
+                      }
+
+                      return object({
+                          owners: array().of(
+                              object({
+                                  name: string()
+                                      .required(
+                                          "Le nom du propriétaire est obligatoire"
+                                      )
+                                      .matches(
+                                          /^[^<>{}]*$/,
+                                          "Le nom du propriétaire contient des caractères non valides"
+                                      ),
+                                  type: number().required(
+                                      "Le type du propriétaire est obligatoire"
                                   ),
-                              type: number().required(
-                                  "Le type du propriétaire est obligatoire"
-                              ),
-                          })
-                      ),
+                              })
+                          ),
+                      })
+                          .nullable()
+                          .label(labels.owner);
                   })
-                      .nullable()
-                      .label(labels.owner)
-                : //   .matches(
-                  //       /^[^<>{}]*$/,
-                  //       `Le contenu du champ "${labels.owner}" n'est pas valide`
-                  //   )
-                  undefined,
+                : undefined,
             population_total: number()
                 .nullable()
                 .min(1)
