1a594918aeb55ea295f431aad2a614a9d6e7bf21
Gilles COGNIN <gillescognin@gmail.com>
2025-08-07 16:27:20 +0200

    fix: Correction de l'index des models de mise à jour des propriétaires

--- packages/api/server/models/shantytownParcelOwnerModel/index.ts
+++ packages/api/server/models/shantytownParcelOwnerModel/index.ts
@@ -1,11 +1,9 @@
 import create from './create';
-import deleteOwner from './delete';
 import findAll from './findAll';
 import update from './update';
 
 export default {
     create,
-    deleteOwner,
     findAll,
     update,
 };
