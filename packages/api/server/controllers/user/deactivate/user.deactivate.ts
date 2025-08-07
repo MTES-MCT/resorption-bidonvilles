17c82da53ee98bdd6d615fcd928ea42b78305678
Victor Hristea <victor.hristea@epitech.eu>
2025-08-07 11:58:47 +0200

    Fix: Correction d'une modification de fichier involontaire (lint)

--- packages/api/server/controllers/user/deactivate/user.deactivate.ts
+++ packages/api/server/controllers/user/deactivate/user.deactivate.ts
@@ -4,31 +4,15 @@ import { User } from '#root/types/resources/User.d';
 
 const ERRORS = {
     undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
-    deactivation_permission_failure: {
-        code: 500,
-        message: "Vous n'avez pas la permission de désactiver ce compte",
-    },
-    deactivation_failure: {
-        code: 500,
-        message: 'Une erreur est survenue lors de la désactivation du compte',
-    },
-    refresh_failure: {
-        code: 500,
-        message: 'Une erreur est survenue lors de la lecture des données en base',
-    },
-    transaction_failure: {
-        code: 500,
-        message:
-      'Une erreur est survenue lors de la validation de la désactivation',
-    },
-    user_already_inactive: {
-        code: 400,
-        message: "L'utilisateur est déjà inactif",
-    },
+    deactivation_permission_failure: { code: 500, message: 'Vous n\'avez pas la permission de désactiver ce compte' },
+    deactivation_failure: { code: 500, message: 'Une erreur est survenue lors de la désactivation du compte' },
+    refresh_failure: { code: 500, message: 'Une erreur est survenue lors de la lecture des données en base' },
+    transaction_failure: { code: 500, message: 'Une erreur est survenue lors de la validation de la désactivation' },
+    user_already_inactive: { code: 400, message: 'L\'utilisateur est déjà inactif' },
 };
 
 interface UserDeactivateRequest extends Request {
-    user: User;
+    user: User,
     body: {
         user: User;
         reason: string | null;
@@ -36,20 +20,10 @@ interface UserDeactivateRequest extends Request {
     };
 }
 
-export default async (
-    req: UserDeactivateRequest,
-    res: Response,
-    next: NextFunction,
-): Promise<void> => {
+export default async (req: UserDeactivateRequest, res: Response, next: NextFunction): Promise<void> => {
     try {
         const userId: number = parseInt(req.params.id, 10);
-        const updatedUser: User = await userService.deactivate(
-            userId,
-            userId === req.user.id,
-            req.user,
-            req.body.reason,
-            req.body.anonymizationRequested,
-        );
+        const updatedUser: User = await userService.deactivate(userId, userId === req.user.id, req.user, req.body.reason, req.body.anonymizationRequested);
         res.status(200).send(updatedUser);
     } catch (error) {
         const { code, message } = ERRORS[error?.code] ?? ERRORS.undefined;
