fc87547b9508e64c5f5d1d2acd069240ac38b4f7
Christophe BENARD <ch.benard@gmail.com>
2025-08-08 14:42:35 +0200

    Feat: Ajouter la pagination dans la liste des mesages refers to https://trello.com/c/JIY11gW1/2482

--- packages/frontend/webapp/src/components/FicheSite/FicheSiteJournal/FicheSiteJournalListeDesMessages.vue
+++ packages/frontend/webapp/src/components/FicheSite/FicheSiteJournal/FicheSiteJournalListeDesMessages.vue
@@ -1,16 +1,22 @@
 <template>
     <section>
         <CarteCommentaireSite
-            v-for="comment in comments"
+            v-for="comment in paginatedResults"
             :key="comment.id"
             :townId="townId"
             :comment="comment"
+        />
+        <DsfrPagination
+            v-model:current-page="currentPage"
+            :pages="pages"
+            :trunc-limit="4"
+            class="mt-4"
         />
     </section>
 </template>
 
 <script setup>
-import { defineProps, toRefs } from "vue";
+import { computed, defineProps, ref, toRefs, watch } from "vue";
 import CarteCommentaireSite from "@/components/CarteCommentaire/CarteCommentaireSite.vue";
 
 const props = defineProps({
@@ -18,4 +24,34 @@ const props = defineProps({
     townId: Number,
 });
 const { comments, townId } = toRefs(props);
+const currentPage = ref(0);
+const itemsPerPage = 5;
+const totalPages = computed(() => {
+    return Math.ceil(comments.value.length / itemsPerPage);
+});
+const pages = computed(() => {
+    let results = [];
+
+    for (let i = 1; i < totalPages.value + 1; i++) {
+        results.push({
+            title: `${i}`,
+            href: `${i}`,
+            label: i,
+        });
+    }
+    return results;
+});
+const paginatedResults = computed(() => {
+    const start = currentPage.value * itemsPerPage;
+    const end = start + itemsPerPage;
+
+    return comments.value.slice(start, end);
+});
+
+watch(currentPage, () => {
+    const element = document.getElementById("journal_du_site");
+    if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+    }
+});
 </script>
