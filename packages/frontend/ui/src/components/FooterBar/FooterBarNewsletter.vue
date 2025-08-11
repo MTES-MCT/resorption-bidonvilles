62df549a48c831ecc0116cf8458c61f248bd6780
Gilles COGNIN <gillescognin@gmail.com>
2025-08-11 14:35:31 +0200

    fix: Correction du bouton passé en DsfrButton par erreur

--- packages/frontend/ui/src/components/FooterBar/FooterBarNewsletter.vue
+++ packages/frontend/ui/src/components/FooterBar/FooterBarNewsletter.vue
@@ -3,24 +3,13 @@
         <h2 class="font-bold text-lg">{{ $t('footer.newsletterTitle') }}</h2>
         <p class="text-sm">{{ $t('footer.newsletterBody') }}</p>
 
-        <DsfrButton
-            size="sm"
-            icon="fr-icon-external-link-fill"
-            iconRight
-            :priority="'secondary'"
-            :is-external="true"
-            @click="goToNewsletter"
-            >
-            {{ $t('footer.newsletterCta') }}
-        </DsfrButton>
+         <Button href="https://a757ac69.sibforms.com/serve/MUIFALVz73sp9nySQNDVaSPuG57ypOIGQrx7oMDqyu-lukbiAq1DqhoTh4UQfghOgE-jTVCzMUDQJ6CAQG5GtpsztQ3C3hPleVgcZDEEU0Y_3aPMffVdQjm_YRNdGAnjF4sET4aCQynW4QVbe1bjXnRuyTR0ETJCNgdje0QbaxOzYTKMIAPCdKNJkcYdS3Boj9Vsbj1RXYTzE_Q=" target="_blank" class="mt-3 hover:!bg-primaryDark" size="sm"
+            icon="arrow-up-right-from-square">{{ $t('footer.newsletterCta') }}</Button>
     </section>
 </template>
 
 <script setup>
-const newsletterLink = 'https://a757ac69.sibforms.com/serve/MUIFALVz73sp9nySQNDVaSPuG57ypOIGQrx7oMDqyu-lukbiAq1DqhoTh4UQfghOgE-jTVCzMUDQJ6CAQG5GtpsztQ3C3hPleVgcZDEEU0Y_3aPMffVdQjm_YRNdGAnjF4sET4aCQynW4QVbe1bjXnRuyTR0ETJCNgdje0QbaxOzYTKMIAPCdKNJkcYdS3Boj9Vsbj1RXYTzE_Q='
-function goToNewsletter() {
-  window.open(newsletterLink)
-}
+import Button from "../Button.vue";
 </script>
 
 <style scoped>
