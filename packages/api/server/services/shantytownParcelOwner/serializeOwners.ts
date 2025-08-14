37c63dc3c882189efdf7c04bf8641f13c64c1bb9
Gilles COGNIN <gillescognin@gmail.com>
2025-08-14 14:14:20 +0200

    feat: Ajout des propriétaires dans l'export unitaire de site

--- packages/api/server/services/shantytownParcelOwner/serializeOwners.ts
+++ packages/api/server/services/shantytownParcelOwner/serializeOwners.ts
@@ -1,10 +1,13 @@
 import userModel from '#server/models/userModel';
+import findAll from '#server/models/ownerTypeModel/findAll';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 import { User } from '#root/types/resources/User.d';
 import { SerializedOwner, RawParcelOwner, ParcelOwners } from '#root/types/resources/ParcelOwner.d';
+import { OwnerType } from '#root/types/resources/OwnerType.d';
 
-export default async (user: AuthUser, owners: RawParcelOwner[]): Promise<ParcelOwners> => {
+export default async (user: AuthUser, owners: RawParcelOwner[], withSerializedOwnerTypes: boolean = false): Promise<ParcelOwners> => {
     const serializedOwners: SerializedOwner[] = [];
+    const ownerTypes: OwnerType[] = withSerializedOwnerTypes ? await findAll() : [];
 
     await Promise.all(
         owners.map(async (ownerDatas) => {
@@ -16,6 +19,7 @@ export default async (user: AuthUser, owners: RawParcelOwner[]): Promise<ParcelO
                     ownerId: ownerDatas.shantytown_parcel_owner_id,
                     name: ownerDatas.owner_name,
                     type: ownerDatas.fk_owner_type,
+                    ...(withSerializedOwnerTypes && { typeDetails: ownerTypes.find(t => t.id === ownerDatas.fk_owner_type) }),
                     active: ownerDatas.active,
                     createdAt: ownerDatas.created_at,
                     createdBy: {
