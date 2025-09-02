427c8ab4b7d118ddea5575c21ebac5643d89f9f8
Gilles COGNIN <gillescognin@gmail.com>
2025-09-02 15:56:54 +0200

    feat: Génération des données des propriétaires dans l'export de sites

--- packages/api/server/services/shantytown/exportTowns.fetchCurrentData.ts
+++ packages/api/server/services/shantytown/exportTowns.fetchCurrentData.ts
@@ -10,10 +10,13 @@ import actionModel from '#server/models/actionModel';
 import enrichShantytown from '#server/services/shantytown/_common/enrichShantytownWithALeastOneActionFinanced';
 import { ShantytownWithFinancedAction } from '#root/types/resources/Shantytown.d';
 import { FinancedShantytownAction } from '#root/types/resources/Action.d';
+<<<<<<< HEAD
 import { PostSqlFilters, ShantytownFilters } from '#root/types/resources/shantytownFilters.d';
 import setQueryFilters from './exportTowns.setQueryFilters'; import shantytownParcelOwner from '../shantytownParcelOwner';
 import serializeOwners from '../shantytownParcelOwner/serializeOwners';
 import { ParcelOwners, RawParcelOwner } from '#root/types/resources/ParcelOwner.d';
+=======
+>>>>>>> 570b67d40 (feat: Génération des données des propriétaires dans l'export de sites)
 
 
 export default async function fetchCurrentData(user: AuthUser, locations: Location[], filters: ShantytownFilters): Promise<ShantytownWithFinancedAction[]> {
@@ -110,32 +113,12 @@ export default async function fetchCurrentData(user: AuthUser, locations: Locati
     const townsWithFinancedActions = await actionModel.fetchFinancedActionsByYear(null, parseInt(currentYear, 10), clauseGroup);
     const transformedShantytowns = enrichShantytown(townsWithFinancedActions);
 
-    // Récupération des owners
-    const townIds = towns.map(t => t.id);
-    const allOwners = await shantytownParcelOwner.find(user, townIds);
-    const serializedOwners = await serializeOwners(user, allOwners, true);
-
-    const ownersByShantytownId = serializedOwners.reduce((acc, owner) => {
-        const shantytownId = owner.shantytown.id;
-        if (!acc[shantytownId]) {
-            acc[shantytownId] = { owners: [] };
-        }
-        acc[shantytownId].owners.push(owner);
-        return acc;
-    }, {} as Record<number, ParcelOwners>);
-
     return Promise.all(towns.map(async (town: ShantytownWithFinancedAction) => {
-        // const owners: RawParcelOwner[] = await shantytownParcelOwner.find(user, town);
         const townWithFinancedActions: FinancedShantytownAction = transformedShantytowns.find((t:FinancedShantytownAction) => t.shantytown_id === town.id);
-        const owner = ownersByShantytownId[town.id];
 
-        // if (townWithFinancedActions) {
         return {
             ...town,
             hasAtLeastOneActionFinanced: townWithFinancedActions ? townWithFinancedActions.hasAtLeastOneActionFinanced : undefined,
-            owner: owner || undefined,
         };
-        // }
-        // return town;
     }));
 }
