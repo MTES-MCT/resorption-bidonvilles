fe35d3cae97a7572adadb94a94f419902a9e0a5c
Gilles COGNIN <gillescognin@gmail.com>
2025-09-03 15:32:54 +0200

    fix: Correction du test Create

--- packages/api/server/services/shantytownParcelOwner/create.spec.ts
+++ packages/api/server/services/shantytownParcelOwner/create.spec.ts
@@ -15,6 +15,8 @@ const { expect } = chai;
 chai.use(sinonChai);
 chai.use(chaiSubset);
 
+const fakeTestUser: AuthUser = fakeUser();
+
 const sandbox = sinon.createSandbox();
 const stubs = {
     shantytownParcelOwnerModel: {
@@ -28,6 +30,7 @@ const stubs = {
         rollback: sandbox.stub(),
     },
     user: {
+        ...fakeTestUser,
         isAllowedTo: sandbox.stub(),
     },
 };
@@ -42,7 +45,7 @@ rewiremock.disable();
 
 describe('services/shantytownParcelOwners.create()', () => {
     let fakeOwner: ParcelOwnerInsert[];
-    let fakeTestUser: AuthUser;
+    // let fakeTestUser: AuthUser;
     let fakeTown: Shantytown;
 
     beforeEach(() => {
@@ -51,8 +54,9 @@ describe('services/shantytownParcelOwners.create()', () => {
             name: 'Jean Bon',
             type: 1,
         }];
-        fakeTestUser = fakeUser();
+        // fakeTestUser = fakeUser();
         fakeTown = fakeShantytown();
+        // stubs.user = { ...stubs.user, ...fakeTestUser };
         stubs.user.isAllowedTo.returns(true);
         stubs.sequelize.transaction.resolves(stubs.transaction);
     });
@@ -63,14 +67,14 @@ describe('services/shantytownParcelOwners.create()', () => {
 
     it('vérifie que l\'utilisateur a le droit d\'ajouter un propriétaire', async () => {
         try {
-            await create(fakeTestUser, fakeTown.id, fakeOwner);
+            await create(stubs.user, fakeTown.id, fakeOwner);
         } catch (error) {
             // eslint-disable-next-line no-console
             console.error(error);
         }
 
         expect(stubs.user.isAllowedTo).to.have.been.calledWith('access', 'shantytown_owner');
-        expect(stubs.user.isAllowedTo).to.equal(true);
+        expect(stubs.user.isAllowedTo.firstCall.returnValue).to.equal(true);
     });
 
     it('renvoie un ServiceError si l\'utilisateur n\'a pas le droit d\'ajouter un propriétaire', async () => {
@@ -78,7 +82,7 @@ describe('services/shantytownParcelOwners.create()', () => {
         stubs.user.isAllowedTo.returns(false);
 
         try {
-            await create(fakeTestUser, fakeTown.id, fakeOwner);
+            await create(stubs.user, fakeTown.id, fakeOwner);
         } catch (error) {
             returnedError = error as ServiceError;
         }
@@ -93,7 +97,7 @@ describe('services/shantytownParcelOwners.create()', () => {
         let parcelOwnerId: { parcelOwnerId: number };
 
         try {
-            parcelOwnerId = await create(fakeTestUser, fakeTown.id, fakeOwner);
+            parcelOwnerId = await create(stubs.user, fakeTown.id, fakeOwner);
         } catch (error) {
             // eslint-disable-next-line no-console
             console.error(error);
@@ -110,7 +114,7 @@ describe('services/shantytownParcelOwners.create()', () => {
         let parcelOwnerId: { parcelOwnerId: number };
 
         try {
-            parcelOwnerId = await create(fakeTestUser, fakeTown.id, fakeOwners);
+            parcelOwnerId = await create(stubs.user, fakeTown.id, fakeOwners);
         } catch (error) {
             // eslint-disable-next-line no-console
             console.error(error);
@@ -125,11 +129,11 @@ describe('services/shantytownParcelOwners.create()', () => {
         stubs.shantytownParcelOwnerModel.create.rejects(new Error('Échec de la création du propriétaire de parcelle'));
 
         try {
-            await create(fakeTestUser, fakeTown.id, fakeOwner);
+            await create(stubs.user, fakeTown.id, fakeOwner);
         } catch (error) {
             returnedError = error as ServiceError;
         }
-        expect(stubs.shantytownParcelOwnerModel.create).to.have.been.calledOnceWith(fakeTestUser, fakeTown.id, fakeOwner, stubs.transaction);
+        expect(stubs.shantytownParcelOwnerModel.create).to.have.been.calledOnceWith(stubs.user, fakeTown.id, fakeOwner, stubs.transaction);
         expect(stubs.transaction.rollback).to.have.been.calledOnce;
         expect(returnedError).to.be.instanceOf(ServiceError);
         expect(returnedError.code).to.equal('parcel_owner_creation_failed');
