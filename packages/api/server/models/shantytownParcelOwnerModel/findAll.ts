2ccd2de7959738c49ddd65deb6e900f8a7182c56
Gilles COGNIN <gillescognin@gmail.com>
2025-09-04 15:23:49 +0200

    fix: Correction de l'utilisation de la transaction

--- packages/api/server/models/shantytownParcelOwnerModel/findAll.ts
+++ packages/api/server/models/shantytownParcelOwnerModel/findAll.ts
@@ -1,14 +1,15 @@
 import { sequelize } from '#db/sequelize';
+import { Transaction } from 'sequelize';
 import ServiceError from '#server/errors/ServiceError';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 import { RawParcelOwner } from '#root/types/resources/ParcelOwner.d';
 
-export default async (user: AuthUser, shantytownId: number): Promise<RawParcelOwner[]> => {
+export default async (user: AuthUser, shantytownId: number, transaction?: Transaction): Promise<RawParcelOwner[]> => {
     try {
         const [results]: any = await sequelize.query(`
             SELECT * FROM shantytown_parcel_owners WHERE fk_shantytown = :shantytownId
         `, {
-            replacements: { shantytownId },
+            replacements: { shantytownId }, transaction,
         });
         return results;
     } catch (error) {
