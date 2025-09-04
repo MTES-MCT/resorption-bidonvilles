88d6c1299f8642a4d645ffbddf6c7c91c1c7f6d4
Christophe BENARD <ch.benard@gmail.com>
2025-09-04 10:45:51 +0200

    Fix: Supprilmer la transaction inutile refers to https://trello.com/c/thYcnztd/2452

--- packages/api/server/models/shantytownParcelOwnerModel/findAll.ts
+++ packages/api/server/models/shantytownParcelOwnerModel/findAll.ts
@@ -1,29 +1,17 @@
 import { sequelize } from '#db/sequelize';
-import { Transaction } from 'sequelize';
 import ServiceError from '#server/errors/ServiceError';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 import { RawParcelOwner } from '#root/types/resources/ParcelOwner.d';
 
-export default async (user: AuthUser, shantytownId: number, argTransaction: Transaction | undefined = undefined): Promise<RawParcelOwner[]> => {
-    let transaction: Transaction = argTransaction;
-    transaction ??= await sequelize.transaction();
-
+export default async (user: AuthUser, shantytownId: number): Promise<RawParcelOwner[]> => {
     try {
         const [results]: any = await sequelize.query(`
             SELECT * FROM shantytown_parcel_owners WHERE fk_shantytown = :shantytownId
         `, {
             replacements: { shantytownId },
-            transaction,
         });
-
-        if (argTransaction === undefined) {
-            await transaction.commit();
-        }
         return results;
     } catch (error) {
-        if (argTransaction === undefined) {
-            await transaction.rollback();
-        }
         throw new ServiceError('parcel_owner_fetch_failed', new Error(error?.message));
     }
 };
