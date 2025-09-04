07b8ad979c45eb6d5d75d58676ff5af3a88ced89
Christophe BENARD <ch.benard@gmail.com>
2025-09-04 10:26:14 +0200

    Fix: Ajouter une transaction refers to https://trello.com/c/thYcnztd/2452

--- packages/api/db/migrations/30000090-02-populate-table-shantytown_parcel_owners.js
+++ packages/api/db/migrations/30000090-02-populate-table-shantytown_parcel_owners.js
@@ -1,15 +1,33 @@
 module.exports = {
-    up: queryInterface => queryInterface.sequelize.query(
-        `INSERT INTO shantytown_parcel_owners(fk_shantytown, fk_user, owner_name, fk_owner_type)
-        SELECT
-            shantytowns.shantytown_id,
-            shantytowns.created_by,
-            shantytowns.owner,
-            shantytowns.fk_owner_type
-        FROM shantytowns
-        WHERE shantytowns.fk_owner_type IS NOT NULL AND shantytowns.status = 'open'
-        `,
-    ),
+    async up(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await queryInterface.sequelize.query(
+                `INSERT INTO shantytown_parcel_owners(fk_shantytown, fk_user, owner_name, fk_owner_type)
+                SELECT
+                    shantytowns.shantytown_id,
+                    shantytowns.created_by,
+                    shantytowns.owner,
+                    shantytowns.fk_owner_type
+                FROM shantytowns
+                WHERE shantytowns.fk_owner_type IS NOT NULL AND shantytowns.status = 'open'`,
+                { transaction },
+            );
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+    },
 
-    down: queryInterface => queryInterface.sequelize.query('DELETE FROM shantytown_parcel_owners'),
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await queryInterface.sequelize.query('DELETE FROM shantytown_parcel_owners', { transaction });
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+    },
 };
