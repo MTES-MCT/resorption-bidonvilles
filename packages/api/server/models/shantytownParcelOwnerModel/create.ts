815c846a459518feceecf5f65b4064f84cd04d94
Gilles COGNIN <gillescognin@gmail.com>
2025-09-04 18:06:57 +0200

    fix: Correction de la présence d'une variable dans la query

--- packages/api/server/models/shantytownParcelOwnerModel/create.ts
+++ packages/api/server/models/shantytownParcelOwnerModel/create.ts
@@ -9,32 +9,36 @@ interface ShantytownParcelOwnerCreationResult {
 }
 
 export default async (user: AuthUser, shantytownId: number, owners: ParcelOwnerInsert[], argTransaction: Transaction | undefined = undefined): Promise<number[]> => {
-    let transaction: Transaction = argTransaction;
-    transaction ??= await sequelize.transaction();
+    const transaction: Transaction = argTransaction ?? await sequelize.transaction();
 
     if (!owners || owners.length === 0) {
         throw new ServiceError('invalid_data', new Error('Au moins un propriétaire de parcelle doit être fourni'));
     }
 
     try {
-        const results = await sequelize.query<ShantytownParcelOwnerCreationResult>(
-            `
-                INSERT INTO shantytown_parcel_owners
-                    (fk_shantytown, owner_name, fk_owner_type, fk_user)
-                VALUES
-                    ${owners.map(() => '(?, ?, ?, ?)').join(', ')}
-                RETURNING shantytown_parcel_owner_id
-            `,
-            {
-                replacements: owners.flatMap(
-                    owner => [shantytownId, owner.name, owner.type, user.id],
-                ),
-                transaction,
-                type: QueryTypes.SELECT,
-            },
+        let createdIds: number[] = [];
+        const query = `
+            INSERT INTO shantytown_parcel_owners
+                (fk_shantytown, owner_name, fk_owner_type, fk_user)
+            VALUES
+                (?, ?, ?, ?)
+            RETURNING shantytown_parcel_owner_id
+        `;
+
+        const results = await Promise.all(
+            owners.map(owner => sequelize.query<ShantytownParcelOwnerCreationResult>(
+                query,
+                {
+                    replacements: [shantytownId, owner.name, owner.type, user.id],
+                    transaction,
+                    type: QueryTypes.SELECT,
+                },
+            )),
         );
 
-        return results.map(r => r.shantytown_parcel_owner_id);
+        createdIds = results.map(([result]) => result.shantytown_parcel_owner_id);
+
+        return createdIds;
     } catch (error) {
         throw new ServiceError('parcel_owner_creation_failed', error);
     }
