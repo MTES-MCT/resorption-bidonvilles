060e0203782b6971b99bbf2877e9a35b963303c4
Christophe BENARD <ch.benard@gmail.com>
2025-09-04 10:27:08 +0200

    Fix: Corriger le risque d'injection SQL refers to https://trello.com/c/thYcnztd/2452

--- packages/api/server/models/shantytownParcelOwnerModel/create.ts
+++ packages/api/server/models/shantytownParcelOwnerModel/create.ts
@@ -1,10 +1,14 @@
 import { sequelize } from '#db/sequelize';
-import { Transaction } from 'sequelize';
+import { QueryTypes, Transaction } from 'sequelize';
 import ServiceError from '#server/errors/ServiceError';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 import { ParcelOwnerInsert } from '#root/types/resources/ParcelOwner.d';
 
-export default async (user: AuthUser, shantytownId: number, owners: ParcelOwnerInsert[], argTransaction: Transaction | undefined = undefined) => {
+interface ShantytownParcelOwnerCreationResult {
+    shantytown_parcel_owner_id: number;
+}
+
+export default async (user: AuthUser, shantytownId: number, owners: ParcelOwnerInsert[], argTransaction: Transaction | undefined = undefined): Promise<number[]> => {
     let transaction: Transaction = argTransaction;
     transaction ??= await sequelize.transaction();
 
@@ -12,34 +16,26 @@ export default async (user: AuthUser, shantytownId: number, owners: ParcelOwnerI
         throw new ServiceError('invalid_data', new Error('Au moins un propriétaire de parcelle doit être fourni'));
     }
 
-    const { names, types } = owners.reduce((acc, owner) => {
-        acc.names.push(owner.name);
-        acc.types.push(owner.type);
-        return acc;
-    }, { names: [] as ParcelOwnerInsert['name'][], types: [] as ParcelOwnerInsert['type'][] });
-
     try {
-        const [results]: [any[], any] = await sequelize.query(`
-            INSERT INTO shantytown_parcel_owners (fk_shantytown, owner_name, fk_owner_type, fk_user)
-        SELECT
-            ${shantytownId},
-            u_name.name,
-            u_type.type,
-            ${user.id}
-        FROM
-            unnest($names::text[]) WITH ORDINALITY AS u_name(name, ord)
-        JOIN
-            unnest($types::int[]) WITH ORDINALITY AS u_type(type, ord) ON u_name.ord = u_type.ord
-        RETURNING shantytown_parcel_owner_id, fk_shantytown, owner_name, fk_owner_type;
-    `, {
-            bind: {
-                names,
-                types,
+        const results = await sequelize.query<ShantytownParcelOwnerCreationResult>(
+            `
+                INSERT INTO shantytown_parcel_owners
+                    (fk_shantytown, owner_name, fk_owner_type, fk_user)
+                VALUES
+                    ${owners.map(() => '(?, ?, ?, ?)').join(', ')}
+                RETURNING shantytown_parcel_owner_id
+            `,
+            {
+                replacements: owners.flatMap(
+                    owner => [shantytownId, owner.name, owner.type, user.id],
+                ),
+                transaction,
+                type: QueryTypes.SELECT,
             },
-            transaction,
-        });
+        );
+
         return results.map(r => r.shantytown_parcel_owner_id);
     } catch (error) {
-        throw new ServiceError('parcel_owner_creation_failed', new Error(error?.message));
+        throw new ServiceError('parcel_owner_creation_failed', error);
     }
 };
