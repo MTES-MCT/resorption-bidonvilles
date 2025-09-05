0c6598801bb20795eed5f6899b649c25dff5018b
Gilles COGNIN <gillescognin@gmail.com>
2025-09-05 10:18:20 +0200

    fix: Amélioration du score de duplication en externalisant/mutualisant la création de l'objet baseTown

--- packages/api/server/services/shantytown/create.spec.ts
+++ packages/api/server/services/shantytown/create.spec.ts
@@ -5,8 +5,7 @@ import chaiSubset from 'chai-subset';
 import { creationInput as fakeShantytown } from '#test/utils/shantytown';
 import { serialized as fakeUser } from '#test/utils/user';
 import { rewiremock } from '#test/rewiremock';
-
-import { AuthUser } from '#server/middlewares/authMiddleware';
+import baseShantytown from '#server/services/shantytown/_common/baseShantytown';
 import { ParcelOwnerInsert } from '#root/types/resources/ParcelOwner.d';
 
 const { expect } = chai;
@@ -49,6 +48,7 @@ rewiremock('#server/utils/mattermost').with({
 rewiremock('#server/models/incomingTownsModel/create').with(stubs.incomingTownsCreate);
 rewiremock('#server/models/userModel/getLocationWatchers').with(stubs.getLocationWatchers);
 rewiremock('#server/mails/mails').with({ sendUserShantytownDeclared: stubs.sendUserShantytownDeclared });
+rewiremock('#server/services/shantytown/_common/baseShantytown').callThrough();
 
 rewiremock.enable();
 // eslint-disable-next-line import/newline-after-import, import/first
@@ -58,28 +58,19 @@ rewiremock.disable();
 describe('services/shantytown', () => {
     describe('create()', () => {
         let shantytownId: string;
-        let user: any;
-        let fakeUserData: AuthUser;
+        let fakeUserData: any;
         let townData: any;
         let owners: ParcelOwnerInsert[];
         let social_origins: string;
         let town: any;
         let baseTown: any;
         let justice: any;
+        const watchers = [{ user_id: 10 }, { user_id: 11 }, { user_id: 12 }];
 
         beforeEach(() => {
             shantytownId = global.generate('string');
-            user = {
-                id: global.generate('string'),
-                isAllowedTo: sinon.stub(),
-            };
             fakeUserData = fakeUser();
-            stubs.getLocationWatchers.returns([{
-                user_id: fakeUserData.id,
-                email: fakeUserData.email,
-                first_name: fakeUserData.first_name,
-                last_name: fakeUserData.last_name,
-            }]);
+            fakeUserData.isAllowedTo = sandbox.stub();
             townData = fakeShantytown();
             social_origins = global.generate('string');
             town = {
@@ -102,72 +93,7 @@ describe('services/shantytown', () => {
                 name: 'Pierre Quiroul',
                 type: 2,
             }];
-            baseTown = {
-                name: townData.name,
-                latitude: townData.latitude,
-                longitude: townData.longitude,
-                address: townData.address,
-                addressDetails: townData.detailed_address,
-                builtAt: townData.built_at,
-                populationTotal: townData.population_total,
-                populationTotalFemales: townData.population_total_females,
-                populationCouples: townData.population_couples,
-                populationMinors: townData.population_minors,
-                populationMinorsGirls: townData.population_minors_girls,
-                populationMinors0To3: townData.population_minors_0_3,
-                populationMinors3To6: townData.population_minors_3_6,
-                populationMinors6To12: townData.population_minors_6_12,
-                populationMinors12To16: townData.population_minors_12_16,
-                populationMinors16To18: townData.population_minors_16_18,
-                minorsInSchool: townData.minors_in_school,
-                caravans: townData.caravans,
-                huts: townData.huts,
-                tents: townData.tents,
-                cars: townData.cars,
-                mattresses: townData.mattresses,
-                fieldType: townData.field_type,
-                isReinstallation: townData.is_reinstallation,
-                reinstallationComments: townData.reinstallation_comments,
-                city: townData.citycode,
-                createdBy: user.id,
-                declaredAt: townData.declared_at,
-                censusStatus: townData.census_status,
-                censusConductedAt: townData.census_conducted_at,
-                censusConductedBy: townData.census_conducted_by,
-                living_conditions_version: 2,
-
-                water_access_type: townData.water_access_type,
-                water_access_type_details: townData.water_access_type_details,
-                water_access_is_public: townData.water_access_is_public,
-                water_access_is_continuous: townData.water_access_is_continuous,
-                water_access_is_continuous_details: townData.water_access_is_continuous_details,
-                water_access_is_local: townData.water_access_is_local,
-                water_access_is_close: townData.water_access_is_close,
-                water_access_is_unequal: townData.water_access_is_unequal,
-                water_access_is_unequal_details: townData.water_access_is_unequal_details,
-                water_access_has_stagnant_water: townData.water_access_has_stagnant_water,
-                water_access_comments: townData.water_access_comments,
-
-                sanitary_open_air_defecation: townData.sanitary_open_air_defecation,
-                sanitary_access_working_toilets: townData.sanitary_working_toilets,
-                sanitary_access_toilets_are_inside: townData.sanitary_toilets_are_inside,
-                sanitary_access_toilets_are_lighted: townData.sanitary_toilets_are_lighted,
-                sanitary_access_hand_washing: townData.sanitary_hand_washing,
-
-                electricity_access: townData.electricity_access,
-                electricity_access_is_unequal: townData.electricity_access_is_unequal,
-
-                trash_is_piling: townData.trash_is_piling,
-                trash_evacuation_is_close: townData.trash_evacuation_is_close,
-                trash_evacuation_is_safe: townData.trash_evacuation_is_safe,
-                trash_evacuation_is_regular: townData.trash_evacuation_is_regular,
-                trash_bulky_is_piling: townData.trash_bulky_is_piling,
-
-                pest_animals: townData.pest_animals_presence,
-                pest_animals_details: townData.pest_animals_details,
-
-                fire_prevention: townData.fire_prevention_diagnostic,
-            };
+            baseTown = baseShantytown(townData, fakeUserData);
             justice = {
                 ownerComplaint: townData.owner_complaint,
                 justiceProcedure: townData.justice_procedure,
@@ -188,10 +114,10 @@ describe('services/shantytown', () => {
         });
 
         it('créer un site en bdd avec les données fournies par l\'utilisateur', async () => {
-            user.isAllowedTo.returns(true);
+            fakeUserData.isAllowedTo.returns(true);
             stubs.shantytownCreate.resolves(shantytownId);
             stubs.findOne.resolves(town);
-            await createService(townData, user);
+            await createService(townData, fakeUserData);
             expect(stubs.shantytownCreate).to.have.been.calledOnceWith(
                 {
                     ...baseTown,
@@ -202,10 +128,10 @@ describe('services/shantytown', () => {
         });
 
         it('n\'enregistre pas les données judiciaires et du propriétaire si l\'utilisateur n\'y a pas accès', async () => {
-            user.isAllowedTo.returns(false);
+            fakeUserData.isAllowedTo.returns(false);
             stubs.shantytownCreate.resolves(shantytownId);
             stubs.findOne.resolves(town);
-            await createService(townData, user);
+            await createService(townData, fakeUserData);
             expect(stubs.electricityAccessTypesCreate).to.have.been.calledOnceWith(
                 shantytownId,
                 townData.electricity_access_types,
@@ -221,47 +147,45 @@ describe('services/shantytown', () => {
         });
 
         it('enregistre les origines sociales en bdd si elles sont indiquées par l\'utilisateur', async () => {
-            user.isAllowedTo.returns(true);
+            fakeUserData.isAllowedTo.returns(true);
             stubs.shantytownCreate.resolves(shantytownId);
             stubs.findOne.resolves(town);
             await createService({
                 ...townData, social_origins,
-            }, user);
+            }, fakeUserData);
             // eslint-disable-next-line no-unused-expressions
             expect(stubs.socialOriginCreate).to.have.been.calledOnceWith(shantytownId, social_origins);
         });
 
         it('enregistre les propriétaires de parcelles en bdd si elles sont indiquées par l\'utilisateur', async () => {
-            user.isAllowedTo.returns(true);
+            fakeUserData.isAllowedTo.returns(true);
             stubs.shantytownCreate.resolves(shantytownId);
             stubs.findOne.resolves(town);
-            // townData.owner = owners;
             await createService({
                 ...townData,
                 owner: { owners },
-            }, user);
+            }, fakeUserData);
             // eslint-disable-next-line no-unused-expressions
-            expect(stubs.shantytownParcelOwnerService.create).to.have.been.calledOnceWith(user, shantytownId, owners, stubs.transaction);
+            expect(stubs.shantytownParcelOwnerService.create).to.have.been.calledOnceWith(fakeUserData, shantytownId, owners, stubs.transaction);
         });
 
         it('si la connexion à mattermost est établie, envoie une alerte', async () => {
-            user.isAllowedTo.returns(true);
+            fakeUserData.isAllowedTo.returns(true);
             stubs.shantytownCreate.resolves(shantytownId);
             stubs.findOne.resolves(town);
-            await createService(townData, user);
+            await createService(townData, fakeUserData);
 
             // eslint-disable-next-line no-unused-expressions
-            expect(stubs.triggerShantytownCreationAlert).to.have.been.calledOnceWith(town, user);
+            expect(stubs.triggerShantytownCreationAlert).to.have.been.calledOnceWith(town, fakeUserData);
         });
 
         it('envoie une alerte mail à tous les utilisateurs du département', async () => {
             townData.city = { code: townData.citycode, departement: townData.departement };
-            const watchers = [{ user_id: 0 }, { user_id: 1 }, { user_id: 2 }];
             stubs.getLocationWatchers.resolves(watchers);
-            user.isAllowedTo.returns(true);
+            fakeUserData.isAllowedTo.returns(true);
             stubs.shantytownCreate.resolves(shantytownId);
             stubs.findOne.resolves({ ...town, city: { code: townData.citycode, departement: townData.departement } });
-            await createService(townData, user);
+            await createService(townData, fakeUserData);
             // eslint-disable-next-line no-unused-expressions
             expect(stubs.getLocationWatchers).to.have.been.calledOnce;
             expect(stubs.sendUserShantytownDeclared).to.have.callCount(watchers.length);
--- packages/api/server/services/shantytown/create.ts
+++ packages/api/server/services/shantytown/create.ts
@@ -8,78 +8,12 @@ import shantytownParcelOwnerService from '#server/services/shantytownParcelOwner
 import insertIncomingTown from '#server/models/incomingTownsModel/create';
 import getLocationWatchers from '#server/models/userModel/getLocationWatchers';
 import { triggerShantytownCreationAlert } from '#server/utils/mattermost';
+import baseShantytown from '#server/services/shantytown/_common/baseShantytown';
 import ServiceError from '#server/errors/ServiceError';
 import mails from '#server/mails/mails';
 
 export default async (townData, user) => {
-    const baseTown = {
-        name: townData.name,
-        latitude: townData.latitude,
-        longitude: townData.longitude,
-        address: townData.address,
-        addressDetails: townData.detailed_address,
-        builtAt: townData.built_at,
-        populationTotal: townData.population_total,
-        populationTotalFemales: townData.population_total_females,
-        populationCouples: townData.population_couples,
-        populationMinors: townData.population_minors,
-        populationMinorsGirls: townData.population_minors_girls,
-        populationMinors0To3: townData.population_minors_0_3,
-        populationMinors3To6: townData.population_minors_3_6,
-        populationMinors6To12: townData.population_minors_6_12,
-        populationMinors12To16: townData.population_minors_12_16,
-        populationMinors16To18: townData.population_minors_16_18,
-        minorsInSchool: townData.minors_in_school,
-        caravans: townData.caravans,
-        huts: townData.huts,
-        tents: townData.tents,
-        cars: townData.cars,
-        mattresses: townData.mattresses,
-        fieldType: townData.field_type,
-        isReinstallation: townData.is_reinstallation,
-        reinstallationComments: townData.reinstallation_comments,
-        city: townData.citycode,
-        createdBy: user.id,
-        declaredAt: townData.declared_at,
-        censusStatus: townData.census_status,
-        censusConductedAt: townData.census_conducted_at,
-        censusConductedBy: townData.census_conducted_by,
-
-        // living conditions
-        living_conditions_version: 2,
-
-        water_access_type: townData.water_access_type,
-        water_access_type_details: townData.water_access_type_details,
-        water_access_is_public: townData.water_access_is_public,
-        water_access_is_continuous: townData.water_access_is_continuous,
-        water_access_is_continuous_details: townData.water_access_is_continuous_details,
-        water_access_is_local: townData.water_access_is_local,
-        water_access_is_close: townData.water_access_is_close,
-        water_access_is_unequal: townData.water_access_is_unequal,
-        water_access_is_unequal_details: townData.water_access_is_unequal_details,
-        water_access_has_stagnant_water: townData.water_access_has_stagnant_water,
-        water_access_comments: townData.water_access_comments,
-
-        sanitary_open_air_defecation: townData.sanitary_open_air_defecation,
-        sanitary_access_working_toilets: townData.sanitary_working_toilets,
-        sanitary_access_toilets_are_inside: townData.sanitary_toilets_are_inside,
-        sanitary_access_toilets_are_lighted: townData.sanitary_toilets_are_lighted,
-        sanitary_access_hand_washing: townData.sanitary_hand_washing,
-
-        electricity_access: townData.electricity_access,
-        electricity_access_is_unequal: townData.electricity_access_is_unequal,
-
-        trash_is_piling: townData.trash_is_piling,
-        trash_evacuation_is_close: townData.trash_evacuation_is_close,
-        trash_evacuation_is_safe: townData.trash_evacuation_is_safe,
-        trash_evacuation_is_regular: townData.trash_evacuation_is_regular,
-        trash_bulky_is_piling: townData.trash_bulky_is_piling,
-
-        pest_animals: townData.pest_animals_presence,
-        pest_animals_details: townData.pest_animals_details,
-
-        fire_prevention: townData.fire_prevention_diagnostic,
-    };
+    const baseTown = baseShantytown(townData, user);
 
     const transaction = await sequelize.transaction();
     let shantytown_id: number;
