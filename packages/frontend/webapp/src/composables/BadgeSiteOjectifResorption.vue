c892a43dc00c88ae42b0ff44413ccb5fb92a61e8
Christophe BENARD <ch.benard@gmail.com>
2025-09-08 15:47:44 +0200

    Fix: Remplacer TagObjectifResorption par BadgeSiteOjectifResorption refers to https://trello.com/c/XS5HSPHE/2493

---
+++ packages/frontend/webapp/src/composables/BadgeSiteOjectifResorption.vue
@@ -0,0 +1,28 @@
+<template>
+    <DsfrBadge
+        v-if="resorptionTarget"
+        :noIcon="noicon ? true : false"
+        :label="objectifResorptionLabel(resorptionTarget)"
+        type="info"
+        class="mt-1 text-xs py-2 mr-2"
+    />
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+    resorptionTarget: {
+        type: String,
+        default: null,
+    },
+    noicon: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+function objectifResorptionLabel(target) {
+    return `Objectif résorption ${target} par la Préfecture`;
+}
+</script>
