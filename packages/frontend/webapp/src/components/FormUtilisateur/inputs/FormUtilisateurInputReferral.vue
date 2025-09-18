66e6d6823b56d4d5fc70dc8b719c6214cd3972a7
Gilles COGNIN <gillescognin@gmail.com>
2025-09-18 09:17:04 +0200

    design: Remplacement des boutons radio par la version DSFR

--- packages/frontend/webapp/src/components/FormUtilisateur/inputs/FormUtilisateurInputReferral.vue
+++ packages/frontend/webapp/src/components/FormUtilisateur/inputs/FormUtilisateurInputReferral.vue
@@ -1,19 +1,20 @@
 <template>
-    <CheckableGroup id="referral" :label="label">
-        <Radio
-            v-for="item in items[language]"
-            :key="item.value"
-            :value="item.value"
-            :label="item.label"
+    <CheckableGroup id="referral" :label="label" class="!mb-2">
+        <DsfrRadioButtonSet
             name="referral"
+            :options="radioItems"
+            small
+            v-model="values"
+            class="!mb-0"
         />
     </CheckableGroup>
 </template>
 
 <script setup>
-import { CheckableGroup, Radio } from "@resorptionbidonvilles/ui";
+import { CheckableGroup } from "@resorptionbidonvilles/ui";
 import items from "@/utils/contact_referrals.js";
-import { defineProps, toRefs } from "vue";
+import { defineProps, toRefs, computed } from "vue";
+import { useField } from "vee-validate";
 
 const props = defineProps({
     label: String,
@@ -23,4 +24,16 @@ const props = defineProps({
     },
 });
 const { label, language } = toRefs(props);
+
+const { value: values } = useField("referral");
+
+const radioItems = computed(() => {
+    return items[language.value];
+});
 </script>
+
+<style scoped>
+:deep(fieldset) {
+    margin-bottom: 0 !important;
+}
+</style>
