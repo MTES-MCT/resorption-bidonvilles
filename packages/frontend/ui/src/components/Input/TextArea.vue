7a0241cc5f21e5b3fede4351979c42ee758708d5
Gilles COGNIN <gillescognin@gmail.com>
2025-09-24 15:13:52 +0200

    fix: Correction du TextArea passé en Dsfr

--- packages/frontend/ui/src/components/Input/TextArea.vue
+++ packages/frontend/ui/src/components/Input/TextArea.vue
@@ -1,131 +1,92 @@
 <template>
-    <Field :id="id" :name="id" v-slot="{ field, errors }">
+    <Field :name="validationName || id" :rules="rules" v-slot="{ field, errors, handleChange }" as="div">
         <InputWrapper :hasErrors="!!errors.length">
-            <InputLabel :label="label" :info="info" :showMandatoryStar="showMandatoryStar" :for="`rb-select-${id}`" />
+            <InputLabel :label="label" :info="info" :showMandatoryStar="showMandatoryStar" :for="id" />
 
             <div class="relative">
                 <InputIcon position="before" :icon="prefixIcon" v-if="prefixIcon" />
-                <DsfrInput isTextarea ref="input" :id="`rb-select-${id}`" :data-cy-field="cypressName" :disabled="isSubmitting || disabled"
-                :readonly="isSubmitting || disabled" :class="['bg-white', classes]" v-bind="{ ...field, ...filteredProps }" v-model="currentValue" 
-                @update:modelValue="(val) => { currentValue = val; $emit('input', val) }" @focus="onFocus" @blur="(e) => { onBlur(e); $emit('blur', e) }"/>
+                <DsfrInput
+                    isTextarea
+                    ref="textarea"
+                    :id="id"
+                    :data-cy-field="cypressName"
+                    :disabled="isSubmitting || disabled"
+                    class="h-40"
+                    :class="['bg-white', classes]"
+                    :model-value="field.value"
+                    @update:model-value="handleChange"
+                    @blur="onBlur"
+                />
                 <InputIcon position="after" :icon="suffixIcon" v-if="suffixIcon" />
             </div>
-            <InputError>{{ errors[0] }}</InputError>
+            <InputError v-if="errors.length">{{ errors[0] }}</InputError>
         </InputWrapper>
     </Field>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from "vue";
 import { Field, useIsSubmitting } from "vee-validate";
-
-import filteredProps from "./mixins/filteredProps";
 import InputLabel from "./utils/InputLabel.vue";
 import InputWrapper from "./utils/InputWrapper.vue";
 import InputError from "./utils/InputError.vue";
 import InputIcon from "./utils/InputIcon.vue";
 import getInputClasses from "./utils/getInputClasses";
 
-export default {
-    name: "TextArea",
-    mixins: [filteredProps],
-    setup() {
-        const isSubmitting = useIsSubmitting();
-        return {
-            isSubmitting,
-        };
+const props = defineProps({
+    label: String,
+    info: String,
+    placeholder: String,
+    rules: String,
+    validationName: String,
+    id: { type: String, required: true },
+    variant: {
+        type: String,
+        default: "default"
     },
-    props: {
-        label: {
-            type: String
-        },
-        info: {
-            type: String
-        },
-        placeholder: {
-            type: String
-        },
-        type: {
-            type: String,
-            default: "text"
-        },
-        value: {
-            type: String
-        },
-        rules: {
-            type: String
-        },
-        validationName: {
-            type: String
-        },
-        id: {
-            type: String
-        },
-        variant: {
-            type: String,
-            default: "default"
-        },
-        prefixIcon: {
-            type: String
-        },
-        suffixIcon: {
-            type: String
-        },
-        rows: {
-            type: [String, Number]
-        },
-        cols: {
-            type: String
-        },
-        cypressName: {
-            type: String
-        },
-        showMandatoryStar: {
-            required: false,
-            type: Boolean,
-            default: false
-        },
-        disabled: {
-            type: Boolean,
-            default: false
-        }
+    prefixIcon: String,
+    suffixIcon: String,
+    rows: [String, Number],
+    cols: String,
+    cypressName: String,
+    showMandatoryStar: {
+        type: Boolean,
+        default: false
     },
-    data() {
-        return {
-            isFocused: false,
-        };
-    },
-    computed: {
-        classes() {
-            const inputOptions = {
-                // error: !!this.errors?.length,
-                prefixIcon: this.prefixIcon,
-                suffixIcon: this.suffixIcon,
-                disabled: this.disabled
-            };
-
-            return {
-                state: [...getInputClasses("state", inputOptions)],
-                default: getInputClasses("default", inputOptions)
-            }[this.variant];
-        }
-    },
-    methods: {
-        onFocus() {
-            this.isFocused = true;
-        },
-        onBlur() {
-            this.isFocused = false;
-        },
-        focus() {
-            this.$refs.textarea.focus();
-        },
-    },
-    components: {
-        InputLabel,
-        InputWrapper,
-        InputError,
-        InputIcon,
-        Field
+    disabled: {
+        type: Boolean,
+        default: false
     }
+});
+
+const emit = defineEmits(['blur']);
+
+const isSubmitting = useIsSubmitting();
+const textarea = ref(null);
+
+const classes = computed(() => {
+    const inputOptions = {
+        prefixIcon: props.prefixIcon,
+        suffixIcon: props.suffixIcon,
+        disabled: props.disabled
+    };
+
+    return {
+        state: [...getInputClasses("state", inputOptions)],
+        default: getInputClasses("default", inputOptions)
+    }[props.variant];
+});
+
+const onBlur = (event) => {
+    emit('blur', event);
 };
+
+const focus = () => {
+    textarea.value?.focus();
+};
+
+// Exposer la méthode 'focus' au composant parent
+defineExpose({
+    focus,
+});
 </script>
