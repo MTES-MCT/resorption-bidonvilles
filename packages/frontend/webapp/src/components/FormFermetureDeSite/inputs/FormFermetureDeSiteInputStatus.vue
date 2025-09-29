74d4cfa19495fb827b101414c0bf6c6f7d728414
Gilles COGNIN <gillescognin@gmail.com>
2025-09-29 10:41:21 +0200

    design: DSFRisation du composant

--- packages/frontend/webapp/src/components/FormFermetureDeSite/inputs/FormFermetureDeSiteInputStatus.vue
+++ packages/frontend/webapp/src/components/FormFermetureDeSite/inputs/FormFermetureDeSiteInputStatus.vue
@@ -1,18 +1,22 @@
 <template>
-    <CheckableGroup id="status" :label="labels.status">
-        <Radio
-            v-for="item in items"
-            :key="item.value"
-            :value="item.value"
-            :label="item.label"
-            name="status"
-            v-bind="$attrs"
-        />
-    </CheckableGroup>
+    <DsfrRadioButtonSet
+        v-model="status"
+        id="status"
+        name="status"
+        small
+        :options="items"
+        :errorMessage="errors.length > 0 ? errors[0] : ''"
+    >
+        <template #legend>
+            <p class="font-bold">{{ labels.status }}</p>
+        </template>
+    </DsfrRadioButtonSet>
 </template>
 
 <script setup>
-import { CheckableGroup, Radio } from "@resorptionbidonvilles/ui";
 import items from "@/utils/closing_reasons";
 import labels from "../FormFermetureDeSite.labels";
+import { useField } from "vee-validate";
+
+const { value: status, errors } = useField("status", "required");
 </script>
