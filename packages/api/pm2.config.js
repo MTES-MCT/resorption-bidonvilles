3673c0947636aee41e31c73600d59c52b6155077
Gilles COGNIN <gillescognin@gmail.com>
2025-10-02 16:09:12 +0200

    fix: Modification du nombre d'instance en fonction du nombre de CPU

--- packages/api/pm2.config.js
+++ packages/api/pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: 'api',
             script: 'server/index.js',
             exec_mode: 'cluster',
-            instances: 'max',
+            instances: process.env.RB_API_CPU_INSTANCES || 1,
             log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS Z',
             watch: false,
             env: {
