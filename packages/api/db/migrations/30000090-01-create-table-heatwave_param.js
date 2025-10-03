b61c6c9814c54dcd5f0de5153a3c0143cfb44e9e
Gilles COGNIN <gillescognin@gmail.com>
2025-10-03 18:00:55 +0200

    feat: Ajout de la table et de la donnée initiale

---
+++ packages/api/db/migrations/30000090-01-create-table-heatwave_param.js
@@ -0,0 +1,39 @@
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.createTable(
+                'heatwave_param',
+                {
+                    start_date: {
+                        type: Sequelize.INTEGER,
+                        allowNull: false,
+                    },
+                    end_date: {
+                        type: Sequelize.DATE,
+                        allowNull: false,
+                    },
+                },
+                { transaction },
+            );
+
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+    },
+
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.dropTable('heatwave_param', { transaction });
+            await transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+};
