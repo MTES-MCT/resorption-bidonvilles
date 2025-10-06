358dee7d7a39dd7537073c300dd8bdf5e0a69969
Christophe BENARD <ch.benard@gmail.com>
2025-10-06 17:32:07 +0200

    feat: Regrouper lles types pour permettre l'héritage et l'imbrication de "AND" dans les "OU" refers to https://trello.com/c/3zqUkzR1/2480

--- packages/api/server/models/_common/types/Where.ts
+++ packages/api/server/models/_common/types/Where.ts
@@ -1,3 +1,17 @@
-import { WhereClauseGroup } from './WhereClauseGroup';
+import { Primitive } from './Primitive';
+
+export type WhereClause = {
+    value?: Primitive | Primitive[],
+    query?: string,
+    operator?: string,
+    anyOperator?: string,
+    arrayOperator?: boolean,
+    not?: boolean,
+    andClauses?: Where
+};
+
+export type WhereClauseGroup = {
+    [column: string]: WhereClause
+};
 
 export type Where = Array<WhereClauseGroup>;
