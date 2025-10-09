7f898ddfc927f892e8c4d7e367edbd69d5932b9f
Christophe BENARD <ch.benard@gmail.com>
2025-10-09 16:02:58 +0200

    Fix: Compléter le typage refers to https://trello.com/c/3zqUkzR1/2480

--- packages/api/server/controllers/shantytown/exportList/shantytownFiltersAsQueryParamList.ts
+++ packages/api/server/controllers/shantytown/exportList/shantytownFiltersAsQueryParamList.ts
@@ -1,4 +1,12 @@
-const all = {
+type FilterLabel = string;
+
+type FilterMap = Record<string, FilterLabel>;
+
+type SiteStatus = 'open' | 'inProgress' | 'closed' | 'resorbed';
+
+type ShantytownFiltersAsQueryParamList = Record<SiteStatus, FilterMap>;
+
+const all: FilterMap = {
     population: 'Nombre de personnes',
     fieldType: 'Type de sites',
     origin: 'Origines',
@@ -6,20 +14,20 @@ const all = {
     justice: 'Justice',
     administrativeOrder: 'Ordre administratif',
     rhi: 'RHI',
-};
+} as const;
 
-const openOrInProgress = {
+const openOrInProgress: FilterMap = {
     conditions: 'Conditions de vie',
     actors: 'Intervenants',
     heatwave: 'Risque canicule',
-};
+} as const;
 
-const closeOrResobed = {
+const closeOrResobed: FilterMap = {
     resorbedOrClosed: 'Résorbé / fermé',
     closingReason: 'Cause de la fermeture',
-};
+} as const;
 
-export default {
+const shantytownFiltersAsQueryParamList: ShantytownFiltersAsQueryParamList = {
     open: {
         ...all,
         ...openOrInProgress,
@@ -37,3 +45,5 @@ export default {
         ...closeOrResobed,
     },
 };
+
+export default shantytownFiltersAsQueryParamList;
