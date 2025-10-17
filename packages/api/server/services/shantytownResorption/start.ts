34a3d237abba6148eda41f989ee2a678fd5951fb
Christophe BENARD <ch.benard@gmail.com>
2025-10-17 11:53:17 +0200

    Feat: Gérer l'historisation lors de la création des phases refers to https://trello.com/c/PZ4JI9pn/2491

--- packages/api/server/services/shantytownResorption/start.ts
+++ packages/api/server/services/shantytownResorption/start.ts
@@ -2,6 +2,7 @@ import { sequelize } from '#db/sequelize';
 import ServiceError from '#server/errors/ServiceError';
 import preparatoryPhasesTowardResorptionModel from '#server/models/preparatoryPhasesTowardResorptionModel';
 import shantytownPreparatoryPhasesTowardResorptionModel from '#server/models/shantytownPreparatoryPhasesTowardResorptionModel';
+import shantytownModel from '#server/models/shantytownModel';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 import { ShantytownPreparatoryPhasesTowardResorption } from '#root/types/resources/ShantytownPreparatoryPhasesTowardResorption.d';
 import { PreparatoryPhaseTowardResorption } from '#root/types/resources/PreparatoryPhaseTowardResorption.d';
@@ -9,6 +10,7 @@ import { PreparatoryPhaseTowardResorption } from '#root/types/resources/Preparat
 export default async (townId: number, user: AuthUser): Promise<{ townId: number, phases: ShantytownPreparatoryPhasesTowardResorption[] }> => {
     const transaction = await sequelize.transaction();
     let resorptionStartingPhases: PreparatoryPhaseTowardResorption[] = [];
+
 
     // Récupérer les phases préparatoires définissant le démarrage de la résorption
     resorptionStartingPhases = await preparatoryPhasesTowardResorptionModel.getAll(true);
@@ -28,6 +30,25 @@ export default async (townId: number, user: AuthUser): Promise<{ townId: number,
                 },
                 transaction,
             )));
+
+            // Mettre à jour updated_at et updated_by dans la table shantytowns AVANT l'historisation
+            await sequelize.query(
+                `UPDATE shantytowns 
+                 SET updated_at = NOW(), updated_by = :userId 
+                 WHERE shantytown_id = :townId`,
+                {
+                    replacements: { userId: user.id, townId },
+                    transaction,
+                },
+            );
+
+            // Historiser l'état avec les phases
+            await shantytownModel.update(
+                user,
+                townId,
+                {},
+                transaction,
+            );
         } catch (error) {
             await transaction.rollback();
             throw new ServiceError('insert_failed', error);
