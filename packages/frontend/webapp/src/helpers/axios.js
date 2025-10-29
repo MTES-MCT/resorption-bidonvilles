09de9bb30e196a968c739bc89c136fb034678898
Christophe BENARD <ch.benard@gmail.com>
2025-10-29 17:41:21 +0100

    Feat: Transformer tous les objets Date avant envoi refers to https://trello.com/c/YF7H9V3b/2533

--- packages/frontend/webapp/src/helpers/axios.js
+++ packages/frontend/webapp/src/helpers/axios.js
@@ -2,6 +2,7 @@ import axiosLib from "axios";
 import ENV from "@/helpers/env.js";
 import { useUserStore } from "@/stores/user.store.js";
 import logout from "@/utils/logout";
+import dateToString from "@common/utils/dateToString";
 
 const { API_URL } = ENV;
 const ERRORS = {
@@ -9,12 +10,51 @@ const ERRORS = {
     EXPIRED_OR_INVALID_TOKEN: 2,
 };
 
+/**
+ * Transforme récursivement les objets Date en strings YYYY-MM-DD.
+ * Cela évite le bug où axios utilise toISOString() qui décale les dates d'un jour
+ * (ex: minuit en France = 22h UTC la veille).
+ */
+function transformDates(data) {
+    if (data instanceof Date) {
+        return dateToString(data);
+    }
+    if (Array.isArray(data)) {
+        return data.map(transformDates);
+    }
+    if (data !== null && typeof data === "object") {
+        const transformed = {};
+        for (const key in data) {
+            if (Object.prototype.hasOwnProperty.call(data, key)) {
+                transformed[key] = transformDates(data[key]);
+            }
+        }
+        return transformed;
+    }
+    return data;
+}
+
 export const axios = axiosLib.create({
     baseURL: API_URL,
     timeout: 60000,
     headers: {
         "x-app-version": __APP_VERSION__,
     },
+    transformRequest: [
+        (data) => {
+            // Ne transformer que si on a des données et que c'est du JSON
+            if (!data) {
+                return data;
+            }
+            // Ne pas transformer si c'est déjà une string (déjà sérialisé) ou FormData
+            if (typeof data === "string" || data instanceof FormData) {
+                return data;
+            }
+            // Transformer les dates avant la sérialisation JSON par défaut d'axios
+            return transformDates(data);
+        },
+        ...axiosLib.defaults.transformRequest,
+    ],
 });
 
 axios.interceptors.request.use((config) => {
