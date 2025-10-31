ee5caa1af4979f8461af3f1a572b027bc581943c
Christophe BENARD <ch.benard@gmail.com>
2025-10-31 16:29:17 +0100

    Fix: Mutualiser en important _common/formatTimestamp refers to https://trello.com/c/YF7H9V3b/2533

--- packages/api/server/services/shantytown/export/option_section_comments.ts
+++ packages/api/server/services/shantytown/export/option_section_comments.ts
@@ -3,6 +3,7 @@ import {
 } from 'docx';
 
 import moment from 'moment';
+import formatTimestamp from '#server/utils/formatTimestamp';
 import heading from './heading';
 
 export default (shantytown) => {
@@ -23,7 +24,7 @@ export default (shantytown) => {
                     },
                     children: [
                         new TextRun({
-                            text: `${moment(comment.createdAt * 1000).locale('fr').format('DD MMMM YYYY [à] HH:mm')}`,
+                            text: `${formatTimestamp(comment.createdAt, 'd M y à h:i')}`,
                             break: 1,
                             bold: false,
                             size: 22,
