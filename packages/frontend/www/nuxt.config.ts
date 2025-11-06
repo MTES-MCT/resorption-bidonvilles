f3c63778047f8b878162b08f0242f8d4b8a1461b
Gilles COGNIN <gillescognin@gmail.com>
2025-11-06 21:22:45 +0100

    fix: Réintégration du système de focus initial

--- packages/frontend/www/nuxt.config.ts
+++ packages/frontend/www/nuxt.config.ts
@@ -40,7 +40,6 @@ export default defineNuxtConfig({
     alias: {
         "@common": fileURLToPath(new URL("../common/", import.meta.url)),
         "@stores": fileURLToPath(new URL("../webapp/src/stores", import.meta.url)),
-        "@webapp": fileURLToPath(new URL("../webapp/src", import.meta.url)),
     },
     compatibilityDate: "2024-08-30"
 });
