fb41b09105593ec2cd76d96595a14d7de1443ee1
Gilles COGNIN <gillescognin@gmail.com>
2025-11-13 16:59:34 +0100

    fix: Amélioration de la regex de validation de l'email

--- packages/frontend/webapp/src/components/FormUtilisateur/FormUtilisateur.schema.js
+++ packages/frontend/webapp/src/components/FormUtilisateur/FormUtilisateur.schema.js
@@ -78,7 +78,7 @@ export default (
     schema.email = string()
         .required()
         .matches(
-            /^(([^<()>[\]\\.,;:\s@"]+(\.[^<()>[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
             `${labels.email} n'est pas valide`
         )
         .label(labels.email);
