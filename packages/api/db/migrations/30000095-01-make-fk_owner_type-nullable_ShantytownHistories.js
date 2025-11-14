97f12b9b5c9d2d5dde68ff97f3372289fa46e981
Gilles COGNIN <gillescognin@gmail.com>
2025-11-14 14:42:38 +0100

    fix: Modification du champ "owner_type" dans ShantytowHistories pour permettre les MAJ de site

---
+++ packages/api/db/migrations/30000095-01-make-fk_owner_type-nullable_ShantytownHistories.js
@@ -0,0 +1,87 @@
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            // Supprimer les contraintes de clé étrangère sur fk_owner_type
+            await queryInterface.removeConstraint(
+                'ShantytownHistories',
+                'fk_shantytowns_owner_type',
+                { transaction },
+            );
+
+            // Rendre fk_owner_type nullable dans les deux tables
+            await queryInterface.changeColumn(
+                'ShantytownHistories',
+                'fk_owner_type',
+                {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                },
+                { transaction },
+            );
+
+            // Recréer la contrainte de clé étrangère avec set null
+            await queryInterface.addConstraint('ShantytownHistories', {
+                fields: ['fk_owner_type'],
+                type: 'foreign key',
+                name: 'fk_shantytowns_owner_type',
+                references: {
+                    table: 'owner_types',
+                    field: 'owner_type_id',
+                },
+                onUpdate: 'cascade',
+                onDelete: 'set null',
+                transaction,
+            });
+
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+    },
+
+    async down(queryInterface, Sequelize) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            // Supprimer la contrainte de clé étrangère
+            await queryInterface.removeConstraint(
+                'ShantytownHistories',
+                'fk_shantytowns_owner_type',
+                { transaction },
+            );
+
+            // Rendre fk_owner_type non nullable
+            await queryInterface.changeColumn(
+                'ShantytownHistories',
+                'fk_owner_type',
+                {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                { transaction },
+            );
+
+            // Recréer la contrainte de clé étrangère avec restrict
+            await queryInterface.addConstraint('ShantytownHistories', {
+                fields: ['fk_owner_type'],
+                type: 'foreign key',
+                name: 'fk_shantytowns_owner_type',
+                references: {
+                    table: 'owner_types',
+                    field: 'owner_type_id',
+                },
+                onUpdate: 'cascade',
+                onDelete: 'restrict',
+                transaction,
+            });
+
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+    },
+};
