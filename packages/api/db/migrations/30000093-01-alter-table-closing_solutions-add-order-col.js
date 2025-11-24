035021b748d8651a643ebb3fca2b149991f41b79
Gilles COGNIN <gillescognin@gmail.com>
2025-11-24 14:20:39 +0100

    fix: amélioration du score de duplication

--- packages/api/db/migrations/30000093-01-alter-table-closing_solutions-add-order-col.js
+++ packages/api/db/migrations/30000093-01-alter-table-closing_solutions-add-order-col.js
@@ -1,26 +1,39 @@
-async function addColumnsTo(queryInterface, Sequelize, tableName, transaction) {
-    const initialOrder = {
-        7: 3,
-        8: 2,
-        9: 1,
-        10: 4,
-        11: 5,
-    };
-    Promise.all([
-        queryInterface.addColumn(
-            tableName,
-            'order',
-            {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-            },
-            {
-                transaction,
-            },
-        ),
-        Object.entries(initialOrder).map(([id, order]) => queryInterface.sequelize.query(
+const COLUMN_NAME = 'order';
+const TABLE_NAMES = ['closing_solutions'];
+const DEFAULT_ORDER_BY_ID = {
+    7: 3,
+    8: 2,
+    9: 1,
+    10: 4,
+    11: 5,
+};
+
+const runInTransaction = async (queryInterface, callback) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+        await callback(transaction);
+        await transaction.commit();
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+};
+
+const addOrderColumnWithDefaults = async (queryInterface, Sequelize, tableName, transaction) => {
+    await queryInterface.addColumn(
+        tableName,
+        COLUMN_NAME,
+        {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+        },
+        { transaction },
+    );
+
+    await Promise.all(
+        Object.entries(DEFAULT_ORDER_BY_ID).map(([id, order]) => queryInterface.sequelize.query(
             `UPDATE "${tableName}"
-                 SET "order" = :order, updated_at = NOW()
+                 SET "${COLUMN_NAME}" = :order, updated_at = NOW()
                  WHERE closing_solution_id = :id;`,
             {
                 transaction,
@@ -30,42 +43,30 @@ async function addColumnsTo(queryInterface, Sequelize, tableName, transaction) {
                 },
             },
         )),
-    ]);
-}
+    );
+};
 
-async function removeColumnsFrom(queryInterface, tableName, transaction) {
-    return Promise.all([
-        queryInterface.removeColumn(tableName, 'order', { transaction }),
-    ]);
-}
-
-async function addOrDeleteTableColumns(upOrDown, tableNames, queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-        await Promise.all(
-            tableNames.map((tableName) => {
-                if (upOrDown === 'up') {
-                    return addColumnsTo(queryInterface, Sequelize, tableName, transaction);
-                }
-                if (upOrDown === 'down') {
-                    return removeColumnsFrom(queryInterface, tableName, transaction);
-                }
-                return Promise.resolve();
-            }),
-        );
-        await transaction.commit();
-    } catch (error) {
-        await transaction.rollback();
-        throw error;
-    }
-}
+const removeOrderColumn = (queryInterface, tableName, transaction) => queryInterface.removeColumn(
+    tableName,
+    COLUMN_NAME,
+    { transaction },
+);
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await addOrDeleteTableColumns('up', ['closing_solutions'], queryInterface, Sequelize);
+        await runInTransaction(queryInterface, transaction => Promise.all(
+            TABLE_NAMES.map(tableName => addOrderColumnWithDefaults(
+                queryInterface,
+                Sequelize,
+                tableName,
+                transaction,
+            )),
+        ));
     },
 
     async down(queryInterface) {
-        await addOrDeleteTableColumns('down', ['closing_solutions'], queryInterface);
+        await runInTransaction(queryInterface, transaction => Promise.all(
+            TABLE_NAMES.map(tableName => removeOrderColumn(queryInterface, tableName, transaction)),
+        ));
     },
 };
