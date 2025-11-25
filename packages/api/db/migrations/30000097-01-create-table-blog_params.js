58b7dd619af8481592afc0dd23a50c34e7d50972
Gilles COGNIN <gillescognin@gmail.com>
2025-11-25 14:27:03 +0100

    fix: Correction de l'erreur SQ

--- packages/api/db/migrations/30000097-01-create-table-blog_params.js
+++ packages/api/db/migrations/30000097-01-create-table-blog_params.js
@@ -1,31 +1,23 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
-        try {
-            await queryInterface.createTable(
-                'blog_params',
-                {
-                    from_date: {
-                        type: Sequelize.DATE,
-                        allowNull: false,
-                        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                    },
-                    to_date: {
-                        type: Sequelize.DATE,
-                        allowNull: false,
-                        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP + INTERVAL \'10 days\''),
-                    },
+        await queryInterface.createTable(
+            'blog_params',
+            {
+                from_date: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
                 },
-            );
-        } catch (error) {
-            throw error;
-        }
+                to_date: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP + INTERVAL \'10 days\''),
+                },
+            },
+        );
     },
 
     async down(queryInterface) {
-        try {
-            await queryInterface.dropTable('blog_params');
-        } catch (error) {
-            throw error;
-        }
+        await queryInterface.dropTable('blog_params');
     },
 };
