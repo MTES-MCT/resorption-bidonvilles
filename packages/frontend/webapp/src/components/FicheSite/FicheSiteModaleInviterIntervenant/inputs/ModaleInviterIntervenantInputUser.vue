57032952267f3b787c1988517d1d05706aacdcca
Gilles COGNIN <gillescognin@gmail.com>
2025-11-27 14:16:28 +0100

    fix: Correction de l'utilisateur non renvoyé au formulaire

--- packages/frontend/webapp/src/components/FicheSite/FicheSiteModaleInviterIntervenant/inputs/ModaleInviterIntervenantInputUser.vue
+++ packages/frontend/webapp/src/components/FicheSite/FicheSiteModaleInviterIntervenant/inputs/ModaleInviterIntervenantInputUser.vue
@@ -4,11 +4,13 @@
         label="L'intervenant est-il un utilisateur de la plateforme Résorption-bidonvilles ?"
         info="Cherchez le nom de la personne en tapant au minimum les 3 premières lettres de son nom ou de son prénom."
         :fn="autocompleteFn"
+        v-model="modelValue"
     />
 </template>
 
 <script setup>
-import { defineProps, toRefs } from "vue";
+import { defineProps, toRefs, ref, watch } from "vue";
+import { useField } from "vee-validate";
 import { Autocomplete } from "@resorptionbidonvilles/ui";
 import { findRelations } from "@/api/towns.api.js";
 import formatUserName from "@/utils/formatUserName";
@@ -17,6 +19,12 @@ const props = defineProps({
     townId: Number,
 });
 const { townId } = toRefs(props);
+const { setValue } = useField("user");
+const modelValue = ref(null);
+
+watch(modelValue, (option) => {
+    setValue(option ?? null);
+});
 
 async function autocompleteFn(value) {
     const { relations } = await findRelations(townId.value, value);
@@ -26,6 +34,7 @@ async function autocompleteFn(value) {
         label: formatUserName(relation),
         data: {
             id: relation.id,
+            email: relation.email,
         },
     }));
 }
