93a5f2c48ce1e76ade0fc33ef0bd19da68ae72a1
Gilles COGNIN <gillescognin@gmail.com>
2025-11-27 09:18:25 +0100

    feat: Migration pour ajouter la procédure stockée

---
+++ packages/api/db/migrations/30000098-01-create-procedure-deactivate_expirated_users.js.js
@@ -0,0 +1,28 @@
+module.exports = {
+    async up(queryInterface) {
+        await queryInterface.sequelize.query(
+            `CREATE OR REPLACE FUNCTION deactivate_expired_users()
+                RETURNS INTEGER AS $$
+                DECLARE
+                    updated_count INTEGER;
+                BEGIN
+                    UPDATE "users"
+                    SET fk_status = 'inactive',
+                        deactivation_type = 'expired',
+                        deactivated_at = NOW(),
+                        updated_at = NOW()
+                    WHERE fk_status = 'new'
+                    AND created_at < NOW() - INTERVAL '3 months';
+
+                    GET DIAGNOSTICS updated_count = ROW_COUNT;
+
+                    RETURN updated_count;
+                END;
+                $$ LANGUAGE plpgsql;`,
+        );
+    },
+
+    async down(queryInterface) {
+        await queryInterface.sequelize.query('DROP FUNCTION deactivate_expired_users()');
+    },
+};
