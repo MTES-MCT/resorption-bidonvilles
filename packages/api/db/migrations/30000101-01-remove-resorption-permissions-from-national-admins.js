113957e27a815d091c622422f9e8ad95f292f605
Christophe BENARD <ch.benard@gmail.com>
2025-12-01 15:30:38 +0100

    Fix: Supprimer les permissions relatives aux phases de résorption aux admin nationaux refers to https://trello.com/c/mUycPmLj/2553

---
+++ packages/api/db/migrations/30000101-01-remove-resorption-permissions-from-national-admins.js
@@ -0,0 +1,89 @@
+const { QueryTypes } = require('sequelize');
+
+module.exports = {
+    async up(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            // Supprimer les permissions 'create' et 'update' pour shantytown_resorption
+            // pour tous les utilisateurs ayant le rôle 'national_admin'
+            // car ils ont maintenant un bypass automatique via "is_superuser"
+            await queryInterface.sequelize.query(
+                `DELETE FROM permissions
+                WHERE fk_entity = 'shantytown_resorption'
+                AND fk_feature IN ('create', 'update')
+                AND fk_user IN (
+                    SELECT user_id
+                    FROM users
+                    WHERE fk_role = 'national_admin'
+                )`,
+                { transaction },
+            );
+
+            await transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+
+    async down(queryInterface) {
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            // Récupérer tous les utilisateurs avec le rôle national_admin
+            const nationalAdmins = await queryInterface.sequelize.query(
+                `SELECT DISTINCT
+                    ur.user_id,
+                    u.fk_organization
+                FROM users ur
+                WHERE u.fk_role = 'national_admin'`,
+                {
+                    transaction,
+                    type: QueryTypes.SELECT,
+                },
+            );
+
+            if (nationalAdmins.length > 0) {
+                // Recréer les permissions 'create' pour les admins nationaux
+                const createPermissions = nationalAdmins.map(admin => ({
+                    fk_user: admin.fk_user,
+                    fk_organization: admin.fk_organization,
+                    fk_feature: 'create',
+                    fk_entity: 'shantytown_resorption',
+                    allowed: true,
+                    type: 'nation',
+                    fk_region: null,
+                    fk_departement: null,
+                    fk_epci: null,
+                    fk_city: null,
+                }));
+
+                // Recréer les permissions 'update' pour les admins nationaux
+                const updatePermissions = nationalAdmins.map(admin => ({
+                    fk_user: admin.fk_user,
+                    fk_organization: admin.fk_organization,
+                    fk_feature: 'update',
+                    fk_entity: 'shantytown_resorption',
+                    allowed: true,
+                    type: 'nation',
+                    fk_region: null,
+                    fk_departement: null,
+                    fk_epci: null,
+                    fk_city: null,
+                }));
+
+                await queryInterface.bulkInsert(
+                    'permissions',
+                    [...createPermissions, ...updatePermissions],
+                    { transaction },
+                );
+            }
+
+            await transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
+    },
+};
