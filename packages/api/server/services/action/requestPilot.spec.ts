7dce58c51102bd3c4abb2555217709bb0e7160b0
Gilles COGNIN <gillescognin@gmail.com>
2025-12-05 09:30:30 +0100

    fix: Correction du test unitaire en concéquence

--- packages/api/server/services/action/requestPilot.spec.ts
+++ packages/api/server/services/action/requestPilot.spec.ts
@@ -15,12 +15,14 @@ chai.use(chaiSubset);
 
 const sandbox = sinon.createSandbox();
 const stubs = {
+    fetch: sandbox.stub(),
     mattermost: {
         triggerRequestActionPilot: sandbox.stub(),
     },
 };
 
 rewiremock('#server/utils/mattermost').with(stubs.mattermost);
+rewiremock('./fetch').with(stubs.fetch);
 
 rewiremock.enable();
 // eslint-disable-next-line import/newline-after-import, import/first
@@ -34,6 +36,7 @@ describe('services/action.requestPilot()', () => {
     beforeEach(() => {
         user = fakeUser();
         user.permissions.action = { read: { allowed: true, allowed_on: null, allowed_on_national: null } };
+        stubs.fetch.resolves([action]);
     });
 
     afterEach(() => {
@@ -45,10 +48,29 @@ describe('services/action.requestPilot()', () => {
     });
 
     it('déclenche une notification Mattermost de demande de pilote', async () => {
-        await requestPilot(action, user);
+        await requestPilot(action.id, user);
 
         expect(stubs.mattermost.triggerRequestActionPilot).to.have.been.calledOnce;
         expect(stubs.mattermost.triggerRequestActionPilot).to.have.been.calledOnceWithExactly(action, user);
+    });
+
+    it('renvoi \'true\' si la demande est bien transmise par message Mattermost', async () => {
+        stubs.mattermost.triggerRequestActionPilot.returns(true);
+        const result = await requestPilot(action.id, user);
+
+        expect(stubs.mattermost.triggerRequestActionPilot).to.have.been.calledOnce;
+        expect(stubs.mattermost.triggerRequestActionPilot).to.have.been.calledOnceWithExactly(action, user);
+        expect(result).to.be.true;
+    });
+
+    it('renvoi \'false\' si la notification Mattermost échoue', async () => {
+        stubs.mattermost.triggerRequestActionPilot.returns(false);
+
+        const result = await requestPilot(action.id, user);
+
+        expect(stubs.mattermost.triggerRequestActionPilot).to.have.been.calledOnce;
+        expect(stubs.mattermost.triggerRequestActionPilot).to.have.been.calledOnceWithExactly(action, user);
+        expect(result).to.be.false;
     });
 
     it('renvoi une ServiceError si la requête ne contient pas d\'action', async () => {
@@ -67,7 +89,7 @@ describe('services/action.requestPilot()', () => {
     it('renvoi une ServiceError si la requête ne contient pas d\'utilisateur', async () => {
         let caughtError: ServiceError | null = null;
         try {
-            await requestPilot(action, null as any);
+            await requestPilot(action.id, null as any);
         } catch (err) {
             caughtError = err as ServiceError;
         }
@@ -75,20 +97,5 @@ describe('services/action.requestPilot()', () => {
         expect(caughtError).to.be.instanceOf(ServiceError);
         expect(caughtError?.code).to.equal('action_pilot_request_failed');
         expect(caughtError?.nativeError.message).to.equal('Aucun utilisateur spécifié');
-    });
-
-    it('renvoi une ServiceError si la notification Mattermost échoue', async () => {
-        stubs.mattermost.triggerRequestActionPilot.rejects(new Error('Mattermost error'));
-
-        let caughtError: ServiceError | null = null;
-        try {
-            await requestPilot(action, user);
-        } catch (err) {
-            caughtError = err as ServiceError;
-        }
-
-        expect(caughtError).to.be.instanceOf(ServiceError);
-        expect(caughtError?.code).to.equal('action_pilot_request_failed');
-        expect(caughtError?.nativeError.message).to.equal('Mattermost error');
     });
 });
