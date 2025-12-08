953cadb4ed89b41e85351cd66a76c56ea583c2a0
Christophe BENARD <ch.benard@gmail.com>
2025-12-08 13:47:01 +0100

    Fix: Utiliser le helper pour limiter la duplication de code refers to https://trello.com/c/fvbTRXQz/2560

--- packages/api/db/migrations/30000090-01-create-table-shantytown_parcel_owners.js
+++ packages/api/db/migrations/30000090-01-create-table-shantytown_parcel_owners.js
@@ -1,50 +1,44 @@
-const { addForeignKey, removeForeignKey } = require('./common/helpers/manageForeignKeys');
+const { createTableWithForeignKeys, dropTableWithForeignKeys } = require('./common/helpers/migrationHelper');
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        const transaction = await queryInterface.sequelize.transaction();
-
-        try {
-            await queryInterface.createTable(
-                'shantytown_parcel_owners',
-                {
-                    shantytown_parcel_owner_id: {
-                        type: Sequelize.INTEGER,
-                        allowNull: false,
-                        autoIncrement: true,
-                        primaryKey: true,
-                    },
-                    fk_shantytown: {
-                        type: Sequelize.INTEGER,
-                        allowNull: false,
-                    },
-                    fk_user: {
-                        type: Sequelize.INTEGER,
-                        allowNull: false,
-                    },
-                    owner_name: {
-                        type: Sequelize.STRING,
-                        allowNull: true,
-                    },
-                    fk_owner_type: {
-                        type: Sequelize.INTEGER,
-                        allowNull: false,
-                    },
-                    active: {
-                        type: Sequelize.BOOLEAN,
-                        allowNull: false,
-                        defaultValue: true,
-                    },
-                    created_at: {
-                        type: Sequelize.DATE,
-                        allowNull: false,
-                        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-                    },
+        await createTableWithForeignKeys(queryInterface, {
+            tableName: 'shantytown_parcel_owners',
+            tableDefinition: {
+                shantytown_parcel_owner_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
                 },
-                { transaction },
-            );
-
-            const foreignKeys = [
+                fk_shantytown: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                fk_user: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                owner_name: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                fk_owner_type: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                active: {
+                    type: Sequelize.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: true,
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+                },
+            },
+            foreignKeys: [
                 {
                     fields: ['fk_user'],
                     refTable: 'users',
@@ -60,51 +54,20 @@ module.exports = {
                     refTable: 'owner_types',
                     refField: 'owner_type_id',
                 },
-            ];
-
-            await Promise.all(
-                foreignKeys.map(fk => addForeignKey(queryInterface, {
-                    table: 'shantytown_parcel_owners',
-                    onUpdate: 'cascade',
-                    onDelete: 'cascade',
-                    transaction,
-                    ...fk,
-                })),
-            );
-
-            await queryInterface.addIndex(
-                'shantytown_parcel_owners',
-                ['fk_shantytown'],
+            ],
+            indexes: [
                 {
+                    fields: ['fk_shantytown'],
                     name: 'id__shantytown_parcel_owners__fk_shantytown',
-                    using: 'BTREE',
-                    schema: 'public',
-                    transaction,
                 },
-            );
-            await transaction.commit();
-        } catch (err) {
-            await transaction.rollback();
-            throw err;
-        }
+            ],
+        });
     },
 
     async down(queryInterface) {
-        const transaction = await queryInterface.sequelize.transaction();
-
-        try {
-            await Promise.all(
-                ['users', 'shantytowns', 'owner_types'].map(refTable => removeForeignKey(queryInterface, {
-                    table: 'shantytown_parcel_owners',
-                    refTable,
-                    transaction,
-                })),
-            );
-            await queryInterface.dropTable('shantytown_parcel_owners', { transaction });
-            await transaction.commit();
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        }
+        await dropTableWithForeignKeys(queryInterface, {
+            tableName: 'shantytown_parcel_owners',
+            refTables: ['users', 'shantytowns', 'owner_types'],
+        });
     },
 };
