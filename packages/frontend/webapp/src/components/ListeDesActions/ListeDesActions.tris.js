775ede2fa183835434b051b369b51f9cf7e39895
Christophe BENARD <ch.benard@gmail.com>
2025-12-31 17:01:49 +0100

    Feat: Ajouter une liste de critères de tri refers to https://trello.com/c/lF5kSF8o/2579

---
+++ packages/frontend/webapp/src/components/ListeDesActions/ListeDesActions.tris.js
@@ -0,0 +1,14 @@
+export default {
+    startedAt: {
+        value: "started_at",
+        label: "Date de démarrage",
+    },
+    updatedAt: {
+        value: "updated_at",
+        label: "Date de mise à jour",
+    },
+    lastMetricUpdate: {
+        value: "lastMetricUpdate",
+        label: "Date de mise à jour des indicateurs",
+    },
+};
