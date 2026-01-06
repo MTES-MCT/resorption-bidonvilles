28081403860e3b0ccf797bdd83ec6fac79f62128
Christophe BENARD <ch.benard@gmail.com>
2026-01-06 19:04:38 +0100

    Feat: Afficher la date de dernière mise à jour des indicateurs sous les informations de statut de l'action refers to https://trello.com/c/RcMhVjVG/2582

--- packages/frontend/webapp/src/components/FicheAction/FicheActionHeader/FicheActionHeaderStatus.vue
+++ packages/frontend/webapp/src/components/FicheAction/FicheActionHeader/FicheActionHeaderStatus.vue
@@ -1,25 +1,34 @@
 <template>
-    <p class="flex items-center uppercase text-sm space-x-2">
-        <span
-            class="inline-block rounded-full h-3 w-3"
-            :class="isClosed ? 'bg-corail' : 'bg-green'"
-        />
+    <div class="flex flex-col">
+        <p class="flex items-center uppercase text-sm space-x-2">
+            <span
+                class="inline-block rounded-full h-3 w-3"
+                :class="isClosed ? 'bg-corail' : 'bg-green'"
+            />
 
-        <span>
-            <template v-if="isClosed">
-                Terminée le {{ formatDate(action.ended_at / 1000, "d/m/y") }}
-            </template>
+            <span>
+                <template v-if="isClosed">
+                    Terminée le
+                    {{ formatDate(action.ended_at / 1000, "d/m/y") }}
+                </template>
 
-            <template v-else-if="action.updated_at">
-                Mise à jour le
-                {{ formatDate(action.updated_at / 1000, "d/m/y") }}
-            </template>
+                <template v-else-if="action.updated_at">
+                    Mise à jour le
+                    {{ formatDate(action.updated_at / 1000, "d/m/y") }}
+                </template>
 
-            <template v-else>
-                Déclarée le {{ formatDate(action.created_at / 1000, "d/m/y") }}
-            </template>
-        </span>
-    </p>
+                <template v-else>
+                    Déclarée le
+                    {{ formatDate(action.created_at / 1000, "d/m/y") }}
+                </template>
+            </span>
+        </p>
+        <p class="text-black text-sm" v-if="action.metricsUpdatedAt">
+            Indicateurs mis à jour le
+            {{ formatDate(action.metricsUpdatedAt / 1000, "d/m/y") }}
+        </p>
+        <p class="text-black text-sm" v-else>Aucun indicateur renseigné</p>
+    </div>
 </template>
 
 <script setup>
