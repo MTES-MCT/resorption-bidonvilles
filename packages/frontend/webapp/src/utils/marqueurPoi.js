d22d4317c13e57482d9bb286181a37b495586b82
Christophe BENARD <ch.benard@gmail.com>
2026-01-23 12:01:41 +0100

    Fix: Optimiser le chargement de l'icône du marqueur refers to https://trello.com/c/maUChAYe/2598

--- packages/frontend/webapp/src/utils/marqueurPoi.js
+++ packages/frontend/webapp/src/utils/marqueurPoi.js
@@ -1,17 +1,20 @@
 import L from "leaflet";
 import cutlery from "@/assets/img/map/cutlery.png";
 
+// Créer une icône unique réutilisée pour tous les POI
+const poiIcon = L.divIcon({
+    className: "my-marker",
+    html: `<img src="${cutlery}" width="12" height="12" />`,
+    iconAnchor: [13, 28],
+});
+
 const createMarqueurPoi = (poi) => {
     // Longitude/latitudes returned by soliguide are in the wrong order
     const coordinates = [...poi.position.location.coordinates].reverse();
 
     return L.marker(coordinates, {
         title: poi.address,
-        icon: L.divIcon({
-            className: "my-marker",
-            html: `<img src="${cutlery}" width="12" height="12" />`,
-            iconAnchor: [13, 28],
-        }),
+        icon: poiIcon, // Réutiliser la même icône
     });
 };
 
