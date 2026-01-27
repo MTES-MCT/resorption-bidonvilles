8277a8358f2f1729dd9d0ab52662d5c69bd70f93
Christophe BENARD <ch.benard@gmail.com>
2026-01-27 16:45:15 +0100

    Feat: Ajouter un service pour mettre à jour le champ "resorption_target" refers to https://trello.com/c/fKhlqWQ8/2603

---
+++ packages/api/server/services/shantytown/setResorptionTarget.ts
@@ -0,0 +1,22 @@
+import shantytownModel from '#server/models/shantytownModel';
+import ServiceError from '#server/errors/ServiceError';
+import permissionUtils from '#server/utils/permission';
+
+export default async (user, data) => {
+    if (!permissionUtils.can(user).do('update', 'shantytown').on(data.shantytown)) {
+        throw new ServiceError('permission_denied', new Error('Vous n\'avez pas la permission de marquer ce site comme "Objectif résorption"'));
+    }
+
+    const currentYear = new Date().getFullYear();
+
+    try {
+        await shantytownModel.setResorptionTarget(
+            data.shantytown.id,
+            currentYear,
+        );
+    } catch (error) {
+        throw new ServiceError('write_failed', new Error('Une erreur est survenue pendant l\'écriture en base de données'));
+    }
+
+    return shantytownModel.findOne(user, data.shantytown.id);
+};
