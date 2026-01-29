8fa680488f8f150038be25b4c6795a6f0b0d11d3
Gilles COGNIN <gillescognin@gmail.com>
2026-01-29 13:51:28 +0100

    design: Réorganisation des éléments

--- packages/frontend/webapp/src/utils/access_request_types.js
+++ packages/frontend/webapp/src/utils/access_request_types.js
@@ -5,10 +5,6 @@ export default {
         { value: "help-request", label: "Demander de l'aide" },
         { value: "info-request", label: "Demander des infos" },
         {
-            value: "register-newsletter",
-            label: "Vous abonner à la lettre d'info",
-        },
-        {
             value: "submit-blog-post",
             label: "Proposer un article pour le blog",
         },
@@ -18,10 +14,6 @@ export default {
         { value: "report", label: "Report" },
         { value: "help-request", label: "Ask for help" },
         { value: "info-request", label: "Ask for information" },
-        {
-            value: "register-newsletter",
-            label: "Subscribe to the newsletter",
-        },
         {
             value: "submit-blog-post",
             label: "Submit a blog post",
@@ -33,10 +25,6 @@ export default {
         { value: "help-request", label: "Să cereţi ajutor" },
         { value: "info-request", label: "Să cereţi informații" },
         {
-            value: "register-newsletter",
-            label: "Abonați-vă la newsletter",
-        },
-        {
             value: "submit-blog-post",
             label: "Trimiteți o postare pe blog",
         },
@@ -46,10 +34,6 @@ export default {
         { value: "report", label: "Подадете сигнал" },
         { value: "help-request", label: "Поискате помощ" },
         { value: "info-request", label: "Поискате информация" },
-        {
-            value: "register-newsletter",
-            label: "Абонирайте се за бюлетина",
-        },
         {
             value: "submit-blog-post",
             label: "Изпратете публикация в блог",
