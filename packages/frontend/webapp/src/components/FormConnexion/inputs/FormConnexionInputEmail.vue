13f4a7dd30801c3ea559d2b3fc4bd86b571eb046
Christophe BENARD <ch.benard@gmail.com>
2026-02-12 16:07:32 +0100

    Fix: remplacer le TextInput parun DsfrInput refers to: https://trello.com/c/nOjdJMku/2611

--- packages/frontend/webapp/src/components/FormConnexion/inputs/FormConnexionInputEmail.vue
+++ packages/frontend/webapp/src/components/FormConnexion/inputs/FormConnexionInputEmail.vue
@@ -1,15 +1,34 @@
 <template>
-    <TextInput
-        id="email"
-        :label="label"
-        placeholder="marcel.dupont@exemple.fr"
-        autocomplete="username"
-    />
+    <Field name="email" v-slot="{ field, errors }">
+        <DsfrInputGroup :error-message="errors[0]">
+            <DsfrInput
+                v-bind="field"
+                id="email"
+                :label="label"
+                label-visible
+                placeholder="marcel.dupont@exemple.fr"
+                autocomplete="username"
+                type="email"
+                required
+                :is-invalid="!!errors.length"
+                :disabled="disabled"
+                :model-value="field.value"
+            />
+        </DsfrInputGroup>
+    </Field>
 </template>
 
 <script setup>
-import { TextInput } from "@resorptionbidonvilles/ui";
+import { Field } from "vee-validate";
 import labels from "../FormConnexion.labels.js";
+
+defineProps({
+    disabled: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+});
 
 const { email: label } = labels;
 </script>
