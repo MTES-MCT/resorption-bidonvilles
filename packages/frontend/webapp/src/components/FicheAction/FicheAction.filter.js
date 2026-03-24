2e2a67cf00787129f7f43806f1f2ae77ee037502
Christophe BENARD <ch.benard@gmail.com>
2026-03-24 21:19:21 +0100

    Feat: créer l'interface d'affichage de l'historique refers to https://trello.com/c/otR94k9k/2653
    - Création du composant FicheActionHistorique avec panneau latéral
    - Création du composant FicheActionHistoriqueItem pour afficher chaque modification
    - Implémentation du filtrage par catégorie (Financements, Thématiques, Contacts, Sites)
    - Support du filtrage par préfixes de champs (finances.2024.state matche finances)
    - Formatage des noms d'auteur et des dates
    - Rechargement forcé de l'historique à chaque ouverture du panneau
    - Création du fichier de configuration des filtres

---
+++ packages/frontend/webapp/src/components/FicheAction/FicheAction.filter.js
@@ -0,0 +1,27 @@
+export default {
+    categories: [
+        { value: "caracteristiques", label: "Intervention" },
+        { value: "localisation", label: "Lieu" },
+        { value: "contacts", label: "Contacts" },
+        { value: "financements", label: "Financements" },
+        { value: "indicateurs", label: "Indicateurs" },
+        { value: "sites", label: "Sites concernés" },
+        { value: "thematiques", label: "Thématiques" },
+    ],
+    fields: {
+        caracteristiques: ["name", "started_at", "ended_at", "goals"],
+        localisation: [
+            "location.departement.name",
+            "location_type",
+            "eti.address",
+            "eti.latitude",
+            "eti.longitude",
+            "location_other",
+        ],
+        contacts: ["managers", "operators"],
+        financements: ["finances"],
+        indicateurs: ["metrics"],
+        sites: ["location_shantytowns"],
+        thematiques: ["topics"],
+    },
+};
