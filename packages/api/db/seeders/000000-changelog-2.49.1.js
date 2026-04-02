ad575d3d2edf68d68210da46d6967e312a9e6b08
Gilles COGNIN <gillescognin@gmail.com>
2026-04-02 11:29:18 +0200

    fix: Utilisation de la version mutualisée utilisant le JSON

--- packages/api/db/seeders/000000-changelog-2.49.1.js
+++ packages/api/db/seeders/000000-changelog-2.49.1.js
@@ -1,16 +1,3 @@
-const { backUrl } = require('./config/config');
-const seeder = require('./helpers/seederBuild');
+const createChangelogSeederFromJson = require('./helpers/createChangelogSeederFromJson');
 
-const changelog = {
-    app_version: '2.49.1',
-    date: new Date('2026-04-02T02:01:00'),
-    items: [
-        {
-            title: 'Une nouvelle fonctionnalité est disponible !',
-            description: '<p>Vous pouvez désormais télécharger les données du volet "actions" dans la liste des actions.</p>',
-            image: `${backUrl}/assets/changelog/2.49.1/item_1.jpg`,
-        },
-    ],
-};
-
-module.exports = seeder(changelog);
+module.exports = createChangelogSeederFromJson('2.49.1.json');
