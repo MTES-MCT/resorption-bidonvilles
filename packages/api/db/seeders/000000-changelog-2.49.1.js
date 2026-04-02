e277873a23d948ebf109ff11de70feecec729d17
Gilles COGNIN <gillescognin@gmail.com>
2026-04-02 11:10:02 +0200

    fix: Meilleure image et modification de la date

--- packages/api/db/seeders/000000-changelog-2.49.1.js
+++ packages/api/db/seeders/000000-changelog-2.49.1.js
@@ -2,7 +2,7 @@ const { backUrl } = require('./config/config');
 
 const changelog = {
     app_version: '2.49.1',
-    date: new Date('2026-04-02T00:01:00'),
+    date: new Date('2026-04-02T02:01:00'),
     items: [
         {
             title: 'Une nouvelle fonctionnalité est disponible !',
