327e77e741404240eb28192d8eedb3871b554a55
Gilles COGNIN <gillescognin@gmail.com>
2026-04-10 11:35:52 +0200

    design: Update de l'apparence du footer

--- packages/frontend/www/components/LandingPage/LandingPage.vue
+++ packages/frontend/www/components/LandingPage/LandingPage.vue
@@ -1,22 +1,33 @@
 <template>
     <section id="main-content">
         <div class="flex flex-row">
-            <ContentWrapper class="flex-1">
-                <div class="flex flex-col items-center justify-center h-full px-6 py-8">
-                    <img src="assets/img/LandingPage/carte.png" alt="Illustration de la page d'accueil" class="mb-4 w-96 h-auto" />
+            <ContentWrapper class="flex-1 bg-[#f5f5fe]">
+                <div class="flex flex-col items-center justify-center h-full px-6">
+                     <DsfrPicture
+                        src="_nuxt/assets/img/LandingPage/carte.png"
+                        alt="Impact des actions de résorption en France"
+                        size="small"
+                        
+                     />
                 </div>
             </ContentWrapper>
-            <ContentWrapper class="flex flex-1 flex-col text-justify px-6 py-8 bg-[#f6f6f6]">
+            <ContentWrapper class="flex flex-1 flex-col text-justify p-8">
                 <h4>Quelle est la politique de résorption des bidonvilles ?</h4>
                 <p>
                     Dans l'instruction du 25 janvier 2018, le gouvernement fixe un objectif de résorption des bidonvilles en dépassant une approche centrée sur les évacuations, au profit d'une approche globale alliant insertion sociale, respect de l'ordre public et prévention des réinstallations. La mission Résorption des bidonvilles accompagne et appuie les territoires concernés dans la mise en oeuvre de stratégies locales.
                 </p>
+                <a href="https://www.gouvernement.fr/action/resorption-des-bidonvilles" target="_blank" class="text-blue-600 mt-4 mb-14 inline-block place-self-center">
+                    En savoir plus sur la politique de résorption des bidonvilles
+                </a>
+                <div class="flex flex-row gap-1 justify-evenly">
+                <DsfrButton
+                    label="S'inscrire à une session de prise en main"
+                />
                 <DsfrButton
                     secondary
-                    size="small"
-                    label="En savoir plus sur la politique de résorption des bidonvilles"
-                    class="place-self-center"
+                    label="Consulter le blog de la résorption"
                 />
+                </div>
             </ContentWrapper>
         </div>
     </section>
--- packages/frontend/www/components/Layout/Layout.vue
+++ packages/frontend/www/components/Layout/Layout.vue
@@ -30,7 +30,68 @@
       <slot />
     </main>
 
-    <DsfrFooter descText="Un problème, une question ?"/>
+    <DsfrFollow
+      :newsletterData="{
+        title: 'Un problème, une question ?',
+        onlyCallout: true,
+        description: 'Appelez nous au +33 1 40 81 98 80',
+        buttonText: 'Nous contacter par courriel',
+        buttonTitle: 'Contactez-nous par courriel',
+        buttonAction: () => 'mailto:contact-resorption-bidonvilles@dihal.gouv.fr'
+      }"
+      :networks="[
+        {
+          type: 'twitter-x',
+          name: 'Compte Twitter de la DIHAL',
+          href: 'https://twitter.com/dihal_gouv'
+        },
+        {
+          type: 'facebook',
+          name: 'Page Facebook de la DIHAL',
+          href: 'https://www.facebook.com/DIHAL.delegation.interministerielle/'
+        },
+        {
+          type: 'linkedin',
+          name: 'Page LinkedIn de la DIHAL',
+          href: 'https://fr.linkedin.com/company/dihal'
+        }
+      ]"
+    />
+
+    <DsfrFooter
+      descText="Un produit de la Délégation Interministérielle à l'Hébergement et à l'Accès au Logement."
+      :ecosystemLinks="[
+        {
+          label: 'contact-resorption-bidonvilles@dihal.gouv.fr',
+          href: 'mailto:contact-resorption-bidonvilles@dihal.gouv.fr'
+        },
+        {
+          label: 'DIHAL',
+          href: 'https://www.dihal.gouv.fr/'
+        }
+      ]"
+      operatorImgSrc="_nuxt/assets/img/LandingPage/logo.png"
+      operatorImgAlt="Logo Résorption-bidonvilles"
+      :beforeMandatoryLinks="[
+        {
+          label: 'Code source',
+          button: true,
+          class: 'fr-icon-github-fill fr-link--icon-left mr-0',
+          href: 'https://github.com/MTES-MCT/resorption-bidonvilles'
+        },
+        {
+          label: 'Mesures d\'impact',
+          href: 'https://app.resorption-bidonvilles.dihal.gouv.fr/stats'
+        },
+        {
+          label: 'Conditions générales d\'utilisation',
+          href: 'https://app.resorption-bidonvilles.dihal.gouv.fr/doc/conditions-d-utilisation.pdf'
+        }
+      ]"
+      a11yCompliance="non conforme"
+      a11yComplianceLink="https://app.resorption-bidonvilles.dihal.gouv.fr/accessibilite"
+      legalLink="https://app.resorption-bidonvilles.dihal.gouv.fr/mentions-legales"
+    />
   </div>
 </template>
 
@@ -56,7 +117,7 @@ const serviceDescription = "L'accès à la plateforme est restreint aux acteurs 
 // Menu de navigation
 const navItems = [
   {
-    to: '/login',
+    to: 'app.resorption-bidonvilles.localhost/login',
     text: 'Se connecter',
   },
   {
@@ -66,3 +127,8 @@ const navItems = [
 ]
 
 </script>
+<style scoped>
+:deep(.fr-link--icon-left[class*=" fr-icon-"]::before) {
+  margin-right: 0 !important;
+}
+</style>
