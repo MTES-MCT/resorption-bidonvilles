ad1e9073945deaa8566c0a7adaed5e196a86f885
Gilles COGNIN <gillescognin@gmail.com>
2026-04-16 14:53:36 +0200

    design: Refonte du layout global

--- packages/frontend/www/components/Layout/Layout.vue
+++ packages/frontend/www/components/Layout/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div lang="fr">
     <Html lang="fr">
 
     <Head>
@@ -11,125 +11,233 @@
     </Html>
 
     <DsfrHeader
-      :logoText="logoText"
-      :homeTo="homeTo"
-      :homeLabel="homeLabel"
-      :serviceTitle="serviceTitle"
-      :serviceDescription="serviceDescription"
-      :quickLinks="quickLinks"
+      :logoText="headerDatas.logoText"
+      :homeTo="headerDatas.homeTo"
+      :homeLabel="headerDatas.homeLabel"
+      :serviceTitle="headerDatas.serviceTitle"
+      :serviceDescription="headerDatas.serviceDescription"
+      :quickLinks="headerDatas.quickLinks"
+    />
+
+    <DsfrModal
+      :opened="openedModal"
+      :title="modalTitle"
+      size="sm"
+      lang="fr"
+      @close="openedModal = false"
     >
-      <template v-slot:mainnav>
-        <DsfrNavigation 
-          :nav-items="navItems"
-          label="Menu principal"
+      <template #default>
+        <p lang="fr">Choisissez un thème pour personnaliser l’apparence du site.</p>
+        <DsfrRadioButtonSet
+          v-model="preferences.scheme"
+          :options="modalParams"
+          name="theme"
         />
       </template>
-    </DsfrHeader>
+    </DsfrModal>
 
+    <!-- Début du contenu dynamique de la page -->
     <main id="contenu" role="main">
       <slot />
     </main>
+    <!-- Fin du contenu dynamique de la page -->
 
-    <DsfrFollow
-      :newsletterData="{
-        title: 'Un problème, une question ?',
-        onlyCallout: true,
-        description: 'Appelez nous au +33 1 40 81 98 80',
-        buttonText: 'Nous contacter par courriel',
-        buttonTitle: 'Contactez-nous par courriel',
-        buttonAction: () => 'mailto:contact-resorption-bidonvilles@dihal.gouv.fr'
-      }"
-      :networks="[
-        {
-          type: 'twitter-x',
-          name: 'Compte Twitter de la DIHAL',
-          href: 'https://twitter.com/dihal_gouv'
-        },
-        {
-          type: 'facebook',
-          name: 'Page Facebook de la DIHAL',
-          href: 'https://www.facebook.com/DIHAL.delegation.interministerielle/'
-        },
-        {
-          type: 'linkedin',
-          name: 'Page LinkedIn de la DIHAL',
-          href: 'https://fr.linkedin.com/company/dihal'
-        }
-      ]"
-    />
+    <DsfrFollow>
+      <template #default lang="fr" class="fr-container">
+          <div class="fr-col-12 fr-col-md-8">
+            <div class="fr-follow__newsletter">
+              <div>
+                <h2 class="fr-h5">{{ followDatas.contact.title }}</h2>
+                <p class="fr-text--sm">{{ followDatas.contact.description }}</p>
+              </div>
+              <div class="w-full">
+                <DsfrButtonGroup
+                  inline-layout-when="md"
+                  :iconRight="false"
+                  :buttons="followDatas.contact.button"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="fr-col-12 fr-col-md-4">
+            <div class="fr-follow__social">
+              <h2 class="fr-h5">{{ followDatas.networks.title }}</h2>
+              <DsfrButtonGroup
+                equisized
+                :buttons="followDatas.networks.buttons"
+              />
+            </div>
+          </div>
+      </template>
+    </DsfrFollow>
 
     <DsfrFooter
-      descText="Un produit de la Délégation Interministérielle à l'Hébergement et à l'Accès au Logement."
-      :ecosystemLinks="[
-        {
-          label: 'contact-resorption-bidonvilles@dihal.gouv.fr',
-          href: 'mailto:contact-resorption-bidonvilles@dihal.gouv.fr'
-        },
-        {
-          label: 'DIHAL',
-          href: 'https://www.dihal.gouv.fr/'
-        }
-      ]"
-      :operatorImgSrc="operatorLogo"
-      operatorImgAlt="Logo Résorption-bidonvilles"
-      :beforeMandatoryLinks="[
-        {
-          label: 'Code source',
-          button: true,
-          class: 'fr-icon-github-fill fr-link--icon-left mr-0',
-          href: 'https://github.com/MTES-MCT/resorption-bidonvilles'
-        },
-        {
-          label: 'Mesures d\'impact',
-          href: 'https://app.resorption-bidonvilles.dihal.gouv.fr/stats'
-        },
-        {
-          label: 'Conditions générales d\'utilisation',
-          href: 'https://app.resorption-bidonvilles.dihal.gouv.fr/doc/conditions-d-utilisation.pdf'
-        }
-      ]"
-      a11yCompliance="non conforme"
-      a11yComplianceLink="https://app.resorption-bidonvilles.dihal.gouv.fr/accessibilite"
-      legalLink="https://app.resorption-bidonvilles.dihal.gouv.fr/mentions-legales"
+      :descText="footerDatas.descText"
+      :ecosystemLinks="footerDatas.ecosystemLinks"
+      :beforeMandatoryLinks="footerDatas.beforeMandatoryLinks"
+      :mandatoryLinks="footerDatas.mandatoryLinks"
+      :afterMandatoryLinks="footerDatas.afterMandatoryLinks"
+      :licenceName="footerDatas.licenceName"
+      :licenceTo="footerDatas.licenceTo""
     />
   </div>
 </template>
 
 <script setup>
-import operatorLogo from "~/assets/img/LandingPage/logo.png";
+import { ref, reactive, onMounted, watchEffect } from 'vue';
+import { useScheme } from '@gouvminint/vue-dsfr'
+import openMail from '~~/utils/openMail';
+const { WEBAPP_URL, CONTACT_EMAIL, BLOG_URL } = useRuntimeConfig().public;
 
-defineProps({
-  stickyHeader: {
-    type: Boolean,
-    default: true
+// Icones SVG pour les thèmes clair, sombre et système
+const sunSvgPath = '/artwork/pictograms/environment/sun.svg';
+const moonSvgPath = '/artwork/pictograms/environment/moon.svg';
+const systemSvgPath = '/artwork/pictograms/system/system.svg';
+
+// Gestion de la modale et de l'affichage (thème)
+const openedModal = ref(false);
+const modalTitle = "Paramètres d'affichage";
+const preferences = reactive({
+  theme: undefined,
+  scheme: 'system',
+})
+const modalParams = [
+  {
+    label: 'Thème clair',
+    value: 'light',
+    svgPath: sunSvgPath
   },
-  displayLanguagePicker: {
-    type: Boolean,
-    default: true
+  {
+    label: 'Thème sombre',
+    value: 'dark',
+    svgPath: moonSvgPath
+  },
+  {
+    label: 'Thème système',
+    value: 'system',
+    svgPath: systemSvgPath
   }
+];
+onMounted(() => {
+  const { theme, scheme, setScheme } = useScheme()
+  preferences.scheme = scheme.value
+  watchEffect(() => { preferences.theme = theme.value })
+  watchEffect(() => setScheme(preferences.scheme))
 });
 
 // Paramétrage du header DSFR
-const logoText = ["Délégation interministérielle", "à l'hébergement et à l'accès", "au logement"];
-const homeTo = "/";
-const homeLabel = "Accueil Résorption des bidonvilles";
-const serviceTitle = 'Plateforme Résorption-Bidonvilles';
-const serviceDescription = "L'accès à la plateforme est restreint aux acteurs de la résorption des bidonvilles"
+const headerDatas = {
+    homeTo: "/",
+    homeLabel: "Accueil Résorption des bidonvilles",
+    logoText: ["Gouvernement"],
+    serviceTitle: "Plateforme Résorption-bidonvilles",
+    serviceDescription: 'Délégation interministérielle à l\'hébergement et à l\'accès au logement',
+    quickLinks: [
+        {
+            label: 'Connexion',
+            href: `${WEBAPP_URL}/connexion`,
+            icon: 'fr-icon-lock-line',
+        }, {
+            label: 'Paramètres d\'affichage',
+            button: true,
+            onClick: () => openedModal.value = true,
+            icon: 'fr-icon-theme-fill',
+        }, {
+            label: 'Accès au blog',
+            href: `${BLOG_URL}`,
+            icon: 'fr-icon-article-line',
+            target: '_blank'
+        }
+    ]
+}
 
-// Menu de navigation
-const navItems = [
-  {
-    to: 'app.resorption-bidonvilles.localhost/login',
-    text: 'Se connecter',
+// Paramétrage du follow DSFR
+const followDatas = {
+  contact: {
+    title: 'Une question sur la plateforme\u00A0?',
+    description: 'Appelez nous au +33\u00A01\u00A040\u00A081\u00A098\u00A080',
+    button: [
+      {
+        label: 'Nous contacter par courriel',
+        title: 'Contactez-nous par courriel  - Ouverture de votre outils de courrier électronique',
+        primary: true,
+        onClick: () => openMail()
+      }
+    ],
   },
-  {
-    to: '/access',
-    text: 'Demander un accès',
-  },
-]
+  networks: {
+    title: 'Suivez nous sur LinkedIn',
+    buttons: [
+      {
+        label: 'LinkedIn',
+        title: 'Page LinkedIn de la DIHAL - Ouverture dans une nouvelle fenêtre',
+        icon: 'fr-icon-linkedin-box-fill',
+        href: 'https://fr.linkedin.com/company/dihal',
+        target: '_blank'
+      }
+    ]
+  }
+}
 
+// Paramétrage du footer DSFR
+const footerDatas = {
+    descText: "Un produit de la Délégation Interministérielle à l'Hébergement et à l'Accès au Logement (DIHAL).",
+    ecosystemLinks: [
+      {
+        label: CONTACT_EMAIL,
+        href: `mailto:${CONTACT_EMAIL}`
+      },
+      {
+        label: 'DIHAL',
+        href: 'https://www.dihal.gouv.fr/'
+      }
+    ],
+    beforeMandatoryLinks: [
+      {
+        label: 'Code source',
+        target: '_blank',
+        class: 'fr-icon-github-fill fr-link--icon-left mr-0',
+        href: 'https://github.com/MTES-MCT/resorption-bidonvilles'
+      },
+      {
+        label: 'Mesures d\'impact',
+        href: `${WEBAPP_URL}/stats`
+      },
+      {
+        label: 'Conditions générales d\'utilisation',
+        href: `${WEBAPP_URL}/doc/conditions-d-utilisation.pdf`
+      },
+    ],
+    mandatoryLinks: [
+      {
+        label: 'Accessibilité: non conforme',
+        href: `${WEBAPP_URL}/accessibilite`
+      }, {
+        label: 'Mentions légales',
+        href: `${WEBAPP_URL}/mentions-legales`
+      }, {
+        label: 'Données personnelles',
+        href: `${WEBAPP_URL}/doc/conditions-d-utilisation.pdf`
+      }, {
+        label: 'Gestion des cookies',
+        href: `${WEBAPP_URL}/doc/conditions-d-utilisation.pdf`
+      }
+    ],
+    afterMandatoryLinks: [
+      {
+        label: 'Paramètres d\'affichage',
+        button: true,
+        onClick: () => openedModal.value = true,
+        icon: 'fr-icon-theme-fill',
+        class: 'fr-link--icon-left pl-1.5 md:pl-0'
+      }
+    ],
+    licenceName: "licence AGPL-3.0",
+    licenceTo: "https://github.com/MTES-MCT/resorption-bidonvilles/blob/develop/LICENSE"
+}
 </script>
 <style scoped>
+/* Amélioration de l'affichage des icônes du footer */
 :deep(.fr-link--icon-left[class*=" fr-icon-"]::before) {
   margin-right: 0 !important;
 }
