10a0ddf23818a791b33645733d88a5ea3efe80fc
Gilles COGNIN <gillescognin@gmail.com>
2026-04-16 14:54:45 +0200

    tech: Intégration des composants DSFR utilisés

--- packages/frontend/www/utils/dsfr.js
+++ packages/frontend/www/utils/dsfr.js
@@ -1,11 +1,14 @@
 import {
     DsfrButton,
+    DsfrButtonGroup,
     DsfrCard,
     DsfrFollow,
     DsfrFooter,
     DsfrHeader,
+    DsfrModal,
     DsfrNavigation,
     DsfrPicture,
+    DsfrRadioButtonSet,
 } from "@gouvminint/vue-dsfr";
 
 import "@gouvfr/dsfr/dist/dsfr.min.css";
@@ -16,10 +19,13 @@ import "@gouvfr/dsfr/dist/component/component.main.min.css";
 
 export function useDsfr(app) {
     app.component("DsfrButton", DsfrButton);
+    app.component("DsfrButtonGroup", DsfrButtonGroup);
     app.component("DsfrCard", DsfrCard);
     app.component("DsfrFollow", DsfrFollow);
     app.component("DsfrFooter", DsfrFooter);
     app.component("DsfrHeader", DsfrHeader);
+    app.component("DsfrModal", DsfrModal);
     app.component("DsfrNavigation", DsfrNavigation);
     app.component("DsfrPicture", DsfrPicture);
+    app.component("DsfrRadioButtonSet", DsfrRadioButtonSet);
 }
