19e70080b403c3f6ba5fff77c4de98065a6e1104
Christophe BENARD <ch.benard@gmail.com>
2026-04-24 18:03:47 +0200

    fix: corriger le test refers to https://trello.com/c/isf50e6H/2673
    - Ajout d'un stub pour console.error
    - Configuration par défaut des stubs
    - Restauration du stub
    - Nettoyage du console.error() inutile

--- packages/api/server/services/organization/create.spec.ts
+++ packages/api/server/services/organization/create.spec.ts
@@ -32,10 +32,16 @@ import create from './create';
 rewiremock.disable();
 
 describe('services/organization/create', () => {
+    let consoleErrorStub;
+
     beforeEach(() => {
+        consoleErrorStub = sandbox.stub(console, 'error');
         sequelize.transaction.resolves(transaction);
+        createOrganization.resolves(1);
+        findOrganizationById.resolves(fakeOrganization());
     });
     afterEach(() => {
+        consoleErrorStub.restore();
         sandbox.reset();
     });
 
@@ -146,8 +152,7 @@ describe('services/organization/create', () => {
                 new_type_default_role: null,
             });
         } catch (error) {
-            // eslint-disable-next-line no-console
-            console.error(error);
+            // expected error
         }
 
         expect(transaction.rollback).to.have.been.calledOnce;
