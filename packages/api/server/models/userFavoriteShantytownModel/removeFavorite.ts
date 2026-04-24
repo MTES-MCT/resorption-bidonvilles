9b7e885b01887ff470a1e5721a8f6ac20b426613
Christophe BENARD <ch.benard@gmail.com>
2026-04-24 12:24:03 +0200

    fix(sonarqube): nommer la fonction fléchée refers to https://trello.com/c/isf50e6H/2673

--- packages/api/server/models/userFavoriteShantytownModel/removeFavorite.ts
+++ packages/api/server/models/userFavoriteShantytownModel/removeFavorite.ts
@@ -1,12 +1,14 @@
 import { sequelize } from '#db/sequelize';
 
-export default (userId: number, shantytownId: number) => sequelize.query(
-    `DELETE FROM user_favorite_shantytowns
-     WHERE fk_user = :userId AND fk_shantytown = :shantytownId`,
-    {
-        replacements: {
-            userId,
-            shantytownId,
+export default function removeFavorite(userId: number, shantytownId: number) {
+    return sequelize.query(
+        `DELETE FROM user_favorite_shantytowns
+        WHERE fk_user = :userId AND fk_shantytown = :shantytownId`,
+        {
+            replacements: {
+                userId,
+                shantytownId,
+            },
         },
-    },
-);
+    );
+}
