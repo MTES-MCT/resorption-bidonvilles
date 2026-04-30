ab7e17385bf58c188404f63ea66a2e3d718e8b6c
Christophe BENARD <ch.benard@gmail.com>
2026-04-30 16:40:54 +0200

    refactor(api): extraire les solutions de fermeture dans closingSolutionModel
    refers to https://trello.com/c/ZPU4RYve/2668

--- packages/api/server/models/closingSolutionModel/findByShantytown.ts
+++ packages/api/server/models/closingSolutionModel/findByShantytown.ts
@@ -9,18 +9,20 @@ export type ShantytownClosingSolution = {
     message: string,
 };
 
-export default (shantytownIds: string[]): Promise<ShantytownClosingSolution[]> => sequelize.query(
-    `SELECT
-            shantytown_closing_solutions.fk_shantytown AS "shantytownId",
-            closing_solutions.closing_solution_id AS "id",
-            shantytown_closing_solutions.number_of_people_affected AS "peopleAffected",
-            shantytown_closing_solutions.number_of_households_affected AS "householdsAffected",
-            shantytown_closing_solutions.message AS "message"
-        FROM shantytown_closing_solutions
-        LEFT JOIN closing_solutions ON shantytown_closing_solutions.fk_closing_solution = closing_solutions.closing_solution_id
-        WHERE shantytown_closing_solutions.fk_shantytown IN (:ids)`,
-    {
-        type: QueryTypes.SELECT,
-        replacements: { ids: shantytownIds },
-    },
-);
+export default function findByShantytown(shantytownIds: string[]): Promise<ShantytownClosingSolution[]> {
+    return sequelize.query(
+        `SELECT
+                shantytown_closing_solutions.fk_shantytown AS "shantytownId",
+                closing_solutions.closing_solution_id AS "id",
+                shantytown_closing_solutions.number_of_people_affected AS "peopleAffected",
+                shantytown_closing_solutions.number_of_households_affected AS "householdsAffected",
+                shantytown_closing_solutions.message AS "message"
+            FROM shantytown_closing_solutions
+            LEFT JOIN closing_solutions ON shantytown_closing_solutions.fk_closing_solution = closing_solutions.closing_solution_id
+            WHERE shantytown_closing_solutions.fk_shantytown IN (:ids)`,
+        {
+            type: QueryTypes.SELECT,
+            replacements: { ids: shantytownIds },
+        },
+    );
+}
