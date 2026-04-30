3babde14cb5d31b51ee23f3e5dd2c6742f69c94e
Christophe BENARD <ch.benard@gmail.com>
2026-04-30 13:01:16 +0200

    refactor: corrigzer les alertes SonarQube refers to https://trello.com/c/ZPU4RYve/2668
        - Nommer la fonctuion fléchée
        - Inverser les tests avec des conditions négatives

--- packages/api/server/models/shantytownModel/_common/serializeComment.ts
+++ packages/api/server/models/shantytownModel/_common/serializeComment.ts
@@ -6,21 +6,23 @@ type shantytownCommentRowWithTags = ShantytownCommentRow & {
     tags: ShantytownCommentTag[]
 };
 
-export default (comment: shantytownCommentRowWithTags): ShantytownRawComment => ({
-    id: comment.commentId,
-    description: comment.commentDescription,
-    createdAt: comment.commentCreatedAt !== null ? (comment.commentCreatedAt.getTime() / 1000) : null,
-    organization_target_name: comment.organization_target_name || [],
-    user_target_name: comment.user_target_name || [],
-    createdBy: {
-        id: comment.commentCreatedBy,
-        first_name: comment.userFirstName,
-        last_name: comment.userLastName,
-        position: comment.userPosition,
-        organization: comment.organizationAbbreviation || comment.organizationName,
-        organization_id: comment.organizationId,
-    },
-    shantytown: comment.shantytownId,
-    tags: comment.tags,
-    attachments: comment.attachments?.length ? comment.attachments : [],
-});
+export default function serializeComment(comment: shantytownCommentRowWithTags): ShantytownRawComment {
+    return ({
+        id: comment.commentId,
+        description: comment.commentDescription,
+        createdAt: comment.commentCreatedAt === null ? null : (comment.commentCreatedAt.getTime() / 1000),
+        organization_target_name: comment.organization_target_name || [],
+        user_target_name: comment.user_target_name || [],
+        createdBy: {
+            id: comment.commentCreatedBy,
+            first_name: comment.userFirstName,
+            last_name: comment.userLastName,
+            position: comment.userPosition,
+            organization: comment.organizationAbbreviation || comment.organizationName,
+            organization_id: comment.organizationId,
+        },
+        shantytown: comment.shantytownId,
+        tags: comment.tags,
+        attachments: comment.attachments?.length ? comment.attachments : [],
+    });
+}
