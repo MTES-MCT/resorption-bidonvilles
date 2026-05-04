e1213338ff807f967cdae3245e56548b712c5391
Christophe BENARD <ch.benard@gmail.com>
2026-05-04 13:04:33 +0200

    refactor(api): ajouter le type de retour explicite à resetAddresses
    - Ajout de async/await pour cohérence avec les autres fonctions
    - Ajout du type de retour explicite : Promise<void>
    - Cohérence avec resetAsideData et historizeAddresses
    - Amélioration de la documentation de la signature de fonction

--- packages/api/server/models/actionModel/update/resetAddresses.ts
+++ packages/api/server/models/actionModel/update/resetAddresses.ts
@@ -1,8 +1,8 @@
 import { QueryTypes, Transaction } from 'sequelize';
 import { sequelize } from '#db/sequelize';
 
-export default function resetAddresses(actionId: number, transaction: Transaction) {
-    return sequelize.query(
+export default async function resetAddresses(actionId: number, transaction: Transaction): Promise<void> {
+    await sequelize.query(
         'DELETE FROM action_addresses WHERE fk_action = :actionId',
         {
             type: QueryTypes.DELETE,
