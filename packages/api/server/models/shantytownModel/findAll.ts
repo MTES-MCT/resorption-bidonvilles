84d1e31b6c4a266f3f5cf1d69a40e3f8bd8e06db
Christophe BENARD <ch.benard@gmail.com>
2026-05-11 16:00:11 +0200

    refactor(api): nommer explicitement la fonction findAll refers to https://trello.com/c/cCbgradg/2675
    Remplacer l'export par défaut anonyme par une fonction nommée

--- packages/api/server/models/shantytownModel/findAll.ts
+++ packages/api/server/models/shantytownModel/findAll.ts
@@ -2,6 +2,6 @@ import { Where } from '#server/models/_common/types/Where.d';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 import query from './_common/query';
 
-export default function findAll(user: AuthUser, filters: Where = [], feature: string = 'list', order = ['shantytowns.updated_at DESC']) {
+export default async function findAll(user: AuthUser, filters: Where = [], feature: string = 'list', order = ['shantytowns.updated_at DESC']) {
     return query(user, feature, filters, order);
 }
