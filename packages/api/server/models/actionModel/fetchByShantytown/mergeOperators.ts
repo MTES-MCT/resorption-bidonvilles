69f2e44e4a80de002ab1b659d29d48643e277964
Christophe BENARD <ch.benard@gmail.com>
2026-05-11 15:57:54 +0200

    feat(api): propager is_principal lors du merge des opérateurs refers to https://trello.com/c/cCbgradg/2675
    Si un utilisateur d'une organisation est principal, toute l'organisation
    est marquée comme principale

--- packages/api/server/models/actionModel/fetchByShantytown/mergeOperators.ts
+++ packages/api/server/models/actionModel/fetchByShantytown/mergeOperators.ts
@@ -14,6 +14,7 @@ export default (hash: ActionHash, operators: ActionUserRow[]): void => {
             phone: row.phone,
             role: row.admin_role_name || row.regular_role_name,
             is_admin: row.admin_role_name !== null,
+            is_principal: row.is_principal,
             organization: {
                 id: row.organization_id,
                 name: row.organization_name,
@@ -26,9 +27,15 @@ export default (hash: ActionHash, operators: ActionUserRow[]): void => {
                 id: row.organization_id,
                 name: row.organization_name,
                 abbreviation: row.organization_abbreviation,
+                is_principal: row.is_principal,
                 users: [user],
             });
         } else {
+            // Si cet utilisateur est principal, marquer toute l'organisation comme principale
+            if (row.is_principal) {
+                /* eslint no-param-reassign: "error" */
+                hash[row.action_id].operators[index].is_principal = true;
+            }
             hash[row.action_id].operators[index].users.push(user);
         }
     });
