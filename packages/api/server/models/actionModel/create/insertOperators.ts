c380c0431fc315859431da761a0086ac8ba263fa
Christophe BENARD <ch.benard@gmail.com>
2026-05-26 12:23:15 +0200

    refactor(api) Corriger les alertes SonarQube refers to https://trello.com/c/ZPU4RYve/2668
    - Nommer la fonction fléchée
    - Préférer `.flatMap` à `.map`

--- packages/api/server/models/actionModel/create/insertOperators.ts
+++ packages/api/server/models/actionModel/create/insertOperators.ts
@@ -1,14 +1,16 @@
 import { QueryTypes, Transaction } from 'sequelize';
 import { sequelize } from '#db/sequelize';
 
-export default (actionId: number, operators: number[], transaction: Transaction) => sequelize.query(
-    `INSERT INTO action_operators(
-        fk_action,
-        fk_user
-    ) VALUES ${operators.map(() => '(?, ?)').join(',')}`,
-    {
-        type: QueryTypes.INSERT,
-        transaction,
-        replacements: operators.map(userId => [actionId, userId]).flat(),
-    },
-);
+export default function insertOperators(actionId: number, operators: number[], transaction: Transaction) {
+    return sequelize.query(
+        `INSERT INTO action_operators(
+            fk_action,
+            fk_user
+        ) VALUES ${operators.map(() => '(?, ?)').join(',')}`,
+        {
+            type: QueryTypes.INSERT,
+            transaction,
+            replacements: operators.flatMap(userId => [actionId, userId]),
+        },
+    );
+}
