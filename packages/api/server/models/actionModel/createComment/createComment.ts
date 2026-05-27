0ae8d7ccf18c057a929d3be237fccf8c188fa678
Christophe BENARD <ch.benard@gmail.com>
2026-05-27 11:42:10 +0200

    refactor(api): simplifier la fonction `createComment` du modèle refers to https://trello.com/c/ZPU4RYve/2668
    - Suppression de l'import inutilisé `QueryTypes`
    - Utilisation de la destructuration directe du résultat de `sequelize.query`
    - Simplification du typage avec assertion de type inline
    - Suppression de la variable intermédiaire `ReturnValue`

--- packages/api/server/models/actionModel/createComment/createComment.ts
+++ packages/api/server/models/actionModel/createComment/createComment.ts
@@ -1,4 +1,4 @@
-import { QueryTypes, type Transaction } from 'sequelize';
+import { type Transaction } from 'sequelize';
 import { sequelize } from '#db/sequelize';
 
 export type ActionCommentInsertionRow = {
@@ -7,12 +7,11 @@ export type ActionCommentInsertionRow = {
 };
 
 export default async function createComment(actionId: number, comment: ActionCommentInsertionRow, transaction?: Transaction): Promise<number> {
-    const data = await sequelize.query(
+    const [rows] = await sequelize.query(
         `INSERT INTO action_comments(fk_action, description, created_by)
         VALUES (:actionId, :description, :created_by)
         RETURNING action_comment_id`,
         {
-            type: QueryTypes.INSERT,
             replacements: {
                 actionId,
                 description: comment.description,
@@ -20,9 +19,9 @@ export default async function createComment(actionId: number, comment: ActionCom
             },
             transaction,
         },
-    );
-    type ReturnValue = { action_comment_id: number };
-    const rows: ReturnValue[] = (data[0] as unknown) as ReturnValue[];
+    ) as [[{ action_comment_id: number }], unknown];
 
-    return rows[0].action_comment_id;
+    const { action_comment_id } = rows[0];
+
+    return action_comment_id;
 }
