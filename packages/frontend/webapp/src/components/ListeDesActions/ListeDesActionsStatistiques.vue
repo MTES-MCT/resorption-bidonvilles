1a1a0e297b6802cc38a731f4356d169e01becd1a
Christophe BENARD <ch.benard@gmail.com>
2026-06-05 14:15:52 +0200

    feat(action): statistiques séparées pour actions en cours et terminées refers to https://trello.com/c/wvr2hd4B/2692
    - Affichage séparé des actions en cours et terminées pour "Ma structure"
    - Statistiques DIHAL et mise à jour uniquement pour actions en cours
    - Gestion conditionnelle de l'affichage selon le nombre d'actions

--- packages/frontend/webapp/src/components/ListeDesActions/ListeDesActionsStatistiques.vue
+++ packages/frontend/webapp/src/components/ListeDesActions/ListeDesActionsStatistiques.vue
@@ -7,40 +7,91 @@
             <div>
                 <h1 class="text-3xl text-info font-bold">{{ title }}</h1>
                 <div>
-                    {{ currentActionsCount }} action{{
-                        isPlural(currentActionsCount) ? "s" : ""
-                    }}
-                    <template v-if="actionsStore.filters.status === 'open'"
-                        >en cours
-                        <div>
-                            <DsfrBadge
-                                :type="badgeDIHALVariant"
-                                :label="badgeDIHALLabel"
-                                noIcon
-                                small
-                            />
-                            des actions financées par la DIHAL ont des
-                            indicateurs mis à jour depuis moins de 3 mois ({{
-                                updatedActionsFinancedByDIHAL
+                    <template
+                        v-if="actionsStore.filters.status === 'myOrganization'"
+                    >
+                        <div v-if="myOrgOpenActionsCount > 0">
+                            {{ myOrgOpenActionsCount }} action{{
+                                isPlural(myOrgOpenActionsCount) ? "s" : ""
                             }}
-                            actions sur {{ actionsFinancedByDIHAL }})
+                            en cours
+                            <div>
+                                <DsfrBadge
+                                    :type="badgeDIHALVariantMyOrg"
+                                    :label="badgeDIHALLabelMyOrg"
+                                    noIcon
+                                    small
+                                />
+                                des actions financées par la DIHAL ont des
+                                indicateurs mis à jour depuis moins de 3 mois
+                                ({{
+                                    updatedActionsFinancedByDIHALMyOrg
+                                }}
+                                actions sur {{ actionsFinancedByDIHALMyOrg }})
+                            </div>
+                            <div>
+                                <DsfrBadge
+                                    :type="badgeVariantMyOrg"
+                                    :label="badgeLabelMyOrg"
+                                    noIcon
+                                    small
+                                />
+                                des actions ont des indicateurs mis à jour dans
+                                les 6 derniers mois ({{
+                                    updatedActionsInTheLastSixMonthsMyOrg
+                                }}
+                                actions sur {{ myOrgOpenActionsCount }} )
+                            </div>
                         </div>
-                        <div>
-                            <DsfrBadge
-                                :type="badgeVariant"
-                                :label="badgeLabel"
-                                noIcon
-                                small
-                            />
-                            des actions ont des indicateurs mis à jour dans les
-                            6 derniers mois ({{
-                                updatedActionsInTheLastSixMonths
+                        <div
+                            v-if="myOrgClosedActionsCount > 0"
+                            :class="{ 'mt-4': myOrgOpenActionsCount > 0 }"
+                        >
+                            {{ myOrgClosedActionsCount }} action{{
+                                isPlural(myOrgClosedActionsCount) ? "s" : ""
                             }}
-                            actions sur {{ currentActionsCount }} )
+                            terminée{{
+                                isPlural(myOrgClosedActionsCount) ? "s" : ""
+                            }}
                         </div>
                     </template>
-                    <template v-else
-                        >terminée{{ isPlural(currentActionsCount) ? "s" : "" }}
+                    <template v-else>
+                        {{ currentActionsCount }} action{{
+                            isPlural(currentActionsCount) ? "s" : ""
+                        }}
+                        <template v-if="actionsStore.filters.status === 'open'"
+                            >en cours
+                            <div>
+                                <DsfrBadge
+                                    :type="badgeDIHALVariant"
+                                    :label="badgeDIHALLabel"
+                                    noIcon
+                                    small
+                                />
+                                des actions financées par la DIHAL ont des
+                                indicateurs mis à jour depuis moins de 3 mois
+                                ({{ updatedActionsFinancedByDIHAL }} actions sur
+                                {{ actionsFinancedByDIHAL }})
+                            </div>
+                            <div>
+                                <DsfrBadge
+                                    :type="badgeVariant"
+                                    :label="badgeLabel"
+                                    noIcon
+                                    small
+                                />
+                                des actions ont des indicateurs mis à jour dans
+                                6 derniers mois ({{
+                                    updatedActionsInTheLastSixMonths
+                                }}
+                                actions sur {{ currentActionsCount }} )
+                            </div>
+                        </template>
+                        <template v-else
+                            >terminée{{
+                                isPlural(currentActionsCount) ? "s" : ""
+                            }}
+                        </template>
                     </template>
                 </div>
             </div>
@@ -153,5 +204,106 @@ const badgeDIHALLabel = computed(() => {
 
 const badgeDIHALVariant = computed(() => {
     return getBadgeVariant(actionFinancedByDIHALPercentage.value, 95, 80);
+});
+
+const myOrgOpenActions = computed(() => {
+    if (actionsStore.filters.status !== "myOrganization") {
+        return [];
+    }
+    const now = new Date();
+    return currentActions.value.filter(
+        (action) => action.ended_at === null || now < action.ended_at
+    );
+});
+
+const myOrgClosedActions = computed(() => {
+    if (actionsStore.filters.status !== "myOrganization") {
+        return [];
+    }
+    const now = new Date();
+    return currentActions.value.filter(
+        (action) => action.ended_at !== null && now > action.ended_at
+    );
+});
+
+const myOrgOpenActionsCount = computed(() => {
+    return formatStat(myOrgOpenActions.value.length);
+});
+
+const myOrgClosedActionsCount = computed(() => {
+    return formatStat(myOrgClosedActions.value.length);
+});
+
+const updatedActionsInTheLastSixMonthsMyOrg = computed(() => {
+    return myOrgOpenActions.value.filter((action) => {
+        const lastUpdate = {
+            metricUpdatedAt:
+                getSince(action.metrics_updated_at / 1000).months < 6,
+        };
+        return lastUpdate.metricUpdatedAt;
+    }).length;
+});
+
+const updatedActionsPercentageMyOrg = computed(() => {
+    if (myOrgOpenActionsCount.value === 0) {
+        return 0;
+    }
+    const percentage =
+        (updatedActionsInTheLastSixMonthsMyOrg.value /
+            myOrgOpenActions.value.length) *
+        100;
+    return Math.round(percentage * 10) / 10;
+});
+
+const badgeLabelMyOrg = computed(() => {
+    return getBadgeLabel(
+        updatedActionsInTheLastSixMonthsMyOrg.value,
+        updatedActionsPercentageMyOrg.value,
+        false,
+        false,
+        true
+    );
+});
+
+const badgeVariantMyOrg = computed(() => {
+    return getBadgeVariant(updatedActionsPercentageMyOrg.value, 80, 60);
+});
+
+const actionsFinancedByDIHALMyOrg = computed(() => {
+    return myOrgOpenActions.value.filter((action) => action.hasDihalFinancing)
+        .length;
+});
+
+const updatedActionsFinancedByDIHALMyOrg = computed(() => {
+    return myOrgOpenActions.value.filter(
+        (action) =>
+            action.hasDihalFinancing &&
+            getSince(action.metrics_updated_at / 1000).months < 3
+    ).length;
+});
+
+const actionFinancedByDIHALPercentageMyOrg = computed(() => {
+    if (actionsFinancedByDIHALMyOrg.value === 0) {
+        return 0;
+    }
+    const percentage =
+        (updatedActionsFinancedByDIHALMyOrg.value /
+            actionsFinancedByDIHALMyOrg.value) *
+        100;
+    return Math.round(percentage * 10) / 10;
+});
+
+const badgeDIHALLabelMyOrg = computed(() => {
+    return getBadgeLabel(
+        actionsFinancedByDIHALMyOrg.value,
+        actionFinancedByDIHALPercentageMyOrg.value,
+        false,
+        false,
+        true
+    );
+});
+
+const badgeDIHALVariantMyOrg = computed(() => {
+    return getBadgeVariant(actionFinancedByDIHALPercentageMyOrg.value, 95, 80);
 });
 </script>
