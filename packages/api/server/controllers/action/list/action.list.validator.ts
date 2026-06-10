d10b541d8012fcfae8e4d010f4c40d7d561ad3ca
Christophe BENARD <ch.benard@gmail.com>
2026-06-10 15:13:00 +0200

    refactor(api): simplifier le validateur refers to https://trello.com/c/fvbTRXQz/2560

--- packages/api/server/controllers/action/list/action.list.validator.ts
+++ packages/api/server/controllers/action/list/action.list.validator.ts
@@ -2,12 +2,10 @@ import { query } from 'express-validator';
 
 export default [
     query('organizationId')
-        .optional()
+        .customSanitizer(value => value ?? null)
+        .if(value => value !== null)
         .toInt()
         .isInt()
         .bail()
         .withMessage('L\'identifiant de structure doit être un nombre entier'),
-
-    query('organizationId')
-        .customSanitizer(value => value ?? null),
 ];
