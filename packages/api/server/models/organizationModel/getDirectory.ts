578eeccf061b48fa530afd40a3c30216b4b27226
Christophe BENARD <ch.benard@gmail.com>
2026-07-06 15:44:41 +0200

    refactor(api): utiliser un Set pour tester le type de localisation dans getAdminsFor et nommer des fonctions fléchées refers to https://trello.com/c/GRDthuOV/2724
    1. Remplace les appels répétés à Array.includes() par Set.has() (lint SonarQube S7776).
    2. Nomme les fonctions fléchées dans:
    
        - packages/api/server/models/organizationModel/findOneById.ts
        - packages/api/server/models/organizationModel/getDirectory.ts
    
    refactor(api): nommer les fonctions fléchées exportées par défaut
    refers to https://trello.com/c/GRDthuOV/2724

--- packages/api/server/models/organizationModel/getDirectory.ts
+++ packages/api/server/models/organizationModel/getDirectory.ts
@@ -3,4 +3,6 @@ import find from './_common/find';
 import { Organization } from '#root/types/resources/Organization.d';
 import { User } from '#root/types/resources/User.d';
 
-export default (requestingUser?: User, transaction?: Transaction): Promise<Organization[]> => find({ activeOnly: true, nonEmpty: true }, requestingUser, transaction);
+const getDirectory = (requestingUser?: User, transaction?: Transaction): Promise<Organization[]> => find({ activeOnly: true, nonEmpty: true }, requestingUser, transaction);
+
+export default getDirectory;
--- packages/api/server/models/userModel/getAdminsFor.ts
+++ packages/api/server/models/userModel/getAdminsFor.ts
@@ -6,10 +6,10 @@ import getNationalAdmins from './_common/getNationalAdmins';
 const getAdminsFor = async (user: User): Promise<User[]> => {
     const admins: User[] = [];
     const mainAreas = user.intervention_areas.areas.filter(area => area.is_main_area);
-    const types: LocationType[] = mainAreas.map(area => area.type);
+    const types: Set<LocationType> = new Set(mainAreas.map(area => area.type));
 
     // si l'utilisateur est lié à au moins un département, on prévient les administrateurs de ce/ces départements
-    if (types.includes('city') || types.includes('departement') || types.includes('epci')) {
+    if (types.has('city') || types.has('departement') || types.has('epci')) {
         admins.push(
             ...await getLocalAdminsForDepartement(
                 mainAreas.filter(area => area.departement !== null).map(area => area.departement.code),
@@ -18,7 +18,7 @@ const getAdminsFor = async (user: User): Promise<User[]> => {
     }
 
     // si l'utilisateur est lié à une région, au national, ou qu'aucun admin local n'a été trouvé, on prévient les admins nationaux
-    if (types.includes('nation') || types.includes('region') || admins.length === 0) {
+    if (types.has('nation') || types.has('region') || admins.length === 0) {
         admins.push(
             ...await getNationalAdmins(),
         );
