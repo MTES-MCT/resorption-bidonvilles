9358e045a2ddba4469a839a2a4529aee5b886446
Christophe BENARD <ch.benard@gmail.com>
2026-07-08 00:35:32 +0200

    feat(frontend): ajouter les actions updateComment dans les stores refers to https://trello.com/c/evsOelCU/2717

--- packages/frontend/webapp/src/stores/towns.store.js
+++ packages/frontend/webapp/src/stores/towns.store.js
@@ -25,6 +25,7 @@ import {
     setHeatwaveStatus,
     setResorptionTarget,
     updateActorThemes,
+    updateComment,
     startResorption,
 } from "@/api/towns.api";
 import enrichShantytown from "@/utils/enrichShantytown";
@@ -374,6 +375,20 @@ export const useTownsStore = defineStore("towns", () => {
             );
             activitiesStore.removeComment(commentId);
             dashboardActivitiesStore.removeComment(commentId);
+        },
+        async updateComment(shantytownId, commentId, description) {
+            const notificationStore = useNotificationStore();
+            const { comments } = await updateComment(
+                shantytownId,
+                commentId,
+                description
+            );
+            updateShantytownComments(shantytownId, comments.comments);
+            setTowns(shantytownId);
+            notificationStore.success(
+                "Modification du message",
+                "Votre message a bien été modifié"
+            );
         },
         async deleteDecree(shantytownId, commentId, reason = "") {
             const activitiesStore = useActivitiesStore();
