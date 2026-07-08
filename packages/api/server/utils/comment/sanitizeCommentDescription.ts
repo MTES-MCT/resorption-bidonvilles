9631b4df6de389f799b0300c396e0ecaac1a1e5d
Christophe BENARD <ch.benard@gmail.com>
2026-07-08 16:39:33 +0200

    refactor(api): factoriser le check d'auteur et la validation de description
    assertIsCommentOwner et sanitizeCommentDescription étaient dupliqués à
    l'identique entre services/action/updateComment.ts et
    services/shantytownComment/updateComment.ts. Extraction dans
    server/utils/comment/, réutilisé par les deux services.
    
    refers to https://trello.com/c/evsOelCU/2717

---
+++ packages/api/server/utils/comment/sanitizeCommentDescription.ts
@@ -0,0 +1,11 @@
+import validator from 'validator';
+import ServiceError from '#server/errors/ServiceError';
+
+export default function sanitizeCommentDescription(description: string): string {
+    const trimmedDescription = validator.trim(description ?? '');
+    if (trimmedDescription === '') {
+        throw new ServiceError('data_incomplete', new Error('La description du commentaire ne peut pas être vide'));
+    }
+
+    return trimmedDescription;
+}
