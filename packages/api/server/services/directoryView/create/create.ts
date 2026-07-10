f86eaf55b7b9e793f8939d741cdd90d01f33845e
Christophe BENARD <ch.benard@gmail.com>
2026-07-10 18:01:13 +0200

    refactor(api): typer organization refers to https://trello.com/c/GRDthuOV/2724

--- packages/api/server/services/directoryView/create/create.ts
+++ packages/api/server/services/directoryView/create/create.ts
@@ -2,9 +2,10 @@ import ServiceError from '#server/errors/ServiceError';
 import organizationModel from '#server/models/organizationModel/index';
 import statsDirectoryViewsModel from '#server/models/statsDirectoryViewsModel';
 import { User } from '#root/types/resources/User.d';
+import { Organization } from '#root/types/resources/Organization.d';
 
 const create = async (organizationId: number, requestingUser: User): Promise<void> => {
-    let organization;
+    let organization: Organization | null;
     try {
         organization = await organizationModel.findOneById(organizationId, false, requestingUser);
     } catch (error) {
