098fcde9658efd0bf09b1fb1452981b04d034265
Christophe BENARD <ch.benard@gmail.com>
2026-07-13 16:37:37 +0200

    refactor(api): limiter l'export à la seule méthode utilisée refers to https://trello.com/c/evsOelCU/2717

--- packages/api/server/utils/comment/sanitizeCommentDescription.ts
+++ packages/api/server/utils/comment/sanitizeCommentDescription.ts
@@ -1,8 +1,8 @@
-import validator from 'validator';
+import { trim } from 'validator';
 import ServiceError from '#server/errors/ServiceError';
 
 export default function sanitizeCommentDescription(description: string): string {
-    const trimmedDescription = validator.trim(description ?? '');
+    const trimmedDescription = trim(description ?? '');
     if (trimmedDescription === '') {
         throw new ServiceError('data_incomplete', new Error('La description du commentaire ne peut pas être vide'));
     }
