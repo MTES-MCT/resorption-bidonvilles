2eac40f27c081cbb566dd73c72b70116063f6ce6
Christophe BENARD <ch.benard@gmail.com>
2026-07-13 19:42:37 +0200

    refactor(api): mutualiser deleteComment via runDeleteComment refers to https://trello.com/c/evsOelCU/2717
    actionComment.deleteComment et shantytownComment.deleteComment délèguent
    désormais leur logique commune à runDeleteComment. Chaque service conserve
    ses propres imports (modèles, mails, permissionUtils, dateUtils, validator)
    pour ne pas casser l'interception des mocks rewiremock utilisée par leurs
    tests, et ne garde que ce qui lui est propre : construction de la location,
    des variables d'email et refetch/enrichissement final des commentaires
    restants.

--- packages/api/server/services/shantytownComment/deleteComment.ts
+++ packages/api/server/services/shantytownComment/deleteComment.ts
@@ -1,14 +1,12 @@
-import validator from 'validator';
+import { trim } from 'validator';
 import shantytownModel from '#server/models/shantytownModel';
 import shantytownCommentModel from '#server/models/shantytownCommentModel';
 import userModel from '#server/models/userModel';
 import mails from '#server/mails/mails';
 import permissionUtils from '#server/utils/permission';
 import dateUtils from '#server/utils/date';
-import ServiceError from '#server/errors/ServiceError';
-import { Location } from '#server/models/geoModel/Location.d';
 import { AuthUser } from '#server/middlewares/authMiddleware';
-import { User } from '#root/types/resources/User.d';
+import runDeleteComment from '#server/utils/comment/runDeleteComment';
 import { ShantytownEnrichedComment } from '#root/types/resources/ShantytownCommentEnriched.d';
 import { Shantytown } from '#root/types/resources/Shantytown.d';
 import { ShantytownRawComment } from '#root/types/resources/ShantytownCommentRaw.d';
@@ -18,58 +16,33 @@ const { fromTsToFormat: tsToString } = dateUtils;
 
 export default async function deleteComment(user: AuthUser, shantytownId: number, commentId: number, deletionMessage: string): Promise<{ comments: ShantytownEnrichedComment[] }> {
     let town: Shantytown;
-    try {
-        town = await shantytownModel.findOne(user, shantytownId);
-    } catch (error) {
-        throw new ServiceError('fetch_failed', error);
-    }
 
-    const comment: ShantytownRawComment = town.comments.find(({ id }) => id === commentId);
-    if (comment === undefined) {
-        throw new ServiceError('fetch_failed', new Error('Le commentaire à supprimer n\'a pas été retrouvé en base de données'));
-    }
-    let author: User;
-    try {
-        author = await userModel.findOne(comment.createdBy.id);
-    } catch (error) {
-        throw new ServiceError('fetch_failed', error);
-    }
-    const location: Location = {
-        type: 'city',
-        region: town.region,
-        departement: town.departement,
-        epci: town.epci,
-        city: town.city,
-    };
-
-    const isOwner = author.id === user.id;
-    if (!isOwner && !permissionUtils.can(user).do('moderate', 'data').on(location)) {
-        throw new ServiceError('permission_denied', new Error('Vous n\'avez pas accès à ces données'));
-    }
-
-    let message: string;
-    if (!isOwner) {
-        message = validator.trim(deletionMessage ?? '');
-        if (message === '') {
-            throw new ServiceError('data_incomplete', new Error('Vous devez préciser le motif de suppression du commentaire'));
-        }
-    }
-
-    try {
-        await shantytownCommentModel.deleteComment(commentId);
-    } catch (error) {
-        throw new ServiceError('delete_failed', error);
-    }
-
-    try {
-        if (!isOwner) {
-            const nationalAdmins: User[] = await userModel.getNationalAdmins();
-            const mailVariables: { [key: string]: any } = {
+    await runDeleteComment<Shantytown, ShantytownRawComment>(
+        user.id,
+        deletionMessage,
+        {
+            fetchEntity: async () => {
+                town = await shantytownModel.findOne(user, shantytownId);
+                return town;
+            },
+            findComment: shantytown => shantytown.comments.find(({ id }) => id === commentId),
+            fetchAuthor: authorId => userModel.findOne(authorId),
+            buildLocation: shantytown => ({
+                type: 'city',
+                region: shantytown.region,
+                departement: shantytown.departement,
+                epci: shantytown.epci,
+                city: shantytown.city,
+            }),
+            checkPermission: location => permissionUtils.can(user).do('moderate', 'data').on(location),
+            sanitizeDeletionMessage: message => trim(message ?? ''),
+            persistDelete: () => shantytownCommentModel.deleteComment(commentId),
+            buildMailVariables: (shantytown, comment, message) => ({
                 entity: {
                     type: 'le site',
-                    name: town.usename,
+                    name: shantytown.usename,
                     location: {
-                        name: town.city.name,
+                        name: shantytown.city.name,
                     },
                 },
                 comment: {
@@ -77,25 +50,24 @@ export default async function deleteComment(user: AuthUser, shantytownId: number
                     created_at: tsToString(comment.createdAt, 'd/m/Y'),
                 },
                 message,
-            };
-            if (author.status === 'active') {
-                await mails.sendUserCommentDeletion(author, {
-                    variables: mailVariables,
-                    bcc: nationalAdmins,
-                });
-            } else {
-                await Promise.all(nationalAdmins.map(nationalAdmin => mails.sendAdminCommentDeletion(nationalAdmin, {
-                    variables: mailVariables,
-                    preserveRecipient: false,
-                })));
-            }
-        }
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-    }
+            }),
+            sendMail: async (author, mailVariables, nationalAdmins) => {
+                if (author.status === 'active') {
+                    await mails.sendUserCommentDeletion(author, {
+                        variables: mailVariables,
+                        bcc: nationalAdmins,
+                    });
+                } else {
+                    await Promise.all(nationalAdmins.map(async nationalAdmin => mails.sendAdminCommentDeletion(nationalAdmin, {
+                        variables: mailVariables,
+                        preserveRecipient: false,
+                    })));
+                }
+            },
+            getNationalAdmins: () => userModel.getNationalAdmins(),
+        },
+    );
 
-    // on retourne la liste mise à jour des commentaires du site
     let commentsWithEnrichedAttachments: ShantytownEnrichedComment[] = [];
     try {
         const rawComments: ShantytownRawComment[] = town.comments.filter(({ id }) => id !== commentId);
