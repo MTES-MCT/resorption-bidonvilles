8c7f8fca56ad2701454b052857137ee91aba421a
Gilles COGNIN <gillescognin@gmail.com>
2026-07-15 16:37:05 +0200

    fix: Correction et intégration du typage ERRORS

--- packages/api/server/controllers/shantytown/addFavorite/shantytown.addFavorite.ts
+++ packages/api/server/controllers/shantytown/addFavorite/shantytown.addFavorite.ts
@@ -1,11 +1,12 @@
 import userFavoriteShantytown from '#server/services/userFavoriteShantytown';
 import { type Response, type NextFunction } from 'express';
+import { ControllerErrors } from '#server/errors/ControllerErrors';
 import { ManageFavoriteRequest } from '#root/types/resources/ManageFavoriteRequestInterface.d';
 
-const ERRORS = {
-    permission_denied: { status: 403, message: 'Vous n\'avez pas les droits pour effectuer cette action' },
-    write_failed: { status: 500, message: 'Une erreur est survenue lors de l\'ajout du site épinglé' },
-    undefined: { status: 500, message: 'Une erreur inconnue est survenue' },
+const ERRORS: ControllerErrors = {
+    permission_denied: { code: 403, message: 'Vous n\'avez pas les droits pour effectuer cette action' },
+    write_failed: { code: 500, message: 'Une erreur est survenue lors de l\'ajout du site épinglé' },
+    undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
 };
 
 export default async function addFavoriteController(req: ManageFavoriteRequest, res: Response, next: NextFunction) {
@@ -13,8 +14,8 @@ export default async function addFavoriteController(req: ManageFavoriteRequest, 
         await userFavoriteShantytown.add(req.user, Number.parseInt(req.params.id, 10));
         return res.status(200).json({});
     } catch (error) {
-        const { status, message } = ERRORS[error?.code] ?? ERRORS.undefined;
-        res.status(status).send({ user_message: message });
+        const { code, message } = ERRORS[error?.code] ?? ERRORS.undefined;
+        res.status(code).send({ user_message: message });
         return next(error.nativeError ?? error);
     }
 }
--- packages/api/server/controllers/user/listFavorites/user.listFavorites.ts
+++ packages/api/server/controllers/user/listFavorites/user.listFavorites.ts
@@ -1,15 +1,16 @@
 import userFavoriteShantytown from '#server/services/userFavoriteShantytown';
 import { type Request, type Response, type NextFunction } from 'express';
+import { ControllerErrors } from '#server/errors/ControllerErrors';
 import { AuthUser } from '#server/middlewares/authMiddleware';
 
 interface ListFavoritesRequest extends Request {
     user: AuthUser;
 }
 
-const ERRORS = {
-    permission_denied: { status: 403, message: 'Vous n\'avez pas les droits pour effectuer cette action' },
-    fetch_failed: { status: 500, message: 'Une erreur est survenue lors de la récupération des sites épinglés' },
-    undefined: { status: 500, message: 'Une erreur inconnue est survenue' },
+const ERRORS: ControllerErrors = {
+    permission_denied: { code: 403, message: 'Vous n\'avez pas les droits pour effectuer cette action' },
+    fetch_failed: { code: 500, message: 'Une erreur est survenue lors de la récupération des sites épinglés' },
+    undefined: { code: 500, message: 'Une erreur inconnue est survenue' },
 };
 
 export default async function listUserFavoritesController(req: ListFavoritesRequest, res: Response, next: NextFunction) {
@@ -17,8 +18,8 @@ export default async function listUserFavoritesController(req: ListFavoritesRequ
         const towns = await userFavoriteShantytown.fetch(req.user);
         return res.status(200).json(towns);
     } catch (error) {
-        const { status, message } = ERRORS[error?.code] ?? ERRORS.undefined;
-        res.status(status).send({ user_message: message });
+        const { code, message } = ERRORS[error?.code] ?? ERRORS.undefined;
+        res.status(code).send({ user_message: message });
         return next(error.nativeError ?? error);
     }
 }
