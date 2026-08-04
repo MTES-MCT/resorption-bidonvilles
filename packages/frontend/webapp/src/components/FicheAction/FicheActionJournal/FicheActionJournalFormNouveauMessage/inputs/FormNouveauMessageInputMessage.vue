6738415a929a177336cb6de2d28e9730fb9d2bc9
Gilles COGNIN <gillescognin@gmail.com>
2026-08-04 15:57:12 +0200

    feat: Ajout de la fonctionnalité en front

--- packages/frontend/webapp/src/components/FicheAction/FicheActionJournal/FicheActionJournalFormNouveauMessage/inputs/FormNouveauMessageInputMessage.vue
+++ packages/frontend/webapp/src/components/FicheAction/FicheActionJournal/FicheActionJournalFormNouveauMessage/inputs/FormNouveauMessageInputMessage.vue
@@ -1,6 +1,7 @@
 <template>
     <TextArea
-        :rows="5"
+        ref="textarea"
+        :rows="rows"
         id="comment"
         :label="labels.comment"
         placeholder="Partagez vos informations concernant l'action"
@@ -9,11 +10,28 @@
 </template>
 
 <script setup>
+import { computed, ref, toRefs } from "vue";
 import labels from "../FicheActionJournalFormNouveauMessage.labels";
 import { TextArea } from "@resorptionbidonvilles/ui";
 
+const props = defineProps({
+    rows: Number,
+});
+const { rows } = toRefs(props);
+const textarea = ref(null);
+
 const emit = defineEmits(["paste"]);
+
+const isFocused = computed(() => {
+    return textarea.value?.isFocused;
+});
+
 function onPaste(...args) {
     emit("paste", ...args);
 }
+
+defineExpose({
+    isFocused,
+    focus: (...args) => textarea.value.focus(...args),
+});
 </script>
