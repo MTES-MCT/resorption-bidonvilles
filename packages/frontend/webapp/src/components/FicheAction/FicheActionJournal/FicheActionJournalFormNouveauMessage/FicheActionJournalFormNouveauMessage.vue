529bbfb4db376543aae092a7630bc12452a65aa6
Gilles COGNIN <gillescognin@gmail.com>
2026-08-04 15:57:32 +0200

    feat: Refactoring commun actions/sites

--- packages/frontend/webapp/src/components/FicheAction/FicheActionJournal/FicheActionJournalFormNouveauMessage/FicheActionJournalFormNouveauMessage.vue
+++ packages/frontend/webapp/src/components/FicheAction/FicheActionJournal/FicheActionJournalFormNouveauMessage/FicheActionJournalFormNouveauMessage.vue
@@ -3,55 +3,113 @@
         <h1 class="font-bold text-lg">Partager une info</h1>
 
         <DragZone class="bg-white p-6" @drop="attachmentsInput?.addFiles">
-            <FormNouveauMessageInputMessage @paste="onPaste" />
-            <FormNouveauMessageInputAttachments ref="attachmentsInput" />
-
-            <ErrorSummary v-if="error" :message="error" class="mt-2" />
-            <p class="text-right">
-                <DsfrButton
-                    :disabled="isLoading"
-                    :icon="
-                        isLoading
-                            ? {
-                                  name: 'fa-solid:spinner',
-                                  animation: 'spin',
-                              }
-                            : 'fr-icon-send-plane-fill'
-                    "
-                    @click="submit"
-                    >Publier le message</DsfrButton
+            <div ref="interactionRoot">
+                <FormNouveauMessageInputMessage
+                    :rows="rows"
+                    ref="messageInput"
+                    @paste="onPaste"
+                />
+                <div
+                    class="transition-height h-0 overflow-y-hidden"
+                    ref="formContainer"
                 >
-            </p>
+                    <FormNouveauMessageInputAttachments
+                        ref="attachmentsInput"
+                    />
+                    <FormNouveauMessageInputMode @click="onModeChange" />
+                    <FormNouveauMessageInputTarget
+                        v-show="values.mode === 'custom'"
+                        :departement="action?.location?.departement?.code"
+                    />
+                    <p class="text-sm mb-4">
+                        (*) Quelle que soit l'option retenue, les
+                        administrateurs locaux et nationaux auront accès au
+                        message à des fins de modération
+                    </p>
+
+                    <ErrorSummary v-if="error" :message="error" class="mt-2" />
+                    <p class="text-right">
+                        <DsfrButton
+                            :disabled="isLoading"
+                            :icon="
+                                isLoading
+                                    ? {
+                                          name: 'fa-solid:spinner',
+                                          animation: 'spin',
+                                      }
+                                    : 'fr-icon-send-plane-fill'
+                            "
+                            @click="submit"
+                            >Publier le message</DsfrButton
+                        >
+                    </p>
+                </div>
+            </div>
         </DragZone>
     </form>
 </template>
 
+<style scoped>
+.transition-height {
+    transition-property: height;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 300ms;
+}
+</style>
+
 <script setup>
-import { defineProps, toRefs, ref } from "vue";
+import { ref, toRefs } from "vue";
 import { useForm } from "vee-validate";
 import { useActionsStore } from "@/stores/actions.store";
 import schema from "./FicheActionJournalFormNouveauMessage.schema";
 import getFileFromPasteEvent from "@/utils/getFileFromPasteEvent";
+import useJournalMessageFormDropdown from "@/composables/useJournalMessageFormDropdown";
 
 import { ErrorSummary } from "@resorptionbidonvilles/ui";
 import DragZone from "@/components/DragZone/DragZone.vue";
 import FormNouveauMessageInputMessage from "./inputs/FormNouveauMessageInputMessage.vue";
 import FormNouveauMessageInputAttachments from "./inputs/FormNouveauMessageInputAttachments.vue";
+import FormNouveauMessageInputMode from "./inputs/FormNouveauMessageInputMode.vue";
+import FormNouveauMessageInputTarget from "./inputs/FormNouveauMessageInputTarget.vue";
 
 const props = defineProps({
     action: Object,
 });
 const { action } = toRefs(props);
+const interactionRoot = ref(null);
+const formContainer = ref(null);
 const attachmentsInput = ref(null);
+const messageInput = ref(null);
 const isLoading = ref(false);
 
-const { handleSubmit, setErrors, resetForm } = useForm({
-    validationSchema: schema,
-    initialValues: {
-        comment: "",
-        attachments: new DataTransfer().files,
+const initialValues = {
+    comment: "",
+    mode: "public",
+    target: {
+        organizations: [],
+        users: [],
     },
+    attachments: new DataTransfer().files,
+};
+
+const { handleSubmit, setErrors, resetForm, values } = useForm({
+    validationSchema: schema,
+    initialValues,
 });
+
+const { rows, setAutoHeight } = useJournalMessageFormDropdown({
+    values,
+    initialValues,
+    resetForm,
+    messageInput,
+    attachmentsInput,
+    formContainer,
+    interactionRoot,
+});
+
+function onModeChange() {
+    setAutoHeight();
+}
 
 function onPaste(event) {
     const file = getFileFromPasteEvent(event);
@@ -70,7 +128,13 @@ const submit = handleSubmit(async (values) => {
 
         await actionsStore.addComment(
             action.value.id,
-            { description: values.comment },
+            {
+                description: values.comment,
+                targets: {
+                    mode: values.mode,
+                    ...values.target,
+                },
+            },
             values.attachments
         );
 
