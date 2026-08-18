c963b8ae96f6ae8491785be357715e76b0b2cb41
Christophe BENARD <ch.benard@gmail.com>
2026-08-18 09:21:58 +0200

    refactor(api): nommer la fonction fléchée refers to https://trello.com/c/8t3PQzIB/2749

--- packages/api/server/controllers/location/epciDepartements/location.epciDepartements.route.ts
+++ packages/api/server/controllers/location/epciDepartements/location.epciDepartements.route.ts
@@ -1,9 +1,9 @@
 import { type ApplicationWithCustomRoutes } from '#server/loaders/customRouteMethodsLoader';
 import controller from './location.epciDepartements';
 
-export default (app: ApplicationWithCustomRoutes): void => {
+export default function locationEpciGetEpciDepartements(app: ApplicationWithCustomRoutes): void {
     app.customRoutes.get('/locations/epci/:code/departements', controller, undefined, {
         authenticate: false,
         multipart: false,
     });
-};
+}
