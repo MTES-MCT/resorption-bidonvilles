3e4800ccef66805cd9449cac12b7fde2d94f819c
Christophe BENARD <ch.benard@gmail.com>
2026-09-08 14:31:27 +0200

    feat(api): capturer les erreurs d'envoi de mail non gérées via sentry refers to https://trello.com/c/gNejZ0C2/2752

--- packages/api/server/utils/sendMailsWithConcurrencyLimit.ts
+++ packages/api/server/utils/sendMailsWithConcurrencyLimit.ts
@@ -1,18 +1,20 @@
+import * as Sentry from '@sentry/node';
 import PromisePool from '@supercharge/promise-pool';
 import config from '#server/config';
+
+const defaultOnError = <T>(error: Error, item: T): void => {
+    Sentry.captureException(error, { extra: { item } });
+};
 
 const sendMailsWithConcurrencyLimit = async <T>(
     items: T[],
     sendOne: (item: T) => Promise<any>,
-    onError: (error: Error, item: T) => void = null,
+    onError: (error: Error, item: T) => void = defaultOnError,
 ): Promise<void> => {
-    let pool = PromisePool
+    const pool = PromisePool
         .for(items)
-        .withConcurrency(config.mail.sendConcurrency);
-
-    if (onError !== null) {
-        pool = pool.handleError(onError);
-    }
+        .withConcurrency(config.mail.sendConcurrency)
+        .handleError(onError);
 
     await pool.process(sendOne);
 };
