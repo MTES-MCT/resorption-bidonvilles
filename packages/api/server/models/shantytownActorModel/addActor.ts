324637888118f2c3863325f8ab1bb6cc47bb6234
Christophe BENARD <ch.benard@gmail.com>
2026-09-09 10:56:30 +0200

    refactor(api): typer les paramètres des requêtes sur les thèmes d'intervenant refers to https://trello.com/c/fuWB0RlZ/2763
    Ajoute le type ActorTheme/ProcessedActorThemes et type explicitement les
    paramètres de processThemes, addActor et updateThemes (shantytownId,
    userId, themes, createdBy/updatedBy, transaction), à la place des
    implicit any. Nomme au passage la fonction fléchée de processThemes.ts.

--- packages/api/server/models/shantytownActorModel/addActor.ts
+++ packages/api/server/models/shantytownActorModel/addActor.ts
@@ -1,7 +1,9 @@
 import { sequelize } from '#db/sequelize';
+import { Transaction } from 'sequelize';
+import { ActorTheme } from '#root/types/resources/ShantytownActor.d';
 import processThemes from './_common/processThemes';
 
-export default (shantytownId, userId, themes, createdBy, transaction = undefined) => {
+export default function addActor(shantytownId: number, userId: number, themes: ActorTheme[], createdBy: number, transaction: Transaction | undefined = undefined) {
     const processedThemes = processThemes(themes);
 
     return sequelize.query(
@@ -30,4 +32,4 @@ export default (shantytownId, userId, themes, createdBy, transaction = undefined
             transaction,
         },
     );
-};
+}
