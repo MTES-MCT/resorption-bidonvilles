cf0396465f3540a738aa58c2dceab5bc8036de7e
Christophe BENARD <ch.benard@gmail.com>
2026-09-09 16:13:12 +0200

    fix(api): vérifier l'appartenance réelle de fullTableName à la whitelist refers to https://trello.com/c/fuWB0RlZ/2763
    Aikido a maintenu l'alerte (déclassée en medium) après le premier
    correctif : une simple vérification de format (schema.table) ne
    "vérifie" pas que la valeur provient effectivement de la whitelist.
    validateFullTableName recalcule maintenant indépendamment
    getAllowedSchemas/getAllowedTables et vérifie une appartenance exacte,
    avec ou sans le suffixe _encrypted. Nomme aussi la fonction fléchée de
    findParcel.ts. Vérifié contre les valeurs réellement produites par
    getFullTableName et contre un payload malveillant.

--- packages/api/server/models/majicModel/findParcel/findParcel.ts
+++ packages/api/server/models/majicModel/findParcel/findParcel.ts
@@ -4,14 +4,14 @@ import { RawParcel } from './RawParcel.d';
 import getParcelTableName from '../common/getFullTableName';
 import validateFullTableName from '../common/validateFullTableName';
 
-export default async (id: string, dept: string, schema: string, shortTableName: string, tableName: string): Promise<RawParcel> => {
+export default async function findParcel(id: string, dept: string, schema: string, shortTableName: string, tableName: string): Promise<RawParcel> {
     const fullTableName = getParcelTableName(dept, schema, shortTableName, tableName);
 
     if (!fullTableName) {
         throw new Error(`Impossible de construire le nom de la table des parcelles pour le département ${dept}`);
     }
 
-    validateFullTableName(fullTableName, dept);
+    validateFullTableName(fullTableName, dept, shortTableName);
 
     const parcelles: RawParcel[] = await sequelize.query(
         `SELECT idpar, idcom, dnupro, dnuvoi, cconvo, dvoilib, idcomtxt
@@ -30,4 +30,4 @@ export default async (id: string, dept: string, schema: string, shortTableName: 
     }
 
     return parcelles[0];
-};
+}
