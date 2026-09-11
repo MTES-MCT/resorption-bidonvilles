f96312aae80ef35f049a2e510d3bc86af362675e
Gilles COGNIN <gillescognin@gmail.com>
2026-09-11 14:16:38 +0200

    design: responsivness des filtres de l'administration

--- packages/frontend/webapp/src/components/ListeLogsConnexion/ListeLogsConnexionFiltres.vue
+++ packages/frontend/webapp/src/components/ListeLogsConnexion/ListeLogsConnexionFiltres.vue
@@ -2,7 +2,7 @@
     <section
         class="flex flex-col sm:flex-row sm:items-end sm:space-x-8 space-y-4 sm:space-y-0 border-t border-b border-france-blue-925-100 py-4"
     >
-        <article class="flex items-center space-x-2 h-10">
+        <article class="flex sm:items-center gap-4 flex-col sm:flex-row">
             <p class="whitespace-nowrap">Filtrer par:</p>
             <DsfrFiltre
                 v-model="statusFilter"
