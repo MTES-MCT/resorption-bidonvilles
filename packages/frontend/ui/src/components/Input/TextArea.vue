1aea3f8f688d00db90a835e1766a276915767df6
Gilles COGNIN <gillescognin@gmail.com>
2026-09-11 10:18:04 +0200

    design: Forçage du fond par défaut DSFR

--- packages/frontend/ui/src/components/Input/TextArea.vue
+++ packages/frontend/ui/src/components/Input/TextArea.vue
@@ -12,7 +12,7 @@
                     :data-cy-field="cypressName"
                     :disabled="isSubmitting || disabled"
                     class="h-40"
-                    :class="['bg-white', classes]"
+                    :class="[classes]"
                     :model-value="field.value"
                     @update:model-value="handleChange"
                     @blur="onBlur"
