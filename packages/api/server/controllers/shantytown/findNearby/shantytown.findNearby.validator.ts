9bddc9b9ba948ebd5c411c98c989ff15accb5a6c
Gilles COGNIN <gillescognin@gmail.com>
2026-09-24 16:41:17 +0200

    chore: correction du linting

--- packages/api/server/controllers/shantytown/findNearby/shantytown.findNearby.validator.ts
+++ packages/api/server/controllers/shantytown/findNearby/shantytown.findNearby.validator.ts
@@ -3,9 +3,13 @@ import { query } from 'express-validator';
 export default [
     query('longitude')
         .notEmpty().bail().withMessage('Le paramètre longitude est obligatoire')
-        .isFloat().bail().withMessage('Le paramètre longitude doit être un nombre décimal'),
+        .isFloat()
+        .bail()
+        .withMessage('Le paramètre longitude doit être un nombre décimal'),
 
     query('latitude')
         .notEmpty().bail().withMessage('Le paramètre latitude est obligatoire')
-        .isFloat().bail().withMessage('Le paramètre latitude doit être un nombre décimal'),
+        .isFloat()
+        .bail()
+        .withMessage('Le paramètre latitude doit être un nombre décimal'),
 ];
